cee35a6a8682832b979b024ef6fd95f696c06b96
Jan Varwig <jan@varwig.org>
2012-11-20 15:58:01 +0100

    first working entry list

--- app/assets/javascripts/controllers/content_controller.js
+++ app/assets/javascripts/controllers/content_controller.js
@@ -1,6 +1,7 @@
 define([
   'controllers',
-], function(controllers, contentTemplate){
+  'templates/entry_list'
+], function(controllers, entryListTemplate){
   'use strict';
 
   return controllers.controller('ContentCtrl', function($scope, client) {
@@ -9,10 +10,30 @@ define([
 
     $scope.switchContentType = function(type){
       $scope.contentType = type;
-    }
+    };
 
     $scope.switchEntrySection = function(section){
       $scope.entrySection = section;
+    };
+
+    function performNav() {
+      if ($scope.contentType == 'entries' ) {
+        if ($scope.entrySection == 'published') {
+          $scope.contentTemplate = entryListTemplate;
+          $scope.bucket.getEntries(function(err, entries){
+            if (!err) {
+              $scope.$apply(function($scope){
+                console.log(entries)
+                $scope.entries = entries;
+              })
+            }
+          });
+        }
+      }
     }
+
+    $scope.$watch('contentType' , performNav);
+    $scope.$watch('entrySection', performNav);
+
   });
 });
