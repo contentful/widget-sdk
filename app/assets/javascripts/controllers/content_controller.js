69baebf85e92c14cfe4987a6546c85f1c08b7b18
Jan Varwig <jan@varwig.org>
2012-11-21 12:33:12 +0100

    EntySection ALL added

--- app/assets/javascripts/controllers/content_controller.js
+++ app/assets/javascripts/controllers/content_controller.js
@@ -6,7 +6,7 @@ define([
 
   return controllers.controller('ContentCtrl', function($scope, client) {
     $scope.contentType = 'entries';
-    $scope.entrySection = 'published'
+    $scope.entrySection = 'all'
 
     $scope.switchContentType = function(type){
       $scope.contentType = type;
@@ -18,7 +18,7 @@ define([
 
     function performNav() {
       if ($scope.contentType == 'entries' ) {
-        if ($scope.entrySection == 'published') {
+        if ($scope.entrySection.match(/all|published/)) {
           $scope.contentTemplate = entryListTemplate;
           reloadEntries();
         }
@@ -27,13 +27,27 @@ define([
 
     function reloadEntries() {
       if ($scope.bucket && $scope.contentType == 'entries') {
-        if ($scope.entrySection == 'published') {
+        if ($scope.entrySection == 'all') {
+          $scope.entries = [];
           $scope.bucket.getEntries(function(err, entries){
-            if (!err) {
-              $scope.$apply(function($scope){
-                $scope.entries = entries;
-              })
-            }
+            if (err) return;
+            $scope.$apply(function($scope){
+              $scope.entries = $scope.entries.concat(entries);
+            })
+          });
+          // $scope.bucket.getDrafts(function(err, drafts){
+          //   if (err) return;
+          //   $scope.$apply(function($scope){
+          //     $scope.entries = $scope.entries.concat(drafts);
+          //   })
+          // });
+        } else if ($scope.entrySection == 'published') {
+          $scope.entries = [];
+          $scope.bucket.getEntries(function(err, entries){
+            if (err) return;
+            $scope.$apply(function($scope){
+              $scope.entries = entries;
+            })
           });
         } else {
           $scope.entries = [];
