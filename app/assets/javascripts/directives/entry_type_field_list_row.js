a5139253ced084644f4ab4fd8bea04373626e0d2
Jan Varwig <jan@varwig.org>
2012-12-18 20:51:04 +0100

    Listen to ShareJS events directly

--- app/assets/javascripts/directives/entry_type_field_list_row.js
+++ app/assets/javascripts/directives/entry_type_field_list_row.js
@@ -28,63 +28,56 @@ define([
             scope.deletable = true;
             scope.field = _(scope.initialField).clone();
 
-            // TODO entweder die ShareJS ops selbst verarbeiten oder auf
-            // den snapshot binden
-
             scope.$watch('publishedIds', function(ids, old, scope) {
-              if (ids)
+              if (ids) {
                 scope.deletable = !_(ids).contains(scope.field.id);
+              }
             });
 
             scope.$watch('sjDoc', function(sjDoc, old, scope) {
               if (old && old !== sjDoc) {
                 scope.field = null;
-                //old.removeListener(scope.childListener);
-                //scope.childListener = null;
+                old.removeListener(scope.childListener);
+                scope.childListener = null;
               }
               if (sjDoc) {
                 scope.field = sjDoc.snapshot.fields[scope.index];
-                //scope.childListener = sjDoc.at(['fields', scope.index]).on('child op', function(path, op) {
-                  //console.log('child op',path, op);
-                //});
-              }
-            });
-
-            scope.$watch('sjDoc', function linkName(sjDoc, old, scope) {
-              if (old && old !== sjDoc) {
-                scope.cleanupTextArea();
-                scope.cleanupTextArea = null;
-                scope.nameDoc = null;
-              }
-              if (sjDoc) {
-                scope.nameDoc  = sjDoc.at(['fields', scope.index, 'name']);
-                scope.cleanupTextArea = scope.nameDoc.attach_textarea(elm.find('.field-name')[0]);
-              }
-            });
-
-            scope.$watch('sjDoc', function linkType(sjDoc, old, scope) {
-              if (old && old !== sjDoc) {
-                scope.typeWatcher();
-                scope.typeWatcher = null;
-                old.removeListener(scope.typeListener);
-                scope.typeListener = null;
-              }
-              if (sjDoc){
-                scope.typeWatcher = scope.$watch('field.type', function(type, old, scope) {
-                  if (type === old) return;
-                  sjDoc.at(['fields', scope.index, 'type']).set(type, function(err) {
-                    if (err) scope.$apply(function(scope) {
-                        scope.field.type = old;
+                scope.childListener = sjDoc.at(['fields']).on('child op', function(path) {
+                  if (path[0] == scope.index){
+                    //console.log('child op',path, op);
+                    if (path[1] === 'disabled' || path[1] == 'type') {
+                      scope.$apply(function(scope) {
+                        scope.field = sjDoc.snapshot.fields[scope.index];
                       });
-                  });
-                });
-                scope.fieldDoc = sjDoc.at(['fields', scope.index]);
-                scope.typeListener = scope.fieldDoc.on('replace', function(position, was, now) {
-                  if (position === 'type') scope.$apply(function(scope) {
-                      scope.field.type = now;
-                    });
+                    }
+                  }
                 });
               }
+            });
+
+            function attachNameField(val, old, scope) {
+              if (old !== val) {
+                if (scope.cleanupTextArea){
+                  scope.cleanupTextArea();
+                  scope.cleanupTextArea = null;
+                }
+              }
+              if (scope.sjDoc && scope.index !== undefined && scope.index !== null) {
+                var nameDoc  = scope.sjDoc.at(['fields', scope.index, 'name']);
+                scope.cleanupTextArea = nameDoc.attach_textarea(elm.find('.field-name')[0]);
+              }
+            }
+
+            scope.$watch('sjDoc', attachNameField);
+            scope.$watch('index', attachNameField);
+
+            scope.$watch('field.type', function(type, old, scope) {
+              if (type === old) return;
+              scope.sjDoc.at(['fields', scope.index, 'type']).set(type, function(err) {
+                if (err) scope.$apply(function(scope) {
+                    scope.field.type = old;
+                  });
+              });
             });
 
             scope.$watch('index', function linkIndex(index, old, scope) {
@@ -106,19 +99,6 @@ define([
                   });
               });
             };
-            scope.$watch('fieldDoc', function(fieldDoc, old, scope) {
-              if (old && old !== fieldDoc) {
-                old.removeListener(scope.disabledListener);
-                scope.disabledListener = null;
-              }
-              if (fieldDoc) {
-                scope.disabledListener = fieldDoc.on('replace', function(position, was, now){
-                  if (position === 'disabled') scope.$apply(function(scope) {
-                      scope.field.disabled = now;
-                    });
-                });
-              }
-            });
 
             scope.delete = function() {
               var event = this.$emit('deleteField');
