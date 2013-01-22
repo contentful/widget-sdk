eccc456c4e5de0ab3a0d5b1d2e0ba6d95b532bef
Jan Varwig <jan@varwig.org>
2013-01-22 10:57:13 +0100

    cfClickLocal directive

---
+++ app/assets/javascripts/directives/cf_click_local.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// A version of ngClick that performs stopPropagation() and
+// preventDefault() to support nested click targets
+angular.module('contentful/directives').directive('cfClickLocal', function($parse){
+  return {
+    restrict: 'A',
+    link: function(scope, element, attr) {
+      var fn = $parse(attr.cfClickLocal);
+      element.bind('click', function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        scope.$apply(function() {
+          fn(scope, {$event:event});
+        });
+      });
+    }
+  };
+});
--- app/assets/javascripts/lib/tab_list.js
+++ app/assets/javascripts/lib/tab_list.js
@@ -115,18 +115,6 @@ TabItem.prototype = {
     this.list.closeTab(this);
   },
 
-  closeAsync: function(){
-    // This is a workaround for a strange behavior when adding ngClick
-    // directives on nested elements (namely the close buttons in the
-    // tab)
-    var self = this;
-    setTimeout(function(){
-      self.list.scope.$apply(function() {
-        self.close();
-      });
-    }, 1);
-  },
-
   canClose: function(){
     return this.list.items.length > 1 && this._canClose;
   },
