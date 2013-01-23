3aada894d3ca138aec10d13a04fb4675ea958db4
Jan Varwig <jan@varwig.org>
2013-01-23 18:13:38 +0100

    SearchResultsPosition directive and css transitions for animating it

---
+++ app/assets/javascripts/directives/search_results_position.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('contentful/directives').directive('searchResultsPosition', function() {
+  return {
+    restrict: 'C',
+    template: JST['search_results_position'],
+    scope: {
+      paginator: '='
+    },
+    link: function(scope, element) {
+      var paginatorTipTop = function(progress) {
+        progress = Math.min(Math.max(progress, 0.2), 0.8);
+        return Math.floor(progress * 100) + '%';
+      };
+
+      scope.$watch('paginator.progress()', function(progress) {
+        element.find('.fill').css({height: progress*100+'%'});
+        element.find('.tip').css({top: paginatorTipTop(progress)});
+      });
+    }
+  };
+});
+
+
