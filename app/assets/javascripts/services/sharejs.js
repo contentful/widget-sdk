17e1409a06d17e2670feded4a69a7ab077f2e21d
Stephan Seidt <evilhackerdude@gmail.com>
2013-02-07 11:39:49 +0100

    Pass fake token as authentication parameter to sharejs client wrapper

--- app/assets/javascripts/services/sharejs.js
+++ app/assets/javascripts/services/sharejs.js
@@ -1,15 +1,20 @@
 'use strict';
 
 function ShareJSProvider() {
+  var authentication = 'faketoken';
   var sharejs = require('contentful_client/sharejs');
   var url = document.location.protocol + '//' + document.location.hostname + ':8000/channel';
+
+  this.authentication = function(e) {
+    authentication = e;
+  };
 
   this.url = function(e) {
     url = e;
   };
 
   this.$get = function() {
-    return new sharejs.Client(url);
+    return new sharejs.Client(url, authentication);
   };
 }
 
