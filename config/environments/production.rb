1baab72e154b910af193e3193a7e5561f9199d19
hungryblank <hungryblank+github@gmail.com>
2013-03-14 13:22:07 +0100

    don't set Uglifier preference if is not loaded

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,9 @@ Contentful::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.assets.js_compressor = Uglifier.new(:mangle => false)
+  if defined?(Uglifier)
+    config.assets.js_compressor = Uglifier.new(:mangle => false)
+  end
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
