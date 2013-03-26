82e794eb833e6acbd33b1da9b3a4268ca0e9606b
Stephan Seidt <evilhackerdude@gmail.com>
2013-03-26 19:24:08 +0100

    Define angular module if available

--- app/assets/commonjs_modules/user_interface/index.js
+++ app/assets/commonjs_modules/user_interface/index.js
@@ -9,3 +9,11 @@ module.exports = UserInterface;
 if (window) {
   window.UserInterface = UserInterface;
 }
+
+if (angular) {
+  angular.module('contentful/user_interface', []).
+    value('contentfulClient', UserInterface.client).
+    value('validation', UserInterface.validation).
+    value('worf', UserInterface.worf);
+}
+
