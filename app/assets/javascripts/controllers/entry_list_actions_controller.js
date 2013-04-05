e5baa8871eb61af3b22b40a0c134af8d82cb8a23
Jan Varwig <jan@varwig.org>
2013-04-05 12:16:52 +0200

    Improved button behavior for Action buttons und EntryList

--- app/assets/javascripts/controllers/entry_list_actions_controller.js
+++ app/assets/javascripts/controllers/entry_list_actions_controller.js
@@ -2,6 +2,25 @@
 
 angular.module('contentful/controllers').controller('EntryListActionsCtrl', function EntryListCtrl($scope) {
 
+  var _cacheSelected;
+
+  // At the beginning of every digest cycle destroy the cache of selected entries
+  $scope.$watch(function () { _cacheSelected = null; });
+
+  var getSelected = function () {
+    // Memoize result of getSelected call for duration of cycle
+    if (_cacheSelected === null || _cacheSelected === undefined) {
+      _cacheSelected = $scope.selection.getSelected($scope.entries);
+    }
+    return _cacheSelected;
+  };
+
+  var every = function (predicate) {
+    return _.every(getSelected(), function (entry) {
+      return entry[predicate]();
+    });
+  };
+
   var forAllEntries = function(callback) {
     var entries = $scope.selection.getSelected();
     _.each(entries, callback);
@@ -43,5 +62,40 @@ angular.module('contentful/controllers').controller('EntryListActionsCtrl', func
 
   $scope.unarchiveSelected = function() {
     perform('unarchive');
+  };
+
+  $scope.showDelete = function () {
+    return every('canDelete');
+  };
+
+  $scope.showArchive = function () {
+    return every('canArchive');
+  };
+
+  $scope.showUnarchive = function () {
+    return every('canUnarchive');
+  };
+
+  $scope.showUnpublish = function () {
+    return every('canUnpublish');
+  };
+
+  $scope.showPublish = function () {
+    return every('canPublish');
+  };
+
+  $scope.publishButtonName = function () {
+    var published = 0;
+    var unpublished = 0;
+    _.each(getSelected(), function (entry) {
+      if (entry.isPublished()) {
+        published++;
+      } else {
+        unpublished++;
+      }
+    });
+    if (  published === 0) return 'Publish';
+    if (unpublished === 0) return 'Republish';
+    return '(Re)publish';
   };
 });
