1e23288f645f5b71a4c2def6b34f4969ad9c7413
Stephan Seidt <evilhackerdude@gmail.com>
2013-04-08 18:28:01 +0200

    Create entity info panel directive, use in both editors
    [Delivers #47429707]
    [Delivers #47431003]

---
+++ app/assets/javascripts/directives/entity_info_panel.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('contentful/directives').
+  directive('entityInfoPanel', function() {
+    return {
+      restrict: 'C',
+      controller: function EntityInfoPanelCtrl($scope) {
+        $scope.$watch('entry', function(entry) {
+          if (!entry) return;
+          $scope.entryTypeName = $scope.bucketContext.typeForEntry(entry).data.name;
+        });
+
+        $scope.$watch('doc.snapshot.sys', function(sys) {
+          $scope.sys = sys;
+        });
+      },
+      template: JST['entity_info_panel']
+    };
+  });
+
