0b77005c88d16b45f33133d89636878ed5c57d4c
Jan Varwig <jan@varwig.org>
2013-04-08 13:31:37 +0200

    Added missing image type

--- app/assets/javascripts/classes/available_field_types.js
+++ app/assets/javascripts/classes/available_field_types.js
@@ -1,27 +1,27 @@
-'use strict';
+angular.module('contentful/classes').
+  constant('availableFieldTypes', [
+    {name: 'Text'           , value: {type: 'text'                            }},
+    {name: 'Symbol'         , value: {type: 'string'                          }},
+    {name: 'Integer'        , value: {type: 'integer'                         }},
+    {name: 'Floating-point' , value: {type: 'number'                          }},
+    {name: 'Yes/No'         , value: {type: 'boolean'                         }},
+    {name: 'Date/Time'      , value: {type: 'date'                            }},
+    {name: 'Object'         , value: {type: 'object'                          }},
+    {name: 'Link to Entry'  , value: {type: 'link'                            }},
+    {name: 'List of Entries', value: {type: 'array' , items: {type: 'link'  } }},
+    {name: 'List of Strings', value: {type: 'array' , items: {type: 'string'} }},
+    {name: 'Location'       , value: {type: 'location'                        }}
+  ]).
+  factory('getFieldTypeName', function(availableFieldTypes) {
+    'use strict';
 
-var availableFieldTypes = [
-  {name: 'Text'           , value: {type: 'text'                            }},
-  {name: 'Symbol'         , value: {type: 'string'                          }},
-  {name: 'Integer'        , value: {type: 'integer'                         }},
-  {name: 'Floating-point' , value: {type: 'number'                          }},
-  {name: 'Yes/No'         , value: {type: 'boolean'                         }},
-  {name: 'Date/Time'      , value: {type: 'date'                            }},
-  {name: 'Object'         , value: {type: 'object'                          }},
-  {name: 'List of Entries', value: {type: 'array' , items: {type: 'link'  } }},
-  {name: 'List of Strings', value: {type: 'array' , items: {type: 'string'} }},
-  {name: 'Location'       , value: {type: 'location'                        }}
-];
-
-angular.module('contentful/classes', []).
-  constant('availableFieldTypes', availableFieldTypes).
-  constant('getFieldTypeName', function(field) {
-    return _.find(availableFieldTypes, function(availableFieldType) {
-      if (field.type !== availableFieldType.value.type)
-        return false;
-      if (field.items && field.items.type !== availableFieldType.value.items.type)
-        return false;
-      return true;
-    }).name;
+    return function(field) {
+      return _.find(availableFieldTypes, function(availableFieldType) {
+        if (field.type !== availableFieldType.value.type)
+          return false;
+        if (field.items && field.items.type !== availableFieldType.value.items.type)
+          return false;
+        return true;
+      }).name;
+    };
   });
-
