461c7abd93485ed82f7ed191e8093b0ee4e5a42b
Jan Varwig <jan@varwig.org>
2013-04-10 17:00:46 +0200

    Removed the doc watcher

--- app/assets/javascripts/controllers/entry_type_field_settings_controller.js
+++ app/assets/javascripts/controllers/entry_type_field_settings_controller.js
@@ -9,33 +9,6 @@ angular.module('contentful/controllers').controller('EntryTypeFieldSettingsCtrl'
     }
   });
 
-  $scope.$watch('doc', function(doc, old, scope) {
-    if (old && old !== doc) {
-      scope.field = null;
-      old.removeListener(scope.childListener);
-      scope.childListener = null;
-    }
-    if (doc) {
-      scope.field = doc.snapshot.fields[scope.index];
-      scope.childListener = doc.at([]).on('child op', function(path, op) {
-        if (path[0] === 'fields' && path[1] === scope.index) {
-          //console.log('child op applying at', scope.index, path, op);
-          if (path[2] === 'disabled' || path[2] == 'type') {
-            scope.$apply(function(scope) {
-              scope.field = doc.snapshot.fields[scope.index];
-            });
-          } else if (path.length == 2 && op.ld) {
-            scope.$destroy();
-          }
-        }
-      });
-    }
-  });
-
-  $scope.$on('otRemoteOp', function (event, op) {
-    console.log('fieldListRowController received op', op, event);
-  });
-
   $scope.enable = function() {
     this.doc.at(['fields', this.index, 'disabled']).set(false, function(err) {
       if (!err) $scope.$apply(function(scope) {
@@ -43,6 +16,7 @@ angular.module('contentful/controllers').controller('EntryTypeFieldSettingsCtrl'
       });
     });
   };
+
   $scope.disable = function() {
     this.doc.at(['fields', this.index, 'disabled']).set(true, function(err) {
       if (!err) $scope.$apply(function(scope) {
