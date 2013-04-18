24527dcd9d7cdb9fed0de555f312996a64ce4575
Stephan Seidt <evilhackerdude@gmail.com>
2013-04-18 14:27:24 +0200

    Hide tooltips when its element is ng-disabled
    [Delivers #47429569]

--- app/assets/javascripts/directives/tooltip.js
+++ app/assets/javascripts/directives/tooltip.js
@@ -4,9 +4,13 @@ angular.module('contentful/directives').
   directive('title', function() {
     return {
       restrict: 'A',
-      link: function(scope, element) {
+      link: function(scope, element, attr) {
         $(element).tooltip({
           delay: {show: 500, hide: 100}
+        });
+
+        scope.$watch(attr.ngDisabled, function(disabled) {
+          if (disabled) $(element).tooltip('hide');
         });
       }
     };
