42a75c4de60e1a173e1616b784eb58189ae3ae8a
Jan Varwig <jan@varwig.org>
2013-04-25 13:22:51 +0200

    set asset pipeline back to normal settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,10 +6,6 @@ Contentful::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  if defined?(Uglifier)
-    config.assets.js_compressor = Uglifier.new(:mangle => false, :output => {:max_line_len => 200})
-  end
-
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
@@ -27,8 +23,8 @@ Contentful::Application.configure do
   config.action_dispatch.best_standards_support = :builtin
 
   # Do not compress assets
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = false
+  config.assets.debug = true
 end
