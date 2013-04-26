4f348cfb33a4fcac185233975aedd1c0e354a16b
Jan Varwig <jan@varwig.org>
2013-04-26 11:35:40 +0200

    Fixed a bug in cfFieldEditor where changes to value weren't properly written back to the entry

--- app/assets/javascripts/directives/cf_field_editor.js
+++ app/assets/javascripts/directives/cf_field_editor.js
@@ -13,7 +13,7 @@ angular.module('contentful/directives').directive('cfFieldEditor', function(widg
       // Necessary because the widgets can't access the entry directly, only the value variable
       scope.$watch('value', function (value, old, scope) {
         if (value === old) return;
-        scope.entry[scope.field.id][scope.locale.code] = value;
+        scope.entry.data.fields[scope.field.id][scope.locale.code] = value;
       });
 
       var widget = widgets.editor(scope.field.type, attr.editor);
