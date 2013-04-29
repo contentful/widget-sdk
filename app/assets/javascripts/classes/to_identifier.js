867f2b2461a91742ed3369448e57684ee505f939
Stephan Seidt <evilhackerdude@gmail.com>
2013-04-29 11:53:56 +0200

    Improve field ID generator
    1. Split into words
    2. Clean up words
    3. Convert to lowerCamelCase
    
    [Delivers front-end portion of #47430993]

--- app/assets/javascripts/classes/to_identifier.js
+++ app/assets/javascripts/classes/to_identifier.js
@@ -3,14 +3,33 @@
 angular.module('contentful/classes').
   constant('toIdentifier', toIdentifier);
 
-function toIdentifier(str) {
-  var words = _.compact(str.split(/[\s\-_]/));
+function capitalize(string) {
+  if (_.isEmpty(string)) return '';
+  return string[0].toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function cleanPrefix(string) {
+  return string.replace(/^[^a-z]+/, function(prefix) {
+    return prefix.toLowerCase().replace(/[0-9]/g, '');
+  });
+}
+
+function splitIntoWords(string) {
+  return _.compact(
+    string
+      .replace(/([a-z])([A-Z])/g, '$1 $2')
+      .replace(/\b([A-Z]+)([A-Z])([a-z])/g, '$1 $2$3')
+      .split(/[\s\-_.)]+/));
+}
+
+function stripInvalidChars(string) {
+  return string.replace(/\W/g, '');
+}
+
+function toIdentifier(string) {
+  var words = splitIntoWords(string).map(stripInvalidChars);
   if (_.isEmpty(words)) return;
   var first = words[0].toLowerCase();
   var rest =  words.slice(1).map(capitalize);
-  return [first].concat(rest).join('');
-}
-
-function capitalize(str) {
-  return str[0].toUpperCase() + str.slice(1).toLowerCase();
+  return cleanPrefix([first].concat(rest).join(''));
 }
