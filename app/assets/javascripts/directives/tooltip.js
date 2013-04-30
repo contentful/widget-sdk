7e7b439f1d844df163feb4708493b1c691f27663
Stephan Seidt <evilhackerdude@gmail.com>
2013-04-30 18:09:45 +0200

    Change tooltip attr names, add tooltips to save statuses
    [Delivers #48993215]

--- app/assets/javascripts/directives/tooltip.js
+++ app/assets/javascripts/directives/tooltip.js
@@ -1,15 +1,20 @@
 'use strict';
 
 angular.module('contentful/directives').
-  directive('title', function() {
+  directive('tooltip', function() {
     return {
       restrict: 'A',
-      link: function(scope, element, attr) {
-        $(element).tooltip({
-          delay: {show: 100, hide: 100}
+      link: function(scope, element, attrs) {
+        attrs.$observe('tooltip', function() {
+          $(element).tooltip('destroy');
+          $(element).tooltip({
+            delay: {show: 100, hide: 100},
+            placement: attrs.tooltipPlacement,
+            title: attrs.tooltip
+          });
         });
 
-        scope.$watch(attr.ngDisabled, function(disabled) {
+        scope.$watch(attrs.ngDisabled, function(disabled) {
           if (disabled) $(element).tooltip('hide');
         });
       }
