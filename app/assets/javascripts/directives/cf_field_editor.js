8f2ed5013be01c8ba6330ae7e9b92895d097d8da
Jan Varwig <jan@varwig.org>
2013-05-02 16:18:01 +0200

    Fix cfFieldEditor value writeback for new fields

--- app/assets/javascripts/directives/cf_field_editor.js
+++ app/assets/javascripts/directives/cf_field_editor.js
@@ -13,7 +13,10 @@ angular.module('contentful/directives').directive('cfFieldEditor', function(widg
       // Necessary because the widgets can't access the entry directly, only the value variable
       scope.$watch('value', function (value, old, scope) {
         if (value === old) return;
-        scope.entry.data.fields[scope.field.id][scope.locale.code] = value;
+        if (!scope.entry.data.fields[scope.field.id]) {
+          scope.entry.data.fields[scope.field.id] = {};
+        }
+          scope.entry.data.fields[scope.field.id][scope.locale.code] = value;
       });
 
       var widget = widgets.editor(scope.field.type, attr.editor);
