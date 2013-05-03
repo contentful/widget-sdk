d1fedfcbc5bb70dc0f1ed27b7fdf210c234c30d9
Stephan Seidt <evilhackerdude@gmail.com>
2013-05-03 13:53:21 +0200

    Use proper cdn_host setting for public api example url

--- app/assets/javascripts/controllers/api_key_editor_controller.js
+++ app/assets/javascripts/controllers/api_key_editor_controller.js
@@ -11,7 +11,7 @@ angular.module('contentful/controllers').
     $scope.$watch('apiKey.data.accessToken', function(accessToken) {
       $scope.exampleUrl =
         'http://' +
-        environment.settings.api_host.replace(/^api/, 'cdn') +
+        environment.settings.cdn_host +
         '/buckets/' +
         $scope.bucketContext.bucket.getId() +
         '/entries?access_token=' +
