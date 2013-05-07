62625a51436687b80b572df687b7e2ceef4dfb01
Stephan Seidt <evilhackerdude@gmail.com>
2013-05-07 18:07:42 +0200

    Look up users

---
+++ app/assets/javascripts/directives/cf_user.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('contentful/directives').
+  directive('cfUser', function() {
+    return {
+      restrict: 'C',
+      controller: function($scope, $attrs, userCache) {
+        $scope.$watch($attrs.link, function(link) {
+          if (!link || $attrs.as) return;
+          userCache.get($scope.bucketContext.bucket, link.sys.id, function(err, user) {
+            if (err) return;
+            $scope.$apply(function() {
+              $scope[$attrs.as] = user;
+            });
+          });
+        }, true);
+      }
+    };
+  });
---
+++ app/assets/javascripts/services/user_cache.js
@@ -0,0 +1,38 @@
+'use strict';
+angular.module('contentful/services').
+  service('userCache', function UserCache($browser) {
+    var cache = [];
+    var inflight = false;
+    var pending = [];
+
+    return {
+      get: function(bucket, id, callback) {
+        if (cache[id])
+          return $browser.defer(function() {
+            callback(null, cache[id]);
+          });
+
+        pending.push(function(err) {
+          if (err)
+            return callback(err);
+          if (!cache[id])
+            return callback(new Error('User not found'));
+          callback(null, cache[id]);
+        });
+
+        if (inflight) return;
+        inflight = true;
+
+        bucket.getUsers(null, function(err, users) {
+          _.forEach(users, function(user) {
+            cache[user.getId()] = user;
+          });
+          _.forEach(pending, function(cb) {
+            cb(err);
+          });
+          pending = [];
+          inflight = false;
+        });
+      }
+    };
+  });
