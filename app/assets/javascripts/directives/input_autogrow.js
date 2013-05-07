700cc63c6ccab73c4a42fb5872561533d155dbad
Stephan Seidt <evilhackerdude@gmail.com>
2013-05-07 13:41:53 +0200

    Use jquery autosize plugin in inputAutogrow
    [Delivers #49009623]

--- app/assets/javascripts/directives/input_autogrow.js
+++ app/assets/javascripts/directives/input_autogrow.js
@@ -4,36 +4,18 @@ angular.module('contentful/directives').
   directive('inputAutogrow', function() {
     return {
       restrict: 'C',
-      link: function(scope, elem) {
-        var lineHeight = 18; // TODO: Read from configuration as reported
-                             //       line-height can be 'auto'
-        var maxLines = 20;
-        var verticalPadding =
-          _.parseInt(elem.css('padding-bottom')) +
-          _.parseInt(elem.css('padding-top'));
+      link: function(scope, element) {
+        element.autosize();
 
-        function adjustHeight() {
-          if (!elem.is(':focus')) return resetHeight();
-          var linesHeight = verticalPadding + Math.min(maxLines, numLines()) * lineHeight;
-          elem.height(Math.max(defaultHeight, linesHeight));
-        }
+        element.on('focus', function() {
+          element.trigger('autosize');
+        });
 
-        function numLines() {
-          try {
-            return elem.val().split('\n').length;
-          } catch (e) {
-            return 1;
-          }
-        }
-
-        var defaultHeight = elem.height();
-        function resetHeight() {
-          elem.height(defaultHeight);
-        }
-
-        elem.on('keyup', adjustHeight);
-        elem.on('focus', adjustHeight);
-        elem.on('blur', adjustHeight);
+        scope.$on('otValueChanged', function() {
+          setTimeout(function() {
+            element.trigger('autosize');
+          }, 100);
+        });
       }
     };
   });
