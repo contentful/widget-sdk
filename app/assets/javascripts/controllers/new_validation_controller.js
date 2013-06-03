7620178566548939969b83df1409a36a1c8dbdf5
Jan Varwig <jan@varwig.org>
2013-06-03 16:17:38 +0200

    Validate new validations before they land in the entryType

--- app/assets/javascripts/controllers/new_validation_controller.js
+++ app/assets/javascripts/controllers/new_validation_controller.js
@@ -1,11 +1,38 @@
 'use strict';
 
 angular.module('contentful').controller('NewValidationCtrl', function ($scope, analytics) {
+  var _validationListPath;
   $scope.validation = null;
 
   $scope.prepareNewValidation = function () {
     $scope.validation = {};
     $scope.validation[$scope.newValidationType] = {};
+  };
+
+  $scope.$watch('field', function (field, old, scope) {
+    scope.fieldWithNewValidation = angular.copy(field);
+    if (field.type == 'array') {
+      scope.fieldWithNewValidation.items.validations = [];
+      _validationListPath = ['items', 'validations', 0];
+    } else {
+      scope.fieldWithNewValidation.validations = [];
+      _validationListPath = ['validations', 0];
+    }
+  });
+
+  $scope.$watch('validation', function (validation, old, scope) {
+    var validations;
+    if (scope.field.type == 'array') {
+      validations = scope.fieldWithNewValidation.items.validations;
+    } else {
+      validations = scope.fieldWithNewValidation.validations;
+    }
+    validations.length = 0;
+    validations.push(validation);
+  });
+
+  $scope.validationListPath = function () {
+    return _validationListPath;
   };
 
   $scope.createValidation = function () {
