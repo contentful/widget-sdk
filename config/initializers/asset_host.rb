03596cff28604aece3509cdb3207ec8e6bb380c7
Jan Varwig <jan@varwig.org>
2013-06-06 11:10:16 +0200

    Fix asset host configuration [Delivers #51154717]

--- config/initializers/asset_host.rb
+++ config/initializers/asset_host.rb
@@ -1,1 +1,2 @@
-ActionController::Base.asset_host = "//#{Contentful.config.asset_host}"
+Contentful::Application.config.asset_host = "//#{Contentful.config.asset_host}"
+Contentful::Application.config.action_controller.asset_host = "//#{Contentful.config.asset_host}"
