dfc6b65e5aaaa0400b6972ed1055bda30c52396a
Jan Varwig <jan@varwig.org>
2013-06-17 23:38:51 +0200

    Tutorial Part 2

--- app/assets/javascripts/services/tutorial.js
+++ app/assets/javascripts/services/tutorial.js
@@ -24,10 +24,9 @@ angular.module('contentful').factory('tutorial', function ($compile, notificatio
       };
 
       function contentTypeValid(contentType) {
-        var hasText     = _.any(contentType.data.fields, {type: 'Text'});
-        //var hasObject   = _.any(contentType.data.fields, {type: 'Object'});
-        //var hasDateTime = _.any(contentType.data.fields, {type: 'Date'});
-        return hasText //&& hasObject && hasDateTime;
+        var numText     = _.countBy(contentType.data.fields, {type: 'Text'})['true'];
+        var hasDateTime = _.any(contentType.data.fields, {type: 'Date'});
+        return numText == 2 && hasDateTime;
       }
 
       guiders.createGuider({
@@ -38,13 +37,7 @@ angular.module('contentful').factory('tutorial', function ($compile, notificatio
         description: JST['tutorial_overview'](),
         next: 'contentTypeCreate1',
         overlay: true,
-        width: '70%',
-        onShow: function () {
-          _.delay(function () {
-            tutorialScope.contentTypeDone = _.any(tutorialScope.spaceContext.publishedContentTypes, contentTypeValid);
-            if (!tutorialScope.$$phase) tutorialScope.$apply();
-          }, 10);
-        }
+        width: '70%'
       });
       $compile(angular.element('.guider#overview'))(tutorialScope);
 
@@ -112,7 +105,6 @@ angular.module('contentful').factory('tutorial', function ($compile, notificatio
               guiders.next();
               d();
             });
-
           });
         }
       });
@@ -122,7 +114,7 @@ angular.module('contentful').factory('tutorial', function ($compile, notificatio
         title: 'Now add a name and a description',
         attachTo: '.tab-content:visible [ng-model="contentType.data.name"]',
         position: '3',
-        description: 'This is the name of this type of entry. For example you can call it BlogPosts.',
+        description: 'This is the name of this type of entry. Call it "Blog Post" for now.',
         next: 'contentTypeFields',
         onShow: function () {
           $(this.attachTo).one('blur', function () {
@@ -137,7 +129,7 @@ angular.module('contentful').factory('tutorial', function ($compile, notificatio
         attachTo: '.tab-content:visible .new-field-form .button.new',
         offset: {top: -15, left: 0},
         position: '5',
-        description: 'Here you add the "placeholders" for your content. Add one text, one Object and one Date/Time.',
+        description: 'Here you add the "placeholders" for your content. Add one text field called "Title", one text field called "Content" and one Date/Time field called "Timestamp". You can also add more fields if you like, a location for example.',
         next: 'contentTypeActivate',
         onShow: function () {
           var scope = $(this.attachTo).scope();
@@ -156,6 +148,7 @@ angular.module('contentful').factory('tutorial', function ($compile, notificatio
           });
         }
       });
+
       guiders.createGuider({
         id: 'contentTypeActivate',
         title: 'Now activate your Content Type',
@@ -169,11 +162,13 @@ angular.module('contentful').factory('tutorial', function ($compile, notificatio
           var d1 = scope.$on('newContentTypePublished', function (event, contentType) {
             if (contentType.getId() == scope.contentType.getId()) {
               d1();
+              tutorialScope.contentTypeDone = true;
               guiders.next();
             }
           });
         }
       });
+
       guiders.createGuider({
         id: 'contentTypeDone',
         title: 'Done!',
@@ -182,6 +177,142 @@ angular.module('contentful').factory('tutorial', function ($compile, notificatio
         overlay: true,
         buttons: [{name: 'Next'}]
       });
+
+      guiders.createGuider({
+        id: 'entrySeed',
+        title: 'Preparing example data',
+        description: 'Please hang on',
+        next: 'entryCreate1',
+        onShow: function () {
+          var contentType = _.find(tutorialScope.spaceContext.publishedContentTypes, contentTypeValid);
+          if (!contentType) {
+            tutorialScope.spaceContext.space.createContentType({
+             'fields': [
+              {
+               'id': 'title',
+               'name': 'Title',
+               'type': 'Text',
+               'required': false,
+               'localized': false
+              },
+              {
+               'id': 'content',
+               'name': 'Content',
+               'type': 'Text',
+               'required': false,
+               'localized': false
+              },
+              {
+               'id': 'timestamp',
+               'name': 'Timestamp',
+               'type': 'Date',
+               'required': false,
+               'localized': false
+              }
+             ],
+             'name': 'Blog Post',
+             'sys': {
+              'id': 'blogpost'
+              },
+             'description': 'asdsa'
+            }, function (err, contentType) {
+              if (err) {
+                notification.error('Could not create content type');
+              } else {
+                contentType.publish(contentType.version, function (err, contentType) {
+                  tutorialScope.$apply(function (scope) {
+                    scope.spaceContext.registerPublishedContentType(contentType);
+                    scope.spaceContext.refreshContentTypes();
+                  });
+                  guiders.next();
+                });
+              }
+            });
+          } else {
+            setTimeout(function () {
+              guiders.next();
+            }, 500);
+          }
+        }
+      });
+
+      guiders.createGuider({
+        id: 'entryCreate1',
+        title: 'Click here and add a new Blog Post',
+        attachTo: '.tab-list .add.button',
+        position: '2',
+        description: '"Blog Post" is the Content Type you just created (if you didn\'t name it differently).',
+        next: 'entryCreate2',
+        onShow: function () {
+          $(this.attachTo).one('click', function () {
+            guiders.next();
+          });
+        }
+      });
+
+      guiders.createGuider({
+        id: 'entryCreate2',
+        title: 'Click here and add a new Blog Post',
+        attachTo: '.tab-list .add-btn .dropdown-menu ul.content-types',
+        position: '2',
+        description: '"Blog Post" is the Content Type you just created (if you didn\'t name it differently).',
+        next: 'entryContent',
+        onShow: function () {
+          $(this.attachTo).one('click', function () {
+            var d = spaceScope.$on('otBecameEditable', function () {
+              guiders.next();
+              d();
+            });
+          });
+        }
+      });
+
+      guiders.createGuider({
+        id: 'entryContent',
+        title: 'Now fill all the fields you have',
+        attachTo: '.tab-content:visible textarea:first',
+        position: '2',
+        description: 'Here you can add your content, title, the timestamp and any other fields you have.',
+        buttons: [{
+          name: 'Close',
+          onclick: function () {
+            $('.guider#entryContent').fadeOut('fast');
+          }
+        }],
+        onShow: function () {
+          _.defer(guiders.show, 'entryPublish');
+        }
+      });
+
+      guiders.createGuider({
+        id: 'entryPublish',
+        title: 'When you\'re done, publish your Blog Post',
+        attachTo: '.entry-editor:visible button.publish',
+        offset: {top: 15, left: 0},
+        position: '1',
+        description: 'Publishing makes an entry visible to public users of your content feed.',
+        next: 'entryDone',
+        onShow: function () {
+          var scope = $(this.attachTo).scope();
+          var d1 = scope.$watch('entry.isPublished()', function (published) {
+            if (published) {
+              d1();
+              tutorialScope.entryDone = true;
+              guiders.next();
+            }
+          });
+        }
+      });
+
+      guiders.createGuider({
+        id: 'entryDone',
+        title: 'Done!',
+        description: 'You just published your first entry. In the next tutorial you will learn how to set up access to the public content feed for your Space.',
+        next: 'overview',
+        overlay: true,
+        buttons: [{name: 'Next'}]
+      });
+
     }
 
   };
