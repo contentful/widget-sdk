1fd2b8c9b88e5a7e14445824081adaf966f98d9a
Jan Varwig <jan@varwig.org>
2013-06-25 11:30:15 +0200

    Much improved and more robust handling of missing/invalid links in cfAutocomplete [Delivers #52284641]

--- app/assets/javascripts/controllers/cf_autocomplete_controller.js
+++ app/assets/javascripts/controllers/cf_autocomplete_controller.js
@@ -1,8 +1,10 @@
 'use strict';
 
-angular.module('contentful').controller('cfAutocompleteCtrl', function ($scope, $parse, $attrs, validation, cfSpinner, ShareJS) {
+angular.module('contentful').controller('cfAutocompleteCtrl', function ($scope, $parse, $attrs, validation, cfSpinner, ShareJS, $q) {
   $scope.links = [];
   $scope.linkedEntries = [];
+
+  var entriesCache = {};
 
   var ngModelGet = $parse($attrs.ngModel),
       ngModelSet = ngModelGet.assign;
@@ -39,19 +41,26 @@ angular.module('contentful').controller('cfAutocompleteCtrl', function ($scope, 
       };
     }
 
+    saveEntryInCache(entry);
     if ($attrs.cfAutocomplete === 'entry') {
-      $scope.otChangeValue(link, cb(function(scope){scope.links = [link];}));
+      $scope.otChangeValue(link, cb(function(scope){
+        scope.links = [link];
+      }));
     } else {
       // TODO Build this pattern into mkpath
       if (_.isArray(ShareJS.peek($scope.otDoc, $scope.otPath))) {
-        $scope.otDoc.at($scope.otPath).push(link, cb(function(scope){scope.links.push(link);}));
+        $scope.otDoc.at($scope.otPath).push(link, cb(function(scope){
+          scope.links.push(link);
+        }));
       } else {
-        ShareJS.mkpath($scope.otDoc, $scope.otPath, [link], cb(function(scope){scope.links = [link];}));
+        ShareJS.mkpath($scope.otDoc, $scope.otPath, [link], cb(function(scope){
+          scope.links = [link];
+        }));
       }
     }
   };
 
-  $scope.removeLink = function(entry) {
+  $scope.removeLink = function(index, entry) {
     if ($attrs.cfAutocomplete === 'entry') {
       return $scope.otChangeValue(null, function(err) {
         if (!err) $scope.$apply(function (scope) {
@@ -60,32 +69,52 @@ angular.module('contentful').controller('cfAutocompleteCtrl', function ($scope, 
         });
       });
     } else {
-      var entryIndex = _.reduce($scope.links, function (result, link, index) {
-        return entry.getId() === link.sys.id ? index : result;
-      }, -1);
-      $scope.otDoc.at($scope.otPath.concat(entryIndex)).remove(function (err) {
+      // TODO solve this cleaner, with tombstones? It's bad to have dead entries lying around in the identitymap
+      // Entry was deleted if getId() returns undefined
+      //           vvvvvvvvvvvvv
+      if (entry && entry.getId() && entry.getId() != $scope.links[index].sys.id) throw new Error('Index mismatch!');
+      $scope.otDoc.at($scope.otPath.concat(index)).remove(function (err) {
         if (!err) $scope.$apply(function (scope) {
-          scope.links.splice(entryIndex,1);
+          scope.links.splice(index,1);
           scope.updateModel();
         });
       });
     }
   };
 
-  $scope.$watch('links', function (links, old, scope) {
-    var ids = _.map(links, function (link) { return link.sys.id; });
+  function saveEntryInCache(entry) {
+    if (entry) entriesCache[entry.getId()] = entry;
+  }
 
-    if (ids.length === 0) {
+  function entryFromCache(id) {
+    return entriesCache[id];
+  }
+
+  function lookupEntries(scope, links) {
+    var lookup = $q.defer();
+    var ids        = _.map(links, function (link) { return link.sys.id; });
+    var missingIds = _.reject(ids, function (id) { return !!entryFromCache(id); });
+    scope.spaceContext.space.getEntries({'sys.id[in]': missingIds.join(',')}, function (err, entries) {
+      scope.$apply(function () {
+        if (err) return lookup.reject(err);
+        _.each(entries, saveEntryInCache);
+        lookup.resolve();
+      });
+    });
+    return lookup.promise.then(function () {
+      return _.map(ids, entryFromCache);
+    });
+  }
+
+  $scope.$watch('links', function (links, old, scope) {
+    if (!links || links.length === 0) {
       $scope.linkedEntries = [];
     } else {
       var stopSpinner = cfSpinner.start();
-      scope.spaceContext.space.getEntries({'sys.id[in]': ids.join(',')}, function (err, entries) {
-        entries = _.reduce(entries, function (map, entry) {
-          map[entry.getId()] = entry;
-          return map;
-        }, {} );
-        entries = _.map(links, function (link) { return entries[link.sys.id]; });
-        $scope.$apply(function (scope) { scope.linkedEntries = entries; });
+      lookupEntries(scope, links).then(function (entries) {
+        scope.linkedEntries = entries;
+        stopSpinner();
+      }, function () {
         stopSpinner();
       });
     }
@@ -96,10 +125,18 @@ angular.module('contentful').controller('cfAutocompleteCtrl', function ($scope, 
   });
 
   $scope.linkDescription= function(entry) {
-    if (entry) {
+    if (entry && entry.getId()) {
       return $scope.spaceContext.entryTitle(entry, $scope.locale.code);
     } else {
-      return '(nothing)';
+      return '(Missing entry)';
     }
   };
+
+  $scope.$on('$destroy', function () {
+    entriesCache =
+    ngModelSet =
+    ngModelGet =
+    validations =
+    linkContentTypeValidation = null;
+  });
 });
