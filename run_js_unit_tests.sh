56123cd6e2971204fe23adb1c5926745f81a001a
Tiago Rodrigues <tmcrodrigues@gmail.com>
2013-07-03 18:06:19 +0200

    Added some helpful output to the js test runner

--- run_js_unit_tests.sh
+++ run_js_unit_tests.sh
@@ -1,2 +1,14 @@
-#/bin/sh
-phantomjs run-jasmine.js http://127.0.0.1:8888
+#/bin/bash
+
+curl -I http://127.0.0.1:8888 > /dev/null 2>&1
+if [ $? -gt 0 ] ; then
+  echo "ERROR: Couldn't reach the server. Did you start \`rake jasmine\` ?"
+  exit 1
+fi
+
+if [ -f "$(which phantomjs)" ] ; then
+  phantomjs run-jasmine.js http://127.0.0.1:8888
+else
+  echo "ERROR: You need to install phantomjs"
+  exit 1
+fi
