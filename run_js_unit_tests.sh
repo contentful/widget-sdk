d3563d4eedbed821b75cdeda87f4d5911ecffae9
Tiago Rodrigues <tmcrodrigues@gmail.com>
2013-07-04 10:17:25 +0200

    Specify port on js unit test runner script

--- run_js_unit_tests.sh
+++ run_js_unit_tests.sh
@@ -1,13 +1,24 @@
 #/bin/bash
 
-curl -I http://127.0.0.1:8888 > /dev/null 2>&1
+host=http://127.0.0.1
+
+if [ "$1" != "" ] ; then
+  port=$1
+else
+  port=8112
+fi
+
+echo "Running phantomjs on $host:$port"
+
+curl -I $host:$port> /dev/null 2>&1
 if [ $? -gt 0 ] ; then
-  echo "ERROR: Couldn't reach the server. Did you start \`rake jasmine\` ?"
+  echo "ERROR: Couldn't reach the server. Did you start \`rake jasmine JASMINE_PORT=<port_number>\` ?"
+  echo "Run $0 with a port number as a parameter"
   exit 1
 fi
 
 if [ -f "$(which phantomjs)" ] ; then
-  phantomjs run-jasmine.js http://127.0.0.1:8888
+  phantomjs run-jasmine.js $host:$port
 else
   echo "ERROR: You need to install phantomjs"
   exit 1
