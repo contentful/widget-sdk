9d0d1f0e5cf6de17e176573675c71f53b6b0a678
Jan Varwig <jan@varwig.org>
2013-07-08 17:27:17 +0200

    Reordering of stuff in ot_presence.js

--- app/assets/javascripts/ot/ot_presence.js
+++ app/assets/javascripts/ot/ot_presence.js
@@ -1,17 +1,142 @@
 'use strict';
 
 angular.module('contentful').
+  directive('otDocPresence', function() {
+    return {
+      require: '^otDocFor',
+      controller: 'otDocPresenceCtrl'
+    };
+  }).
 
   value('otPresenceConfig', {
     focusThrottle: 10e3,
     pingTimeout: 60e3
   }).
 
-  directive('otDocPresence', function() {
-    return {
-      require: '^otDocFor',
-      controller: OtDocPresenceCtrl
+  controller('otDocPresenceCtrl', function($scope, $timeout, otPresenceConfig) {
+    var presence = {};
+    var ownPresence = {};
+    var user  = $scope.user.sys.id;
+    var timeout;
+
+    $scope.$watch(function() {
+      return presence;
+    }, function(presence) {
+      $scope.presence = {
+        fields: groupPresenceByField(presence),
+        users: presenceUsers(presence)
+      };
+    }, true);
+
+    function closedHandler() {
+      /*jshint validthis:true */
+      this.shout(['close', user]);
+      $timeout.cancel(timeout);
+    }
+
+    function shoutHandler(shout) {
+      $scope.$apply(function(scope) {
+        var type = shout[0];
+        var from = shout[1];
+
+        if (!presence[from]) presence[from] = {};
+
+        presence[from].shoutedAt = new Date();
+
+        if (type === 'open') {
+          if (ownPresence.focus)
+            scope.otDoc.shout(['focus', user, ownPresence.focus]);
+          else
+            scope.otDoc.shout(['ping', user]);
+          presence[from] = {};
+        }
+
+        if (type === 'ping') {}
+
+        if (type === 'focus') {
+          var id = shout[2];
+          presence[from].focus = id;
+        }
+
+        if (type === 'close')
+          delete presence[from];
+      });
+    }
+
+    $scope.$watch('otDoc', function(doc, old) {
+      if (old) {
+        old.removeListener('closed', closedHandler);
+        old.removeListener('shout', shoutHandler);
+      }
+
+      if (!doc) return;
+
+      doc.shout(['open', user]);
+      doc.on('closed', closedHandler);
+      doc.on('shout', shoutHandler);
+    });
+
+    var lastId;
+    var lastFocus;
+    this.focus = function(id) {
+      var now = new Date();
+      if (id === lastId && now - lastFocus < otPresenceConfig.focusThrottle) return;
+      lastId = id;
+      lastFocus = now;
+      ownPresence.focus = id;
+      var doc = $scope.otDoc;
+      if (!doc) return;
+      doc.shout(['focus', user, id]);
     };
+
+    function removeTimedOutUsers() {
+      var timedOutUsers = _(presence).map(function(p, u) {
+        var timeSinceLastShout = new Date() - p.shoutedAt;
+        var timedOut = timeSinceLastShout > otPresenceConfig.pingTimeout;
+        if (timedOut) return u;
+      }).compact().value();
+
+      _.forEach(timedOutUsers, function(u) {
+        delete presence[u];
+      });
+
+    }
+
+    function doLater() {
+      removeTimedOutUsers();
+      timeout = $timeout(doLater, otPresenceConfig.pingTimeout);
+    }
+
+    doLater();
+
+    $scope.$on('$destroy', function(event) {
+      var scope = event.currentScope;
+      $timeout.cancel(timeout);
+      if (scope.otDoc) {
+        scope.otDoc.removeListener('closed', closedHandler);
+        scope.otDoc.removeListener('shout', shoutHandler);
+      }
+
+    });
+
+    function groupPresenceByField(presence) {
+      return _.reduce(presence, function(fields, userPresence, user) {
+        var field = userPresence.focus;
+        if (!field) return fields;
+        if (!fields[field]) fields[field] = {users: []};
+        fields[field].users.push(toUserLink(user));
+        return fields;
+      }, {});
+    }
+
+    function presenceUsers(presence) {
+      return _(presence).keys().map(toUserLink).value();
+    }
+
+    function toUserLink(id) {
+      return {sys: {type: 'Link', linkType: 'User', id: id}};
+    }
+
   }).
 
   directive('otFieldPresence', function() {
@@ -38,127 +163,3 @@ angular.module('contentful').
     };
   });
 
-function OtDocPresenceCtrl($scope, $timeout, otPresenceConfig) {
-  var presence = {};
-  var ownPresence = {};
-  var user  = $scope.user.sys.id;
-  var timeout;
-
-  $scope.$watch(function() {
-    return presence;
-  }, function(presence) {
-    $scope.presence = {
-      fields: groupPresenceByField(presence),
-      users: presenceUsers(presence)
-    };
-  }, true);
-
-  function closedHandler() {
-    /*jshint validthis:true */
-    this.shout(['close', user]);
-    $timeout.cancel(timeout);
-  }
-
-  function shoutHandler(shout) {
-    $scope.$apply(function(scope) {
-      var type = shout[0];
-      var from = shout[1];
-
-      if (!presence[from]) presence[from] = {};
-
-      presence[from].shoutedAt = new Date();
-
-      if (type === 'open') {
-        if (ownPresence.focus)
-          scope.otDoc.shout(['focus', user, ownPresence.focus]);
-        else
-          scope.otDoc.shout(['ping', user]);
-        presence[from] = {};
-      }
-
-      if (type === 'ping') {}
-
-      if (type === 'focus') {
-        var id = shout[2];
-        presence[from].focus = id;
-      }
-
-      if (type === 'close')
-        delete presence[from];
-    });
-  }
-
-  $scope.$watch('otDoc', function(doc, old) {
-    if (old) {
-      old.removeListener('closed', closedHandler);
-      old.removeListener('shout', shoutHandler);
-    }
-
-    if (!doc) return;
-
-    doc.shout(['open', user]);
-    doc.on('closed', closedHandler);
-    doc.on('shout', shoutHandler);
-  });
-
-  var lastId;
-  var lastFocus;
-  this.focus = function(id) {
-    var now = new Date();
-    if (id === lastId && now - lastFocus < otPresenceConfig.focusThrottle) return;
-    lastId = id;
-    lastFocus = now;
-    ownPresence.focus = id;
-    var doc = $scope.otDoc;
-    if (!doc) return;
-    doc.shout(['focus', user, id]);
-  };
-
-  function removeTimedOutUsers() {
-    var timedOutUsers = _(presence).map(function(p, u) {
-      var timeSinceLastShout = new Date() - p.shoutedAt;
-      var timedOut = timeSinceLastShout > otPresenceConfig.pingTimeout;
-      if (timedOut) return u;
-    }).compact().value();
-
-    _.forEach(timedOutUsers, function(u) {
-      delete presence[u];
-    });
-
-  }
-
-  function doLater() {
-    removeTimedOutUsers();
-    timeout = $timeout(doLater, otPresenceConfig.pingTimeout);
-  }
-
-  doLater();
-
-  $scope.$on('$destroy', function(event) {
-    var scope = event.currentScope;
-    $timeout.cancel(timeout);
-    if (scope.otDoc) {
-      scope.otDoc.removeListener('closed', closedHandler);
-      scope.otDoc.removeListener('shout', shoutHandler);
-    }
-
-  });
-}
-
-function groupPresenceByField(presence) {
-  return _.reduce(presence, function(fields, userPresence, user) {
-    var field = userPresence.focus;
-    if (!field) return fields;
-    if (!fields[field]) fields[field] = {users: []};
-    fields[field].users.push(toUserLink(user));
-    return fields;
-  }, {});
-}
-
-function presenceUsers(presence) {
-  return _(presence).keys().map(toUserLink).value();
-}
-
-function toUserLink(id) {
-  return {sys: {type: 'Link', linkType: 'User', id: id}};
-}
