c3d229e59761c8e9598b20aff5326babcc397268
Jan Varwig <jan@varwig.org>
2013-07-08 11:00:40 +0200

    In FeatureSpecs skip the login dance if possible by providing the access_token in the url

--- spec/support/feature_helper.rb
+++ spec/support/feature_helper.rb
@@ -1,6 +1,21 @@
 module FeatureHelper
+  @@access_token = nil
+
+  def access_token
+    @@access_token
+  end
+
+  def access_token=(t)
+    @@access_token = t
+  end
+
   def ensure_login
-    visit '/'
+    if access_token
+      visit "/#access_token=#{access_token}"
+    else
+      visit "/"
+    end
+
     begin
       page.find('space-view')
     rescue Capybara::ElementNotFound
@@ -15,6 +30,7 @@ module FeatureHelper
     rescue Capybara::ElementNotFound
     end
     page.find('space-view')
+    self.access_token = page.evaluate_script('angular.element($("space-view")).injector().get("authentication").token')
   end
 
   def clear_cookies
