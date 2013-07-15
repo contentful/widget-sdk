785b38a5421bc2376674824669fd20b627820cfa
Tiago Rodrigues <tmcrodrigues@gmail.com>
2013-07-15 18:00:11 +0200

    Add a test for removing cfAutocomplete entries

--- spec/javascripts/contentful/unit/cf_autocomplete_controller_spec.js
+++ spec/javascripts/contentful/unit/cf_autocomplete_controller_spec.js
@@ -33,43 +33,58 @@ describe('cf Autocomplete controller', function () {
     });
   });
 
-  it('adds a link for an entry and updates linked entries', inject(function ($q) {
-    this.async(function (done) {
-      var addLinkDeferred = $q.defer();
-      var callbackSpy = sinon.spy(function () {
-        _.defer(function () {
-          scope.$apply(function () {
-            addLinkDeferred.resolve();
-          });
-        });
-      });
-      var entry1 = window.createMockEntity('entry1');
-
+  describe('attaches to a entry directive', function () {
+    var entry;
+    beforeEach(function () {
       attrs.cfAutocomplete = 'entry';
+      entry = window.createMockEntity('entry1');
       scope.otChangeValue = sinon.stub();
-      scope.otChangeValue.callsArg(1);
+    });
 
-      var linkedEntriesDeferred = $q.defer();
-      scope.$watch('linkedEntries', function () {
-        _.defer(function () {
-          scope.$apply(function () {
-            linkedEntriesDeferred.resolve();
-            expect(scope.linkedEntries.length).toBeGreaterThan(0);
+    it('adds a link for an entry and updates linked entries', inject(function ($q) {
+      this.async(function (done) {
+        var addLinkDeferred = $q.defer();
+        var callbackSpy = sinon.spy(function () {
+          _.defer(function () {
+            scope.$apply(function () {
+              addLinkDeferred.resolve();
+            });
           });
         });
+
+        scope.otChangeValue.callsArgAsync(1);
+
+        var linkedEntriesDeferred = $q.defer();
+        scope.$watch('linkedEntries', function () {
+          _.defer(function () {
+            scope.$apply(function () {
+              linkedEntriesDeferred.resolve();
+            });
+          });
+        });
+
+        $q.all([addLinkDeferred.promise, linkedEntriesDeferred.promise]).then(function () {
+          done();
+          expect(scope.otChangeValue.called).toBeTruthy();
+          expect(scope.updateModel.called).toBeTruthy();
+          expect(callbackSpy.called).toBeTruthy();
+          expect(scope.links[0].sys.id).toEqual('entry1');
+          expect(scope.linkedEntries.length).toBeGreaterThan(0);
+        });
+
+        scope.addLink(entry, callbackSpy);
       });
+    }));
 
-      $q.all([addLinkDeferred.promise, linkedEntriesDeferred.promise]).then(function () {
-        done();
-      });
-
-      scope.addLink(entry1, callbackSpy);
-
+    it('removes a link for an entry and updates linked entries', function () {
+      scope.links = [1,2,3];
+      scope.otChangeValue.callsArg(1);
+      scope.removeLink(0, entry);
       expect(scope.otChangeValue.called).toBeTruthy();
       expect(scope.updateModel.called).toBeTruthy();
-      expect(callbackSpy.called).toBeTruthy();
-      expect(scope.links[0].sys.id).toEqual('entry1');
+      expect(scope.links.length).toBe(0);
     });
-  }));
+
+  });
 
 });
