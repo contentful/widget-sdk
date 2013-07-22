b7473aad05c75f78f510c326410bb403ab6efe35
Jan Varwig <jan@varwig.org>
2013-07-22 12:53:46 +0200

    In ValidationErrorDisplay, use data and schema to customize the error messages further. [Delivers #52861919]

---
+++ spec/javascripts/contentful/unit/validation_error_display_spec.js
@@ -0,0 +1,49 @@
+'use strict';
+
+describe('Validation Error Display Controller', function () {
+  var controller, scope, attrs;
+
+  beforeEach(module('contentful/test'));
+
+  beforeEach(inject(function ($controller, $rootScope){
+    scope = $rootScope;
+    scope.entity = {};
+    scope.validationResult = {
+      errors : [],
+      data: scope.entity
+    };
+    controller = $controller('ValidationErrorDisplayCtrl', {
+      $scope: $rootScope,
+      $attrs: attrs = {}
+    });
+  }));
+
+  describe('data-dependent message generation', function () {
+    beforeEach(function () {
+      attrs.path = '["foo", "bar"]';
+      scope.entity = {
+        foo: {
+          bar: 'asdasd'
+        }
+      };
+      scope.validationResult.data = scope.entity;
+      scope.validationResult.errors.push({
+        name: 'size',
+        path: ['foo', 'bar'],
+        min: 10
+      });
+    });
+
+    it('should say "Length" for strings', function () {
+      scope.entity.foo.bar = 'asdasd';
+      scope.$apply();
+      expect(scope.errorMessages[0]).toBe('Length must be at least 10.');
+    });
+
+    it('should say "Size" for strings', function () {
+      scope.entity.foo.bar = [1,2,3];
+      scope.$apply();
+      expect(scope.errorMessages[0]).toBe('Size must be at least 10.');
+    });
+  });
+});
