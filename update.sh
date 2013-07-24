b40ac4a46f75d03e7110fad46ffa4d0cb6c09316
Tiago Rodrigues <tmcrodrigues@gmail.com>
2013-07-24 16:53:36 +0200

    Revert update.sh. Run npm install straight on travis.yml.

--- update.sh
+++ update.sh
@@ -4,13 +4,9 @@ set -ev
 
 bundle install --no-color
 pushd ./app/assets/commonjs_modules/user_interface
-if [ "$1" == "force_install" ] ; then
-  npm link worf
-  npm link spock
-  npm link contentful-client
-  npm link validation
-  npm link share
-else
-  npm install
-fi
+npm link worf
+npm link spock
+npm link contentful-client
+npm link validation
+npm link share
 popd
