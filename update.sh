6116206a74bb08cfddd8549e343b77c8a54528b0
Stephan Seidt <evilhackerdude@gmail.com>
2013-07-30 16:09:46 +0200

    For CI install dependencies instead of linking

--- update.sh
+++ update.sh
@@ -4,9 +4,15 @@ set -ev
 
 bundle install --no-color
 pushd ./app/assets/commonjs_modules/user_interface
-npm link worf
-npm link spock
-npm link contentful-client
-npm link validation
-npm link share
+
+if [ "$CI" = "true" ]; then
+  npm install
+else
+  npm link worf
+  npm link spock
+  npm link contentful-client
+  npm link validation
+  npm link share
+fi
+
 popd
