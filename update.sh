310d3b7a137dfa54ccf91164f931dbdd256acdc9
Stephan Seidt <evilhackerdude@gmail.com>
2013-07-30 15:17:15 +0200

    Use cd instead of pushd/popd to support travis

--- update.sh
+++ update.sh
@@ -3,10 +3,9 @@
 set -ev
 
 bundle install --no-color
-pushd ./app/assets/commonjs_modules/user_interface
+cd ./app/assets/commonjs_modules/user_interface
 npm link worf
 npm link spock
 npm link contentful-client
 npm link validation
 npm link share
-popd
