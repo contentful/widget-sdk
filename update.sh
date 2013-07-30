5e46e92db5ad5ed54fbf8b07aa8c28740f8eed9f
Stephan Seidt <evilhackerdude@gmail.com>
2013-07-30 15:52:49 +0200

    Revert "Use cd instead of pushd/popd to support travis"
    This reverts commit 7b32ecbe62d3914d64e4fe6bdb79bed3c3ab2e8e.

--- update.sh
+++ update.sh
@@ -3,9 +3,10 @@
 set -ev
 
 bundle install --no-color
-cd ./app/assets/commonjs_modules/user_interface
+pushd ./app/assets/commonjs_modules/user_interface
 npm link worf
 npm link spock
 npm link contentful-client
 npm link validation
 npm link share
+popd
