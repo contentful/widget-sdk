e26b936b324bac6a27667cb2fd60f45757b222c1
Jan Varwig <jan@varwig.org>
2013-08-14 18:01:53 +0200

    Use new cfClickOutside directive to close the search results in cfLinkEditor

---
+++ app/assets/javascripts/directives/cf_click_outside_directive.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular.module('contentful').directive('cfClickOutside', function($parse, $document){
+  return {
+    restrict: 'A',
+    link: function(scope, element, attr) {
+      var fn = $parse(attr.cfClickOutside);
+      $document.bind('click', clickOutsideHandler);
+      element.bind('remove', function () {
+        $document.unbind('click', clickOutsideHandler);
+      });
+
+      function clickOutsideHandler(event) {
+        var targetParents = $(event.target).parents();
+        var inside = targetParents.index(element) !== -1;
+        var on     = event.target === element[0];
+        var clickOutside = !inside && !on;
+
+        if (clickOutside) scope.$apply(function() {
+          fn(scope, {$event:event});
+        });
+      }
+
+      element.bind('click', function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        scope.$apply(function() {
+          fn(scope, {$event:event});
+        });
+      });
+    }
+  };
+});
+
