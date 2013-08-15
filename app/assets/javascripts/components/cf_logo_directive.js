6d056e7f4a02569e5d94ed5d0ac6b2cee0a39fc3
Jan Varwig <jan@varwig.org>
2013-08-15 13:43:24 +0200

    new loading indicator [Delivers #49383989]

--- app/assets/javascripts/components/cf_logo_directive.js
+++ app/assets/javascripts/components/cf_logo_directive.js
@@ -1,25 +1,8 @@
-angular.module('contentful').directive('cfLogo', ['cfSpinner', function (cfSpinner) {
+angular.module('contentful').directive('cfLogo', function () {
   'use strict';
 
   return {
     restrict: 'C',
-    template: '<div class="logopart yellow"/><div class="logopart blue"/><div class="logopart red"/>',
-    link: function (scope, element) {
-      var start = function () {
-        element.addClass('animate');
-      };
-
-      var stop = function () {
-        element.removeClass('animate');
-      };
-
-      cfSpinner.setCallback(function (state) {
-        if (state) {
-          start();
-        } else {
-          stop();
-        }
-      });
-    }
+    template: '<div class="logopart yellow"/><div class="logopart blue"/><div class="logopart red"/>'
   };
-}]);
+});
