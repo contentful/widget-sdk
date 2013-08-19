c42463a7e50af869ea9839dff96b9148518b4b65
Tiago Rodrigues <tmcrodrigues@gmail.com>
2013-08-19 11:07:26 +0200

    Show 2 step delete on assets

--- app/assets/javascripts/directives/cf_click_outside_directive.js
+++ app/assets/javascripts/directives/cf_click_outside_directive.js
@@ -5,17 +5,24 @@ angular.module('contentful').directive('cfClickOutside', function($parse, $docum
     restrict: 'A',
     link: function(scope, element, attr) {
       var fn = $parse(attr.cfClickOutside);
+      var ignored = $(attr.cfClickOutsideIgnore);
       $document.bind('click', clickOutsideHandler);
       element.bind('remove', function () {
         $document.unbind('click', clickOutsideHandler);
       });
+
+      function isIgnored(target) {
+        return _.any(ignored, function (el) {
+          return el === target;
+        });
+      }
 
       function clickOutsideHandler(event) {
         event.stopPropagation();
         var targetParents = $(event.target).parents();
         var inside = targetParents.index(element) !== -1;
         var on     = event.target === element[0];
-        var clickOutside = !inside && !on;
+        var clickOutside = !inside && !on && !isIgnored(event.target);
 
         if (clickOutside) scope.$apply(function() {
           fn(scope, {$event:event});
