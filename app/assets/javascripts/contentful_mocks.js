6ef251bd438b9876836b156f01e79afd79033961
Jan Varwig <jan@varwig.org>
2013-09-03 13:23:55 +0200

    Extended contentful/mocks module with cfStub

--- app/assets/javascripts/contentful_mocks.js
+++ app/assets/javascripts/contentful_mocks.js
@@ -1,5 +1,61 @@
 'use strict';
 
+var mocks = angular.module('contentful/mocks', []);
+
+mocks.factory('cfStub', function (contentfulClient) {
+  var Client = contentfulClient;
+  var Adapter = contentfulClient.adapters.testing;
+  var adapter = new Adapter();
+
+  var cfStub = {};
+  cfStub.locale = function (code, extensions) {
+    return _.extend({
+      code: code,
+      contentDeliveryApi: true,
+      contentManagementApi: true,
+      'default': true,
+      name: code,
+      publish: true
+    }, extensions);
+  };
+
+  cfStub.space = function (id) {
+    id = id || 'testSpace';
+    var client = new Client(adapter);
+    var testSpace;
+    client.getSpace(id, function (err, space) {
+      testSpace = space;
+    });
+    adapter.respondWith(null, {
+      locales: [
+        cfStub.locale('en-US'),
+        cfStub.locale('de-DE', {'default': false}),
+        cfStub.locale('pt-PT', {'default': false}),
+        cfStub.locale('pt-BR', {'default': false})
+      ]
+    });
+    return testSpace;
+  };
+
+  cfStub.contentTypeData = function (id, extensions) {
+    return _.merge({
+      fields: []
+    }, extensions);
+  };
+
+  cfStub.field = function (id, extension) {
+    return _.extend({
+      'id': id,
+      'name': id,
+      'required': false,
+      'localized': true,
+      'type': 'Text'
+    }, extension);
+  };
+
+  return cfStub;
+});
+
 window.createMockEntity = function (id, contentType) {
   return {
     getId: function () {
@@ -52,7 +108,7 @@ window.createMockSpace = function (id) {
   return entity;
 };
 
-angular.module('contentful/mocks', []).provider('ShareJS', function () {
+mocks.provider('ShareJS', function () {
   function FakeShareJSClient() {
   }
 
