7431ad872ff4f893a9a35a36dff83dd868fe74dc
Tiago Rodrigues <tmcrodrigues@gmail.com>
2013-09-09 15:05:06 +0200

    Make sure navbar tooltip doesn't stick around

--- app/assets/javascripts/directives/cf_clear_outlines.js
+++ app/assets/javascripts/directives/cf_clear_outlines.js
@@ -4,11 +4,15 @@ angular.module('contentful').directive('cfClearOutlines', ['$document', function
   return {
     restrict: 'C',
     link: function () {
+      var $body = $document.find('body');
       var disableOutlines = $('#disable-outlines');
+      $body.addClass('outlines-enabled');
       $document.on('mousedown', function () {
+        $body.addClass('outlines-enabled');
         disableOutlines.html('*,*:focus{outline:none !important;}');
       });
       $document.on('keydown', function () {
+        $body.removeClass('outlines-enabled');
         disableOutlines.html('');
       });
     }
