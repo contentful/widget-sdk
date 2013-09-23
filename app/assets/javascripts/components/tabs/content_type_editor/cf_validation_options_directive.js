689c725ba301d1df45804e1c394ce0d2b4a3ba58
Jan Varwig <jan@varwig.org>
2013-09-23 15:23:26 +0200

    Remove ngModel Approach for validation options

--- app/assets/javascripts/components/tabs/content_type_editor/cf_validation_options_directive.js
+++ app/assets/javascripts/components/tabs/content_type_editor/cf_validation_options_directive.js
@@ -1,41 +1,9 @@
 'use strict';
-angular.module('contentful').directive('cfValidationOptions', function ($parse) {
+angular.module('contentful').directive('cfValidationOptions', function () {
 
   return {
     restrict: 'C',
     template: JST['cf_validation_options'](),
-    require: 'ngModel',
-    link: function (scope, elem, attr, ngModel) {
-      var ngModelGet = $parse(attr.ngModel),
-          ngModelSet = ngModelGet.assign;
-
-      ngModel.$render = function () {
-        scope.validationOptions = ngModel.$viewValue;
-      };
-
-      ngModel.$parsers.push(function (viewValue) {
-        var modelValue = {};
-        modelValue[viewValue.type] = viewValue.params;
-        return modelValue;
-      });
-
-      ngModel.$formatters.push(function (modelValue) {
-        var type = scope.validationType(modelValue);
-        return {
-          type: type,
-          params: modelValue[type]
-        };
-      });
-
-      scope.validationOptionsUpdated = function () {
-        ngModel.$setViewValue(scope.validationOptions);
-        // setViewValue is not enough, since setting it on the scope will only
-        // replace the property in the ng-repeat scope, so we
-        // also need to replace it in the parent.
-        scope.validationList()[scope.validationIndex] = ngModel.$modelValue;
-        scope.updateDoc();
-      };
-    },
     controller: function CfValidationOptionsCtrl($scope, mimetype) {
       $scope.mimetypeGroups = mimetype.groupDisplayNames;
 
@@ -47,10 +15,11 @@ angular.module('contentful').directive('cfValidationOptions', function ($parse) 
       };
 
       $scope.changeType = function (newValidation) {
-        var type = $scope.validationType(newValidation);
-        $scope.validationOptions.type = type;
-        $scope.validationOptions.params = newValidation[type];
-        $scope.validationOptionsUpdated();
+        var oldType = $scope.validationType($scope.validation);
+        var newType = $scope.validationType(newValidation);
+        delete $scope.validation[oldType];
+        $scope.validation[newType] = newValidation[newType];
+        $scope.updateDoc();
       };
 
     }
