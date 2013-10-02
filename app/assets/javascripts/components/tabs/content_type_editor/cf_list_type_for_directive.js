7081a1329e6474be59143bb8317177c5a6685f66
Jan Varwig <jan@varwig.org>
2013-10-02 15:19:09 +0200

    cfListTypeFor directive that enhances ng-list with the ability to convert the list elements to numbers or ints, depending on the type of the field [#57808260]

---
+++ app/assets/javascripts/components/tabs/content_type_editor/cf_list_type_for_directive.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('contentful').directive('cfListTypeFor', function () {
+  return {
+    require: 'ngModel',
+    link: function (scope, elem, attr, ngModel) {
+      ngModel.$parsers.push(function (viewValue) {
+        var field = scope.$eval(attr.cfListTypeFor);
+        if (field.type === 'Number') {
+          return _.map(viewValue, function (s) { return parseFloat(s, 10); });
+        } else if (field.type === 'Integer') {
+          return _.map(viewValue, function (s) { return parseInt(s, 10); });
+        } else {
+          return viewValue;
+        }
+      });
+    }
+  };
+});
+
---
+++ spec/javascripts/contentful/unit/components/shared/cf_list_type_for_spec.js
@@ -0,0 +1,26 @@
+'use strict';
+
+describe('cfListTypeFor Directve', function () {
+  var element, scope;
+  beforeEach(module('contentful/test'));
+
+  beforeEach(inject(function ($compile, $rootScope) {
+    scope = $rootScope;
+    scope.field = {};
+    element = $compile('<input type="text" ng-model="list" ng-list cf-list-type-for="field"/>')(scope);
+  }));
+
+  it('should convert numbers to numbers', function () {
+    scope.field.type = 'Number';
+    element.val('1,2,3.5').trigger('input');
+    scope.$apply();
+    expect(scope.list).toLookEqual([1,2,3.5]);
+  });
+
+  it('should convert integers to integers', function () {
+    scope.field.type = 'Integer';
+    element.val('1,2,3.5').trigger('input');
+    scope.$apply();
+    expect(scope.list).toLookEqual([1,2,3]);
+  });
+});
