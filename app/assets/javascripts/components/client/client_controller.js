ed4285c88466e34ecc4a37d029b248291354ed6a
Tiago Rodrigues <tmcrodrigues@gmail.com>
2013-10-09 15:03:57 +0200

    Additional check for when authContext isn't ready

--- app/assets/javascripts/components/client/client_controller.js
+++ app/assets/javascripts/components/client/client_controller.js
@@ -38,7 +38,7 @@ angular.module('contentful').controller('ClientCtrl', function ClientCtrl(
   }, function (c) {
     if (c.tokenLookup){
       authorization.setTokenLookup(c.tokenLookup);
-      if (c.space && authorization.authContext.hasSpace(c.space.getId()))
+      if (c.space && authorization.authContext && authorization.authContext.hasSpace(c.space.getId()))
         authorization.setSpace(c.space);
     }
   });
