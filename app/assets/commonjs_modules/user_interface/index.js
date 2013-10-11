f56ceecf49c2ef3c4b3d770886f4e7b1953109d0
Tiago Rodrigues <tmcrodrigues@gmail.com>
2013-10-11 18:23:25 +0200

    Add json-stringify-safe to node dependencies

--- app/assets/commonjs_modules/user_interface/index.js
+++ app/assets/commonjs_modules/user_interface/index.js
@@ -4,7 +4,8 @@ var UserInterface = {
   worf: require('worf'),
   validation: require('validation'),
   mimetype: require('contentful-mimetype'),
-  client: require('contentful-client')
+  client: require('contentful-client'),
+  stringifySafe: require('json-stringify-safe')
 };
 
 module.exports = UserInterface;
@@ -14,6 +15,7 @@ if (angular) {
     constant('contentfulClient', UserInterface.client).
     constant('validation', UserInterface.validation).
     constant('mimetype', UserInterface.mimetype).
-    constant('worf', UserInterface.worf);
+    constant('worf', UserInterface.worf).
+    constant('stringifySafe', UserInterface.stringifySafe);
 }
 
