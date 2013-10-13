283d7fda871c1e4dc7269e46dbc9068dd0183725
Jan Varwig <jan@varwig.org>
2013-10-13 23:07:38 +0200

    spec fix

--- spec/javascripts/contentful/unit/components/forms/number_widget_spec.js
+++ spec/javascripts/contentful/unit/components/forms/number_widget_spec.js
@@ -3,30 +3,21 @@
 describe('Number widgets', function () {
   var elem, scope;
   beforeEach(module('contentful/test'));
-  beforeEach(module(function ($compileProvider){
-    $compileProvider.directive('testWidget', function (widgets, $compile) {
-      return {
-        link: function (scope, elem, attr) {
-          var widget = widgets.editor(attr.testWidget);
-          elem.html(widget.template);
-          $compile(elem.contents())(scope);
-          if(typeof widget.link === 'function') widget.link(scope, elem, attr);
-        }
-      };
-    });
-  }));
   beforeEach(inject(function ($rootScope) {
     scope = $rootScope;
     scope.fieldData = {value: null};
+    scope.field = {type: null};
+    scope.otEditable = true;
   }));
   
   describe('Number widget', function () {
     beforeEach(inject(function ($rootScope, $compile){
-      elem = $compile('<div test-widget="Number"></div>')(scope);
+      scope.field = {type: 'Number'};
+      elem = $compile('<div class="cf-number-editor"></div>')(scope);
     }));
 
     it('should generate 0', function () {
-      elem.find('input').val('0').trigger('input');
+      elem.val('0').trigger('input');
       scope.$apply();
       expect(scope.fieldData.value).toBe(0);
     });
@@ -34,11 +25,12 @@ describe('Number widgets', function () {
 
   describe('Integer widget', function () {
     beforeEach(inject(function ($rootScope, $compile){
-      elem = $compile('<div test-widget="Integer"></div>')(scope);
+      scope.field = {type: 'Number'};
+      elem = $compile('<div class="cf-number-editor"></div>')(scope);
     }));
 
     it('should generate 0', function () {
-      elem.find('input').val('0').trigger('input');
+      elem.val('0').trigger('input');
       scope.$apply();
       expect(scope.fieldData.value).toBe(0);
     });
