d723799fca82e65a92d7cd28fc258faadf5db5eb
Jan Varwig <jan@varwig.org>
2013-10-28 10:25:33 +0100

    Initialize tooltip directive lazily

--- app/assets/javascripts/directives/tooltip_directive.js
+++ app/assets/javascripts/directives/tooltip_directive.js
@@ -5,32 +5,46 @@ angular.module('contentful').
     return {
       restrict: 'A',
       link: function(scope, element, attrs) {
-        scope.disableTooltip = false;
-        element = $(element);
-        function destroy() {
-          element.tooltip('destroy');
+        var tooltipInitialized = false;
+
+        element.one('mouseenter', initialize);
+        element.one('focus', initialize);
+
+        function initialize() {
+          if (tooltipInitialized) return;
+          createTooltip(true);
+          tooltipInitialized = true;
         }
 
-        function createTooltip() {
-          destroy();
+        function createTooltip(show) {
           element.tooltip({
             delay: {show: 100, hide: 100},
             placement: attrs.tooltipPlacement,
-            title: attrs.tooltip
+            title: function () {
+              return attrs.tooltip;
+            }
           });
+          if (show) element.tooltip('show');
         }
 
-        attrs.$observe('tooltip', createTooltip);
+        function destroyTooltip() {
+          element.tooltip('destroy');
+        }
 
         scope.$watch('disableTooltip', disableHandler);
         scope.$watch(attrs.ngDisabled, disableHandler);
 
         function disableHandler(disabled) {
-          if(disabled) destroy();
+          if (!tooltipInitialized) return;
+          if(disabled) destroyTooltip();
           if(!disabled) createTooltip();
         }
 
-        element.on('$destroy', destroy);
+        element.on('$destroy', function () {
+          destroyTooltip();
+          element.off('mouseenter', initialize);
+          element.off('focus', initialize);
+        });
       }
     };
   });
