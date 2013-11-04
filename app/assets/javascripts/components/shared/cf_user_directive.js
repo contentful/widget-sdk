cdd49b17d60fd5e7ce9cc80f6c15c84a431beb26
Tiago Rodrigues <tmcrodrigues@gmail.com>
2013-11-04 16:09:18 +0100

    fix a check on cf user directive

--- app/assets/javascripts/components/shared/cf_user_directive.js
+++ app/assets/javascripts/components/shared/cf_user_directive.js
@@ -11,7 +11,8 @@ angular.module('contentful').
             if (err) return;
             $scope.$apply(function() {
               $scope[$attrs.as] = user;
-              $scope.currentUserName = (user.getId() === $scope.user.sys.id) ? 'You' : user.getName();
+              var currentUserId = ($scope.user && $scope.user.sys && $scope.user.sys.id) ? $scope.user.sys.id : null;
+              $scope.currentUserName = (currentUserId && (user.getId() === currentUserId)) ? 'You' : user.getName();
             });
           });
         }, true);
