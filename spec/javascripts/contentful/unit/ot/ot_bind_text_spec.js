9859b78f79503dee8b3790e17e04658e6879a773
Tiago Rodrigues <tmcrodrigues@gmail.com>
2013-11-07 16:14:56 +0100

    Test the ot_bind_text parsers for accentuated characters

--- spec/javascripts/contentful/unit/ot/ot_bind_text_spec.js
+++ spec/javascripts/contentful/unit/ot/ot_bind_text_spec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe('otBindText', function () {
-  var scope, doc, subdoc, unbindTextArea, elem;
+  var scope, doc, subdoc, unbindTextArea, elem, controller;
 
   beforeEach(module('contentful/test', function ($provide) {
     $provide.value('ShareJS', {
@@ -25,11 +25,21 @@ describe('otBindText', function () {
     elem = $compile('<input type="text" ng-model="entity.value" ot-doc-for="entity" ot-path="[\'value\']" ot-subdoc ot-bind-text>')($rootScope);
     scope = elem.scope();
     scope.otDisabled = false;
+    controller = elem.controller('ngModel');
   }));
 
   afterEach(inject(function ($log) {
     $log.assertEmpty();
   }));
+
+  it('should not insert an accent into the text field', function () {
+    expect(controller.$parsers[0]('´')).toBeNull();
+  });
+
+  it('should not insert a character into the text field', function () {
+    expect(controller.$parsers[0]('e')).toBe('e');
+  });
+
 
   it('should detach the textfield when otSubdoc is removed', function () {
     scope.$apply();
