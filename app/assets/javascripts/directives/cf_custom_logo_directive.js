500e0dd47e6b48d25aa60f93b1d967ec3f427693
Jan Varwig <jan@varwig.org>
2013-11-13 10:39:07 +0100

    Use customer-provided logo from space theme [Delivers #60457148]

---
+++ app/assets/javascripts/directives/cf_custom_logo_directive.js
@@ -0,0 +1,12 @@
+'use strict';
+angular.module('contentful').directive('cfCustomLogo', function(){
+  return {
+    link: function(scope, elem){
+      var themeExpression = 'spaceContext.space.data.subscription.subscriptionPlan.limits.features.customTheme && spaceContext.space.data.theme';
+      scope.$watch(themeExpression, function (theme) {
+        var logo = _.isEmpty(theme && theme.logoUrl) ? '' : 'url("'+theme.logoUrl+'")';
+        elem.css({'background-image': logo});
+      });
+    }
+  };
+});
