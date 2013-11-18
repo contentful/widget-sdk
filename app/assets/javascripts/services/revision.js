56595c9513894dc8f352ef4940c1dd4792f93b83
Tiago Rodrigues <tmcrodrigues@gmail.com>
2013-11-18 15:57:47 +0100

    check if git revision actually exists

--- app/assets/javascripts/services/revision.js
+++ app/assets/javascripts/services/revision.js
@@ -4,7 +4,9 @@ angular.module('contentful').factory('revision', function RevisionFactory($rootS
     hasNewVersion: function() {
       return $http.get('/manifest.json?cfv='+Math.ceil(Math.random()*10000000)).
       then(function (response) {
-        if(response && response.data && response.data.git_revision !== environment.settings.git_revision){
+        if(response && response.data &&
+           response.data.git_revision &&
+           response.data.git_revision !== environment.settings.git_revision){
           return $q.reject('App revision has changed');
         }
       });
