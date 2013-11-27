c806d4b7b019e4fdb3be8b14e1b7b3d334c34e53
Tiago Rodrigues <tmcrodrigues@gmail.com>
2013-11-27 22:28:23 +0100

    Fix permission tests

--- app/assets/javascripts/components/tabs/api_key_editor/api_key_editor_directive.js
+++ app/assets/javascripts/components/tabs/api_key_editor/api_key_editor_directive.js
@@ -1,16 +1,14 @@
-'use strict';
-
-angular.module('contentful').
-  directive('apiKeyEditor', function() {
-    return {
-      template: JST.api_key_editor(),
-      restrict: 'C',
-      controller: 'ApiKeyEditorCtrl',
-      link: function(scope, elem) {
-        elem.on('keydown', function(e) {
-          if (e.keyCode === 13) scope.save();
-        });
-        _.defer(function(){elem.find('input').eq(0).focus();});
-      }
-    };
-  });
+angular.module('contentful').directive('apiKeyEditor', function() {
+  'use strict';
+  return {
+    template: JST.api_key_editor(),
+    restrict: 'C',
+    controller: 'ApiKeyEditorCtrl',
+    link: function(scope, elem) {
+      elem.on('keydown', function(e) {
+        if (e.keyCode === 13) scope.save();
+      });
+      _.defer(function(){elem.find('input').eq(0).focus();});
+    }
+  };
+});
