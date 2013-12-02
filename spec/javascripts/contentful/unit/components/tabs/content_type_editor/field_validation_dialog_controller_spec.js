fe9dad2f9cc242a9a9f789ae6f7946dc60f7169c
Tiago Rodrigues <tmcrodrigues@gmail.com>
2013-12-02 15:09:06 +0100

    merge validation dialog specs

--- spec/javascripts/contentful/unit/components/tabs/content_type_editor/field_validation_dialog_controller_spec.js
+++ spec/javascripts/contentful/unit/components/tabs/content_type_editor/field_validation_dialog_controller_spec.js
@@ -1,5 +1,38 @@
 'use strict';
 
+// TODO cleanup this file
+
+describe('Validation Dialog', function () {
+  var scope, controller;
+  beforeEach(module('contentful/test'));
+
+  beforeEach(inject(function ($controller, $rootScope){
+    scope = $rootScope;
+    scope.field = {
+      'name': 'aaa',
+      'id': 'aaa',
+      'type': 'Text',
+      'uiid': '4718xi6vshs',
+      //'validations': [ { 'in': null } ]
+    };
+    controller = $controller('FieldValidationDialogController', {$scope: scope});
+    scope.$apply();
+  }));
+
+  afterEach(inject(function ($log) {
+    $log.assertEmpty();
+  }));
+
+  it('should remove used validations from list of available validations', function () {
+    expect(_.keys(scope.availableValidations).length).toBe(3);
+    expect(_.keys(scope.availableValidations)).toContain('Predefined Values');
+    scope.field.validations = [ { 'in': null } ];
+    scope.$apply();
+    expect(_.keys(scope.availableValidations).length).toBe(2);
+    expect(_.keys(scope.availableValidations)).not.toContain('Predefined Values');
+  });
+});
+
 describe('ContentType field validation dialog Controller', function () {
   var controller, scope;
   var canStub;
