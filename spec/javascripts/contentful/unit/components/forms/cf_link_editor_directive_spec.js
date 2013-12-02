8520577ff59e0d01077410949aea30907f52c0b9
Tiago Rodrigues <tmcrodrigues@gmail.com>
2013-12-02 17:32:35 +0100

    test new button states on link editor

--- spec/javascripts/contentful/unit/components/forms/cf_link_editor_directive_spec.js
+++ spec/javascripts/contentful/unit/components/forms/cf_link_editor_directive_spec.js
@@ -4,33 +4,112 @@ describe('cfLinkEditor Directve', function () {
   var element, scope;
   var compileElement;
   var searchField;
-  beforeEach(module('contentful/test'));
+  var canStub;
 
-  beforeEach(inject(function ($compile, $rootScope) {
-    scope = $rootScope.$new();
-    scope.fieldData = { value: {
-      sys: {id: 123}
-    }};
-    scope.field = {
-      items: {}
-    };
-    scope.spaceContext = {
-      space: {
-        getEntries: sinon.stub(),
-        getAssets: sinon.stub()
-      }
-    };
+  beforeEach(function () {
+    module('contentful/test');
+    inject(function ($compile, $rootScope) {
+      scope = $rootScope.$new();
+      canStub = sinon.stub();
+      scope.can = canStub;
+      scope.fieldData = { value: {
+        sys: {id: 123}
+      }};
+      scope.field = {
+        items: {}
+      };
+      scope.spaceContext = {
+        space: {
+          getEntries: sinon.stub(),
+          getAssets: sinon.stub()
+        }
+      };
 
-    compileElement = function () {
-      element = $compile('<div cf-link-editor="field.items.linkType" ng-model="fieldData.value"></div>')(scope);
-      scope.$digest();
-      searchField = element.find('.search-field');
-    };
-  }));
+      compileElement = function () {
+        element = $compile('<div cf-link-editor="field.items.linkType" ng-model="fieldData.value"></div>')(scope);
+        scope.$digest();
+        searchField = element.find('.search-field');
+      };
+    });
+  });
 
   afterEach(inject(function ($log) {
     $log.assertEmpty();
   }));
+
+  describe('shows new button for link to entries with no validation', function () {
+    var newButton;
+    beforeEach(function () {
+      scope.field.items.linkType = 'Entry';
+      scope.linkContentType = null;
+      scope.spaceContext.publishedContentTypes = [
+        {getId: function(){return Math.random();}}
+      ];
+
+      canStub.withArgs('create', 'Entry').returns(true);
+
+      compileElement();
+      newButton = element.find('.add-new');
+    });
+
+    it('to have button', function () {
+      expect(newButton.get(0)).toBeDefined();
+    });
+
+    it('has dropdown menu', function () {
+      expect(newButton.find('.dropdown-menu').get(0)).toBeDefined();
+    });
+
+    it('has menu elements', function () {
+      expect(newButton.find('.dropdown-menu li').get(0)).toBeDefined();
+    });
+
+    it('has action on menu elements', function () {
+      expect(newButton.find('.dropdown-menu li').attr('ng-click')).toMatch('addNewEntry');
+    });
+  });
+
+  describe('shows new button for link to entries with validations', function () {
+    var newButton;
+    beforeEach(function () {
+      scope.field.items.linkType = 'Entry';
+      scope.linkContentType = {
+        getName: sinon.stub()
+      };
+
+      canStub.withArgs('create', 'Entry').returns(true);
+
+      compileElement();
+      newButton = element.find('.add-new');
+    });
+
+    it('to have button', function () {
+      expect(newButton.get(0)).toBeDefined();
+    });
+
+    it('has action on button', function () {
+      expect(newButton.attr('ng-click')).toMatch('addNewEntry');
+    });
+  });
+
+  describe('shows new button for link to assets with no validations', function () {
+    var newButton;
+    beforeEach(function () {
+      scope.field.items.linkType = 'Asset';
+      canStub.withArgs('create', 'Asset').returns(true);
+
+      compileElement();
+      newButton = element.find('.add-new');
+    });
+
+    it('to have button', function () {
+      expect(newButton.get(0)).toBeDefined();
+    });
+
+    it('has action on button', function () {
+      expect(newButton.attr('ng-click')).toMatch('addNewAsset');
+    });
+  });
 
   describe('links entries', function () {
     beforeEach(function () {
