bf9d23010a80bf5b5933af015896fec0343414b7
Jan Varwig <jan@varwig.org>
2013-12-03 16:22:06 +0100

    space settings path fix

--- app/assets/javascripts/components/tabs/space_settings/space_settings_directive.js
+++ app/assets/javascripts/components/tabs/space_settings/space_settings_directive.js
@@ -61,8 +61,7 @@ angular.module('contentful').directive('spaceSettings', function($window, $rootS
 
       function extractPathSuffix(path) {
         var match = path.match(/settings\/spaces\/\w+\/(.*$)/);
-        if (match === null) throw new Error('Match is null for path', path);
-        return match[1];
+        return match && match[1];
       }
 
       function appendToken(url) {
