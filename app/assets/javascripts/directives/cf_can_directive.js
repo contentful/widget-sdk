739bac39287ac4942f74e7a04b1e0fb5435f8bb7
Tiago Rodrigues <tmcrodrigues@gmail.com>
2013-12-06 00:09:12 +0100

    Fix cf-can directive watcher flow

--- app/assets/javascripts/directives/cf_can_directive.js
+++ app/assets/javascripts/directives/cf_can_directive.js
@@ -1,4 +1,4 @@
-angular.module('contentful').directive('cfCan', ['determineEnforcement', function (determineEnforcement) {
+angular.module('contentful').directive('cfCan', ['determineEnforcement', 'authorization', function (determineEnforcement, authorization) {
   'use strict';
 
   function makePropGetter(elem){
@@ -10,38 +10,45 @@ angular.module('contentful').directive('cfCan', ['determineEnforcement', functio
   return {
     restrict: 'A',
     link: function (scope, elem, attrs) {
-      scope.$watch(attrs.cfCan, function (can) {
-        if(can || !scope.spaceContext.space) return;
-        var reasons = attrs.cfCanEntity ? determineEnforcement.computeUsage(attrs.cfCanEntity) : undefined;
+      var setupWatcher = scope.$watch(function () {
+        return authorization.spaceContext;
+      }, function (space) {
+        if(!space) return;
+        setupWatcher();
+        scope.$watch(attrs.cfCan, function (can) {
+          if(can) return;
+          var reasons = attrs.cfCanEntity ? determineEnforcement.computeUsage(attrs.cfCanEntity) : undefined;
 
-        var disableButton = !('cfCanNoDisable' in attrs);
-        var hideButton = !('cfCanNoHide' in attrs);
-        if(reasons){
-          if(disableButton){
-            elem.attr('disabled', true);
-            var layer = $('<div class="transparent-button-layer"></div>');
-            var position = elem.position();
-            var prop = makePropGetter(elem);
-            layer.css({
-              top: position.top + prop('marginTop'),
-              left: position.left + prop('marginLeft'),
-              width: elem.width() + prop('paddingLeft')+
-                                    prop('paddingRight')+
-                                    prop('borderLeft')+
-                                    prop('borderRight'),
-              height: elem.height() + prop('paddingTop')+
-                                    prop('paddingBottom')+
-                                    prop('borderTop')+
-                                    prop('borderBottom'),
-            });
-            layer.prependTo(elem.parent());
-            layer.tooltip({
-              title: reasons.tooltip
-            });
+          var disableButton = !('cfCanNoDisable' in attrs);
+          var hideButton = !('cfCanNoHide' in attrs);
+          if(reasons){
+            if(disableButton){
+              elem.attr('disabled', true);
+              var layer = $('<div class="transparent-button-layer"></div>');
+              var position = elem.position();
+              var prop = makePropGetter(elem);
+              layer.css({
+                top: position.top + prop('marginTop'),
+                left: position.left + prop('marginLeft'),
+                width: elem.width() + prop('paddingLeft')+
+                                      prop('paddingRight')+
+                                      prop('borderLeft')+
+                                      prop('borderRight'),
+                height: elem.height() + prop('paddingTop')+
+                                      prop('paddingBottom')+
+                                      prop('borderTop')+
+                                      prop('borderBottom'),
+              });
+              layer.prependTo(elem.parent());
+              layer.tooltip({
+                title: reasons,
+                trigger: 'hover'
+              });
+            }
+          } else if(hideButton){
+            elem.addClass('ng-hide');
           }
-        } else if(hideButton){
-          elem.addClass('ng-hide');
-        }
+        });
       });
     }
   };
