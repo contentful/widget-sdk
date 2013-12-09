37650d2a828a8e0123005770bbd6d70652b0d7de
Tiago Rodrigues <tmcrodrigues@gmail.com>
2013-12-09 12:45:47 +0100

    Change the way cf-can gets its arguments - Now uses a custom expression which gets parsed - Each scope.can call evaluated separately

--- app/assets/javascripts/directives/cf_can_directive.js
+++ app/assets/javascripts/directives/cf_can_directive.js
@@ -1,5 +1,50 @@
-angular.module('contentful').directive('cfCan', ['determineEnforcement', 'authorization', function (determineEnforcement, authorization) {
+angular.module('contentful').directive('cfCan', [
+  'enforcements', 'authorization', 'reasonsDenied',
+  function (enforcements, authorization, reasonsDenied) {
   'use strict';
+
+  var canGroupsRe = /\[\s*(\w*)\s*,\s*(\w*)\s*\]/g;
+
+  function parseMultiCanExpression(expression) {
+    var groupedParams;
+    var results = [];
+    while(groupedParams = canGroupsRe.exec(expression)){
+      results.push([
+        groupedParams[1],
+        groupedParams[2]
+      ]);
+    }
+    return results.length > 0 ? results : undefined;
+  }
+
+  function parseSingleCanExpression(expression) {
+    var results = _.map(expression.split(','), function (param) {
+      return param.trim();
+    });
+    return results.length > 1 ? results : undefined;
+  }
+
+  function parseCanExpression(canExpression) {
+    var canParams = parseMultiCanExpression(canExpression);
+    if(canParams) return canParams;
+    if(!canParams) {
+      canParams = parseSingleCanExpression(canExpression);
+      if(canParams) return [canParams];
+    }
+    throw new Error('Invalid can expression');
+  }
+
+  function evalExpressions(canParams, can) {
+    var results = [], params;
+    while(canParams.length > 0){
+      params = canParams.shift();
+      results.push({
+        can: can.apply(can, params),
+        reasons: reasonsDenied.apply(reasonsDenied, params)
+      });
+    }
+    return results;
+  }
 
   function makePropGetter(elem){
     return function getCssProperty(name) {
@@ -9,7 +54,8 @@ angular.module('contentful').directive('cfCan', ['determineEnforcement', 'author
 
   /**
    * Attributes:
-   * cf-can="expression" - hides or disables the element if expression is false
+   * cf-can="action, Entity" or cf-can="[action1, Entity1], [action2, Entity2]"
+   * Hides or disables the element if expression is false. Element is disabled if reasons exist.
    * cf-can-no-disable - makes sure this element can't ever be disabled
    * cf-can-no-hide - makes sure this element can't ever be hidden
    */
@@ -21,45 +67,50 @@ angular.module('contentful').directive('cfCan', ['determineEnforcement', 'author
       }, function (space) {
         if(!space) return;
         deactivateWatcher();
-        scope.$watch(attrs.cfCan, function (can) {
-          if(elem.hasClass('ng-hide')) elem.removeClass('ng-hide');
-          if(elem.attr('disabled')) elem.attr('disabled', false);
-          if(elem.attr('disable-layer')) $('#'+elem.attr('disable-layer')).remove();
-          if(can) return;
-          var reasons = attrs.cfCanEntity ? determineEnforcement.computeUsage(attrs.cfCanEntity) : undefined;
+        if(elem.hasClass('ng-hide')) elem.removeClass('ng-hide');
+        if(elem.attr('disabled')) elem.attr('disabled', false);
+        if(elem.attr('disable-layer')) $('#'+elem.attr('disable-layer')).remove();
 
-          var disableButton = !('cfCanNoDisable' in attrs);
-          var hideButton = !('cfCanNoHide' in attrs);
-          if(reasons){
-            if(disableButton){
-              elem.attr('disabled', true);
-              var id = 'transparent-button-layer-'+Math.ceil(Math.random()*100000);
-              elem.attr('disable-layer', id);
-              var layer = $('<div id="'+id+'" class="transparent-button-layer"></div>');
-              var position = elem.position();
-              var prop = makePropGetter(elem);
-              layer.css({
-                top: position.top + prop('marginTop'),
-                left: position.left + prop('marginLeft'),
-                width: elem.width() + prop('paddingLeft')+
-                                      prop('paddingRight')+
-                                      prop('borderLeft')+
-                                      prop('borderRight'),
-                height: elem.height() + prop('paddingTop')+
-                                      prop('paddingBottom')+
-                                      prop('borderTop')+
-                                      prop('borderBottom'),
-              });
-              layer.prependTo(elem.parent());
-              layer.tooltip({
-                title: reasons,
-                trigger: 'hover'
-              });
-            }
-          } else if(hideButton){
-            elem.addClass('ng-hide');
-          }
+        var canParams = parseCanExpression(attrs.cfCan);
+        var canResults = evalExpressions(_.clone(canParams), scope.can);
+        var paramIndex = _.findIndex(canResults, function (val) {
+          return !val.can;
         });
+        if(paramIndex < 0) return;
+
+        var reasons = enforcements.determineEnforcement(canResults[paramIndex].reasons, canParams[paramIndex][1]);
+
+        var disableButton = !('cfCanNoDisable' in attrs);
+        var hideButton = !('cfCanNoHide' in attrs);
+        if(reasons){
+          if(disableButton){
+            elem.attr('disabled', true);
+            var id = 'transparent-button-layer-'+Math.ceil(Math.random()*100000);
+            elem.attr('disable-layer', id);
+            var layer = $('<div id="'+id+'" class="transparent-button-layer"></div>');
+            var position = elem.position();
+            var prop = makePropGetter(elem);
+            layer.css({
+              top: position.top + prop('marginTop'),
+              left: position.left + prop('marginLeft'),
+              width: elem.width() + prop('paddingLeft')+
+                                    prop('paddingRight')+
+                                    prop('borderLeft')+
+                                    prop('borderRight'),
+              height: elem.height() + prop('paddingTop')+
+                                    prop('paddingBottom')+
+                                    prop('borderTop')+
+                                    prop('borderBottom'),
+            });
+            layer.prependTo(elem.parent());
+            layer.tooltip({
+              title: reasons.tooltip,
+              trigger: 'hover'
+            });
+          }
+        } else if(hideButton){
+          elem.addClass('ng-hide');
+        }
       });
     }
   };
--- spec/javascripts/contentful/unit/directives/cf_can_directive_spec.js
+++ spec/javascripts/contentful/unit/directives/cf_can_directive_spec.js
@@ -3,28 +3,35 @@
 describe('The can directive', function () {
 
   var container, elem, scope;
-  var canStub;
-  var usageStub;
+  var canStub, reasonsStub;
+  var enforcementSpy;
   var compileElement;
 
   beforeEach(function () {
     canStub = sinon.stub();
-    usageStub = sinon.stub();
+    reasonsStub = sinon.stub();
     module('contentful/test', function ($provide) {
-      $provide.value('can', canStub);
+      $provide.value('reasonsDenied', reasonsStub);
       $provide.value('authorization', {
-        spaceContext: {}
+        spaceContext: {
+          space: {
+            sys: { createdBy: { sys: {id: 123} } }
+          }
+        }
       });
-      $provide.value('determineEnforcement', {
-        computeUsage: usageStub
+      var userStub = sinon.stub();
+      userStub.returns({ sys: {id: 123} });
+      $provide.value('authentication', {
+        getUser: userStub
       });
     });
-    inject(function ($rootScope, $compile) {
+    inject(function ($rootScope, $compile, enforcements) {
       scope = $rootScope.$new();
       scope.can = canStub;
+      enforcementSpy = sinon.spy(enforcements, 'determineEnforcement');
 
-      compileElement = function (extra) {
-        container = $('<div><div cf-can="can(\'create\', \'Entry\')" '+extra+'></div></div>');
+      compileElement = function (expression, extra) {
+        container = $('<div><div cf-can="'+expression+'" '+extra+'></div></div>');
         $compile(container)(scope);
         scope.$digest();
         elem = container.find('[cf-can]');
@@ -33,6 +40,7 @@ describe('The can directive', function () {
   });
 
   afterEach(inject(function ($log) {
+    enforcementSpy.restore();
     container.remove();
     $('.transparent-button-layer').remove();
     $log.assertEmpty();
@@ -42,7 +50,12 @@ describe('The can directive', function () {
   describe('lets the element be used', function () {
     beforeEach(function () {
       canStub.returns(true);
-      compileElement();
+      reasonsStub.returns(undefined);
+      compileElement('create, Entry', 'class="ng-hide" disabled="disabled"');
+    });
+
+    it('can is called with supplied condition', function () {
+      expect(canStub.args[0]).toEqual(['create', 'Entry']);
     });
 
     it('has no hide class', function () {
@@ -57,7 +70,12 @@ describe('The can directive', function () {
   describe('hides the element', function () {
     beforeEach(function () {
       canStub.returns(false);
-      compileElement();
+      reasonsStub.returns(undefined);
+      compileElement('create, Entry');
+    });
+
+    it('can is called with supplied condition', function () {
+      expect(canStub.args[0]).toEqual(['create', 'Entry']);
     });
 
     it('has a hide class', function () {
@@ -74,12 +92,16 @@ describe('The can directive', function () {
     beforeEach(function () {
       tooltipStub = sinon.stub($.fn, 'tooltip');
       canStub.returns(false);
-      usageStub.returns('reasons');
-      compileElement('cf-can-entity="entry"');
+      reasonsStub.returns(['usageExceeded']);
+      compileElement('create, Entry');
     });
 
     afterEach(function () {
       tooltipStub.restore();
+    });
+
+    it('can is called with supplied condition', function () {
+      expect(canStub.args[0]).toEqual(['create', 'Entry']);
     });
 
     it('has no hide class', function () {
@@ -90,63 +112,97 @@ describe('The can directive', function () {
       expect(elem.attr('disabled')).toBeTruthy();
     });
 
+    it('enforcement is called with reason', function () {
+      expect(enforcementSpy.args[0][0]).toEqual(['usageExceeded']);
+    });
+
+    it('enforcement is called with entity type', function () {
+      expect(enforcementSpy.args[0][1]).toEqual('Entry');
+    });
+
     it('creates a transparent button layer', function () {
       expect(container.find('.transparent-button-layer').get(0)).toBeDefined();
     });
 
+    it('sets a tooltip on button layer', function () {
+      expect(tooltipStub.args[0][0].title).toMatch(/Entries/g);
+    });
+
+  });
+
+  describe('hides the element with multiple conditions', function () {
+    beforeEach(function () {
+      canStub.withArgs('create', 'Entry').returns(true);
+      reasonsStub.withArgs('create', 'Entry').returns(undefined);
+      canStub.withArgs('create', 'Asset').returns(false);
+      reasonsStub.withArgs('create', 'Asset').returns(undefined);
+      compileElement('[create, Entry], [create, Asset]');
+    });
+
+    it('can is called with supplied condition first time', function () {
+      expect(canStub.args[0]).toEqual(['create', 'Entry']);
+    });
+
+    it('can is called with supplied condition second time', function () {
+      expect(canStub.args[1]).toEqual(['create', 'Asset']);
+    });
+
+    it('has no hide class', function () {
+      expect(elem.hasClass('ng-hide')).toBeTruthy();
+    });
+
+    it('is deactivated', function () {
+      expect(elem.attr('disabled')).toBeFalsy();
+    });
+  });
+
+  describe('disables the element with multiple conditions', function () {
+    var tooltipStub;
+    beforeEach(function () {
+      tooltipStub = sinon.stub($.fn, 'tooltip');
+      canStub.withArgs('create', 'Entry').returns(true);
+      reasonsStub.withArgs('create', 'Entry').returns(undefined);
+      canStub.withArgs('create', 'Asset').returns(false);
+      reasonsStub.withArgs('create', 'Asset').returns(['usageExceeded']);
+      compileElement('[create, Entry], [create, Asset]');
+    });
+
+    afterEach(function () {
+      tooltipStub.restore();
+    });
+
+    it('can is called with supplied condition first time', function () {
+      expect(canStub.args[0]).toEqual(['create', 'Entry']);
+    });
+
+    it('can is called with supplied condition second time', function () {
+      expect(canStub.args[1]).toEqual(['create', 'Asset']);
+    });
+
+    it('has no hide class', function () {
+      expect(elem.hasClass('ng-hide')).toBeFalsy();
+    });
+
+    it('is deactivated', function () {
+      expect(elem.attr('disabled')).toBeTruthy();
+    });
+
+    it('enforcement is called with reason', function () {
+      expect(enforcementSpy.args[0][0]).toEqual(['usageExceeded']);
+    });
+
+    it('enforcement is called with entity type', function () {
+      expect(enforcementSpy.args[0][1]).toEqual('Asset');
+    });
+
     it('creates a transparent button layer', function () {
-      expect(tooltipStub.args[0][0]).toEqual({
-        title: 'reasons',
-        trigger: 'hover'
-      });
+      expect(container.find('.transparent-button-layer').get(0)).toBeDefined();
     });
+
+    it('sets a tooltip on button layer', function () {
+      expect(tooltipStub.args[0][0].title).toMatch(/Assets/g);
+    });
+
   });
-
-  /*
-  describe('hides the element with an OR condition', function () {
-    beforeEach(function () {
-      canStub.returns(false);
-      compileElement('create, Entry || create, Asset');
-    });
-
-    it('has no hide class', function () {
-      expect(elem.hasClass('ng-hide')).toBeTruthy();
-    });
-
-    it('is deactivated', function () {
-      expect(elem.attr('disabled')).toBeFalsy();
-    });
-  });
-
-  describe('hides the element with an AND condition', function () {
-    beforeEach(function () {
-      canStub.returns(false);
-      compileElement('create, Entry && create, Asset');
-    });
-
-    it('has no hide class', function () {
-      expect(elem.hasClass('ng-hide')).toBeTruthy();
-    });
-
-    it('is deactivated', function () {
-      expect(elem.attr('disabled')).toBeFalsy();
-    });
-  });
-
-  describe('hides the element with a mixed condition', function () {
-    beforeEach(function () {
-      canStub.returns(false);
-      compileElement('create, Entry && create, Asset || update, Asset');
-    });
-
-    it('has no hide class', function () {
-      expect(elem.hasClass('ng-hide')).toBeTruthy();
-    });
-
-    it('is deactivated', function () {
-      expect(elem.attr('disabled')).toBeFalsy();
-    });
-  });
- */
 
 });
