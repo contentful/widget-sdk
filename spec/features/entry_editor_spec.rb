6b3286ea4a17132ab9283753831b72a62773263f
Jan Varwig <jan@varwig.org>
2013-12-11 15:42:30 +0100

    More Entry Editor spec

--- spec/features/entry_editor_spec.rb
+++ spec/features/entry_editor_spec.rb
@@ -8,7 +8,6 @@ feature 'Entry Editor', js: true, sauce: true do
     ensure_login
     remove_test_space
     create_test_space
-    create_content_type 'Text'
   end
 
   after do
@@ -16,6 +15,7 @@ feature 'Entry Editor', js: true, sauce: true do
   end
 
   scenario 'Playing with the tab actions' do
+    create_content_type 'Text'
     add_button 'Entry with Text'
     edit_field('textField', 'en-US', 'textarea').set 'Foo'
     click_button 'Publish'
@@ -36,6 +36,42 @@ feature 'Entry Editor', js: true, sauce: true do
     find('li.delete-confirm').click
     page.should have_selector('.tab-title', count: 1)
     expect_success 'deleted successfully'
+  end
+
+  scenario 'toggling languages and disabled fields' do
+    nav_bar 'space-settings'
+    tab_iframe do
+      click_link 'Locales'
+      click_link 'New Locale'
+      fill_in 'locale_name', with: 'German'
+      fill_in 'locale_code', with: 'de-DE'
+      click_button 'Create Locale'
+    end
+
+    create_content_type 'Text' do
+      add_field 'Localized Field', 'Text', localized: true
+      add_field 'Disabled Field', 'Text'
+      wait_for_sharejs
+      click_button 'Activate'
+      find('.toggle-disabled').click
+    end
+
+    add_button 'Entry with Text'
+    wait_for_sharejs
+    page.should have_selector('.form-field[data-field-id=textField] textarea')
+    page.should have_selector('.form-field[data-field-id=localizedField] .locale[data-locale="en-US"] textarea')
+
+    find('.editor-top-right .dropdown-toggle').click
+    find('label', text: 'German').click
+    page.should have_selector('.form-field[data-field-id=textField] textarea')
+    page.should have_selector('.form-field[data-field-id=localizedField] .locale[data-locale="en-US"] textarea')
+    page.should have_selector('.form-field[data-field-id=localizedField] .locale[data-locale="de-DE"] textarea')
+
+    find('label', text: 'disabled').click
+    page.should have_selector('.form-field[data-field-id=textField] textarea')
+    page.should have_selector('.form-field[data-field-id=localizedField] .locale[data-locale="en-US"] textarea')
+    page.should have_selector('.form-field[data-field-id=localizedField] .locale[data-locale="de-DE"] textarea')
+    page.should have_selector('.form-field[data-field-id=disabledField] textarea')
   end
 
   # The different editing widgets should be tested separately
