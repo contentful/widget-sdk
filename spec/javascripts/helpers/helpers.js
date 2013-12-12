d72ead7caedcafbd7ef93061075d29e4b31516b5
Tiago Rodrigues <tmcrodrigues@gmail.com>
2013-12-12 17:13:52 +0100

    Add some spec performance gathering code

--- spec/javascripts/helpers/helpers.js
+++ spec/javascripts/helpers/helpers.js
@@ -1,4 +1,16 @@
+/*global performance*/
 'use strict';
+
+// uncomment for gathering spec performance
+/*
+window.specPerformance = window.specPerformance || {
+  results: [],
+  sorted: function () {
+    return window.specPerformance.results.sort(function(a,b){return a.time > b.time;});
+  }
+};
+*/
+
 
 beforeEach(function() {
   this.addMatchers({
@@ -6,6 +18,34 @@ beforeEach(function() {
       return angular.equals(this.actual, other);
     }
   });
+  if(window.specPerformance){
+    this.performanceStart = performance.now();
+  }
+});
+
+afterEach(function() {
+  function specName(spec) {
+    var name = '';
+    if(spec.suite.description){
+      name += spec.suite.description;
+    }
+    var parent, previousParent;
+    while(parent = (spec.suite && spec.suite.parentSuite || parent && parent.parentSuite)){
+      if(parent && previousParent && parent.description == previousParent.description) break;
+      previousParent = parent;
+      name = parent.description+ ' ' +name;
+    }
+    return name + ' ' + spec.description;
+  }
+
+  if(window.specPerformance){
+    var spec = {
+      desc: specName(jasmine.getEnv().currentSpec),
+      time: (performance.now() - this.performanceStart)/1000
+    };
+    window.specPerformance.results.push(spec);
+    console.log(spec.desc, spec.time);
+  }
 });
 
 window.scope = function(elem) {
