180030d9c9de0070d82d5a8f692bc236a9aa876e
Jan Varwig <jan@varwig.org>
2014-01-07 17:27:36 +0100

    Docs, Cleanup

--- spec/javascripts/support/jasmine_helper.rb
+++ spec/javascripts/support/jasmine_helper.rb
@@ -5,6 +5,6 @@
 #Example: using a different boot file.
 Jasmine.configure do |config|
   config.server_port = ENV['JASMINE_PORT'] || 8889
-   #config.boot_dir = '/absolute/path/to/boot_dir'
-   #config.boot_files = lambda { ['/absolute/path/to/boot_dir/file.js'] }
+  #config.boot_dir = '/absolute/path/to/boot_dir'
+  #config.boot_files = lambda { ['/absolute/path/to/boot_dir/file.js'] }
 end
