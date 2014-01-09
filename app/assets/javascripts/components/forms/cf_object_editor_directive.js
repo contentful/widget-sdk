5fe60e77efc39828bef566e67ea92fe675c00ea9
Jan Varwig <jan@varwig.org>
2014-01-09 10:40:12 +0100

    Some improvements to the JSON editor

--- app/assets/javascripts/components/forms/cf_object_editor_directive.js
+++ app/assets/javascripts/components/forms/cf_object_editor_directive.js
@@ -2,12 +2,12 @@
 angular.module('contentful').directive('cfObjectEditor', function(){
   return {
     restrict: 'C',
-    template: '<textarea class="input-xxlarge" ng-model="fieldData.value" ot-bind-model ng-disabled="!otEditable"></textarea>',
+    template: '<textarea class="input-xxlarge" cf-input-autogrow ng-model="fieldData.value" ot-bind-model ng-disabled="!otEditable"></textarea>',
     replace: true,
     require: 'ngModel',
     link: function(scope, elem, attr, ngModel){
       ngModel.$formatters.push(function (object) {
-        return JSON.stringify(object);
+        return JSON.stringify(object, null, 2);
       });
       ngModel.$parsers.push(function (string) {
         try {
