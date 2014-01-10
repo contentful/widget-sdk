a4cf2c98dc8d0a66dab15152238a0d5dc93dfbf8
Tiago Rodrigues <tmcrodrigues@gmail.com>
2014-01-10 15:03:45 +0100

    Asset and Entry list directive tests

--- spec/javascripts/contentful/unit/components/tabs/asset_list/asset_list_directive_spec.js
+++ spec/javascripts/contentful/unit/components/tabs/asset_list/asset_list_directive_spec.js
@@ -13,8 +13,16 @@ describe('The Asset list directive', function () {
     module('contentful/test', function (cfCanStubsProvider) {
       cfCanStubsProvider.setup(reasonsStub);
     });
-    inject(function ($rootScope, $compile) {
+    inject(function ($rootScope, $compile, assetListDirective) {
       scope = $rootScope.$new();
+
+      assetListDirective[0].controller = function () {};
+
+      scope.selection = {
+        getSelected: sinon.stub(),
+        isSelected: sinon.stub()
+      };
+
       scope.can = canStub;
       scope.tab = {
         params: {}
@@ -71,5 +79,40 @@ describe('The Asset list directive', function () {
     compileElement();
     expect(container.find('.results-empty-advice .primary-button').attr('disabled')).toBeUndefined();
   });
+
+  describe('list of assets is filtered', function() {
+    var list;
+    var idStub1, idStub2, idStub3, nameStub, listFilterStub;
+    beforeEach(function() {
+      idStub1 = sinon.stub();
+      idStub1.returns(1);
+      idStub2 = sinon.stub();
+      idStub2.returns(2);
+      idStub3 = sinon.stub();
+      idStub3.returns(3);
+      nameStub = sinon.stub();
+      nameStub.returns('name');
+      scope.assets = [
+        {getId: idStub1, getName: nameStub},
+        {getId: idStub2, getName: nameStub},
+        {getId: idStub3, getName: nameStub}
+      ];
+
+      listFilterStub = sinon.stub();
+      scope.visibleInCurrentList = listFilterStub;
+      listFilterStub.withArgs(scope.assets[0]).returns(true);
+      listFilterStub.withArgs(scope.assets[1]).returns(true);
+      listFilterStub.withArgs(scope.assets[2]).returns(false);
+
+      compileElement();
+      list = container.find('.main-results tbody');
+    });
+
+    it('list has 2 elements', function () {
+      expect(list.find('tr').length).toBe(2);
+    });
+  });
+
+
 
 });
--- spec/javascripts/contentful/unit/components/tabs/entry_list/entry_list_directive_spec.js
+++ spec/javascripts/contentful/unit/components/tabs/entry_list/entry_list_directive_spec.js
@@ -12,10 +12,16 @@ describe('The Entry list directive', function () {
     module('contentful/test', function (cfCanStubsProvider) {
       cfCanStubsProvider.setup(reasonsStub);
     });
-    inject(function ($rootScope, $compile) {
+    inject(function ($rootScope, $compile, entryListDirective) {
       scope = $rootScope.$new();
       scope.can = canStub;
 
+      entryListDirective[0].controller = function () {};
+
+      scope.selection = {
+        getSelected: sinon.stub(),
+        isSelected: sinon.stub()
+      };
       scope.tab = {
         params: {}
       };
@@ -72,6 +78,69 @@ describe('The Entry list directive', function () {
     expect(container.find('.results-empty-advice .primary-button').attr('disabled')).toBeUndefined();
   });
 
+  describe('list of content type filters', function() {
+    var list;
+    var idStub1, idStub2, nameStub;
+    beforeEach(function() {
+      idStub1 = sinon.stub();
+      idStub1.returns(1);
+      idStub2 = sinon.stub();
+      idStub2.returns(2);
+      nameStub = sinon.stub();
+      nameStub.returns('name');
+      scope.tab.params.contentTypeId = 1;
+      scope.spaceContext.publishedContentTypes = [
+        {getId: idStub1, getName: nameStub},
+        {getId: idStub2, getName: nameStub}
+      ];
+      compileElement();
+      list = container.find('.filter-list').eq(1);
+    });
 
+    it('list has 2 elements', function () {
+      expect(list.find('li').length).toBe(2);
+    });
+
+    it('first element is active', function() {
+      expect(list.find('li').eq(0).hasClass('active')).toBeTruthy();
+    });
+
+    it('second element is inactive', function() {
+      expect(list.find('li').eq(1).hasClass('active')).toBeFalsy();
+    });
+  });
+
+  describe('list of entries is filtered', function() {
+    var list;
+    var idStub1, idStub2, idStub3, nameStub, listFilterStub;
+    beforeEach(function() {
+      idStub1 = sinon.stub();
+      idStub1.returns(1);
+      idStub2 = sinon.stub();
+      idStub2.returns(2);
+      idStub3 = sinon.stub();
+      idStub3.returns(3);
+      nameStub = sinon.stub();
+      nameStub.returns('name');
+      scope.entries = [
+        {getId: idStub1, getName: nameStub},
+        {getId: idStub2, getName: nameStub},
+        {getId: idStub3, getName: nameStub}
+      ];
+
+      listFilterStub = sinon.stub();
+      scope.visibleInCurrentList = listFilterStub;
+      listFilterStub.withArgs(scope.entries[0]).returns(true);
+      listFilterStub.withArgs(scope.entries[1]).returns(true);
+      listFilterStub.withArgs(scope.entries[2]).returns(false);
+
+      compileElement();
+      list = container.find('.main-results tbody');
+    });
+
+    it('list has 2 elements', function () {
+      expect(list.find('tr').length).toBe(2);
+    });
+  });
 
 });
