f036c65f0e37386c0458ee17c58006b69f9eb504
Jan Varwig <jan@varwig.org>
2014-01-14 11:07:06 +0100

    Tests for otBindValue Removed otEditor

---
+++ spec/javascripts/contentful/unit/ot/ot_bind_value_spec.js
@@ -0,0 +1,42 @@
+'use strict';
+
+describe('otBindValue', function () {
+  var elem, scope;
+
+  beforeEach(function () {
+    module('contentful/test', function ($provide) {
+      $provide.stubDirective('otPath', {
+        priority: 600,
+        controller: function() { }
+      });
+    });
+    inject(function ($compile, $rootScope) {
+      $rootScope.otPath = 'path';
+      $rootScope.otChangeValue = sinon.stub();
+      $rootScope.fieldData = {value: 'a'};
+      elem = $compile('<div ot-path ot-bind-value="fieldData.value"></div>')($rootScope);
+      scope = elem.scope();
+      scope.$apply();
+      scope.otChangeValue.reset();
+    });
+  });
+
+
+  afterEach(function () {
+    inject(function ($log) {
+      $log.assertEmpty();
+    });
+  });
+
+  it('should call otChangeValue whenever the value is changed', function () {
+    scope.fieldData.value = 'b';
+    scope.$apply();
+    expect(scope.otChangeValue.calledWith('b')).toBe(true);
+  });
+
+  it('should update the model value in response to otValueChanged', function () {
+    scope.$emit('otValueChanged', 'path', 'b');
+    expect(scope.fieldData.value).toBe('b');
+  });
+});
+
