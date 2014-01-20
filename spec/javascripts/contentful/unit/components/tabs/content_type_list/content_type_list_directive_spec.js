3ddbbe668c24e63f47cd315d1d09295772b78409
Tiago Rodrigues <tmcrodrigues@gmail.com>
2014-01-20 12:13:03 +0100

    Content type list directive tests

--- spec/javascripts/contentful/unit/components/tabs/content_type_list/content_type_list_directive_spec.js
+++ spec/javascripts/contentful/unit/components/tabs/content_type_list/content_type_list_directive_spec.js
@@ -12,18 +12,11 @@ describe('The ContentType list directive', function () {
     module('contentful/test', function (cfCanStubsProvider) {
       cfCanStubsProvider.setup(reasonsStub);
     });
-    inject(function ($rootScope, $compile) {
+    inject(function ($rootScope, $compile, contentTypeListDirective) {
       scope = $rootScope.$new();
       scope.can = canStub;
-      scope.spaceContext = {
-        space: {
-        },
-        contentTypes: [],
-        refreshContentTypes: sinon.stub()
-      };
-      scope.tab = {
-        params: {}
-      };
+
+      contentTypeListDirective[0].controller = function () {};
 
       compileElement = function () {
         container = $('<div class="content-type-list"></div>');
@@ -50,5 +43,40 @@ describe('The ContentType list directive', function () {
     compileElement();
     expect(container.find('.results-empty-advice .primary-button').attr('disabled')).toBeUndefined();
   });
+
+  describe('list of content types is filtered', function() {
+    var list;
+    var idStub1, idStub2, idStub3, nameStub, listFilterStub;
+    beforeEach(function() {
+      idStub1 = sinon.stub();
+      idStub1.returns(1);
+      idStub2 = sinon.stub();
+      idStub2.returns(2);
+      idStub3 = sinon.stub();
+      idStub3.returns(3);
+      nameStub = sinon.stub();
+      nameStub.returns('name');
+      scope.contentTypes = [
+        {getId: idStub1, getName: nameStub},
+        {getId: idStub2, getName: nameStub},
+        {getId: idStub3, getName: nameStub}
+      ];
+
+      listFilterStub = sinon.stub();
+      scope.visibleInCurrentList = listFilterStub;
+      listFilterStub.withArgs(scope.contentTypes[0]).returns(true);
+      listFilterStub.withArgs(scope.contentTypes[1]).returns(true);
+      listFilterStub.withArgs(scope.contentTypes[2]).returns(false);
+
+      compileElement();
+      list = container.find('.main-results tbody');
+    });
+
+    it('list has 2 elements', function () {
+      expect(list.find('tr').length).toBe(2);
+    });
+  });
+
+
 
 });
