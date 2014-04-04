907b263dfb2c6c2fd7a67b0f0cb347362ffbd86a
Tiago Rodrigues <tmcrodrigues@gmail.com>
2014-04-04 17:15:52 +0200

    don't check for old file

--- app/assets/javascripts/components/forms/cf_file_editor_controller.js
+++ app/assets/javascripts/components/forms/cf_file_editor_controller.js
@@ -4,7 +4,7 @@ angular.module('contentful').controller('CfFileEditorCtrl', function ($scope) {
   $scope.$watch('file', function (file, old, scope) {
     if (!scope.otDoc) return;
     console.log('file was changed', file, old);
-    if (!old && file && !file.url) {
+    if (/*!old && */file && !file.url) {
       scope.$emit('fileUploaded', file);
     } else if (!file && old) {
       scope.$emit('fileRemoved', old);
