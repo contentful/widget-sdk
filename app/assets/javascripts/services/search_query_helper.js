05da34ed2f373f32837674ca51b0de1b06d0c2f4
Jan Varwig <jan@varwig.org>
2014-04-14 14:08:16 +0200

    Autocompletion support for predefined fields, integer ranges and booleans. Removal of disabled fields [Delivers #69349920]

--- app/assets/javascripts/services/search_query_helper.js
+++ app/assets/javascripts/services/search_query_helper.js
@@ -17,7 +17,7 @@ angular.module('contentful').factory('searchQueryHelper', function(searchParser)
     return 'after';
   }
 
-  var stuff = {
+  var api = {
     currentToken: function (queryString, cursorPos) {
       if (!queryString) return null;
 
@@ -33,7 +33,7 @@ angular.module('contentful').factory('searchQueryHelper', function(searchParser)
     },
 
     currentSubToken: function (queryString, cursorPos) {
-      var token = stuff.currentToken(queryString, cursorPos);
+      var token = api.currentToken(queryString, cursorPos);
       if (token && token.type === 'Pair') {
         if (cursorPos <= token.content.operator.offset) {
           return token.content.key;
@@ -46,31 +46,13 @@ angular.module('contentful').factory('searchQueryHelper', function(searchParser)
     },
 
     offerCompletion: function (contentType, queryString, cursorPos) {
-      var token = stuff.currentToken(queryString, cursorPos);
+      var token = api.currentToken(queryString, cursorPos);
       
       if (token && token.type === 'Pair' && token.content.operator.end <= cursorPos) {
         return valueCompletion(token.content.key.content, contentType);
       }
 
       return keyCompletion(contentType);
-
-      function keyCompletion(contentType) {
-        var completions = [];
-        if (contentType)
-          completions.push.apply(completions, _.map(contentType.data.fields, 'id'));
-        completions.push.apply(completions, _.keys(autocompletion));
-        return completions;
-      }
-
-      function valueCompletion(key, contentType) {
-        if (!autocompletion[key]) return;
-        var complete = autocompletion[key].complete;
-        if (_.isFunction(complete)) {
-          return complete(contentType);
-        } else {
-          return complete;
-        }
-      }
     },
 
     buildQuery: function (contentType, queryString) {
@@ -128,35 +110,71 @@ angular.module('contentful').factory('searchQueryHelper', function(searchParser)
         }
       }
     }
-
-    // Date: with all the usual operators, bonus - relative times (e.g. 7 days ago)
-    // Author: intelligent solution - just show people who authored entries,
-    //         basic solution - show all the people in the Space
-    // Content type: intelligent solution display content types with at least one entry,
-    //               basic solution - retrieve all content types within a given space.
-    //
-    //
-    // Content type specific variables:
-    //     Only consider visible fields
-    //     Autocompletion for predefined fields
-    //     Make sure that we grab predefined values from all the supported fields (e.g. not only text, but also symbols, numbers);
-    //     When grabbing predefined values from a text field
-    //         Think through how we will show it if values are > than 50 characters
-    //         there are more than 10 values in the list
-    //     Support filtering through boolean value fields;
-    //     Support for searching assets:
-    //         By specifying asset type
-    //         By specifying asset extension (? if we can do it)
-    //         By allowing to search only asset titles (e.g. asset_title : contains / does not contain X)
-    //         By specifying asset size
-    //         By combining asset creation with the date parameter from above (e.g. I imagine the query would look something like "status:created" "date:before 23, April 2014" "type:asset" "size:less than (Mb):2)
-    //               
   };
+
+  function keyCompletion(contentType) {
+    var completions = [];
+    if (contentType)
+      completions.push.apply(completions, _.keys(createFieldsLookup(contentType)));
+    completions.push.apply(completions, _.keys(autocompletion));
+    return completions;
+  }
+
+  function valueCompletion(key, contentType) {
+    if (!autocompletion[key]) {
+      return predefinedFieldCompletion(key, contentType);
+    }
+
+    var complete = autocompletion[key].complete;
+    if (_.isFunction(complete)) {
+      return complete(contentType);
+    } else {
+      return complete;
+    }
+  }
+
+  function predefinedFieldCompletion(key, contentType) {
+    var field = createFieldsLookup(contentType)[key];
+    if (field) {
+      var val;
+      // Predefined values
+      val = _.find(field.validations, 'in');
+      if (val) return val['in'];
+      // Integer ranges
+      val = field.type === 'Integer' && _.find(field.validations, 'range');
+      if (val) return buildRange(val.range.min, val.range.max);
+      // Booleans
+      if (field.type === 'Boolean') return ['true', 'false'];
+    }
+    return null;
+  }
+
+  function buildRange(min, max) {
+    if (!_.isNumber(max) || !_.isNumber(min) || max - min > 25 ) return null;
+    var res = [];
+    for (var i=min; i<= max; i++) res.push(i);
+    return res;
+  }
+
+  // Date: with all the usual operators, bonus - relative times (e.g. 7 days ago)
+  // Author: intelligent solution - just show people who authored entries,
+  //         basic solution - show all the people in the Space
+  // Content type: intelligent solution display content types with at least one entry,
+  //               basic solution - retrieve all content types within a given space.
+  //
+  //
+  // Content type specific variables:
+  //     Support for searching assets:
+  //         By specifying asset type
+  //         By specifying asset extension (? if we can do it)
+  //         By allowing to search only asset titles (e.g. asset_title : contains / does not contain X)
+  //         By specifying asset size
+  //         By combining asset creation with the date parameter from above (e.g. I imagine the query would look something like "status:created" "date:before 23, April 2014" "type:asset" "size:less than (Mb):2)
 
   function createFieldsLookup(contentType) {
     if (!contentType) return {};
     return _.reduce(contentType.data.fields, function (list, field) {
-      list[field.id] = field;
+      if (!field.disabled) list[field.id] = field;
       return list;
     }, {});
   }
@@ -207,5 +225,5 @@ angular.module('contentful').factory('searchQueryHelper', function(searchParser)
     });
   }
 
-return stuff;
+  return api;
 });
