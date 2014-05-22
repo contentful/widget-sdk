f54b84877092ffe5bff16f9be7688ffbc69d3fa3
Jan Varwig <jan@varwig.org>
2014-05-22 14:36:21 +0200

    Reorder views and folders [#71837566] [#71555906]

--- app/assets/javascripts/components/shared/view_menu_directive.js
+++ app/assets/javascripts/components/shared/view_menu_directive.js
@@ -3,9 +3,6 @@ angular.module('contentful').directive('viewMenu', function(){
   return {
     restrict: 'A',
     template: JST['view_menu'](),
-    link: function(scope, elem, attr){
-      
-    },
     controller: function ($scope) {
       $scope.folderStates = {};
 
@@ -19,6 +16,27 @@ angular.module('contentful').directive('viewMenu', function(){
 
       $scope.isFolderOpen = function (folder) {
         return $scope.folderStates[folder.id] !== 'closed';
+      };
+
+      $scope.$watch('canEditUiConfig', function (can) {
+        $scope.viewMenuEditable = can;
+        $scope.viewSortOptions.disabled = !can;
+        $scope.folderSortOptions.disabled = !can;
+      });
+
+      $scope.viewSortOptions = {
+        connectWith: '[ui-sortable=viewSortOptions]',
+        stop: function () {
+          $scope.saveEntryListViews();
+        }
+      };
+
+      $scope.folderSortOptions = {
+        connectWith: '[ui-sortable=folderSortOptions]',
+        handle: 'header',
+        stop: function () {
+          $scope.saveEntryListViews();
+        }
       };
     }
   };
