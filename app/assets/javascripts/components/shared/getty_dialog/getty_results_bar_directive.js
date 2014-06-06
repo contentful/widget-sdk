9251456abc43ced994f418308500b32022dee3d1
Tiago Rodrigues <tmcrodrigues@gmail.com>
2014-06-06 17:06:02 +0200

    fixed results bar for getty dialog

---
+++ app/assets/javascripts/components/shared/getty_dialog/getty_results_bar_directive.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('contentful').directive('gettyResultsBar', function(){
+  return {
+    template: JST.getty_results_bar(),
+    restrict: 'C',
+    scope: true,
+    link: function (scope, elem, attrs) {
+      scope.isActive = function () {
+        return !!scope.$eval(attrs.activeWhen);
+      };
+    }
+  };
+});
