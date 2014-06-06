ba0ca14f52776c4e6b8a69b809603b97ec76287a
Jan Varwig <jan@varwig.org>
2014-06-06 12:41:32 +0200

    ui-sortable for view customizer

--- app/assets/javascripts/components/tabs/entry_list/view_customizer_directive.js
+++ app/assets/javascripts/components/tabs/entry_list/view_customizer_directive.js
@@ -4,37 +4,7 @@ angular.module('contentful').directive('viewCustomizer', function(){
   return {
     template: JST.view_customizer(),
     restrict: 'C',
-    link: function (scope, elem) {
-
-      // Index is always -1 because of fixed items in list
-      function getIndex(ui) {
-        return ui.item.index() - 1;
-      }
-
-      elem.find('.displayed-fields').sortable({
-        axis: 'y',
-        cancel: '.close-button',
-        containment: '.displayed-fields',
-        cursor: 'move',
-        items: 'li[data-field-id]',
-        start: function (ev, ui) {
-          ui.item.startIndex = getIndex(ui);
-          ui.item.addClass('dragging');
-        },
-        stop: function (ev, ui) {
-          ui.item.removeClass('dragging');
-        },
-        update: function (ev, ui) {
-          var oldIndex = ui.item.startIndex;
-          var newIndex = getIndex(ui);
-          delete ui.item.startIndex;
-          scope.$apply(function(){
-            var list = scope.tab.params.view.displayedFieldIds;
-            list.splice(newIndex, 0, list.splice(oldIndex, 1)[0]);
-          });
-        }
-      });
-
+    link: function (scope) {
       scope.displayField = function (ev, field) {
         ev.stopPropagation();
         scope.addDisplayField(field);
@@ -43,6 +13,20 @@ angular.module('contentful').directive('viewCustomizer', function(){
       scope.hideField = function (ev, field) {
         ev.stopPropagation();
         scope.removeDisplayField(field);
+      };
+    },
+    controller: function ($scope) {
+      $scope.displayedFieldsSortOptions = {
+        axis: 'y',
+        cancel: '.close-button',
+        containment: '.displayed-fields[ui-sortable]',
+        cursor: 'move',
+        start: function (ev, ui) {
+          _.defer(function () { ui.item.addClass('dragging'); });
+        },
+        stop: function (ev, ui) {
+          ui.item.removeClass('dragging');
+        },
       };
     }
   };
