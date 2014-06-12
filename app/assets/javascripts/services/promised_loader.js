44a60634a9d92fcd8cfe80e40fbd3fe26d169cd6
Tiago Rodrigues <tmcrodrigues@gmail.com>
2014-06-12 15:06:43 +0200

    fix debounce parameters

--- app/assets/javascripts/services/promised_loader.js
+++ app/assets/javascripts/services/promised_loader.js
@@ -23,7 +23,7 @@ angular.module('contentful').factory('PromisedLoader', function ($q, $rootScope,
     _loadCallback: debounce(function (host, methodName, args) {
       this.startLoading();
       host[methodName].apply(host, args);
-    }, 500),
+    }, 500, {leading: true}),
 
     loadCallback: function (host, methodName/*, args[] */) {
       var deferred = $q.defer();
@@ -77,7 +77,7 @@ angular.module('contentful').factory('PromisedLoader', function ($q, $rootScope,
         deferred.reject(err);
         loader.endLoading();
       });
-    }, 500),
+    }, 500, {leading: true}),
 
     loadPromise: function (promiseLoader/*, args[]*/) {
       var deferred = $q.defer();
@@ -87,7 +87,7 @@ angular.module('contentful').factory('PromisedLoader', function ($q, $rootScope,
         return deferred.promise;
       }
 
-      this._load(promiseLoader, args, deferred);
+      this._loadPromise(promiseLoader, args, deferred);
 
       return deferred.promise;
     }
