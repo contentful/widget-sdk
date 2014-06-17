1ecd71aab69f94223cbdf3e881553921b8c4c199
Jan Varwig <jan@varwig.org>
2014-06-17 16:15:43 +0200

    Extract common functionality from AssetListViewsController and EntryListViewsController into ListViewsController

---
+++ app/assets/javascripts/components/shared/list_views_controller.js
@@ -0,0 +1,41 @@
+'use strict';
+angular.module('contentful').controller('ListViewsController', function($scope, modalDialog, notification,
+                                                                       getBlankView, viewCollectionName, generateDefaultViews, resetList){
+  var blankView = getBlankView();
+
+  $scope.tab.params.view = $scope.tab.params.view || getBlankView();
+
+  $scope.$watch('uiConfig', function (uiConfig) {
+    if (uiConfig && !uiConfig[viewCollectionName]) {
+      uiConfig[viewCollectionName] = generateDefaultViews();
+    }
+  });
+
+  $scope.resetViews = function () {
+    $scope.uiConfig[viewCollectionName] = generateDefaultViews();
+    $scope.saveViews();
+  };
+
+  $scope.clearView = function () {
+    $scope.tab.params.view = getBlankView();
+    resetList();
+  };
+
+  $scope.loadView = function (view) {
+    $scope.tab.params.view = _.cloneDeep(view);
+    $scope.tab.params.view.title = 'New View';
+    resetList();
+  };
+
+  $scope.saveViews = function () {
+    return $scope.saveUiConfig().catch(function () {
+      notification.serverError('Error trying to save view');
+    });
+  };
+
+  $scope.viewIsActive = function (view){
+    var p = $scope.tab.params.view;
+    if (!view) view = blankView;
+    return p.id === view.id;
+  };
+});
