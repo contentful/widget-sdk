220a11e47210517cdaeae2e0508230461997e2bd
Jan Varwig <jan@varwig.org>
2014-07-11 11:29:46 +0200

    InsertAssetDialog that uses AssetSearchController and Views

--- app/assets/javascripts/components/forms/insert_asset_dialog_controller.js
+++ app/assets/javascripts/components/forms/insert_asset_dialog_controller.js
@@ -1,6 +1,31 @@
 'use strict';
 angular.module('contentful').controller('InsertAssetDialogController', ['$scope', '$injector', function($scope, $injector){
-  var keycodes = $injector.get('keycodes');
+  var $controller       = $injector.get('$controller');
+  var keycodes          = $injector.get('keycodes');
+  var searchQueryHelper = $injector.get('searchQueryHelper');
+
+  $scope.canEditUiConfig = false;
+  $scope.currentView = null;
+  $scope.assetContentType = searchQueryHelper.assetContentType;
+  $controller('AssetListViewsController', {
+    $scope: $scope,
+    currentViewLocation: 'currentView'
+  });
+  $scope.searchController = $controller('AssetSearchController', {
+    $scope: $scope,
+    getSearchTerm: getSearchTerm
+  });
+
+  $scope.$watch(function pageParameters(scope){
+    return {
+      searchTerm:  scope.searchTerm,
+      page:        scope.searchController.paginator.page,
+      pageLength:  scope.searchController.paginator.pageLength,
+    };
+  }, function(pageParameters, old, scope){
+    scope.searchController.resetAssets(pageParameters.page === old.page);
+  }, true);
+
 
   $scope.entities = [];
   $scope.selectAsset = function (asset) {
@@ -10,4 +35,8 @@ angular.module('contentful').controller('InsertAssetDialogController', ['$scope'
   $scope.handleKeys = function (event) {
     if (event.keyCode === keycodes.ENTER) event.stopPropagation();
   };
+
+  function getSearchTerm() {
+    return $scope.searchTerm;
+  }
 }]);
