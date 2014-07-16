6ce1bbf130c0054c5c56b75c12a5af98fe2e4f22
Tiago Rodrigues <tmcrodrigues@gmail.com>
2014-07-16 12:12:38 +0200

    reindent and clean api key editor modules

--- app/assets/javascripts/components/tabs/api_key_editor/api_key_editor_controller.js
+++ app/assets/javascripts/components/tabs/api_key_editor/api_key_editor_controller.js
@@ -1,108 +1,112 @@
 'use strict';
 
-angular.module('contentful').controller('ApiKeyEditorCtrl', ['$scope', 'authentication', 'environment', 'notification', 'sentry', '$window', function($scope, authentication, environment, notification, sentry, $window) {
+angular.module('contentful').controller('ApiKeyEditorCtrl', ['$scope', '$injector', function($scope, $injector) {
+  var environment = $injector.get('environment');
+  var notification = $injector.get('notification');
+  var sentry = $injector.get('sentry');
+  var $window = $injector.get('$window');
 
-    var deviceRegexps = {
-      iOS: /(iphone os|ipad|iphone)/gi
-    };
+  var deviceRegexps = {
+    iOS: /(iphone os|ipad|iphone)/gi
+  };
 
-    var detectedDevice = 'any';
+  var detectedDevice = 'any';
 
-    _.forEach(deviceRegexps, function (re, id) {
-      if(re.test($window.navigator.userAgent)){
-        detectedDevice = id;
-        return false;
-      }
-    });
-
-    $scope.$watch('tab.params.apiKey', 'apiKey=tab.params.apiKey');
-
-    $scope.tab.closingMessage = 'You have unsaved changes.';
-    $scope.tab.closingMessageDisplayType = 'dialog';
-
-    $scope.authCodeExampleLang = 'http';
-
-    $scope.$watch('apiKey.data.name', function(name) {
-      $scope.headline = $scope.tab.title = name || 'Untitled';
-    });
-
-    $scope.$watch('apiKey.data.accessToken', function(accessToken) {
-      $scope.exampleUrl =
-        'http://' +
-        environment.settings.cdn_host +
-        '/spaces/' +
-        $scope.spaceContext.space.getId() +
-        '/entries?access_token=' +
-        accessToken;
-
-      $scope.mobileAppUrl =
-        'contentful://open/space/' +
-        $scope.spaceContext.space.getId() +
-        '?access_token=' +
-        accessToken;
-    });
-
-    $scope.isDevice = function (id) {
-      return id === detectedDevice;
-    };
-
-    $scope.$watch('apiKeyForm.$dirty', function (modified, old, scope) {
-      scope.tab.dirty = modified;
-    });
-
-    function title() {
-      return '"' + $scope.apiKey.getName() + '"';
+  _.forEach(deviceRegexps, function (re, id) {
+    if(re.test($window.navigator.userAgent)){
+      detectedDevice = id;
+      return false;
     }
+  });
 
-    $scope.editable = function() {
-      return true;
-    };
+  $scope.$watch('tab.params.apiKey', 'apiKey=tab.params.apiKey');
 
-    $scope['delete'] = function() {
-      var t = title();
-      $scope.apiKey['delete'](function(err) {
-        $scope.$apply(function() {
-          if (err) {
-            notification.warn(t + ' could not be deleted');
-            sentry.captureServerError('ApiKey could not be deleted', err);
-            return;
-          }
-          notification.info(t + ' deleted successfully');
-          $scope.broadcastFromSpace('entityDeleted', $scope.apiKey);
-        });
+  $scope.tab.closingMessage = 'You have unsaved changes.';
+  $scope.tab.closingMessageDisplayType = 'dialog';
+
+  $scope.authCodeExampleLang = 'http';
+
+  $scope.$watch('apiKey.data.name', function(name) {
+    $scope.headline = $scope.tab.title = name || 'Untitled';
+  });
+
+  $scope.$watch('apiKey.data.accessToken', function(accessToken) {
+    $scope.exampleUrl =
+      'http://' +
+      environment.settings.cdn_host +
+      '/spaces/' +
+      $scope.spaceContext.space.getId() +
+      '/entries?access_token=' +
+      accessToken;
+
+    $scope.mobileAppUrl =
+      'contentful://open/space/' +
+      $scope.spaceContext.space.getId() +
+      '?access_token=' +
+      accessToken;
+  });
+
+  $scope.isDevice = function (id) {
+    return id === detectedDevice;
+  };
+
+  $scope.$watch('apiKeyForm.$dirty', function (modified, old, scope) {
+    scope.tab.dirty = modified;
+  });
+
+  function title() {
+    return '"' + $scope.apiKey.getName() + '"';
+  }
+
+  $scope.editable = function() {
+    return true;
+  };
+
+  $scope['delete'] = function() {
+    var t = title();
+    $scope.apiKey['delete'](function(err) {
+      $scope.$apply(function() {
+        if (err) {
+          notification.warn(t + ' could not be deleted');
+          sentry.captureServerError('ApiKey could not be deleted', err);
+          return;
+        }
+        notification.info(t + ' deleted successfully');
+        $scope.broadcastFromSpace('entityDeleted', $scope.apiKey);
       });
-    };
-
-    $scope.$on('entityDeleted', function (event, apiKey) {
-      if (event.currentScope !== event.targetScope) {
-        var scope = event.currentScope;
-        if (apiKey === scope.apiKey)
-          scope.tab.close();
-      }
     });
+  };
 
-    $scope.save = function() {
-      var t = title();
-      $scope.apiKey.save(function(err) {
-        $scope.$apply(function() {
-          if (err) {
-            notification.warn(t + ' could not be saved');
-            sentry.captureServerError('ApiKey could not be saved', err);
-            return;
-          }
-          $scope.apiKeyForm.$setPristine();
-          $scope.navigator.apiKeyEditor($scope.apiKey).goTo();
-          notification.info(t + ' saved successfully');
-        });
+  $scope.$on('entityDeleted', function (event, apiKey) {
+    if (event.currentScope !== event.targetScope) {
+      var scope = event.currentScope;
+      if (apiKey === scope.apiKey)
+        scope.tab.close();
+    }
+  });
+
+  $scope.save = function() {
+    var t = title();
+    $scope.apiKey.save(function(err) {
+      $scope.$apply(function() {
+        if (err) {
+          notification.warn(t + ' could not be saved');
+          sentry.captureServerError('ApiKey could not be saved', err);
+          return;
+        }
+        $scope.apiKeyForm.$setPristine();
+        $scope.navigator.apiKeyEditor($scope.apiKey).goTo();
+        notification.info(t + ' saved successfully');
       });
-    };
+    });
+  };
 
+  $scope.deleteConfirm = false;
+  $scope.activateDeleteConfirm = function () {
+    $scope.deleteConfirm = true;
+  };
+
+  $scope.deactivateDeleteConfirm = function () {
     $scope.deleteConfirm = false;
-    $scope.activateDeleteConfirm = function () {
-      $scope.deleteConfirm = true;
-    };
-
-    $scope.deactivateDeleteConfirm = function () {
-      $scope.deleteConfirm = false;
-    };
+  };
   }]);
