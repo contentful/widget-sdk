7f3d5a1f6bd8cb6b95adb4aad048c8bd01380c7a
Tiago Rodrigues <tmcrodrigues@gmail.com>
2014-07-17 15:17:45 +0200

    improve bugsnag reporting for evaluation

--- app/assets/javascripts/services/exception_handler.js
+++ app/assets/javascripts/services/exception_handler.js
@@ -6,8 +6,7 @@ angular.module('contentful').provider('$exceptionHandler', function () {
       return function(exception) {
         $log.error.apply($log, arguments);
         sentry.captureException(exception, {extra: {'promptedReload': true}});
-        if(window.Bugsnag)
-          window.Bugsnag.notifyException(exception);
+        sentry.captureBugSnag(exception);
         if (environment.env != 'development') {
           var ReloadNotification = $injector.get('ReloadNotification');
           ReloadNotification.trigger();
