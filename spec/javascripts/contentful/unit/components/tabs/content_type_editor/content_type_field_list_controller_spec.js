e79f37176b982a604e599a2ce49a16655129c0d7
Tiago Rodrigues <tmcrodrigues@gmail.com>
2014-07-25 11:32:12 +0200

    split field actions into separate controller

--- spec/javascripts/contentful/unit/components/tabs/content_type_editor/content_type_field_list_controller_spec.js
+++ spec/javascripts/contentful/unit/components/tabs/content_type_editor/content_type_field_list_controller_spec.js
@@ -93,52 +93,6 @@ describe('ContentTypeFieldList Controller', function () {
     });
   });
 
-  describe('toggle a field', function () {
-    var field;
-    beforeEach(function () {
-      field = {uiid: 123};
-      scope.toggleField(field);
-    });
-
-    it('field is open', function () {
-      expect(scope.isFieldOpen(field)).toBeTruthy();
-    });
-
-    it('field is closed', function () {
-      scope.toggleField(field);
-      expect(scope.isFieldOpen(field)).toBeFalsy();
-    });
-  });
-
-  describe('open a field', function () {
-    var field;
-    beforeEach(function () {
-      field = {uiid: 123};
-    });
-
-    it('field is closed', function () {
-      expect(scope.isFieldOpen(field)).toBeFalsy();
-    });
-
-    it('field is open', function () {
-      scope.openField(field);
-      expect(scope.isFieldOpen(field)).toBeTruthy();
-    });
-  });
-
-  describe('click a field', function () {
-    var field;
-    beforeEach(function () {
-      field = {uiid: 123};
-      scope.openField = sinon.stub();
-      scope.fieldClicked(field);
-    });
-
-    it('field is opened', function () {
-      expect(scope.openField).toBeCalledWith(field);
-    });
-  });
-
   it('get field type params', function () {
     expect(scope.fieldTypeParams({
       type: 'type',
