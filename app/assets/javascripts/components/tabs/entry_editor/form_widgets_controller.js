24772f369c21bf3db4e75d51e809ddba2373451b
Tiago Rodrigues <tmcrodrigues@gmail.com>
2014-07-31 14:54:34 +0200

    fix typo

--- app/assets/javascripts/components/tabs/entry_editor/form_widgets_controller.js
+++ app/assets/javascripts/components/tabs/entry_editor/form_widgets_controller.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('contentful').controller('FormWidgetsController', ['$scope', '$injector', function FormFieldsController($scope, $injector){
+angular.module('contentful').controller('FormWidgetsController', ['$scope', '$injector', function FormWidgetsController($scope, $injector){
   var controller = this;
   var editingInterfaces = $injector.get('editingInterfaces');
   var sentry            = $injector.get('sentry');
