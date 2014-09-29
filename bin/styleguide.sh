388eb327bfac56c285e2c96404b1b932e0f3211a
Tiago Rodrigues <tmcrodrigues@gmail.com>
2014-09-29 11:11:56 +0200

    update styleguide and some naming schemes

--- bin/styleguide.sh
+++ bin/styleguide.sh
@@ -4,6 +4,7 @@ set -e
 
 pushd app/assets/stylesheets
 stylus --use ../../../node_modules/nib/ < main.styl > compiled.css
-kss-node --css compiled.css .
+stylus --use ../../../node_modules/nib/ < styleguide_template/public/custom.styl > styleguide_template/public/custom.css 
+kss-node --css compiled.css --template styleguide_template .
 rm -f compiled.css
 popd
