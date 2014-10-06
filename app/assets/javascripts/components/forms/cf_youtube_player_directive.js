6c41691c61b621c5676369b4d9f7bfcb76b44010
Jan Varwig <jan@varwig.org>
2014-10-06 13:46:57 +0200

    Make better use of  and closures in youtube player

--- app/assets/javascripts/components/forms/cf_youtube_player_directive.js
+++ app/assets/javascripts/components/forms/cf_youtube_player_directive.js
@@ -1,61 +1,51 @@
 'use strict';
 
 angular.module('contentful').directive('cfYoutubePlayer', ['$injector', function($injector){
-
+  var $window = $injector.get('$window');
   var youtubePlayerLoader = $injector.get('youtubePlayerLoader');
 
   var YOUTUBE_DOM_ELEMENT_CLASS = '.youtube-player';
-
-  function installPlayerInDOM(element, YoutubePlayer) {
-    return new YoutubePlayer(element, { width: '100%' });
-  }
-
-  function handlePlayerReady(scope, attrs, event) {
-    scope.player = event.target;
-    scope.$apply(function(){
-      scope.handlePlayerInstalled();
-      _.result(scope, attrs.onReady);
-    });
-  }
-
-  function handlePlayerStateChange(scope, attrs, event) {
-    if (event.data == YT.PlayerState.CUED){
-      scope.$apply(function(){
-        _.result(scope, attrs.onReadyToPlayVideo);
-      });
-    }
-  }
-
-  function handlePlayerError(scope, attrs, event) {
-    scope.$apply(function(){
-      _.result(scope, attrs.onFailedToPlayVideo);
-    });
-  }
-
-  function handlePlayerLoaded(scope, attrs, elem, YoutubePlayer) {
-      var player = installPlayerInDOM(elem.find(YOUTUBE_DOM_ELEMENT_CLASS)[0], YoutubePlayer);
-
-      player.addEventListener('onReady', _.curry(handlePlayerReady)(scope, attrs));
-      player.addEventListener('onStateChange', _.curry(handlePlayerStateChange)(scope, attrs));
-      player.addEventListener('onError', _.curry(handlePlayerError)(scope, attrs));
-  }
-
-  function handlePlayerLoadError(scope, attrs) {
-    return function(){
-      _.result(scope, attrs.onFailure);
-    };
-  }
-
-  function link(scope, elem, attrs) {
-    youtubePlayerLoader.load().then( _.curry(handlePlayerLoaded)(scope, attrs, elem), handlePlayerLoadError(scope, attrs));
-  }
 
   return {
     restrict   : 'E',
     scope      : true,
     template   : JST['cf_youtube_player'](),
     controller : 'cfYoutubePlayerController',
-    link       : link
+    link       : function link(scope, elem, attrs){
+      youtubePlayerLoader.load()
+      .then(function(YoutubePlayer){
+        var player = installPlayerInDOM(elem.find(YOUTUBE_DOM_ELEMENT_CLASS)[0], YoutubePlayer);
+        player.addEventListener('onReady',       handlePlayerReady);
+        player.addEventListener('onStateChange', handlePlayerStateChange);
+        player.addEventListener('onError',       handlePlayerError);
+      })
+      .catch(function(){
+        scope.$apply(attrs.onFailure);
+      });
+
+      function installPlayerInDOM(element, YoutubePlayer) {
+        return new YoutubePlayer(element, { width: '100%' });
+      }
+
+      function handlePlayerReady(event) {
+        scope.player = event.target;
+        scope.$apply(function(){
+          scope.handlePlayerInstalled();
+          scope.$eval(attrs.onReady);
+        });
+      }
+
+      function handlePlayerStateChange(event) {
+        if (event.data == $window.YT.PlayerState.CUED){
+          scope.$apply(attrs.onReadyToPlayVideo);
+        }
+      }
+
+      function handlePlayerError() {
+        scope.$apply(attrs.onFailedToPlayVideo);
+      }
+
+    }
   };
 
 }]);
