a66977d192c95856b7665c2c3846336181eded97
Jan Varwig <jan@varwig.org>
2014-10-20 15:39:01 +0200

    Create basic implementation of reordering widgets

--- app/assets/javascripts/components/tabs/editing_interface_editor/editing_interface_editor_directive.js
+++ app/assets/javascripts/components/tabs/editing_interface_editor/editing_interface_editor_directive.js
@@ -4,6 +4,12 @@ angular.module('contentful').directive('editingInterfaceEditor', [function(){
   return {
     template: JST.editing_interface_editor(),
     restrict: 'C',
-    controller: 'EditingInterfaceEditorController'
+    controller: 'EditingInterfaceEditorController',
+    link: function(scope){
+      scope.widgetSortOptions = {
+        //handle: '[cf-drag-handle]',
+        forceHelperSize: true,
+      };
+    }
   };
 }]);
