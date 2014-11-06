788ec4ee2ae0231ed5ab59711675646c02260dfe
Jan Varwig <jan@varwig.org>
2014-11-06 10:26:13 +0100

    Enable editingInterfaceEditor on staging/dev

--- app/assets/javascripts/components/tabs/content_type_editor/content_type_editor_controller.js
+++ app/assets/javascripts/components/tabs/content_type_editor/content_type_editor_controller.js
@@ -6,12 +6,13 @@ angular.module('contentful').controller('ContentTypeEditorController', ['$scope'
   var addCanMethods     = $injector.get('addCanMethods');
   var analytics         = $injector.get('analytics');
   var editingInterfaces = $injector.get('editingInterfaces');
+  var environment       = $injector.get('environment');
   var notification      = $injector.get('notification');
   var random            = $injector.get('random');
   var validation        = $injector.get('validation');
 
   $scope.fieldSchema = validation(validation.schemas.ContentType.at(['fields']).items);
-  this.interfaceEditorEnabled = $scope.user.features.showPreview;
+  this.interfaceEditorEnabled = $scope.user.features.showPreview || environment.env !== 'production';
 
   $scope.$watch('tab.params.contentType', 'contentType=tab.params.contentType');
 
