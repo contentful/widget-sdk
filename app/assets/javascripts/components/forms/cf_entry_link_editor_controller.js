87d8ba35e85c8b2caf88af831927901a728b78b0
Tiago Rodrigues <tmcrodrigues@gmail.com>
2014-11-24 14:43:13 +0100

    Change how EntryLinkEditorController inherits
    Fixes changes in how inheritance works on Angular 1.3

--- app/assets/javascripts/components/forms/cf_entry_link_editor_controller.js
+++ app/assets/javascripts/components/forms/cf_entry_link_editor_controller.js
@@ -3,9 +3,11 @@
 angular.module('contentful').controller('EntryLinkEditorController', [
   '$scope', '$attrs', '$controller', '$injector', function ($scope, $attrs, $controller, $injector) {
 
+  var controller = this;
+
   var EntityCache = $injector.get('EntityCache');
 
-  var entryLinkEditorController = $controller('LinkEditorController', {
+  $controller('LinkEditorController', {
     $scope: $scope,
     ngModel: $attrs.ngModel,
     linkParams: {
@@ -14,33 +16,41 @@ angular.module('contentful').controller('EntryLinkEditorController', [
       validationType: 'linkContentType',
       multiple: $attrs.linkMultiple
     },
-    setValidationType: function (linkTypeValidation) {
-      $scope.linkContentTypes = _(linkTypeValidation.contentTypeId)
-        .map(function (id) { return $scope.spaceContext.getPublishedContentType(id); })
-        .compact()
-        .value();
-      // TODO This means the validation contains unpublished content  types.
-      // It should never happen but I don't know how to deal with it here
-      if ($scope.linkContentTypes.length === 0) $scope.linkContentTypes = null;
-    }
+    setValidationType: setValidationType
   });
 
   // Cache for assets linked from linked entries
-  entryLinkEditorController.linkedAssetsCache = new EntityCache($scope.spaceContext.space, 'getAssets');
+  controller.linkedAssetsCache = new EntityCache($scope.spaceContext.space, 'getAssets');
+
+  controller.linkTitle = linkTitle;
+  controller.linkDescription = linkDescription;
+  controller.entityExists = entityExists;
 
   $scope.$on('$destroy', function () {
-    entryLinkEditorController.linkedAssetsCache = null;
+    controller.linkedAssetsCache = null;
   });
 
-  entryLinkEditorController.linkTitle = function(entity) {
+  return controller;
+
+  function setValidationType(linkTypeValidation) {
+    $scope.linkContentTypes = _(linkTypeValidation.contentTypeId)
+      .map(function (id) { return $scope.spaceContext.getPublishedContentType(id); })
+      .compact()
+      .value();
+    // TODO This means the validation contains unpublished content  types.
+    // It should never happen but I don't know how to deal with it here
+    if ($scope.linkContentTypes.length === 0) $scope.linkContentTypes = null;
+  }
+
+  function linkTitle(entity) {
     if (entityExists(entity)) {
       return $scope.spaceContext.entryTitle(entity, $scope.locale.code);
     } else {
       return '(Missing entity)';
     }
-  };
+  }
 
-  entryLinkEditorController.linkDescription = function (entity) {
+  function linkDescription(entity) {
     if(entityExists(entity)){
       var contentType = $scope.spaceContext.publishedTypeForEntry(entity);
       var field = _.find(contentType.data.fields, function(field){
@@ -50,13 +60,10 @@ angular.module('contentful').controller('EntryLinkEditorController', [
         return $scope.spaceContext.localizedField(entity, 'data.fields.'+field.id);
     }
     return null;
-  };
+  }
 
-  entryLinkEditorController.entityExists = entityExists;
   function entityExists(entity) {
     return entity && !entity.isMissing && entity.getId();
   }
-
-  return entryLinkEditorController;
 
 }]);
