f5144b968393cfb27265b618f361bd2003158979
Aditya Bhatt <aditya@bhatts.org>
2015-01-16 14:03:26 +0100

    Add a method scope.otChangeStringP
     This computes the operations required to change the string from the
     old value to the new one, and gives them to ShareJS.

--- app/assets/javascripts/ot/ot_path.js
+++ app/assets/javascripts/ot/ot_path.js
@@ -29,6 +29,63 @@ angular.module('contentful').directive('otPath', ['ShareJS', 'cfSpinner', '$q', 
         }
       });
 
+      /**
+       * Replaces a string type field in the document  with a new string value by
+       * computing the difference and generating a set of ShareJS operations to
+       * manipulate the older string. This is useful in keeping the workflow natural
+       * for editors in the scenario when a field is being hand-edited by one editor
+       * but programmatically updated in the client of the other editor.
+       * It also optimistically sends small operations whenever required instead of
+       * sending the entire replacement string, which would be expensive with the kind
+       * of incremental changes performed during typing.
+       *
+       * TL;DR - Simulates manual editing of the old string into the new string
+       * for more natural collaboration.
+       */
+      $scope.otChangeStringP = function (newValue) {
+        if ($scope.otDoc) {
+          var oldValue = $scope.otGetValue();
+          var cb = $q.callbackWithApply(),
+              cbOp1 = $q.callbackWithApply(),
+              cbOp2 = $q.callbackWithApply();
+
+          if (!oldValue || !newValue) {
+            ShareJS.mkpath({
+              doc: $scope.otDoc,
+              path: $scope.otPath,
+              types: $scope.otPathTypes,
+              value: newValue || null
+            }, cb);
+            return cb.promise;
+          } else if (oldValue === newValue) {
+            return $q.when(cb.promise);
+          } else {
+            var commonStart = 0,
+                commonEnd = 0,
+                oldEnd = oldValue.length - 1,
+                newEnd = newValue.length - 1;
+
+            while (oldValue.charAt(commonStart) === newValue.charAt(commonStart)) {
+              commonStart += 1;
+            }
+            while (oldValue.charAt(oldEnd - commonEnd) === newValue.charAt(newEnd - commonEnd) &&
+                   commonStart + commonEnd < oldValue.length && commonStart + commonEnd < newValue.length) {
+              commonEnd += 1;
+            }
+
+            if (oldValue.length !== commonStart + commonEnd) {
+              $scope.otDoc.deleteTextAt($scope.otPath, oldValue.length - commonStart - commonEnd, commonStart, cbOp1);
+            }
+            if (newValue.length !== commonStart + commonEnd) {
+              $scope.otDoc.insertAt($scope.otPath, commonStart, newValue.slice(commonStart, newValue.length - commonEnd), cbOp2);
+            }
+            return $q.all(cbOp1.promise, cbOp2.promise);
+          }
+        } else {
+          return $q.reject('No otDoc to push to');
+        }
+      };
+
       $scope.otChangeValueP = function (value) {
         if ($scope.otDoc) {
           var stopSpin = cfSpinner.start();
