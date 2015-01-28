4fa306ec886a95816de28320761aaeecde09df0b
Thomas Scholtes <thomas-scholtes@gmx.de>
2015-01-28 16:28:03 +0100

    Separate widget rendering and field editing
    The code that is responsible for rendering a editor widget is extracted
    into a separate `cf-widget-renderer` directive. The `cf-field-editor`
    directive only is responsible for syncing the editing widget data with
    the edited entity.

--- src/javascripts/components/shared/field_editor/cf_field_editor_directive.js
+++ src/javascripts/components/shared/field_editor/cf_field_editor_directive.js
@@ -1,56 +1,9 @@
 'use strict';
 
-// To build a nice widget that works with ShareJS and updates the local model correctly
-// it needs to require an ngModelCtrl and do this:
-//
-// var ngModelGet = $parse(attr.ngModel),
-//     ngModelSet = ngModelGet.assign;
-//
-// changeHandler (widget value changed internally)
-//   ShareJS submit
-//    success: ngModelCtrl.$setViewValue(internal value)
-//    fail: reset internal value to ngModelCtr.$modelValue
-//          OR simply abort if nothing has been changed on the scope
-//
-// ngModelCtrl.$render
-//    set internal value to ngModelCtrl.$viewValue
-//
-// otValueChanged
-//   ngModelSet(scope, incomingValue)
-//
-// This is implemented in a generic fashion in otBindInternal
-
-angular.module('contentful').directive('cfFieldEditor', ['$injector', function($injector) {
-  var $compile    = $injector.get('$compile');
-  var widgets = $injector.get('widgets');
-
+angular.module('contentful').directive('cfFieldEditor', [function() {
   return {
-    restrict: 'C',
+    restrict: 'A',
     require: '^otPath',
     controller: 'CfFieldEditorController',
-    link: function (scope, element) {
-      var childScope;
-      scope.$watch('widget.widgetId', installWidget);
-      scope.$on('$destroy', clearChildScope);
-
-      function installWidget(widgetId) {
-        if (childScope) {
-          childScope.$destroy();
-          childScope = null;
-          element.empty();
-        }
-        if (widgetId) {
-          var template = widgets.widgetTemplate(widgetId);
-          childScope = scope.$new();
-          var $widget = $(template);
-          element.append($widget);
-          $compile($widget)(childScope);
-        }
-      }
-
-      function clearChildScope() {
-        childScope = null;
-      }
-    }
   };
 }]);
