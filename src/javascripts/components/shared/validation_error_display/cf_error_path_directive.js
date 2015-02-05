d326f56e8ed05282e279088d9873515433098401
Thomas Scholtes <thomas-scholtes@gmx.de>
2015-02-05 10:31:42 +0100

    Refactor and document cf-error-path directive
    * Move some code from the `link` function into the controller.
    * Separate error messages from `$scope`
    * Use `dotty`

--- src/javascripts/components/shared/validation_error_display/cf_error_path_directive.js
+++ src/javascripts/components/shared/validation_error_display/cf_error_path_directive.js
@@ -1,23 +1,28 @@
 'use strict';
 
+/**
+ * Provide error messages for the selected part of a document from the
+ * `validate` directive.
+ *
+ * The directive exposes the errors for the part of the document corresponding to
+ * the `cf-error-path` attribute. It provides the following scope properties:
+ *
+ * - `errorMessages` A list of error message strings.
+ * - `hasErrors` True if and only if there are any error messages.
+ * - `noErrors` Complement of `hasErrors`
+ *
+ * In addition it hides the element if there are no error messages.
+ *
+ */
 angular.module('contentful').directive('cfErrorPath', function () {
   return {
     scope: true,
     controller: 'ErrorPathController',
     require: 'cfErrorPath',
     link: function (scope, elem, attrs) {
-      scope.$watch('errorMessages.length', function (numErrors) {
-        if (0 < numErrors) {
-          scope.noErrors = false;
-          scope.hasErrors = true;
-          if (attrs['ngHide'] || attrs['ngShow']) return;
-          elem.show();
-        } else {
-          scope.noErrors = true;
-          scope.hasErrors = false;
-          if (attrs['ngHide'] || attrs['ngShow']) return;
-          elem.hide();
-        }
+      scope.$watch('hasErrors', function (hasErrors) {
+        if (!attrs['ngHide'] && !attrs['ngShow'])
+          elem.toggle(hasErrors);
       });
     }
   };
