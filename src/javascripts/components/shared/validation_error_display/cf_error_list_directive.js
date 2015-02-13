566e265cf9e70875584a960f04b7e7ca9afc8b03
Thomas Scholtes <thomas-scholtes@gmx.de>
2015-02-13 13:58:11 +0100

    Separate error directives and styling
    Replaces the `cf-form-box-error` and `cf-form-inline-error` class
    directives wieh one `cf-error-list` directive. Styling is handled with
    the `.form-box-error` and `.form-inline-error` classes.
    
    All instances of the obsolete directives have been greped for and
    replaced.

---
+++ src/javascripts/components/shared/validation_error_display/cf_error_list_directive.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * Shows a list of errors in `scope.errors.messages`.
+ *
+ * The directive hides its element if there are no error messages.
+ */
+angular.module('contentful').directive('cfErrorList', function () {
+  return {
+    restrict: 'E',
+    template: JST['cf_error_list'],
+    link: function(scope, element, attrs) {
+      scope.$watchCollection('errors.messages', function(messages) {
+        if (attrs['ngHide'] || attrs['ngShow'])
+          return;
+
+        if (messages && messages.length > 0)
+          element.show();
+        else
+          element.hide();
+      });
+    }
+  };
+});
