51267c03092c4701b39bb7cfba90da409c9a9100
Thomas Scholtes <thomas-scholtes@gmx.de>
2015-02-18 09:27:24 +0100

    Add 'cf-error-messages' directive
    This will be used in the validation settings to display errors.

---
+++ src/javascripts/components/shared/validation_error_display/cf_error_messages_directive.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * Specify the source of error messages for the `cf-error-list`
+ * directive.
+ *
+ * @example(jade)
+ *   cf-error-list(cf-error-messages='me.errors')
+ *
+ * The directive sets the scope's `error.messages` property by
+ * evaluating the attribute value on the scope. In the above example
+ * `scope.error.messages` is set to `scope.me.errors`
+ *
+ * This serves as a simpler alternative to the `cf-error-path`
+ * directive.
+ */
+angular.module('contentful').directive('cfErrorMessages', function() {
+  return {
+    restrict: 'A',
+    controller: ['$scope', '$attrs', function($scope, $attrs) {
+      $scope.$watch($attrs.cfErrorMessages, function(messages) {
+        $scope.errors = {messages: messages};
+      });
+    }]
+  };
+});
