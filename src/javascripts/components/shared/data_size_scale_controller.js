51e9d0133456d4d7448ca7190ed96bbf4a63bd63
Thomas Scholtes <thomas-scholtes@gmx.de>
2015-03-05 10:55:36 +0100

    Added controller for scaled data size

---
+++ src/javascripts/components/shared/data_size_scale_controller.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/**
+ * Manipulate a raw value by setting the unit and a scaled value.
+ *
+ * The controller exposes to properties on its scope. The `unitFactor`
+ * and the `value` numbers. This numbers are multiplied and written to
+ * a parent scope property. The name of the parent scope property is
+ * given by the `data-model` attribute.
+ *
+ * The controller has a `units` object whose keys are labels for the
+ * units and whose values are the unit factors.
+ */
+angular.module('contentful')
+.controller('DataSizeScaleController',
+['$scope', '$attrs', '$injector', function($scope, $attrs, $injector) {
+  var $parse = $injector.get('$parse');
+  var controller = this;
+
+  var getModelValue = _.partial($parse($attrs.model), $scope.$parent);
+  var setModelValue = _.partial($parse($attrs.model).assign, $scope.$parent);
+
+  var units = controller.units = {
+    'Bytes': 1,
+    'kB':    1024,
+    'MB':    1024 * 1024,
+  };
+
+  var unitFactors = _.sortBy(_.values(units));
+
+  $scope.unitFactor = getUnitFactor(getModelValue());
+
+  $scope.$watch($attrs.model, setScaledValue);
+  $scope.$watch('unitFactor', computeRawValue);
+  $scope.$watch('value', computeRawValue);
+
+  function computeRawValue() {
+    var raw = _.isFinite($scope.value) ? $scope.value * $scope.unitFactor : null;
+    setModelValue(raw);
+  }
+
+  function setScaledValue(raw) {
+    $scope.value = _.isFinite(raw) ? raw / $scope.unitFactor : null;
+  }
+
+  /**
+   * Return the largest unit factor such that the scaled value is
+   * larger than 1.
+   */
+  function getUnitFactor(baseValue) {
+    var factor = _.findLast(unitFactors, function(factor) {
+      return baseValue / factor >= 1;
+    });
+    return factor || unitFactors[0];
+  }
+}]);
---
+++ test/unit/components/shared/data_size_scale_controller_spec.js
@@ -0,0 +1,37 @@
+'use strict';
+
+describe('DataSizeScaleController', function() {
+
+  beforeEach(module('contentful/test'));
+
+  beforeEach(function() {
+    var $controller = this.$inject('$controller');
+    this.parentScope = this.$inject('$rootScope');
+    this.parentScope.size = 1;
+    this.scope = this.parentScope.$new();
+    this.controller = $controller('DataSizeScaleController', {
+      $scope: this.scope,
+      $attrs: {model: 'size'}});
+    this.scope.$apply();
+  });
+
+  it('update model value if unit changes', function() {
+    this.scope.unitFactor = 1024;
+    this.scope.$apply();
+    expect(this.parentScope.size).toEqual(1024);
+  });
+
+  it('update model value if scaled value changes', function() {
+    this.scope.value = 3;
+    this.scope.$apply();
+    expect(this.parentScope.size).toEqual(3);
+  });
+
+  it('update scaled value if model value changes', function() {
+    this.scope.unitFactor = 1024;
+    this.parentScope.size = 2048;
+    this.scope.$apply();
+    expect(this.scope.value).toEqual(2);
+  });
+
+});
