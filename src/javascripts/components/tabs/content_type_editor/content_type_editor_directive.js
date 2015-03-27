ebf6607459cca60503fab5f7d78fc8246cd1dc08
Tiago Rodrigues <tmcrodrigues@gmail.com>
2015-03-27 15:59:49 +0100

    Created initial form field focusing directives
    Replaced code in entry/asset editor and inline editor with two
    directives (cf-focus-on-render and cf-focus-ot-input) which will focus
    the element or child input of the element they are attached to.

--- src/javascripts/components/tabs/content_type_editor/content_type_editor_directive.js
+++ src/javascripts/components/tabs/content_type_editor/content_type_editor_directive.js
@@ -1,16 +1,10 @@
 'use strict';
 
-angular.module('contentful').directive('cfContentTypeEditor', ['$timeout', function($timeout){
+angular.module('contentful').directive('cfContentTypeEditor', [function(){
   return {
     template: JST.content_type_editor(),
     restrict: 'A',
     controller: 'ContentTypeEditorController',
-    controllerAs: 'ctEditorController',
-    link: function (scope, elem) {
-      var input = elem.find('[name=contentTypeForm] input').eq(0);
-      $timeout(function () {
-        input.focus();
-      }, 750);
-    }
+    controllerAs: 'ctEditorController'
   };
 }]);
