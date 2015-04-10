8ba7f79ef98bb36c1b443612f91d702c69e54ec0
Aditya Bhatt <aditya@bhatts.org>
2015-04-10 16:16:26 +0200

    Improved breadcrumb templating
    * No tooltips if length < 50
    * Moved sref and type calculation into the directive

--- src/javascripts/components/client/cf_activity_breadcrumbs_directive.js
+++ src/javascripts/components/client/cf_activity_breadcrumbs_directive.js
@@ -4,6 +4,22 @@ angular.module('contentful').directive('cfActivityBreadcrumbs', function () {
   return {
     template: JST.cf_activity_breadcrumbs(),
     restrict: 'AE',
-    replace: true
+    replace: true,
+    link: function ($scope) {
+      $scope.title = function (entity) {
+        switch(entity.getType()) {
+          case 'Entry': return $scope.spaceContext.entryTitle(entity);
+          case 'Asset': return $scope.spaceContext.assetTitle(entity);
+          default     : return '';
+        }
+      };
+      $scope.sref = function (entity) {
+        switch(entity.getType()) {
+          case 'Entry': return 'spaces.detail.entries.detail({ entryId: "' + entity.getId() + '", addToContext: true })';
+          case 'Asset': return 'spaces.detail.assets.detail({ assetId: "' + entity.getId() + '", addToContext: true })';
+          default     : return '';
+        }
+      };
+    }
   };
 });
