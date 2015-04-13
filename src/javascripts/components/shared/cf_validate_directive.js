cd1bda5cf84be8dba2809ef5edf4bbadc888b6a2
Thomas Scholtes <thomas-scholtes@gmx.de>
2015-04-13 17:10:17 +0200

    Add ValidationController to error message test
    Instead of stubing the behavior of the controller from the `cfValidate`
    directive, we include it in the tests and stub the schema.
    
    This is preparation work for moving logic from the Error Path
    Controller to the Validation Controller.

--- src/javascripts/components/shared/cf_validate_directive.js
+++ src/javascripts/components/shared/cf_validate_directive.js
@@ -1,89 +1,96 @@
 'use strict';
 
-angular.module('contentful').directive('cfValidate', ['validation', function (validation) {
+angular.module('contentful').directive('cfValidate', [function () {
   return {
     restrict: 'A',
     scope: true,
-    controller: ['$scope', '$attrs', function ($scope, $attrs) {
-      $scope.validationResult = {};
-
-      $scope.validate = function () {
-        validate(getData(), getSchema());
-        return $scope.validationResult.valid;
-      };
-
-      function getSchema() {
-        if ('withSchema' in $attrs) {
-          return $scope.$eval($attrs.withSchema);
-        } else {
-          var data = getData();
-          switch (data && data.sys && data.sys.type) {
-            case 'ContentType':
-              return validation.schemas.ContentType;
-            case 'Asset':
-              throw new Error('Validating Assets requires passing a schema in the "withSchema"-attribute.');
-            case 'Entry':
-              throw new Error('Validating Entries requires passing a schema in the "withSchema"-attribute.');
-            default:
-              return null;
-          }
-        }
-      }
-
-      function getData() {
-        return $scope.$eval($attrs.cfValidate);
-      }
-
-      function validate(data, schema){
-        if(!data || !schema) return;
-        if(!data.fields) data.fields = {};
-        var schemaErrors = schema.errors(data);
-        $scope.setValidationResult(schemaErrors, data, schema);
-      }
-
-      function pathErrors(errors) {
-        var retval = {};
-        _.each(errors, function (error) {
-          var pointer = retval;
-          for (var i = 0, l = error.path.length; i < l; i ++) {
-            var pathSeg = error.path[i];
-            pointer = pointer[pathSeg] = pointer[pathSeg] || {};
-          }
-        });
-        return retval;
-      }
-
-      $scope.setValidationResult = function (schemaErrors, data, schema) {
-        var errors = _.filter(schemaErrors, function (error) {
-          if(error && error.path) {
-            return error.path[error.path.length - 1] != '$$hashKey';
-          } else {
-            return true;
-          }
-        });
-        var valid = _.isEmpty(errors);
-        $scope.validationResult = {
-          data: data,
-          schema: schema,
-          errors: errors,
-          valid:  valid,
-          pathErrors: pathErrors(errors)
-        };
-      };
-
-      $scope.setValidationErrors = function (schemaErrors) {
-        var data = getData();
-        var schema = getSchema();
-        if (!data || !schema) return;
-
-        $scope.setValidationResult(schemaErrors, data, schema);
-      };
-
-      $scope.$on('$destroy', function (event) {
-        var scope = event.currentScope;
-        scope.validationResult = {};
-      });
-
-    }]
+    controller: 'ValidationController'
   };
+}]);
+
+angular.module('contentful')
+.controller('ValidationController', ['$injector', '$scope', '$attrs',
+function ValidationController ($injector, $scope, $attrs) {
+  var validation          = $injector.get('validation');
+
+  $scope.validationResult = {};
+
+  $scope.validate = function () {
+    validate(getData(), getSchema());
+    return $scope.validationResult.valid;
+  };
+
+  function getSchema() {
+    if ('withSchema' in $attrs) {
+      return $scope.$eval($attrs.withSchema);
+    } else {
+      var data = getData();
+      switch (data && data.sys && data.sys.type) {
+        case 'ContentType':
+          return validation.schemas.ContentType;
+        case 'Asset':
+          throw new Error('Validating Assets requires passing a schema in the "withSchema"-attribute.');
+        case 'Entry':
+          throw new Error('Validating Entries requires passing a schema in the "withSchema"-attribute.');
+        default:
+          return null;
+      }
+    }
+  }
+
+  function getData() {
+    return $scope.$eval($attrs.cfValidate);
+  }
+
+  function validate(data, schema){
+    if(!data || !schema) return;
+    if(!data.fields) data.fields = {};
+    var schemaErrors = schema.errors(data);
+    $scope.setValidationResult(schemaErrors, data, schema);
+  }
+
+  function pathErrors(errors) {
+    var retval = {};
+    _.each(errors, function (error) {
+      var pointer = retval;
+      for (var i = 0, l = error.path.length; i < l; i ++) {
+        var pathSeg = error.path[i];
+        pointer = pointer[pathSeg] = pointer[pathSeg] || {};
+      }
+    });
+    return retval;
+  }
+
+  $scope.setValidationResult = function (schemaErrors, data, schema) {
+    var errors = _.filter(schemaErrors, function (error) {
+      if (error && error.path) {
+        return error.path[error.path.length - 1] != '$$hashKey';
+      } else {
+        return true;
+      }
+    });
+
+    var valid = _.isEmpty(errors);
+    $scope.validationResult = {
+      data: data,
+      schema: schema,
+      errors: errors,
+      valid:  valid,
+      pathErrors: pathErrors(errors)
+    };
+  };
+
+  $scope.setValidationErrors = function (schemaErrors) {
+    var data = getData();
+    var schema = getSchema();
+    if (!data || !schema) return;
+
+    $scope.setValidationResult(schemaErrors, data, schema);
+  };
+
+  $scope.$on('$destroy', function (event) {
+    var scope = event.currentScope;
+    scope.validationResult = {};
+  });
+
 }]);
--- test/unit/components/shared/cf_validation_error_display_spec.js
+++ test/unit/components/shared/cf_validation_error_display_spec.js
@@ -5,18 +5,37 @@ describe('ErrorPathController', function () {
 
   beforeEach(module('contentful/test'));
 
-  beforeEach(inject(function ($controller, $rootScope){
-    scope = $rootScope;
+  beforeEach(function () {
+    var $rootScope = this.$inject('$rootScope');
+    var $controller = this.$inject('$controller');
+
+    scope = $rootScope.$new();
+
     scope.entity = {};
-    scope.validationResult = {
-      errors : [],
-      data: scope.entity
-    };
-    controller = $controller('ErrorPathController', {
-      $scope: $rootScope,
-      $attrs: attrs = {}
+    scope.schema = {errors: sinon.stub()};
+
+    $controller('ValidationController', {
+      $scope: scope,
+      $attrs: {
+        cfValidate: 'entity',
+        withSchema: 'schema'
+      }
     });
-  }));
+
+    attrs = {};
+    controller = $controller('ErrorPathController', {
+      $scope: scope,
+      $attrs: attrs
+    });
+
+    this.processError = function (error) {
+      if (error)
+        scope.schema.errors.returns([error]);
+      scope.validate();
+      scope.$apply();
+    };
+
+  });
 
   describe('"size" error message', function () {
     beforeEach(function () {
@@ -26,26 +45,26 @@ describe('ErrorPathController', function () {
           bar: null
         }
       };
-      scope.validationResult.data = scope.entity;
-      scope.validationResult.errors.push({
+
+      scope.schema.errors.returns([{
         name: 'size',
         path: ['foo', 'bar'],
         min: 10
-      });
+      }]);
     });
 
     it('shows error for string data', function () {
       scope.entity.foo.bar = 'asdasd';
-      scope.$apply();
-      expect(controller.messages[0])
-      .toBe('Please expand the text so it\'s no shorter than 10 characters');
+      this.processError();
+      expect(controller.messages)
+      .toEqual(['Please expand the text so it\'s no shorter than 10 characters']);
     });
 
     it('shows error for array data', function () {
       scope.entity.foo.bar = [1,2,3];
-      scope.$apply();
-      expect(controller.messages[0])
-      .toBe('Please provide at least 10 items');
+      this.processError();
+      expect(controller.messages)
+      .toEqual(['Please provide at least 10 items']);
     });
   });
 
@@ -57,58 +76,52 @@ describe('ErrorPathController', function () {
           bars: ['asdasd', 'asdasd','asdasd']
         }
       };
-      scope.validationResult.data = scope.entity;
     });
 
     it('builds size error message', function () {
-      scope.validationResult.errors.push({
+      this.processError({
         name: 'size',
         path: ['foo', 'bars'],
         min: 10
       });
-      scope.$apply();
       expect(controller.messages)
       .toEqual(['Please provide at least 10 items']);
     });
 
     it('shows custom error message', function () {
-      scope.validationResult.errors.push({
+      this.processError({
         name: 'size',
         path: ['foo', 'bars'],
         customMessage: 'CUSTOM MESSAGE',
         min: 10
       });
-      scope.$apply();
       expect(controller.messages).toEqual(['CUSTOM MESSAGE']);
     });
 
     it('falls back to "details" property', function () {
-      scope.validationResult.errors.push({
+      this.processError({
         name: 'this is an unknown validation',
         path: ['foo', 'bars'],
         details: 'DETAILS'
       });
-      scope.$apply();
       expect(controller.messages).toEqual(['DETAILS']);
     });
 
     it('falls back to error name property', function () {
-      scope.validationResult.errors.push({
+      this.processError({
         name: 'this is an unknown validation',
         path: ['foo', 'bars'],
       });
-      scope.$apply();
       expect(controller.messages)
       .toEqual(['Error: this is an unknown validation']);
     });
 
     it('shows errors in sub-path', function () {
-      scope.validationResult.errors.push({
+      this.processError({
         name: 'size',
         path: ['foo', 'bars', 1],
         min: 10
       });
-      scope.$apply();
       expect(controller.messages)
       .toEqual(['Please expand the text so it\'s no shorter than 10 characters']);
     });
