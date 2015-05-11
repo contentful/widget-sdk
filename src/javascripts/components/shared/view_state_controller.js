ce8c4222ff40858505e92ab1f0c7b9c3b087395c
Tiago Rodrigues <tmcrodrigues@gmail.com>
2015-05-11 11:28:56 +0200

    Add view state controller

---
+++ src/javascripts/components/shared/view_state_controller.js
@@ -0,0 +1,53 @@
+'use strict';
+
+angular.module('contentful').controller('ViewStateController', ['$injector', '$scope', 'defaultState', function ViewStateController($injector, $scope, defaultState) {
+  /**
+   * @ngdoc controller
+   * @name ViewStateController
+   *
+   * @usage
+   *  $scope.viewState = $controller('ViewStateController', {
+   *    $scope: $scope,
+   *    defaultState: 'loading' // optional
+   *  });
+   *
+   * @description
+   * Simple directive to help management of a view with multiple states
+   * which do not require separate views or navigation changes
+   *
+   */
+
+  var state = defaultState;
+
+  /**
+   * @ngdoc method
+   * @name ViewStateController#set
+   *
+   * @param {string} name of state to transition to
+  */
+  this.set = function (newState) {
+    state = newState;
+  };
+
+  /**
+   * @ngdoc method
+   * @name ViewStateController#get
+  */
+  this.get = function () {
+    return state;
+  };
+
+  /**
+   * @ngdoc method
+   * @name ViewStateController#is
+   * @description
+   * Verifies if the expected state is the current state.
+   * Useful for use in conjunction with ng-show/ng-if/ng-switch in templates.
+   *
+   * @param {string} name of state to verify
+  */
+  this.is = function (expectedState) {
+    return state === expectedState;
+  };
+
+}]);
