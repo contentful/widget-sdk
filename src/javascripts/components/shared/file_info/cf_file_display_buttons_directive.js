18172fbf62cd0ddf66660f2265132066e6a3b83c
Thomas Scholtes <thomas@contentful.com>
2015-07-03 09:45:04 +0200

    Move logic from asset preview buttons into controller

--- src/javascripts/components/shared/file_info/cf_file_display_buttons_directive.js
+++ src/javascripts/components/shared/file_info/cf_file_display_buttons_directive.js
@@ -3,6 +3,35 @@
 angular.module('contentful').directive('cfFileDisplayButtons', function () {
   return {
     restrict: 'A',
-    template: JST.cf_file_display_buttons
+    template: JST.cf_file_display_buttons,
+    controller: ['$scope', '$state', function ($scope, $state) {
+
+      $scope.tryOpenAsset = function () {
+        var entity = $scope.entity;
+        if (entity && !entity.isMissing) {
+          $state.go('spaces.detail.assets.detail', {
+            assetId: entity.getId(),
+            addToContext: true
+          });
+        }
+      };
+
+      $scope.canOpenAsset = function () {
+        return !$scope.enableUpload && $scope.entity && !$scope.entity.isMissing;
+      };
+
+      $scope.canEditFile = function () {
+        var file = $scope.file;
+        var isReady = $scope.imageHasLoaded && file && file.url;
+        return !$scope.otDisabled && $scope.enableUpload && isReady;
+      };
+
+      $scope.canDeleteFile = function () {
+        var file = $scope.file;
+        var uploaded = file && (file.url || file.upload);
+        return !$scope.otDisabled && $scope.deleteFile && uploaded;
+      };
+
+    }]
   };
 });
