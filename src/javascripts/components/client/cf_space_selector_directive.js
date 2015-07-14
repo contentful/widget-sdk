e066324d8cc41fb076c14d180bd2fee86ee79e61
Thomas Scholtes <thomas@contentful.com>
2015-07-14 14:58:28 +0200

    feat(context-menu): Implement new context menu styles

--- src/javascripts/components/client/cf_space_selector_directive.js
+++ src/javascripts/components/client/cf_space_selector_directive.js
@@ -4,6 +4,17 @@ angular.module('contentful').directive('cfSpaceSelector', function() {
   return {
     template: JST.cf_space_selector(),
     restrict: 'E',
-    replace: true
+    replace: true,
+    controller: ['$scope', function ($scope) {
+
+      $scope.$watch('spaces.length', function (len) {
+        $scope.hasSpaces = len && len > 0;
+      });
+
+      $scope.isCurrentSpace = function (space) {
+        return space.getId() === $scope.getCurrentSpaceId();
+      };
+
+    }]
   };
 });
