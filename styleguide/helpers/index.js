146ceb5c5f9fd883702c2645163db45903c507a8
Thomas Scholtes <thomas@contentful.com>
2015-07-14 17:15:10 +0200

    feat(styleguide): Show styleguide section in page title

--- styleguide/helpers/index.js
+++ styleguide/helpers/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var _ = require('lodash-node');
 var jade = require('jade');
 
 module.exports.register = function(handlebars) {
@@ -9,5 +10,12 @@ module.exports.register = function(handlebars) {
       pretty: true
     });
     return template();
+  });
+
+  handlebars.registerHelper('pageTitle', function (options) {
+    var mainSection = _.find(options.data.root.sections, {depth: 1});
+    if (mainSection) {
+      return mainSection.header;
+    }
   });
 };
