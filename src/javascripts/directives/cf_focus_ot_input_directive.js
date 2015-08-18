cf13dd09d792928de761e02a2dfb7efc4204902e
Tiago Rodrigues <tmcrodrigues@gmail.com>
2015-08-18 18:09:06 +0200

    Document cfFocusOtInput

--- src/javascripts/directives/cf_focus_ot_input_directive.js
+++ src/javascripts/directives/cf_focus_ot_input_directive.js
@@ -1,8 +1,9 @@
 'use strict';
 /**
+ * @ngdoc directive
+ * @name cfFocusOtInput
+ * @description
  * Directive to focus a child OT enabled input element
- *
- * Usage: <div cf-focus-ot-input="optionalExpression"></div>
  *
  * The cf-focus-ot-input attribute can be empty which means the element will
  * always be focused, or it can contain an expression which will be evaluated
@@ -11,6 +12,9 @@
  * This directive can be used to focus the title field on an entry/asset,
  * which while simple to determine on an asset, it can be more complex on an
  * entry, hence the condition to verify if focus should occur.
+ *
+ * @usage[html]
+ * <div cf-focus-ot-input="optionalExpression"></div>
  */
 angular.module('contentful').directive('cfFocusOtInput', ['defer', function(defer){
 
