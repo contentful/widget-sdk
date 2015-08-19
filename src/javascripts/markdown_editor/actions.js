bc251a5e416f98c7db353577f0ee6f9dba85b454
Jakub Elzbieciak <jakub@contentful.com>
2015-08-19 12:47:43 +0200

    Actions refactoring

--- src/javascripts/markdown_editor/actions.js
+++ src/javascripts/markdown_editor/actions.js
@@ -2,29 +2,119 @@
 
 angular.module('contentful').factory('MarkdownEditor/actions', ['$injector', function ($injector) {
 
-  var advancedActions = $injector.get('MarkdownEditor/advancedActions');
+  var $rootScope        = $injector.get('$rootScope');
+  var modalDialog       = $injector.get('modalDialog');
+  var specialCharacters = $injector.get('specialCharacters');
+  var LinkOrganizer     = $injector.get('LinkOrganizer');
+  var assetUrl          = $injector.get('$filter')('assetUrl');
+  var spaceContext      = $injector.get('spaceContext');
 
   return { for: prepareActions };
 
   function prepareActions(editor, localeCode) {
-    var actions = {};
-    var actionNames = [];
 
-    _.forEach(editor.actions, add);
+    var advancedActions = {
+      link:          link,
+      asset:         asset,
+      special:       special,
+      table:         table,
+      embed:         embed,
+      organizeLinks: organizeLinks,
+      openHelp:      openHelp
+    };
 
-    add(function () { advancedActions.asset(localeCode, editor.insert); }, 'asset');
-    add(function () { advancedActions.link(editor.insert);              }, 'link');
-    add(function () { advancedActions.special(editor.insert);           }, 'special');
-    add(function () { advancedActions.table(editor.actions.table);      }, 'table');
-    add(function () { advancedActions.embed(editor.insert);             }, 'embed');
-    add(function () { advancedActions.organizeLinks(editor);            }, 'organizeLinks');
-    add(function () { advancedActions.openHelp();                       }, 'openHelp');
+    return _.defaults(advancedActions, editor.actions);
 
-    return actions;
+    function link() {
+      modalDialog.open({
+        scopeData: { model: {} },
+        template: 'insert_link_dialog',
+        ignoreEnter: true
+      }).promise.then(function (data) {
+        editor.insert(_makeLink(data));
+      });
+    }
 
-    function add(fn, key) {
-      actions[key] = fn;
-      actionNames.push(key);
+    function _makeLink(data) {
+      if (data.title) {
+        return '[' + data.title + '](' + data.url + ')';
+      } else {
+        return '<' + data.url + '>';
+      }
+    }
+
+    function asset() {
+      modalDialog.open({
+        template: 'insert_asset_dialog',
+        ignoreEnter: true
+      }).promise.then(function (assets) {
+        if (_.isEmpty(assets)) { return; }
+        var links = _.map(assets, _makeAssetLink).join(' ');
+        editor.insert(links);
+      });
+    }
+
+    function _makeAssetLink(asset) {
+      try {
+        // "localizedField" may throw TypeError
+        var title = spaceContext.localizedField(asset, 'data.fields.title', localeCode);
+        var file = spaceContext.localizedField(asset, 'data.fields.file', localeCode);
+        if (title && file && file.url) {
+          return '![' + title + '](' + assetUrl(file.url) + ')';
+        }
+      } catch (e) {}
+
+      return '';
+    }
+
+    function special() {
+      var scopeData = {
+        specialCharacters: specialCharacters,
+        model: { choice: _.first(specialCharacters) },
+        entity: function (x) { return '&' + x.id + ';'; }
+      };
+
+      modalDialog.open({
+        scopeData: scopeData,
+        template: 'insert_special_character_dialog',
+        ignoreEnter: true
+      }).promise.then(editor.insert);
+    }
+
+    function table() {
+      modalDialog.open({
+        scopeData: { model: { rows: 2, cols: 2, width: 10 } },
+        template: 'insert_table_dialog',
+        ignoreEnter: true
+      }).promise.then(editor.actions.table);
+    }
+
+    function embed() {
+      modalDialog.open({
+        scopeData: { fieldData: { value: '' }, urlStatus: 'invalid' },
+        template: 'embed_external_content_dialog',
+        ignoreEnter: true
+      }).promise.then(function (data) {
+        editor.insert(_makeEmbedlyLink(data.value));
+      });
+    }
+
+    function _makeEmbedlyLink(url) {
+      return '<a href="' + url + '" class="embedly-card">Embedded content: ' + url + '</a>';
+    }
+
+    function organizeLinks() {
+      var text = editor.getContent();
+      text = LinkOrganizer.convertInlineToRef(text);
+      text = LinkOrganizer.rewriteRefs(text);
+      editor.setContent(text);
+    }
+
+    function openHelp() {
+      modalDialog.open({
+        template: 'markdown_help_dialog',
+        ignoreEnter: true
+      });
     }
   }
 }]);
