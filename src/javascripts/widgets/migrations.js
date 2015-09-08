78688d39989832a486151c0228730d95a8d715b4
Thomas Scholtes <thomas@contentful.com>
2015-09-08 13:53:19 +0200

    feat(widgets): Add migration mechanism for widgets

---
+++ src/javascripts/widgets/migrations.js
@@ -0,0 +1,61 @@
+'use strict';
+
+angular.module('contentful')
+
+/**
+ * @ngdoc service
+ * @name widgets/migrations/data
+ *
+ * @description
+ * This service exposes a list of widget migration specifications that
+ * migrate the editing interface data of a content type.
+ *
+ * Each element is an object with a `from` and a `to` property. The
+ * corresponding values are strings that represent widget IDs. A
+ * migration description may also have a `fieldTypes` property. This is
+ * an array of field type names. The migration will only take place for
+ * fields whose type is included in that list.
+ */
+.value('widgets/migrations/data', [
+])
+
+/**
+ * @ngdoc service
+ * @name widgets/migrations
+ */
+.factory('widgets/migrations', ['$injector', function ($injector) {
+
+  var MIGRATIONS = $injector.get('widgets/migrations/data');
+
+  return function editingInterfaceMigrator (contentType) {
+    return function (editingInterface) {
+      _.each(editingInterface.data.widgets, function (widget) {
+        var field = findFieldForWidget(widget, contentType);
+        migrateWidget(widget, field);
+      });
+      return editingInterface;
+    };
+  };
+
+
+  /**
+   * @param {API.Widget} widget
+   * @param {API.Field} field
+   */
+  function migrateWidget (widget, field) {
+    var id = widget.widgetId;
+    var migration = _.find(MIGRATIONS, function (migration) {
+      var fieldMatches = !migration.fieldTypes || _.contains(migration.fieldTypes, field.type)
+      return fieldMatches && migration.from === id
+    });
+
+    if (migration) {
+      widget.widgetId = migration.to;
+    }
+  }
+
+  function findFieldForWidget (widget, contentType) {
+    return _.find(contentType.data.fields, {id: widget.fieldId});
+  }
+
+}]);
---
+++ test/unit/widgets/migrations_spec.js
@@ -0,0 +1,51 @@
+'use strict';
+
+describe('widgets/migrations', function() {
+  beforeEach(function () {
+
+    module('contentful/test', function ($provide) {
+      $provide.value('widgets/migrations/data', []);
+    });
+
+    this.migrations = this.$inject('widgets/migrations/data');
+
+    var createMigrator = this.$inject('widgets/migrations');
+    this.migrate = function (ct, ei) {
+      createMigrator(ct)(ei);
+    };
+  });
+
+  it('migrates widget regardless of type', function () {
+    var contentType = makeContentType({id: 'FID'});
+    var editingInterface = makeEditingInterface({fieldId: 'FID', widgetId: 'oldWidget'});
+
+    this.migrations.push({
+      from: 'oldWidget',
+      to: 'newWidget'
+    });
+
+    expect(editingInterface.data.widgets[0].widgetId).toEqual('oldWidget');
+    this.migrate(contentType, editingInterface);
+    expect(editingInterface.data.widgets[0].widgetId).toEqual('newWidget');
+  });
+
+  it('migrates widget if field type matches');
+
+  it('does not migrate widget if field type does not match');
+
+  function makeEditingInterface (widget) {
+    return {
+      data: {
+        widgets: [widget]
+      }
+    };
+  }
+
+  function makeContentType (field) {
+    return {
+      data: {
+        fields: [field]
+      }
+    };
+  }
+});
