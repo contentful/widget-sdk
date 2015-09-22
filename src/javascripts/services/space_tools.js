5b6a1c023d6a1f8282a4691a4609a162d232a046
Jakub Elzbieciak <jakub@contentful.com>
2015-09-22 13:02:30 +0200

    Docs for spaceTools

--- src/javascripts/services/space_tools.js
+++ src/javascripts/services/space_tools.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+ * @ngdoc service
+ * @name spaceTools
+ * @description
+ * Utility service for dealing with spaces
+ */
 angular.module('contentful').factory('spaceTools', ['$injector', function($injector) {
 
   var TheStore       = $injector.get('TheStore');
@@ -17,16 +23,42 @@ angular.module('contentful').factory('spaceTools', ['$injector', function($injec
     goToInitialSpace: goToInitialSpace
   };
 
+  /**
+   * @ngdoc method
+   * @name spaceTools#getLastUsed
+   * @returns {string}
+   * @description
+   * Returns ID of last used space
+   */
   function getLastUsed() {
     return TheStore.get('lastUsedSpace');
   }
 
+  /**
+   * @ngdoc method
+   * @name spaceTools#getFromList
+   * @param {string} id
+   * @param {Client.Space[]} spaceList
+   * @returns {Client.Space}
+   * @description
+   * Given ID and list of spaces, returns space with provided ID
+   */
   function getFromList(id, spaceList) {
     return _.find(spaceList, function (space) {
       return space.getId() === id;
     });
   }
 
+
+  /**
+   * @ngdoc method
+   * @name spaceTools#goTo
+   * @param {Client.Space} space
+   * @param {boolean} doNotTrack
+   * @description
+   * Navigates to another space if not in the account view and provided
+   * space actually differs from the previous one; stores ID (for "getLastUsed")
+   */
   function goTo(space, doNotTrack) {
     if (!TheAccountView.isActive() && spaceContext.getId() === space.getId()) {
       return;
@@ -41,6 +73,14 @@ angular.module('contentful').factory('spaceTools', ['$injector', function($injec
     $state.go('spaces.detail', { spaceId: space.getId() });
   }
 
+  /**
+   * @ngdoc method
+   * @name spaceTools#goToInitialSpace
+   * @param {string} forcedId
+   * @description
+   * Determines initial space and navigates there.
+   * Navigates to new space view if there are no spaces at all.
+   */
   function goToInitialSpace(forcedId) {
     tokenStore.getSpaces().then(function (spaceList) {
       var space = determineInitialSpace(spaceList, forcedId);
