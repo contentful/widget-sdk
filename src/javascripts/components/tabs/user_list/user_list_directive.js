382025040982a0b1b8e0d8c9440768e7aa97ac23
Jakub Elzbieciak <jakub@contentful.com>
2015-10-19 16:54:52 +0200

    User list sorting/ordering/grouping

--- src/javascripts/components/tabs/user_list/user_list_directive.js
+++ src/javascripts/components/tabs/user_list/user_list_directive.js
@@ -18,6 +18,8 @@ angular.module('contentful').controller('UserListController', ['$scope', '$injec
   var roleNameMap  = {};
   var userRolesMap = {};
 
+  var ADMIN_ROLE_ID = '__cf_builtin_admin';
+
   $q.all({
     memberships: load('space_memberships'),
     roles: load('roles'),
@@ -25,6 +27,7 @@ angular.module('contentful').controller('UserListController', ['$scope', '$injec
   }).then(function (data) {
     prepareMaps(data.memberships, data.roles);
     $scope.users = prepareUsers(data.users);
+    $scope.by = groupUsers();
     $scope.context.ready = true;
   })
   .catch(ReloadNotification.apiErrorHandler);
@@ -34,26 +37,31 @@ angular.module('contentful').controller('UserListController', ['$scope', '$injec
   }
 
   function prepareUsers(users) {
-    return _.map(users, function (user) {
+    return _(users).map(function (user) {
       var data = user.data;
       var id = user.getId();
 
       return {
         id: id,
-        avatar: data.avatarUrl,
-        name: data.firstName && data.lastName ? user.getName() : 'Not defined',
-        roles: getRolesForUser(id),
+        avatarUrl: data.avatarUrl,
+        name: data.firstName && data.lastName ? user.getName() : 'Name not defined',
+        roles: userRolesMap[id],
+        roleNames: getRoleNamesForUser(id),
         isAdmin: adminMap[id]
       };
-    });
+    }).sortBy('name').value();
   }
 
-  function getRolesForUser(userId) {
-    var roleString = _.map(userRolesMap[userId], function (roleId) {
-      return roleNameMap[roleId];
-    }).join(', ');
-
+  function getRoleNamesForUser(userId) {
+    var roleIds = _.clone(userRolesMap[userId]);
+    if (adminMap[userId]) { roleIds.unshift(ADMIN_ROLE_ID); }
+    var roleString = _(roleIds).map(getRoleName).value().join(', ');
     return roleString.length > 0 ? roleString: 'None';
+  }
+
+  function getRoleName(id) {
+    if (id === ADMIN_ROLE_ID) { return 'Administrator'; }
+    return roleNameMap[id] || 'Unknown';
   }
 
   function prepareMaps(memberships, roles) {
@@ -71,6 +79,50 @@ angular.module('contentful').controller('UserListController', ['$scope', '$injec
       _.forEach(membership.roles, function (role) {
         userRolesMap[userId].push(role.sys.id);
       });
+    });
+  }
+
+  function groupUsers() {
+    return {
+      name: groupUsersByName(),
+      role: groupUsersByRole()
+    };
+  }
+
+  function groupUsersByName() {
+    var byLetter = {};
+
+    _.forEach($scope.users, function (user) {
+      var first = user.name.substr(0, 1).toLowerCase();
+      byLetter[first] = byLetter[first] || [];
+      byLetter[first].push(user);
+    });
+
+    var sortedLetters = _.keys(byLetter).sort();
+
+    return _.map(sortedLetters, function (letter) {
+      return { label: letter, users: byLetter[letter ]};
+    });
+  }
+
+  function groupUsersByRole() {
+    var byRole = {};
+    var admins = [];
+
+    _.forEach($scope.users, function (user) {
+      if (user.isAdmin) { admins.push(user); }
+      _.forEach(user.roles, function (roleId) {
+        byRole[roleId] = byRole[roleId] || [];
+        byRole[roleId].push(user);
+      });
+    });
+
+    var sortedRoleIds = _(byRole).keys().sortBy(getRoleName).value();
+    sortedRoleIds.unshift(ADMIN_ROLE_ID);
+    byRole[ADMIN_ROLE_ID] = admins;
+
+    return _.map(sortedRoleIds, function (roleId) {
+      return { label: getRoleName(roleId), users: byRole[roleId] };
     });
   }
 }]);
