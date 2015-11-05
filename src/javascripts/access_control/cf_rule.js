7dc0cec4a8c11c57584b2fe6992752e6a59a5f90
Jakub Elzbieciak <jakub@contentful.com>
2015-11-05 16:01:45 +0100

    Additional conditions for "create" action

--- src/javascripts/access_control/cf_rule.js
+++ src/javascripts/access_control/cf_rule.js
@@ -5,21 +5,40 @@ angular.module('contentful').directive('cfRule', [function () {
     restrict: 'E',
     template: JST['rule'](),
     controller: ['$scope', function ($scope) {
+
+      // when selected action changes...
       $scope.$watch('rule.action', function (action) {
-        var value = (action === 'edit') ? 'all' : null;
-        $scope.rule.locale = value;
-        setField(value);
+        // ...to "edit" -> reset locale and field
+        if (action === 'edit') {
+          setFieldAndLocale('all');
+        }
+        // ...to "create" -> reset scope, remove locale and field
+        else if (action === 'create') {
+          $scope.rule.scope = 'any';
+          setFieldAndLocale(null);
+        }
+        // otherwise -> remove locale and field
+        else { setFieldAndLocale(null); }
       });
 
+      // when selected content type is changed
       $scope.$watch('rule.contentType', function (id) {
         var ct = $scope.spaceContext._publishedContentTypesHash[id];
+        // get fields of selected content type
         $scope.contentTypeFields = dotty.get(ct, 'data.fields', []);
-        setField('all');
+        // reset selected field to default one
+        if (ct) { setField('all'); }
       });
 
-      function setField(value) {
+      function setFieldAndLocale(field, locale) {
+        setField(field);
+        $scope.rule.locale = locale || field || null;
+      }
+
+      // this sets field involved in a rule (only if rule can hold field)
+      function setField(field) {
         if ('field' in $scope.rule) {
-          $scope.rule.field = value;
+          $scope.rule.field = field || null;
         }
       }
     }]
