08d8e6f4b04b8aa294ac75848fef177d38322ad9
Thomas Scholtes <thomas@contentful.com>
2016-02-24 15:31:20 +0100

    feat(test): Add 'defers' method to sinon stubs

--- test/helpers/sinon.js
+++ test/helpers/sinon.js
@@ -1,3 +1,5 @@
+'use strict';
+
 sinon.stub.resolves = function (value) {
   return this.returns(get$q().when(value));
 };
@@ -6,11 +8,19 @@ sinon.stub.rejects = function (err) {
   return this.returns(get$q().reject(err));
 };
 
+sinon.stub.defers = function () {
+  var deferred = get$q().defer();
+  this.returns(deferred.promise);
+  this.resolve = deferred.resolve.bind(deferred);
+  this.reject = deferred.reject.bind(deferred);
+  return this;
+};
+
 function get$q () {
   var $q;
   inject(function (_$q_) {
-    $q = _$q_
-  })
-  return $q
+    $q = _$q_;
+  });
+  return $q;
 }
 
