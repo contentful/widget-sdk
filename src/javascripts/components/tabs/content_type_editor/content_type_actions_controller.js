contentful/widget-sdk

commit e4435327ad62019488b6f9150360a217f71d2a18

fix(ct-editor): No auto redirect when confirming changes on new CT
When the state manager calls `save()` for the content type we need to
make sure that no redirects happen because the state manager already
redirects

Fixes [TP#6314](https://contentful.tpondemand.com/entity/6314)

src/javascripts/components/tabs/content_type_editor/content_type_actions_controller.js
@@ -13,7 +13,6 @@ function ContentTypeActionsController($scope, $injector) {
   var $rootScope         = $injector.get('$rootScope');
   var analytics          = $injector.get('analytics');
   var logger             = $injector.get('logger');
-  var defer              = $injector.get('defer');
   var notify             = $injector.get('contentType/notifications');
   var $q                 = $injector.get('$q');
   var modalDialog        = $injector.get('modalDialog');
@@ -182,7 +181,9 @@ function ContentTypeActionsController($scope, $injector) {
    * @name ContentTypeActionsController#save
    * @type {Command}
    */
-  controller.save = Command.create(save, {
+  controller.save = Command.create(function () {
+    return save(true);
+  }, {
     disabled: function () {
       var dirty = $scope.contentTypeForm.$dirty ||
                   !$scope.contentType.getPublishedVersion();
@@ -194,9 +195,14 @@ function ContentTypeActionsController($scope, $injector) {
     }
   });
 
-  controller.runSave = save;
+  // This is called by the state manager in case the user leaves the
+  // Content Type editor without saving. We do not redirect in that
+  // case.
+  controller.saveAndClose = function () {
+    return save(false);
+  };
 
-  function save () {
+  function save (redirect) {
     trackSavedContentType($scope.contentType);
     ctHelpers.assureDisplayField($scope.contentType.data);
 
@@ -223,7 +229,13 @@ function ContentTypeActionsController($scope, $injector) {
     })
     .then(publishContentType)
     .then(saveEditingInterface)
-    .then(postSaveActions, triggerApiErrorNotification);
+    .catch(triggerApiErrorNotification)
+    .then(setFormPristine)
+    .then(function () {
+      if (redirect && $scope.context.isNew) {
+        return goToDetails();
+      }
+    }).then(notify.saveSuccess);
   }
 
   function publishContentType(contentType) {
@@ -269,30 +281,18 @@ function ContentTypeActionsController($scope, $injector) {
     return $scope.editingInterface.save();
   }
 
-  function postSaveActions() {
+  function setFormPristine () {
+    // Since this is called by asynchronously the scope data may have
+    // already been removed.
     if ($scope.contentTypeForm) {
       $scope.contentTypeForm.$setPristine();
     }
-    return redirectWhenNew().then(notify.saveSuccess);
   }
 
-  /**
-    * Lets the digest cycle have time to properly set the pristine state
-    * before trying to redirect the user, otherwise they'll be prompted
-    * to save changes
-  */
-  function redirectWhenNew() {
-    if($scope.context.isNew){
-      return $q((function (res, reject) {
-        defer(function () {
-          $state.go('spaces.detail.content_types.detail', {
-            contentTypeId: $scope.contentType.getId()
-          }).then(res, reject);
-        });
-      }));
-    } else {
-      return $q.when();
-    }
+  function goToDetails () {
+    return $state.go('spaces.detail.content_types.detail', {
+      contentTypeId: $scope.contentType.getId()
+    });
   }
 
   function triggerApiErrorNotification(err) {

src/javascripts/components/tabs/content_type_editor/content_type_editor_controller.js
@@ -37,7 +37,7 @@ function ContentTypeEditorController($scope, $injector) {
 
   $scope.hints = hints;
 
-  $scope.context.requestLeaveConfirmation = leaveConfirmator($scope.actions.runSave);
+  $scope.context.requestLeaveConfirmation = leaveConfirmator($scope.actions.saveAndClose);
   $scope.fieldSchema = validation(validation.schemas.ContentType.at(['fields']).items);
   $scope.updatePublishedContentType = updatePublishedContentType;
 

commit b8356f0eba125932bc65c0e7d66a2191a96fe4cd

fix(ct-editor): Set state to not dirty before redirecting to new CT
Fixes a regression introduced in e4435327ad62019488b6f9150360a217f71d2a18.

When we redirect after a new CT is created the state change handlers
tests for the `context.dirty` flag. Previously this was implicitly
handled by a watcher on the CT forms `$dirty` property. But that
watcher failed to trigger before the redirection took place.

src/javascripts/components/tabs/content_type_editor/content_type_actions_controller.js
@@ -230,7 +230,7 @@ function ContentTypeActionsController($scope, $injector) {
     .then(publishContentType)
     .then(saveEditingInterface)
     .catch(triggerApiErrorNotification)
-    .then(setFormPristine)
+    .then(setPristine)
     .then(function () {
       if (redirect && $scope.context.isNew) {
         return goToDetails();
@@ -281,11 +281,14 @@ function ContentTypeActionsController($scope, $injector) {
     return $scope.editingInterface.save();
   }
 
-  function setFormPristine () {
+  function setPristine () {
     // Since this is called by asynchronously the scope data may have
     // already been removed.
     if ($scope.contentTypeForm) {
       $scope.contentTypeForm.$setPristine();
+    }
+    if ($scope.context) {
+      $scope.context.dirty = false;
     }
   }
 

test/unit/components/tabs/content_type_editor/content_type_actions_controller_spec.js
@@ -232,9 +232,11 @@ describe('ContentType Actions Controller', function () {
       scope.updatePublishedContentType = sinon.stub();
     });
 
-    pit('resets form to pristine state', function () {
+    pit('resets form and state context to pristine state', function () {
+      scope.context.dirty = true;
       return controller.save.execute()
       .then(function () {
+        expect(scope.context.dirty).toBe(false);
         expect(scope.contentTypeForm.$pristine).toBe(true);
       });
     });