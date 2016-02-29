902fb49ed359e547d40c4c46f5c93ae1ee85090a
Jakub Elzbieciak <jakub@contentful.com>
2016-02-29 15:18:51 +0100

    Implement in-app view for space settings

--- src/javascripts/components/tabs/space_settings/space_settings_directive.js
+++ src/javascripts/components/tabs/space_settings/space_settings_directive.js
@@ -1,34 +1,96 @@
 'use strict';
 
-angular.module('contentful').directive('cfSpaceSettings', ['$injector', function ($injector) {
+angular.module('contentful')
 
-  var authentication = $injector.get('authentication');
-  var spaceContext   = $injector.get('spaceContext');
-
+.directive('cfSpaceSettings', [function () {
   return {
-    template: JST['iframe_view'](),
+    template: JST['space_settings'](),
     restrict: 'E',
-    link: function (scope, el) {
-      // @todo legacy thing, has to be true all the time:
-      scope.hasLoaded = true;
-
-      scope.iframeId  = 'spaceSettingsFrame';
-      scope.iframeSrc = buildSrc();
-
-      scope.$on('iframeMessage', markContextAsReady);
-
-      function buildSrc() {
-        return [
-          authentication.spaceSettingsUrl(spaceContext.getId()),
-          '/edit?access_token=', authentication.token
-        ].join('');
-      }
-
-      function markContextAsReady(event, data, iframe) {
-        if (iframe === el.find('iframe').get(0)) {
-          scope.context.ready = true;
-        }
-      }
-    }
+    controller: 'SpaceSettingsController'
   };
+}])
+
+.controller('SpaceSettingsController', ['$injector', '$scope', function ($injector, $scope) {
+
+  var $q                 = $injector.get('$q');
+  var $rootScope         = $injector.get('$rootScope');
+  var spaceContext       = $injector.get('spaceContext');
+  var client             = $injector.get('client');
+  var Command            = $injector.get('command');
+  var tokenStore         = $injector.get('tokenStore');
+  var spaceTools         = $injector.get('spaceTools');
+  var modalDialog        = $injector.get('modalDialog');
+  var notification       = $injector.get('notification');
+  var ReloadNotification = $injector.get('ReloadNotification');
+
+  $scope.context.ready = true;
+  $scope.spaceId = spaceContext.space.getId();
+  $scope.model = {name: spaceContext.space.data.name};
+  $scope.save = Command.create(save, {disabled: isSaveDisabled});
+  $scope.openRemovalDialog = Command.create(openRemovalDialog);
+
+  function save() {
+    return endpoint().payload($scope.model)
+    .headers(header())
+    .put()
+    .then(function () {
+      notification.info('Space renamed to ' + $scope.model.name + ' successfully.');
+    })
+    .then(tokenStore.refresh)
+    .catch(handleSaveError);
+  }
+
+  function handleSaveError(err) {
+    if (dotty.get(err, 'body.details.errors', []).length > 0) {
+      notification.error('Please provide a valid space name.');
+    } else {
+      ReloadNotification.basicErrorHandler();
+    }
+  }
+
+  function remove() {
+    return endpoint().delete()
+    .then(function () {
+      notification.info('Space ' + $scope.model.name + ' deleted successfully.');
+    })
+    .then(tokenStore.refresh)
+    .then(spaceTools.leaveCurrent)
+    .catch(ReloadNotification.basicErrorHandler);
+  }
+
+  function endpoint() {
+    return client.endpoint('spaces', spaceContext.space.getId());
+  }
+
+  function header() {
+    return {'X-Contentful-Version': spaceContext.space.getVersion()};
+  }
+
+  function isSaveDisabled() {
+    var input = dotty.get($scope, 'model.name');
+    var currentName = dotty.get(spaceContext, 'space.data.name');
+
+    return !input || input === currentName;
+  }
+
+  function openRemovalDialog() {
+    var space = spaceContext.space;
+    var scope = _.extend($rootScope.$new(), {
+      space: space,
+      input: {spaceName: ''},
+      remove: Command.create(remove, {
+        disabled: function () {
+          return scope.input.spaceName !== space.data.name;
+        }
+      })
+    });
+
+    modalDialog.open({
+      template: 'space_removal_dialog',
+      noNewScope: true,
+      scope: scope
+    });
+
+    return $q.when();
+  }
 }]);
