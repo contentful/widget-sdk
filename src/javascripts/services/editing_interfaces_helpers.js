158dee8ae088a74de44435610c0976921793eb50
Thomas Scholtes <thomas@contentful.com>
2016-03-07 19:30:35 +0100

    refactor(ei): Remove unused helper method

--- src/javascripts/services/editing_interfaces_helpers.js
+++ src/javascripts/services/editing_interfaces_helpers.js
@@ -11,35 +11,8 @@ angular.module('contentful')
 .factory('editingInterfaces/helpers', [function () {
 
   return {
-    findField: findField,
     findWidget: findWidget
   };
-
-  /**
-   * @ngdoc method
-   * @name editingInterfaces/helpers#findField
-   * @description
-   * Find a field in a content types fields based on the passed in `widget`'s
-   * fieldId.  Since we can't be sure that all content types have the `apiName`
-   * property in the field, we need to fall back to the `id`.
-   *
-   * @param {Array<API.Field>} contentTypeFields
-   * @param {API.Widget} widget
-   * @return {API.Field?}
-   */
-  function findField(contentTypeFields, widget) {
-    // Both widget.fieldId and field.apiName could be undefined due to legacy
-    // data. For this reason a comparison between a fieldId that is undefined
-    // and a apiName that is undefined would result in true, causing mismatched
-    // mapping and a subtle bug.
-    if(!_.isString(widget.fieldId)) {
-      return;
-    }
-    return _.find(contentTypeFields, function(field) {
-      return field.apiName === widget.fieldId || field.id === widget.fieldId;
-    });
-  }
-
   /**
    * @ngdoc method
    * @name editingInterfaces/helpers#findWidget
