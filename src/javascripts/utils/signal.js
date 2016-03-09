c0da5c94b5c29cbeb3d91d56c70367d8c5b89e6a
Thomas Scholtes <thomas@contentful.com>
2016-03-09 14:23:06 +0100

    feat(signals): Add memoization to signals

--- src/javascripts/utils/signal.js
+++ src/javascripts/utils/signal.js
@@ -12,25 +12,55 @@ angular.module('cf.utils')
  * })
  * signal.dispatch('Hello')
  * detach();
+ * @description
+ * Signals also memoize their most recently dispatched value. This
+ * allows you to trigger a listener immediately after it is attached by
+ * passing `true` as the second argument.
+ * ~~~js
+ * signal.dispatch('value')
+ * signal.attach(function (v) {
+ *   console.log(v)
+ * }, true);
+ * // logs 'value' immediately
+ * ~~~
+ *
+ * You can pass an initial value when creating a signal. This value is
+ * only sent to listeners that attach with the `true` flag and is
+ * overwritten by subsequent calls to `.dispatch()`.
+ *
+ * ~~~js
+ * var createSignal = $injector.get('signal')
+ * var signal = createSignal('initial')
+ * signal.attach(function (v) {
+ *   console.log(v)
+ * }, true);
+ * // logs 'initial' immediately
+ * ~~~
  */
 .factory('signal', [function () {
 
   return function createSignal () {
     var listeners = {};
     var nextId = 0;
+    var lastArgs = arguments;
+
     return {
       _listeners: listeners,
 
       dispatch: function () {
-        var args = arguments;
+        lastArgs = arguments;
         _.forEach(listeners, function (listener) {
-          listener.apply(null, args);
+          listener.apply(null, lastArgs);
         });
       },
 
-      attach: function (listener) {
+      attach: function (listener, sendLast) {
         var id = nextId++;
         listeners[id] = listener;
+
+        if (sendLast) {
+          listener.apply(null, lastArgs);
+        }
         return function removeListener () {
           delete listeners[id];
         };
