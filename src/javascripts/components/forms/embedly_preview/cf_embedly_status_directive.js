ec47c7e6a5a7ce599422f09495dc877fb8448af2
Mudit Ameta <zeusdeux@gmail.com>
2016-03-17 19:19:30 +0100

    Refactor embedly status into a jade mixin

--- src/javascripts/components/forms/embedly_preview/cf_embedly_status_directive.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('contentful').directive('cfEmbedlyStatus', function () {
-  return {
-    restrict: 'E',
-    scope: { urlStatus: '=' },
-    template: JST['cf_embedly_status']()
-  };
-});
