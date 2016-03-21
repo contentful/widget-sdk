cdce05a2c4c1db4b7cb5dc61653ab78ba5c4aa48
Mudit Ameta <zeusdeux@gmail.com>
2016-03-21 02:53:57 +0100

    Rewrite number editor using custom widget api

--- src/javascripts/components/forms/number_editor/cf_number_editor_directive.js
+++ src/javascripts/components/forms/number_editor/cf_number_editor_directive.js
@@ -1,37 +1,116 @@
 'use strict';
 
-angular.module('contentful').directive('cfNumberEditor', function(){
+angular.module('contentful')
+.directive('cfNumberEditor', ['$injector', function ($injector) {
+  var makeInputUpdater = $injector.get('ui/caretHelper').makeInputUpdater;
+  var parseNumber = $injector.get('cfNumberEditor/parseNumber');
+  var debounce = $injector.get('debounce');
+
   return {
-    restrict: 'A',
+    scope: {},
+    restrict: 'E',
+    require: '^cfWidgetApi',
     template: [
-      '<input type="text" class="form-control" ng-model="fieldData.value" ot-bind-ng-model ng-disabled="isDisabled(field, locale)">',
+      '<input type="text" class="form-control" ng-disabled="isDisabled">',
       '<i cf-field-alert="{{parseWarning}}" tooltip-placement="left"></i>'
     ].join(''),
-    link: function(scope, elem){
-      var ngModel = elem.find('input').controller('ngModel');
-      ngModel.$parsers.push(function (viewValue) {
-        var floatVal = parseFloat(viewValue, 10);
-        var intVal   = parseInt(viewValue, 10);
-        var modelVal;
-        if (scope.field.type === 'Integer' && floatVal !== intVal) {
-          ngModel.$setValidity('integer', false);
-        } else {
-          ngModel.$setValidity('integer', true);
+    link: function (scope, $el, attrs, widgetApi) {
+      var field = widgetApi.field;
+      var $inputEl = $el.children('input');
+      var updateInput = makeInputUpdater($inputEl);
+
+      // update input field value when new synced value received via ot magic
+      var detachOnValueChangedHandler = field.onValueChanged(function (val) {
+        updateInput(val === 0 ? String(val) : (val ? String(val) : ''));
+      }, true);
+      // call handler when the disabled status of the field changes
+      var detachOnFieldDisabledHandler = field.onDisabledStatusChanged(updateDisabledStatus, true);
+
+      // remove attached handlers when element is evicted from dom
+      scope.$on('$destroy', detachOnValueChangedHandler);
+      scope.$on('$destroy', detachOnFieldDisabledHandler);
+
+      scope.$watch(function () {
+        return $inputEl.val();
+      }, function (val) {
+        var parseResult = parseNumber(val.trim(), field.type);
+
+        scope.parseWarning = parseResult.warning;
+        if (parseResult.isValid) {
+          field.setValue(parseResult.value);
         }
-        modelVal = scope.field.type === 'Integer' ? intVal : floatVal;
-        if (_.isEmpty(viewValue)) {
-          scope.parseWarning = '';
-        } else if (isNaN(modelVal)) {
-          scope.parseWarning = 'Unrecognized Number';
-        } else if (modelVal.toString() !== viewValue) {
-          scope.parseWarning = 'Recognized value: ' + modelVal;
-        } else {
-          scope.parseWarning = '';
-        }
-        modelVal = isNaN(modelVal) ? null : modelVal;
-        return modelVal;
       });
 
+      // Since all input handling is done in scope.$watch
+      // this is to run the watcher when user interacts
+      // with the text input
+      $inputEl.on('input change', debounce(function () {
+        scope.$apply();
+      }, 200));
+
+      function updateDisabledStatus (disabledStatus) {
+        scope.isDisabled = disabledStatus;
+      }
     }
   };
-});
+}])
+.factory('cfNumberEditor/parseNumber', [function () {
+  /**
+   * @ngdoc method
+   * @name cfNumberEditor#parseNumber
+   * @description
+   * Tries to parse given string as number
+   *
+   * @param {string} viewValue  String to be parsed
+   * @param {string} type       Type to parse given string as
+   * @return {object}
+   */
+  return function (viewValue, type) {
+    // This has saner semantics than parseFloat.
+    // For values with chars in 'em, it gives
+    // us NaN unlike parseFloat
+    var floatVal = +viewValue;
+    var hasDot = (/\./g).test(viewValue);
+    var hasFractional = (/\.\d+/g).test(viewValue);
+
+    if (_.isEmpty(viewValue)) {
+      return {
+        isValid: true,
+        warning: '',
+        value: undefined
+      };
+    }
+
+    if (isNaN(floatVal)) {
+      return {
+        isValid: false,
+        warning: 'Unrecognized Number',
+        value: viewValue
+      };
+    }
+
+    if (type === 'Integer' && hasDot) {
+      var intVal = parseInt(viewValue, 10);
+
+      return {
+        isValid: false,
+        warning: 'Recognized value: ' + intVal,
+        value: intVal
+      };
+    }
+
+    if (hasDot && !hasFractional) {
+      return {
+        isValid: false,
+        warning: 'Recognized value: ' + floatVal,
+        value: floatVal
+      };
+    }
+
+    return {
+      isValid: true,
+      warning: '',
+      value: floatVal
+    };
+  };
+}]);
