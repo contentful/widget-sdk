ce3c96418fcef1d32b5c2fafb46726d9c2017e2c
Thomas Scholtes <thomas@contentful.com>
2016-04-01 15:38:39 +0200

    refactor: Move controller into directive file

--- src/javascripts/components/forms/kaltura/cf_kaltura_multi_video_editor_directive.js
+++ src/javascripts/components/forms/kaltura/cf_kaltura_multi_video_editor_directive.js
@@ -10,4 +10,24 @@ angular.module('contentful').directive('cfKalturaMultiVideoEditor', [function(){
     controllerAs: 'providerVideoEditorController'
   };
 
+}])
+
+.controller('cfKalturaMultiVideoEditorController', ['$injector', '$scope', function($injector, $scope){
+  var KalturaEditorControllerMixin = $injector.get('KalturaEditorControllerMixin');
+  var kalturaClientWrapper = $injector.get('kalturaClientWrapper');
+
+  kalturaClientWrapper.setOrganizationId($scope.spaceContext.space.getOrganizationId());
+
+  _.extend(this, KalturaEditorControllerMixin);
+
+  //overwrite this function
+  this.customAttrsForPlayer = customAttrsForPlayer;
+
+  function customAttrsForPlayer(video) {
+    //TODO: remove coupling with assetId
+    //see cfMultiVideoEditorController for
+    //a related comment
+    return {entryId: video.assetId};
+  }
+
 }]);
