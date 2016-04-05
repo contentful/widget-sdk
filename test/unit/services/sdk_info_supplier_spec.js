17e84f1e354b5fdedb9ef4b6311a90d5808e5b90
Lyn Nagara <lyn.nagara@gmail.com>
2016-04-05 17:27:32 +0200

    Rename service sdkInfoProvider -> sdkInfoSupplier

--- test/unit/services/sdk_info_supplier_spec.js
+++ test/unit/services/sdk_info_supplier_spec.js
@@ -1,18 +1,18 @@
 'use strict';
 
-describe('sdkInfoProvider', function () {
+describe('sdkInfoSupplier', function () {
   beforeEach(function () {
     module('contentful/test');
-    this.sdkInformation = this.$inject('sdkInfoProvider');
+    this.sdkInfoSupplier = this.$inject('sdkInfoSupplier');
   });
 
   it('returns a list containing all elements', function() {
-    var data = this.sdkInformation.get();
+    var data = this.sdkInfoSupplier.get();
     expect(data.length).toBe(6);    
   });
 
   it('returns an empty data object when called without arguments', function() {
-    var data = this.sdkInformation.get();
+    var data = this.sdkInfoSupplier.get();
     _.forEach(data, function(language) {
       expect(language.data).toEqual({});
     });
@@ -20,7 +20,7 @@ describe('sdkInfoProvider', function () {
  
   it('filters data object', function() {
     var keys = ['documentation'];
-    var data = this.sdkInformation.get(keys);
+    var data = this.sdkInfoSupplier.get(keys);
     data.forEach(function(language) {
       expect(_.keys(language.data)).toEqual(keys);
     });
@@ -28,7 +28,7 @@ describe('sdkInfoProvider', function () {
 
   it('removes invalid keys', function() {
     var keys = ['deliveryApi', 'documentation', 'invalid'];
-    var data = this.sdkInformation.get(keys);
+    var data = this.sdkInfoSupplier.get(keys);
     expect(_.keys(data[0].data)).toEqual(['deliveryApi', 'documentation']);
     expect(_.keys(data[5].data)).toEqual(['documentation']);
   });
