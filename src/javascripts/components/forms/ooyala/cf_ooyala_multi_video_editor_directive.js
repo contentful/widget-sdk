d1ce062cf3740e29bde8a62f5309d7e0d901c1f9
Thomas Scholtes <thomas@contentful.com>
2016-04-12 16:33:56 +0200

    fix: Attach Widget API to scope before loading controllers

--- src/javascripts/components/forms/ooyala/cf_ooyala_multi_video_editor_directive.js
+++ src/javascripts/components/forms/ooyala/cf_ooyala_multi_video_editor_directive.js
@@ -8,9 +8,12 @@ angular.module('contentful').directive('cfOoyalaMultiVideoEditor', [function(){
     scope: true,
     template: '<cf-multi-video-editor />',
     require: '^cfWidgetApi',
-    link: function (scope, elem, attrs, widgetApi) {
+    link: {pre: function (scope, elem, attrs, widgetApi) {
+      // TODO We should not expose the widget API on the scope, but
+      // currently this is the only way we can deal with the
+      // architecture of the video widgets.
       scope.widgetApi = widgetApi;
-    },
+    }},
     controller: 'cfOoyalaEditorController',
     controllerAs: 'providerVideoEditorController'
   };
