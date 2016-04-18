d1db61cd8c4a1b7cf943725c6927a1021e15c389
Thomas Scholtes <thomas@contentful.com>
2016-04-18 12:55:20 +0200

    fix: Do not update rating widget when field is disabled
    [TP#9546](https://contentful.tpondemand.com/entity/9546)

--- src/javascripts/app/widgets/cf_rating_editor_directive.js
+++ src/javascripts/app/widgets/cf_rating_editor_directive.js
@@ -54,7 +54,7 @@ angular.module('cf.app')
       };
 
       $scope.setRating = function (rating) {
-        if ($scope.isDiabled) {
+        if ($scope.isDisabled) {
           return;
         }
 
