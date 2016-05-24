32596c0dcc790f101b1f036b40af6297e495aa3d
Thomas Scholtes <thomas@contentful.com>
2016-05-24 16:02:58 +0200

    feat: Add directive to reject key inputs

---
+++ test/unit/ui/allow_input_directive_spec.js
@@ -0,0 +1,27 @@
+'use strict';
+
+describe('uiKeyInputReject directive', function () {
+
+  beforeEach(function () {
+    module('cf.ui');
+    this.el = this.$compile('<input ui-allow-input="a-z">')[0];
+  });
+
+  it('prevents keypress event default when character is invalid', function () {
+    var triggerDefault = dispatchKeyPress(this.el, 'X');
+    expect(triggerDefault).toBe(false);
+  });
+
+  it('does not prevent keypress event default when character is valid', function () {
+    var triggerDefault = dispatchKeyPress(this.el, 'a');
+    expect(triggerDefault).toBe(true);
+  });
+
+  function dispatchKeyPress (el, character) {
+    var ev = new window.KeyboardEvent('keypress', {
+      charCode: character.charCodeAt(0),
+      cancelable: true
+    });
+    return el.dispatchEvent(ev);
+  }
+});
