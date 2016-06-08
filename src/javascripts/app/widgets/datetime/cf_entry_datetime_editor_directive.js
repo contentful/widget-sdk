24e1d0d7e3a95c4d0539bbe7a51c9c76cb1b304c
Jakub Elzbieciak <jakub@contentful.com>
2016-06-08 19:24:30 +0200

    Revert "fix(datetime-editor): Do not overide API value without user input"
    This reverts commit 90b3234af628bdd9ce211840886456000d0ec4ef.
    
    Conflicts:
    	test/integration/app/widgets/datetime_editor_spec.js

--- src/javascripts/app/widgets/datetime/cf_entry_datetime_editor_directive.js
+++ src/javascripts/app/widgets/datetime/cf_entry_datetime_editor_directive.js
@@ -100,19 +100,17 @@ angular.module('cf.app')
         datepicker.destroy();
       });
 
-      // This is emitted whenever the user changes on of the input fields.
-      $scope.$on('ngModel:update', function () {
-        if (!$scope.data.date) {
+      $scope.$watchCollection('data.date', function (date) {
+        if (!date) {
           $scope.data.time = null;
         }
         // Prevent calling the datepicker’s `onSelect` method which
         // would lead to an infinite loop.
-        datepicker.setMoment($scope.data.date, true);
+        datepicker.setMoment(date, true);
+      });
 
-        var value = Data.buildFieldValue(
-          $scope.data,
-          $scope.uses12hClock, $scope.usesTime, $scope.usesTimezone
-        );
+      $scope.$watchCollection('data', function (data) {
+        var value = Data.buildFieldValue(data, $scope.uses12hClock, $scope.usesTime, $scope.usesTimezone);
         if (value.valid === null) {
           field.removeValue();
         } else if (value.valid) {
