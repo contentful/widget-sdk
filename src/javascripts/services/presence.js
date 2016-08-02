e5466991ab68ff5cd9ddb43e117c7bca9f06cfec
Thomas Scholtes <thomas@contentful.com>
2016-08-02 18:12:29 +0200

    refactor: Remove unused code from presence service
    Nothing but `isActive()` was being used. In addition we need to
    explicitly activate the service so that the tests don’t leak memory.

--- src/javascripts/services/presence.js
+++ src/javascripts/services/presence.js
@@ -1,77 +1,55 @@
 'use strict';
 
+// Code related to the hidden Property is from http://www.html5rocks.com/en/tutorials/pagevisibility/intro/
 angular.module('contentful')
-.factory('presence', ['$rootScope', 'debounce', function ($rootScope, debounce) {
-  // Code related to the hidden Property is from http://www.html5rocks.com/en/tutorials/pagevisibility/intro/
+.factory('presence', ['require', function (require) {
+  var TIMEOUT = 10 * 60 * 1000;
+  var debounce = require('debounce');
+  var lastActive = Date.now();
 
-  function watchVisibility() {
-    var visProp = getHiddenProp();
-    if (visProp) {
-      var evtname = visProp.replace(/[H|h]idden/,'') + 'visibilitychange';
-      $(document).on(evtname, visibilityChangeHandler);
-    }
+  var trackActivity = debounce(function () {
+    lastActive = Date.now();
+  }, 1000 * 10);
+
+  return {
+    isActive: isActive,
+    startTracking: startTracking
+  };
+
+
+  function isActive () {
+    var now = Date.now();
+    return !isHidden() && (now - lastActive < TIMEOUT);
   }
 
-  function visibilityChangeHandler() {
-    var oldHidden = hidden;
-    var newHidden = isHidden();
-    if (newHidden !== oldHidden) {
-      hidden = newHidden;
-      $rootScope.$evalAsync(function () {
-        $rootScope.$broadcast('presenceChanged', newHidden, oldHidden);
-      });
-    }
+
+  function startTracking () {
+    $(document).on('keydown mousemove', trackActivity);
   }
 
-  function isHidden() {
+
+  function isHidden () {
     var prop = getHiddenProp();
     if (!prop) return false;
 
     return document[prop];
   }
 
-  var hidden = isHidden();
 
-  function getHiddenProp(){
-    var prefixes = ['webkit','moz','ms','o'];
+  function getHiddenProp () {
+    var prefixes = ['webkit', 'moz', 'ms', 'o'];
 
     // if 'hidden' is natively supported just return it
     if ('hidden' in document) return 'hidden';
 
     // otherwise loop over all the known prefixes until we find one
-    for (var i = 0; i < prefixes.length; i++){
-      if ((prefixes[i] + 'Hidden') in document)
+    for (var i = 0; i < prefixes.length; i++) {
+      if ((prefixes[i] + 'Hidden') in document) {
         return prefixes[i] + 'Hidden';
+      }
     }
 
     // otherwise it's not supported
     return null;
   }
-
-  watchVisibility();
-
-  var lastActive = Date.now();
-
-  var trackActivity = debounce(function() {
-    lastActive = Date.now();
-  }, 1000 * 10);
-
-  $(document).on('keydown mousemove', trackActivity);
-
-  return {
-    isHidden: function () {
-      return hidden;
-    },
-
-    lastActive: function () {
-      return lastActive;
-    },
-
-    timeOut: 1000 * 60 * 10,
-
-    isActive: function () {
-      var now = Date.now();
-      return !this.isHidden() && (now - lastActive < this.timeOut);
-    }
-  };
 }]);
