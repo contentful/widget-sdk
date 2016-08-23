04e81cba76c60de4a7649ec43d99d03be1bbbae5
Mudit Ameta <zeusdeux@gmail.com>
2016-08-23 18:20:22 +0200

    Feat: Add tracking for breadcrumbs replacement
    TP: https://contentful.tpondemand.com/entity/10983

--- src/javascripts/components/client/cf_breadcrumbs_directive.js
+++ src/javascripts/components/client/cf_breadcrumbs_directive.js
@@ -10,6 +10,7 @@
 angular.module('contentful').directive('cfBreadcrumbs', ['require', function (require) {
   var $parse = require('$parse');
   var $state = require('$state');
+  var analytics = require('analytics');
   var $document = require('$document');
   var contextHistory = require('contextHistory');
 
@@ -19,6 +20,25 @@ angular.module('contentful').directive('cfBreadcrumbs', ['require', function (re
   var ancestorMenuSelector = '[aria-label="breadcrumbs-ancestor-menu"]';
   var ancestorLinkSelector = ancestorMenuSelector + ' [role="link"]';
   var hintDismissBtnSelector = '[aria-label="ui-hint-dismiss-btn"]';
+
+  var analyticsData = {
+    clickedOn: {
+      BACK: 'back',
+      ANCESTOR: 'ancestor',
+      TOP_STATE: 'topState'
+    },
+    appSection: {
+      ContentType: 'contentModel',
+      Entries: 'content',
+      Assets: 'media',
+      CDAKeys: 'apis.cda',
+      CMAKeys: 'apis.cma',
+      Locales: 'settings.locales',
+      Roles: 'settings.roles',
+      Webhooks: 'settings.webhooks',
+      PreviewEnvironments: 'settings.contentPreview'
+    }
+  };
 
   return {
     template: JST.cf_breadcrumbs(),
@@ -33,7 +53,7 @@ angular.module('contentful').directive('cfBreadcrumbs', ['require', function (re
 
       $el.on('click', ancestorBtnSelector, toggleAncestorList);
 
-      $el.on('click', ancestorLinkSelector, toggleAncestorList);
+      $el.on('click', ancestorLinkSelector, trackAncestorLinkClickAndToggleList);
 
       document.addEventListener('click', closeAncestorListIfVisible, true);
 
@@ -44,10 +64,22 @@ angular.module('contentful').directive('cfBreadcrumbs', ['require', function (re
         document.removeEventListener('click', closeAncestorListIfVisible, true);
       });
 
+      function track (clickedOn) {
+        analytics.track('navigation-control', {
+          clickedOn: clickedOn,
+          appSection: analyticsData.appSection[$scope.crumbs[0].type]
+        });
+      }
+
+      function isTopState (sref) {
+        return /list(\(\))?$/.test(sref);
+      }
+
       function goBackToPreviousPage () {
         var crumbs = $scope.crumbs;
         var link = crumbs[crumbs.length - 2].link;
 
+        track(analyticsData.clickedOn.BACK);
         $state.go(link.state, link.params || {});
         dismissHints();
       }
@@ -63,6 +95,14 @@ angular.module('contentful').directive('cfBreadcrumbs', ['require', function (re
 
         $ancestorMenu.attr('aria-hidden', !$parse(menuAriaHidden)());
         dismissHints();
+      }
+
+      function trackAncestorLinkClickAndToggleList (e) {
+        var sref = $(e.target).attr('ui-sref');
+        var clickedOn = isTopState(sref) ? analyticsData.clickedOn.TOP_STATE : analyticsData.clickedOn.ANCESTOR;
+
+        track(clickedOn);
+        toggleAncestorList(e);
       }
 
       function closeAncestorListIfVisible (e) {
