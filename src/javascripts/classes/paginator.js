ae6061ed9c32f53054b75e1f9872c69798dea6c0
Jakub Elzbieciak <jakub@contentful.com>
2016-09-27 12:49:10 +0200

    Document "Paginator" service

--- src/javascripts/classes/paginator.js
+++ src/javascripts/classes/paginator.js
@@ -2,6 +2,36 @@
 
 angular.module('cf.app')
 
+/**
+ * @ngdoc service
+ * @name Paginator
+ * @module cf.app
+ *
+ * @description
+ * This service can be used to hold the state
+ * of list pagination. It doesn't store any data,
+ * it only keeps track of counts and exposes
+ * modifier and getter functions.
+ *
+ * @usage[js]
+ * // Constructor accepts only one parameter
+ * // (page length). The default is 40.
+ * var paginator = Paginator.create(35);
+ *
+ * paginator.setTotal(37);
+ * paginator.getPageCount(); // => 2
+ * paginator.isAtLast(); // => false
+ * paginator.next();
+ * paginator.isAtLast(); // => true
+ *
+ * // Setters accept both value and function:
+ * paginator.setPage(2);
+ * paginator.getPage(); // => 2
+ * paginator.setPage(function (currentPage) {
+ *   return currentPage + 2;
+ * });
+ * paginator.getPage(); // => 4
+ */
 .factory('Paginator', [function () {
   var DEFAULT_PER_PAGE = 40;
 
@@ -13,33 +43,101 @@ angular.module('cf.app')
     perPage = perPage || DEFAULT_PER_PAGE;
 
     return {
+      /**
+       * @ngdoc method
+       * @name Paginator#setTotal
+       * @description
+       * Sets total number of collection items.
+       */
       setTotal: function (newTotal) {
         total = updateValue(newTotal, total);
       },
+      /**
+       * @ngdoc method
+       * @name Paginator#getTotal
+       * @returns {number}
+       * @description
+       * Gets total number of collection items
+       */
       getTotal: function () {
         return total;
       },
+      /**
+       * @ngdoc method
+       * @name Paginator#setPage
+       * @description
+       * Sets the current page.
+       */
       setPage: function (newPage) {
         page = updateValue(newPage, page);
       },
+      /**
+       * @ngdoc method
+       * @name Paginator#getPage
+       * @returns {number}
+       * @description
+       * Gets the current page.
+       */
       getPage: function () {
         return page;
       },
+      /**
+       * @ngdoc method
+       * @name Paginator#next
+       * @description
+       * Increases the current page number.
+       */
       next: function () {
         page += 1;
       },
+      /**
+       * @ngdoc method
+       * @name Paginator#prev
+       * @description
+       * Decreases the current page number.
+       */
       prev: function () {
         page -= 1;
       },
+      /**
+       * @ngdoc method
+       * @name Paginator#getPerPage
+       * @returns {number}
+       * @description
+       * Gets number of items per page.
+       */
       getPerPage: function () {
         return perPage;
       },
+      /**
+       * @ngdoc method
+       * @name Paginator#getSkipParam
+       * @returns {number}
+       * @description
+       * Gets "skip" param that should be used
+       * to query the current page.
+       */
       getSkipParam: function () {
         return page * perPage;
       },
+      /**
+       * @ngdoc method
+       * @name Paginator#getPageCount
+       * @returns {number}
+       * @description
+       * Gets number of pages.
+       */
       getPageCount: function () {
         return Math.ceil(total / perPage);
       },
+      /**
+       * @ngdoc method
+       * @name Paginator#isAtLast
+       * @returns {boolean}
+       * @description
+       * Returns true if the current page
+       * is the last one.
+       */
       isAtLast: function () {
         return page >= this.getPageCount() - 1;
       }
