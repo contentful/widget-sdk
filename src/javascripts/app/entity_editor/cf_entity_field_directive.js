8ea534ae5241ca049d8e9d54e4169043ae21c848
Jakub Elzbieciak <jakub@contentful.com>
2016-10-05 13:01:55 +0200

    Revert changes to cfEntityField directive

--- src/javascripts/app/entity_editor/cf_entity_field_directive.js
+++ src/javascripts/app/entity_editor/cf_entity_field_directive.js
@@ -19,92 +19,90 @@ angular.module('cf.app')
  * Provided by the `cfValidate` directive.
  *
  */
-.directive('cfEntityField', [function () {
+.directive('cfEntityField', ['$injector', function ($injector) {
+  var TheLocaleStore = $injector.get('TheLocaleStore');
   return {
     restrict: 'E',
     template: JST.cf_entity_field(),
     controllerAs: 'fieldController',
-    controller: 'EntityFieldController'
+    controller: ['$scope', function ($scope) {
+      $scope.hasInitialFocus = $scope.$index === 0 &&
+                               $scope.widget.isFocusable;
+
+      $scope.field = $scope.widget.field;
+      // TODO I think this never changes
+      $scope.$watch('widget.field', function (field) {
+        $scope.field = field;
+      });
+
+      // Records the 'invalid' flag for each locale’s control. Keys are public
+      // locale codes.
+      var invalidControls = {};
+
+      /**
+       * @ngdoc method
+       * @name cfEntityField#fieldController.setInvalid
+       * @description
+       *
+       * @param {string} localeId Public locale code
+       * @param {boolean} isInvalid
+       */
+      this.setInvalid = function (localeId, isInvalid) {
+        invalidControls[localeId] = isInvalid;
+        updateErrorStatus();
+      };
+
+      $scope.$watch('widget.settings.helpText', function (helpText) {
+        $scope.helpText = helpText || $scope.widget.defaultHelpText;
+      });
+
+      $scope.$watch('widget.rendersHelpText', function (rendersHelpText) {
+        $scope.showHelpText = !rendersHelpText;
+      });
+
+      $scope.$watchCollection(getActiveLocaleCodes, updateLocales);
+
+      // TODO Changes to 'validator.errors' change the behavior of
+      // 'validator.hasError()'. We should make this dependency explicity
+      // by listening to signal on the validator.
+      $scope.$watch('validator.errors', updateLocales);
+      $scope.$watch('validator.errors', updateErrorStatus);
+
+      var offFocusChanged = $scope.focus.onChanged(function (value) {
+        $scope.fieldHasFocus = value === $scope.field.id;
+      });
+
+      $scope.$on('$destroy', function () {
+        offFocusChanged();
+      });
+
+      function updateErrorStatus () {
+        var hasSchemaErrors = $scope.validator.hasError(['fields', $scope.field.id]);
+        var hasControlErrors = _.some(invalidControls);
+        $scope.fieldHasErrors = hasSchemaErrors || hasControlErrors;
+      }
+
+      function getActiveLocaleCodes () {
+        return _.map(TheLocaleStore.getActiveLocales(), 'internal_code');
+      }
+
+      function updateLocales () {
+        var field = $scope.widget.field;
+
+        $scope.locales = _.filter(getFieldLocales(field), function (locale) {
+          var isActive = TheLocaleStore.isLocaleActive(locale);
+          var hasError = $scope.validator.hasError(['fields', field.id, locale.internal_code]);
+          return isActive || hasError;
+        });
+      }
+
+      function getFieldLocales (field) {
+        if (field.localized) {
+          return TheLocaleStore.getPrivateLocales();
+        } else {
+          return [TheLocaleStore.getDefaultLocale()];
+        }
+      }
+    }]
   };
-}])
-
-.controller('EntityFieldController', ['require', '$scope', function (require, $scope) {
-  var TheLocaleStore = require('TheLocaleStore');
-  $scope.hasInitialFocus = $scope.$index === 0 &&
-                           $scope.widget.isFocusable;
-
-  $scope.field = $scope.widget.field;
-  // TODO I think this never changes
-  $scope.$watch('widget.field', function (field) {
-    $scope.field = field;
-  });
-
-  // Records the 'invalid' flag for each locale’s control. Keys are public
-  // locale codes.
-  var invalidControls = {};
-
-  /**
-   * @ngdoc method
-   * @name cfEntityField#fieldController.setInvalid
-   * @description
-   *
-   * @param {string} localeId Public locale code
-   * @param {boolean} isInvalid
-   */
-  this.setInvalid = function (localeId, isInvalid) {
-    invalidControls[localeId] = isInvalid;
-    updateErrorStatus();
-  };
-
-  $scope.$watch('widget.settings.helpText', function (helpText) {
-    $scope.helpText = helpText || $scope.widget.defaultHelpText;
-  });
-
-  $scope.$watch('widget.rendersHelpText', function (rendersHelpText) {
-    $scope.showHelpText = !rendersHelpText;
-  });
-
-  $scope.$watchCollection(getActiveLocaleCodes, updateLocales);
-
-  // TODO Changes to 'validator.errors' change the behavior of
-  // 'validator.hasError()'. We should make this dependency explicity
-  // by listening to signal on the validator.
-  $scope.$watch('validator.errors', updateLocales);
-  $scope.$watch('validator.errors', updateErrorStatus);
-
-  var offFocusChanged = $scope.focus.onChanged(function (value) {
-    $scope.fieldHasFocus = value === $scope.field.id;
-  });
-
-  $scope.$on('$destroy', function () {
-    offFocusChanged();
-  });
-
-  function updateErrorStatus () {
-    var hasSchemaErrors = $scope.validator.hasError(['fields', $scope.field.id]);
-    var hasControlErrors = _.some(invalidControls);
-    $scope.fieldHasErrors = hasSchemaErrors || hasControlErrors;
-  }
-
-  function getActiveLocaleCodes () {
-    return _.map(TheLocaleStore.getActiveLocales(), 'internal_code');
-  }
-
-  function updateLocales () {
-    var field = $scope.widget.field;
-
-    $scope.locales = _.filter(getFieldLocales(field), function (locale) {
-      var isActive = TheLocaleStore.isLocaleActive(locale);
-      var hasError = $scope.validator.hasError(['fields', field.id, locale.internal_code]);
-      return isActive || hasError;
-    });
-  }
-
-  function getFieldLocales (field) {
-    if (field.localized) {
-      return TheLocaleStore.getPrivateLocales();
-    } else {
-      return [TheLocaleStore.getDefaultLocale()];
-    }
-  }
 }]);
