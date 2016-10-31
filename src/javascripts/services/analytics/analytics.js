1d81dd3bec2e91d4e8298061c08d922b3973a57d
Jakub Elzbieciak <jakub@contentful.com>
2016-10-31 13:19:23 +0100

    Simplify tracking implementation selection

--- src/javascripts/services/analytics/analytics.js
+++ src/javascripts/services/analytics/analytics.js
@@ -6,38 +6,15 @@
  * @description
  *
  * This service provides an object with different methods to trigger
- * analytics events.
- *
- * The service is disabled in all but the production environment.
- * It can be enabled by appending the '?forceAnalytics' query string to
- * the URL.
- * In the development environment you can send all tracking events to
- * the console for testing by using '?forceAnalyticsDevMode'.
+ * analytics events. External services are called only in production
+ * and staging environments.
  */
 angular.module('contentful')
-.factory('analytics', ['$injector', function ($injector) {
 
-  var $location = $injector.get('$location');
-  var environment = $injector.get('environment');
+.factory('analytics', ['require', function (require) {
+  var whitelist = ['production', 'staging'];
+  var env = dotty.get(require('environment'), 'env');
+  var serviceName = _.includes(whitelist, env) ? 'track' : 'noop';
 
-  var serviceName;
-
-  if (forceDevMode()) {
-    serviceName = 'analytics/consoleLog';
-  } else if (shouldLoadAnalytics()) {
-    serviceName = 'analytics/track';
-  } else {
-    serviceName = 'analytics/noop';
-  }
-
-  return $injector.get(serviceName);
-
-  function shouldLoadAnalytics () {
-    var load = _.includes(['production', 'staging'], environment.env);
-    return load || $location.search().forceAnalytics;
-  }
-
-  function forceDevMode () {
-    return $location.search().forceAnalyticsDevMode;
-  }
+  return require('analytics/' + serviceName);
 }]);
