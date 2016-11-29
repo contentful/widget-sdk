59a364558893265d57323b288a35238752f59d32
Thomas Scholtes <thomas@contentful.com>
2016-11-29 20:00:16 +0100

    feat: Add Kefir.holdWhen() combinator

--- test/unit/utils/kefir_spec.js
+++ test/unit/utils/kefir_spec.js
@@ -223,4 +223,32 @@ describe('utils/kefir', function () {
       expect(() => K.getValue(prop)).toThrowError('Property does not have current value');
     });
   });
+
+  describe('#holdWhen', function () {
+    it('emits values until predicate is true', function () {
+      const prop = KMock.createMockProperty('A');
+
+      const hold = K.holdWhen(prop, (x) => x === 'X');
+      KMock.assertCurrentValue(hold, 'A');
+
+      prop.set('B');
+      KMock.assertCurrentValue(hold, 'B');
+
+      prop.set('X');
+      KMock.assertCurrentValue(hold, 'X');
+
+      prop.set('Y');
+      KMock.assertCurrentValue(hold, 'X');
+    });
+
+    it('ends after predicate is true', function () {
+      const prop = KMock.createMockProperty('A');
+      const hold = K.holdWhen(prop, (x) => x === 'X');
+
+      const ended = sinon.spy();
+      hold.onEnd(ended);
+      prop.set('X');
+      sinon.assert.calledOnce(ended);
+    });
+  });
 });
