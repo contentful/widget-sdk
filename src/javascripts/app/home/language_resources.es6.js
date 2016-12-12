2071dce93701224ea98d6171fd06949b4d50d3cd
Chris Ward <chriswhward@gmail.com>
2016-12-12 15:48:05 +0100

    Add initial links
    Confiorm to code standards

--- src/javascripts/app/home/language_resources.es6.js
+++ src/javascripts/app/home/language_resources.es6.js
@@ -2,39 +2,125 @@ export function get () {
   return {
     'JavaScript': {
       examples: [
-        {name: 'Web app', url: '#', description: 'Integrate Contentful with a Web app'},
-        {name: 'Mobile app', url: '#', description: 'Integrate Contentful with a mobile app'}
+        {
+          name: 'Product catalogue',
+          url: 'https://www.contentful.com/developers/docs/javascript/tutorials/create-expressjs-app-using-contentful/',
+          description: 'Learn how to create a web app with Contentful and Express.js.'
+        },
+        {
+          name: 'Image gallery',
+          url: 'https://github.com/contentful/gallery-app-react',
+          description: 'This GitHub project is a React based gallery application for you to explore.'
+        }
       ],
       links: [
-        {name: 'Overview', url: '#'},
-        {name: 'Getting started', url: 'https://www.contentful.com/developers/docs/javascript/tutorials/using-js-cda-sdk/'},
-        {name: 'Tutorials', url: '#'},
-        {name: 'Example apps', url: '#'},
-        {name: 'FAQ', url: '#'}
+        {name: 'Overview', url: 'https://www.contentful.com/developers/docs/javascript/'},
+        {name: 'Getting started', url: 'https://www.contentful.com/developers/api-demo/javascript/'},
+        {name: 'Tutorials', url: 'https://www.contentful.com/developers/docs/javascript/#tutorials'},
+        {name: 'Example apps', url: 'https://www.contentful.com/developers/docs/javascript/#example-apps'}
       ]
     },
     'PHP': {
       examples: [
-        {name: 'Symfony', url: 'https://www.contentful.com/ecosystem/symfony-bundle'}
+        {
+          name: 'Symfony',
+          url: 'https://www.contentful.com/developers/docs/php/tutorials/getting-started-with-contentful-and-symfony/',
+          description: 'Learn how to use Contentful in your Symfony based applications.'
+        },
+        {
+          name: 'Laravel',
+          url: 'https://www.contentful.com/developers/docs/php/tutorials/getting-started-with-contentful-and-laravel/',
+          description: 'Learn how to use Contentful in your Laravel based applications.'
+        }
+      ],
+      links: [
+        {name: 'Overview', url: 'https://www.contentful.com/developers/docs/php/'},
+        {name: 'Getting started', url: 'https://www.contentful.com/developers/api-demo/php/'},
+        {name: 'Tutorials', url: 'https://www.contentful.com/developers/docs/php/#tutorials'},
+        {name: 'Example apps', url: 'https://www.contentful.com/developers/docs/php/#example-apps'}
       ]
     },
     '.NET': {
       examples: [],
-      links: []
+      links: [
+        {name: 'Overview', url: 'https://www.contentful.com/developers/docs/net/'},
+        {name: 'Tutorials', url: 'https://www.contentful.com/developers/docs/net/#tutorials'}
+      ]
     },
     'Ruby': {
       examples: [
-        {name: 'Jekyll', url: 'https://www.contentful.com/ecosystem/jekyll'},
-        {name: 'Middleman', url: 'https://www.contentful.com/ecosystem/middleman'}
+        {
+          name: 'Ruby on Rails',
+          url: 'https://www.contentful.com/developers/docs/ruby/tutorials/create-your-own-rails-app/',
+          description: 'Learn how to use Contentful in a Rails based application.'
+        },
+        {
+          name: 'Product catalogue',
+          url: 'https://github.com/contentful/contentful_rails_tutorial',
+          description: 'Explore this GitHub repository to learn how to make a product catalogue style application.'
+        }
+      ],
+      links: [
+        {name: 'Overview', url: 'https://www.contentful.com/developers/docs/ruby/'},
+        {name: 'Getting started', url: 'https://www.contentful.com/developers/api-demo/ruby/'},
+        {name: 'Tutorials', url: 'https://www.contentful.com/developers/docs/ruby/#tutorials'},
+        {name: 'Example apps', url: 'https://www.contentful.com/developers/docs/ruby/#example-apps'}
       ]
     },
     'iOS': {
-      examples: [],
-      links: []
+      examples: [
+        {
+          name: 'Swift app',
+          url: 'https://www.contentful.com/developers/docs/ios/tutorials/using-delivery-api-with-swift/',
+          description: 'Create your first Swift based app with Contentful'
+        },
+        {
+          name: 'Objective-C app',
+          url: 'https://www.contentful.com/developers/docs/ios/tutorials/using-delivery-api-with-swift/',
+          description: 'Create your first Objective-C based app with Contentful'
+        },
+        {
+          name: 'Apple Watch',
+          url: 'https://github.com/contentful/ContentfulWatchKitExample',
+          description: 'Explore this GitHub repository to learn how to use Contentful with Apple Watch.'
+        },
+        {
+          name: 'tvOS',
+          url: 'https://github.com/contentful/tvful',
+          description: 'Explore this GitHub repository to learn how to use Contentful with Apple TV.'
+        }
+      ],
+      links: [
+        {name: 'Overview', url: 'https://www.contentful.com/developers/docs/ios/'},
+        {name: 'Getting started', url: 'https://www.contentful.com/developers/api-demo/ios/'},
+        {name: 'Tutorials', url: 'https://www.contentful.com/developers/docs/ios/#tutorials'},
+        {name: 'Example apps', url: 'https://www.contentful.com/developers/docs/ios/#example-apps'}
+      ]
     },
     'Android': {
-      examples: [],
-      links: []
+      examples: [
+        {
+          name: 'A blog app',
+          url: 'https://github.com/contentful/blog-app-android',
+          description: 'Explore this GitHub repository to learn how to make a blog style app.'
+        },
+        {
+          name: 'A gallery app',
+          url: 'https://github.com/contentful/gallery-app-android',
+          description: 'Explore this GitHub repository to learn how to make a gallery style app.'
+        },
+        {
+          name: 'Google cardboard',
+          url: 'https://github.com/contentful-labs/contentful-cardboard',
+          description: 'This GitHub project shows you how to create a VR app with Contentful.'
+        }
+      ],
+      links: [
+        {name: 'Overview', url: 'https://www.contentful.com/developers/docs/android/'},
+        {name: 'Getting started', url: 'https://www.contentful.com/developers/api-demo/android/'},
+        {name: 'Tutorials', url: 'https://www.contentful.com/developers/docs/android/#tutorials'},
+        {name: 'Example apps', url: 'https://www.contentful.com/developers/docs/android/#example-apps'}
+      ]
     },
     'Python': {
       examples: [],
