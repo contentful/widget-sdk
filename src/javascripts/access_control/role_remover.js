8c72fc8716d322659a9919e82ccc4c2e01e8fec4
Thomas Scholtes <thomas@contentful.com>
2017-04-03 11:46:10 +0200

    Lint files

--- src/javascripts/access_control/role_remover.js
+++ src/javascripts/access_control/role_remover.js
@@ -2,19 +2,19 @@
 
 angular.module('contentful').factory('createRoleRemover', ['$injector', function ($injector) {
 
-  var ReloadNotification  = $injector.get('ReloadNotification');
-  var $q                  = $injector.get('$q');
-  var $rootScope          = $injector.get('$rootScope');
-  var modalDialog         = $injector.get('modalDialog');
-  var notification        = $injector.get('notification');
-  var Command             = $injector.get('command');
-  var spaceContext        = $injector.get('spaceContext');
-  var roleRepo            = $injector.get('RoleRepository').getInstance(spaceContext.space);
+  var ReloadNotification = $injector.get('ReloadNotification');
+  var $q = $injector.get('$q');
+  var $rootScope = $injector.get('$rootScope');
+  var modalDialog = $injector.get('modalDialog');
+  var notification = $injector.get('notification');
+  var Command = $injector.get('command');
+  var spaceContext = $injector.get('spaceContext');
+  var roleRepo = $injector.get('RoleRepository').getInstance(spaceContext.space);
   var spaceMembershipRepo = $injector.get('SpaceMembershipRepository').getInstance(spaceContext.endpoint);
 
-  return function createRoleRemover(listHandler, doneFn) {
+  return function createRoleRemover (listHandler, doneFn) {
 
-    return function removeRole(role) {
+    return function removeRole (role) {
       if (getCountFor(role)) {
         modalDialog.open({
           template: 'role_removal_dialog',
@@ -27,7 +27,7 @@ angular.module('contentful').factory('createRoleRemover', ['$injector', function
         remove();
       }
 
-      function remove() {
+      function remove () {
         return roleRepo.remove(role)
         .then(doneFn)
         .then(function () { notification.info('Role successfully deleted.'); })
@@ -35,7 +35,7 @@ angular.module('contentful').factory('createRoleRemover', ['$injector', function
       }
     };
 
-    function prepareRemovalDialogScope(role, remove) {
+    function prepareRemovalDialogScope (role, remove) {
       var scope = $rootScope.$new();
 
       return _.extend(scope, {
@@ -48,7 +48,7 @@ angular.module('contentful').factory('createRoleRemover', ['$injector', function
         })
       });
 
-      function moveUsersAndRemoveRole() {
+      function moveUsersAndRemoveRole () {
         var users = listHandler.getUsersByRole(role.sys.id);
         var memberships = _.map(users, 'membership');
         var moveToRoleId = scope.input.id;
@@ -69,7 +69,7 @@ angular.module('contentful').factory('createRoleRemover', ['$injector', function
       }
     }
 
-    function getCountFor(role) {
+    function getCountFor (role) {
       var counts = listHandler.getMembershipCounts();
       return counts[role.sys.id];
     }
--- src/javascripts/access_control/space_membership_repository.js
+++ src/javascripts/access_control/space_membership_repository.js
@@ -8,7 +8,7 @@ angular.module('contentful').factory('SpaceMembershipRepository', ['require', fu
 
   return { getInstance: getInstance };
 
-  function getInstance(spaceEndpoint) {
+  function getInstance (spaceEndpoint) {
 
     return {
       getAll: getAll,
@@ -19,7 +19,7 @@ angular.module('contentful').factory('SpaceMembershipRepository', ['require', fu
       remove: remove
     };
 
-    function invite(mail, roleId) {
+    function invite (mail, roleId) {
       return spaceEndpoint({
         method: 'POST',
         path: ['space_memberships'],
@@ -31,7 +31,7 @@ angular.module('contentful').factory('SpaceMembershipRepository', ['require', fu
       });
     }
 
-    function inviteAdmin(mail) {
+    function inviteAdmin (mail) {
       return spaceEndpoint({
         method: 'POST',
         path: ['space_memberships'],
@@ -39,21 +39,21 @@ angular.module('contentful').factory('SpaceMembershipRepository', ['require', fu
       });
     }
 
-    function getAll() {
+    function getAll () {
       return fetchAll(spaceEndpoint, ['space_memberships'], PER_PAGE);
     }
 
-    function changeRoleTo(membership, roleId) {
+    function changeRoleTo (membership, roleId) {
       var newMembership = prepareRoleMembership(membership, roleId);
       return changeRole(membership, newMembership);
     }
 
-    function changeRoleToAdmin(membership) {
+    function changeRoleToAdmin (membership) {
       var newMembership = prepareAdminMembership(membership);
       return changeRole(membership, newMembership);
     }
 
-    function changeRole(oldMembership, newMembership) {
+    function changeRole (oldMembership, newMembership) {
       return spaceEndpoint({
         method: 'PUT',
         path: ['space_memberships', oldMembership.sys.id],
@@ -62,7 +62,7 @@ angular.module('contentful').factory('SpaceMembershipRepository', ['require', fu
       });
     }
 
-    function remove(membership) {
+    function remove (membership) {
       return spaceEndpoint({
         method: 'DELETE',
         path: ['space_memberships', membership.sys.id.toString()]
@@ -70,17 +70,17 @@ angular.module('contentful').factory('SpaceMembershipRepository', ['require', fu
     }
   }
 
-  function prepareAdminMembership(membership) {
+  function prepareAdminMembership (membership) {
     var base = _.omit(membership, ['sys', 'user']);
     return _.extend(base, { admin: true, roles: [] });
   }
 
-  function prepareRoleMembership(membership, roleId) {
+  function prepareRoleMembership (membership, roleId) {
     var base = _.omit(membership, ['sys', 'user']);
     return _.extend(base, { admin: false, roles: getRoleLink(roleId) });
   }
 
-  function getRoleLink(roleId) {
+  function getRoleLink (roleId) {
     return [{ type: 'Link', linkType: 'Role', id: roleId }];
   }
 }]);
