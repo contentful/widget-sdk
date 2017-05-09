1cf74967a43b5cc38a0a9ea82f962b59182bac58
Maria Kuzovchikova <iseesomuch@gmail.com>
2017-05-09 16:59:51 +0200

    fixup! Changed invitation notes to directives

--- src/javascripts/access_control/user_invitation_note_directives.js
+++ src/javascripts/access_control/user_invitation_note_directives.js
@@ -5,18 +5,51 @@ angular.module('contentful').directive('cfAddUsersToSpaceNote', ['require', func
 
   return {
     restrict: 'E',
-    template: addUsersNoteTemplate(h),
+    template: addUsersNoteTemplate(),
     controller: 'UserInvitationNotesController'
   };
+
+  function addUsersNoteTemplate () {
+    return h('p', [
+      h('span.add-users-to-space-note', ['You can only add users to this space who are already part of your organization.&#32;']),
+      h('span', { ngIf: 'canInviteUsersToOrganization()' }, [
+        'To invite new users to your organization, and ultimately this space, head to&#32;',
+        h('a', { href: '', ngClick: 'goToOrganizationUsers()' }, ['organizations &amp; billing']),
+        '.'
+      ]),
+      h('span', { ngIf: '!canInviteUsersToOrganization()' }, ['Inviting new users to your organization can be done by an organization owner or admin.'])
+    ]);
+  }
 }])
 .directive('cfNoUsersToAddToSpaceDialog', ['require', function (require) {
   var h = require('utils/hyperscript').h;
 
   return {
     restrict: 'E',
-    template: noUsersToAddToSpaceDialog(h),
+    template: noUsersToAddToSpaceDialog(),
     controller: 'UserInvitationNotesController'
   };
+
+  function noUsersToAddToSpaceDialog () {
+    return h('div', [
+      h('p', [
+        'There are no users in your organization who are not part of this space already. ',
+        h('span', { ngIf: 'canInviteUsersToOrganization()' }, [
+          'Go to ',
+          h('a', { href: '', ngClick: 'goToOrganizationUsers()' }, ['organizations & billings']),
+          ' to invite new users to your organization.'
+        ]),
+        h('span', { ngIf: '!canInviteUsersToOrganization()' }, [
+          'Get in touch with an organization owner or admin to invite new users to your organization.'
+        ])
+      ]),
+      h('button.btn-primary-action.modal-dialog__controls-confirm', {
+        type: 'button',
+        ngClick: 'dialog.cancel()',
+        style: { margin: '10px 0' }
+      }, ['Okay, got it'])
+    ]);
+  }
 }])
 .controller('UserInvitationNotesController', ['$scope', 'require', function ($scope, require) {
   var spaceContext = require('spaceContext');
@@ -29,36 +62,3 @@ angular.module('contentful').directive('cfAddUsersToSpaceNote', ['require', func
   };
   $scope.goToOrganizationUsers = TheAccountView.goToUsers;
 }]);
-
-function addUsersNoteTemplate (h) {
-  return h('p', [
-    h('span.add-users-to-space-note', ['You can only add users to this space who are already part of your organization.&#32;']),
-    h('span', { ngIf: 'canInviteUsersToOrganization()' }, [
-      'To invite new users to your organization, and ultimately this space, head to&#32;',
-      h('a', { href: '', ngClick: 'goToOrganizationUsers()' }, ['organizations &amp; billing']),
-      '.'
-    ]),
-    h('span', { ngIf: '!canInviteUsersToOrganization()' }, ['Inviting new users to your organization can be done by an organization owner or admin.'])
-  ]);
-}
-
-function noUsersToAddToSpaceDialog (h) {
-  return h('div', [
-    h('p', [
-      'There are no users in your organization who are not part of this space already. ',
-      h('span', { ngIf: 'canInviteUsersToOrganization()' }, [
-        'Go to ',
-        h('a', { href: '', ngClick: 'goToOrganizationUsers()' }, ['organizations & billings']),
-        ' to invite new users to your organization.'
-      ]),
-      h('span', { ngIf: '!canInviteUsersToOrganization()' }, [
-        'Get in touch with an organization owner or admin to invite new users to your organization.'
-      ])
-    ]),
-    h('button.btn-primary-action.modal-dialog__controls-confirm', {
-      type: 'button',
-      ngClick: 'dialog.cancel()',
-      style: { margin: '10px 0' }
-    }, ['Okay, got it'])
-  ]);
-}
