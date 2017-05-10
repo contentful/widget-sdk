c9b879387e01f26cfd5c06205f3dd68ba0d68d3c
Thomas Scholtes <thomas@contentful.com>
2017-05-10 11:39:29 +0200

    refactor: Simplify entity helper
    * Remove unused `api` export
    * Use generators in tests
    * Merge partial application and spaceContext delegation logic

--- test/unit/app/entity_editor/entity_helpers_spec.js
+++ test/unit/app/entity_editor/entity_helpers_spec.js
@@ -10,34 +10,33 @@ describe('EntityHelpers', function () {
     });
 
     this.spaceContext = this.$inject('mocks/spaceContext').init();
-    this.helpers = this.$inject('EntityHelpers').api;
+    this.helpers = this.$inject('EntityHelpers').newForLocale('en-US');
   });
 
   describe('#assetUrl', function () {
-    const fileEn = {};
-    const entity = {
-      fields: {test: {}, file: {'en-US': fileEn, 'de-DE': {}}}
-    };
-
-    pit('rejects if the file object cannot be found', function () {
-      return this.helpers.assetFile(entity, 'en-GB').then(throwingFn, _.noop);
+    it('rejects if the file object cannot be found', function* () {
+      yield this.helpers.assetFile(makeAsset({})).then(throwingFn, _.noop);
     });
 
-    pit('resolves with file field for a specific locale', function () {
-      return this.helpers.assetFile(entity, 'en-US').then((file) => {
-        expect(file).toBe(fileEn);
-      });
+    it('resolves with file field for a specific locale', function* () {
+      const fileEn = {};
+      const file = yield this.helpers.assetFile(makeAsset({'en-US': fileEn}));
+      expect(file).toBe(fileEn);
     });
+
+    function makeAsset (file) {
+      return {fields: {file}};
+    }
   });
 
   describe('#assetFileUrl', function () {
-    pit('rejects if invalid file is provided', function () {
-      return this.helpers.assetFileUrl({}).then(throwingFn, _.noop);
+    it('rejects if invalid file is provided', function* () {
+      yield this.helpers.assetFileUrl({}).then(throwingFn, _.noop);
     });
 
-    pit('resolves with URL', function () {
-      return this.helpers.assetFileUrl({url: 'http://some.url/file.txt'})
-      .then((url) => { expect(url).toBe(REWRITTEN_URL); });
+    it('resolves with URL', function* () {
+      const url = yield this.helpers.assetFileUrl({url: 'http://some.url/file.txt'});
+      expect(url).toBe(REWRITTEN_URL);
     });
   });
 
@@ -46,23 +45,23 @@ describe('EntityHelpers', function () {
   itConvertsToEntityAndCallsMethod('entryImage');
 
   function itConvertsToEntityAndCallsMethod (methodName) {
-    pit(`converts data to entity and calls #${methodName}`, function () {
+    it(`converts data to entity and calls #${methodName}`, function* () {
       this.spaceContext.publishedCTs.fetch.resolves({
         data: {fields: [{apiName: 'test', id: 'realid'}]}
       });
 
-      return this.helpers[methodName]({
+      yield this.helpers[methodName]({
         sys: {type: 'Entry', contentType: {sys: {id: 'ctid'}}},
         fields: {test: {}}
-      }, 'en-US').then(() => {
-        sinon.assert.calledOnce(this.spaceContext[methodName]);
-        const [entity, locale] = this.spaceContext[methodName].firstCall.args;
-
-        expect(entity.data.fields).toEqual({realid: {}});
-        expect(entity.getType()).toBe('Entry');
-        expect(entity.getContentTypeId()).toBe('ctid');
-        expect(locale).toBe('en-US');
       });
+
+      sinon.assert.calledOnce(this.spaceContext[methodName]);
+      const [entity, locale] = this.spaceContext[methodName].firstCall.args;
+
+      expect(entity.data.fields).toEqual({realid: {}});
+      expect(entity.getType()).toBe('Entry');
+      expect(entity.getContentTypeId()).toBe('ctid');
+      expect(locale).toBe('en-US');
     });
   }
 });
