88b8a318288f3cbd683f94e15aa535c65b3f9941
Lyn Nagara <lyn.nagara@gmail.com>
2017-05-22 15:55:05 +0200

    Remove next step hints test

--- src/javascripts/app/entry_editor/entry_editor_controller.js
+++ src/javascripts/app/entry_editor/entry_editor_controller.js
@@ -43,13 +43,6 @@ angular.module('contentful')
   var installTracking = require('app/entity_editor/Tracking').default;
   var deepFreeze = require('utils/DeepFreeze').deepFreeze;
   var initDocErrorHandler = require('app/entity_editor/DocumentErrorHandler').default;
-  var LD = require('utils/LaunchDarkly');
-  var analytics = require('analytics/Analytics');
-  var hasAccessToSpaceHome = require('accessChecker').getSectionVisibility().spaceHome;
-  var State = require('data/CMA/EntityState').State;
-  var SumTypes = require('libs/sum-types/caseof-eq');
-  var caseof = SumTypes.caseof;
-  var otherwise = SumTypes.otherwise;
 
   var editorData = $scope.editorData;
   var entityInfo = this.entityInfo = editorData.entityInfo;
@@ -162,67 +155,4 @@ angular.module('contentful')
   var fields = contentTypeData.fields;
   $scope.fields = DataFields.create(fields, $scope.otDoc);
   $scope.transformedContentTypeData = ContentTypes.internalToPublic(contentTypeData);
-
-
-  // A/B experiment - ps-03-2017-next-step-hints
-  $scope.trackNextStepHint = function () {
-    analytics.track('experiment:interaction', {
-      experiment: {
-        id: 'ps-03-2017-next-step-hints',
-        variation: true,
-        interaction_context: 'entry_editor'
-      }
-    });
-  };
-
-  var nextStepHintsTest$ = LD.getTest('ps-03-2017-next-step-hints');
-  var notActivated = !spaceContext.getData('activatedAt');
-  var learnModeOn = hasAccessToSpaceHome && notActivated;
-  var showNextStepHint;
-
-  var HINT_API_CALL = {
-    title: 'Now let’s fetch the content using the API',
-    html: '<a ui-sref="spaces.detail.home" ng-click="trackNextStepHint()">Get an API access token</a>'
-  };
-
-  var HINT_PUBLISH = {
-    title: 'Make any change and publish it',
-    html: 'Click the “Publish” button on the right'
-  };
-
-  K.onValueScope($scope, nextStepHintsTest$, function (shouldShow) {
-    showNextStepHint = shouldShow;
-
-    if (learnModeOn) {
-      if (_.isBoolean(showNextStepHint)) {
-        analytics.track('experiment:start', {
-          experiment: {
-            id: 'ps-03-2017-next-step-hints',
-            variation: showNextStepHint
-          }
-        });
-      }
-    }
-  });
-
-  var nextStepHint$ = K.combineProperties(
-    [nextStepHintsTest$, doc.resourceState.state$],
-    function (showTest, docState) {
-      if (showTest && learnModeOn) {
-        return caseof(docState, [
-          [State.Published(), _.constant(HINT_API_CALL)],
-          [State.Archived(), _.constant(null)],
-          [otherwise, _.constant(HINT_PUBLISH)]
-        ]);
-      } else {
-        return null;
-      }
-    }
-  );
-
-  K.onValueScope($scope, nextStepHint$, function (nextStepHint) {
-    $scope.nextStepHint = nextStepHint;
-  });
-  // End A/B experiment - ps-03-2017-next-step-hints
-
 }]);
