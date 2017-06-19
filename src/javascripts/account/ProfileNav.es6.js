d09e8ddea2f4ed47ae27a935429cfc3d99031fcd
Maria Kuzovchikova <iseesomuch@gmail.com>
2017-06-19 11:20:41 +0200

    Profile nav using NavBar helper

--- src/javascripts/account/ProfileNav.es6.js
+++ src/javascripts/account/ProfileNav.es6.js
@@ -1,15 +1,27 @@
-import {h} from 'utils/hyperscript';
+import {navBar} from 'ui/NavBar';
 
 export default function () {
-  return h('header.workbench-header.x--with-nav', [
-    h('nav.workbench-nav__tabs', [
-      h('a', {
-        role: 'tab',
-        dataTestId: '{{tab.testId}}',
-        cfSref: 'tab.state',
-        ngRepeat: 'tab in nav.tabs track by $index',
-        ariaSelected: '{{tab.selected}}'
-      }, ['{{tab.name}}'])
-    ])
+  return navBar([
+    {
+      title: 'Settings',
+      sref: 'account.profile.user',
+      dataViewType: 'profile-settings'
+    }, {
+      title: 'Spaces',
+      sref: 'account.profile.space_memberships',
+      dataViewType: 'profile-spaces'
+    }, {
+      title: 'Organizations',
+      sref: 'account.profile.organization_memberships',
+      dataViewType: 'profile-organizations'
+    }, {
+      title: 'Access Tokens',
+      sref: 'account.profile.access_grants',
+      dataViewType: 'profile-tokens'
+    }, {
+      title: 'Applications',
+      sref: 'account.profile.applications',
+      dataViewType: 'profile-applications'
+    }
   ]);
 }
