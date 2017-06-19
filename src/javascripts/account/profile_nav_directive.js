6f1559cdfdb3e7ea1a5f0049a9f2b6b847accc49
Maria Kuzovchikova <iseesomuch@gmail.com>
2017-06-19 11:36:01 +0200

    Mark user profile nav tabs as selected

--- src/javascripts/account/profile_nav_directive.js
+++ src/javascripts/account/profile_nav_directive.js
@@ -3,40 +3,8 @@ angular.module('contentful')
   return {
     template: require('account/ProfileNav').default(),
     restrict: 'E',
-    scope: {},
-    controller: function () {
-      var $state = require('$state');
-      var controller = this;
-      var tabs = [
-        {
-          name: 'Settings',
-          state: { path: ['account', 'profile', 'user'] }
-        },
-        {
-          name: 'Spaces',
-          state: { path: ['account', 'profile', 'space_memberships'] }
-        },
-        {
-          name: 'Organizations',
-          state: { path: ['account', 'profile', 'organization_memberships'] }
-        },
-        {
-          name: 'Access Tokens',
-          state: { path: ['account', 'profile', 'access_grants'] }
-        },
-        {
-          name: 'Applications',
-          state: { path: ['account', 'profile', 'applications'] }
-        }
-      ];
-
-      controller.tabs = tabs.map(function (tab) {
-        tab.testId = 'profile-nav-tab-' + tab.name.toLowerCase().replace(/\s+/g, '-');
-        tab.selected = $state.current.name === tab.state.path.join('.');
-        return tab;
-      });
-
-    },
-    controllerAs: 'nav'
+    controller: ['$scope', function ($scope) {
+      $scope.$state = require('$state');
+    }]
   };
 }]);
