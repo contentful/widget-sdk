01d42a36fb1c3fddc69b7b41b930065e6d5383a8
Maria Kuzovchikova <iseesomuch@gmail.com>
2017-06-20 14:23:47 +0200

    Moved navbar templates to directives

--- src/javascripts/account/profile_nav_directive.js
+++ src/javascripts/account/profile_nav_directive.js
@@ -1,10 +1,39 @@
 angular.module('contentful')
 .directive('cfProfileNav', ['require', function (require) {
+  var navBar = require('ui/NavBar').default;
+
   return {
-    template: require('account/ProfileNav').default(),
+    template: template(),
     restrict: 'E',
     controller: ['$scope', function ($scope) {
+      // Required by navbar to highlight the active tab
       $scope.$state = require('$state');
     }]
   };
+
+  function template () {
+    return navBar([
+      {
+        title: 'Settings',
+        sref: 'account.profile.user',
+        dataViewType: 'profile-settings'
+      }, {
+        title: 'Spaces',
+        sref: 'account.profile.space_memberships',
+        dataViewType: 'profile-spaces'
+      }, {
+        title: 'Organizations',
+        sref: 'account.profile.organization_memberships',
+        dataViewType: 'profile-organizations'
+      }, {
+        title: 'Access Tokens',
+        sref: 'account.profile.access_grants',
+        dataViewType: 'profile-tokens'
+      }, {
+        title: 'Applications',
+        sref: 'account.profile.applications',
+        dataViewType: 'profile-applications'
+      }
+    ]);
+  }
 }]);
