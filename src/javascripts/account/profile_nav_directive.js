de49369a7be759e224721a6c89af03d7a6468dd4
Maria Kuzovchikova <iseesomuch@gmail.com>
2017-06-20 15:44:57 +0200

    Added user nav icons

--- src/javascripts/account/profile_nav_directive.js
+++ src/javascripts/account/profile_nav_directive.js
@@ -15,22 +15,27 @@ angular.module('contentful')
     return navBar([
       {
         title: 'Settings',
+        icon: 'nav-user-settings',
         sref: 'account.profile.user',
         dataViewType: 'profile-settings'
       }, {
         title: 'Spaces',
+        icon: 'nav-spaces',
         sref: 'account.profile.space_memberships',
         dataViewType: 'profile-spaces'
       }, {
         title: 'Organizations',
+        icon: 'nav-user-organizations',
         sref: 'account.profile.organization_memberships',
         dataViewType: 'profile-organizations'
       }, {
         title: 'Access Tokens',
+        icon: 'nav-user-oauth',
         sref: 'account.profile.access_grants',
         dataViewType: 'profile-tokens'
       }, {
         title: 'Applications',
+        icon: 'nav-user-applications',
         sref: 'account.profile.applications',
         dataViewType: 'profile-applications'
       }
