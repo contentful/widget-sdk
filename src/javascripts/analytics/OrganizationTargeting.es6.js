46648ff1f9121f6a41c18f182ca643b1f1b74021
Jakub Elżbieciak <jakub@contentful.com>
2017-07-25 12:27:34 +0200

    Add OrganizationTargeting service

---
+++ src/javascripts/analytics/OrganizationTargeting.es6.js
@@ -0,0 +1,44 @@
+import spaceContext from 'spaceContext';
+import {get as getAtPath} from 'lodash';
+import md5 from 'md5';
+
+const ORG_ID_PATH = ['space', 'data', 'organization', 'sys', 'id'];
+
+/**
+ * @ngdoc service
+ * @name analytics/OrganizationTargeting
+ * @description
+ * A service that allows to target selected
+ * organizations with "testable" features.
+ *
+ * `FEATURE_TO_ORG_IDS` is a map of feature
+ * keys to arrays of *hashed* organization IDs.
+ *
+ * A hash can be generated with this command:
+ * `node -e "console.log(require('blueimp-md5')('orgid'))"`
+ *
+ * Make sure you add a comment telling which
+ * organization is represented by the hash.
+ */
+
+const FEATURE_TO_ORG_IDS = {
+  view_roles: [
+    '2d1a93a348d003ae117ee423cb9e89e4' // Jakub's private org (prod)
+  ]
+};
+
+/**
+ * @ngdoc method
+ * @name analytics/OrganizationTargeting#default
+ * @param {string} featureKey
+ * @returns {boolean}
+ * @description
+ * Returns `true` if the feature with `featureKey`
+ * is enabled for the current organization.
+ * Returns `false` otherwise.
+ */
+export default function isEnabled (featureKey) {
+  const hashes = FEATURE_TO_ORG_IDS[featureKey] || [];
+  const orgId = getAtPath(spaceContext, ORG_ID_PATH);
+  return hashes.indexOf(md5(orgId)) > -1;
+}
