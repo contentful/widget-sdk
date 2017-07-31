3f226c54315a02597be650d28be406a175f2f9d1
Jakub Elżbieciak <jelz@post.pl>
2017-07-31 15:29:36 +0200

    Enable role-view assignment for TELUS, Eli Lilly (#1979)
    * Enable role-view assignment for TELUS, Eli Lilly
    
    * fixup! Enable role-view assignment for TELUS, Eli Lilly

--- src/javascripts/analytics/OrganizationTargeting.es6.js
+++ src/javascripts/analytics/OrganizationTargeting.es6.js
@@ -1,6 +1,7 @@
 import {get as getAtPath} from 'lodash';
 import md5 from 'md5';
 
+const SPACE_ID_PATH = ['data', 'sys', 'id'];
 const ORG_ID_PATH = ['data', 'organization', 'sys', 'id'];
 
 /**
@@ -8,16 +9,27 @@ const ORG_ID_PATH = ['data', 'organization', 'sys', 'id'];
  * @name analytics/OrganizationTargeting
  * @description
  * A service that allows to target selected
- * organizations with "testable" features.
+ * organizations and spaces with "testable"
+ * features.
  *
- * `FEATURE_TO_ORG_IDS` is a map of feature
- * keys to arrays of *hashed* organization IDs.
+ * Configuration is stored in two maps:
+ *
+ * - `FEATURE_TO_ORG_IDS` is a map of feature
+ *   keys to arrays of *hashed* organization IDs.
+ * - `FEATURE_TO_SPACE_IDS` is a map of feature
+ *   keys to arrays of *hashed* space IDs.
+ * - A feature is enabled for a space if either
+ *   the space is whitelisted or the organization
+ *   of the space is whitelisted.
+ * - Feature keys must be added to both maps,
+ *   an empty array should be used as a value if
+ *   no space/organization targeting is needed.
  *
  * A hash can be generated with this command:
- * `node -e "console.log(require('blueimp-md5')('orgid'))"`
+ * `node -e "console.log(require('blueimp-md5')('id'))"`
  *
  * Make sure you add a comment telling which
- * organization is represented by the hash.
+ * space/organization is represented by the hash.
  */
 
 const FEATURE_TO_ORG_IDS = {
@@ -28,6 +40,28 @@ const FEATURE_TO_ORG_IDS = {
     '281d50856fc72daca2e39b4e68bdb3ee',
     // Google Docs Test (preview)
     'afbd22d6944eb429cf2b16d5a8ad279b'
+
+    // TELUS digital (prod) - waiting for confirmation
+    // '3dfbcbf7667b2f443cba4415ca963b46'
+  ]
+};
+
+const FEATURE_TO_SPACE_IDS = {
+  view_roles: [
+    // Jakub's contentfiddle space (prod)
+    'f6868d368b5e2fc62d0a3fc7ab6eb29e',
+    // Eli Lilly GSIT-CONN-CONNECT (prod)
+    '3e027c8e222e4efdb3d82dacd4f9db6b',
+    // Eli Lilly gsit-conn-connect-tst (prod)
+    'bfb379e41c085d18f4cf95ff4df31e26',
+    // Eli Lilly GSIT-CONN-CONNECT-MIG (prod)
+    '852fca113cb2dd65c563dfc56c63ceab',
+    // Eli Lilly gsit-conn-connect-dmo (prod)
+    'e1f053b6ad94769135505bfdbb0be407',
+    // Eli Lilly GSIT-CONN-CONNECT-STG (prod)
+    '6ec110b6d3c1bb19d76c04ee612b55c4',
+    // Eli Lilly IdentityHub-DEV (prod)
+    'dceb6b907bf7707ac4e75cc3894c460a'
   ]
 };
 
@@ -39,15 +73,18 @@ const FEATURE_TO_ORG_IDS = {
  * @returns {boolean}
  * @description
  * Returns `true` if the feature with `featureKey`
- * is enabled for the current organization.
+ * is enabled for the given space or its organization.
  * Returns `false` otherwise.
  */
 export default function isEnabled (featureKey, space) {
-  const hashes = FEATURE_TO_ORG_IDS[featureKey];
-  const orgId = getAtPath(space, ORG_ID_PATH);
+  const spaceIdHashes = FEATURE_TO_SPACE_IDS[featureKey];
+  const orgIdHashes = FEATURE_TO_ORG_IDS[featureKey];
 
-  if (Array.isArray(hashes)) {
-    return hashes.indexOf(md5(orgId)) > -1;
+  if (Array.isArray(spaceIdHashes) && Array.isArray(orgIdHashes)) {
+    const spaceId = getAtPath(space, SPACE_ID_PATH);
+    const orgId = getAtPath(space, ORG_ID_PATH);
+    const hasHashed = (coll, id) => coll.indexOf(md5(id)) > -1;
+    return hasHashed(spaceIdHashes, spaceId) || hasHashed(orgIdHashes, orgId);
   } else {
     throw new Error(`Unknown feature: ${featureKey}`);
   }
