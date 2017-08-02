5580703e995a6aae2a9e2a44cb77bf722b72f236
Mudit Ameta <zeusdeux@gmail.com>
2017-08-02 21:36:50 +0200

    Implement entries help page

--- src/javascripts/components/docs_sidebar/Entries.es6.js
+++ src/javascripts/components/docs_sidebar/Entries.es6.js
@@ -1,35 +1,89 @@
 import {h} from 'ui/Framework';
+import clickToCopy from './InputWithCopy';
+import { byName as colorByName } from 'Styles/Colors';
+import { clickLink as trackLinkClick } from 'analytics/events/DocsSidebar';
+import { domain } from 'Config';
 
 export default function template (data) {
-  return h('div', [
-    h('h3', {style: 'color:#bd19ce;'}, ['Entries. Entries! Entries!!!!!!']),
-    h('.docs-sidebar__line', ['Entries are your content.']),
-    h('hr'),
+  const otherQueriesLink = 'https://www.contentful.com/developers/docs/references/content-delivery-api/#/reference/entries';
+  const contentTypesLink = `https://app.${domain}/spaces/${data.state.spaceId}/content_types`;
+
+  return h('div', {
+    style: {
+      padding: '20px 30px'
+    }
+  }, [
     h('.docs-sidebar__line', [
-      h('strong', {style: 'color: #3eab59;'}, ['Get them all'])
+      h('p', ['These are your entries, where you create and manage your written content.'])
     ]),
-    h('.docs-sidebar__line', ['You can fetch all of your entries in this space like this:']),
-    h('code.docs-sidebar__line', [
-      `curl https://cdn.contentful.com/spaces/${data.state.spaceId}/entries?access_token=${data.state.token}`
-    ]),
-    h('hr'),
     h('.docs-sidebar__line', [
-      h('strong', {style: 'color: #3eab59;'}, ['Get just the ninjas'])
+      h('p', ['Each entry’s fields are defined by the way you customize the fields in your content types'])
     ]),
-    h('.docs-sidebar__line', ['Or, just the entries of the ', h('strong', ['ninja']), ' type:']),
-    h('code.docs-sidebar__line', [
-      `curl https://cdn.contentful.com/spaces/${data.state.spaceId}/entries?content_type=ninjas&access_token=${data.state.token}`
-    ]),
-    h('hr'),
     h('.docs-sidebar__line', [
-      'There are many other queries you can do. Read more about ',
-      h('strong', ['entries']),
-      ' in our ',
-      h('a', {
-        href: 'https://www.contentful.com/developers/docs/references/content-delivery-api/#/reference/entries',
-        target: '_blank'
-      }, ['developer docs']),
-      '.'
+      h('strong', [`Fetch entries for "${data.state.contentType.name}" content type:`]),
+      clickToCopy(curl(data), data.actions.render),
+      h('p', [
+        'There are also ',
+        h('a.text-link', {
+          href: otherQueriesLink,
+          target: '_blank',
+          onClick: () => trackLinkClick(otherQueriesLink)
+        }, ['other queries']),
+        ' that you can perform on an entry.'
+      ])
+    ]),
+    h('.docs-sidebar__line', [
+      h('strong', ['What‘s next?']),
+      h('a.text-link', {
+        href: contentTypesLink,
+        style: {
+          display: 'block',
+          marginTop: '10px'
+        },
+        onClick: () => trackLinkClick(contentTypesLink)
+      }, ['Play with your content types'])
     ])
   ]);
+}
+
+function curl (data) {
+  const colorBlue = colorize(colorByName.blueDarkest);
+  const colorGreen = colorize(colorByName.greenDarkest);
+
+  return {
+    children: [
+      h('pre', {
+        style: {
+          whiteSpace: 'pre-wrap',
+          wordWrap: 'break-word',
+          color: colorByName.textMid
+        }
+      }, [
+        h('span', [`curl https://cdn.${domain}/`]),
+        h('span', colorBlue, ['spaces']),
+        h('span', ['/']),
+        h('span', colorGreen, [`${data.state.spaceId}`]),
+        h('span', ['/']),
+        h('span', colorBlue, ['entries']),
+        h('span', ['?']),
+        h('span', colorBlue, ['access_token']),
+        h('span', ['=']),
+        h('span', colorGreen, [`${data.state.token}`]),
+        h('span', ['&']),
+        h('span', colorBlue, ['content_type']),
+        h('span', ['=']),
+        h('span', colorGreen, [`${data.state.contentType.id}`])
+      ])
+    ],
+    text: `curl 'https://cdn.${domain}/spaces/${data.state.spaceId}/entries?access_token=${data.state.token}&content_type=${data.state.contentType.id}'`,
+    id: 'entriesCurl'
+  };
+
+  function colorize (color) {
+    return {
+      style: {
+        color: `${color}`
+      }
+    };
+  }
 }
