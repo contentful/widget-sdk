b29e78d0ad1e5866d36a1f460778855b8ded9023
Maria Kuzovchikova <iseesomuch@gmail.com>
2017-08-09 14:15:39 +0200

    Updated nav svg icons

---
+++ src/javascripts/svg/hamburger.es6.js
@@ -0,0 +1,17 @@
+import {h} from 'ui/Framework';
+
+export default h('svg#Layer_1', {
+  id: 'Layer_1',
+  xmlns: 'http://www.w3.org/2000/svg',
+  width: '18',
+  height: '18',
+  viewBox: '0 0 18 18'
+}, [
+  h('style', '.st0{fill:none}'),
+  h('path.st0', {
+    d: 'M0 0h18v18H0V0z'
+  }),
+  h('path', {
+    d: 'M2.2 13.5h13.5V12H2.2v1.5zm0-3.7h13.5V8.2H2.2v1.6zm0-5.3V6h13.5V4.5H2.2z'
+  })
+]);
