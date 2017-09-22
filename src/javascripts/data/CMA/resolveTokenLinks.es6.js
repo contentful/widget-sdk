79f47fe773d249e39de70cabf99c80311ea67d54
Jakub Elżbieciak <jakub@contentful.com>
2017-09-22 11:43:36 +0200

    Add docs

--- src/javascripts/data/CMA/resolveTokenLinks.es6.js
+++ src/javascripts/data/CMA/resolveTokenLinks.es6.js
@@ -2,6 +2,28 @@ import {isPlainObject, set as setPath, get as getPath} from 'lodash';
 
 const isLink = item => getPath(item, ['sys', 'type']) === 'Link';
 
+/**
+ * @ngdoc service
+ * @name data/CMA/resolveTokenLinks
+ * @description
+ * This module gets data returned by the CMA's `/token` endpoint and resolves
+ * all links in it. Even unreferenced `includes` are resolved too.
+ *
+ * Please note:
+ * - this is not a general link resolution function; assumptions about the
+ *   expected shape of data were made
+ * - this function mutates the object passed into it
+ * - while the full `/token` response should be passed into the function, it
+ *   will return a resolved `tokenData.items[0]`
+ */
+
+/**
+ * @ngdoc method
+ * @name data/CMA/resolveTokenLinks#resolveTokenLinks
+ * @param {object} tokenData
+ * @returns {object}
+ */
+
 export default function resolveTokenLinks (tokenData) {
   const root = tokenData.items[0];
   const {includes} = tokenData;
