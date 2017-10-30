4166433d7f9826f1dd477926387c350062a70797
Mudit Ameta <zeusdeux@gmail.com>
2017-10-30 19:51:34 +0100

    Lazy load WalkMe script for Eli Lilly's space
    TP: https://contentful.tpondemand.com/entity/23806

--- src/javascripts/components/roles/cf_decorate_with_roles_directive.js
+++ src/javascripts/components/roles/cf_decorate_with_roles_directive.js
@@ -5,7 +5,10 @@ angular.module('contentful')
 .directive('cfDecorateWithRoles', ['require', function (require) {
   var SpaceContext = require('spaceContext');
   var LD = require('utils/LaunchDarkly');
+  var LazyLoader = require('LazyLoader');
   var $rootScope = require('$rootScope');
+  var $window = require('$window');
+
   var isAdminAttr = 'data-space-role-is-admin';
   var roleNamesAttr = 'data-space-role-names';
   var featureName = 'feature-fe-10-2017-expose-user-space-role-in-dom-eli-lilly';
@@ -24,8 +27,13 @@ angular.module('contentful')
             var isSpaceAdmin = spaceMembership.admin;
             var spaceRoleNames = _.sortBy(_.map(spaceMembership.roles, 'name')).join(',');
 
+            // load attributes needed by walkME
             $el.attr(isAdminAttr, isSpaceAdmin);
             $el.attr(roleNamesAttr, JSON.stringify(spaceRoleNames));
+
+            // load walkMe
+            $window._walkmeConfig = {smartLoad: true};
+            LazyLoader.get('walkMe');
           }
         });
       });
