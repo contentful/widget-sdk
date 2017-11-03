343e6a7adbe0ca5050cd4bae6a53db23f171fd20
Mudit Ameta <zeusdeux@gmail.com>
2017-11-03 11:26:18 +0100

    WalkMe integration update for Eli Lilly
    This change also makes it easy to add support for new spaces and files.
    All you have to do is add the new file url in our lazy loader manifest
    and add the prop name as a variation in the feature flag.
    
    TP: [TP#23891](https://contentful.tpondemand.com/entity/23981)
    LD: [LD#feature-fe-10-2017-walkme-integration-eli-lilly](https://app.launchdarkly.com/default/production/features/feature-fe-10-2017-walkme-integration-eli-lilly/targeting)

--- src/javascripts/components/roles/cf_roles_for_walk_me.js
+++ src/javascripts/components/roles/cf_roles_for_walk_me.js
@@ -11,24 +11,22 @@ angular.module('contentful')
 
   var isAdminAttr = 'data-space-role-is-admin';
   var roleNamesAttr = 'data-space-role-names';
-  var featureName = 'feature-fe-10-2017-expose-user-space-role-in-dom-eli-lilly';
-  var lastVariation = false;
+  var featureName = 'feature-fe-10-2017-walkme-integration-eli-lilly';
+  var lastVariation = null;
 
   return {
     restrict: 'A',
     scope: {},
     link: function (_scope, $el) {
-      $rootScope.$on('$stateChangeSuccess', function (_e, _to) {
+      $rootScope.$on('$stateChangeSuccess', function () {
         LD.getCurrentVariation(featureName).then(function (variation) {
-          // if the last variation was true and latest is false
-          // reload the page to unload WalkMe script
-          if (lastVariation && !variation) {
+          // if the last variation was for a targeted space
+          // when you move out of it, reload to unload WalkMe scripts
+          if (lastVariation && lastVariation !== variation) {
             $window.location.reload();
           }
 
-          lastVariation = variation;
-
-          if (variation) {
+          if (variation && lastVariation !== variation) {
             var spaceMembership = SpaceContext.getData('spaceMembership');
             var isSpaceAdmin = spaceMembership.admin;
             var spaceRoleNames = _.sortBy(_.map(spaceMembership.roles, 'name')).join(',');
@@ -39,11 +37,21 @@ angular.module('contentful')
 
             // load walkMe
             $window._walkmeConfig = {smartLoad: true};
-            LazyLoader.get('walkMe');
-          } else {
+            // variation will be the key into lazy_loader
+            // this is to make it easy to handle requests for
+            // different files to be loaded for different spaces
+            // without having the space id's hard coded here
+            // and relying on LD to do it for us. The code changes
+            // will be minimal due to this as well
+            LazyLoader.get(variation);
+          }
+
+          if (!variation) {
             $el.removeAttr(isAdminAttr);
             $el.removeAttr(roleNamesAttr);
           }
+
+          lastVariation = variation;
         });
       });
     }
