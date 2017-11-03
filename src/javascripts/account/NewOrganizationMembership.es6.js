a8bc2d49979c81ee84e24cfe22cebea18f6736e9
Guilherme Barbosa <guipvb@gmail.com>
2017-11-03 17:27:03 +0100

    Move template to a separate file to improve readability

--- src/javascripts/account/NewOrganizationMembership.es6.js
+++ src/javascripts/account/NewOrganizationMembership.es6.js
@@ -1,4 +1,4 @@
-import {includes, omit, pick, negate, trim, sortedUniq, sortBy, get as getAtPath} from 'lodash';
+import {omit, pick, negate, trim, sortedUniq, sortBy, get as getAtPath} from 'lodash';
 import {h} from 'ui/Framework';
 import { assign } from 'utils/Collections';
 import {getOrganization} from 'services/TokenStore';
@@ -6,25 +6,29 @@ import * as RoleRepository from 'RoleRepository';
 import {runTask} from 'utils/Concurrent';
 import {ADMIN_ROLE_ID} from 'access_control/SpaceMembershipRepository';
 import {getUsers, getSpaces, createEndpoint} from 'access_control/OrganizationMembershipRepository';
-import {makeCtor, match, isTag} from 'utils/TaggedValues';
+import {makeCtor, match} from 'utils/TaggedValues';
 import {invite, progress$} from 'account/SendOrganizationInvitation';
 import {isValidEmail} from 'stringUtils';
 import {go} from 'states/Navigator';
 import client from 'client';
-import {default as successIcon} from 'svg/checkmark-alt';
-import {default as errorIcon} from 'svg/error';
+import {
+  header,
+  sidebar,
+  emailsInput,
+  organizationRole,
+  accessToSpaces,
+  progressMessage,
+  errorMessage,
+  successMessage
+} from 'account/NewOrganizationMembershipTemplate';
 
 const adminRole = {
   name: 'Admin',
   id: ADMIN_ROLE_ID
 };
 const maxNumberOfEmails = 100;
-const orgRoles = [
-  { name: 'Owner', value: 'owner', description: 'Organization owners can manage subscriptions, billing and organization memberships.' },
-  { name: 'Admin', value: 'admin', description: 'Organization admins cannot manage organization subscriptions nor billing but can manage organization memberships.' },
-  { name: 'Member', value: 'member', description: 'Organization members do not have access to any organization information and only have access to assigned spaces.' }
-];
 const defaultOrgRole = 'member';
+
 const Idle = makeCtor('idle');
 const Invalid = makeCtor('invalid'); // used to show form errors after user tried to submit
 const InProgress = makeCtor('inProgress');
@@ -97,6 +101,7 @@ export default function ($scope) {
       state = assign(state, {spaces: [
         ...state.spaces, {
           id: space.data.sys.id,
+          createdAt: space.data.sys.createdAt,
           name: space.data.name,
           roles: sortBy(spaceRoles, role => role.name)
         }
@@ -334,253 +339,25 @@ function render (state, actions) {
           [InProgress]: () => progressMessage(state.emails, state.successfulOrgInvitations),
           _: () => h('', [
             emailsInput(
+              maxNumberOfEmails,
+              Invalid,
               pick(state, ['emails', 'emailsInputValue', 'invalidAddresses', 'organization', 'status']),
               pick(actions, ['updateEmails'])
             ),
             organizationRole(state.orgRole, actions.updateOrgRole),
-            accessToSpaces(state.spaces, state.orgSpacesCount, state.spaceMemberships, actions.updateSpaceRole)
+            accessToSpaces(
+              adminRole,
+              pick(state, ['spaces', 'orgSpacesCount', 'spaceMemberships']),
+              pick(actions, ['updateSpaceRole'])
+            )
           ])
         })
       ]),
       sidebar(
+        {Idle, Invalid},
         pick(state, ['status', 'organization', 'suppressInvitation']),
         pick(actions, ['toggleInvitationEmailOption'])
       )
     ])
-  ]);
-}
-
-function header () {
-  return h('.workbench-header__wrapper', [
-    h('header.workbench-header', [
-      h('h1.workbench-header__title', ['Organization users'])
-    ])
-  ]);
-}
-
-function sidebar ({
-  status,
-  organization,
-  suppressInvitation
-}, {
-  toggleInvitationEmailOption
-}) {
-  const isDisabled = match(status, {
-    [Idle]: () => false,
-    [Invalid]: () => false,
-    _: () => true
-  });
-
-  return h('.workbench-main__entity-sidebar', [
-    h('.entity-sidebar', [
-      h('p', [`Your organization is using ${organization.membershipsCount} out of ${organization.membershipLimit} users.`]),
-      h('button.cfnext-btn-primary-action.x--block', {
-        type: 'submit',
-        disabled: isDisabled
-      }, ['Send invitation']),
-      organization.hasSsoEnabled ? h('.cfnext-form-option.u-separator--small', [
-        h('label', [
-          h('input', {
-            type: 'checkbox',
-            dataTestId: 'organization-membership.suppress-invitation',
-            checked: !suppressInvitation,
-            onChange: toggleInvitationEmailOption
-          }),
-          'Inform users that they\'ve been added to the organization via email.'
-        ])
-      ]) : '',
-      h('.entity-sidebar__heading', {style: {marginTop: '20px'}}, ['Organization role & space role']),
-      h('p', ['The organization role controls the level of access to the organization settings.']),
-      h('p', ['Access to your organization\'s spaces works independently from that and needs to be defined per space.'])
-    ])
-  ]);
-}
-
-function emailsInput ({
-  emails,
-  emailsInputValue,
-  invalidAddresses,
-  organization,
-  status
-}, {
-  updateEmails
-}) {
-  return h('div', [
-    h('h3.section-title', ['Select users']),
-    h('p', ['Add multiple users by filling in a comma-separated list of email addresses. You can add a maximum of 100 users at a time.']),
-    h('.cfnext-form__field.input', [
-      h('textarea', {
-        dataTestId: 'organization-membership.user-email',
-        autofocus: true,
-        class: 'cfnext-form__input',
-        style: {width: '600px'},
-        value: emailsInputValue,
-        onChange: (evt) => updateEmails(evt.target.value)
-      }),
-      emails.length > organization.remainingInvitations
-        ? h('.cfnext-form__field-error', [`
-          You are trying to add ${emails.length} users but you only have ${organization.remainingInvitations}
-          more available under your plan. Please remove ${emails.length - organization.remainingInvitations} users to proceed.
-          You can upgrade your plan if you need to add more users.
-        `]) : '',
-      emails.length > maxNumberOfEmails
-        ? h('.cfnext-form__field-error', ['Please fill in no more than 100 email addresses.']) : '',
-      invalidAddresses.length
-        ? h('.cfnext-form__field-error', [
-          h('p', ['The following email addresses are not valid:']),
-          h('', [invalidAddresses.join(', ')])
-        ]) : '',
-      !emails.length && isTag(status, Invalid)
-        ? h('.cfnext-form__field-error', ['Please fill in at least one email address.']) : ''
-    ])
-  ]);
-}
-
-function organizationRole (orgRole, updateOrgRole) {
-  return h('div', [
-    h('h3.section-title', ['Organization role']),
-    h('fieldset.cfnext-form__field', orgRoles.map(role => {
-      return h('.cfnext-form-option', [
-        h('label', [
-          h('input', {
-            name: 'organization_membership_role',
-            type: 'radio',
-            id: `organization-membership.org-role.${role.value}`,
-            checked: role.value === orgRole,
-            onChange: (evt) => updateOrgRole(evt.target.checked, role.value)
-          }),
-          ` ${role.name} `,
-          h('span.tooltip-trigger', {style: {position: 'relative'}}, [
-            h('i.fa.fa-question-circle'),
-            h('.tooltip.fade.top.hidden', {
-              style: {
-                width: '200px',
-                bottom: '100%',
-                left: '50%',
-                marginLeft: '-100px'
-              }
-            }, [
-              h('.tooltip-arrow', {style: {left: '50%'}}),
-              h('.tooltip-inner', [role.description])
-            ])
-          ])
-        ])
-      ]);
-    }))
-  ]);
-}
-
-function accessToSpaces (spaces, orgSpacesCount, spaceMemberships, updateSpaceRole) {
-  const isLoading = orgSpacesCount !== 0 && orgSpacesCount > spaces.length;
-  const isEmpty = orgSpacesCount === 0;
-
-  function isChecked (role) {
-    return spaceMemberships.hasOwnProperty(role.spaceId) && includes(spaceMemberships[role.spaceId], role.id);
-  }
-
-  function roleCell (role) {
-    return h('span.cfnext-form-option', {
-      style: { marginBottom: '0' }
-    }, [
-      h('label', [
-        h('input', {
-          type: 'checkbox',
-          checked: isChecked(role),
-          dataTestId: `organization-membership.space.${role.spaceId}.role.${role.name}`,
-          onChange: (evt) => updateSpaceRole(evt.target.checked, role, spaceMemberships)
-        }),
-        ` ${role.name}`
-      ])
-    ]);
-  }
-
-  return h('div', [
-    h('h3.section-title', ['Access to spaces']),
-    h('p', ['Assign one or multiple roles for each space you want the user to be able to access.']),
-    h('table.deprecated-table', [
-      h('thead', [
-        h('th', ['Space']),
-        h('th', {
-          colspan: '2'
-        }, ['Roles'])
-      ]),
-      isLoading
-        ? h('p.u-separator--small', [
-          h('span.spinner--text-inline'),
-          ` Loading your spaces: ${spaces.length} out of ${orgSpacesCount} completed.`
-        ])
-        : h('tbody', spaces.map(space => {
-          return h('tr', [
-            h('td', [space.name]),
-            h('td', [
-              h('.cfnext-form__fieldset--horizontal', [
-                roleCell(assign({spaceId: space.id}, adminRole), updateSpaceRole)
-              ].concat(space.roles.map(role => roleCell(role, updateSpaceRole))))
-            ])
-          ]);
-        }))
-    ]),
-
-    isEmpty ? h('p.u-separator--small', ['You don\'t have any spaces.']) : ''
-  ]);
-}
-
-function progressMessage (emails, successfulOrgInvitations) {
-  const isSuccessful = (email) => includes(successfulOrgInvitations, email);
-
-  return h('', [
-    h('.note-box--info', [
-      h('h3', [`Almost there! ${successfulOrgInvitations.length}/${emails.length} have been added to your organization`]),
-      h('p', ['Please don\'t close this tab until all users have been added successfully.'])
-    ]),
-    h('ul.pill-list.u-separator--small', emails.map(email => {
-      const className = isSuccessful(email) ? 'pill-item--success' : 'is-loading';
-      const icon = isSuccessful(email) ? successIcon : '';
-      return h('li.pill-item', {class: className}, [email, icon]);
-    }))
-  ]);
-}
-
-function errorMessage (failedEmails, restart) {
-  const userString = failedEmails.length > 1 ? 'users' : 'user';
-
-  return h('', [
-    h('.note-box--warning', [
-      h('h3', ['Whoops! something went wrong']),
-      h('p', [
-        `The process failed for the following ${userString}. Please try to `,
-        h('a', {
-          onClick: () => restart(failedEmails)
-        }, ['invite them again']),
-        '.'
-      ])
-    ]),
-    h('ul.pill-list.u-separator--small', failedEmails.map(email => {
-      return h('li.pill-item.pill-item--warning', [email, errorIcon]);
-    }))
-  ]);
-}
-
-function successMessage (emails, successfulOrgInvitations, restart, goToList) {
-  const userString = emails.length > 1 ? 'users have' : 'user has';
-
-  return h('', [
-    h('.note-box--success', [
-      h('h3', [`Yay! ${emails.length} ${userString} been invited to your organization`]),
-      h('p', [
-        'They should have received an email to confirm the invitation in their inbox. Go ahead and ',
-        h('a', {
-          onClick: () => restart()
-        }, ['invite more users']),
-        ' or ',
-        h('a', {
-          onClick: goToList
-        }, ['go back to the users list']),
-        '.'
-      ])
-    ]),
-    h('ul.pill-list.u-separator--small', successfulOrgInvitations.map(email => {
-      return h('li.pill-item.pill-item--success', [email, successIcon]);
-    }))
   ]);
 }
