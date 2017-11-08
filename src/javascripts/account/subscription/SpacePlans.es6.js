5b92cb2501480177cff474433bf9b89abbd5c901
Maria Kuzovchikova <iseesomuch@gmail.com>
2017-11-08 14:11:48 +0100

    Page layout with dummy data

--- src/javascripts/account/subscription/SpacePlans.es6.js
+++ src/javascripts/account/subscription/SpacePlans.es6.js
@@ -4,9 +4,9 @@ export default function ($scope) {
   $scope.component = h('noscript');
   const {properties} = $scope;
 
-  let state = {};
+  let state = {orgId: '', spaces: []};
 
-  rerender({orgId: properties.orgId});
+  rerender({orgId: properties.orgId, spaces: loadSpaces(properties.orgId)});
 
   function rerender (nextState) {
     $scope.properties.context.ready = true;
@@ -16,6 +16,33 @@ export default function ($scope) {
   }
 }
 
-function render ({orgId}) {
-  return h('h2', [`List of spaces for ${orgId} - coming soon`]);
+function loadSpaces () {
+  return Array(4).fill({
+    name: 'MarketingWebsite',
+    planShortName: 'XL'
+  });
+}
+
+function render ({spaces, orgId}) {
+  return h('.workbench', [
+    h('.workbench-header__wrapper', [
+      h('header.workbench-header', [
+        h('.workbench-header__icon', [/* TODO missing icon */]),
+        h('h1.workbench-header__title', ['Spaces'])
+      ])
+    ]),
+    h('.workbench-main', [
+      h('.workbench-main__content', {
+      }, [renderSpaces(spaces)]),
+      h('.workbench-main__sidebar', [renderRightSidebar(orgId)])
+    ])
+  ]);
+}
+
+function renderSpaces (spaces) {
+  return h('ul', spaces.map(({name}) => h('li', [name])));
+}
+
+function renderRightSidebar (orgId) {
+  return h('p', [`Add space to org ${orgId}`]);
 }
