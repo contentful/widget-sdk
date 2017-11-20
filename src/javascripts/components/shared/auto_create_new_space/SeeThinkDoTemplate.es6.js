3da7cfe2619389ddc4c80846ee01f2c15f5a12ce
Vsevolod Zaikov <seva.zaikov@gmail.com>
2017-11-20 13:12:32 +0100

    update design

--- src/javascripts/components/shared/auto_create_new_space/SeeThinkDoTemplate.es6.js
+++ src/javascripts/components/shared/auto_create_new_space/SeeThinkDoTemplate.es6.js
@@ -1,6 +1,7 @@
 import { h } from 'utils/hyperscript';
 import spinner from 'ui/Components/Spinner';
 import { byName, genBoxShadow } from 'Styles/Colors';
+import {default as backgroundIcon} from 'svg/header-illustration-wide';
 
 export default function () {
   return h('.auto-create-space-modal.modal-background', [
@@ -13,12 +14,11 @@ export default function () {
     }, [
       h('div', {
         style: {
-          backgroundColor: '#FFB059',
           width: '100%',
-          height: '125px',
+          height: '154px',
           marginBottom: '40px'
         }
-      }),
+      }, [backgroundIcon]),
       h('.modal-dialog__content', {
         style: {
           padding: '20px',
@@ -61,96 +61,48 @@ export default function () {
             }
           }, ['Where would you like to start?'])
         ]),
-        h('div', {
-          ngShow: '!chosenProjectStatus',
-          style: {
-            padding: '40px 0',
-            display: 'flex',
-            justifyContent: 'space-around'
-          }
-        }, [
-          projectStatusItem({
-            title: 'Understand Contentful',
-            bodyCopy: 'The basics about our flexible APIs and structured content',
-            btnText: 'Start learning',
-            btnId: 'project-status-see'
-          }),
-          projectStatusItem({
-            title: 'Explore an example app',
-            bodyCopy: 'Explore how an example project is structured',
-            btnText: 'Start exploring',
-            btnId: 'project-status-think'
-          }),
-          projectStatusItem({
-            title: 'Start building',
-            bodyCopy: 'The fast track to setting your own project up',
-            btnText: 'Start building',
-            btnId: 'project-status-do'
-          })
-        ]),
-        h('div', {
-          ngShow: 'chosenProjectStatus',
-          style: {
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center'
-          }
-        }, [
-          h('div', {
-            style: {
-              display: 'flex',
-              alignItems: 'center',
-              flexDirection: 'column'
-            }
-          }, [
-            h('div.note-box--success', {
-              style: {
-                marginBottom: '40px'
-              }
-            }, [
-              h('h3', ['Great! We’ll send you an email']),
-              h('p', ['Check your email to view the relevant materials for what you need.'])
-            ]),
-            h('div', {
-              style: {
-                marginBottom: '40px'
-              }
-            }, [
-              h('div', {
-                ngShow: 'chosenProjectStatus === "project-status-see"'
-              }, [
-                h('iframe', {
-                  width: '560',
-                  height: '315',
-                  src: 'https://www.youtube.com/embed/LOcSLS9T6NI?rel=0&showinfo=0',
-                  frameborder: '0',
-                  allowfullscreen: ''
-                })
-              ]),
-              h('div', {
-                ngShow: 'chosenProjectStatus === "project-status-think"'
-              }, ['Stuff for "Think" status']),
-              h('div', {
-                ngShow: 'chosenProjectStatus === "project-status-do"'
-              }, ['Stuff for "Do" status'])
-            ])
-          ]),
-          h('button.btn-action', {
-            ngDisabled: 'isCreatingSpace',
-            ngClick: 'dialog.confirm()'
-          }, ['Continue'])
-        ])
+        renderProjectStatusChoice(),
+        renderChosenProjectStatus()
       ])
     ])
   ]);
 }
 
+function renderProjectStatusChoice () {
+  return h('div', {
+    ngShow: '!chosenProjectStatus',
+    style: {
+      padding: '40px 0',
+      display: 'flex',
+      justifyContent: 'space-around'
+    }
+  }, [
+    projectStatusItem({
+      title: 'Understand Contentful',
+      bodyCopy: 'The basics about our flexible APIs and structured content',
+      btnText: 'Start learning',
+      btnId: 'project-status-see'
+    }),
+    projectStatusItem({
+      title: 'Explore an example app',
+      bodyCopy: 'Explore how an example project is structured',
+      btnText: 'Start exploring',
+      btnId: 'project-status-think'
+    }),
+    projectStatusItem({
+      title: 'Start building',
+      bodyCopy: 'The fast track to setting your own project up',
+      btnText: 'Start building',
+      btnId: 'project-status-do'
+    })
+  ]);
+}
 
 function projectStatusItem ({title, bodyCopy, btnText, btnId}) {
   return h('div', {
     style: {
       border: `1px solid ${byName.elementMid}`,
-      padding: '20px 10px',
+      padding: '24px 15px 20px',
       width: '220px',
       justifyContent: 'space-around',
       boxShadow: genBoxShadow()
@@ -175,5 +127,97 @@ function projectStatusItem ({title, bodyCopy, btnText, btnId}) {
       },
       ngClick: `onProjectStatusSelect('${btnId}')`
     }, [btnText])
+  ]);
+}
+
+function renderChosenProjectStatus () {
+  const titleStyle = {
+    marginTop: '5px',
+    textAlign: 'center'
+  };
+  return h('div', {
+    ngShow: 'chosenProjectStatus',
+    style: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center'
+    }
+  }, [
+    h('div', {
+      style: {
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column'
+      }
+    }, [
+      h('div', {
+        style: {
+          marginBottom: '40px'
+        }
+      }, [
+        h('div', {
+          ngShow: 'chosenProjectStatus === "project-status-see"'
+        }, [
+          h('h2', { style: titleStyle }, ['Understand Contentful']),
+          h('iframe', {
+            width: '560',
+            height: '315',
+            src: 'https://www.youtube.com/embed/LOcSLS9T6NI?rel=0&showinfo=0',
+            frameborder: '0',
+            allowfullscreen: '',
+            style: {
+              marginTop: '15px'
+            }
+          })
+        ]),
+        h('div', {
+          ngShow: 'chosenProjectStatus === "project-status-think"'
+        }, [
+          h('h2', { style: titleStyle }, ['Dig through an example app']),
+          chosenProjectList([
+            'Explore the app code on Github',
+            'Play with the sample content',
+            'Use the APIs for content in seconds'
+          ])
+        ]),
+        h('div', {
+          ngShow: 'chosenProjectStatus === "project-status-do"'
+        }, [
+          h('h2', { style: titleStyle }, ['Dive right in and start building']),
+          chosenProjectList([
+            'All you need to get the project up and running',
+            'Make your first API calls in seconds',
+            'An example project to serve as inspiration'
+          ])
+        ])
+      ])
+    ]),
+    h('button.btn-action', {
+      ngDisabled: 'isCreatingSpace',
+      ngClick: 'dialog.confirm()'
+    }, ['Continue'])
+  ]);
+}
+
+function chosenProjectList (items) {
+  return h('div', [
+    h('div', {
+      style: {
+        height: '1px',
+        margin: '15px auto',
+        width: '350px',
+        background: byName.elementLight
+      }
+    }),
+    h('ul', {
+      style: {
+        fontSize: '20px'
+      }
+    }, items.map(item => h('li', {
+      style: {
+        listStyle: 'disc',
+        lineHeight: '30px'
+      }
+    }, [item])))
   ]);
 }
