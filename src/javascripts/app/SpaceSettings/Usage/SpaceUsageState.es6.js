ec30b4143393670dc9be74b975408327e0d2b06a
Guilherme Barbosa <guipvb@gmail.com>
2018-01-17 17:39:27 +0100

    fixup! Create space Usage page

--- src/javascripts/app/SpaceSettings/Usage/SpaceUsageState.es6.js
+++ src/javascripts/app/SpaceSettings/Usage/SpaceUsageState.es6.js
@@ -3,7 +3,7 @@ import accessChecker from 'accessChecker';
 export default {
   name: 'usage',
   url: '/usage',
-  template: '<cf-space-usage properties="properties" />',
+  template: '<cf-space-usage />',
   controller: ['$scope', '$stateParams', '$state', function ($state) {
     const hasAccess = accessChecker.can('update', 'settings');
     if (!hasAccess) {
