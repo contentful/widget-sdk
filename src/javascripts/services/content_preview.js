f0b1cb0d657a7be5845c9f47d4c6215297908cab
Vsevolod Zaikov <seva.zaikov@gmail.com>
2018-01-23 16:12:47 +0100

    add references pattern to the content preview URLs

--- src/javascripts/services/content_preview.js
+++ src/javascripts/services/content_preview.js
@@ -17,6 +17,21 @@ angular.module('contentful')
 
   var ENTRY_ID_PATTERN = /\{\s*entry_id\s*\}/g;
   var ENTRY_FIELD_PATTERN = /\{\s*entry_field\.(\w+)\s*\}/g;
+
+  // this pattern is used for references resolving. It means that you can get entries,
+  // which link the current one, and use it in the url.
+  // the example syntax is:
+  // `/course/{references.current.course}/lessons/{entry_id}`
+  //
+  // the replacer will find all entities which have current entry as a linked one,
+  // pick the first, and insert `sys.id` value instead of `{references.current.course}`.
+  // Also you can use several references, and the third argument (`course` in our case)
+  // stores it's value, and you can use it in every reference on the left:
+  // `/learning/{references.course.path}/courses/{references.current.course}/lessons/{entry_id}`
+  //
+  // This functionality is primarily needed for rich preview expirience for TEA (the example app):
+  // https://contentful.atlassian.net/wiki/spaces/PROD/pages/204079331/The+example+app+-+Documentation+of+functionality
+  var REFERENCES_PATTERN = /{references\.([a-z]+?\.[a-z]+?):?(\.?\w+?)*\}/g;
   var MAX_PREVIEW_ENVIRONMENTS = 25;
   var STORE_KEY = 'selectedPreviewEnvsForSpace.' + spaceContext.getId();
 
@@ -336,7 +351,7 @@ angular.module('contentful')
    * @param {string} urlTemplate
    * @param {API.Entry} entry
    * @param {API.ContentType} contentType
-   * @returns {string} url
+   * @returns {Promise<string>} url
    *
    * @description
    * Returns the compiled URL with the entry data.
@@ -345,7 +360,7 @@ angular.module('contentful')
   */
   function replaceVariablesInUrl (urlTemplate, entry, contentType) {
     var defaultLocale = TheLocaleStore.getDefaultLocale().internal_code;
-    return urlTemplate
+    var processedUrl = urlTemplate
     .replace(ENTRY_ID_PATTERN, entry.sys.id)
     .replace(ENTRY_FIELD_PATTERN, function (match, fieldId) {
       var internalId = _.get(
@@ -357,6 +372,141 @@ angular.module('contentful')
       var fieldValue = _.get(entry, ['fields', internalId, defaultLocale]);
       return _.toString(fieldValue) || match;
     });
+
+    return resolveReferences({ url: processedUrl, entry: entry, defaultLocale: defaultLocale });
+  }
+
+  /**
+   * @description function to resolve all references in content preview url
+   * all references are resolved one-by-one, starting from the last (the most "right")
+   * they are named, and you can use already resolved references in deeper. By default
+   * you start with `current` entry. As an example:
+   *
+   *                `lesson` was resolved before                         `lesson` - new var
+   * `/course/${references.lesson.course:fields.slug}/lessons/${references.current.lesson:fields.slug}`
+   *
+   * By default, it will resolve to `sys.id` value of the reference. However, you can provide
+   * `:fields.slug` appendix, which will go and grab the value (in default locale) of the referenced entry
+   * @param {Object} params
+   * @param {string} params.url
+   * @param {API.Entry} params.entry
+   * @param {string} params.defaultLocale
+   * @returns {Promise<string>} - url with resolved references (if any)
+   */
+  function resolveReferences (params) {
+    var url = params.url;
+    var entry = params.entry;
+    var defaultLocale = params.defaultLocale;
+    var references = url.match(REFERENCES_PATTERN);
+    if (references) {
+      var promiseChain = Promise.resolve({
+        // current is an id of the initial entry, which we edit
+        // after iterating over references, new values will be added
+        // to this object
+        current: entry.sys.id,
+        // we store all resolved values here, to replace in the url
+        values: []
+      });
+
+      // we pop values from the array each step, so length of the array
+      // goes down with each step
+      while (references.length !== 0) {
+        // IIFE to keep `reference` and `valuePath` in closure - we need to
+        // get the value immediately, but it will be executed only after promise
+        // resolving
+        // after rewriting to ES6 we can just use `let`
+        (function () {
+          var referenceWithBraces = references.pop();
+          // remove first and the last curly braces, and split access parts
+          var wholeReference = referenceWithBraces.replace(/(^\{|\}$)/g, '').split(':');
+          // reference is like `references.current.lesson`. It will get all entries which
+          // this entry is linked to, choose the first, and assign it's value to the third
+          // param (so `lesson` will have a new value, which is picked by valuePath)
+          var reference = wholeReference[0];
+          // valuePath shows how to get value we need in the url. Default value is `sys.id`.
+          // value will be always taken in default locale
+          var valuePath = wholeReference[1];
+
+          // each "previous" reference get's resolved objects from all other references,
+          // so we need to chain them.
+          // this can be rewritten to generators afer switching to es6
+          promiseChain = promiseChain.then(function (params) {
+            var elements = reference.split('.');
+            // since the structure is `references.current.lesson`, we need to get
+            // the first element to get current ID
+            var entryId = params[elements[1]];
+
+            return spaceContext.cma.getEntries({
+              // we are interested only in entries, where current entry is linked to it
+              links_to_entry: entryId
+            }).then(function (entries) {
+              // this element could not exist, which is fine. we will receive an error,
+              // which we handle in the `catch` clause
+              var element = entries.items[0];
+              // we need to resolve value, which we'll insert into URL after resolving all references
+              // by default it is `sys.id`, but we use slugs in TEA
+              var path = getValuePath({ defaultLocale: defaultLocale, valuePath: valuePath });
+              var slugValue = _.get(element, path);
+              // we still need actual ID, since we can resolve entries with linked entities by this ID
+              var elementId = _.get(element, ['sys', 'id']);
+
+              var newParams = {};
+              // since the structure is `references.current.lesson`, we need to get
+              // the last element to assign it to a variable
+              newParams[elements[2]] = elementId;
+              // we need to add this value to the end. Because we are popping references as well,
+              // we'll end up with having first resolved reference as a last element
+              newParams.values = params.values.concat(slugValue);
+
+              return _.extend({}, params, newParams);
+            });
+          });
+        })();
+      }
+
+      return promiseChain.then(function (params) {
+        return url.replace(REFERENCES_PATTERN, function () {
+          return params.values.pop();
+        });
+      }).catch(function () {
+        // in case of failure, some references were not possible to resolve
+        // so we can not create full URL, and in this case we just redirect
+        // to the main page
+        var baseUrl = url.match(/^https?:\/\/.+?\//);
+
+        if (baseUrl) {
+          return baseUrl[0];
+        }
+      });
+    } else {
+      return Promise.resolve(url);
+    }
+  }
+
+  /**
+   * @description fn to retrieve value, which will be resolved and put into URL
+   * by default it returns `sys.id` path, but in case we want to resolve some fields,
+   * default locale's value will be retrieved
+   * @param {Object} params
+   * @param {string|undefined} params.valuePath - selector to get value
+   * @param {string} params.defaultLocale
+   * @returns {string[]} - selector to get value from entry
+   */
+  function getValuePath (params) {
+    var valuePath = params.valuePath;
+    var defaultLocale = params.defaultLocale;
+
+    if (!valuePath) {
+      return ['sys', 'id'];
+    }
+
+    var path = valuePath.split('.');
+
+    if (path[0] === 'fields') {
+      path.push(defaultLocale);
+    }
+
+    return path;
   }
 
   /**
