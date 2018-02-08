contentful/widget-sdk

commit 74363c4b33b2bc019b695b86755752609e81c6e8

Remove user personal info from Bugsnag reports

src/javascripts/analytics/bugsnag.js
@@ -107,8 +107,6 @@ angular.module('contentful')
     if (userId) {
       bugsnag.user = {
         id: userId,
-        firstName: _.get(user, 'firstName'),
-        lastName: _.get(user, 'lastName'),
         adminLink: getAdminLink(user),
         organizations: getOrganizations(user)
       };
@@ -116,10 +114,9 @@ angular.module('contentful')
   }
 
   function getOrganizations (user) {
-    var organizationNames = _.map(user.organizationMemberships, function (m) {
-      return m.organization.name;
-    });
-    return organizationNames.join(', ');
+    return user.organizationMemberships.map(function (membership) {
+      return membership.organization.sys.id;
+    }).join(', ');
   }
 
   function getAdminLink (user) {

test/unit/analytics/bugsnag_spec.js
@@ -44,13 +44,14 @@ describe('bugsnag', function () {
   it('adds user information', function () {
     this.bugsnag.enable({
       sys: {id: 'UID'},
-      firstName: 'FIRST NAME',
-      lastName: 'LAST NAME'
+      organizationMemberships: [
+        {organization: {sys: {id: 'foo'}}},
+        {organization: {sys: {id: 'bar'}}}
+      ]
     });
     this.$apply();
     expect(this.BugsnagStub.user.id).toEqual('UID');
-    expect(this.BugsnagStub.user.firstName).toEqual('FIRST NAME');
-    expect(this.BugsnagStub.user.lastName).toEqual('LAST NAME');
+    expect(this.BugsnagStub.user.organizations).toEqual('foo, bar');
   });
 
   it('enabling after disabling does not send notifications', function () {

commit bfe7ad77f2903390bb7bddec0dfba9d5256e5130

Fix crash for users without organization
This got broken in 74363c4 because `_.map` got replaced with native
`map`.

src/javascripts/analytics/bugsnag.js
@@ -114,7 +114,8 @@ angular.module('contentful')
   }
 
   function getOrganizations (user) {
-    return user.organizationMemberships.map(function (membership) {
+    var organizationMemberships = user.organizationMemberships || [];
+    return organizationMemberships.map(function (membership) {
       return membership.organization.sys.id;
     }).join(', ');
   }

test/unit/analytics/bugsnag_spec.js
@@ -41,17 +41,28 @@ describe('bugsnag', function () {
     sinon.assert.calledOnce(this.LazyLoader.get);
   });
 
-  it('adds user information', function () {
-    this.bugsnag.enable({
-      sys: {id: 'UID'},
-      organizationMemberships: [
-        {organization: {sys: {id: 'foo'}}},
-        {organization: {sys: {id: 'bar'}}}
-      ]
+  describe('user information', function () {
+    it('is added for user without organizations', function () {
+      this.bugsnag.enable({
+        sys: {id: 'USER_ID'}
+      });
+      this.$apply();
+      expect(this.BugsnagStub.user.id).toEqual('USER_ID');
+      expect(this.BugsnagStub.user.organizations).toEqual('');
     });
-    this.$apply();
-    expect(this.BugsnagStub.user.id).toEqual('UID');
-    expect(this.BugsnagStub.user.organizations).toEqual('foo, bar');
+
+    it('is added for user with organizations', function () {
+      this.bugsnag.enable({
+        sys: {id: 'UID'},
+        organizationMemberships: [
+          {organization: {sys: {id: 'foo'}}},
+          {organization: {sys: {id: 'bar'}}}
+        ]
+      });
+      this.$apply();
+      expect(this.BugsnagStub.user.id).toEqual('UID');
+      expect(this.BugsnagStub.user.organizations).toEqual('foo, bar');
+    });
   });
 
   it('enabling after disabling does not send notifications', function () {