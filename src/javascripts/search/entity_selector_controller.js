contentful/widget-sdk

commit c0682695e3cabf83752b42a318cf5b9cd253cfdc

Use new search for entity selector (#2493)
* Use new search for entity selector

* Solve entity selector search z-index issue

* Alow custom search input placeholder

* Remove custom search input placeholder

We do not need this after all since we will use the default placeholder
for the entity selector instead.

* Pass only valid referred CTs to entity selector modal

This shows only valid content types in the search bar of the
entity selector modal and removes the "any" option if there is only
one option. This also removes the spinner from the modal body when
the user searches.

* Do not flush previous results while waiting for response

* Show list of all CTs for entries w/o specific CT

src/javascripts/search/entity_selector_controller.js
@@ -36,12 +36,24 @@ angular.module('contentful')
   var Paginator = require('Paginator');
   var createQueue = require('overridingRequestQueue');
   var EntityHelpers = require('EntityHelpers');
+  var K = require('utils/kefir');
+  var Kefir = require('libs/kefir');
+  var LD = require('utils/LaunchDarkly');
+  var createSearchInput = require('app/ContentList/Search').default;
 
+  var NEW_SEARCH_FLAG_NAME = 'feature-at-01-2018-entity-selector-new-search';
   var MIN_SEARCH_TRIGGERING_LEN = 1;
   var MODES = {AVAILABLE: 1, SELECTED: 2};
 
   var config = $scope.config;
   var itemsById = {};
+
+  LD.onFeatureFlag($scope, NEW_SEARCH_FLAG_NAME, function (isEnabled) {
+    $scope.isNewSearchEnabled = isEnabled;
+    if (isEnabled) {
+      initializeSearchUI();
+    }
+  });
 
   var load = createQueue(fetch, function (resultPromise) {
     resultPromise.then(handleResponse);
@@ -55,7 +67,7 @@ angular.module('contentful')
     return entity.sys.id;
   });
 
-  _.extend($scope, MODES, {
+  _.assign($scope, MODES, {
     spaceContext: spaceContext,
     view: {mode: MODES.AVAILABLE},
     paginator: Paginator.create(),
@@ -84,6 +96,48 @@ angular.module('contentful')
     }
   }
 
+  function initializeSearchUI () {
+    var withAssets = config.entityType === 'Asset';
+    var initialSearchState = {};
+    if ($scope.singleContentType) {
+      initialSearchState.contentTypeId = $scope.singleContentType.getId();
+    }
+    var isSearching$ = K.fromScopeValue($scope, function ($scope) {
+      return $scope.isLoading;
+    });
+    var contentTypes = getValidContentTypes(
+      $scope.config.linkedContentTypeIds,
+      spaceContext.publishedCTs.getAllBare()
+    );
+
+    createSearchInput({
+      $scope: $scope,
+      contentTypes: contentTypes,
+      onSearchChange: onSearchChange,
+      isSearching$: isSearching$,
+      initState: initialSearchState,
+      users$: Kefir.fromPromise(spaceContext.users.getAll()),
+      withAssets: withAssets
+    });
+
+    function getValidContentTypes (linkedContentTypeIds, contentTypes) {
+      var acceptsOnlySpecificContentType = linkedContentTypeIds.length > 0;
+
+      if (acceptsOnlySpecificContentType) {
+        contentTypes = contentTypes.filter(function (ct) {
+          return linkedContentTypeIds.indexOf(ct.sys.id) > -1;
+        });
+      }
+
+      return contentTypes;
+    }
+  }
+
+  function onSearchChange (newSearchState) {
+    _.assign($scope.view, newSearchState);
+    resetAndLoad();
+  }
+
   function fetch () {
     $scope.isLoading = true;
     return config.fetch(getParams());
@@ -91,16 +145,29 @@ angular.module('contentful')
 
   function getParams () {
     var params = {
-      searchTerm: $scope.view.searchTerm,
       order: getOrder(),
       paginator: $scope.paginator
     };
+    if ($scope.isNewSearchEnabled) {
+      _.assign(params, getSearch());
+    } else {
+      params.searchTerm = $scope.view.searchTerm;
+    }
 
     if (config.entityType === 'Entry' && $scope.singleContentType) {
       params.contentTypeId = $scope.singleContentType.getId();
     }
 
     return params;
+  }
+
+  function getSearch () {
+    var view = $scope.view || {};
+    return {
+      searchText: view.searchText || '',
+      searchFilters: view.searchFilters || [],
+      contentTypeId: view.contentTypeId
+    };
   }
 
   function getOrder () {
@@ -172,6 +239,7 @@ angular.module('contentful')
 
   function handleResponse (res) {
     $scope.paginator.setTotal(res.total);
+    $scope.items = [];
     $scope.items.push.apply($scope.items, getItemsToAdd(res));
 
     $timeout(function () {
@@ -192,7 +260,6 @@ angular.module('contentful')
   }
 
   function resetAndLoad () {
-    $scope.items = [];
     itemsById = {};
     $scope.paginator.setTotal(0);
     $scope.paginator.setPage(0);

src/javascripts/app/ContentList/Search/Components/QueryInput.es6.js
@@ -30,16 +30,15 @@ const QueryInput = createReactClass({
   },
   render () {
     const {
-      isPlaceholderVisible,
+      placeholder,
       autoFocus,
       onKeyDown
     } = this.props;
     const { value } = this.state;
-    const placeholder = isPlaceholderVisible ? 'Type to search for entries' : '';
 
     // Replacing spaces with `|` to make the width of the shadow element equal
     // the width of the input.
-    const shadowValue = (this.state.value || placeholder).replace(/\s/g, '|');
+    const shadowValue = (this.state.value || placeholder || '').replace(/\s/g, '|');
 
     // TODO: extract shadow-resize and reuse in TextValueInput
     return h(
@@ -69,7 +68,7 @@ const QueryInput = createReactClass({
 });
 
 QueryInput.propTypes = {
-  isPlaceholderVisible: PropTypes.bool,
+  placeholder: PropTypes.string,
   autoFocus: PropTypes.bool,
   onKeyDown: PropTypes.func.isRequired,
   onChange: PropTypes.func.isRequired

src/javascripts/app/ContentList/Search/Filters.es6.js
@@ -130,14 +130,17 @@ const assetsFieldFilters = [
  * to contstruct the available options to select from.
  */
 export function contentTypeFilter (contentTypes) {
+  let valueInputSelect = [...contentTypes.map(ct => [ct.sys.id, ct.name])];
+
+  if (contentTypes.length > 1) {
+    valueInputSelect = [['', 'Any'], ...valueInputSelect];
+  }
+
   return {
     name: 'Content type',
     queryKey: 'content_type',
     operators: [equalityOperator],
-    valueInput: ValueInput.Select([
-      ['', 'Any'],
-      ...contentTypes.map(ct => [ct.sys.id, ct.name])
-    ])
+    valueInput: ValueInput.Select(valueInputSelect)
   };
 }
 

src/javascripts/app/ContentList/Search/View.es6.js
@@ -50,6 +50,8 @@ export default function render ({
   const hasSpinner = isSearching || isTyping;
   const hasFilters = filters.length > 0;
   const defaultFocus = focus;
+  const placeholder = hasFilters
+    ? '' : 'Type to search for ' + (withAssets ? 'assets' : 'entries');
 
   if (!hasLoaded) {
     return renderLoader();
@@ -103,7 +105,7 @@ export default function render ({
           onRemoveAttempt: ({ index }) => actions.SetFocusOnPill(index)
         }),
         React.createElement(QueryInput, {
-          isPlaceholderVisible: !hasFilters,
+          placeholder,
           value: input || '',
           onChange: value => actions.SetQueryInput(value),
           autoFocus: !input && !hasFilters,

src/javascripts/app/ContentList/Search/index.es6.js
@@ -15,17 +15,16 @@ import {
   sanitizeSearchFilters
 } from './Filters';
 
-export default function create (
+export default function create ({
   $scope,
-  spaceContext,
+  contentTypes = [],
   onSearchChange,
   isSearching$,
   initState = {},
   users$,
   withAssets = false
-) {
+}) {
   try {
-    const contentTypes = spaceContext.publishedCTs.getAllBare();
     // Removes invalid filters before initializing the state.
     const sanitizedFilters = sanitizeSearchFilters(
       initState.searchFilters,
@@ -58,11 +57,13 @@ export default function create (
       }
     );
 
-    const unsubscribeFromSearchStore = K.onValueScope($scope, store.state$, state => {
-      window._state = state;
-
-      $scope.search = renderSearch(mapStateToProps(state, actions));
-    });
+    const unsubscribeFromSearchStore = K.onValueScope(
+      $scope,
+      store.state$,
+      state => {
+        $scope.search = renderSearch(mapStateToProps(state, actions));
+      }
+    );
 
     $scope.unsubscribeSearch = () => {
       unsubscribeSearchWidget();

src/javascripts/components/tabs/asset_list/asset_search_controller.js
@@ -177,15 +177,15 @@ angular.module('contentful')
       return;
     }
     lastUISearchState = initialSearchState;
-    createSearchInput(
-      $scope,
-      spaceContext,
-      onSearchChange,
-      isSearching$,
-      initialSearchState,
-      Kefir.fromPromise(spaceContext.users.getAll()),
-      withAssets
-    );
+    createSearchInput({
+      $scope: $scope,
+      contentTypes: spaceContext.publishedCTs.getAllBare(),
+      onsSearchChange: onSearchChange,
+      isSearching$: isSearching$,
+      initState: initialSearchState,
+      users$: Kefir.fromPromise(spaceContext.users.getAll()),
+      withAssets: withAssets
+    });
   }
 
   function prepareQuery () {

src/javascripts/components/tabs/entry_list/entry_list_search_controller.js
@@ -139,14 +139,14 @@ angular.module('contentful')
       return;
     }
     lastUISearchState = initialSearchState;
-    createSearchInput(
-      $scope,
-      spaceContext,
-      onSearchChange,
-      isSearching$,
-      initialSearchState,
-      Kefir.fromPromise(spaceContext.users.getAll())
-    );
+    createSearchInput({
+      $scope: $scope,
+      contentTypes: spaceContext.publishedCTs.getAllBare(),
+      onSearchChange: onSearchChange,
+      isSearching$: isSearching$,
+      initState: initialSearchState,
+      users$: Kefir.fromPromise(spaceContext.users.getAll())
+    });
   }
 
   function setupEntriesHandler (promise) {

src/javascripts/search/entity_selector_dialog.jade
@@ -37,12 +37,15 @@ mixin single-list-item
             | Hide {{ labels.selected }} ({{ selected.length }})
 
       .entity-selector__search(
-        ng-if="supportsAdvancedSearch"
+        ng-if="supportsAdvancedSearch && !isNewSearchEnabled"
         cf-tokenized-search="view.searchTerm"
         search-content-type="singleContentType"
         placeholder="{{ getSearchPlaceholder() }}"
         cf-on-blur-component="leftSearch($event)"
         cf-on-blur-component-when="showAutocompletions")
+
+      .entity-selector__search(ng-if="supportsAdvancedSearch && isNewSearchEnabled")
+        cf-component-bridge(component="search")
 
       .entity-selector__search(ng-if="!supportsAdvancedSearch")
         input.cfnext-form__input--full-size(
@@ -65,8 +68,6 @@ mixin single-list-item
           ng-repeat="entity in items track by entity.sys.id"
           ng-class="{'x--selected': selectedIds[entity.sys.id],'entity-selector__last-item': $last && paginator.atLast()}")
           +single-list-item
-
-        .loading(ng-if="isLoading")
 
       .entity-selector__item-list(
         ng-if="view.mode === SELECTED"

commit ccabbdeca414caefcdaf43b51827f51e90068ad6

Fix entity selector result list scrolling
This got broken in c068269. When scrolling to the bottom, the list
did always reset instead of adding the new results.

src/javascripts/search/entity_selector_controller.js
@@ -56,9 +56,7 @@ angular.module('contentful')
     }
   });
 
-  var load = createQueue(fetch, function (resultPromise) {
-    resultPromise.then(handleResponse);
-  });
+  var load = createQueue(fetch);
 
   // Returns a promise for the content type of the given entry.
   // We cache this by the entry id
@@ -241,17 +239,15 @@ angular.module('contentful')
 
   function handleResponse (res) {
     $scope.paginator.setTotal(res.total);
-    $scope.items = [];
     $scope.items.push.apply($scope.items, getItemsToAdd(res));
-
     $timeout(function () {
       $scope.isLoading = false;
     });
   }
 
   function getItemsToAdd (res) {
-    // @TODO - does backend ever return duplicate items for any query?
-    // If no, we should remove this
+    // The api could theoretically return some of the entities returned already if
+    // new entities were created in the meantime.
     return _.transform(res.items, function (acc, item) {
       var id = _.get(item, 'sys.id');
       if (id && !itemsById[id]) {
@@ -262,10 +258,14 @@ angular.module('contentful')
   }
 
   function resetAndLoad () {
-    itemsById = {};
-    $scope.paginator.setTotal(0);
-    $scope.paginator.setPage(0);
-    load();
+    $scope.isLoading = true;
+    load().then(function (response) {
+      $scope.items = [];
+      itemsById = {};
+      $scope.paginator.setTotal(0);
+      $scope.paginator.setPage(0);
+      handleResponse(response);
+    });
   }
 
   function loadMore () {
@@ -274,8 +274,9 @@ angular.module('contentful')
     if (!$scope.config.noPagination &&
         !$scope.isLoading &&
         !$scope.paginator.isAtLast()) {
+      $scope.isLoadingMore = true;
       $scope.paginator.next();
-      load();
+      load().then(handleResponse);
     }
   }
 