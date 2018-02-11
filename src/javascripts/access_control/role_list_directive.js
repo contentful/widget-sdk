8373ec81b7bab42b1731264450f5814ca3bb93f9
Guilherme Barbosa <guipvb@gmail.com>
2018-02-12 00:07:14 +0100

    Fix usage and limit display in roles list

--- src/javascripts/access_control/role_list_directive.js
+++ src/javascripts/access_control/role_list_directive.js
@@ -10,6 +10,7 @@ angular.module('contentful').directive('cfRoleList', function () {
 
 angular.module('contentful').controller('RoleListController', ['$scope', 'require', function ($scope, require) {
   var $state = require('$state');
+  var $q = require('$q');
   var ReloadNotification = require('ReloadNotification');
   var listHandler = require('UserListHandler').create();
   var createRoleRemover = require('createRoleRemover');
@@ -17,6 +18,34 @@ angular.module('contentful').controller('RoleListController', ['$scope', 'requir
   var jumpToRoleMembers = require('UserListController/jumpToRole');
   var spaceContext = require('spaceContext');
   var ADMIN_ROLE_ID = require('access_control/SpaceMembershipRepository').ADMIN_ROLE_ID;
+  var createResourceService = require('services/ResourceService').default;
+  var ResourceUtils = require('utils/ResourceUtils');
+
+  var org = spaceContext.organizationContext.organization;
+
+  function checkIfCanModifyRoles () {
+    var subscription = spaceContext.subscription;
+    var trialLockdown = subscription &&
+    subscription.isTrial() && subscription.hasTrialEnded();
+    if (trialLockdown) { return Promise.resolve(false); } else { return accessChecker.canModifyRoles(); }
+  }
+
+  $q.all({
+    canModifyRoles: accessChecker.canModifyRoles(),
+    resource: createResourceService(spaceContext.getId()).get('role'),
+    useLegacy: ResourceUtils.useLegacy(org)
+  }).then(function (result) {
+    var subscription = spaceContext.subscription;
+    var isTrial = subscription.isTrial();
+    var trialLockdown = isTrial && subscription.hasTrialEnded();
+    var canCreate = ResourceUtils.canCreate(result.resource);
+
+    $scope.resourceContext = result.useLegacy ? 'organization' : 'space';
+    $scope.usage = result.resource.usage;
+    $scope.limit = result.resource.limits.maximum;
+    $scope.reachedLimit = !canCreate;
+    $scope.canModifyRoles = !trialLockdown && result.canModifyRoles;
+  });
 
   checkIfCanModifyRoles().then(function (value) {
     $scope.canModifyRoles = value;
@@ -32,12 +61,7 @@ angular.module('contentful').controller('RoleListController', ['$scope', 'requir
     jumpToRoleMembers(ADMIN_ROLE_ID);
   }
 
-  function checkIfCanModifyRoles () {
-    var subscription = spaceContext.subscription;
-    var trialLockdown = subscription &&
-      subscription.isTrial() && subscription.hasTrialEnded();
-    if (trialLockdown) { return Promise.resolve(false); } else { return accessChecker.canModifyRoles(); }
-  }
+
 
   function duplicateRole (role) {
     $state.go('spaces.detail.settings.roles.new', {baseRoleId: role.sys.id});
