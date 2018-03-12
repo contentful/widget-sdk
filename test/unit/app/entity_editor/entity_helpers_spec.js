151953120d30021e987c793a6e3ae34076b75c36
Daniel Werner <daniel.werner@contentful.com>
2018-03-12 09:33:07 +0100

    Fix inline ref editor toggle tracking
    With the `reference_editor:toggle_inline_editor` event we were tracking
    data related to the CT of the entry containing the inline editing field.
    Instead we now track data related to the CT of the inlined entry.
    Therefore we bump the `version` of the generic event to `2`.

--- test/unit/app/entity_editor/entity_helpers_spec.js
+++ test/unit/app/entity_editor/entity_helpers_spec.js
@@ -10,7 +10,8 @@ describe('EntityHelpers', function () {
     });
 
     this.spaceContext = this.$inject('mocks/spaceContext').init();
-    this.helpers = this.$inject('EntityHelpers').newForLocale('en-US');
+    this.EntityHelpers = this.$inject('EntityHelpers');
+    this.helpers = this.EntityHelpers.newForLocale('en-US');
   });
 
   describe('#assetFileUrl', function () {
@@ -48,4 +49,54 @@ describe('EntityHelpers', function () {
       expect(locale).toBe('en-US');
     });
   }
+
+  describe('.contentTypeFieldLinkCtIds()', function () {
+    beforeEach(function () {
+      this.getIds = this.EntityHelpers.contentTypeFieldLinkCtIds;
+    });
+
+    it('returns valid IDs for `Link` field', function () {
+      const ids = [ 'ID_1', 'ID_2' ];
+      const field = newLinkField(ids);
+      expect(this.getIds(field)).toEqual(ids);
+    });
+
+    it('returns empty array for `Link` field without validation', function () {
+      const field = newLinkField();
+      delete field.validations;
+      expect(this.getIds(newLinkField())).toEqual([]);
+    });
+
+    it('returns valid IDs for `Array<Link>` field', function () {
+      const ids = [ 'ID_FOO', 'ID_BAR' ];
+      const field = newArrayField(newLinkField(ids));
+      expect(this.getIds(field)).toEqual(ids);
+    });
+
+    it('returns empty array for `Array<Link>` field without validation', function () {
+      const linksField = newLinkField();
+      delete linksField.validations;
+      const field = newArrayField(linksField);
+      delete field.validations;
+      expect(this.getIds(newLinkField())).toEqual([]);
+    });
+
+    function newArrayField (itemsField) {
+      return {
+        type: 'Array',
+        items: itemsField,
+        validations: []
+      };
+    }
+
+    function newLinkField (ids) {
+      return {
+        type: 'Link',
+        validations: [
+          { someValidation: 'abc' },
+          { linkContentType: ids || [] }
+        ]
+      };
+    }
+  });
 });
