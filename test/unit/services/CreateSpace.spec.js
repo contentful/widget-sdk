feaaa0c664329e214c959cf83e49faf2763dbb2c
Maria Kuzovchikova <iseesomuch@gmail.com>
2018-03-26 14:20:23 +0200

    fixup! Fixed unit tests for CreateSpace service

--- test/unit/services/CreateSpace.spec.js
+++ test/unit/services/CreateSpace.spec.js
@@ -7,9 +7,9 @@ describe('CreateSpace', function () {
     this.v1Org = {sys: {id: 'v1'}, pricingVersion: 'pricing_version_1'};
     this.v2Org = {sys: {id: 'v2'}, pricingVersion: 'pricing_version_2'};
 
-    this.getOrganization = sinon.stub()
-      .withArgs('v1').resolves(this.v1Org)
-      .withArgs('v2').resolves(this.v2Org);
+    this.getOrganization = sinon.stub().rejects();
+    this.getOrganization.withArgs('v1').resolves(this.v1Org);
+    this.getOrganization.withArgs('v2').resolves(this.v2Org);
 
     module('contentful/test', ($provide) => {
       $provide.value('services/TokenStore', {getOrganization: this.getOrganization});
@@ -22,25 +22,25 @@ describe('CreateSpace', function () {
   });
 
   describe('#showDialog', function () {
-    it('opens old dialog with v1 org', async function () {
-      await this.CreateSpace.showDialog('v1');
+    it('opens old dialog with v1 org', function* () {
+      yield this.CreateSpace.showDialog('v1');
       const modalArgs = this.modalDialog.open.firstCall.args[0];
       expect(modalArgs.scopeData.organizationId).toBe('v1');
       expect(modalArgs.template).toContain('cf-create-new-space');
       sinon.assert.calledOnce(this.modalDialog.open);
     });
 
-    it('opens wizard with v2 org ID', async function () {
-      await this.CreateSpace.showDialog('v1');
+    it('opens wizard with v2 org ID', function* () {
+      yield this.CreateSpace.showDialog('v2');
       const modalArgs = this.modalDialog.open.firstCall.args[0];
       expect(modalArgs.scopeData.organizationId).toBe('v2');
       expect(modalArgs.template).toContain('cf-create-space-wizard');
       sinon.assert.calledOnce(this.modalDialog.open);
     });
 
-    it('takes org id from space context if not passed explicitly', async function () {
+    it('takes org id from space context if not passed explicitly', function* () {
       this.spaceContext.organization = {sys: {id: 'SPACE_ORG'}, pricingVersion: 'pricing_version_1'};
-      await this.CreateSpace.showDialog();
+      yield this.CreateSpace.showDialog();
       const modalArgs = this.modalDialog.open.firstCall.args[0];
       expect(modalArgs.scopeData.organizationId).toBe('SPACE_ORG');
     });
