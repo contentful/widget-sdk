655b8ce8545ef235bc2c1271cd9100341541d719
Maria Kuzovchikova <iseesomuch@gmail.com>
2018-04-09 12:04:31 +0200

    Show space details on confirmation page

--- src/javascripts/components/shared/create-space-wizard/ConfirmScreen.es6.js
+++ src/javascripts/components/shared/create-space-wizard/ConfirmScreen.es6.js
@@ -4,12 +4,20 @@ import PropTypes from 'libs/prop-types';
 
 const ConfirmScreen = createReactClass({
   propTypes: {
+    spaceRatePlan: PropTypes.object.isRequired,
+    spaceName: PropTypes.string.isRequired,
+    template: PropTypes.object,
+    organization: PropTypes.object.isRequired,
     isFormSubmitted: PropTypes.bool,
     onSubmit: PropTypes.func.isRequired,
     onDimensionsChange: PropTypes.func.isRequired
   },
   render () {
     const {
+      spaceRatePlan,
+      spaceName,
+      template,
+      organization,
       isFormSubmitted,
       onSubmit
     } = this.props;
@@ -21,6 +29,15 @@ const ConfirmScreen = createReactClass({
         </h2>
         <p className="create-space-wizard__subheading">
           Make sure everything is in order before creating the space.
+        </p>
+        <p>
+          You are about to purchase a {spaceRatePlan.name.toLowerCase()} space
+          for ${spaceRatePlan.price} / month for the organization {organization.name}.
+          The space’s name will be {spaceName}, and
+          {template
+            ? ` we'll fill it with example content for ${template.name}.`
+            : ' we won’t fill it with example content.'
+          }
         </p>
         <div style={{textAlign: 'center', margin: '1.2em 0'}}>
           <button
