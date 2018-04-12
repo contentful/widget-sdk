5a0e3a88603b6859c66916147a97b2dc30bfbcc0
Jakub Elżbieciak <jakub@contentful.com>
2018-04-12 16:49:18 +0200

    Remove "getIntegrationToken" method of "client"

--- src/javascripts/kaltura_credentials.js
+++ src/javascripts/kaltura_credentials.js
@@ -1,52 +1,54 @@
 'use strict';
 
-angular.module('contentful').service('kalturaCredentials', ['require', function(require){
-  var $q     = require('$q');
+angular.module('contentful').service('kalturaCredentials', ['require', function (require) {
+  var $q = require('$q');
   var client = require('client');
   var moment = require('moment');
 
   var credentialsCache = {
-    credentials : undefined,
-    store: function(c) {
+    credentials: undefined,
+    store: function (c) {
       // Substract 10 seconds to ensure that we request the credentials before they actually expire.
       // Simple way to avoid clock discrepancies
-      this.credentials = _.extend(c, {expiry_time: moment().subtract(10, 's').add(c.expires_in, 's') });
+      this.credentials = _.extend(c, { expiry_time: moment().subtract(10, 's').add(c.expires_in, 's') });
     },
 
-    get: function() {
+    get: function () {
       return this.credentials;
     },
 
-    isEmpty: function() {
+    isEmpty: function () {
       return !this.credentials;
     },
 
-    areExpired: function() {
+    areExpired: function () {
       return moment(this.credentials.expiry_time).isBefore(moment());
     }
   };
 
   return {
-    get: function(organizationId) {
+    get: function (organizationId) {
       var deferred = $q.defer();
 
-      if (credentialsCache.isEmpty() || credentialsCache.areExpired())
+      if (credentialsCache.isEmpty() || credentialsCache.areExpired()) {
         this._fetch(organizationId, deferred);
-      else
+      } else {
         deferred.resolve(credentialsCache.get());
+      }
 
       return deferred.promise;
     },
 
-    _fetch: function(organizationId, deferred) {
-      return client.getIntegrationToken(['kaltura', organizationId].join('/'))
-        .then(function(credentials){
-          credentialsCache.store(credentials);
-          deferred.resolve(credentials);
-        })
-        .catch(function(){
-          deferred.reject();
-        });
+    _fetch: function (organizationId, deferred) {
+      return client.request({
+        method: 'GET',
+        path: '/' + ['integrations', 'kaltura', organizationId].join('/')
+      }).then(function (credentials) {
+        credentialsCache.store(credentials);
+        deferred.resolve(credentials);
+      }).catch(function () {
+        deferred.reject();
+      });
     }
   };
 }]);
