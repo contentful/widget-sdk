390214fa5d30934fb02f5272a753a1a3cffd6a9f
Joshua Smock <joshua@contentful.com>
2018-04-12 14:07:33 +0200

    Migrate SpacePlans component to React
    Add "Add Space" link always

--- src/javascripts/account/pricing/SubscriptionOverview.es6.js
+++ src/javascripts/account/pricing/SubscriptionOverview.es6.js
@@ -213,64 +213,54 @@ BasePlan.propTypes = {
 };
 
 function SpacePlans ({spacePlans, onCreateSpace, onDeleteSpace, isOrgOwner}) {
-  if (!spacePlans.length) {
-    return h('div', {
-      'data-test-id': 'subscription-page.no-spaces'
-    },
-      h('h2', {
-        className: 'section-title'
-      }, null, 'Spaces'),
-      h('p', null,
-        'Your organization doesn\'t have any spaces. ',
-        h('button', {
-          className: 'btn-link',
-          onClick: onCreateSpace
-        }, 'Add space')
-      )
-    );
-  } else {
-    const spacesTotal = calculatePlansCost({ plans: spacePlans });
+  const numSpaces = spacePlans.length;
+  const hasSpacePlans = numSpaces > 0;
+  const totalCost = calculatePlansCost({ plans: spacePlans });
 
-    return h('div', null,
-      h('h2', {
-        className: 'section-title'
-      }, null, 'Spaces'),
-      h('p', {
-        style: {marginBottom: '2em'},
-        'data-test-id': 'subscription-page.spaces-total'
-      }, null,
-        'The total for your ',
-        h('b', null, `${spacePlans.length} spaces`),
-        ' is ',
-        h(Price, {value: spacesTotal, style: {fontWeight: 'bold'}}),
-        ' per month.'
-        // TODO show available free spaces
-      ),
-      h('table', {
-        className: 'simple-table',
-        'data-test-id': 'subscription-page.spaces-list'
-      },
-        h('thead', null,
-          h('tr', null,
-            h('th', {style: {width: '25%'}}, 'Name'),
-            h('th', {style: {width: '30%'}}, 'Space type / price'),
-            h('th', {style: {width: '10%'}}, 'Created by'),
-            h('th', {style: {width: '10%'}}, 'Created on'),
-            h('th', {style: {width: '25%'}}, 'Actions')
-          )
-        ),
-        h('tbody', {className: 'clickable'}, spacePlans.map(
-          (plan) => h(SpacePlanRow, {
-            key: plan.sys.id || (plan.space && plan.space.sys.id),
-            plan,
-            onDeleteSpace,
-            isOrgOwner
-          })
-        ))
-      )
-    );
-  }
+  return <div>
+    <h2 className='section-title'>Spaces</h2>
+    <p style={{ marginBottom: '1.5em' }}>
+      { !hasSpacePlans &&
+        "Your organization doesn't have any spaces. "
+      }
+      { hasSpacePlans &&
+        <span>The total for your <b><Pluralized text="space" count={numSpaces} /></b> is <b><Price value={totalCost} /></b> per month.&#32;</span>
+      }
+      <a className='text-link' onClick={onCreateSpace}>Add Space</a>
+    </p>
+
+    { hasSpacePlans &&
+      <table className='simple-table'>
+        <thead>
+          <tr>
+            <th style={{width: '25%'}}>Name</th>
+            <th style={{width: '30%'}}>Space type / price</th>
+            <th style={{width: '10%'}}>Created by</th>
+            <th style={{width: '10%'}}>Created on</th>
+            <th style={{width: '25%'}}>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          { spacePlans.map(plan => {
+            return <SpacePlanRow
+              key={plan.sys.id || plan.space && plan.space.sys.id}
+              plan={plan}
+              onDeleteSpace={onDeleteSpace}
+              isOrgOwner={isOrgOwner}
+            />;
+          })}
+        </tbody>
+      </table>
+    }
+  </div>;
 }
+
+SpacePlans.propTypes = {
+  spacePlans: PropTypes.array.isRequired,
+  onCreateSpace: PropTypes.func.isRequired,
+  onDeleteSpace: PropTypes.func.isRequired,
+  isOrgOwner: PropTypes.bool.isRequired
+};
 
 function SpacePlanRow ({plan, onDeleteSpace, isOrgOwner}) {
   const space = plan.space;
