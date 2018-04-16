d67c18eff979909a21a13481383a559b23b6c95f
Joshua Smock <joshua@contentful.com>
2018-04-16 15:18:05 +0200

    Use custom propType for children in Workbench
    Use node propType for title

--- src/javascripts/ui/Components/Workbench/JSX.es6.js
+++ src/javascripts/ui/Components/Workbench/JSX.es6.js
@@ -5,20 +5,37 @@ import Icon from 'ui/Components/Icon';
 
 const Workbench = createReactClass({
   propTypes: {
-    title: PropTypes.oneOfType([PropTypes.string, PropTypes.element]).isRequired,
+    title: PropTypes.node.isRequired,
     icon: PropTypes.string,
     testId: PropTypes.string,
-    children: PropTypes.oneOfType([PropTypes.array, PropTypes.element])
+    children: function (props, propName) {
+      let children = props[propName];
+
+      if (!Array.isArray(children)) {
+        children = [children];
+      }
+
+      if (children.length > 2) {
+        return new Error(`Workbench must have 1 or 2 children, has ${children.length}`);
+      }
+
+      for (const child in children) {
+        if (child.type !== Workbench.Content && child.type !== Workbench.Sidebar) {
+          return new Error('Workbench should only have Content or Sidebar children.');
+        }
+      }
+
+      const contentChildren = children.filter(child => child.type === Workbench.Content);
+
+      if (contentChildren.length !== 1) {
+        return new Error(`Workbench should have 1 child with type Workbench.Content, but has ${contentChildren.length}`);
+      }
+    }
   },
   render () {
     const { title, icon, testId, children } = this.props;
-    const contentChildren = React.Children.toArray(children).filter(child => child.type === Workbench.Content);
     const content = React.Children.toArray(children).find(child => child.type === Workbench.Content);
     const sidebar = React.Children.toArray(children).find(child => child.type === Workbench.Sidebar);
-
-    if (contentChildren.length !== 1) {
-      throw new Error(`Workbench should have 1 child with type Workbench.Content, but has ${contentChildren.length}`);
-    }
 
     return <div className='workbench' data-test-id={testId}>
       <div className='workbench-header__wrapper'>
