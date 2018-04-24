56a031fecc40bfe074e17668b270db016e5ea84b
Jakub Elżbieciak <jakub@contentful.com>
2018-04-24 06:59:42 +0200

    Remove Ooyala for good

--- src/javascripts/classes/ooyala_error_messages.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('contentful').value('OoyalaErrorMessages', {
-  missingCredentials      : 'Missing or invalid Ooyala credentials. Please check your organizational settings or contact our support',
-  invalidAssetID          : 'Cannot load the video. Please check the content id',
-  playerFailedToLoad      : 'Cannot load the player. Please reload the page',
-  playerFailedToPlayVideo : 'Cannot play the video. Please reload the page',
-  unknownError            : 'Cannot play the video. Please reload the page'
-});
