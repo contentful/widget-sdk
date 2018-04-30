42acc1d9aea13b3a9ac2ccf4f7ffdfe771f96d54
Mike Mitchell <mike.mitchell@contentful.com>
2018-04-30 16:55:22 +0200

    Improve performance of sliding/peeking

--- src/javascripts/app/entity_editor/EntityPageController.es6.js
+++ src/javascripts/app/entity_editor/EntityPageController.es6.js
@@ -1,3 +1,4 @@
+import $ from 'jquery';
 import {
   getSlideInEntities,
   goToSlideInEntity
@@ -12,6 +13,36 @@ export default ($scope, _$state) => {
 
   $scope.close = (entity) => {
     goToSlideInEntity(entity);
+  };
+
+  $scope.peekIn = ($event) => {
+    const $elements = $($event.currentTarget).not($('.ng-enter')).nextAll();
+
+    $elements.map((index, element) => {
+      if (element.classList.contains('.ng-enter')) return;
+      const layerCalculation = element.getAttribute('data-layer-calculation');
+
+      if (index + 1 === $elements.length) {
+        element.style.transform = 'translate3d(10vw,0,0)';
+      } else {
+        element.style.transform = `translate3d(calc(10vw + ${layerCalculation}px),0,0)`;
+      }
+    });
+  };
+
+  $scope.peekOut = ($event) => {
+    const $elements = $($event.currentTarget).not($('.ng-enter')).nextAll();
+
+    $elements.map((index, element) => {
+      if (element.classList.contains('.ng-enter')) return;
+      const layerCalculation = element.getAttribute('data-layer-calculation');
+
+      if (index + 1 === $elements.length) {
+        element.style.transform = 'translate3d(0,0,0)';
+      } else {
+        element.style.transform = `translate3d(calc(${layerCalculation}px),0,0)`;
+      }
+    });
   };
 
   setEntities($scope);
