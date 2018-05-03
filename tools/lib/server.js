99deafb476f7c2aa222431a65737a320270e2ee7
Vsevolod Zaikov <seva.zaikov@gmail.com>
2018-05-03 17:27:23 +0200

    add webpack-dev-middleware to serve bundled files

--- tools/lib/server.js
+++ tools/lib/server.js
@@ -6,6 +6,9 @@ const anymatch = require('anymatch');
 const express = require('express');
 const runSequence = require('run-sequence');
 const {readJSON} = require('./utils');
+const middleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const createWebpackConfig = require('../webpack.config');
 
 const IndexPage = require('./index-page');
 
@@ -81,6 +84,23 @@ function createServer (configName, getBuild) {
   const docIndex = sendIndex(P.join(publicDir, 'docs'));
 
   const app = express();
+  const config = createWebpackConfig({ dev: true });
+  const compiler = webpack(config);
+
+  app.use(middleware(compiler, {
+    publicPath: '/app/',
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+    },
+    stats: {
+      colors: true,
+      modules: false,
+      providedExports: false,
+      usedExports: false
+    }
+  }));
+
   app.use(express.static(publicDir));
   app.use('/docs/', docIndex);
   app.get('*', function (req, res, next) {
