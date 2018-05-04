b1adadcfc948052e1ba6eaabd418ba70db6d2dd6
Joshua Smock <joshua@contentful.com>
2018-05-04 17:06:41 +0200

    MOI-21 #comment Update copy

--- src/javascripts/components/shared/space-wizard/ConfirmScreen.es6.js
+++ src/javascripts/components/shared/space-wizard/ConfirmScreen.es6.js
@@ -94,7 +94,7 @@ const ConfirmScreen = createReactClass({
                     Make sure everything is in order before confirming the change.
                   </p>
                   <p className="create-space-wizard__info">
-                    You&apos;re about to change the space <em>{space.name}</em> from a {currentSpaceRatePlan.name} to {newSpaceRatePlan.name} space type. This will increase your organization&apos;s monthly subscription for your spaces by <strong><Price unit='month' value={newSpaceRatePlan.price} /></strong> to a new total of <strong><Price unit='month' value={(totalPrice + newSpaceRatePlan.price - currentSpaceRatePlan.price)} /></strong>.
+                    You&apos;re about to change the space <em>{space.name}</em> from a {currentSpaceRatePlan.name} to a {newSpaceRatePlan.name} space type. This will increase the total price of the spaces in your organization by <strong><Price value={newSpaceRatePlan.price} /></strong> to <strong><Price unit='month' value={(totalPrice + newSpaceRatePlan.price - currentSpaceRatePlan.price)} /></strong>.
                   </p>
                 </Fragment>
               }
