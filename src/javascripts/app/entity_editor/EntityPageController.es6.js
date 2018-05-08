c30cde44a80f278297dbdb95aecd1719795d5346
Mike Mitchell <mike.mitchell@contentful.com>
2018-05-08 11:23:52 +0200

    Reimplement peeking using JavaScript

--- src/javascripts/app/entity_editor/EntityPageController.es6.js
+++ src/javascripts/app/entity_editor/EntityPageController.es6.js
@@ -19,8 +19,41 @@ export default ($scope, _$state) => {
     $scope.isSlideinEntryEditorEnabled = isEnabled;
   });
 
+  $scope.topPeekingLayerIndex = -1;
+
   $scope.close = entity => {
     goToSlideInEntity(entity, $scope.isSlideinEntryEditorEnabled);
+    $scope.testingArray = [];
+  };
+
+  $scope.initPeeking = (index) => {
+    const length = $scope.entities.length;
+    const previous = index - 1;
+
+    if (index === length - 1) {
+      $scope.topPeekingLayerIndex = previous;
+    }
+  };
+
+  $scope.peekIn = (index) => {
+    const length = $scope.entities.length;
+    const arr = [].slice.apply(document.querySelectorAll('.workbench-layer'));
+    const next = index + 1;
+
+    if ($scope.topPeekingLayerIndex >= index) {
+      arr.slice(next, length).map((item) => {
+        item.classList.add('workbench-layer--peeked');
+      });
+    }
+  };
+
+  $scope.peekOut = (index) => {
+    const length = $scope.entities.length;
+    const arr = [].slice.apply(document.querySelectorAll('.workbench-layer'));
+
+    arr.slice(index, length).map((item) => {
+      item.classList.remove('workbench-layer--peeked');
+    });
   };
 
   setEntities($scope);
