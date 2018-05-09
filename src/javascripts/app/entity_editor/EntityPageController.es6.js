2c2ff21bef41b985b349b686df602213aca251f4
Mike Mitchell <mike.mitchell@contentful.com>
2018-05-09 13:32:33 +0200

    Fix peeking bugs

--- src/javascripts/app/entity_editor/EntityPageController.es6.js
+++ src/javascripts/app/entity_editor/EntityPageController.es6.js
@@ -8,7 +8,7 @@ import { onFeatureFlag } from 'utils/LaunchDarkly';
 const SLIDEIN_ENTRY_EDITOR_FEATURE_FLAG =
   'feature-at-05-2018-sliding-entry-editor-multi-level';
 const PEEK_IN_DELAY = 1000;
-const PEEK_OUT_DELAY = 200;
+const PEEK_OUT_DELAY = 500;
 
 function setEntities ($scope) {
   $scope.entities = getSlideInEntities();
@@ -27,7 +27,6 @@ export default ($scope, _$state) => {
 
   $scope.close = entity => {
     goToSlideInEntity(entity, $scope.isSlideinEntryEditorEnabled);
-    $scope.testingArray = [];
   };
 
   $scope.initPeeking = (index) => {
@@ -35,35 +34,38 @@ export default ($scope, _$state) => {
     const previous = index - 1;
 
     if (index === length - 1) {
+      const entityLayers = [].slice.apply(document.querySelectorAll('.workbench-layer'));
       $scope.topPeekingLayerIndex = previous;
+
+      peekOutTimeoutReference = window.setTimeout(() => {
+        entityLayers.map((item) => {
+          item.classList.remove('workbench-layer--peeked');
+        });
+      }, PEEK_OUT_DELAY);
     }
   };
 
   $scope.peekIn = (index) => {
     const length = $scope.entities.length;
-    const arr = [].slice.apply(document.querySelectorAll('.workbench-layer'));
+    const entityLayers = [].slice.apply(document.querySelectorAll('.workbench-layer'));
     const next = index + 1;
 
     window.clearTimeout(peekOutTimeoutReference);
     peekInTimeoutReference = window.setTimeout(() => {
+      entityLayers.map((item) => {
+        item.classList.remove('workbench-layer--peeked');
+      });
+
       if ($scope.topPeekingLayerIndex >= index) {
-        arr.slice(next, length).map((item) => {
+        entityLayers.slice(next, length).map((item) => {
           item.classList.add('workbench-layer--peeked');
         });
       }
     }, PEEK_IN_DELAY);
   };
 
-  $scope.peekOut = (index) => {
-    const length = $scope.entities.length;
-    const arr = [].slice.apply(document.querySelectorAll('.workbench-layer'));
-
+  $scope.peekOut = () => {
     window.clearTimeout(peekInTimeoutReference);
-    peekOutTimeoutReference = window.setTimeout(() => {
-      arr.slice(index, length).map((item) => {
-        item.classList.remove('workbench-layer--peeked');
-      });
-    }, PEEK_OUT_DELAY);
   };
 
   setEntities($scope);
