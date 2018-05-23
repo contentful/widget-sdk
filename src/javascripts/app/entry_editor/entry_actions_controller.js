contentful/widget-sdk

commit eee52bf63064e3b6860720875d6c1b6f7ddff5f2

Open references in slide in editor

src/javascripts/app/entry_editor/entry_actions_controller.js
@@ -95,7 +95,9 @@ angular.module('contentful')
     // TODO: Create a service that works like $state.go, but cancels
     // if the state has changed in the meantime
     // X.detail -> X.detail with altered entryId param
-    $state.go('^.detail', {
+    // We need the full state name to make sure it's accessible
+    // from nested routes (slidein entry editor).
+    $state.go('spaces.detail.entries.detail', {
       entryId: entry.getId(),
       addToContext: false
     });

test/unit/app/entry_editor/entry_actions_controller_spec.js
@@ -97,7 +97,7 @@ describe('Entry Actions Controller', function () {
           it('opens the editor', function () {
             sinon.assert.calledWith(
               this.$state.go,
-              '^.detail',
+              'spaces.detail.entries.detail',
               {
                 entryId: 'NEW ID',
                 addToContext: false

src/javascripts/app/entity_editor/AssetController.es6.js
@@ -1,4 +1,5 @@
 import $controller from '$controller';
+import closeState from 'navigation/closeState';
 
 import * as K from 'utils/kefir';
 import {truncate, fileNameToTitle} from 'stringUtils';
@@ -57,6 +58,10 @@ export default function create ($scope, editorData) {
   );
 
   editorContext.focus = Focus.create();
+
+  editorContext.closeSlideinEditor = function () {
+    closeState();
+  };
 
   $scope.state = $controller('entityEditor/StateController', {
     $scope: $scope,

src/javascripts/app/entity_editor/EntryController.es6.js
@@ -1,4 +1,5 @@
 import $controller from '$controller';
+import closeState from 'navigation/closeState';
 
 import {deepFreeze} from 'utils/Freeze';
 import * as K from 'utils/kefir';
@@ -120,6 +121,16 @@ export default function create ($scope, editorData) {
   };
 
   editorContext.createReferenceContext = createReferenceContext;
+
+  // This will only be available if feature-at-03-2018-sliding-entry-editor
+  // is not enabled.
+  editorContext.toggleSlideinEditor = function () {
+    $scope.inlineEditor = !$scope.inlineEditor;
+  };
+
+  editorContext.closeSlideinEditor = function () {
+    closeState();
+  };
 
   function createReferenceContext (field, locale, index, cb) {
     // The links$ property should end when the editor is closed

src/javascripts/app/entity_editor/cf_test_entry_editor_directive.js
@@ -1,0 +1,11 @@
+angular.module('contentful')
+.directive('cfTestEntryEditor', ['require', function (require) {
+  var createEditorController = require('app/entity_editor/EntryController').default;
+
+  return {
+    restrict: 'E',
+    scope: true,
+    template: JST.entry_editor(),
+    controller: ['$scope', createEditorController]
+  };
+}]);

src/javascripts/app/entry_editor/cf_widget_api_directive.js
@@ -73,6 +73,10 @@ angular.module('contentful')
     };
   }
 
+  if ($scope.editorContext.toggleSlideinEditor) {
+    this._internal.toggleSlideinEditor = $scope.editorContext.toggleSlideinEditor;
+  }
+
   this.entry = {
     // TODO only used by slug and reference editor; we should
     // remove it and only offer a property interface

src/javascripts/app/entry_editor/entry_editor.jade
@@ -1,6 +1,14 @@
 include ../entity_editor/entity_editor.mixins.jade
 
+
 div.entry-editor.workbench
+  div.workbench-stack.animate(
+    ng-if="inlineEditor")
+    .workbench-stack__padding
+    .workbench-stack__content
+
+  div(ui-view="")
+
   div.workbench-stack.animate(
     ng-if="referenceContext")
     .workbench-stack__padding(

src/javascripts/app/snapshots/SnapshotSidebarListTemplates.es6.js
@@ -83,7 +83,7 @@ function compareBtn () {
     role: 'button',
     ariaDisabled: '{{!selectedId}}',
     dataTestId: 'compare-versions',
-    uiSref: '.compare.withCurrent({snapshotId: selectedId})',
+    uiSref: 'spaces.detail.entries.detail.compare.withCurrent({entryId: entityInfo.id, snapshotId: selectedId})',
     ngDisabled: '!selectedId'
   }, ['Compare with current version']);
 }

commit d6cc2dac6a93eca61b2cb16ed7d6c26344e91661

Fix entry "Duplicate" action for environments
This was broken in eee52bf as the BETA slide-in implementation was
relying on an absolute path and environments were not considered.

src/javascripts/app/entry_editor/entry_actions_controller.js
@@ -92,12 +92,7 @@ angular.module('contentful')
   }
 
   function goToEntryDetail (entry) {
-    // TODO: Create a service that works like $state.go, but cancels
-    // if the state has changed in the meantime
-    // X.detail -> X.detail with altered entryId param
-    // We need the full state name to make sure it's accessible
-    // from nested routes (slidein entry editor).
-    $state.go('spaces.detail.entries.detail', {
+    $state.go('^.detail', {
       entryId: entry.getId(),
       slideIn: '',
       addToContext: false

test/unit/app/entry_editor/entry_actions_controller_spec.js
@@ -97,7 +97,7 @@ describe('Entry Actions Controller', function () {
           it('opens the editor', function () {
             sinon.assert.calledWith(
               this.$state.go,
-              'spaces.detail.entries.detail',
+              '^.detail',
               {
                 entryId: 'NEW ID',
                 slideIn: '',