b9eb7faa745f025b4c4b0055842fe50536ecffc3
Mudit Ameta <zeusdeux@gmail.com>
2018-05-25 11:20:23 +0200

    Fix eslint errors from WIP code

--- src/javascripts/components/shared/stack-onboarding/DevNextSteps.js
+++ src/javascripts/components/shared/stack-onboarding/DevNextSteps.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import createReactClass from 'create-react-class';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 const moduleName = 'ms-dev-next-steps';
 
-console.log(moduleName)
 angular.module('contentful')
   .factory(moduleName, ['require', _require => {
     return createReactClass({
