contentful/widget-sdk

commit 0096a3ea406697cb196759dea8324de67e6f5a2f

AUTH-137. Cleanup search code (#2991)

src/javascripts/search/entity_selector_controller.js
@@ -38,22 +38,16 @@ angular.module('contentful')
   var EntityHelpers = require('EntityHelpers');
   var K = require('utils/kefir');
   var Kefir = require('kefir');
-  var LD = require('utils/LaunchDarkly');
+  var _ = require('lodash');
   var createSearchInput = require('app/ContentList/Search').default;
   var getAccessibleCTs = require('data/ContentTypeRepo/accessibleCTs').default;
 
-  var NEW_SEARCH_FLAG_NAME = 'feature-at-01-2018-entity-selector-new-search';
   var MIN_SEARCH_TRIGGERING_LEN = 1;
   var MODES = {AVAILABLE: 1, SELECTED: 2};
 
   var config = $scope.config;
 
-  LD.onFeatureFlag($scope, NEW_SEARCH_FLAG_NAME, function (isEnabled) {
-    $scope.isNewSearchEnabled = isEnabled;
-    if (isEnabled) {
-      initializeSearchUI();
-    }
-  });
+  initializeSearchUI();
 
   var load = createQueue(fetch);
 
@@ -144,11 +138,7 @@ angular.module('contentful')
       order: getOrder(),
       paginator: $scope.paginator
     };
-    if ($scope.isNewSearchEnabled) {
-      _.assign(params, getSearch());
-    } else {
-      params.searchTerm = $scope.view.searchTerm;
-    }
+    _.assign(params, getSearch());
 
     if (config.entityType === 'Entry' && $scope.singleContentType) {
       params.contentTypeId = $scope.singleContentType.getId();
@@ -160,10 +150,7 @@ angular.module('contentful')
   function getSearch () {
     var view = $scope.view || {};
     return {
-      // if we have old interface and searchText is stored inside
-      // `view.searchTerm`, we won't perform any search at all
-      // unless we provide this value as `searchText`
-      searchText: view.searchText || view.searchTerm || '',
+      searchText: view.searchText || '',
       searchFilters: view.searchFilters || [],
       contentTypeId: view.contentTypeId
     };
@@ -305,6 +292,6 @@ angular.module('contentful')
   }
 
   function showCustomEmptyMessage () {
-    return $scope.labels.noEntitiesCustomHtml && !$scope.isLoading && $scope.items.length < 1 && !$scope.view.searchTerm;
+    return $scope.labels.noEntitiesCustomHtml && !$scope.isLoading && $scope.items.length < 1;
   }
 }]);

src/javascripts/search/entity_selector_dialog.jade
@@ -36,15 +36,7 @@ mixin single-list-item
           a(href ng-click="view.mode = AVAILABLE" ng-if="view.mode === SELECTED")
             | Hide {{ labels.selected }} ({{ selected.length }})
 
-      .entity-selector__search(
-        ng-if="supportsAdvancedSearch && !isNewSearchEnabled"
-        cf-tokenized-search="view.searchTerm"
-        search-content-type="singleContentType"
-        placeholder="{{ getSearchPlaceholder() }}"
-        cf-on-blur-component="leftSearch($event)"
-        cf-on-blur-component-when="showAutocompletions")
-
-      .entity-selector__search(ng-if="supportsAdvancedSearch && isNewSearchEnabled")
+      .entity-selector__search(ng-if="supportsAdvancedSearch")
         cf-component-bridge(component="search")
 
       .entity-selector__search(ng-if="!supportsAdvancedSearch")
@@ -69,8 +61,7 @@ mixin single-list-item
           ng-class="{'x--selected': selectedIds[entity.sys.id],'entity-selector__last-item': $last && paginator.atLast()}")
           +single-list-item
 
-        .loading(ng-if="!isNewSearchEnabled && isLoading")
-        .loading(ng-if="isNewSearchEnabled && isLoadingMore")
+        .loading(ng-if="isLoadingMore")
 
       .entity-selector__item-list(
         ng-if="view.mode === SELECTED"

test/unit/search/entity_selector_controller_spec.js
@@ -8,13 +8,26 @@ describe('EntitySelectorController', function () {
     const $controller = this.$inject('$controller');
     const $timeout = this.$inject('$timeout');
 
+    const spaceContext = this.$inject('mocks/spaceContext').init();
+    const scope = $rootScope.$new();
+    scope.spaceContext = spaceContext;
+
+    const ct = {
+      getId: _.constant(1),
+      data: {fields: [{id: 'fieldId'}], sys: {id: 1}}
+    };
+    spaceContext.publishedCTs.fetch.resolves(ct);
+    spaceContext.publishedCTs.getAllBare.returns([]);
+
+    spaceContext.space.getEntries.defers();
+
     this.entitySelector = this.$inject('entitySelector');
 
     this.fetch = sinon.stub().resolves({items: []});
 
     this.createController = function (config = {}, labels = {}, singleContentType) {
       config = _.extend({ entityType: 'Entry', fetch: this.fetch }, config);
-      this.scope = _.extend($rootScope.$new(), {
+      this.scope = _.extend(scope, {
         config,
         labels,
         singleContentType

src/javascripts/components/shared/search_field/cf_tokenized_search_controller.js
@@ -1,257 +1,0 @@
-'use strict';
-
-/**
- * Controller for the tokenized search
- *
- * The interaction with the tokenized search happens mainly in three ways:
- * - keyboard input, focusing, blurring is triggered from the template
- * - when the input changes, we parse the searchterm ($scope.inner.term)
- * - depending on the parse tree and the current position of the cursor we display autocompletions.
- * - certain keypresses are redirected to the autocompletions instead of editing the text input
- */
-angular.module('contentful')
-.controller('cfTokenizedSearchController', ['$scope', 'require', '$attrs', function ($scope, require, $attrs) {
-  var searchQueryHelper = require('searchQueryHelper');
-  var keycodes = require('utils/keycodes').default;
-  var $parse = require('$parse');
-  var spaceContext = require('spaceContext');
-
-  $scope.inner = { term: null };
-  $scope.position = 0;
-  $scope.showAutocompletions = false; // Indicate if completions should be shown
-
-  $scope.getContentType = function () {
-    return $scope.$eval($attrs.searchContentType);
-  };
-
-  // The parse token that the cursor is currently on
-  $scope.getCurrentToken = function () {
-    return searchQueryHelper.currentSubToken($scope.getContentType(), $scope.inner.term, $scope.position);
-  };
-
-  // Update the available autocompletions
-  //
-  // scope.autocompletion contains the current autocompletion object,
-  // as generated by the searchQueryAutocompletions service
-  $scope.updateAutocompletions = function () {
-    var contentType = $scope.getContentType();
-    var space = spaceContext.space;
-    var term = $scope.inner.term;
-    var position = $scope.position;
-
-    searchQueryHelper.offerCompletion(space, contentType, term, position)
-    .then(function (completion) {
-      $scope.autocompletion = completion;
-      $scope.$broadcast('autocompletionsUpdated', completion);
-    });
-  };
-
-  // Get out of autocompleting
-  $scope.clearAutocompletions = function () {
-    $scope.restoreString();
-    $scope.showAutocompletions = false;
-  };
-
-  $scope.$watch('getContentType()', _.bind($scope.updateAutocompletions, $scope));
-
-  $scope.inputChanged = function () {
-    // TODO: This is necessary because $scope.position will only be updated in the next digtest cycle.
-    // When deleting from the end, this can cause the position to be $scope.inner.term.length + 1
-    // which gives a current token of undefined
-    $scope.position = $scope.getPosition();
-    $scope.updateAutocompletions();
-  };
-
-  $scope.keyReleased = function (event) {
-    if ($scope.position !== $scope.getPosition()) {
-      $scope.position = $scope.getPosition();
-      $scope.updateAutocompletions();
-    }
-
-    if (event.keyCode !== keycodes.DOWN &&
-        event.keyCode !== keycodes.UP &&
-        event.keyCode !== keycodes.LEFT &&
-        event.keyCode !== keycodes.RIGHT &&
-        event.keyCode !== keycodes.ESC &&
-        !$scope.showAutocompletions) {
-      submitSearch();
-    }
-  };
-
-  // We communicate keypresses as events downwards so that different autocompletion widgets can handle them as they see fit
-  $scope.keyPressed = function (event) {
-    if (event.keyCode === keycodes.DOWN) {
-      $scope.showAutocompletions = true;
-      if ($scope.autocompletion && $scope.showAutocompletions) {
-        $scope.$broadcast('selectNextAutocompletion');
-        event.preventDefault();
-      }
-    } else if (event.keyCode === keycodes.UP) {
-      $scope.showAutocompletions = true;
-      if ($scope.autocompletion && $scope.showAutocompletions) {
-        $scope.$broadcast('selectPreviousAutocompletion');
-        event.preventDefault();
-      }
-    } else if (event.keyCode === keycodes.ESC) {
-      if ($scope.autocompletion && $scope.showAutocompletions) {
-        $scope.$broadcast('cancelAutocompletion');
-        $scope.clearAutocompletions();
-        event.preventDefault();
-      }
-    } else if (event.keyCode === keycodes.ENTER) {
-      if ($scope.autocompletion && $scope.showAutocompletions) {
-        var e = $scope.$broadcast('submitAutocompletion');
-        if (e.defaultPrevented) {
-          submitSearch();
-          $scope.showAutocompletions = false;
-          forceSearch();
-        } else {
-          $scope.confirmAutocompletion();
-        }
-      } else {
-        submitSearch();
-        forceSearch();
-      }
-      event.preventDefault();
-    }
-  };
-
-  $scope.updateFromButton = function () {
-    submitSearch();
-    forceSearch();
-  };
-
-  $scope.currentTokenContent = function () {
-    var token = $scope.getCurrentToken();
-    return token && token.content;
-  };
-
-  function forceSearch () {
-    $scope.$emit('forceSearch');
-  }
-
-  // FORMER LINK FUNCTION STUFF;
-
-  var getSearchTerm = $parse($attrs.cfTokenizedSearch);
-  var setSearchTerm = getSearchTerm.assign;
-
-  $scope.$watch(function (scope) {
-    return getSearchTerm(scope.$parent);
-  }, function (term) {
-    $scope.inner.term = term;
-  });
-
-  $attrs.$observe('placeholder', function (placeholder) {
-    $scope.placeholder = placeholder;
-  });
-
-  function submitSearch () {
-    setSearchTerm($scope.$parent, $scope.inner.term);
-  }
-
-  $scope.hasFocus = false;
-
-  $scope.inputFocused = function () {
-    $scope.hasFocus = true;
-    $scope.updateAutocompletions();
-  };
-
-  $scope.inputBlurred = function () {
-    $scope.hasFocus = false;
-  };
-
-  // Handle if the focus moves out of the tokenizedSearch subtree
-  $scope.leftSearch = function (event) {
-    // If we clicked on a button in the datepicker the leftSearch will trigger although it
-    // shouldn't because the DOM node has been removed and the parent lookup won't work correctly
-    // http://stackoverflow.com/questions/22406505
-    if ($(event.target).parents('[data-event=click]').length < 1) {
-      $scope.clearAutocompletions();
-    }
-  };
-
-  // Replace the currently selected (touched by the cursor counts as selection)
-  // token with the insertString. Backs up the current search string before
-  // and selects the replacement afterwards
-  $scope.fillAutocompletion = function (insertString) {
-    var token = $scope.getCurrentToken();
-    var originalString = $scope.inner.term || '';
-    if (!token) {
-      token = {
-        offset: $scope.position || 0,
-        length: 0
-      };
-    }
-    if (token.type !== 'Value') insertString = insertString + ' ';
-    $scope.backupString(originalString, token.offset, token.length);
-    $scope.inner.term = spliceSlice(originalString, token.offset, token.length, insertString);
-    $scope.selectRange(token.offset, insertString.length);
-  };
-
-  // Confirms the currently selected autocompletion.
-  //
-  // The currently selected autocompletion is the selected substring
-  // (corresponding to the current token) in the input field. This
-  // Token is extended, depending on its type (with an operator or a space)
-  // the backupString is cleared and the cursor is placed at the end of the token
-  $scope.confirmAutocompletion = function () {
-    var token = $scope.getCurrentToken();
-    if (!token) {
-      return;
-    }
-    var originalString = $scope.inner.term || '';
-    var appendString = token.type === 'Value' ? ' ' : token.type === 'Operator' ? '' : operator(token.content) + ' ';
-    $scope.inner.term = spliceSlice(originalString, token.end, 0, appendString);
-    $scope.clearBackupString();
-    if (token.type === 'Value') {
-      submitSearch();
-      $scope.showAutocompletions = false;
-    }
-    $scope.selectRange(token.end + appendString.length, 0)
-    .then(function () {
-      if ($scope) {
-        $scope.position = token.end + appendString.length;
-        $scope.updateAutocompletions();
-      }
-    });
-
-    function operator (key) {
-      return searchQueryHelper.operatorForKey(key, $scope.getContentType());
-    }
-  };
-
-  $scope._backupString = null;
-  $scope.backupString = function (str, offset, length) {
-    var b = str.slice(offset, offset + length);
-    if (!$scope._backupString && b.length > 0) {
-      $scope._backupString = b;
-    }
-  };
-
-  $scope.restoreString = function () {
-    var token = $scope.getCurrentToken();
-    if (token && $scope._backupString) {
-      $scope.inner.term = spliceSlice($scope.inner.term, token.offset, token.length, $scope._backupString);
-      $scope._backupString = null;
-    }
-  };
-
-  $scope.clearBackupString = function () {
-    $scope._backupString = null;
-  };
-
-  // The splice function from arrays implemented for strings using
-  // String.prototype.slice
-  //
-  // str: the original string
-  // index: position where to insert
-  // count: how many characters to remove at index
-  // add: which string to insert at the index
-  function spliceSlice (str, index, count, add) {
-    return str.slice(0, index) + add + str.slice(index + count);
-  }
-
-  $scope.$on('$destroy', function () {
-    $scope = null; // MEMLEAK FIX
-  });
-}]);

src/javascripts/components/shared/search_field/cf_tokenized_search_directive.js
@@ -1,161 +1,0 @@
-'use strict';
-
-angular.module('contentful').directive('cfTokenizedSearch', ['require', function (require) {
-  var $timeout = require('$timeout');
-  var h = require('utils/hyperscript').h;
-  var Colors = require('Styles/Colors').byName;
-  var renderString = require('ui/Framework').renderString;
-  var infoIcon = renderString(require('svg/info').default);
-  var filterIcon = renderString(require('svg/filter').default);
-
-  var metallicGray = '#969FA6';
-  var color = 'hasFocus ? "' + Colors.blueMid + '" : "' + metallicGray + '"';
-  var border = '"1px solid " + (' + color + ')';
-
-  var iconStyle = {
-    display: 'flex',
-    alignItems: 'center',
-    padding: '0 15px',
-    fill: 'currentColor'
-  };
-
-  return {
-    template: h('div', {
-      style: {
-        position: 'relative',
-        display: 'flex',
-        backgroundColor: 'white',
-        borderRadius: '2px'
-      },
-      ngStyle: '{border: ' + border + '}'
-    }, [
-      h('input.cfnext-form__input', {
-        type: 'text',
-        style: {
-          flexGrow: '1',
-          border: '0',
-          height: '40px',
-          padding: '0 10px'
-        },
-        ngModel: 'inner.term',
-        ngTrim: 'false',
-        placeholder: '{{placeholder}}',
-        ngChange: 'inputChanged()',
-        ngKeydown: 'keyPressed($event)',
-        ngKeyup: 'keyReleased($event)',
-        ngFocus: 'inputFocused($event)',
-        ngBlur: 'inputBlurred($event)',
-        dataTestId: 'search-input'
-      }),
-      h('cf-inline-loader', {
-        style: {height: '40px'},
-        isShown: 'context.isSearching'
-      }),
-      h('button', {
-        ngIf: 'autocompletion.type',
-        style: iconStyle,
-        ngClick: 'toggleFilters()',
-        ngStyle: '{color: ' + color + '}',
-        tabindex: '1',
-        dataTestId: 'search-filter-button'
-      }, [
-        h('div', {style: {marginTop: '-3px'}}, [filterIcon]),
-        h('div', {style: {marginLeft: '7px'}}, ['Filter'])
-      ]),
-      h('div', {
-        ngIf: 'showAutocompletions && autocompletion.type',
-        style: {
-          zIndex: '300',
-          position: 'absolute',
-          top: '40px',
-          left: '-1px',
-          right: '-1px',
-          fontSize: '14px',
-          backgroundColor: 'white',
-          border: '1px solid ' + Colors.blueMid
-        }
-      }, [
-        h('div', {
-          style: {
-            maxHeight: '50vh',
-            overflowX: 'hidden',
-            overflowY: 'auto'
-          },
-          cfAutocompleteList: true,
-          ngIf: 'autocompletion.type === "List"'
-        }),
-        h('div', {
-          cfAutocompleteDate: true,
-          ngIf: 'autocompletion.type === "Date"'
-        }),
-        h('div', {
-          style: {
-            display: 'flex',
-            alignItems: 'center',
-            background: Colors.iceMid,
-            borderTop: '1px solid ' + Colors.elementLight,
-            height: '56px',
-            padding: '15px 20px'
-          }
-        }, [
-          infoIcon,
-          h('p', {
-            style: {
-              color: Colors.textLight,
-              margin: '0',
-              marginLeft: '10px'
-            }
-          }, [
-            'Get more out of search. Here\'s&#32;',
-            h('a', {
-              style: {textDecoration: 'underline'},
-              href: 'https://www.contentful.com/r/knowledgebase/content-search/',
-              target: '_blank'
-            }, ['how search works']),
-            '.'
-          ])
-        ])
-      ])
-    ]),
-    restrict: 'A',
-    scope: true,
-    controller: 'cfTokenizedSearchController',
-    controllerAs: 'tokenizedSearchController',
-    link: function (scope, element) {
-      var input = element.find('input');
-
-      $timeout(focus);
-
-      scope.toggleFilters = function () {
-        scope.showAutocompletions = !scope.showAutocompletions;
-        focus();
-      };
-
-      scope.searchButtonClicked = function () {
-        scope.showAutocompletions = false;
-        scope.updateFromButton();
-        focus();
-      };
-
-      // Make position query available on the scope so it can be called from the controller
-      scope.getPosition = function () {
-        return scope.hasFocus && input.textrange('get').start;
-      };
-
-      // Make range selection available on the scope so it can be called from the controller
-      scope.selectRange = function (offset, length) {
-        return $timeout(function () {
-          input.textrange('set', offset, length);
-        }, null, false);
-      };
-
-      scope.$on('$destroy', function () {
-        scope = null; // MEMLEAK FIX
-      });
-
-      function focus () {
-        input.first().focus();
-      }
-    }
-  };
-}]);

src/javascripts/directives/cf_on_blur_component.js
@@ -1,39 +1,0 @@
-'use strict';
-
-// A better replacement for clickoutside that tracks both blur and focus
-// so, whether you leave a component by mouse or keyboard doesn't matter
-// - NOTE: Oops this..blur != that.focus. You can blur somthing without focusin something else.
-//   For example elements with tabindex do not cause focus
-// - NOTE: For now this doesn't serve a better purpose than cfClickOutside, with the exception that
-//   it doesn't bind element.click (something nobody knows why cfClickOutside is doing it)
-angular.module('contentful').directive('cfOnBlurComponent', ['require', function (require) {
-  var $document = require('$document');
-  var $parse = require('$parse');
-
-  return {
-    restrict: 'A',
-    link: function (scope, elem, attr) {
-      var fn = $parse(attr.cfOnBlurComponent);
-
-      $document.on('click focus', clickHandler);
-      elem.bind('remove', function () {
-        $document.off('click focus', clickHandler);
-      });
-
-      function clickHandler (event) {
-        if (!scope.$eval(attr.cfOnBlurComponentWhen)) return;
-        event.stopPropagation();
-        var targetParents = $(event.target).parents();
-        var inside = targetParents.index(elem) !== -1;
-        var on = event.target === elem[0];
-        var outside = !inside && !on;
-
-        if (outside) {
-          scope.$apply(function () {
-            fn(scope, {$event: event});
-          });
-        }
-      }
-    }
-  };
-}]);

commit 5e67e711800da2216abedc2e2aca77ba41ef844c

Fix org users selector and finish search cleanup
AUTH-137: 0096a3e broke the user invitation dialog that is used to select
organization users to be invited to a space ("Settings" > "Users"), this
is fixed with this change.
This also improves the legacy search clean-up started in the above commit.

src/javascripts/search/entity_selector_controller.js
@@ -75,7 +75,7 @@ angular.module('contentful')
     getContentType: getContentType
   });
 
-  $scope.$watch('view.searchTerm', handleTermChange);
+  $scope.$watch('view.searchText', handleTermChange);
   $scope.$on('forceSearch', resetAndLoad);
 
   resetAndLoad();
@@ -292,6 +292,11 @@ angular.module('contentful')
   }
 
   function showCustomEmptyMessage () {
-    return $scope.labels.noEntitiesCustomHtml && !$scope.isLoading && $scope.items.length < 1;
+    var currentSearch = getSearch();
+    return $scope.labels.noEntitiesCustomHtml &&
+      !$scope.isLoading &&
+      $scope.items.length < 1 &&
+      !currentSearch.searchText &&
+      !currentSearch.searchFilters.length;
   }
 }]);

src/javascripts/search/entity_selector_dialog.jade
@@ -41,7 +41,7 @@ mixin single-list-item
 
       .entity-selector__search(ng-if="!supportsAdvancedSearch")
         input.cfnext-form__input--full-size(
-          ng-model="view.searchTerm"
+          ng-model="view.searchText"
           placeholder="{{ getSearchPlaceholder() }}")
 
       .entity-selector__item-list.endless-container(
@@ -53,8 +53,8 @@ mixin single-list-item
         num-elements="paginator.getPage()")
         .entity-selector__item-list-placeholder(ng-if="!isLoading && items.length < 1")
           | {{ labels.empty }} found
-          span(ng-if="view.searchTerm.length > 0")
-            |  for <strong>{{ view.searchTerm }}</strong>
+          span(ng-if="!supportsAdvancedSearch && view.searchText.length > 0")
+            |  for <strong>{{ view.searchText }}</strong>
 
         .entity-selector__item(
           ng-repeat="entity in items track by entity.sys.id"

src/javascripts/search/list_query.js
@@ -11,7 +11,6 @@ angular.module('contentful').factory('ListQuery', ['require', function (require)
   var systemFields = require('systemFields');
   var spaceContext = require('spaceContext');
   var buildQueryFromUISearch = require('app/ContentList/Search/QueryBuilder').buildQuery;
-  var buildQueryFromLegacySearchTerm = require('search/queryBuilder').buildQuery;
   var assetContentType = require('assetContentType');
 
   var DEFAULT_ORDER = systemFields.getDefaultOrder();
@@ -24,7 +23,7 @@ angular.module('contentful').factory('ListQuery', ['require', function (require)
      * Prepares an API query for the entry list.
      *
      * @param {string} opts.contentTypeId
-     * @param {string} opts.searchTerm
+     * @param {string} opts.searchText
      * @param {object} opts.order
      * @param {Paginator} opts.paginator
      * @returns {object}
@@ -45,7 +44,7 @@ angular.module('contentful').factory('ListQuery', ['require', function (require)
      * @description
      * Prepares an API query for the asset list.
      *
-     * @param {string} opts.searchTerm
+     * @param {string} opts.searchText
      * @param {object} opts.order
      * @param {Paginator} opts.paginator
      * @returns {object}
@@ -78,19 +77,11 @@ angular.module('contentful').factory('ListQuery', ['require', function (require)
       skip: opts.paginator.getSkipParam(),
       'sys.archivedAt[exists]': 'false' // By default, don't get archived entries.
     };
-
-    // TODO: Remove legacy once entity selector dialog uses new search.
-    var buildQuery = opts.searchFilters || opts.searchText !== undefined
-      ? $q.resolve(buildQueryFromUISearch({
-        contentType: _.get(contentType, 'data'), search: opts
-      }))
-      : buildQueryFromLegacySearchTerm(
-        spaceContext.space, contentType, opts.searchTerm);
-
-    return buildQuery
-    .then(function (searchQuery) {
-      return _.extend(queryObject, searchQuery);
+    var searchQuery = buildQueryFromUISearch({
+      contentType: _.get(contentType, 'data'), search: opts
     });
+    // TODO: Lets not return a promise here.
+    return $q.resolve(_.assign(queryObject, searchQuery));
   }
 
   function getOrderQuery (order, contentType) {

src/javascripts/search/queryBuilder.es6.js
@@ -1,43 +1,7 @@
-import $q from '$q';
-import { filterToQueryObject } from 'searchQueryAutocompletions';
 import createCachedTokenParser from 'search/cachedParser';
-import {map, extend} from 'lodash';
-import {assign} from 'utils/Collections';
+import { map } from 'lodash';
 
 const parseTokens = createCachedTokenParser();
-
-/**
- * @ngdoc method
- * @name search/queryBuilder#default
- * @description
- * Build an API query from a legacy text query (`searchTerm`) provided by the user.
- * @param {Space} space
- * @param {ContentType?} contentType
- * @param {string} textQuery Legacy text query.
- * @returns {Promise<object>}
- */
-export function buildQuery (space, contentType, textQuery) {
-  const {filters, queryText} = parseTextQuery(textQuery);
-
-  const queryItems = $q.all(
-    filters.map((filter) => filterToQueryObject(filter, contentType, space))
-  );
-
-  return queryItems
-  .then((queryItems) => {
-    let queryObject = extend.bind(null, {}).apply(null, queryItems);
-
-    if (queryText.length > 0) {
-      queryObject = assign(queryObject, { query: queryText });
-    }
-
-    if (contentType) {
-      queryObject = assign(queryObject, { content_type: contentType.getId() });
-    }
-
-    return queryObject;
-  });
-}
 
 export function parseTextQuery (textQuery) {
   const tokens = parseTokens(textQuery);

test/unit/components/search.spec.js
@@ -1,6 +1,6 @@
 import * as sinon from 'helpers/sinon';
 
-describe('search#buildQuery()', function () {
+xdescribe('search#buildQuery()', function () {
   beforeEach(function () {
     module('contentful/test');
     this.spaceContext = this.$inject('mocks/spaceContext').init();

test/unit/search/entity_selector_controller_spec.js
@@ -72,27 +72,27 @@ describe('EntitySelectorController', function () {
 
     it('triggers when term >= 1', function () {
       this.createController();
-      this.scope.view.searchTerm = '';
+      this.scope.view.searchText = '';
       this.$apply();
-      this.scope.view.searchTerm = '1';
+      this.scope.view.searchText = '1';
       this.$apply();
       sinon.assert.calledTwice(this.fetch);
     });
 
     it('triggers when clearing', function () {
       this.createController();
-      this.scope.view.searchTerm = '4444';
+      this.scope.view.searchText = '4444';
       this.$apply();
-      this.scope.view.searchTerm = '333';
+      this.scope.view.searchText = '333';
       this.$apply();
       sinon.assert.calledThrice(this.fetch);
     });
 
     it('triggers when deleting the value', function () {
       this.createController();
-      this.scope.view.searchTerm = '4444';
+      this.scope.view.searchText = '4444';
       this.$apply();
-      delete this.scope.view.searchTerm;
+      delete this.scope.view.searchText;
       this.$apply();
       sinon.assert.calledThrice(this.fetch);
     });