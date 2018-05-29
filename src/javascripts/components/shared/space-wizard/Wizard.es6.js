af84b965f7feb52557fb43684b9bb6e6bfd64ba7
Joshua Smock <joshua@contentful.com>
2018-05-29 11:17:31 +0200

    MOI-97 Fix handling errors from server on space creation
    Also fix propTypes issue

--- src/javascripts/components/shared/space-wizard/Wizard.es6.js
+++ src/javascripts/components/shared/space-wizard/Wizard.es6.js
@@ -251,6 +251,7 @@ const Wizard = createReactClass({
     } catch (error) {
       this.handleError(error);
     }
+
     if (newSpace) {
       const spaceEndpoint = createSpaceEndpoint(newSpace.sys.id);
       const apiKeyRepo = createApiKeyRepo(spaceEndpoint);
@@ -307,12 +308,17 @@ const Wizard = createReactClass({
 
   handleError (error) {
     const { action, onCancel } = this.props;
+    const steps = getSteps(action);
 
     logger.logServerWarn(`Could not ${action} space`, {error});
 
     const serverValidationErrors = getFieldErrors(error);
-    if (Object.keys(serverValidationErrors).length) {
-      this.setState({serverValidationErrors, currentStepId: 1});
+
+    if (action === 'create' && Object.keys(serverValidationErrors).length) {
+      this.setState({
+        serverValidationErrors,
+        currentStepId: steps[1].id
+      });
     } else {
       notification.error(`Could not ${action} your space. If the problem persists, get in touch with us.`);
       onCancel(); // close modal without tracking 'cancel' event
