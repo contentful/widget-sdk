41bd3c0baea9ca3ec40b6a83daac156b771a96f2
Claus Hollensteiner <claushollensteiner@gmail.com>
2018-06-01 15:57:23 +0200

    Add styling to structured text editor (#3018)

--- src/javascripts/app/widgets/structured_text/LinkedEntryBlock.es6.js
+++ src/javascripts/app/widgets/structured_text/LinkedEntryBlock.es6.js
@@ -1,42 +1,74 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cn from 'classnames';
 
 import FetchEntry from './FetchEntry';
 
+import { goToSlideInEntity } from 'states/EntityNavigationHelpers';
+
 export default class LinkedEntryBlock extends React.Component {
   static propTypes = {
-    children: PropTypes.node.isRequired,
+    isSelected: PropTypes.bool.isRequired,
     attributes: PropTypes.object.isRequired,
-    node: PropTypes.object
+    node: PropTypes.object.isRequired,
+    children: PropTypes.node
   };
+
+  handleClick = entry => {
+    goToSlideInEntity({
+      id: entry.sys.id,
+      type: 'Entry'
+    }, 2);
+  };
+
   render () {
+    const { node, attributes, isSelected, children } = this.props;
+
     return (
       <FetchEntry
-        node={this.props.node}
-        render={({ entry }) => (
-          <div
-            {...this.props.attributes}
-            className="entity-link"
-            style={{ padding: '5px', maxHeight: '100px' }}
-          >
-            <div className="entity-link-content">
-              <div style={{ flex: '1 1 auto', overflow: 'hidden' }}>
-                <span
-                  style={{
-                    fontSize: '16px',
-                    whiteSpace: 'nowrap',
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis'
-                  }}
-                >
-                  {entry.fields.title && entry.fields.title['en-US']}
-                </span>
-                <p>Content type id: {entry.sys.contentType.sys.id}</p>
-                {this.props.children}
+        node={node}
+        render={({ entry }) => {
+          return (
+            entry && (
+              <div
+                {...attributes}
+                className={cn('entity-link', {
+                  'entity-link--selected': isSelected
+                })}
+                onClick={() => this.handleClick(entry)}
+              >
+                <div className="entity-link-content">
+                  <div
+                    style={{
+                      flex: '1 1 auto',
+                      lineHeight: '1.5',
+                      overflow: 'hidden'
+                    }}
+                  >
+                    <div
+                      style={{
+                        color: '#8091A5'
+                      }}
+                    >
+                      {entry.sys.contentType.sys.id}
+                    </div>
+                    <div
+                      style={{
+                        fontWeight: 'bold',
+                        whiteSpace: 'nowrap',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis'
+                      }}
+                    >
+                      {entry.fields.title && entry.fields.title['en-US']}
+                    </div>
+                    {children}
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        )}
+            )
+          );
+        }}
       />
     );
   }
