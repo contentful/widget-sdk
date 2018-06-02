c7f0ef9abf9b1ccaf264c9480551428cb2548c68
Mudit Ameta <zeusdeux@gmail.com>
2018-06-02 23:10:36 +0200

    Convert unnecessary class into a functional component

--- src/javascripts/components/shared/stack-onboarding/next_steps/ModifyContentStep.js
+++ src/javascripts/components/shared/stack-onboarding/next_steps/ModifyContentStep.js
@@ -17,100 +17,98 @@ angular.module('contentful')
     const {Step} = require('app/home/welcome/OnboardingWithTeaSteps');
     const Code = require(CodeModule);
 
-    class ModifyContentStep extends React.Component {
-      render () {
-        const {
-          isDone,
-          isExpanded,
-          onToggle,
-          managementToken,
-          entry,
-          spaceId,
-          track
-        } = this.props;
+    const ModifyContentStep = (props) => {
+      const {
+        isDone,
+        isExpanded,
+        onToggle,
+        managementToken,
+        entry,
+        spaceId,
+        track
+      } = props;
 
-        const {firstName, lastName} = user;
-        const updatedFields = {
-          ...entry.fields,
-          name: {
-            'en-US': `${firstName} ${lastName}`
-          }
-        };
+      const {firstName, lastName} = user;
+      const updatedFields = {
+        ...entry.fields,
+        name: {
+          'en-US': `${firstName} ${lastName}`
+        }
+      };
 
-        const domain = env === 'production' ? 'contentful' : 'flinkly';
-        const commonSnippetChunk = `curl -X PUT -H 'Authorization: Bearer ${managementToken}'`;
+      const domain = env === 'production' ? 'contentful' : 'flinkly';
+      const commonSnippetChunk = `curl -X PUT -H 'Authorization: Bearer ${managementToken}'`;
 
-        let modifyContentCurlSnippet = `${commonSnippetChunk}`;
-        modifyContentCurlSnippet += ` -H 'X-Contentful-Version: ${entry.sys.version}'`;
-        modifyContentCurlSnippet += ' -H \'Content-Type: application/vnd.contentful.management.v1+json\' -H \'X-Contentful-Content-Type: person\'';
-        modifyContentCurlSnippet += ` --data-binary '{"fields":${JSON.stringify(updatedFields)}}'`;
-        modifyContentCurlSnippet += ` https://api.${domain}.com/spaces/${spaceId}/entries/${entry.sys.id}`;
+      let modifyContentCurlSnippet = `${commonSnippetChunk}`;
+      modifyContentCurlSnippet += ` -H 'X-Contentful-Version: ${entry.sys.version}'`;
+      modifyContentCurlSnippet += ' -H \'Content-Type: application/vnd.contentful.management.v1+json\' -H \'X-Contentful-Content-Type: person\'';
+      modifyContentCurlSnippet += ` --data-binary '{"fields":${JSON.stringify(updatedFields)}}'`;
+      modifyContentCurlSnippet += ` https://api.${domain}.com/spaces/${spaceId}/entries/${entry.sys.id}`;
 
-        let publishContentCurlSnippet = `${commonSnippetChunk}`;
-        publishContentCurlSnippet += ` -H 'X-Contentful-Version: ${entry.sys.version + 1}'`;
-        publishContentCurlSnippet += ` https://api.${domain}.com/spaces/${spaceId}/entries/${entry.sys.id}/published\n`;
+      let publishContentCurlSnippet = `${commonSnippetChunk}`;
+      publishContentCurlSnippet += ` -H 'X-Contentful-Version: ${entry.sys.version + 1}'`;
+      publishContentCurlSnippet += ` https://api.${domain}.com/spaces/${spaceId}/entries/${entry.sys.id}/published\n`;
 
-        const propsForStep = {
-          headerCopy: 'Modify the Gatsby Starter for Contentful blog content',
-          headerIcon: 'page-content',
-          isExpanded,
-          isDone,
-          onToggle,
-          stepKey: MODIFY_CONTENT
-        };
+      const propsForStep = {
+        headerCopy: 'Modify the Gatsby Starter for Contentful blog content',
+        headerIcon: 'page-content',
+        isExpanded,
+        isDone,
+        onToggle,
+        stepKey: MODIFY_CONTENT
+      };
 
-        const personEntry = {
-          path: ['spaces', 'detail', 'entries', 'detail'],
-          params: { spaceId, entryId: entry.sys.id }
-        };
+      const personEntry = {
+        path: ['spaces', 'detail', 'entries', 'detail'],
+        params: { spaceId, entryId: entry.sys.id }
+      };
 
-        const deployStep = {
-          path: ['spaces', 'detail', 'onboarding', 'deploy'],
-          params: { spaceId }
-        };
+      const deployStep = {
+        path: ['spaces', 'detail', 'onboarding', 'deploy'],
+        params: { spaceId }
+      };
 
-        return (
-          <Step {...propsForStep}>
-            <div className='tea-onboarding__step-description'>
-              <p>Interested in making updates to the Gatsby Starter for Contentful blog?</p>
-              <p>
-                You can create or update content items using the&nbsp;
-                <a
-                  rel={'noopener noreferrer'}
-                  href='https://www.contentful.com/developers/docs/references/content-management-api/'
-                >
-                  Content Management API
-                </a>.
-              </p>
-              <p>
-                The snippets below modify and publish&nbsp;
-                <a
-                  target={'_blank'}
-                  rel={'noopener noreferrer'}
-                  href={href(personEntry)}
-                >
-                  the author name
-                </a>.
-              </p>
-              <h5>Modify and publish content</h5>
-              <Code
-                copy
-                code={[modifyContentCurlSnippet, publishContentCurlSnippet]}
-                tooltipPosition={'right'}
-                onCopy={_ => track('copy_curl_snippets')}
-              />
-              <br />
-              <p>
-                To view this change on your deployed site run the snippets above and&nbsp;
-                <a href={href(deployStep)}>
-                  re-deploy
-                </a>!
-              </p>
-            </div>
-          </Step>
-        );
-      }
-    }
+      return (
+        <Step {...propsForStep}>
+          <div className='tea-onboarding__step-description'>
+            <p>Interested in making updates to the Gatsby Starter for Contentful blog?</p>
+            <p>
+              You can create or update content items using the&nbsp;
+              <a
+                rel={'noopener noreferrer'}
+                href='https://www.contentful.com/developers/docs/references/content-management-api/'
+              >
+                Content Management API
+              </a>.
+            </p>
+            <p>
+              The snippets below modify and publish&nbsp;
+              <a
+                target={'_blank'}
+                rel={'noopener noreferrer'}
+                href={href(personEntry)}
+              >
+                the author name
+              </a>.
+            </p>
+            <h5>Modify and publish content</h5>
+            <Code
+              copy
+              code={[modifyContentCurlSnippet, publishContentCurlSnippet]}
+              tooltipPosition={'right'}
+              onCopy={_ => track('copy_curl_snippets')}
+            />
+            <br />
+            <p>
+              To view this change on your deployed site run the snippets above and&nbsp;
+              <a href={href(deployStep)}>
+                re-deploy
+              </a>!
+            </p>
+          </div>
+        </Step>
+      );
+    };
 
     ModifyContentStep.propTypes = {
       isExpanded: PropTypes.bool.isRequired,
