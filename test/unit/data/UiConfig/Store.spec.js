daef1aca96ed7b41526b6375709f74507f68aeac
Daniel Werner <daniel.werner@contentful.com>
2018-06-11 15:13:51 +0200

    Use await instead of yield in UiConfig/store.spec

--- test/unit/data/UiConfig/Store.spec.js
+++ test/unit/data/UiConfig/Store.spec.js
@@ -39,29 +39,29 @@ describe('data/UiConfig/Store', () => {
   });
 
   describe('#entries.shared', () => {
-    it('#get() gets loaded config', function* () {
+    it('#get() gets loaded config', async function () {
       this.store.default = {_migrated: {entryListViews: 'DATA'}};
-      const api = yield this.create();
+      const api = await this.create();
       expect(api.entries.shared.get()).toEqual('DATA');
     });
 
-    it('#get() returns default values if not set', function* () {
+    it('#get() returns default values if not set', async function () {
       this.store.default = {_migrated: {entryListViews: undefined}};
-      const api = yield this.create();
+      const api = await this.create();
       expect(api.entries.shared.get().length).toEqual(3);
     });
 
-    it('#set() creates, updates and deletes UiConfig', function* () {
+    it('#set() creates, updates and deletes UiConfig', async function () {
       expect(this.store.default).toBe(undefined);
-      const api = yield this.create();
+      const api = await this.create();
 
-      yield api.entries.shared.set('DATA1');
+      await api.entries.shared.set('DATA1');
       expect(this.store.default._migrated.entryListViews).toEqual('DATA1');
 
-      yield api.entries.shared.set('DATA2');
+      await api.entries.shared.set('DATA2');
       expect(this.store.default._migrated.entryListViews).toEqual('DATA2');
 
-      yield api.entries.shared.set(undefined);
+      await api.entries.shared.set(undefined);
       expect(this.store.default._migrated.entryListViews).toEqual(undefined);
     });
   });
@@ -84,32 +84,32 @@ describe('data/UiConfig/Store', () => {
       };
     });
 
-    it('does nothing if config is not defined', function* () {
-      const api = yield this.create();
-      yield api.addOrEditCt({name: 'new'});
+    it('does nothing if config is not defined', async function () {
+      const api = await this.create();
+      await api.addOrEditCt({name: 'new'});
       expect(this.store.default).toBe(undefined);
     });
 
-    it('does nothing if there is no "Content Type" folder', function* () {
+    it('does nothing if there is no "Content Type" folder', async function () {
       const config = {_migrated: {entryListViews: [{title: 'foo'}]}};
       this.store.default = cloneDeep(config);
-      const api = yield this.create();
-      yield api.addOrEditCt(this.mockCt);
+      const api = await this.create();
+      await api.addOrEditCt(this.mockCt);
       expect(this.store.default).toEqual(config);
     });
 
-    it('does nothing if a user is not an admin', function* () {
+    it('does nothing if a user is not an admin', async function () {
       this.store.default = cloneDeep(this.withCts);
-      const api = yield this.create(false);
-      yield api.addOrEditCt(this.mockCt);
+      const api = await this.create(false);
+      await api.addOrEditCt(this.mockCt);
       expect(this.store.default).toEqual(this.withCts);
     });
 
-    it('adds content type if it doesn’t exist', function* () {
+    it('adds content type if it doesn’t exist', async function () {
       this.store.default = cloneDeep(this.withCts);
 
-      const api = yield this.create();
-      yield api.addOrEditCt(this.mockCt);
+      const api = await this.create();
+      await api.addOrEditCt(this.mockCt);
 
       sinon.assert.match(this.store.default._migrated.entryListViews, sinon.match([{
         title: 'Content Type',
@@ -123,12 +123,12 @@ describe('data/UiConfig/Store', () => {
       }]));
     });
 
-    it('edits view title when existing Content Type is changed', function* () {
+    it('edits view title when existing Content Type is changed', async function () {
       this.store.default = cloneDeep(this.withCts);
       this.mockCt.sys.id = 2;
 
-      const api = yield this.create();
-      yield api.addOrEditCt(this.mockCt);
+      const api = await this.create();
+      await api.addOrEditCt(this.mockCt);
 
       sinon.assert.match(this.store.default._migrated.entryListViews, sinon.match([{
         title: 'Content Type',
@@ -157,17 +157,17 @@ describe('data/UiConfig/Store', () => {
 
     // NOTE: We originally designed the migration to be stored back immediately
     // but ultimately decided against it (for now) to avoid "mass migration".
-    it('does not immediately save migrated version back to store', function* () {
-      yield this.create(true);
+    it('does not immediately save migrated version back to store', async function () {
+      await this.create(true);
       expect(this.store.default).toEqual(INITIAL_DATA);
     });
 
-    it('does not save migration back to store if non-admin user', function* () {
-      yield this.create(false);
+    it('does not save migration back to store if non-admin user', async function () {
+      await this.create(false);
       expect(this.store.default).toEqual(INITIAL_DATA);
     });
 
-    describe('migration', () => {
+    describe('migration', function () {
       it('results in `_migrated` property in payload', function* () {
         const api = yield this.create();
         yield api.entries.shared.set('UPDATED ENTRY LIST VIEWS');
@@ -179,18 +179,18 @@ describe('data/UiConfig/Store', () => {
         });
       });
 
-      it('is being tracked', function* () {
-        const api = yield this.create();
-        yield api.entries.shared.set(MIGRATED_DATA.entryListViews);
+      it('is being tracked', async function () {
+        const api = await this.create();
+        await api.entries.shared.set(MIGRATED_DATA.entryListViews);
         sinon.assert.calledOnceWith(
           this.trackMigrationSpy, MIGRATED_DATA, 'ui_config');
       });
 
-      it('is not tracked after migration', function* () {
-        const api = yield this.create();
-        yield api.entries.shared.set('DATA 1');
+      it('is not tracked after migration', async function () {
+        const api = await this.create();
+        await api.entries.shared.set('DATA 1');
         this.trackMigrationSpy.reset();
-        yield api.entries.shared.set('DATA 2');
+        await api.entries.shared.set('DATA 2');
         sinon.assert.notCalled(this.trackMigrationSpy);
       });
     });
