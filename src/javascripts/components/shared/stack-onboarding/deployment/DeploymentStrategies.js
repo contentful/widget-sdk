9ac445edf094d6e3848894d8d590b66e511b3cbf
Mudit Ameta <zeusdeux@gmail.com>
2018-06-12 17:04:40 +0200

    Add heroku deployment instructions

--- src/javascripts/components/shared/stack-onboarding/deployment/DeploymentStrategies.js
+++ src/javascripts/components/shared/stack-onboarding/deployment/DeploymentStrategies.js
@@ -3,6 +3,7 @@ import createReactClass from 'create-react-class';
 
 import {name as TabsModule} from '../../../react/molecules/Tabs';
 import {name as CodeModule} from '../../../react/atoms/Code';
+import {name as CreateModernOnboardingModule} from '../../auto_create_new_space/CreateModernOnboarding';
 
 export const name = 'deployment-strategies-onboarding';
 
@@ -10,6 +11,8 @@ angular.module('contentful')
 .factory(name, ['require', function (require) {
   const Tabs = require(TabsModule);
   const Code = require(CodeModule);
+  const {getCredentials} = require(CreateModernOnboardingModule);
+  const spaceContext = require('spaceContext');
 
   const DeploymentStrategies = createReactClass({
     getInitialState () {
@@ -17,12 +20,19 @@ angular.module('contentful')
         active: 'netlify'
       };
     },
+    async componentDidMount () {
+      const spaceId = spaceContext.space && spaceContext.space.getSys().id;
+      const {deliveryToken} = await getCredentials();
+
+      this.setState({
+        spaceId,
+        deliveryToken
+      });
+    },
     selectTab (tabId) {
       this.setState({ active: tabId });
     },
-    renderCode (code) {
-      return <Code lineNumbers={false} copy code={code} />;
-    },
+    renderCode: (code) => <Code lineNumbers={false} copy code={code} />,
     renderList (steps) {
       const stepsMarkup = steps.map((step, i) => (
         <li key={`step_${i}`} className={'modern-stack-onboarding--deployment-list-elem'}>
@@ -38,13 +48,22 @@ angular.module('contentful')
     renderNetlifySteps () {
       /* eslint-disable react/jsx-key */
       const steps = [
-        <a href={'https://github.com/netlify/netlifyctl#installation'} target={'_blank'}>
-          {'Install the Netlify CLI commands'}
-        </a>,
-        this.renderCode('netlifyctl login'),
+        <React.Fragment>
+          <a
+            href={'https://github.com/netlify/netlifyctl#installation'}
+            target={'_blank'}
+            style={{display: 'inline-block', marginBottom: '1.1em'}}
+          >
+            {'Install the Netlify CLI'}
+          </a>
+          <span style={{marginLeft: '10px'}}>(This is a free account. You may create an account and login through your CLI)</span>
+        </React.Fragment>,
         this.renderCode('netlifyctl login'),
         this.renderCode('npm run build'),
-        this.renderCode('netlifyctl deploy -b public')
+        this.renderCode('netlifyctl deploy -b public'),
+        <p style={{marginTop: '2.6em', fontWeight: 'bold'}}>
+          Netlify will ask if you want to create a new website. Select YES to deploy this website.
+        </p>
       ];
       /* eslint-enable react/jsx-key */
       return (
@@ -54,31 +73,44 @@ angular.module('contentful')
         </div>
       );
     },
-    renderZeitSteps () {
+    renderHerokuSteps (spaceId, deliveryToken) {
       /* eslint-disable react/jsx-key */
       const steps = [
-        this.renderCode('npm install -g now'),
-        this.renderCode('now login'),
-        this.renderCode('npm run build'),
         <React.Fragment>
-          {'If you have a free plan on Zeit, you need to remove all source maps to keep the size under 1 MB limit:'}
-          {this.renderCode('rm public/**/*.js.map')}
+          <a
+            href={'https://devcenter.heroku.com/articles/heroku-cli#download-and-install'}
+            target={'_blank'}
+            style={{display: 'inline-block', marginBottom: '1.1em'}}
+          >
+            {'Install the Heroku CLI'}
+          </a>
+          <span style={{marginLeft: '10px'}}>(This is a free account. You may create an account and login through your CLI).</span>
         </React.Fragment>,
-        <a href={'https://github.com/netlify/netlifyctl#installation'} target={'_blank'}>
-          {'Install the Netlify CLI commands'}
-        </a>,
-        this.renderCode('now --public public')
+        this.renderCode('heroku login'),
+        this.renderCode('heroku create --buildpack heroku/nodejs'),
+        this.renderCode('heroku buildpacks:add https://github.com/heroku/heroku-buildpack-static.git'),
+        this.renderCode(`heroku config:set CONTENTFUL_SPACE_ID=${spaceId} CONTENTFUL_DELIVERY_TOKEN=${deliveryToken}`),
+        this.renderCode('git push heroku master')
       ];
       /* eslint-enable react/jsx-key */
-      return (
-        <div className={'modern-stack-onboarding--deployment-strategy'}>
-          {'Zeit CLI commands'}
-          {this.renderList(steps)}
-        </div>
-      );
+      if (!spaceId || !deliveryToken) {
+        return (
+          <div className='loader__container u-separator--small' style={{background: 'transparent'}}>
+            <div className='loading-box__spinner'></div>
+            <div className='loader_message'>Loading Heroku deployment steps</div>
+          </div>
+        );
+      } else {
+        return (
+          <div className={'modern-stack-onboarding--deployment-strategy'}>
+            {'Heroku CLI commands'}
+            {this.renderList(steps)}
+          </div>
+        );
+      }
     },
     render () {
-      const { active } = this.state;
+      const { active, deliveryToken, spaceId } = this.state;
       const tabs = [
         {
           id: 'netlify',
@@ -86,9 +118,9 @@ angular.module('contentful')
           content: this.renderNetlifySteps()
         },
         {
-          id: 'zeit',
-          title: 'Zeit (now)',
-          content: this.renderZeitSteps()
+          id: 'heroku',
+          title: 'Heroku',
+          content: this.renderHerokuSteps(spaceId, deliveryToken)
         }
       ];
       return <Tabs tabs={tabs} active={active} onSelect={this.selectTab} />;
