d48701575b646029f7f101a297991c4d24728710
Daniel Werner <daniel.werner@contentful.com>
2018-06-12 14:19:23 +0200

    Add more tests for widgets/builtin

--- test/unit/widgets/builtin_spec.js
+++ test/unit/widgets/builtin_spec.js
@@ -1,11 +1,36 @@
+import { uniq } from 'lodash';
+
 describe('widgets/builtin', () => {
-  // This asserts that we can put the builtin widget ids into the
-  // editing interface payload and it is validated by the CMA.
-  // https://github.com/contentful/content_api/blob/master/lib/handler/constraints/management/schemas/content-type.js#L46-L51
-  it('returns a valid resource ID for each widget descriptor', function () {
+  beforeEach(function () {
     module('contentful/test');
+
     this.create = this.$inject('widgets/builtin').create;
-    this.create().forEach(({id}) => expectValidCmaId(id));
+  });
+
+  describe('create()', () => {
+    it('returns a list of widget descriptors', function () {
+      const expectedProperties = jasmine.objectContaining({
+        id: jasmine.any(String),
+        name: jasmine.any(String),
+        template: jasmine.any(String),
+        fieldTypes: jasmine.any(Array)
+      });
+      this.create().forEach((widgetDescriptor) => {
+        expect(widgetDescriptor).toEqual(expectedProperties);
+      });
+    });
+
+    // This asserts that we can put the builtin widget ids into the
+    // editing interface payload and it is validated by the CMA.
+    // https://github.com/contentful/content_api/blob/master/lib/handler/constraints/management/schemas/content-type.js#L46-L51
+    it('returns a valid resource ID for each widget descriptor', function () {
+      this.create().forEach(({id}) => expectValidCmaId(id));
+    });
+
+    it('returns no widget descriptors with duplicate ID', function () {
+      const ids = this.create().map(({id}) => id);
+      expect(ids).toEqual(uniq(ids));
+    });
   });
 });
 
