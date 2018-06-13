bb909b741ef433295b126fd19defddf4d63f4cac
Daniel Werner <daniel.werner@contentful.com>
2018-06-13 16:29:29 +0200

    Update widgets/builtin.spec.js ID validation
    Also updates link to ID validation on the backend (CMA).

--- test/unit/widgets/builtin.spec.js
+++ test/unit/widgets/builtin.spec.js
@@ -22,7 +22,6 @@ describe('widgets/builtin', () => {
 
     // This asserts that we can put the builtin widget ids into the
     // editing interface payload and it is validated by the CMA.
-    // https://github.com/contentful/content_api/blob/master/lib/handler/constraints/management/schemas/content-type.js#L46-L51
     it('returns a valid resource ID for each widget descriptor', function () {
       this.create().forEach(({id}) => expectValidCmaId(id));
     });
@@ -35,6 +34,8 @@ describe('widgets/builtin', () => {
 });
 
 function expectValidCmaId (id) {
-  const ID_RE = /^[a-zA-Z][a-zA-Z0-9_]{0,63}$/;
+  // Regex is based on the CMA's ID validation + limited to 64 characters:
+  // https://github.com/contentful/content_api/blob/88aa60a/lib/handler/constraints/management/schemas/id.js
+  const ID_RE = /^[a-zA-Z0-9][a-zA-Z0-9-_]{0,63}$/;
   expect(id).toMatch(ID_RE);
 }
