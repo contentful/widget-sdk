60a760551e253d25a638330cbadb29a72a58ab80
Mudit Ameta <zeusdeux@gmail.com>
2018-06-15 16:00:47 +0200

    Reinstate tracking in Skip module

--- src/javascripts/components/shared/stack-onboarding/components/Skip.js
+++ src/javascripts/components/shared/stack-onboarding/components/Skip.js
@@ -1,7 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import createReactClass from 'create-react-class';
-import {name as CreateModernOnboardingModule} from '../../auto_create_new_space/CreateModernOnboarding';
-
 import {name as CreateModernOnboardingModule} from '../../auto_create_new_space/CreateModernOnboarding';
 
 export const name = 'stack-onboarding-skip';
@@ -10,12 +9,15 @@ angular.module('contentful')
 .factory(name, ['require', function (require) {
   const $state = require('$state');
   const $stateParams = require('$stateParams');
-  const { track } = require(CreateModernOnboardingModule);
   const store = require('TheStore').getStore();
-  const {getStoragePrefix} = require(CreateModernOnboardingModule);
+  const {getStoragePrefix, track} = require(CreateModernOnboardingModule);
 
   const StackOnboardingSkip = createReactClass({
+    propTypes: {
+      link: PropTypes.oneOf(['getStarted', 'copy', 'explore', 'deploy'])
+    },
     onClick () {
+      track(`skip_from_${this.props.link}`);
       $state.go('spaces.detail.home', {
         spaceId: $stateParams.spaceId
       });
