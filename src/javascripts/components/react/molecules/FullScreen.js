8cc9c8e40bdb9b915219e70691ffd077ceeb8e94
Vsevolod Zaikov <seva.zaikov@gmail.com>
2018-06-18 15:45:29 +0200

    DLAB-164 add contentful logo to modern onboarding screens

--- src/javascripts/components/react/molecules/FullScreen.js
+++ src/javascripts/components/react/molecules/FullScreen.js
@@ -3,26 +3,28 @@ import PropTypes from 'prop-types';
 
 export const name = 'full-screen-component';
 
-const FullScreen = ({ children, close }) => {
-  return (
-    <div className='modern-stack-onboarding--container'>
-      <div className='modern-stack-onboarding--wrapper'>
-        <div className='modern-stack-onboarding--header'>
-          CONTENTFUL LOGO HERE
-          {close}
-        </div>
-        {children}
-      </div>
-    </div>
-  );
-};
-
-FullScreen.propTypes = {
-  children: PropTypes.node,
-  close: PropTypes.node
-};
-
 angular.module('contentful')
-.factory(name, [function () {
+.factory(name, ['require', function (require) {
+  const ContentfulLogo = require('svg/ContentfulLogo').default;
+
+  const FullScreen = ({ children, close }) => {
+    return (
+      <div className='modern-stack-onboarding--container'>
+        <div className='modern-stack-onboarding--wrapper'>
+          <div className='modern-stack-onboarding--header'>
+            <ContentfulLogo />
+            {close}
+          </div>
+          {children}
+        </div>
+      </div>
+    );
+  };
+
+  FullScreen.propTypes = {
+    children: PropTypes.node,
+    close: PropTypes.node
+  };
+
   return FullScreen;
 }]);
