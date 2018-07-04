84758ee93e3bb4e7375c1fc453af3b54d6bfd9cb
Daniel Werner <daniel.werner@contentful.com>
2018-07-04 18:45:10 +0200

    Disable "Create and link" until entry created
    [AUTH-245] Adds nicer visuals for the disabled "Create entry and link" button of
    the reference editor. With this change it looks the same for entries and
    assets while previouslty only linking assets resulted in a disabled
    button and the spinner being shown.

--- test/unit/components/tabs/entry_list/CreateEntryButton/CreateEntryButton.es6.spec.js
+++ test/unit/components/tabs/entry_list/CreateEntryButton/CreateEntryButton.es6.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { noop } from 'lodash';
 import sinon from 'npm:sinon';
-import CreateEntryButton from 'components/CreateEntryButton';
+import { default as CreateEntryButton, Style } from 'components/CreateEntryButton';
 
 import { mount } from 'enzyme';
 
@@ -11,26 +12,35 @@ const CONTENT_TYPE_3 = { name: 'name-3', sys: { id: 'ID_3' } };
 describe('CreateEntryButton', () => {
   beforeEach(function () {
     const findByTestId = (id) => this.wrapper.find(`[data-test-id="${id}"]`);
-    this.findCta = () => findByTestId('cta');
+    // TODO: Remove `.at(0)` which is necessary because of odd component library DOM.
+    this.findCta = () => findByTestId('cta').at(0);
     this.findMenu = () => findByTestId('add-entry-menu');
     this.findMenuItems = () => findByTestId('contentType');
     this.findDropdownIcon = () => findByTestId('dropdown-icon');
+    this.findSpinner = () => findByTestId('spinner');
     this.setup = () => {
       this.wrapper = mount(<CreateEntryButton {...this.props} />);
+      delete this.setup; // Allows us to run setup manually in `beforeEach`
       return {
         cta: this.findCta(),
         menu: this.findMenu(),
         dropdownIcon: this.findDropdownIcon()
       };
     };
+    this.flushPromises = () =>
+      new Promise(resolve => {
+        setTimeout(() => {
+          resolve();
+          this.wrapper = this.wrapper.update();
+        }, 50);
+      });
   });
 
   describe('with multiple content types', function () {
     beforeEach(function () {
-      this.onSelect = sinon.spy();
       this.props = {
         contentTypes: [CONTENT_TYPE_1, CONTENT_TYPE_2, CONTENT_TYPE_3],
-        onSelect: this.onSelect
+        onSelect: noop
       };
     });
 
@@ -39,6 +49,11 @@ describe('CreateEntryButton', () => {
     itRendersDropdownIs(true);
 
     describe('menu', function () {
+      beforeEach(function () {
+        this.onSelect = sinon.spy();
+        this.props.onSelect = this.onSelect;
+      });
+
       it('opens after click on btn', function ({ cta }) {
         cta.simulate('click');
         expect(this.findMenu().length).toEqual(1);
@@ -56,9 +71,12 @@ describe('CreateEntryButton', () => {
       });
 
       it('emits onSelect after click on menu item', function ({ cta }) {
+        const TEST_ITEM_INDEX = 1;
+        const testItemCt = this.props.contentTypes[TEST_ITEM_INDEX];
         cta.simulate('click');
-        this.findMenuItems().at(1).simulate('click');
-        sinon.assert.calledWith(this.onSelect, 'ID_2');
+        const testItem = this.findMenuItems().at(TEST_ITEM_INDEX);
+        testItem.simulate('click');
+        sinon.assert.calledWith(this.onSelect, testItemCt.sys.id);
       });
     });
   });
@@ -78,7 +96,7 @@ describe('CreateEntryButton', () => {
 
     it('emits onSelect after clicking on cta', function ({ cta }) {
       cta.simulate('click');
-      sinon.assert.calledWith(this.onSelect, 'ID_1');
+      sinon.assert.calledWith(this.onSelect, CONTENT_TYPE_1.sys.id);
     });
   });
 
@@ -88,14 +106,99 @@ describe('CreateEntryButton', () => {
     beforeEach(function () {
       this.props = {
         contentTypes: [CONTENT_TYPE_1],
-        onSelect: sinon.spy(),
+        onSelect: noop,
         text: CUSTOM_LABEL
       };
     });
 
     itRendersTriggerButtonWithLabel(CUSTOM_LABEL);
   });
+
+  describe('as link', function () {
+    beforeEach(function () {
+      this.onSelect = sinon.stub();
+      this.props = {
+        contentTypes: [CONTENT_TYPE_3],
+        onSelect: this.onSelect,
+        style: Style.Link
+      };
+      this.assertEmittedOnSelect = () =>
+        sinon.assert.calledOnceWith(this.onSelect, CONTENT_TYPE_3.sys.id);
+    });
+
+    itRendersTriggerButtonWithLabel(`Add ${CONTENT_TYPE_3.name}`);
+
+    describe('before clicking on cta', function () {
+      itDisablesLinkIs(false);
+    });
+
+    describe('clicking on cta', function () {
+      describe('with `onSelect` not returning a promise', function () {
+        beforeEach(function () {
+          this.onSelect.returns(undefined);
+          this.setup();
+          this.findCta().simulate('click');
+        });
+
+        itDisablesLinkIs(false);
+
+        it('emits onSelect', function () {
+          this.assertEmittedOnSelect();
+        });
+
+        it('emits onSelect on subsequent click', function () {
+          this.onSelect.reset();
+          this.findCta().simulate('click');
+          this.assertEmittedOnSelect();
+        });
+      });
+
+      describe('with `onSelect` returning a promise', function () {
+        beforeEach(function () {
+          this.onSelect.resolves();
+          this.setup();
+          this.findCta().simulate('click');
+        });
+
+        itDisablesLinkIs(true);
+
+        it('emits onSelect', function () {
+          this.assertEmittedOnSelect();
+        });
+
+        it('does not emit onSelect on subsequent click', function () {
+          this.onSelect.reset();
+          this.findCta().simulate('click');
+          sinon.assert.notCalled(this.onSelect);
+        });
+
+        describe('after resolving promise', function () {
+          beforeEach(async function () {
+            await this.flushPromises();
+          });
+
+          itDisablesLinkIs(false);
+
+          it('emits onSelect on subsequent clicks', function () {
+            this.onSelect.reset();
+            this.findCta().simulate('click');
+            this.assertEmittedOnSelect();
+          });
+        });
+      });
+    });
+  });
 });
+
+function itDisablesLinkIs (isTrue) {
+  it(`${isTrue ? 'shows' : 'shows no'} spinner`, function () {
+    expect(this.findSpinner().length).toEqual(isTrue ? 1 : 0);
+  });
+
+  it(`${isTrue ? 'disables' : 'enables'} link`, function () {
+    expect(this.findCta().prop('disabled')).toBe(isTrue);
+  });
+}
 
 function itRendersTriggerButtonWithLabel (label) {
   it(`renders the trigger button with label “${label}”`, ({ cta, menu }) => {
@@ -105,8 +208,8 @@ function itRendersTriggerButtonWithLabel (label) {
   });
 }
 
-function itRendersDropdownIs (isTrue = true) {
-  it(`${isTrue ? 'renders' : 'does not render'} button as dropdown`,
+function itRendersDropdownIs (isTrue) {
+  it(`${isTrue ? ' renders' : 'does not render'} button as dropdown`,
     ({ dropdownIcon }) => {
       const toBeFn = isTrue ? 'toBeGreaterThan' : 'toBe';
       expect(dropdownIcon.length)[toBeFn](0);
