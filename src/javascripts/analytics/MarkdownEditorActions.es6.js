b101a1ffe722ccbc1704e34068fcbd1c83a90d15
markuslautenbach <markus.lautenbach@contentful.com>
2018-07-19 10:29:31 +0200

    Simplify

--- src/javascripts/analytics/MarkdownEditorActions.es6.js
+++ src/javascripts/analytics/MarkdownEditorActions.es6.js
@@ -3,5 +3,5 @@ import * as Analytics from 'analytics/Analytics';
 export const trackMarkdownEditorAction = (action, {newValue, fullscreen}) =>
   Analytics.track(
     'markdown_editor:action',
-    {...{action, fullscreen: !!fullscreen}, ...(typeof newValue !== 'undefined' ? {new_value: newValue} : {})}
+    {action, fullscreen: !!fullscreen, ...(newValue !== undefined ? {new_value: newValue} : {})}
   );
