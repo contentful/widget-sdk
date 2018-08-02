0c18348404194c6ed798c500f521baaac99d1611
Jolyon Pawlyn <jpawlyn@gmail.com>
2018-08-02 09:50:35 +0200

    Get usage tooltip working in Firefox

--- src/javascripts/directives/cf_when_disabled.js
+++ src/javascripts/directives/cf_when_disabled.js
@@ -21,13 +21,13 @@ angular.module('contentful').directive('cfWhenDisabled', ['require', require => 
       width: elem.width() +
              prop('paddingLeft')+
              prop('paddingRight')+
-             prop('borderLeft')+
-             prop('borderRight'),
+             prop('borderLeftWidth')+
+             prop('borderRightWidth'),
       height: elem.height()+
               prop('paddingTop')+
               prop('paddingBottom')+
-              prop('borderTop')+
-              prop('borderBottom'),
+              prop('borderTopWidth')+
+              prop('borderBottomWidth'),
     });
     return layer;
   }
