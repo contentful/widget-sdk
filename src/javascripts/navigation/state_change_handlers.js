0dddf1e2d34a468640a801e93273539cc2cd9965
Joshua Smock <joshua@contentful.com>
2018-08-07 11:37:27 +0200

    MOI-207 Add comment in state_change_handlers

--- src/javascripts/navigation/state_change_handlers.js
+++ src/javascripts/navigation/state_change_handlers.js
@@ -57,6 +57,14 @@ angular.module('cf.app')
     // using the latter caused problems when redirecting with
     // ui-router's option {reload: true}
     if (toState.name.slice(0, 7) !== 'spaces.') {
+      // This will remove the space from the current analytics session, but will
+      // preserve the organziation. This means that the org will remain the same
+      // and will be used in analytics calls, but the same will not be.
+      //
+      // See states/account_organizations.js#organizationBase for the usage of
+      // setting organization when on an organization settings page, and
+      // states/Spaces.es6.js#spaceDetail for the usage of setting space and org
+      // when in a space page.
       Analytics.trackContextChange(null);
       spaceContext.purge();
     }
