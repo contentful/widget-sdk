8590cd14fe243bdf6e99581444a199efc46aae69
Daniel Werner <daniel.werner@contentful.com>
2018-08-15 21:13:08 +0200

    Simplify StructuredText toolbar buttons jsx

--- src/javascripts/app/widgets/structured_text/StructuredTextEditor.es6.js
+++ src/javascripts/app/widgets/structured_text/StructuredTextEditor.es6.js
@@ -113,7 +113,11 @@ export default class StructuredTextEditor extends React.Component {
     }
   };
   renderToolbar () {
-    const disabled = this.state.isDisabled;
+    const props = {
+      change: this.state.value.change(),
+      onToggle: this.onChange,
+      disabled: this.state.isDisabled
+    };
     return (
       <EditorToolbar>
         <HeadingDropdown
@@ -121,44 +125,21 @@ export default class StructuredTextEditor extends React.Component {
           isToggleActive={true}
           isOpen={this.state.headingMenuOpen}
           onClose={this.closeHeadingMenu}
-          onChange={this.state.value.change()}
-          disabled={disabled}
+          onChange={props.change}
+          disabled={props.disabled}
         >
-          <Heading1
-            disabled={disabled}
-            change={this.state.value.change()}
-            onToggle={this.onChange}
+          <Heading1 {...props}
             menuIsOpen={this.state.headingMenuOpen}
             extraClassNames="toolbar-h1-toggle"
           />
-          <Heading2
-            disabled={disabled}
-            change={this.state.value.change()}
-            onToggle={this.onChange}
+          <Heading2 {...props}
             menuIsOpen={this.state.headingMenuOpen}
           />
         </HeadingDropdown>
-        <Bold
-          disabled={disabled}
-          change={this.state.value.change()}
-          onToggle={this.onChange}
-        />
-        <Italic
-          disabled={disabled}
-          change={this.state.value.change()}
-          onToggle={this.onChange}
-        />
-        <Underlined
-          disabled={disabled}
-          change={this.state.value.change()}
-          onToggle={this.onChange}
-        />
-        <EntryLinkBlock
-          disabled={disabled}
-          change={this.state.value.change()}
-          onToggle={this.onChange}
-          field={this.props.field}
-        />
+        <Bold {...props} />
+        <Italic {...props} />
+        <Underlined {...props} />
+        <EntryLinkBlock {...props} field={this.props.field} />
       </EditorToolbar>
     );
   }
