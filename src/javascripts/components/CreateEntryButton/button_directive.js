6efc08da43cc1d819721a386d1c4ea6d09cf6245
Sergii Bezliudnyi <serg.bezludny@gmail.com>
2018-08-15 16:25:36 +0200

    Fix: Change disabling logic of Create Entry Button (#3232)

--- src/javascripts/components/CreateEntryButton/button_directive.js
+++ src/javascripts/components/CreateEntryButton/button_directive.js
@@ -1,34 +1,43 @@
-angular.module('contentful')
-.directive('cfCreateEntryButton', ['require', require => {
-  const _ = require('lodash');
-  const React = require('react');
-  const ReactDOM = require('react-dom');
-  const Menu = require('components/CreateEntryButton').default;
-  const $parse = require('$parse');
+angular.module('contentful').directive('cfCreateEntryButton', [
+  'require',
+  require => {
+    const _ = require('lodash');
+    const React = require('react');
+    const ReactDOM = require('react-dom');
+    const Menu = require('components/CreateEntryButton').default;
 
-  return {
-    link: function ($scope, elem, attr) {
-      const isDisabled = $parse(attr.disabled);
+    return {
+      link: function ($scope, elem, attr) {
+        $scope.$watchCollection(
+          () => {
+            const contentTypes = _.get($scope, attr.contentTypes, []);
+            const disabled = contentTypes.length === 0;
+            return {
+              contentTypes,
+              suggestedContentTypeId: _.get(
+                $scope,
+                attr.suggestedContentTypeId
+              ),
+              size: attr.size,
+              style: attr.style,
+              text: attr.text,
+              disabled
+            };
+          },
+          props => {
+            props = Object.assign({}, props, {
+              onSelect: $scope.$eval(attr.onSelect)
+            });
 
-      $scope.$watchCollection(() => ({
-        contentTypes: _.get($scope, attr.contentTypes, []),
-        suggestedContentTypeId: _.get($scope, attr.suggestedContentTypeId),
-        size: attr.size,
-        style: attr.style,
-        text: attr.text,
-        disabled: isDisabled($scope) || false
-      }), props => {
-        props = Object.assign({}, props, {
-          onSelect: $scope.$eval(attr.onSelect)
+            ReactDOM.render(React.createElement(Menu, props), elem[0]);
+          }
+        );
+
+        // Remember to unmount
+        $scope.$on('$destroy', () => {
+          ReactDOM.unmountComponentAtNode(elem[0]);
         });
-
-        ReactDOM.render(React.createElement(Menu, props), elem[0]);
-      });
-
-      // Remember to unmount
-      $scope.$on('$destroy', () => {
-        ReactDOM.unmountComponentAtNode(elem[0]);
-      });
-    }
-  };
-}]);
+      }
+    };
+  }
+]);
