9ac5262e2e858bf7b08d9d8db1dbc18c4a859a02
Daniel Werner <daniel.werner@contentful.com>
2018-08-28 15:13:34 +0200

    Add StructuredText className for marks

--- src/javascripts/app/widgets/structured_text/plugins/shared/MarkPlugin.es6.js
+++ src/javascripts/app/widgets/structured_text/plugins/shared/MarkPlugin.es6.js
@@ -1,16 +1,19 @@
 import isHotkey from 'is-hotkey';
 import markDecorator from './MarkDecorator';
 
-export default function (options, hotkey) {
+export default function ({ type, tagName }, hotkey) {
   return {
     renderMark: props => {
-      if (props.mark.type === options.type) {
-        return markDecorator(options.tagName)(props);
+      if (props.mark.type === type) {
+        return markDecorator(
+          tagName,
+          { className: `cf-slate-mark-${type}` }
+        )(props);
       }
     },
     onKeyDown (event, change) {
       if (isHotkey(hotkey, event)) {
-        change.call(change => change.toggleMark(options.type));
+        change.call(change => change.toggleMark(type));
       }
     }
   };
