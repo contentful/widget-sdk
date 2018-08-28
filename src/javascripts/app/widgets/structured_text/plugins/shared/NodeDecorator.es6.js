381cf19d9a9536a059edf1c49fbebac84fd1f673
Daniel Werner <daniel.werner@contentful.com>
2018-08-28 15:11:53 +0200

    Make StructuredText paragraph `div` instead of `p`
    To show a tooltip (and potentially other block elements) within a
    paragraph, we need to render it as a `div` instead of `p`.

--- src/javascripts/app/widgets/structured_text/plugins/shared/NodeDecorator.es6.js
+++ src/javascripts/app/widgets/structured_text/plugins/shared/NodeDecorator.es6.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { NodePropTypes } from './PropTypes';
 
-export default function (Tag) {
+export default function (Tag, tagProps = {}) {
   const CommonNode = ({ attributes, children }) => {
-    return <Tag {...attributes}>{children}</Tag>;
+    return <Tag {...tagProps} {...attributes}>{children}</Tag>;
   };
 
   CommonNode.displayName = `${Tag}-node`;
