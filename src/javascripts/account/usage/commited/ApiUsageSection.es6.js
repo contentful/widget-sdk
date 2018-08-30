74c535a37423852557f9d44b87ade05686187d50
markuslautenbach <markus.lautenbach@contentful.com>
2018-08-30 14:41:05 +0200

    Create new usage page

---
+++ src/javascripts/account/usage/commited/ApiUsageSection.es6.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { sum } from 'lodash';
+
+import ApiUsageChart from './ApiUsageChart';
+import { organizationResourceUsagePropType, periodPropType } from './propTypes';
+import formatNumber from './formatNumber';
+
+const colors = ['#18a16c', '#d57d1f', '#824cb9'];
+
+export default class ApiUsageSection extends React.Component {
+  static propTypes = {
+    includedLimit: PropTypes.number.isRequired,
+    api: PropTypes.string.isRequired,
+    spaceNames: PropTypes.objectOf(PropTypes.string).isRequired,
+    usage: PropTypes.arrayOf(organizationResourceUsagePropType).isRequired,
+    period: periodPropType.isRequired,
+    chartWidth: PropTypes.string.isRequired
+  };
+
+  render() {
+    const { api, spaceNames, usage, includedLimit, period, chartWidth } = this.props;
+    return (
+      <React.Fragment>
+        <h2>{`${api.toUpperCase()} requests`}</h2>
+        <div>
+          <h3>Top 3 spaces</h3>
+          <table className="usage-page__api-table">
+            <tbody>
+              {usage.map(
+                (
+                  {
+                    sys: {
+                      space: {
+                        sys: { id: spaceId }
+                      }
+                    },
+                    usage: spaceUsage
+                  },
+                  index
+                ) => (
+                  <tr key={spaceId}>
+                    <td className="usage-page__space-name">{spaceNames[spaceId]}</td>
+                    <td className="usage-page__space-usage">{formatNumber(sum(spaceUsage), 1)}</td>
+                    <td style={{ color: colors[index] }}>
+                      {`${Math.round((sum(spaceUsage) / includedLimit) * 100)}%`}
+                    </td>
+                  </tr>
+                )
+              )}
+            </tbody>
+          </table>
+        </div>
+        <ApiUsageChart
+          usage={usage}
+          period={period}
+          colors={colors}
+          width={chartWidth}
+          spaceNames={spaceNames}
+        />
+      </React.Fragment>
+    );
+  }
+}
---
+++ src/javascripts/account/usage/commited/OrganizationUsagePage.es6.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { map, sum } from 'lodash';
+
+import { Button } from '@contentful/ui-component-library';
+import * as Intercom from 'intercom';
+
+import OrganisationUsageChart from './OrganisationUsageChart';
+import ApiUsageSection from './ApiUsageSection';
+import formatNumber from './formatNumber';
+import {
+  organizationResourceUsagePropType,
+  organizationUsagePropType,
+  arrayPropType,
+  periodPropType
+} from './propTypes';
+
+const apiUsagePropType = arrayPropType(organizationResourceUsagePropType);
+
+const chartWidth = '700px';
+
+export default class OrganizationUsagePage extends React.Component {
+  static propTypes = {
+    spaceNames: PropTypes.objectOf(PropTypes.string).isRequired,
+    usage: PropTypes.shape({
+      org: organizationUsagePropType,
+      apis: PropTypes.shape({
+        cma: apiUsagePropType,
+        cda: apiUsagePropType,
+        cpa: apiUsagePropType
+      })
+    }).isRequired,
+    period: periodPropType,
+    includedLimit: PropTypes.number.isRequired
+  };
+
+  render() {
+    const {
+      usage: { org, apis },
+      includedLimit,
+      spaceNames,
+      period
+    } = this.props;
+    const totalUsage = sum(org.usage);
+
+    return (
+      <div className="usage-page">
+        <h2>Total number of API requests</h2>
+        <div className="usage-page__org-section">
+          <div>
+            <div className="usage-page__total-usage">{totalUsage.toLocaleString('en-US')}</div>
+            <div className="usage-page__limit">
+              <span className="usage-page__included-limit">{`${formatNumber(
+                includedLimit
+              )} included`}</span>
+              {totalUsage > includedLimit && (
+                <span className="usage-page__overage">{` + ${(
+                  totalUsage - includedLimit
+                ).toLocaleString('en-US')} overage`}</span>
+              )}
+            </div>
+          </div>
+          <div className="usage-page__section">
+            Plan overages help you handle traffic spikes or expand the team while keeping your
+            expenses in check.
+          </div>
+          <Button onClick={() => Intercom.open()}>Talk to support</Button>
+        </div>
+        <OrganisationUsageChart
+          key="total"
+          usage={org}
+          includedLimit={includedLimit}
+          period={period}
+          width={chartWidth}
+        />
+        {map(apis, (usage, api) => (
+          <ApiUsageSection
+            key={api}
+            usage={usage.items}
+            spaceNames={spaceNames}
+            api={api}
+            includedLimit={includedLimit}
+            period={period}
+            chartWidth={chartWidth}
+          />
+        ))}
+      </div>
+    );
+  }
+}
