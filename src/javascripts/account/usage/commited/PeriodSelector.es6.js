93f42f9329992b0e4449ecdb327ebd456a775d62
markuslautenbach <markus.lautenbach@contentful.com>
2018-09-04 13:41:53 +0200

    Add human readable hint

--- src/javascripts/account/usage/commited/PeriodSelector.es6.js
+++ src/javascripts/account/usage/commited/PeriodSelector.es6.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
+import { cond, constant, stubTrue } from 'lodash';
 
 import { periodPropType } from './propTypes.es6';
 
@@ -23,18 +24,20 @@ export default class PeriodSelector extends React.Component {
           defaultValue={selectedPeriodIndex}
           onChange={onChange}>
           {periods.map(({ startDate, endDate }, index) => {
-            const isCurrentPeriod = index === 0;
+            const isCurrentPeriod = endDate === null;
             const start = moment(startDate);
-            const end = endDate
-              ? moment(endDate)
-              : moment(start)
+            const end = isCurrentPeriod
+              ? moment(start)
                   .add(1, 'month')
-                  .subtract(1, 'day');
+                  .subtract(1, 'day')
+              : moment(endDate);
 
             return (
-              <option key={index} value={index}>{`${formatDate(start)} – ${formatDate(end)}${
-                end.year() !== moment().year() ? ` ${end.year()}` : ''
-              }${isCurrentPeriod ? ` (current)` : ''}`}</option>
+              <option key={index} value={index}>{`${formatDate(start)} – ${formatDate(end)} ${cond([
+                [constant(isCurrentPeriod), constant('(current)')],
+                [constant(end.year() === moment().year()), constant(`(${moment().to(start)})`)],
+                [constant(stubTrue), constant(end.year())]
+              ])()}`}</option>
             );
           })}
         </select>
