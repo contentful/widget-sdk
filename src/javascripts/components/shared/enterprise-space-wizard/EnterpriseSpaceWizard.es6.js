de5286ffbca12094c90ffcbc4be73e632b407882
Guilherme Barbosa <guipvb@gmail.com>
2018-09-06 11:42:01 +0200

    Split EnterpriseSpaceWizard

--- src/javascripts/components/shared/enterprise-space-wizard/EnterpriseSpaceWizard.es6.js
+++ src/javascripts/components/shared/enterprise-space-wizard/EnterpriseSpaceWizard.es6.js
@@ -9,13 +9,14 @@ import { get } from 'lodash';
 import * as actionCreators from '../space-wizard/store/actionCreators.es6';
 
 import TemplateSelector from 'components/shared/space-wizard/TemplateSelector.es6';
-import PlanFeatures from 'components/shared/space-wizard/PlanFeatures.es6';
 import ProgressScreen from 'components/shared/space-wizard/ProgressScreen.es6';
 import Dialog from 'app/entity_editor/Components/Dialog';
 
 import ContactUsButton from 'ui/Components/ContactUsButton.es6';
+import EnterpriseSpaceWizardPlan from './EnterpriseSpaceWizardPlan.es6';
+import EnterpriseSpaceWizardInfo from './EnterpriseSpaceWizardInfo.es6';
 
-import { TextField, TextLink } from '@contentful/ui-component-library';
+import { TextField } from '@contentful/ui-component-library';
 
 class EnterpriseSpaceWizard extends React.Component {
   static propTypes = {
@@ -127,7 +128,6 @@ class EnterpriseSpaceWizard extends React.Component {
     const submitted = spaceCreation.isPending;
     const { name, template } = this.props.newSpaceMeta;
     const {
-      loading,
       errorMessage,
       invalidName,
       isDisabled,
@@ -140,7 +140,7 @@ class EnterpriseSpaceWizard extends React.Component {
     // template creation that happens after the space has been
     // successfully created
     const inProgress = spaceCreation.success && template;
-    const showForm = !isDisabled && !reachedLimit && !loading && !inProgress;
+    const showForm = !isDisabled && !reachedLimit && !inProgress;
 
     return (
       <Dialog testId="enterprise-space-creation-dialog" size="large">
@@ -150,67 +150,66 @@ class EnterpriseSpaceWizard extends React.Component {
             <ProgressScreen done={!spaceCreation.isPending} onConfirm={() => this.close()} />
           </Dialog.Body>
         )}
-        {}
-        {!loading &&
-          !inProgress && (
-            <Dialog.Body>
-              <Info />
-              <Plan
-                resources={resources}
-                roleSet={freeSpaceRatePlan.roleSet}
-                reachedLimit={reachedLimit}
-                usage={usage}
-                limit={limit}
-                isDisabled={isDisabled}
-              />
-              {showForm && (
-                <React.Fragment>
-                  <TextField
-                    countCharacters
-                    required
-                    style={{ marginBottom: '30px', display: 'inline-block' }}
-                    value={name || ''}
-                    name="spaceName"
-                    id="spaceName"
-                    labelText="Space name"
-                    helpText="Can have up to 30 characters"
-                    textInputProps={{
-                      maxLength: 30,
-                      width: 'large'
-                    }}
-                    onChange={evt => this.handleSpaceNameChange(evt.target.value)}
-                    validationMessage={errorMessage}
-                  />
-                  {invalidName && <p className="cfnext-form__field-error">Invalid name</p>}
 
-                  <TemplateSelector
-                    onSelect={setNewSpaceTemplate}
-                    onToggle={() => this.reposition()}
-                    templates={templates}
-                    fetchTemplates={fetchTemplates}
-                    formAlign="left"
-                  />
-                </React.Fragment>
-              )}
-              {!showForm && (
-                <React.Fragment>
-                  {reachedLimit &&
-                    !isDisabled && (
-                      <p className="note-box--info">
-                        {`You've created ${limit} proof of concept spaces. Delete existing ones or talk to us if you need more.`}
-                      </p>
-                    )}
-                  {isDisabled && (
-                    <p className="note-box--info">{`You can't create proof of concept spaces because they're not a part of your enterprise deal with Contentful.
-                    Get in touch with us if you want to create new spaces.`}</p>
+        {!inProgress && (
+          <Dialog.Body>
+            <EnterpriseSpaceWizardInfo />
+            <EnterpriseSpaceWizardPlan
+              resources={resources}
+              roleSet={freeSpaceRatePlan.roleSet}
+              reachedLimit={reachedLimit}
+              usage={usage}
+              limit={limit}
+              isDisabled={isDisabled}
+            />
+            {showForm && (
+              <React.Fragment>
+                <TextField
+                  countCharacters
+                  required
+                  style={{ marginBottom: '30px', display: 'inline-block' }}
+                  value={name || ''}
+                  name="spaceName"
+                  id="spaceName"
+                  labelText="Space name"
+                  helpText="Can have up to 30 characters"
+                  textInputProps={{
+                    maxLength: 30,
+                    width: 'large'
+                  }}
+                  onChange={evt => this.handleSpaceNameChange(evt.target.value)}
+                  validationMessage={errorMessage}
+                />
+                {invalidName && <p className="cfnext-form__field-error">Invalid name</p>}
+
+                <TemplateSelector
+                  onSelect={setNewSpaceTemplate}
+                  onToggle={() => this.reposition()}
+                  templates={templates}
+                  fetchTemplates={fetchTemplates}
+                  formAlign="left"
+                />
+              </React.Fragment>
+            )}
+            {!showForm && (
+              <React.Fragment>
+                {reachedLimit &&
+                  !isDisabled && (
+                    <p className="note-box--info">
+                      {`You've created ${limit} proof of concept spaces. Delete existing ones or talk to us if you need more.`}
+                    </p>
                   )}
-                  <ContactUsButton buttonType="button" noIcon>
-                    Talk to us
-                  </ContactUsButton>
-                </React.Fragment>
-              )}
-            </Dialog.Body>
-          )}
+                {isDisabled && (
+                  <p className="note-box--info">{`You can't create proof of concept spaces because they're not a part of your enterprise deal with Contentful.
+                  Get in touch with us if you want to create new spaces.`}</p>
+                )}
+                <ContactUsButton buttonType="button" noIcon>
+                  Talk to us
+                </ContactUsButton>
+              </React.Fragment>
+            )}
+          </Dialog.Body>
+        )}
         {showForm && (
           <Dialog.Controls>
             <button
@@ -222,79 +221,6 @@ class EnterpriseSpaceWizard extends React.Component {
           </Dialog.Controls>
         )}
       </Dialog>
-    );
-  }
-}
-
-function Plan({ resources, roleSet, usage, limit, isDisabled, reachedLimit }) {
-  return (
-    <div
-      className={`
-        space-plans-list__item
-        space-plans-list__item--proof-of-concept
-        ${(isDisabled || reachedLimit) && 'space-plans-list__item--disabled'}
-      `}
-      style={{ marginBottom: '30px' }}>
-      <div className="space-plans-list__item__heading">
-        <strong data-test-id="space-plan-name">Proof of concept</strong>
-        <span data-test-id="space-plan-price">
-          {' '}
-          - {isDisabled ? 'unavailable' : `${usage}/${limit} used`}
-        </span>
-      </div>
-      <PlanFeatures resources={resources} roleSet={roleSet} />
-    </div>
-  );
-}
-Plan.propTypes = {
-  resources: PropTypes.array.isRequired,
-  roleSet: PropTypes.object.isRequired,
-  usage: PropTypes.number.isRequired,
-  limit: PropTypes.number.isRequired,
-  isDisabled: PropTypes.bool.isRequired,
-  reachedLimit: PropTypes.bool.isRequired
-};
-
-class Info extends React.Component {
-  state = {
-    showingMore: false
-  };
-
-  get showMoreText() {
-    return this.state.showingMore ? 'Show less' : 'Show more';
-  }
-
-  onShowMore() {
-    this.setState({ showingMore: !this.state.showingMore });
-  }
-
-  onLearnMore() {
-    window.open(
-      'https://www.contentful.com/pricing/?faq_category=payments&faq=what-is-a-proof-of-concept-poc-space#what-is-a-proof-of-concept-poc-space'
-    );
-  }
-
-  render() {
-    return (
-      <section style={{ marginBottom: '30px' }}>
-        <p className="enterprise-space-wizard__info" style={{ display: 'inline' }}>
-          {`Use a proof of concept space to experiment or start new projects. Talk to us when you decide to launch. `}
-        </p>
-        {this.state.showingMore && (
-          <div style={{ marginTop: '20px' }}>
-            <p style={{ marginBottom: '20px' }}>
-              {`A proof of concept space is free of charge until you decide to use it for a live application.
-                We can then help you to convert it to a regular production space.`}
-            </p>
-            <p style={{ marginBottom: '20px' }}>
-              {`Proof of concept spaces share the same limits for API requests and asset bandwidth with the
-                other spaces in your organization. `}
-              <TextLink onClick={() => this.onLearnMore()}>Learn more</TextLink>
-            </p>
-          </div>
-        )}
-        <TextLink onClick={() => this.onShowMore()}>{this.showMoreText}</TextLink>
-      </section>
     );
   }
 }
---
+++ src/javascripts/components/shared/enterprise-space-wizard/EnterpriseSpaceWizardInfo.es6.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { TextLink } from '@contentful/ui-component-library';
+
+export default class EnterpriseSpaceWizardInfo extends React.Component {
+  state = {
+    showingMore: false
+  };
+
+  get showMoreText() {
+    return this.state.showingMore ? 'Show less' : 'Show more';
+  }
+
+  onShowMore() {
+    this.setState({ showingMore: !this.state.showingMore });
+  }
+
+  onLearnMore() {
+    window.open(
+      'https://www.contentful.com/pricing/?faq_category=payments&faq=what-is-a-proof-of-concept-poc-space#what-is-a-proof-of-concept-poc-space'
+    );
+  }
+
+  render() {
+    return (
+      <section style={{ marginBottom: '30px' }}>
+        <p className="enterprise-space-wizard__info" style={{ display: 'inline' }}>
+          {`Use a proof of concept space to experiment or start new projects. Talk to us when you decide to launch. `}
+        </p>
+        {this.state.showingMore && (
+          <div style={{ marginTop: '20px' }}>
+            <p style={{ marginBottom: '20px' }}>
+              {`A proof of concept space is free of charge until you decide to use it for a live application.
+                We can then help you to convert it to a regular production space.`}
+            </p>
+            <p style={{ marginBottom: '20px' }}>
+              {`Proof of concept spaces share the same limits for API requests and asset bandwidth with the
+                other spaces in your organization. `}
+              <TextLink onClick={() => this.onLearnMore()}>Learn more</TextLink>
+            </p>
+          </div>
+        )}
+        <TextLink onClick={() => this.onShowMore()}>{this.showMoreText}</TextLink>
+      </section>
+    );
+  }
+}
