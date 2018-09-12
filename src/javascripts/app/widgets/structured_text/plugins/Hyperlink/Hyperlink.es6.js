0de5e27db32a14b3ef695faee95c05ff90a28187
Mike Mitchell <omg@lol.xxx>
2018-09-12 14:22:56 +0200

    Tidy appearance of hyperlink within Structured Text editor (#3319)
    Update appearance of Structured Text Hyperlink feature.
    
    * Tidy appearance of hyperlink within Structured Text editor
    
    * Add TODO for fixing text cursor bug in slate.js
    
    * Bump component library version to fix link styling
    
    * Tidy hyperlink styling
    
    * Bump component library version

--- src/javascripts/app/widgets/structured_text/plugins/Hyperlink/Hyperlink.es6.js
+++ src/javascripts/app/widgets/structured_text/plugins/Hyperlink/Hyperlink.es6.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Icon, Tooltip } from '@contentful/ui-component-library';
+import { Icon, Tooltip, TextLink } from '@contentful/ui-component-library';
 import PropTypes from 'prop-types';
 
 export default class Hyperlink extends React.Component {
@@ -17,20 +17,18 @@ export default class Hyperlink extends React.Component {
 
     return (
       <span>
-        <a data-tip={uri} title={title}>
+        <TextLink
+          href="javascript:void(0)"
+          data-tip={uri}
+          title={title}
+          extraClassNames="structured-text__hyperlink">
           {children}
-          <Icon icon="ExternalLink" style={{ position: 'relative', top: '3px' }} />
-        </a>
+          <Icon icon="ExternalLinkTrimmed" extraClassNames="structured-text__hyperlink-icon" />
+        </TextLink>
+
         <Tooltip show={true} />
       </span>
-      // TODO: Use TextLink instead, this messes up cursor behavior though,
-      //  apparently because of `display: inline-flex`.
-      //  See bug report: https://github.com/ianstormtaylor/slate/issues/2148
-      //
-      //<TextLink href="javascript:void(0)" disabled={false} data-tip={uri}>
-      //  {children}
-      //  <Icon icon="ExternalLink" />
-      //</TextLink>
+      // TODO: Add contentEditable={false} to tooltip to fix text cursor bug
     );
   }
 }
