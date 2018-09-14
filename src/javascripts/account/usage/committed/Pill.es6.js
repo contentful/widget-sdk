6b8628a705a44f10fb872ac1ef45dca774d57b55
markuslautenbach <markus.lautenbach@contentful.com>
2018-09-14 17:23:06 +0200

    [MOI-282]: Use tooltip component
    The one for percentage must have gotten lost during a rebase.

--- src/javascripts/account/usage/committed/Pill.es6.js
+++ src/javascripts/account/usage/committed/Pill.es6.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Tooltip } from '@contentful/ui-component-library';
 
 export default class Pill extends React.Component {
   static propTypes = {
@@ -14,9 +15,12 @@ export default class Pill extends React.Component {
   render() {
     const { text, tooltip } = this.props;
     return (
-      <span className="pill" title={tooltip || ''}>
-        {text}
-      </span>
+      <React.Fragment>
+        <span className="pill" data-tip={tooltip || ''}>
+          {text}
+        </span>
+        <Tooltip />
+      </React.Fragment>
     );
   }
 }
