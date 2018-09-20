e44291f72ff5e98d522e12271040c9a6806062fe
Alex Suevalov <alex.suevalov@contentful.com>
2018-09-20 13:58:41 +0200

    Covert JSX icons to svg
    fixup! Covert JSX icons to svg

--- src/javascripts/svg/dd-arrow-down-disabled.es6.js
+++ src/javascripts/svg/dd-arrow-down-disabled.es6.js
@@ -1,36 +1,13 @@
-import { h } from 'ui/Framework';
+import React from 'react';
 
-export default h(
-  'svg',
-  {
-    width: '12',
-    height: '8',
-    viewBox: '-1 -1 14 10',
-    xmlns: 'http://www.w3.org/2000/svg'
-  },
-  [
-    h(
-      'g',
-      {
-        fill: 'none',
-        fillRule: 'evenodd'
-      },
-      [
-        h(
-          'g',
-          {
-            fill: '#9B9B9B'
-          },
-          [
-            h('g', [
-              h('path', {
-                d:
-                  'M11.11 1.917a.45.45 0 0 0 0-.635L9.952.13a.442.442 0 0 0-.628 0L5.62 3.835 1.917.13a.442.442 0 0 0-.628 0L.13 1.28a.45.45 0 0 0 0 .636l5.176 5.168a.442.442 0 0 0 .627 0l5.176-5.168z'
-              })
-            ])
-          ]
-        )
-      ]
-    )
-  ]
+export default () => (
+  <svg width="12" height="8" viewBox="-1 -1 14 10" xmlns="http://www.w3.org/2000/svg">
+    <g fill="none" fillRule="evenodd">
+      <g fill="#9B9B9B">
+        <g>
+          <path d="M11.11 1.917a.45.45 0 0 0 0-.635L9.952.13a.442.442 0 0 0-.628 0L5.62 3.835 1.917.13a.442.442 0 0 0-.628 0L.13 1.28a.45.45 0 0 0 0 .636l5.176 5.168a.442.442 0 0 0 .627 0l5.176-5.168z" />
+        </g>
+      </g>
+    </g>
+  </svg>
 );
--- src/javascripts/svg/dotted-border.es6.js
+++ src/javascripts/svg/dotted-border.es6.js
@@ -1,18 +1,7 @@
-import { h } from 'ui/Framework';
+import React from 'react';
 
-export default h(
-  'svg',
-  {
-    xmlns: 'http://www.w3.org/2000/svg',
-    width: '9',
-    height: '3',
-    viewBox: '-1 -1 11 5'
-  },
-  [
-    h('path', {
-      d: 'M0 2.5h9',
-      stroke: '#424b57',
-      strokeDasharray: '1,3'
-    })
-  ]
+export default () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="9" height="3" viewBox="-1 -1 11 5">
+    <path d="M0 2.5h9" stroke="#424b57" strokeDasharray="1,3" />
+  </svg>
 );
