a2114375a28d5163c3cfd2b5a89a9008150084f2
Daniel Werner <daniel.werner@contentful.com>
2018-09-24 17:39:33 +0200

    Delete `FetchThumbnail`
    All its functionality is not covered in FetchEntity.

--- src/javascripts/app/widgets/structured_text/plugins/shared/FetchThumbnail/index.es6.js
+++
@@ -1,4 +0,0 @@
-import FetchEntry from './FetchThumbnail.es6';
-const ServicesConsumer = require('../../../../../../reactServiceContext').default;
-
-export default ServicesConsumer('spaceContext')(FetchEntry);
