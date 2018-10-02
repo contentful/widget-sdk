29c434a9f12ef339690f7cf6cda214c3598e36be
Joshua Smock <joshua@contentful.com>
2018-10-02 13:50:57 +0200

    MOI-319 Send old and new periods during tracking

--- src/javascripts/account/usage/OrganizationUsage.es6.js
+++ src/javascripts/account/usage/OrganizationUsage.es6.js
@@ -132,9 +132,9 @@ export default class OrganizationUsage extends React.Component {
 
   async loadPeriodData(newIndex) {
     const { periods } = this.state;
-    const { sys: newPeriod } = periods[newIndex];
+    const newPeriod = periods[newIndex];
     if (isNumber(this.state.selectedPeriodIndex)) {
-      const { sys: oldPeriod } = periods[this.state.selectedPeriodIndex];
+      const oldPeriod = periods[this.state.selectedPeriodIndex];
       track('usage:period_selected', {
         oldPeriod: pick(['startDate', 'endDate'], oldPeriod),
         newPeriod: pick(['startDate', 'endDate'], newPeriod)
@@ -142,8 +142,8 @@ export default class OrganizationUsage extends React.Component {
     }
     try {
       const [org, cma, cda, cpa] = await Promise.all([
-        getOrgUsage(this.endpoint, newPeriod.id),
-        ...['cma', 'cda', 'cpa'].map(api => getApiUsage(this.endpoint, newPeriod.id, api))
+        getOrgUsage(this.endpoint, newPeriod.sys.id),
+        ...['cma', 'cda', 'cpa'].map(api => getApiUsage(this.endpoint, newPeriod.sys.id, api))
       ]);
       this.setState({
         isLoading: false,
