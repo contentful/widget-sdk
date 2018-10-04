282572f6bc32e12f41b5be33647000d11dc3390c
markuslautenbach <markus.lautenbach@contentful.com>
2018-10-04 16:39:37 +0200

    Add type in PropType and reuse prop type helper

--- src/javascripts/account/usage/committed/propTypes.es6.js
+++ src/javascripts/account/usage/committed/propTypes.es6.js
@@ -3,30 +3,24 @@ import PropTypes from 'prop-types';
 const arrayPropType = itemType =>
   PropTypes.shape({
     total: PropTypes.number,
-    sys: PropTypes.shape({ type: PropTypes.oneOf(['Array']) }),
+    sys: PropTypes.shape({ type: PropTypes.oneOf(['Array']), id: PropTypes.string }),
     items: PropTypes.arrayOf(itemType)
   });
 
 const periodPropType = PropTypes.shape({
   sys: PropTypes.shape({
-    type: PropTypes.string,
+    type: PropTypes.oneOf(['UsagePeriod']),
     id: PropTypes.string
   }),
   startDate: PropTypes.string,
   endDate: PropTypes.string
 });
 
-const organizationUsagePropType = PropTypes.shape({
-  sys: PropTypes.shape({
-    type: PropTypes.oneOf(['Array']),
-    id: PropTypes.string
-  }),
-  items: PropTypes.arrayOf(
-    PropTypes.shape({
-      usage: PropTypes.arrayOf(PropTypes.number)
-    })
-  )
-});
+const organizationUsagePropType = arrayPropType(
+  PropTypes.shape({
+    usage: PropTypes.arrayOf(PropTypes.number)
+  })
+);
 
 const organizationResourceUsagePropType = PropTypes.shape({
   sys: PropTypes.shape({
