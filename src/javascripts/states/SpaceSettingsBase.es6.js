3b0de96aa0e19e04e5b3b49ef927abb00c0be1fa
Joshua Smock <joshua@contentful.com>
2018-10-19 15:52:51 +0200

    MOI-353 Correctly redirect settings with controllers that have side effects

--- src/javascripts/states/SpaceSettingsBase.es6.js
+++ src/javascripts/states/SpaceSettingsBase.es6.js
@@ -2,25 +2,32 @@ import Base from 'states/Base.es6';
 import { go } from 'states/Navigator.es6';
 import * as TokenStore from 'services/TokenStore.es6';
 
-export async function redirectReadOnlySpace(spaceId) {
-  const _space = await TokenStore.getSpace(spaceId);
-
-  // if (org.readOnlyAt) {
-  go({
-    path: ['home']
-  });
-  // }
+export function redirectReadOnlySpace(space) {
+  if (space.readOnlyAt) {
+    go({
+      path: ['home']
+    });
+  }
 }
 
 export default function SpaceSettingsBase(definition) {
   const defaults = {
-    onEnter: [
-      '$stateParams',
-      async $stateParams => {
-        const spaceId = $stateParams.spaceId;
-        return await redirectReadOnlySpace(spaceId);
-      }
-    ]
+    resolve: {
+      space: [
+        '$q',
+        '$stateParams',
+        async function($q, $stateParams) {
+          const deferred = $q.defer();
+          const spaceId = $stateParams.spaceId;
+
+          const _space = await TokenStore.getSpace(spaceId);
+          deferred.resolve(_space);
+
+          return deferred.promise;
+        }
+      ]
+    },
+    onEnter: ['space', space => redirectReadOnlySpace(space)]
   };
 
   return Base(Object.assign(defaults, definition));
