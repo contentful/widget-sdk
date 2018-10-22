1fa05ed4330430f2c43924d6a324a5fb2613269a
Joshua Smock <joshua@contentful.com>
2018-10-22 16:27:51 +0200

    MOI-355 Show read-only homepage if space is read-only

--- src/javascripts/app/home/HomeTemplate.es6.js
+++ src/javascripts/app/home/HomeTemplate.es6.js
@@ -2,7 +2,7 @@ import { h } from 'utils/legacy-html-hyperscript';
 
 export default function template() {
   return h('.home', { 'cf-ui-sticky-container': true }, [
-    h('.home__container', [
+    h('.home__container', { ngIf: '!readOnlySpace' }, [
       h('.home__content', [
         h('cf-welcome'),
         h('react-component', { name: 'app/home/UpgradePricing.es6' }),
@@ -10,6 +10,9 @@ export default function template() {
         h('cf-developer-resources'),
         h('cf-contact-us-space-home')
       ])
+    ]),
+    h('.home__container .home__container-read-only', { ngIf: 'readOnlySpace' }, [
+      h('.home__content', [h('div', ['Your space is read only!'])])
     ])
   ]);
 }
--- src/javascripts/states/space_home.js
+++ src/javascripts/states/space_home.js
@@ -12,18 +12,24 @@ angular
       var base = require('states/Base.es6').default;
       var accessChecker = require('access_control/AccessChecker');
       var template = require('app/home/HomeTemplate.es6').default;
+      var spaceResolver = require('states/Resolvers.es6').spaceResolver;
 
       return base({
         name: 'home',
         url: '/home',
         label: 'Space home',
+        resolve: {
+          space: spaceResolver
+        },
         template: template(),
         loadingText: 'Loading…',
         controller: [
           '$scope',
-          $scope => {
+          'space',
+          ($scope, space) => {
             $scope.context.ready = true;
             $scope.context.forbidden = !accessChecker.getSectionVisibility().spaceHome;
+            $scope.readOnlySpace = Boolean(space.readOnlyAt);
           }
         ]
       });
