ee29638ca83d95c9f3288b5ee814116afa87e504
Joshua Smock <joshua@contentful.com>
2018-11-02 15:50:49 +0100

    Turn on sourcemap support for bundled files in Karma

--- karma.conf.js
+++ karma.conf.js
@@ -51,7 +51,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/**/*.js': ['babelTest', 'sourcemap']
+      'test/**/*.js': ['babelTest', 'sourcemap'],
+      'public/app/*.js': ['sourcemap']
     },
 
     customPreprocessors: {
