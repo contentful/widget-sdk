6015eb1864b9965648392adbe2f799ef92ea4f59
Markus Lautenbach <markus.lautenbach@contentful.com>
2018-11-16 15:57:16 +0100

    Handle tests where kefir might not be available

--- src/javascripts/ReduxStore/store.es6.js
+++ src/javascripts/ReduxStore/store.es6.js
@@ -1,7 +1,7 @@
 import { user$, organizations$, spacesByOrganization$ } from 'services/TokenStore.es6';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { onValue } from '../utils/kefir.es6';
+import * as kefir from '../utils/kefir.es6';
 
 import combinedReducers from './reducers.es6';
 
@@ -9,23 +9,25 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(combinedReducers, composeEnhancers(applyMiddleware(thunk)));
 
-onValue(user$, user =>
-  store.dispatch({
-    type: 'USER_UPDATE_FROM_TOKEN',
-    payload: { user }
-  })
-);
-onValue(organizations$, organization =>
-  store.dispatch({
-    type: 'ORGANIZATIONS_UPDATE_FROM_TOKEN',
-    payload: { organization }
-  })
-);
-onValue(spacesByOrganization$, spaces =>
-  store.dispatch({
-    type: 'SPACES_BY_ORG_UPDATE_FROM_TOKEN',
-    payload: { spaces }
-  })
-);
+if (kefir && kefir.onValue) {
+  kefir.onValue(user$, user =>
+    store.dispatch({
+      type: 'USER_UPDATE_FROM_TOKEN',
+      payload: { user }
+    })
+  );
+  kefir.onValue(organizations$, organization =>
+    store.dispatch({
+      type: 'ORGANIZATIONS_UPDATE_FROM_TOKEN',
+      payload: { organization }
+    })
+  );
+  kefir.onValue(spacesByOrganization$, spaces =>
+    store.dispatch({
+      type: 'SPACES_BY_ORG_UPDATE_FROM_TOKEN',
+      payload: { spaces }
+    })
+  );
+}
 
 export default store;
