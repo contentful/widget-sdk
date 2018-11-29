1e8388a1f307a18919cba795a12418e1482f586d
Markus Lautenbach <markus.lautenbach@contentful.com>
2018-11-29 18:20:58 +0100

    Remove unused attribute

--- src/javascripts/app/OrganizationSettings/Users/UsersState.es6.js
+++ src/javascripts/app/OrganizationSettings/Users/UsersState.es6.js
@@ -6,7 +6,6 @@ export { default as userDetailState } from './UserDetail/UserDetailState.es6';
 export default {
   name: 'users',
   children: [organizationBase(NewUserState)],
-  title: 'Organization users',
   url: '/:orgId/organization_memberships',
   featureFlag: 'feature-bv-09-2018-new-org-membership-pages',
   reactComponentName: 'app/OrganizationSettings/Users/UsersList/UserListRoute.es6'
