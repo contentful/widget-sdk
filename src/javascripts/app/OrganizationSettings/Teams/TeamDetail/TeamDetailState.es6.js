65b1986e8bee5f0332e81fd38ae8a59d36035896
Guilherme Barbosa <guipvb@gmail.com>
2018-12-03 16:23:25 +0100

    Move Subscription, Users, Usage and Teams to the new route structure

--- src/javascripts/app/OrganizationSettings/Teams/TeamDetail/TeamDetailState.es6.js
+++
@@ -1,8 +0,0 @@
-import { reactStateWrapper } from 'app/OrganizationSettings/OrganizationSettingsRouteUtils.es6';
-
-export default reactStateWrapper({
-  name: 'detail',
-  title: 'Teams',
-  url: '/:teamId',
-  componentPath: 'app/OrganizationSettings/Teams/TeamDetail/TeamDetailRoute.es6'
-});
