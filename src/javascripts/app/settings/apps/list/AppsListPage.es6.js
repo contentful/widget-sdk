42caa83d63e066b978b4292a54615e66238ad36c
Jakub Elżbieciak <jakub@contentful.com>
2018-12-05 09:49:26 +0100

    Add early access note

--- src/javascripts/app/settings/apps/list/AppsListPage.es6.js
+++ src/javascripts/app/settings/apps/list/AppsListPage.es6.js
@@ -4,6 +4,7 @@ import ContentLoader from 'react-content-loader';
 import Workbench from 'app/common/Workbench.es6';
 import AppsList from './AppsList.es6';
 import AppListItem from './AppListItem.es6';
+import { Note, TextLink } from '@contentful/forma-36-react-components';
 
 const AppsListShell = props => (
   <Workbench>
@@ -23,69 +24,95 @@ const AppsListShell = props => (
   </Workbench>
 );
 
+const FakeListContent = () => (
+  <ContentLoader height={200} width={500} ariaLabel="Loading apps list...">
+    <rect x="0" y="0" rx="2" ry="2" width="100" height="10" />
+    <circle cx="15" cy="55" r="15" />
+    <rect x="45" y="52" rx="2" ry="2" width="200" height="6" />
+    <rect x="430" y="52" rx="2" ry="2" width="70" height="6" />
+
+    <circle cx="15" cy="95" r="15" />
+    <rect x="45" y="92" rx="2" ry="2" width="200" height="6" />
+    <rect x="430" y="92" rx="2" ry="2" width="70" height="6" />
+
+    <circle cx="15" cy="135" r="15" />
+    <rect x="45" y="132" rx="2" ry="2" width="200" height="6" />
+    <rect x="430" y="132" rx="2" ry="2" width="70" height="6" />
+  </ContentLoader>
+);
+
 export const AppsListPageLoading = () => (
   <AppsListShell>
-    <ContentLoader height={200} width={500} ariaLabel="Loading apps list...">
-      <rect x="0" y="0" rx="2" ry="2" width="100" height="10" />
-      <circle cx="15" cy="55" r="15" />
-      <rect x="45" y="52" rx="2" ry="2" width="200" height="6" />
-      <rect x="430" y="52" rx="2" ry="2" width="70" height="6" />
-
-      <circle cx="15" cy="95" r="15" />
-      <rect x="45" y="92" rx="2" ry="2" width="200" height="6" />
-      <rect x="430" y="92" rx="2" ry="2" width="70" height="6" />
-
-      <circle cx="15" cy="135" r="15" />
-      <rect x="45" y="132" rx="2" ry="2" width="200" height="6" />
-      <rect x="430" y="132" rx="2" ry="2" width="70" height="6" />
-    </ContentLoader>
+    <FakeListContent />
   </AppsListShell>
 );
 
+const appGroupPropType = PropTypes.arrayOf(
+  PropTypes.shape({
+    id: PropTypes.string.isRequired
+  })
+).isRequired;
+
 export default class AppsListPage extends Component {
   static propTypes = {
-    apps: PropTypes.array.isRequired
+    apps: PropTypes.shape({
+      installed: appGroupPropType,
+      available: appGroupPropType
+    }).isRequired
   };
 
-  state = {
-    installed: [],
-    available: []
-  };
-
-  static getDerivedStateFromProps(props) {
-    const installed = [];
-    const available = [];
-    props.apps.forEach(app => {
-      if (app.installed) {
-        installed.push(app);
-      } else {
-        available.push(app);
-      }
-    });
-    return {
-      installed,
-      available
-    };
+  constructor(props) {
+    super(props);
+    this.state = { optedIn: props.apps.installed.length > 0 };
   }
 
   render() {
     return (
-      <AppsListShell>
-        {this.state.installed.length > 0 && (
+      <AppsListShell>{this.state.optedIn ? this.renderApps() : this.renderOptIn()}</AppsListShell>
+    );
+  }
+
+  renderOptIn() {
+    return (
+      <React.Fragment>
+        <Note
+          extraClassNames="netlify-app__early-access"
+          noteType="primary"
+          title="Early Access Program">
+          <p>
+            Apps are under active development right now and should not be used in production systems
+            yet. We decided to make them available to interested parties but we warn you: it may be
+            broken.
+          </p>
+          <TextLink onClick={() => this.setState({ optedIn: true })} icon="ThumbUp">
+            I want to join the Early Access Program
+          </TextLink>
+        </Note>
+        <FakeListContent />
+      </React.Fragment>
+    );
+  }
+
+  renderApps() {
+    const { apps } = this.props;
+
+    return (
+      <React.Fragment>
+        {apps.installed.length > 0 && (
           <AppsList title="Installed">
-            {this.state.installed.map(app => (
+            {apps.installed.map(app => (
               <AppListItem key={app.id} app={app} />
             ))}
           </AppsList>
         )}
-        {this.state.available.length > 0 && (
+        {apps.available.length > 0 && (
           <AppsList title="Available">
-            {this.state.available.map(app => (
+            {apps.available.map(app => (
               <AppListItem key={app.id} app={app} />
             ))}
           </AppsList>
         )}
-      </AppsListShell>
+      </React.Fragment>
     );
   }
 }
--- src/javascripts/app/settings/apps/routes/AppsListRoute.es6.js
+++ src/javascripts/app/settings/apps/routes/AppsListRoute.es6.js
@@ -6,8 +6,19 @@ import createAppsClient from '../AppsClient.es6';
 import StateRedirect from 'app/common/StateRedirect.es6';
 import createFetcherComponent from 'app/common/createFetcherComponent.es6';
 
-const AppsFetcher = createFetcherComponent(({ spaceId }) => {
-  return createAppsClient(spaceId).getAll();
+const AppsFetcher = createFetcherComponent(async ({ spaceId }) => {
+  const apps = await createAppsClient(spaceId).getAll();
+
+  return apps.reduce(
+    ({ installed, available }, app) => {
+      if (app.installed) {
+        return { installed: installed.concat([app]), available };
+      } else {
+        return { installed, available: available.concat([app]) };
+      }
+    },
+    { installed: [], available: [] }
+  );
 });
 
 export default class AppsListRoute extends Component {
