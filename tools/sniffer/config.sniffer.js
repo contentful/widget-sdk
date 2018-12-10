b571b9646e677c0a799b2a4e0c492c5571d1cac5
Alex Suevalov <alex.suevalov@contentful.com>
2018-12-10 13:15:47 +0100

    Moved roots to config file

--- tools/sniffer/config.sniffer.js
+++ tools/sniffer/config.sniffer.js
@@ -2,6 +2,7 @@ const analyzers = require('./analyzers');
 const attributes = require('./attributes');
 
 module.exports = {
+  roots: ['src/javascripts', 'test'],
   analyzers: analyzers,
   attributes: attributes,
   exclude: /(node_modules|\.git|\.DS_Store|\.js\.snap)/
