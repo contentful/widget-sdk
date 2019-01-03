662288317ad997ec3ab8c1d0164de4de9feaa9c3
Daniel Werner <daniel.werner@contentful.com>
2019-01-03 14:45:46 +0100

    Disable verbose jest output
    When running only a few tests, jest by default uses verbose output.
    This though comes with a bug that might swallow some `console.log()`
    output in `async` tests.
    See https://github.com/facebook/jest/issues/2441 for detail.

--- tools/testing/create-jest-config.js
+++ tools/testing/create-jest-config.js
@@ -7,6 +7,9 @@ module.exports = (resolve, rootDir, srcRoots, coverageDirectory) => {
   const config = {
     setupTestFrameworkScriptFile: resolve('tools/testing/setup-tests.js'),
     testMatch: ['**/?(*.)(spec|test).{js,jsx}'],
+    // verbose output swallows some console.log() output in some async tests.
+    // See: https://github.com/facebook/jest/issues/2441
+    verbose: false,
     // where to search for files/tests
     roots: srcRoots.map(toRelRootDir),
     collectCoverageFrom: ['**/*.js'],
