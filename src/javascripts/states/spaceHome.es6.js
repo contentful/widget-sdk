b9f05cc53ab10d4ae74850b3f775cffb26b512ad
Tatiana Bessonova <tatiana.bessonova@contentful.com>
2019-01-08 10:13:33 +0100

    Sync branch to master

--- src/javascripts/states/spaceHome.es6.js
+++ src/javascripts/states/spaceHome.es6.js
@@ -1,17 +1,15 @@
 import { registerFactory } from 'NgRegistry.es6';
 import base from 'states/Base.es6';
 import homeTemplateDef from 'app/home/HomeTemplate.es6';
-import { isOwnerOrAdmin } from 'services/OrganizationRoles.es6';
+import { spaceHomeController } from 'states/SpaceHomeController.es6';
 
 /**
  * @ngdoc service
  * @name states/space_home
  */
 registerFactory('states/space_home', [
-  'access_control/AccessChecker/index.es6',
-  'Config.es6',
   'states/Resolvers.es6',
-  (accessChecker, Config, { spaceResolver }) => {
+  ({ spaceResolver }) => {
     return base({
       name: 'home',
       url: '/home',
@@ -21,23 +19,7 @@ registerFactory('states/space_home', [
       },
       template: homeTemplateDef(),
       loadingText: 'Loading…',
-      controller: [
-        '$scope',
-        'space',
-        ($scope, space) => {
-          $scope.context.ready = true;
-          $scope.context.forbidden = !accessChecker.getSectionVisibility().spaceHome;
-          $scope.readOnlySpace = Boolean(space.readOnlyAt);
-
-          const spaceId = space.sys.id;
-          const spaceName = space.name;
-          $scope.supportUrl = `${
-            Config.supportUrl
-          }?read-only-space=true&space-id=${spaceId}&space-name=${spaceName}`;
-
-          $scope.orgOwnerOrAdmin = isOwnerOrAdmin(space.organization);
-        }
-      ]
+      controller: spaceHomeController
     });
   }
 ]);
