d1539b0a691049bf19f9290d7cf3c1f1905691b4
Tatiana Bessonova <tatiana.bessonova@contentful.com>
2019-01-10 11:09:16 +0100

    PoC youtube and wistia

--- src/javascripts/app/home/AuthorEditorSpaceHome.es6.js
+++ src/javascripts/app/home/AuthorEditorSpaceHome.es6.js
@@ -7,6 +7,26 @@ export default class SpaceHome extends React.Component {
     return (
       <WidgetContainer>
         <GreetingWidget {...this.props} />
+        <iframe
+          allowTransparency="true"
+          title="Wistia video player"
+          allowFullscreen
+          frameBorder="0"
+          scrolling="no"
+          className="wistia_embed"
+          name="wistia_embed"
+          src="https://fast.wistia.net/embed/iframe/uvvjsg6wdo"
+          width="400"
+          height="225"
+        />
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/sVCgDiU1WHc"
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
       </WidgetContainer>
     );
   }
