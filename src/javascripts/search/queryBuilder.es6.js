9b3f553e83a20606906201e560405c3b1d34c97e
Jakub Elżbieciak <jakub@contentful.com>
2019-01-16 16:35:23 +0100

    Guard parsning, never throw

--- src/javascripts/search/queryBuilder.es6.js
+++ src/javascripts/search/queryBuilder.es6.js
@@ -1,4 +1,4 @@
-import parseTokens from './legacySearchParser.es6';
+import parse from './legacySearchParser.es6';
 
 export function parseTextQuery(textQuery) {
   const tokens = parseTokens(textQuery);
@@ -7,9 +7,18 @@ export function parseTextQuery(textQuery) {
   return { filters, queryText };
 }
 
+function parseTokens(textQuery) {
+  try {
+    const tokens = parse(textQuery);
+    return Array.isArray(tokens) ? tokens : [];
+  } catch (err) {
+    return [];
+  }
+}
+
 // Returns a list of [key, operator, value] pairs from tokens
 function getFilters(tokens) {
-  const pairs = (tokens || []).filter(token => {
+  const pairs = tokens.filter(token => {
     return token.type === 'Pair' && token.content.value.length > 0;
   });
 
@@ -20,7 +29,7 @@ function getFilters(tokens) {
 
 // Takes all tokens that are queries and joins them with spaces
 function getQueryText(tokens) {
-  const queries = (tokens || []).filter(token => {
+  const queries = tokens.filter(token => {
     return token.type === 'Query' && token.content.length > 0;
   });
 
