3fcf047156977fa3ec17920a9de989fd70529500
Joshua Smock <joshua@contentful.com>
2019-01-16 19:33:18 +0100

    MOI-514 WIP add empty test specs

---
+++ src/javascripts/app/OrganizationSettings/SSO/IDPSetupForm.spec.js
@@ -0,0 +1,13 @@
+// import IDPSetupForm from './IDPSetupForm.es6';
+
+describe('IDPSetupForm', () => {
+  it('should provide a default SSO name if no SSO name is present in identityProvider', () => {});
+
+  it('should allow users to input their own identity provider name if not in the list', () => {});
+
+  it('should update a field every 500ms on change', () => {});
+
+  it('should update a field immediately if input is blurred', () => {});
+
+  it('should gracefully handle failed update attempts on the network', () => {});
+});
---
+++ src/javascripts/app/OrganizationSettings/SSO/SSOSetup.spec.js
@@ -0,0 +1,9 @@
+// import SSOSetup from './SSOSetup.es6';
+
+describe('SSOSetup', () => {
+  it('should render the IDPSetupForm component if an identityProvider is given in props', () => {});
+
+  it('should not render the IDPSetupForm component if an identityProvider is not given in props', () => {});
+
+  it('should attempt to create an identity provider if CTA button is clicked', () => {});
+});
