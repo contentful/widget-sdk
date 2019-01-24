88ca50351936b61c311e34367640338b0e6257b5
Mike Mitchell <mike.mitchell@contentful.com>
2019-01-24 16:52:18 +0100

    Create IE11 deprecation banner

---
+++ src/javascripts/components/shared/IE11DeprecationBanner.es6.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Note, TextLink } from '@contentful/forma-36-react-components';
+
+export default class IE11DeprecationBanner extends React.Component {
+  static propTypes = {
+    featureName: PropTypes.string.isRequired,
+    noteType: PropTypes.string,
+    extraClassNames: PropTypes.string
+  };
+
+  static defaultProps = {
+    noteType: undefined,
+    extraClassNames: undefined
+  };
+
+  render() {
+    const { featureName, noteType, extraClassNames } = this.props;
+
+    return (
+      <Note noteType={noteType} extraClassNames={extraClassNames}>
+        The {featureName} is not supported in Internet Explorer 11. For more information{' '}
+        <TextLink href="#">see the FAQ</TextLink>
+      </Note>
+    );
+  }
+}
