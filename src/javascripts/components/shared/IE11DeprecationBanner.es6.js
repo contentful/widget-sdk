d9705ba1a41a4f7ade360da1852cd1e0fe0c5631
Mike Mitchell <mike.mitchell@contentful.com>
2019-01-24 17:32:17 +0100

    Add IE11 support message on entry editor

--- src/javascripts/components/shared/IE11DeprecationBanner.es6.js
+++ src/javascripts/components/shared/IE11DeprecationBanner.es6.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Note, TextLink } from '@contentful/forma-36-react-components';
+import { detect as detectBrowser } from 'detect-browser';
 
 export default class IE11DeprecationBanner extends React.Component {
   static propTypes = {
     featureName: PropTypes.string.isRequired,
     noteType: PropTypes.string,
-    extraClassNames: PropTypes.string
+    extraClassNames: PropTypes.string,
+    ie11Only: PropTypes.bool
   };
 
   static defaultProps = {
     noteType: undefined,
-    extraClassNames: undefined
+    extraClassNames: undefined,
+    ie11Only: false
   };
 
+  browserIsIE11() {
+    return detectBrowser().name === 'ie';
+  }
+
   render() {
-    const { featureName, noteType, extraClassNames } = this.props;
+    const { featureName, noteType, ie11Only, extraClassNames } = this.props;
+
+    if (ie11Only && !this.browserIsIE11()) {
+      return null;
+    }
 
     return (
       <Note noteType={noteType} extraClassNames={extraClassNames}>
