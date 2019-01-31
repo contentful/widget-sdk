537b648119d07e662591b47d77e70227521db87a
Markus Lautenbach <markus.lautenbach@contentful.com>
2019-01-31 16:08:07 +0100

    Try to fix build / tests

--- test/unit/navigation/sidepanel_directive_spec.js
+++ test/unit/navigation/sidepanel_directive_spec.js
@@ -42,6 +42,7 @@ describe('cfNavSidepanel directive', () => {
       $provide.value('utils/LaunchDarkly/index.es6', { onFeatureFlag: sinon.stub() });
       $provide.value('data/EndpointFactory.es6', { createSpaceEndpoint: sinon.stub() });
       $provide.value('data/CMA/SpaceEnvironmentsRepo.es6', { create: () => environmentsRepo });
+      $provide.value('data/CMA/FeatureCatalog.es6', { getOrgFeature: () => sinon.stub() });
     });
 
     const NavState = this.$inject('navigation/NavState.es6');
