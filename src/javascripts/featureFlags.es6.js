bc1e57bc0f286b6c586fd14637d3ac3a1b871806
Markus Lautenbach <markus.lautenbach@contentful.com>
2019-02-01 10:26:18 +0100

    Follow feature flag format

--- src/javascripts/featureFlags.es6.js
+++ src/javascripts/featureFlags.es6.js
@@ -8,4 +8,4 @@ export const UPGRADE_PRICING_FLAG = 'feature-dl-09-2018-upgrade-pricing-space-ho
 export const FEATURE_SERVICE_FLAG = 'feature-bv-2018-01-features-api';
 export const SSO_SELF_CONFIG_FLAG = 'feature-bv-2019-01-sso-self-config';
 export const TEAMS = 'feature-bv-11-2018-teams';
-export const TEAMS_FOR_MEMBERS = 'enabling-teams-for-org-members';
+export const TEAMS_FOR_MEMBERS = 'feature-bv-01-2019-teams-for-members';
