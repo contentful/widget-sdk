495e052b1b7d31f13608de0ffaff695e0b851499
Alexander Suevalov <suevalov.work@gmail.com>
2019-02-05 11:09:15 +0100

    Fixed broken localStorage investigation on IE11 (#3908)

--- src/javascripts/analytics/LocalStorageInvestigation.es6.js
+++ src/javascripts/analytics/LocalStorageInvestigation.es6.js
@@ -1,23 +1,26 @@
 import { fromPairs } from 'lodash';
 import { getModule } from 'NgRegistry.es6';
 
+const logger = getModule('logger');
+
 export function init() {
-  const logger = getModule('logger');
-
-  const tuples = Object.entries(window.localStorage);
-  const totalSize = new Blob(tuples).size;
-  // map values to their respective size in bytes
-  const withSizes = tuples.map(([key, value]) => [key, new Blob([key, value]).size]);
-
-  const warningMinSize = 1e6; // that's 1MB
-
-  // we only log it if the storage size is already too big
-  if (totalSize > warningMinSize) {
-    logger.logWarn('Local storage warning', {
-      data: {
-        rowSizes: fromPairs(withSizes),
-        totalSize
-      }
-    });
+  try {
+    const tuples = Object.entries(window.localStorage);
+    const totalSize = new Blob(tuples).size;
+    // map values to their respective size in bytes
+    const withSizes = tuples.map(([key, value]) => [key, new Blob([key, value]).size]);
+    const warningMinSize = 1e6; // that's 1MB
+    // we only log it if the storage size is already too big
+    if (totalSize > warningMinSize) {
+      logger.logWarn('Local storage warning', {
+        data: {
+          rowSizes: fromPairs(withSizes),
+          totalSize
+        }
+      });
+    }
+  } catch (e) {
+    // doesn't work in IE 11
+    // at least we tried
   }
 }
