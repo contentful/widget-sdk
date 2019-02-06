f7ac6de87d935534cf541f972bdccc7bde655816
Guilherme Barbosa <guipvb@gmail.com>
2019-02-06 15:19:30 +0100

    Add animated SVG

---
+++ src/javascripts/svg/wavy-background.es6.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const WavyBackground = props => (
+  <svg viewBox="0 0 1440 322" {...props}>
+    <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+      <g fill="#EBF5FF" fillRule="nonzero">
+        <path
+          d="M-3.29691829e-12,-3.12638804e-13 L1440,-3.12638804e-13 L1440,272 C1359.6875,258.666667 1234.8776,252 1065.57031,252 C811.609375,252 624,322 398.425781,322 C248.042969,322 115.234375,315.333333 -3.29691829e-12,302 L-3.29691829e-12,-3.12638804e-13 Z"
+          id="wavy-background"
+          transform="translate(720.000000, 161.000000) scale(-1, 1) translate(-720.000000, -161.000000) "
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+export default WavyBackground;
