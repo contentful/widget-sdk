bf1275167b40b658e37d4287a9e794c633d6556c
Sergii Bezliudnyi <serg.bezludny@gmail.com>
2019-02-25 09:49:34 +0100

    cleanup styles

--- src/javascripts/components/tabs/NoSearchResultsAdvice.es6.js
+++ src/javascripts/components/tabs/NoSearchResultsAdvice.es6.js
@@ -15,18 +15,22 @@ const styles = {
 export default function NoSearchResultsAdvice() {
   return (
     <div className={cn('advice', 'advice--row-aligned')}>
-      <Icon extraClassNames={styles.icon} icon="Search" size="large" color="muted" />
+      <Icon
+        extraClassNames={cn(styles.icon, 'f36-margin-right--s')}
+        icon="Search"
+        size="large"
+        color="muted"
+      />
       <div className={cn('advice__frame', 'advice__frame--no-margin', 'advice__frame--align-left')}>
-        <div className={cn('header', 'advice__title', 'advice__title--normal')}>
+        <div className={cn('advice__title', 'advice__title--normal')}>
           Sadly, we didn&#39;t find anything matching your search
         </div>
         <p className="advice__subtext">
-          Have you tried a different search term or filter? Here&#39;s
+          Have you tried a different search term or filter? Here&#39;s&nbsp;
           <a
             href="https://www.contentful.com/r/knowledgebase/content-search/"
             target="_blank"
             rel="noopener noreferrer">
-            {' '}
             how search works
           </a>
         </p>
