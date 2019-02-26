6c97abff4e5881dfc946270536b815eec1c8d616
Daniel A. R. Werner <daniel.a.r.werner@gmail.com>
2019-02-26 11:38:26 +0100

    Remove breadcrumb "..." from entry versions view
    The "..." is buggy as we do not use the legacy breadcrumbs anymore for
    the entry editors, instead we use slide-in.
    This has been buggy for a while so it makes sense to just entirely
    remove this. This was also the last occurence of this feature so it's
    gone entirel from our code base. We are still using the legacy
    breadcrumb "<" in some places (mostly settings views).

--- src/javascripts/navigation/Breadcrumbs/Template.es6.js
+++ src/javascripts/navigation/Breadcrumbs/Template.es6.js
@@ -14,75 +14,7 @@ export function template() {
           role: 'button',
           ariaLabel: 'breadcrumbs-back-btn',
           dataTestId: 'breadcrumbs-back-btn'
-        }),
-        h(
-          'div.breadcrumbs-ancestor-container',
-          {
-            ngIf: 'shouldShowHierarchy'
-          },
-          [
-            h('cf-icon.btn.btn__ancestor', {
-              name: 'breadcrumbs-icon-ancestors',
-              role: 'button',
-              ariaLabel: 'breadcrumbs-ancestor-btn',
-              dataTestId: 'breadcrumbs-ancestor-btn'
-            }),
-            h(
-              'div.breadcrumbs-ancestor-list',
-              {
-                ariaLabel: 'breadcrumbs-ancestor-menu-container',
-                dataTestId: 'breadcrumbs-ancestor-menu-container'
-              },
-              [
-                h('div.breadcrumbs-ancestor-list__heading', [
-                  h(
-                    'p',
-                    {
-                      role: 'note'
-                    },
-                    ['Go back to:']
-                  )
-                ]),
-                h(
-                  'ul.breadcrumbs-ancestor-list__items',
-                  {
-                    role: 'menu',
-                    ariaHidden: 'true',
-                    ariaLabel: 'breadcrumbs-ancestor-menu',
-                    dataTestId: 'breadcrumbs-ancestor-menu'
-                  },
-                  [
-                    h(
-                      'li',
-                      {
-                        ngRepeat: 'crumb in crumbs.slice(0, -1).reverse() track by crumb.id',
-                        role: 'menuitem',
-                        dataTestId: 'breadcrumbs-ancestor-menuitem'
-                      },
-                      [
-                        h(
-                          'a',
-                          {
-                            uiSref: '{{crumb.link.state}}({{crumb.link.params}})',
-                            role: 'link',
-                            dataTestId: 'breadcrumbs.crumb.{{crumb.id}}',
-                            title: "{{crumb.getTitle().length > 20 ? crumb.getTitle() : ''}}"
-                          },
-                          [
-                            h('cf-icon', {
-                              name: 'breadcrumbs-icon-{{crumb.icon}}'
-                            }),
-                            '{{crumb.getTitle()}}'
-                          ]
-                        )
-                      ]
-                    )
-                  ]
-                )
-              ]
-            )
-          ]
-        )
+        })
       ])
     ]
   );
