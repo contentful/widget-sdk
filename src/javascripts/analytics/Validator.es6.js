contentful/widget-sdk

commit 7e9782c6534ee4db9def72aaed6157242f49e7e9

Adopt updated schema nomenclature and clean up

src/javascripts/analytics/Validator.es6.js
@@ -58,18 +58,8 @@ export const validEvents = {
     'published_restored'
   ],
   content_preview: ['created', 'updated', 'deleted'],
-  slide_in_editor: [
-    'peek_click',
-    'arrow_back',
-    'bulk_editor_close',
-    'open',
-    'open_create',
-    'delete',
-    'load_init',
-    'load_sharejs_connected',
-    'load_links_resolved',
-    'load_fully_interactive'
-  ],
+  slide_in_editor: ['peek_click', 'arrow_back', 'open', 'open_create', 'delete'],
+  editor_load: ['init', 'sharejs_connected', 'links_rendered', 'fully_interactive'],
   // https://contentful.atlassian.net/wiki/display/PRODBulk+references+editor+-+Tracking+specs
   bulk_editor: [
     'add',

src/javascripts/analytics/snowplow/Events.es6.js
@@ -12,6 +12,7 @@ import { ClipboardCopyTransform, BoilerplateTransform } from './transformers/Api
 import AppOpen from './transformers/AppOpen.es6';
 import BulkEditor from './transformers/BulkEditor.es6';
 import SlideInEditor from './transformers/SlideInEditor.es6';
+import EditorLoad from './transformers/EditorLoad.es6';
 import Snapshot from './transformers/Snapshot.es6';
 import InviteUserExperiment from './transformers/InviteUserExperiment.es6';
 import SearchAndViews from './transformers/SearchAndViews.es6';
@@ -90,10 +91,11 @@ registerSlideInEditorEvent('slide_in_editor:bulk_editor_close');
 registerSlideInEditorEvent('slide_in_editor:open');
 registerSlideInEditorEvent('slide_in_editor:open_create');
 registerSlideInEditorEvent('slide_in_editor:delete');
-registerSlideInEditorEvent('slide_in_editor:load_init');
-registerSlideInEditorEvent('slide_in_editor:load_sharejs_connected');
-registerSlideInEditorEvent('slide_in_editor:load_links_resolved');
-registerSlideInEditorEvent('slide_in_editor:load_fully_interactive');
+
+registerEditorLoadEvent('editor_load:init');
+registerEditorLoadEvent('editor_load:sharejs_connected');
+registerEditorLoadEvent('editor_load:links_rendered');
+registerEditorLoadEvent('editor_load:fully_interactive');
 
 registerSnapshotEvent('versioning:no_snapshots');
 registerSnapshotEvent('versioning:snapshot_opened');
@@ -208,6 +210,10 @@ function registerBulkEditorEvent(event) {
 
 function registerSlideInEditorEvent(event) {
   registerEvent(event, 'slide_in_editor', SlideInEditor);
+}
+
+function registerEditorLoadEvent(event) {
+  registerEvent(event, 'editor_load', EditorLoad);
 }
 
 function registerSnapshotEvent(event) {

src/javascripts/analytics/snowplow/Schemas.es6.js
@@ -170,6 +170,11 @@ registerSchema({
 });
 
 registerSchema({
+  name: 'editor_load',
+  version: '1-0-0'
+});
+
+registerSchema({
   name: 'ui_extension_install',
   version: '2-0-0'
 });

src/javascripts/analytics/snowplow/transformers/EditorLoad.es6.js
@@ -1,0 +1,22 @@
+import { addUserOrgSpace } from './Decorators.es6';
+
+const extractAction = eventName => eventName.split(':')[1];
+
+/**
+ * @ngdoc service
+ * @name analytics/snowplow/transformers/EditorLoad
+ * @description
+ * Exports a function that transforms data for the editor load events
+ */
+export default addUserOrgSpace((eventName, data) => ({
+  data: {
+    action: extractAction(eventName),
+    slide_uuid: data.slideUuid,
+    slide_level: data.slideLevel,
+    link_count: data.linkCount,
+    rich_text_editor_instance_count: data.richTextEditorInstanceCount,
+    link_field_editor_instance_count: data.linkFieldEditorInstanceCount,
+    total_slide_count: data.totalSlideCount,
+    load_ms: data.loadMs
+  }
+}));

src/javascripts/analytics/snowplow/transformers/SlideInEditor.es6.js
@@ -12,13 +12,7 @@ export default addUserOrgSpace((eventName, data) => ({
     current_slide_level: data.currentSlideLevel,
     target_slide_level: data.targetSlideLevel,
     peek_hover_time_ms: data.peekHoverTimeMs || 0,
-    editor_type: data.editorType,
-    slide_uuid: data.slideUuid,
-    number_of_links: data.numberOfLinks,
-    number_of_rich_text_editors: data.numberOfRichTextEditors,
-    number_of_reference_field_editors: data.numberOfReferenceFieldEditors,
-    number_of_total_slides: data.numberOfTotalSlides,
-    load_ms: data.loadMs
+    editor_type: data.editorType
   }
 }));
 

src/javascripts/app/entity_editor/EntryController.es6.js
@@ -12,8 +12,8 @@ import installTracking, { trackEntryView } from './Tracking.es6';
 import {
   isLinkField,
   createLoadEventTracker,
-  getNumRenderableFieldEditors
-} from './LoadEventTracker.es6';
+  getRenderableLinkFieldInstanceCount
+} from 'app/entity_editor/LoadEventTracker.es6';
 
 import { getModule } from 'NgRegistry.es6';
 import createEntrySidebarProps from 'app/EntrySidebar/EntitySidebarBridge.es6';
@@ -56,19 +56,20 @@ const DataFields = getModule('EntityEditor/DataFields');
 export default async function create($scope, editorData, preferences) {
   $scope.context = {};
 
-  const start = Date.now();
+  const loadStartMs = Date.now();
 
   let loadLinksRendered = false;
   let loadShareJSConnected = false;
 
   $scope.loadEvents = K.createStreamBus($scope);
 
-  Telemetry.record('entry_editor_http_time', Date.now() - start);
+  const trackLoadEvent = createLoadEventTracker(loadStartMs, $scope.slideStates, () => editorData);
+
+  Telemetry.record('entry_editor_http_time', Date.now() - loadStartMs);
+  trackLoadEvent('init');
 
   const linkFieldTypes = editorData.contentType.data.fields.filter(isLinkField);
-  const numberOfRenderableLinkFieldEditors = getNumRenderableFieldEditors(linkFieldTypes);
-  const trackLoadEvent = createLoadEventTracker($scope.slideStates, editorData);
-  trackLoadEvent('init');
+  const renderableLinkFieldInstanceCount = getRenderableLinkFieldInstanceCount(linkFieldTypes);
 
   $scope.context.ready = true;
   $scope.editorData = editorData;
@@ -108,15 +109,15 @@ export default async function create($scope, editorData, preferences) {
     trackEntryView({
       editorData,
       entityInfo,
-      currentSlideLevel: $scope.$parent.slideStates.length,
+      currentSlideLevel: $scope.entities.length,
       locale: localeStore.getDefaultLocale().internal_code,
-      editorType: $scope.$parent.slideStates.length > 1 ? 'slide_in_editor' : 'entry_editor'
+      editorType: $scope.slideStates.length > 1 ? 'slide_in_editor' : 'entry_editor'
     });
   } catch (error) {
     logger.logError(error);
   }
 
-  let numFieldsInteractive = 0;
+  let fieldsInteractiveCount = 0;
   const emit = once(() => {
     trackLoadEvent('links_rendered');
     loadLinksRendered = true;
@@ -125,7 +126,7 @@ export default async function create($scope, editorData, preferences) {
     }
   });
 
-  if (numberOfRenderableLinkFieldEditors === 0) {
+  if (renderableLinkFieldInstanceCount === 0) {
     emit();
   }
 
@@ -133,8 +134,8 @@ export default async function create($scope, editorData, preferences) {
     if (actionName !== 'linksRendered') {
       return;
     }
-    numFieldsInteractive++;
-    if (numFieldsInteractive === numberOfRenderableLinkFieldEditors) {
+    fieldsInteractiveCount++;
+    if (fieldsInteractiveCount === renderableLinkFieldInstanceCount) {
       emit();
     }
   });

src/javascripts/app/entity_editor/LoadEventTracker.es6.js
@@ -1,57 +1,66 @@
 import { track } from 'analytics/Analytics.es6';
-import { once, sum, sumBy, values } from 'lodash';
+import { once, sum, sumBy, values, findIndex } from 'lodash';
 import { getRichTextEntityLinks } from '@contentful/rich-text-links';
 import * as random from 'utils/Random.es6';
 import { getModule } from 'NgRegistry.es6';
 
 const TheLocaleStore = getModule('TheLocaleStore');
 
-export function createLoadEventTracker(slideStates, editorData) {
-  const { fields: fieldTypes } = editorData.contentType.data;
-  const { fields } = editorData.entity.data;
-  const richTextFieldTypes = fieldTypes.filter(isRichTextField);
-  const singleReferenceFieldTypes = fieldTypes.filter(isSingleReferenceField);
-  const multiReferenceFieldTypes = fieldTypes.filter(isMultiReferenceField);
-  const numberOfRichTextEditors = getNumRenderableFieldEditors(richTextFieldTypes);
-  const numberOfSingleReferenceFieldEditors = getNumRenderableFieldEditors(
-    singleReferenceFieldTypes
-  );
-  const numberOfMultiReferenceFieldEditors = getNumRenderableFieldEditors(multiReferenceFieldTypes);
-  const numberOfReferenceFieldEditors =
-    numberOfSingleReferenceFieldEditors + numberOfMultiReferenceFieldEditors;
+const LOAD_EVENT_CATEGORY = 'editor_load';
 
-  const sumMatchingFieldsBy = (fieldTypes, predicate) =>
-    sum(fieldTypes.map(fieldType => sumBy(values(fields[fieldType.id]), predicate)));
-  const numberOfSingleReferenceFieldLinks = sumMatchingFieldsBy(singleReferenceFieldTypes, field =>
-    field ? 1 : 0
-  );
-  const numberOfMultiReferenceFieldLinks = sumMatchingFieldsBy(multiReferenceFieldTypes, 'length');
-  const numberOfRichTextFieldLinks = sumMatchingFieldsBy(richTextFieldTypes, field =>
-    sumBy(values(getRichTextEntityLinks(field)), 'length')
-  );
-  const numberOfLinks =
-    numberOfSingleReferenceFieldLinks +
-    numberOfMultiReferenceFieldLinks +
-    numberOfRichTextFieldLinks;
-
+export function createLoadEventTracker(loadStartMs, slideStates, getEditorData) {
   const slideUuid = random.id();
-  const loadStart = new Date().getTime();
-  return eventName =>
-    track(`slide_in_editor:load_${eventName}`, {
+  const totalSlideCount = slideStates.length;
+
+  return function trackEditorLoadEvent(eventName) {
+    if (eventName === 'init') {
+      return track(`${LOAD_EVENT_CATEGORY}:init`, { slideUuid, totalSlideCount });
+    }
+    const editorData = getEditorData();
+    const { fields: fieldTypes } = editorData.contentType.data;
+    const { fields } = editorData.entity.data;
+    const entityId = editorData.entity.getId();
+    const richTextFieldTypes = fieldTypes.filter(isRichTextField);
+    const singleReferenceFieldTypes = fieldTypes.filter(isSingleReferenceField);
+    const multiReferenceFieldTypes = fieldTypes.filter(isMultiReferenceField);
+    const richTextEditorInstanceCount = getRenderableLinkFieldInstanceCount(richTextFieldTypes);
+    const singleLinkFieldEditorInstanceCount = getRenderableLinkFieldInstanceCount(
+      singleReferenceFieldTypes
+    );
+    const multiLinkFieldEditorInstanceCount = getRenderableLinkFieldInstanceCount(
+      multiReferenceFieldTypes
+    );
+    const linkFieldEditorInstanceCount =
+      singleLinkFieldEditorInstanceCount + multiLinkFieldEditorInstanceCount;
+
+    const sumMatchingFieldsBy = (fieldTypes, predicate) =>
+      sum(fieldTypes.map(fieldType => sumBy(values(fields[fieldType.id]), predicate)));
+    const singleReferenceFieldLinkCount = sumMatchingFieldsBy(singleReferenceFieldTypes, field =>
+      field ? 1 : 0
+    );
+    const multiReferenceFieldLinkCount = sumMatchingFieldsBy(multiReferenceFieldTypes, 'length');
+    const richTextFieldLinkCount = sumMatchingFieldsBy(richTextFieldTypes, field =>
+      sumBy(values(getRichTextEntityLinks(field)), 'length')
+    );
+    const linkCount =
+      singleReferenceFieldLinkCount + multiReferenceFieldLinkCount + richTextFieldLinkCount;
+    track(`${LOAD_EVENT_CATEGORY}:${eventName}`, {
       slideUuid,
-      slideLevel: slideStates.length - 1,
-      numberOfLinks,
-      numberOfRichTextEditors,
-      numberOfReferenceFieldEditors,
-      loadMs: eventName === 'init' ? 0 : new Date().getTime() - loadStart
+      slideLevel: findIndex(slideStates, entity => entity.id === entityId),
+      linkCount,
+      richTextEditorInstanceCount,
+      linkFieldEditorInstanceCount,
+      totalSlideCount,
+      loadMs: new Date().getTime() - loadStartMs
     });
+  };
 }
 
-export function getNumRenderableFieldEditors(fieldTypes) {
-  const numLocalizedFields = fieldTypes.filter(f => f.localized).length;
-  const numNonLocalizedFields = fieldTypes.length - numLocalizedFields;
-  const numActiveLocales = TheLocaleStore.getActiveLocales().length;
-  return numLocalizedFields * numActiveLocales + numNonLocalizedFields;
+export function getRenderableLinkFieldInstanceCount(fieldTypes) {
+  const localizedFieldCount = fieldTypes.filter(f => f.localized).length;
+  const nonLocalizedFieldCount = fieldTypes.length - localizedFieldCount;
+  const activeLocaleCount = TheLocaleStore.getActiveLocales().length;
+  return localizedFieldCount * activeLocaleCount + nonLocalizedFieldCount;
 }
 
 export function createLinksRenderedEvent(loadEvents) {
@@ -89,8 +98,8 @@ function handleRichTextField({ widget, locale, loadEvents, editorData, trackLink
 
   const { code: localeCode, internal_code: fieldLocaleCode } = locale;
 
-  const numLinks = getNumRichTextLinks(editorData, fieldIdFromEntry, fieldLocaleCode);
-  if (numLinks === 0) {
+  const linkCount = getRichTextLinkCount(editorData, fieldIdFromEntry, fieldLocaleCode);
+  if (linkCount === 0) {
     trackLinksRendered();
     return;
   }
@@ -101,7 +110,7 @@ function handleRichTextField({ widget, locale, loadEvents, editorData, trackLink
 
   loadEvents.stream.onValue(({ actionName, ...data }) => {
     if (
-      linksRendered.size === numLinks ||
+      linksRendered.size === linkCount ||
       actionName !== 'linkRendered' ||
       data.field.id !== fieldIdFromCT ||
       data.field.locale !== localeCode
@@ -109,13 +118,13 @@ function handleRichTextField({ widget, locale, loadEvents, editorData, trackLink
       return;
     }
     linksRendered.add(data.key);
-    if (linksRendered.size === numLinks) {
+    if (linksRendered.size === linkCount) {
       trackLinksRendered();
     }
   });
 }
 
-function getNumRichTextLinks(editorData, fieldIdFromEntry, fieldLocaleCode) {
+function getRichTextLinkCount(editorData, fieldIdFromEntry, fieldLocaleCode) {
   const { fields } = editorData.entity.data;
   const field = fields[fieldIdFromEntry];
   if (!field) {

src/javascripts/app/entity_editor/cfWidgetRendererDirective.es6.js
@@ -2,10 +2,6 @@ import { registerDirective } from 'NgRegistry.es6';
 import $ from 'jquery';
 import createBridge from 'widgets/bridges/EditorExtensionBridge.es6';
 import { NAMESPACE_BUILTIN, NAMESPACE_EXTENSION } from 'widgets/WidgetNamespaces.es6';
-import {
-  createLinksRenderedEvent,
-  createWidgetLinkRenderEventsHandler
-} from './LoadEventTracker.es6';
 
 export default function register() {
   /**
@@ -28,6 +24,7 @@ export default function register() {
     'entityCreator',
     'states/Navigator.es6',
     'navigation/SlideInNavigator',
+    'app/entity_editor/LoadEventTracker.es6',
     (
       $compile,
       $rootScope,
@@ -36,7 +33,8 @@ export default function register() {
       entitySelector,
       entityCreator,
       Navigator,
-      SlideInNavigator
+      SlideInNavigator,
+      LoadEventTracker
     ) => {
       return {
         scope: true,
@@ -50,6 +48,10 @@ export default function register() {
             widget: { problem, widgetNamespace, template, descriptor, parameters }
           } = scope;
 
+          const {
+            createLinksRenderedEvent,
+            createWidgetLinkRenderEventsHandler
+          } = LoadEventTracker;
           const trackLinksRendered = createLinksRenderedEvent(loadEvents);
           const handleWidgetLinkRenderEvents = createWidgetLinkRenderEventsHandler({
             widget,

commit db69b626a0884cf4322394fac45798e900e9bcd5

Register slide_in_editor:bulk_editor_close event
This got broken in a rebase in 7e9782c.

src/javascripts/analytics/Validator.es6.js
@@ -58,7 +58,14 @@ export const validEvents = {
     'published_restored'
   ],
   content_preview: ['created', 'updated', 'deleted'],
-  slide_in_editor: ['peek_click', 'arrow_back', 'open', 'open_create', 'delete'],
+  slide_in_editor: [
+    'peek_click',
+    'arrow_back',
+    'bulk_editor_close',
+    'open',
+    'open_create',
+    'delete'
+  ],
   editor_load: ['init', 'sharejs_connected', 'links_rendered', 'fully_interactive'],
   // https://contentful.atlassian.net/wiki/display/PRODBulk+references+editor+-+Tracking+specs
   bulk_editor: [