be5742b31fbd7f7d36d9bfe3772946c32db884ad
Mohamed Turki <hi@mturki.me>
2019-04-01 08:08:10 +0200

    Adds custom headerNode back to EntrySidebarWidget
    This adds headerNode prop to EntrySidebarWidget to be able to render
    FeedbackButton in sidebar widgets.
    This also prevents entry activity from crashing on deleted fields.

--- src/javascripts/app/EntrySidebar/EntryActivity/EntryActivityWidget.es6.js
+++ src/javascripts/app/EntrySidebar/EntryActivity/EntryActivityWidget.es6.js
@@ -32,6 +32,9 @@ const renderDayHeader = (activity, index) => {
 const renderActivityText = (activity, fieldIdNameMap) => {
   if (activity.verb === 'changed' && get(activity, 'path')) {
     const fieldName = fieldIdNameMap[activity.path[1]];
+    if (!fieldName) {
+      return <span title={fieldName}>{`Inaccessible field changed`}</span>;
+    }
     const fieldLabel = fieldName.length > 14 ? `${fieldName.slice(0, 13).trim()}...` : fieldName;
     return <span title={fieldName}>{`Field "${fieldLabel}" changed`}</span>;
   }
