21e1f13f1a60c1654f9869da0042d1db0ed3e5b4
Alexander Suevalov <suevalov.work@gmail.com>
2019-04-02 16:23:07 +0200

    Conditionally hide EntryActivity from sidebar configuration (#4183)

--- src/javascripts/app/EntrySidebar/Configuration/components/SidebarWidgetItem.es6.js
+++ src/javascripts/app/EntrySidebar/Configuration/components/SidebarWidgetItem.es6.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { IconButton, Icon, Note } from '@contentful/forma-36-react-components';
+import { ENTRY_ACTIVITY } from 'featureFlags.es6';
+import SidebarWidgetTypes from 'app/EntrySidebar/SidebarWidgetTypes.es6';
+import { getCurrentVariation } from 'utils/LaunchDarkly/index.es6';
 
-export default function SidebarWidgetItem({
+export function SidebarWidgetItem({
   id,
   name,
   isDraggable,
@@ -52,4 +55,45 @@ SidebarWidgetItem.defaultProps = {
   isDraggable: false,
   isRemovable: false,
   isProblem: false
+};
+
+class ConditionallyRenderedWidget extends React.Component {
+  static propTypes = {
+    isAvailable: PropTypes.func.isRequired
+  };
+
+  state = {
+    visible: false
+  };
+
+  componentDidMount() {
+    this.props.isAvailable().then(result => {
+      this.setState({ visible: Boolean(result) });
+    });
+  }
+
+  render() {
+    if (!this.state.visible) {
+      return null;
+    }
+    return this.props.children;
+  }
+}
+
+export default function WrappedSidebarWidgetItem(props) {
+  if (props.id === SidebarWidgetTypes.ACTIVITY) {
+    return (
+      <ConditionallyRenderedWidget
+        isAvailable={() => {
+          return getCurrentVariation(ENTRY_ACTIVITY);
+        }}>
+        <SidebarWidgetItem {...props} />
+      </ConditionallyRenderedWidget>
+    );
+  }
+  return <SidebarWidgetItem {...props} />;
+}
+
+WrappedSidebarWidgetItem.propTypes = {
+  id: PropTypes.string.isRequired
 };
