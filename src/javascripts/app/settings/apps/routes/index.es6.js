0dc080be86c7261d126df2b42766754e4d08e7b1
Jakub Elżbieciak <jakub@contentful.com>
2019-04-16 10:10:27 +0200

    Snake-case feature IDs

--- src/javascripts/app/settings/apps/routes/index.es6.js
+++ src/javascripts/app/settings/apps/routes/index.es6.js
@@ -1,11 +1,17 @@
+import { snakeCase } from 'lodash';
 import AppsListRoute from './AppsListRoute.es6';
 import AppRoute from './AppRoute.es6';
 import { getOrgFeature } from 'data/CMA/ProductCatalog.es6';
 
-// Product Catalog Feature ID for an app is the ID of the app
-// prefixed with "app". If the Product Catalog is not available,
-// we allow a user to use the app this time.
-const hasAppFeature = (orgId, appId) => getOrgFeature(orgId, `app_${appId}`, true);
+const hasAppFeature = (orgId, appId) => {
+  // Product Catalog Feature ID for an app is a snake-case
+  // ID of the app prefixed with "app"
+  const featureId = `app_${snakeCase(appId)}`;
+
+  // If the Product Catalog is not available, we allow a user
+  // to use the app this time.
+  return getOrgFeature(orgId, featureId, true);
+};
 
 export default {
   name: 'apps',
