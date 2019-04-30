62eb3bf29ad191541559f5f056cf608b012cf238
Guilherme Barbosa <guipvb@gmail.com>
2019-04-30 12:26:23 +0200

    Fix issue in useClickOutside hook

--- src/javascripts/app/common/hooks/useClickOutside.es6.js
+++ src/javascripts/app/common/hooks/useClickOutside.es6.js
@@ -1,12 +1,12 @@
 import { useEffect } from 'react';
 
 export default function useClickOutside(ref, isActive, cb) {
-  useEffect(() => {
-    // run callback function if target is not inside ref
-    const handleClickOutside = evt => {
-      !ref.current.contains(evt.target) && cb();
-    };
+  // run callback function if target is not inside ref
+  const handleClickOutside = evt => {
+    !ref.current.contains(evt.target) && cb();
+  };
 
+  useEffect(() => {
     if (isActive) {
       document.addEventListener('mouseup', handleClickOutside);
     } else {
