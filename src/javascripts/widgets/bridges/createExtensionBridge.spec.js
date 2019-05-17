17bd5e5a8e833138270ab48b8ac734627f144934
Guilherme Barbosa <guipvb@gmail.com>
2019-05-17 16:54:40 +0200

    Add spaceMembership back to extension bridge

--- src/javascripts/widgets/bridges/createExtensionBridge.spec.js
+++ src/javascripts/widgets/bridges/createExtensionBridge.spec.js
@@ -87,7 +87,7 @@ describe('createExtensionBridge', () => {
         getId: () => 'sid',
         getEnvironmentId: () => 'eid',
         cma: { updateEntry: stubs.updateEntry, getEntry: stubs.getEntry },
-        space: { data: { spaceMember: 'MEMBERSHIP ' } }
+        space: { data: { spaceMember: 'MEMBER ', spaceMembership: 'MEMBERSHIP ' } }
       },
       TheLocaleStore: {
         getPrivateLocales: () => [{ code: 'pl', name: 'Polski' }, { code: 'en', name: 'English' }],
@@ -128,7 +128,8 @@ describe('createExtensionBridge', () => {
           available: [{ code: 'pl', name: 'Polski' }, { code: 'en', name: 'English' }],
           default: { code: 'pl', name: 'Polski', default: true }
         },
-        spaceMember: 'MEMBERSHIP ',
+        spaceMember: 'MEMBER ',
+        spaceMembership: 'MEMBERSHIP ',
         editorInterface: {
           controls: [],
           sidebar: []
