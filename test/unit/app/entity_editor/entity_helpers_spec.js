contentful/widget-sdk

commit 4ae47fbca73f3c6bc3dec1d2d77dae2f96e17cbd

Add missing field locale transformation (from `code` to `internal_code`)
Entities are expected to use `internal_code` instead of `code` in their field locales.

src/javascripts/app/entity_editor/entityHelpers.es6.js
@@ -13,8 +13,9 @@ export default function register() {
   registerFactory('EntityHelpers', [
     '$q',
     'spaceContext',
+    'TheLocaleStore',
     '$filter',
-    ($q, spaceContext, $filter) => {
+    ($q, spaceContext, TheLocaleStore, $filter) => {
       const assetUrlFilter = $filter('assetUrl');
 
       return {
@@ -93,8 +94,14 @@ export default function register() {
               return _.transform(
                 ct.data.fields,
                 (acc, ctField) => {
-                  const field = _.get(data, ['fields', ctField.apiName]);
+                  let field = _.get(data, ['fields', ctField.apiName]);
                   if (field) {
+                    // map field locales from `code` to `internal_code` if locale is available
+                    field = _.mapKeys(
+                      field,
+                      // the locale might be missing in some cases, e.g. it was deleted
+                      (_, localeCode) => TheLocaleStore.toInternalCode(localeCode) || localeCode
+                    );
                     acc[ctField.id] = field;
                   }
                 },

commit 13e701d0fd401808fcfdfced0bbc12482c177cd0

Fix wrong link data
Completes changes started by 4ae47fb and fixes some issues introduced by
that commit. `entityHelper` does now also convert given assets locales
to internal locales. It also converts a locale given to any of the
helper functions into an internal locale.

This fixes a couple of possible bugs mostly related to renamed locales
(where the locale's `code` is different from the `internal_code`). Most
notably a bug where customers would use the example app space, rename
both locales and then see wrong data on link fields entity cards.

src/javascripts/app/entity_editor/entityHelpers.es6.js
@@ -17,13 +17,17 @@ export default function register() {
     '$filter',
     ($q, spaceContext, TheLocaleStore, $filter) => {
       const assetUrlFilter = $filter('assetUrl');
+      const toInternalLocaleCode = localeCode =>
+        TheLocaleStore.toInternalCode(localeCode) || localeCode;
 
       return {
         newForLocale: newForLocale,
         contentTypeFieldLinkCtIds: contentTypeFieldLinkCtIds
       };
 
-      function newForLocale(locale) {
+      function newForLocale(localeCode) {
+        const internalLocaleCode = toInternalLocaleCode(localeCode);
+
         return {
           /**
            * Returns a string representing the entity's title in a given locale.
@@ -35,7 +39,7 @@ export default function register() {
            * @param {string} locale
            * @return {string|null}
            */
-          entityTitle: spaceContextDelegator('entityTitle', locale),
+          entityTitle: spaceContextDelegator('entityTitle'),
           /**
            * Returns a string representing the entity's description.
            * Asset: Description field.
@@ -48,7 +52,7 @@ export default function register() {
            * @param {string} locale
            * @return {string}
            */
-          entityDescription: spaceContextDelegator('entityDescription', locale),
+          entityDescription: spaceContextDelegator('entityDescription'),
           /**
            * Returns an object representing the main file associated with the entity.
            * Asset: The asset's file if it has one or `null`.
@@ -59,26 +63,26 @@ export default function register() {
            * @param {string} locale
            * @return {object|null}
            */
-          entityFile: entity => entityFile(entity, locale),
-          entryImage: spaceContextDelegator('entryImage', locale),
-          assetFile: asset => assetFile(asset, locale),
+          entityFile: entity => entityFile(entity, localeCode),
+          entryImage: spaceContextDelegator('entryImage'),
+          assetFile: asset => assetFile(asset, localeCode),
           assetFileUrl: assetFileUrl
         };
-      }
 
-      /**
-       * Create a function that delegates to given space context method
-       * with the given locale code.
-       *
-       * The argument of the returned function is external entity data.
-       * This data is transformed into an entity for the space context
-       * method to work.
-       */
-      function spaceContextDelegator(methodName, localeCode) {
-        return async function(data) {
-          const entity = await dataToEntity(data);
-          return spaceContext[methodName](entity, localeCode);
-        };
+        /**
+         * Create a function that delegates to given space context method
+         * with the given locale code.
+         *
+         * The argument of the returned function is external entity data.
+         * This data is transformed into an entity for the space context
+         * method to work.
+         */
+        function spaceContextDelegator(methodName) {
+          return async function(data) {
+            const entity = await dataToEntity(data);
+            return spaceContext[methodName](entity, internalLocaleCode);
+          };
+        }
       }
 
       /**
@@ -96,14 +100,8 @@ export default function register() {
               return _.transform(
                 ct.data.fields,
                 (acc, ctField) => {
-                  let field = _.get(data, ['fields', ctField.apiName]);
+                  const field = _.get(data, ['fields', ctField.apiName]);
                   if (field) {
-                    // map field locales from `code` to `internal_code` if locale is available
-                    field = _.mapKeys(
-                      field,
-                      // the locale might be missing in some cases, e.g. it was deleted
-                      (_, localeCode) => TheLocaleStore.toInternalCode(localeCode) || localeCode
-                    );
                     acc[ctField.id] = field;
                   }
                 },
@@ -115,24 +113,40 @@ export default function register() {
           });
         }
 
-        return prepareFields.then(fields => ({
-          data: { fields: fields, sys: data.sys },
-          getType: _.constant(data.sys.type),
-          getContentTypeId: _.constant(ctId)
-        }));
+        return prepareFields.then(fields => {
+          const renamedFields = renameFieldLocales(fields);
+          return {
+            data: { fields: renamedFields, sys: data.sys },
+            getType: _.constant(data.sys.type),
+            getContentTypeId: _.constant(ctId)
+          };
+        });
       }
 
-      function entityFile(entity, locale) {
+      /**
+       * Maps field locales from `code` to `internal_code` if locale is available.
+       * The locale might be missing in some cases, e.g. it was deleted.
+       */
+      function renameFieldLocales(fields) {
+        return _.mapValues(fields, field =>
+          _.mapKeys(field, (_, localeCode) => toInternalLocaleCode(localeCode))
+        );
+      }
+
+      function entityFile(entity, localeCode) {
         if (entity.sys.type === 'Entry') {
-          return newForLocale(locale).entryImage(entity);
+          return newForLocale(localeCode).entryImage(entity);
         } else if (entity.sys.type === 'Asset') {
-          return assetFile(entity, locale);
+          return assetFile(entity, localeCode);
         }
         return $q.resolve(null);
       }
 
-      function assetFile(data, locale) {
-        return $q.resolve(spaceContext.getFieldValue({ data: data }, 'file', locale));
+      function assetFile(data, localeCode) {
+        const internalLocaleCode = toInternalLocaleCode(localeCode);
+        return dataToEntity(data).then(entity =>
+          spaceContext.getFieldValue(entity, 'file', internalLocaleCode)
+        );
       }
 
       function assetFileUrl(file) {

src/javascripts/classes/spaceContext.es6.js
@@ -269,11 +269,11 @@ export default function register() {
          *
          * @param {Client.Entity} entity
          * @param {string} internalFieldId
-         * @param {string?} localeCode
+         * @param {string?} internalLocaleCode
          * @return {any}
          */
-        getFieldValue: function(entity, fieldId, localeCode) {
-          const values = _.get(entity, ['data', 'fields', fieldId]);
+        getFieldValue: function(entity, internalFieldId, internalLocaleCode) {
+          const values = _.get(entity, ['data', 'fields', internalFieldId]);
           if (!_.isObject(values)) {
             return;
           }
@@ -281,10 +281,8 @@ export default function register() {
           const defaultLocale = TheLocaleStore.getDefaultLocale();
           const defaultLocaleCode = defaultLocale && defaultLocale.internal_code;
           const firstLocaleCode = Object.keys(values)[0];
-
-          localeCode = localeCode || defaultLocaleCode || firstLocaleCode;
-
-          return values[localeCode] || values[defaultLocaleCode] || values[firstLocaleCode];
+          const relevantLocaleCode = internalLocaleCode || defaultLocaleCode || firstLocaleCode;
+          return values[relevantLocaleCode] || values[defaultLocaleCode] || values[firstLocaleCode];
         },
 
         /**

src/javascripts/components/tabs/asset_list/assetListController.es6.js
@@ -111,8 +111,6 @@ export default function register() {
         resetUsageProps();
       }, 3000);
 
-      
-
       $scope.$watch('paginator.getTotal()', debouncedResetAssets);
 
       $scope.$watch(

test/unit/app/entity_editor/entity_helpers_spec.js
@@ -2,6 +2,10 @@ import _ from 'lodash';
 
 describe('EntityHelpers', () => {
   const REWRITTEN_URL = 'http://rewritten.url/file.txt';
+  const INTERNAL_LOCALE_BY_LOCALE = {
+    'en-US': 'en-US',
+    de: 'de-internal'
+  };
   const throwingFn = () => {
     throw new Error('Should not end up here!');
   };
@@ -9,6 +13,9 @@ describe('EntityHelpers', () => {
   beforeEach(function() {
     module('contentful/test', $provide => {
       $provide.constant('assetUrlFilter', _.constant(REWRITTEN_URL));
+      $provide.constant('TheLocaleStore', {
+        toInternalCode: code => INTERNAL_LOCALE_BY_LOCALE[code]
+      });
     });
 
     this.spaceContext = this.$inject('mocks/spaceContext').init();
@@ -38,23 +45,49 @@ describe('EntityHelpers', () => {
   function itConvertsToEntryAndCallsMethod(methodName, spaceContextMethodName) {
     spaceContextMethodName = spaceContextMethodName || methodName;
 
-    it(`#${methodName}() converts data to entry and calls spaceContext.${spaceContextMethodName}()`, async function() {
-      this.spaceContext.publishedCTs.fetch.resolves({
-        data: { fields: [{ apiName: 'testField', id: 'realid' }] }
+    describe(`#${methodName}()`, function() {
+      const contentType = {
+        fields: [{ apiName: 'fieldA', id: 'fieldA' }, { apiName: 'fieldB', id: 'fieldBRealId' }]
+      };
+      const fields = {
+        fieldA: { 'en-US': 'en' },
+        fieldB: { 'en-US': 'valEN', de: 'valDE' },
+        unknownField: {}
+      };
+      const entry = {
+        sys: { type: 'Entry', contentType: { sys: { id: 'ctId' } } },
+        fields
+      };
+
+      beforeEach(function() {
+        this.spaceContext.publishedCTs.fetch.resolves({ data: contentType });
       });
 
-      await this.helpers[methodName]({
-        sys: { type: 'Entry', contentType: { sys: { id: 'ctid' } } },
-        fields: { testField: {} }
+      it(`converts data to entry and calls spaceContext.${spaceContextMethodName}()`, async function() {
+        const transformedFields = {
+          fieldA: fields.fieldA,
+          fieldBRealId: { 'en-US': 'valEN', 'de-internal': 'valDE' }
+        };
+
+        await this.helpers[methodName](entry);
+
+        sinon.assert.calledOnce(this.spaceContext[spaceContextMethodName]);
+        const [entity, locale] = this.spaceContext[spaceContextMethodName].firstCall.args;
+
+        expect(entity.data.fields).toEqual(transformedFields);
+        expect(entity.getType()).toBe('Entry');
+        expect(entity.getContentTypeId()).toBe('ctId');
+        expect(locale).toBe('en-US');
       });
 
-      sinon.assert.calledOnce(this.spaceContext[spaceContextMethodName]);
-      const [entity, locale] = this.spaceContext[spaceContextMethodName].firstCall.args;
-
-      expect(entity.data.fields).toEqual({ realid: {} });
-      expect(entity.getType()).toBe('Entry');
-      expect(entity.getContentTypeId()).toBe('ctid');
-      expect(locale).toBe('en-US');
+      it(`passes internal locale to spaceContext`, async function() {
+        const helpers = this.EntityHelpers.newForLocale('de');
+        await helpers[methodName]({
+          sys: { type: 'Entry' }
+        });
+        const locale = this.spaceContext[spaceContextMethodName].firstCall.args[1];
+        expect(locale).toBe('de-internal');
+      });
     });
   }
 