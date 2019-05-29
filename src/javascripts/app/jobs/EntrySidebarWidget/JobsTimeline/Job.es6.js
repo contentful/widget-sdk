5ab993d47e1dc4b21a796529a66b6e2abde3f1de
Johannes Bugiel <jo.bugiel@gmail.com>
2019-05-29 11:29:48 +0200

    fix(Timeline): Improve Timeline UI (#4440)

--- src/javascripts/app/jobs/EntrySidebarWidget/JobsTimeline/Job.es6.js
+++ src/javascripts/app/jobs/EntrySidebarWidget/JobsTimeline/Job.es6.js
@@ -14,24 +14,20 @@ const FormattedTime = time => (
       .format('ddd, MMM Do, YYYY - hh:mm A')}
   </span>
 );
-const tagTypeForStatus = {
-  pending: 'warning',
-  success: 'positive',
-  error: 'negative',
-  cancelled: 'muted'
+const tagTypeForAction = {
+  publish: 'positive'
 };
 
-const Job = ({ scheduledAt, action, status }) => (
+const Job = ({ scheduledAt, action }) => (
   <li className={styles.schedule}>
-    <span className={styles.scheduledTo}>Scheduled to</span>
+    {FormattedTime(scheduledAt)}
     <span className={styles.info}>
       <Tag
         className={cn(styles.actionType)}
-        tagType={tagTypeForStatus[status]}
+        tagType={tagTypeForAction[action]}
         testId="scheduled-item">
         {action}
       </Tag>
-      {FormattedTime(scheduledAt)}
     </span>
   </li>
 );
--- src/javascripts/app/jobs/EntrySidebarWidget/JobsTimeline/styles.es6.js
+++ src/javascripts/app/jobs/EntrySidebarWidget/JobsTimeline/styles.es6.js
@@ -3,14 +3,17 @@ import tokens from '@contentful/forma-36-tokens';
 
 export const scheduleStyles = {
   scheduledTo: css({
-    whiteSpace: 'nowrap'
+    whiteSpace: 'nowrap',
+    width: '50%'
   }),
   schedule: css({
     display: 'flex',
+    flexWrap: 'wrap',
     paddingLeft: '20px',
     position: 'relative',
+    color: tokens.colorTextLight,
     '&::before': {
-      top: '1px',
+      top: '2px',
       left: '0px',
       content: `''`,
       width: '10px',
@@ -18,7 +21,7 @@ export const scheduleStyles = {
       position: 'absolute',
       borderRadius: '10px',
       backgroundColor: 'transparent',
-      border: `2px solid ${tokens.colorElementDarkest}`
+      border: `1px solid ${tokens.colorElementDarkest}`
     },
     '&:first-of-type::before': {
       backgroundColor: tokens.colorElementMid
@@ -28,8 +31,8 @@ export const scheduleStyles = {
       left: '5px',
       position: 'absolute',
       content: `''`,
-      width: '4px',
-      height: '10px',
+      width: '2px',
+      height: '11px',
       background: tokens.colorElementDarkest
     }
   }),
@@ -45,6 +48,6 @@ export const scheduleStyles = {
     width: '100%'
   }),
   date: css({
-    marginLeft: 'auto'
+    color: tokens.colorTextLight
   })
 };
