4f16dffef4ff8b3c3242dafc9d810c68aa4043b3
Markus Lautenbach <markus.lautenbach@contentful.com>
2019-06-03 12:03:09 +0200

    Add space name
    Temporarily used redux for testing design

--- src/javascripts/access_control/SpaceTeamsPage.es6.js
+++ src/javascripts/access_control/SpaceTeamsPage.es6.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { map } from 'lodash';
+import { map, find } from 'lodash';
 import {
   Table,
   TableBody,
@@ -11,14 +11,18 @@ import {
   Heading
 } from '@contentful/forma-36-react-components';
 import getOrgId from 'redux/selectors/getOrgId.es6';
+import { css } from 'emotion';
 
+import Workbench from 'app/common/Workbench.es6';
 import createFetcherComponent, { FetcherLoading } from '../app/common/createFetcherComponent.es6';
 import { createOrganizationEndpoint } from '../data/EndpointFactory.es6';
 import StateRedirect from '../app/common/StateRedirect.es6';
 import DocumentTitle from '../components/shared/DocumentTitle.es6';
+import { joinWithAnd } from '../utils/StringUtils.es6';
 
 import { getAllTeamsSpaceMemberships } from './TeamRepository.es6';
-import { joinWithAnd } from '../utils/StringUtils.es6';
+
+import getSpacesByOrgId from 'redux/selectors/getSpacesByOrgId.es6';
 
 const TeamListFetcher = createFetcherComponent(({ orgId }) => {
   const endpoint = createOrganizationEndpoint(orgId);
@@ -28,10 +32,22 @@ const TeamListFetcher = createFetcherComponent(({ orgId }) => {
   return Promise.all(promises);
 });
 
+const styles = {
+  content: css({
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column'
+  }),
+  contentAlignment: css({
+    maxWidth: '1800px'
+  })
+};
+
 class SpaceTeamsPage extends React.Component {
   static propTypes = {
     orgId: PropTypes.string.isRequired,
-    onReady: PropTypes.func.isRequired
+    onReady: PropTypes.func.isRequired,
+    spaceName: PropTypes.string.isRequired
   };
 
   componentDidMount() {
@@ -40,8 +56,10 @@ class SpaceTeamsPage extends React.Component {
   }
 
   render() {
+    const { orgId, spaceName } = this.props;
+
     return (
-      <TeamListFetcher orgId={this.props.orgId}>
+      <TeamListFetcher orgId={orgId}>
         {({ isLoading, isError, data }) => {
           if (isLoading) {
             return <FetcherLoading message="Loading teams..." />;
@@ -55,30 +73,44 @@ class SpaceTeamsPage extends React.Component {
           return (
             <React.Fragment>
               <DocumentTitle title="Teams in Space" />
-              <Heading>{`Teams in XXX space (${TSMS.length})`}</Heading>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Team</TableCell>
-                    <TableCell>Role</TableCell>
-                    <TableCell>Members</TableCell>
-                    <TableCell />
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {TSMS.map(({ sys: { id, team: { name, description } }, roles, admin }) => (
-                    <TableRow key={id}>
-                      <TableCell>
-                        <div>{name}</div>
-                        <div>{description}</div>
-                      </TableCell>
-                      <TableCell>{admin ? 'Admin' : joinWithAnd(map(roles, 'name'))}</TableCell>
-                      <TableCell />
-                      <TableCell />
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
+              <Workbench>
+                <Workbench.Header>
+                  <Workbench.Header.Left>
+                    <Workbench.Icon icon="page-teams" />
+                    <Workbench.Title>Teams</Workbench.Title>
+                  </Workbench.Header.Left>
+                </Workbench.Header>
+                <Workbench.Content className={styles.content}>
+                  <div className={styles.contentAlignment}>
+                    <Heading>{`Teams in ${spaceName} space (${TSMS.length})`}</Heading>
+                    <Table>
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>Team</TableCell>
+                          <TableCell>Role</TableCell>
+                          <TableCell>Members</TableCell>
+                          <TableCell />
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {TSMS.map(({ sys: { id, team: { name, description } }, roles, admin }) => (
+                          <TableRow key={id}>
+                            <TableCell>
+                              <div>{name}</div>
+                              <div>{description}</div>
+                            </TableCell>
+                            <TableCell>
+                              {admin ? 'Admin' : joinWithAnd(map(roles, 'name'))}
+                            </TableCell>
+                            <TableCell />
+                            <TableCell />
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                    </Table>
+                  </div>
+                </Workbench.Content>
+              </Workbench>
             </React.Fragment>
           );
         }}
@@ -87,4 +119,10 @@ class SpaceTeamsPage extends React.Component {
   }
 }
 
-export default connect(state => ({ orgId: getOrgId(state) }))(SpaceTeamsPage);
+export default connect((state, { spaceId }) => {
+  const orgId = getOrgId(state);
+  return {
+    orgId,
+    spaceName: find(getSpacesByOrgId(state)[orgId], { sys: { id: spaceId } }).name
+  };
+})(SpaceTeamsPage);
