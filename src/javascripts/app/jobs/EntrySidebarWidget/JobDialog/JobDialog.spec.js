263cc7d90f6fc2681b4bbd1b484edb16028df66f
Sergii Bezliudnyi <serg.bezludny@gmail.com>
2019-06-18 18:19:04 +0200

    cover jobs dialog with the basic tests

---
+++ src/javascripts/app/jobs/EntrySidebarWidget/JobDialog/JobDialog.spec.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { render, cleanup, fireEvent, wait } from '@testing-library/react';
+import 'jest-dom/extend-expect';
+import JobDialog from './index.es6';
+
+describe('JobDialog', () => {
+  afterEach(cleanup);
+
+  const build = () => {
+    const props = {
+      onCreate: jest.fn(),
+      onCancel: jest.fn()
+    };
+    return [render(<JobDialog {...props} />), props];
+  };
+
+  it('renders scheduling dialog', () => {
+    const [{ getByTestId }] = build();
+
+    expect(getByTestId('schedule-publication-modal')).toBeInTheDocument();
+  });
+
+  it('calls the onConfirm callback', async () => {
+    const [{ getByTestId }, props] = build();
+
+    await schedulePublication(getByTestId);
+
+    expect(props.onCreate).toHaveBeenCalled();
+  });
+
+  it('calls the onCancel callback', async () => {
+    const [{ getByTestId }, props] = build();
+    const cancel = getByTestId('cancel');
+
+    fireEvent.click(cancel);
+
+    await wait();
+    expect(props.onCancel).toHaveBeenCalled();
+  });
+
+  it.each([
+    ['2017-06-18T15:33', '2017-06-18T16:03:00.000+00:00'],
+    ['2018-06-18T15:33:12', '2018-06-18T16:03:00.000+00:00'],
+    ['2018-06-18T00:01', '2018-06-18T00:31:00.000+00:00'],
+    ['2018-06-18T23:55', '2018-06-19T00:25:00.000+00:00'],
+    ['2018-12-31T23:52', '2019-01-01T00:22:00.000+00:00']
+  ])('default values are +30min in the future: %p => %p', async (now, expected) => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(jest.fn(() => new Date(now).valueOf()));
+
+    const [{ getByTestId }, props] = build();
+
+    await schedulePublication(getByTestId);
+
+    expect(props.onCreate).toHaveBeenCalledWith({
+      scheduledAt: expected
+    });
+  });
+});
+
+async function schedulePublication(getByTestId) {
+  const cta = getByTestId('schedule-publication');
+  fireEvent.click(cta);
+  await wait();
+}
