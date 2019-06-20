2ad077117f6e9e44dbe24535e01d2dd315b408c0
Maryna Glukhodid <MarynaG@users.noreply.github.com>
2019-06-20 17:06:21 +0300

    Refactor App ui test (#4523)
    * Refactor app test

--- test/cypress/integration/ui/apps/apps_page.spec.ts
+++ test/cypress/integration/ui/apps/apps_page.spec.ts
@@ -16,7 +16,9 @@ describe('Apps Page', () => {
     })
   );
 
-  function setUpFakeServersAndVisitPage() {
+  before(() => {
+    cy.setAuthTokenToLocalStorage();
+
     cy.resetAllFakeServers();
 
     defaultRequestsMock();
@@ -26,15 +28,9 @@ describe('Apps Page', () => {
     cy.visit(`/spaces/${defaultSpaceId}/apps`);
 
     cy.wait([`@${state.Token.VALID}`]);
-  }
+  });
 
   describe('Opening the Apps page with disabled Alpha feature', () => {
-    before(() => {
-      cy.setAuthTokenToLocalStorage();
-
-      setUpFakeServersAndVisitPage();
-    });
-
     it('Renders the page', () => {
       cy.getByTestId('workbench-title').should('to.have.text', 'Apps');
       cy.getByTestId('cf-ui-note')
@@ -52,55 +48,44 @@ describe('Apps Page', () => {
   });
 
   describe('Enable Alpha Apps feature', () => {
-    beforeEach(() => {
-      cy.setAuthTokenToLocalStorage();
-
-      setUpFakeServersAndVisitPage();
-    });
-
-    it('Enable alpha feature button enables the feature correctly', () => {
-      cy.getByTestId('enable-apps')
+    before(() =>
+      cy
+        .getByTestId('enable-apps')
         .should('have.text', 'Enable alpha feature')
-        .click();
+        .click()
+    );
+
+    const apps = [
+      { title: 'Netlify', expectedUrl: `/spaces/${defaultSpaceId}/apps/netlify` },
+      { title: 'Algolia', expectedUrl: `/spaces/${defaultSpaceId}/apps/algolia` },
+      {
+        title: 'AI image management',
+        expectedUrl: `/spaces/${defaultSpaceId}/apps/aiImageManagement`
+      },
+      {
+        title: 'Basic approval workflow',
+        expectedUrl: `/spaces/${defaultSpaceId}/apps/basicApprovalWorkflow`
+      },
+      {
+        title: 'Optimizely',
+        expectedUrl: `/spaces/${defaultSpaceId}/apps/optimizely`
+      }
+    ];
+
+    it('Apps links are correct', () => {
       cy.getByTestId('install-app').should('be.enabled');
+      cy.getByTestId('app-title')
+        .find('h3')
+        .each(($h3, index) => {
+          cy.wrap($h3).should('have.text', apps[index].title);
+        });
     });
 
-    describe('Apps links work correctly', () => {
-      const apps = [
-        { title: 'Netlify', expectedUrl: `/spaces/${defaultSpaceId}/apps/netlify` },
-        { title: 'Algolia', expectedUrl: `/spaces/${defaultSpaceId}/apps/algolia` },
-        {
-          title: 'AI image management',
-          expectedUrl: `/spaces/${defaultSpaceId}/apps/aiImageManagement`
-        },
-        {
-          title: 'Basic approval workflow',
-          expectedUrl: `/spaces/${defaultSpaceId}/apps/basicApprovalWorkflow`
-        },
-        {
-          title: 'Optimizely',
-          expectedUrl: `/spaces/${defaultSpaceId}/apps/optimizely`
-        }
-      ];
-
-      beforeEach(() => {
-        cy.getByTestId('enable-apps').click();
-      });
-
-      it('App titles are correct', () => {
-        cy.getByTestId('app-title')
-          .find('h3')
-          .each(($h3, index) => {
-            cy.wrap($h3).should('have.text', apps[index].title);
-          });
-      });
-
-      it('Install button works correctly', () => {
-        cy.getByTestId('install-app')
-          .click()
-          .url()
-          .should('eq', `${baseUrl}${apps[0].expectedUrl}`);
-      });
+    it('Install button works correctly', () => {
+      cy.getByTestId('install-app')
+        .click()
+        .url()
+        .should('eq', `${baseUrl}${apps[0].expectedUrl}`);
     });
   });
 });
