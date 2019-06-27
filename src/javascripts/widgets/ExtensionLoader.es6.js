61c67088a4124fbd2a1977cd06fce5835596ecf9
Martin Lehner <lehner.m.e@gmail.com>
2019-06-27 10:52:32 +0200

    Resolve extension definitions

--- src/javascripts/widgets/ExtensionLoader.es6.js
+++ src/javascripts/widgets/ExtensionLoader.es6.js
@@ -1,6 +1,6 @@
 import DataLoader from 'dataloader';
-import { memoize, get } from 'lodash';
-import { createSpaceEndpoint } from 'data/EndpointFactory.es6';
+import { memoize, get, identity, omit } from 'lodash';
+import { createSpaceEndpoint, createOrganizationEndpoint } from 'data/EndpointFactory.es6';
 
 // This module exposes 2 retrieval methods:
 // - `getExtensionsById` allows to get extensions
@@ -27,17 +27,78 @@ import { createSpaceEndpoint } from 'data/EndpointFactory.es6';
 // Produces a key for memoization. `_.memoize` by default
 // only uses first argument to the function being memoized
 // as a cache key.
-const makeMemoizationKey = (spaceId, envId) => [spaceId, envId].join('!');
+const makeMemoizationKey = (orgId, spaceId, envId) => [orgId, spaceId, envId].join('!');
 
-const getEndpointForSpaceEnv = memoize((spaceId, envId) => {
-  return createSpaceEndpoint(spaceId, envId);
+const getEndpointsForSpaceEnv = memoize((orgId, spaceId, envId) => {
+  return {
+    spaceEndpoint: createSpaceEndpoint(spaceId, envId),
+    orgEndpoint: createOrganizationEndpoint(orgId)
+  };
 }, makeMemoizationKey);
 
-const getLoaderForSpaceEnv = memoize((spaceId, envId) => {
-  const endpoint = getEndpointForSpaceEnv(spaceId, envId);
+const loadExtensionDefinitions = async (orgEndpoint, uuids) => {
+  if (!Array.isArray(uuids)) {
+    return [];
+  }
+
+  const definitionResult = await orgEndpoint({
+    method: 'GET',
+    path: '/extension_definitions',
+    query: {
+      'sys.uuid[in]': uuids.join(',')
+    }
+  });
+
+  return definitionResult.items || [];
+};
+
+const isBasedOnExtensionDefinition = extension =>
+  get(extension, ['extensionDefinition', 'linkType']) === 'ExtensionDefinition';
+
+const getExtensionDefinitionUUID = extension => get(extension, ['extensionDefinition', 'uuid']);
+
+const mergeExtensionsAndDefinitions = (extensions, definitions) => {
+  return extensions
+    .map(extension => {
+      const definitionId = getExtensionDefinitionUUID(extension);
+
+      if (definitionId) {
+        const definition = definitions.find(
+          definition => get(definition, ['sys', 'uuid']) === definitionId
+        );
+
+        if (definition) {
+          return {
+            ...extension,
+            extension: omit(definition, ['sys'])
+          };
+        }
+
+        // So what are we going to do about this then?
+        // Dropping the extension for now
+        return null;
+      }
+
+      return extension;
+    })
+    .filter(identity);
+};
+
+const resolveExtensionDefinitions = async (extensions, orgEndpoint) => {
+  const definitionUUIDs = extensions
+    .filter(isBasedOnExtensionDefinition)
+    .map(getExtensionDefinitionUUID);
+
+  const definitions = await loadExtensionDefinitions(orgEndpoint, definitionUUIDs);
+
+  return mergeExtensionsAndDefinitions(extensions, definitions);
+};
+
+const getLoaderForSpaceEnv = memoize((orgId, spaceId, envId) => {
+  const { spaceEndpoint, orgEndpoint } = getEndpointsForSpaceEnv(orgId, spaceId, envId);
 
   return new DataLoader(async extensionIds => {
-    const { items } = await endpoint({
+    const { items } = await spaceEndpoint({
       method: 'GET',
       // TODO: It still loads all extensions.
       // Once we modify the API we should do the following:
@@ -45,49 +106,49 @@ const getLoaderForSpaceEnv = memoize((spaceId, envId) => {
       path: '/extensions'
     });
 
-    return extensionIds.map(extensionId => {
-      const extension = (items || []).find(item => {
-        return extensionId === get(item, ['sys', 'id']);
-      });
+    const resolvedExtensions = resolveExtensionDefinitions(items || [], orgEndpoint);
 
-      return extension || null;
-    });
+    return resolvedExtensions.filter(extension =>
+      extensionIds.includes(get(extension, ['sys', 'id']))
+    );
   });
 }, makeMemoizationKey);
 
-export async function getExtensionsById(spaceId, envId, extensionIds) {
-  const loader = getLoaderForSpaceEnv(spaceId, envId);
+export async function getExtensionsById(orgId, spaceId, envId, extensionIds) {
+  const loader = getLoaderForSpaceEnv(orgId, spaceId, envId);
 
   const extensions = await loader.loadMany(extensionIds);
 
   return extensions.filter(extension => !!extension);
 }
 
-export async function getAllExtensions(spaceId, envId) {
-  const endpoint = getEndpointForSpaceEnv(spaceId, envId);
+export async function getAllExtensions(orgId, spaceId, envId) {
+  const { orgEndpoint, spaceEndpoint } = getEndpointsForSpaceEnv(spaceId, envId);
 
-  const { items } = await endpoint({ method: 'GET', path: '/extensions' });
+  const { items } = await spaceEndpoint({ method: 'GET', path: '/extensions' });
 
-  const loader = getLoaderForSpaceEnv(spaceId, envId);
+  const resolvedExtensions = resolveExtensionDefinitions(items || [], orgEndpoint);
+
+  const loader = getLoaderForSpaceEnv(orgId, spaceId, envId);
 
   // We cannot prime over existing cache entries.
   // Evict all the cached items first and then prime.
   loader.clearAll();
-  (items || []).forEach(extension => {
+  resolvedExtensions.forEach(extension => {
     loader.prime(extension.sys.id, extension);
   });
 
   return items;
 }
 
-export function evictExtension(spaceId, envId, extensionId) {
-  const loader = getLoaderForSpaceEnv(spaceId, envId);
+export function evictExtension(orgId, spaceId, envId, extensionId) {
+  const loader = getLoaderForSpaceEnv(orgId, spaceId, envId);
 
   loader.clear(extensionId);
 }
 
-export function cacheExtension(spaceId, envId, extension) {
-  const loader = getLoaderForSpaceEnv(spaceId, envId);
+export function cacheExtension(orgId, spaceId, envId, extension) {
+  const loader = getLoaderForSpaceEnv(orgId, spaceId, envId);
   const key = extension.sys.id;
 
   // As above, cache eviction is needed before priming.
