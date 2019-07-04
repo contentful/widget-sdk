53b690cffa1b9e39bd1cfe2ea507566ea8c25174
Guilherme Barbosa <guipvb@gmail.com>
2019-07-04 11:24:11 +0200

    Add loading and disabled states to the Autocomplete component

--- src/javascripts/app/common/Autocomplete.es6.js
+++ src/javascripts/app/common/Autocomplete.es6.js
@@ -2,10 +2,12 @@ import React, { useReducer, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import isHotKey from 'is-hotkey';
 import {
-  TextInput,
+  TextField,
   Dropdown,
   DropdownList,
-  DropdownListItem
+  DropdownListItem,
+  SkeletonBodyText,
+  SkeletonContainer
 } from '@contentful/forma-36-react-components';
 
 const TOGGLED_LIST = 'TOGGLED_LIST';
@@ -72,7 +74,9 @@ export default function Autocomplete({
   onQueryChange,
   placeholder = 'Search',
   width,
-  className
+  className,
+  validationMessage,
+  isLoading = false
 }) {
   const [{ isOpen, query, highlightedItemIndex }, dispatch] = useReducer(reducer, initialState);
 
@@ -94,7 +98,7 @@ export default function Autocomplete({
     const isEnter = isHotKey('enter', event);
     const isTab = isHotKey('tab', event) || isHotKey('shift+tab', event);
     const hasSelection = highlightedItemIndex !== null;
-    const lastIndex = items.length - 1;
+    const lastIndex = items.length ? items.length - 1 : 0;
     const direction = getNavigationDirection(isDown, isUp);
 
     if (direction) {
@@ -119,38 +123,48 @@ export default function Autocomplete({
   );
 
   return (
-    <Dropdown
-      isOpen={isOpen && !!items.length}
-      onClose={() => dispatch({ type: TOGGLED_LIST })}
-      disabled={disabled}
-      className={className}
-      toggleElement={
-        <TextInput
-          value={query}
-          onChange={handleQueryChanged}
-          onFocus={() => toggleList(true)}
-          onKeyDown={handleKeyDown}
-          placeholder={placeholder}
-          width={width}
-          testId="autocomplete.input"
-        />
-      }>
-      <DropdownList testId="autocomplete.dropdown-list">
-        {options.map(({ child, option }, index) => {
-          const isActive = index === highlightedItemIndex;
-          return (
-            <DropdownListItem
-              key={index}
-              isActive={isActive}
-              data-selected={isActive} // this should be coming from the component library
-              onClick={() => selectItem(option)}
-              testId="autocomplete.dropdown-list-item">
-              {child}
-            </DropdownListItem>
-          );
-        })}
-      </DropdownList>
-    </Dropdown>
+    <>
+      <Dropdown
+        isOpen={isOpen && (items.length || isLoading)}
+        onClose={() => dispatch({ type: TOGGLED_LIST })}
+        className={className}
+        toggleElement={
+          <TextField
+            className={className}
+            value={query}
+            onChange={handleQueryChanged}
+            onFocus={() => toggleList(true)}
+            onKeyDown={handleKeyDown}
+            textInputProps={{
+              disabled,
+              placeholder,
+              width
+            }}
+            testId="autocomplete.input"
+            validationMessage={validationMessage}
+          />
+        }>
+        <DropdownList testId="autocomplete.dropdown-list">
+          {isLoading ? (
+            <OptionSkeleton />
+          ) : (
+            options.map(({ child, option }, index) => {
+              const isActive = index === highlightedItemIndex;
+              return (
+                <DropdownListItem
+                  key={index}
+                  isActive={isActive}
+                  data-selected={isActive} // this should be coming from the component library
+                  onClick={() => selectItem(option)}
+                  testId="autocomplete.dropdown-list-item">
+                  {child}
+                </DropdownListItem>
+              );
+            })
+          )}
+        </DropdownList>
+      </Dropdown>
+    </>
   );
 }
 
@@ -161,9 +175,33 @@ Autocomplete.propTypes = {
   onQueryChange: PropTypes.func.isRequired,
   placeholder: PropTypes.string,
   width: PropTypes.string,
-  disabled: PropTypes.bool,
-  className: PropTypes.string
+  className: PropTypes.string,
+  validationMessage: PropTypes.string,
+  isLoading: PropTypes.bool,
+  disabled: PropTypes.bool
 };
+
+function OptionSkeleton() {
+  return (
+    <>
+      <DropdownListItem>
+        <SkeletonContainer svgWidth="200" svgHeight={20}>
+          <SkeletonBodyText numberOfLines={1} />
+        </SkeletonContainer>
+      </DropdownListItem>
+      <DropdownListItem>
+        <SkeletonContainer svgWidth="100" svgHeight={20}>
+          <SkeletonBodyText numberOfLines={1} />
+        </SkeletonContainer>
+      </DropdownListItem>
+      <DropdownListItem>
+        <SkeletonContainer svgWidth="150" svgHeight={20}>
+          <SkeletonBodyText numberOfLines={1} />
+        </SkeletonContainer>
+      </DropdownListItem>
+    </>
+  );
+}
 
 function getNavigationDirection(isDown, isUp) {
   if (isDown) return directions.DOWN;
