9c38526274f0792bb3ec83f878e380822ece8fc5
Daniel A. R. Werner <daniel.a.r.werner@gmail.com>
2019-07-05 11:11:16 +0200

    Better eslint rules for __test__
     - Also define that `it` is preferred over `test` (so far we had only one
    spec file using `test`.
     - Updates arraySwap.spec.js to use `it` instead of `test`.

--- src/javascripts/utils/arraySwap.spec.js
+++ src/javascripts/utils/arraySwap.spec.js
@@ -2,9 +2,18 @@ import arraySwap from './arraySwap.es6';
 
 const fixture = [1, 2, 3, 4, 5];
 
-test('utils/arraySwap', () => {
-  expect(arraySwap(fixture, 3, 0)).toEqual([4, 1, 2, 3, 5]);
-  expect(arraySwap(fixture, -1, 0)).toEqual([5, 1, 2, 3, 4]);
-  expect(arraySwap(fixture, 1, -2)).toEqual([1, 3, 4, 2, 5]);
-  expect(arraySwap(fixture, -3, -4)).toEqual([1, 3, 2, 4, 5]);
+describe('utils/arraySwap', () => {
+  itSwaps({ from: 3, to: 0, expected: [4, 1, 2, 3, 5] });
+  itSwaps({ from: -1, to: 0, expected: [5, 1, 2, 3, 4] });
+  itSwaps({ from: 1, to: -2, expected: [1, 3, 4, 2, 5] });
+  itSwaps({ from: -3, to: -4, expected: [1, 3, 2, 4, 5] });
+  itSwaps({ from: 0, to: 0, expected: fixture });
+  itSwaps({ from: -1, to: -1, expected: fixture });
+  itSwaps({ from: fixture.length, to: 0, expected: [undefined, ...fixture] });
 });
+
+function itSwaps({ from, to, expected }) {
+  it(`swaps value at index ${from} to index ${to}`, () => {
+    expect(arraySwap(fixture, from, to)).toEqual(expected);
+  });
+}
