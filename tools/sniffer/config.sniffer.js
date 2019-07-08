6839fbc84b59db84f3b52bee7e4fbd30aad436b5
Alexander Suevalov <suevalov.work@gmail.com>
2019-07-08 13:40:18 +0200

    Remove sniffing stylesheets folder (#4582)

--- tools/sniffer/config.sniffer.js
+++ tools/sniffer/config.sniffer.js
@@ -2,7 +2,7 @@ const analyzers = require('./analyzers');
 const attributes = require('./attributes');
 
 module.exports = {
-  roots: ['src/javascripts', 'src/stylesheets', 'test'],
+  roots: ['src/javascripts', 'test'],
   analyzers: analyzers,
   attributes: attributes,
   exclude: /(node_modules|\.git|\.DS_Store|\.js\.snap)/
