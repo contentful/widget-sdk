b737233aebed654a0865ab5edfa28676fcd00696
Guilherme Barbosa <guipvb@gmail.com>
2019-07-10 11:18:41 +0200

    Chagen copy of the org role descriptions

--- src/javascripts/utils/MembershipUtils.es6.js
+++ src/javascripts/utils/MembershipUtils.es6.js
@@ -1,21 +1,18 @@
 export const orgRoles = [
   {
-    name: 'Owner',
-    value: 'owner',
-    description:
-      'Organization owners can manage subscriptions, billing and organization memberships.'
+    name: 'Member',
+    value: 'member',
+    description: 'Can only access spaces and teams they are added to'
   },
   {
     name: 'Admin',
     value: 'admin',
-    description:
-      'Organization admins cannot manage organization subscriptions nor billing but can manage organization memberships.'
+    description: 'Can manage everything except billing and subscription'
   },
   {
-    name: 'Member',
-    value: 'member',
-    description:
-      'Organization members do not have access to any organization information and can only access assigned spaces.'
+    name: 'Owner',
+    value: 'owner',
+    description: 'Can manage everything'
   }
 ];
 
