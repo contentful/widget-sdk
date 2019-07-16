87a822bc93bb355c9bdd5c51001c3fb4c4d20a4f
Jakub Elżbieciak <jakub@contentful.com>
2019-07-16 11:48:58 +0200

    Add tests for dependency checker

---
+++ src/javascripts/widgets/bridges/checkDependencies.spec.js
@@ -0,0 +1,31 @@
+import checkDependencies from './checkDependencies.es6';
+
+describe('checkDependencies', () => {
+  it('throws if not all required dependencies are provided', () => {
+    expect(() => {
+      checkDependencies('SomeModuleName', { x: true }, ['x', 'y', 'z']);
+    }).toThrow(/"y" not provided to SomeModuleName/);
+  });
+
+  it('does not throw if all dependencies are provided', () => {
+    expect(() => {
+      checkDependencies('SomeModuleName', { einz: 1, zwei: 2 }, ['einz', 'zwei']);
+    }).not.toThrow();
+  });
+
+  it('checks for a key presence, the value can be falsy', () => {
+    expect(() => {
+      checkDependencies('SomeModuleName', { nil: null, undef: undefined, nein: false }, [
+        'nil',
+        'undef',
+        'nein'
+      ]);
+    }).not.toThrow();
+  });
+
+  it('allows extra properties if required are provided', () => {
+    expect(() => {
+      checkDependencies('SomeModuleName', { test: true, extraProp: {} }, ['test']);
+    }).not.toThrow();
+  });
+});
