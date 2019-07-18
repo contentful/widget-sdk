9b03d1cda18d1e087fb4979c98744524df981e85
Mudit Ameta <zeusdeux@gmail.com>
2019-07-18 17:05:39 +0200

    Post comment directly from circleci
    onCompare hook fails on node 10.15.3 due to a Buffer.from().toJSON() issue

--- tools/build-tracker.config.js
+++ tools/build-tracker.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const https = require('https');
 
 const getFileExtension = fileName => {
   return fileName.split('.').pop();
@@ -21,54 +20,6 @@ const nameMapper = fileName => {
   return `${name}.${extension}`;
 };
 
-const postCommentToPR = jsonPayload => {
-  return new Promise((resolve, reject) => {
-    const payload = JSON.stringify(jsonPayload);
-    const url = new URL(process.env.BUNDLESIZE_COMMENT_LAMBDA_URL);
-    const options = {
-      ...url,
-      hostname: url.host,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(payload),
-        Authorization: `Bearer ${process.env.GITHUB_PAT_REPO_SCOPE_SQUIRELY}`
-      }
-    };
-    delete options.host;
-
-    console.log();
-    console.log(options, payload);
-    console.log();
-
-    const req = https.request(options, res => {
-      let result = '';
-      res.setEncoding('utf8');
-
-      console.log(`POST ${url}`);
-      console.log(`\t Status code: ${res.statusCode}`);
-      console.log(`\t Headers: ${res.headers}`);
-
-      res.on('data', chunk => (result += chunk));
-
-      res.on('end', () => {
-        if (res.statusCode >= 400) {
-          resolve(JSON.parse(result));
-        }
-        return resolve({
-          result
-        });
-      });
-    });
-
-    req.on('error', reject);
-    req.on('abort', reject);
-
-    req.write(payload);
-    req.end();
-  });
-};
-
 const pathToBuiltAssets =
   process.env.PATH_TO_BUILT_ASSETS || path.resolve(__dirname, '../build/app');
 
@@ -80,32 +31,5 @@ module.exports = {
   baseDir: pathToBuiltAssets,
   artifacts: [`${pathToBuiltAssets}/**/*.{js,css}`],
   getFilenameHash: getFilenameHash,
-  nameMapper: nameMapper,
-  onCompare: data => {
-    const markdown = data.markdown;
-    const pr = process.env.PR_NUMBER;
-
-    if (!pr) {
-      console.error('Not a PR. Not posting comment to GitHub issue');
-      return Promise.reject();
-    }
-
-    if (!markdown) {
-      console.log();
-      console.error('No markdown found');
-      console.log(data);
-      console.log();
-      return Promise.reject();
-    }
-
-    return postCommentToPR({
-      issue: pr,
-      message: markdown
-    }).then(result => {
-      console.log('');
-      console.log(result);
-      console.log('');
-      return result;
-    });
-  }
+  nameMapper
 };
