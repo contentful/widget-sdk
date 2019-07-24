df3c5c41cf35c8746e74a284c2255184e6dfc612
Mudit Ameta <zeusdeux@gmail.com>
2019-07-24 17:19:10 +0200

    Add eslint rule to disallow inline styles in react components

---
+++ tools/eslint-rules/restrict-inline-styles.js
@@ -0,0 +1,27 @@
+module.exports = {
+  meta: {
+    type: 'suggestion',
+    docs: {
+      description: 'Disallow inline styles in react files',
+      category: 'Best Practices'
+    }
+  },
+  create(context) {
+    return {
+      JSXAttribute(node) {
+        const nodeName = node.name.name;
+        const nodeValueType = node.value.expression ? node.value.expression.type : '';
+        if (nodeName === 'style') {
+          if (nodeValueType === 'ObjectExpression') {
+            context.report({
+              node,
+              message:
+                'Inline styles are disallowed in react components. Please use emotion instead along with @contentful/forma-36-tokens.'
+            });
+            return;
+          }
+        }
+      }
+    };
+  }
+};
