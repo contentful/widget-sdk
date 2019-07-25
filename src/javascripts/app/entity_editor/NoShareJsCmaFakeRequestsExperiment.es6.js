736d42edf5beb2ce25eff4e628615b89727014d7
Daniel A. R. Werner <daniel.a.r.werner@gmail.com>
2019-07-25 17:50:37 +0200

    Refactor fake entry editor CMA calls code

--- src/javascripts/app/entity_editor/NoShareJsCmaFakeRequestsExperiment.es6.js
+++ src/javascripts/app/entity_editor/NoShareJsCmaFakeRequestsExperiment.es6.js
@@ -11,37 +11,36 @@ const PATH = {
 /**
  * The following weirdness is for analysing the viability of replacing ShareJS
  * with plain HTTP calls to the CMA.
+ *
+ * @param {Object} $scope entity editor scope. Expected to have a `otDoc` prop.
+ * @param {string} organizationId
+ * @param {Object} entityInfo Expects an `id` and `type`.
+ * @param {SpaceEndpoint} endpoint
  */
 export default function create({ $scope, organizationId, entityInfo, endpoint }) {
-  let onLocalChangeOff = noop;
-
   getVariation(ENTITY_EDITOR_CMA_EXPERIMENT, {
     organizationId
-  }).then(intervalMs => {
-    onLocalChangeOff();
-    const isEnabled = intervalMs !== undefined && intervalMs > -1;
-
-    if (!isEnabled) {
-      return;
+  }).then(variation => {
+    const isEnabled = variation !== undefined && variation > -1;
+    if (isEnabled) {
+      setupFakeRequests(variation);
     }
-
-    const throttledRelevantChanges$ = $scope.otDoc.docLocalChanges$
-      .filter(change => change && change.name === 'changed')
-      .throttle(intervalMs, { leading: false });
-
-    onLocalChangeOff = K.onValueScope($scope, throttledRelevantChanges$, () => {
-      endpoint(createRequest(intervalMs)).then(noop, noop);
-    });
   });
 
-  function createRequest(intervalMs) {
-    return {
+  function setupFakeRequests(throttleMs) {
+    const createRequest = () => ({
       method: 'GET',
       // .php will result in the request being rejected so it won't count towards rate limiting.
       path: [PATH[entityInfo.type], `${entityInfo.id}.php`],
-      query: {
-        debounce: intervalMs
-      }
-    };
+      query: { throttle: throttleMs }
+    });
+
+    const throttledRelevantChanges$ = $scope.otDoc.docLocalChanges$
+      .filter(change => change && change.name === 'changed')
+      .throttle(throttleMs, { leading: false });
+
+    K.onValueScope($scope, throttledRelevantChanges$, () => {
+      endpoint(createRequest()).then(noop, noop);
+    });
   }
 }
