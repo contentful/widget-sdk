2150870b3363a592b9051729a88d58740570402f
Daniel A. R. Werner <daniel.a.r.werner@gmail.com>
2019-07-29 14:52:30 +0200

    Rename pact `tasks` states to `comments`
    We do not rename the file names for now as we are not sure whether we'll
    got with comments or comments and tasks but at least use `comments` in
    the state names for now to make this work with the backend.

--- test/cypress/interactions/tasks.ts
+++ test/cypress/interactions/tasks.ts
@@ -9,11 +9,11 @@ const empty = require('../fixtures/responses/empty.json');
 const severalTasks = require('../fixtures/responses/tasks-several.json');
 
 export enum States {
-  NONE = 'tasks/none',
-  SEVERAL = 'tasks/several',
-  SEVERAL_ONE_OPEN = 'tasks/several-one-open',
-  SEVERAL_ONE_RESOLVED = 'tasks/several-one-resolved',
-  INTERNAL_SERVER_ERROR = 'tasks/internal-server-error'
+  NONE = 'comments/none',
+  SEVERAL = 'comments/several',
+  SEVERAL_ONE_OPEN = 'comments/several-one-open',
+  SEVERAL_ONE_RESOLVED = 'comments/several-one-resolved',
+  INTERNAL_SERVER_ERROR = 'comments/internal-server-error'
 }
 
 enum Status {
