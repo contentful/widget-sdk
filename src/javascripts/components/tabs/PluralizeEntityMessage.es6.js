331d0396863eeb2f7e77c657ac977dda8ed63357
Sergii Bezliudnyi <serg.bezludny@gmail.com>
2019-08-09 13:09:27 +0200

    refactor pluralize mixin

---
+++ src/javascripts/components/tabs/PluralizeEntityMessage.es6.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * Migrated legacy implementation of the +pluralize-entity mixin
+ * Consider using Intl and PluralRules in future.
+ *
+ */
+export default function PluralizeEntityMessage({ entityType, count, restOfTheMsg }) {
+  const endsWithY = !!entityType.match(/(y|Y)$/);
+  const plural = endsWithY ? entityType.replace(/(.*)(y|Y)$/, '$1ies') : `${entityType}s`;
+
+  return (
+    <span>
+      {count} {count === 1 ? entityType : plural} {restOfTheMsg}
+    </span>
+  );
+}
+
+PluralizeEntityMessage.propTypes = {
+  entityType: PropTypes.string,
+  count: PropTypes.number,
+  restOfTheMsg: PropTypes.string
+};
