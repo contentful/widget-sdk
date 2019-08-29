935ba57d3baacc6007d235e7c8d25d11b2c77a52
Sergii Bezliudnyi <serg.bezludny@gmail.com>
2019-08-29 13:21:51 +0200

    remove interactions from contracts

--- test/cypress/integration/contract/entries/tasks.spec.ts
+++ test/cypress/integration/contract/entries/tasks.spec.ts
@@ -14,7 +14,6 @@ import {
   getAllPublicContentTypesInDefaultSpace
 } from '../../../interactions/content_types';
 import { getDefaultEntry, getFirst7SnapshotsOfDefaultEntry } from '../../../interactions/entries';
-import { generateMicrobackendStreamToken } from '../../../interactions/microbackend';
 import { defaultEntryId, defaultSpaceId } from '../../../util/requests';
 import { FeatureFlag } from '../../../util/featureFlag';
 
@@ -58,7 +57,6 @@ describe('Tasks entry editor sidebar', () => {
       getDefaultEntry.willReturnIt(),
       getFirst7SnapshotsOfDefaultEntry.willReturnNone(),
       getEditorInterfaceForDefaultContentType.willReturnOneWithoutSidebar(),
-      generateMicrobackendStreamToken.willSucceed()
     ];
   }
 
