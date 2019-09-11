contentful/widget-sdk

commit 16503a24f86a2de08add77fdda236021c1d74613

Do not update publication widget on deletion
Once the entry got deleted, the publication widget was updated with an
empty `{}` entity. This caused an error in the browser console and on
bugsnag every time an entry was deleted.

src/javascripts/app/EntrySidebar/EntitySidebarBridge.es6.js
@@ -90,14 +90,16 @@ export default ({ $scope, emitter }) => {
     });
 
     K.onValueScope($scope, $scope.otDoc.sysProperty, sys => {
-      notifyUpdate({
-        status: $scope.state.current,
-        updatedAt: sys.updatedAt
-      });
+      if (!sys.deletedAt) {
+        notifyUpdate({
+          status: $scope.state.current,
+          updatedAt: sys.updatedAt
+        });
+      }
     });
 
     let setNotSavingTimeout;
-    K.onValueScope($scope, $scope.otDoc.state.isSaving$.skipDuplicates(), isSaving => {
+    K.onValueScope($scope, $scope.otDoc.state.isSaving$, isSaving => {
       clearTimeout(setNotSavingTimeout);
       if (isSaving) {
         notifyUpdate({

src/javascripts/app/entity_editor/Document.es6.js
@@ -84,7 +84,7 @@ export function create(docConnection, entity, contentType, user, spaceEndpoint) 
    */
   const isSaving$ = K.sampleBy(docEventsBus.stream, () => {
     return !!(currentDoc && (currentDoc.inflightOp || currentDoc.pendingOp));
-  });
+  }).skipDuplicates();
 
   /**
    * @ngdoc property

commit ca5b004822d7b5c9a49e596224325187395f0421

Fix sidebar crash when another user deletes entry
Fixes crash of sidebar in case another user deletes the entry the
current user is looking at in the web app.

This got broken in 16503a2 which was not considering the possiblity of
another user deleting the entry at the same time.

src/javascripts/app/EntrySidebar/EntitySidebarBridge.es6.js
@@ -69,9 +69,10 @@ export default ({ $scope, emitter }) => {
 
   const initializePublication = once(() => {
     const notifyUpdate = update => {
+      const entity = $scope.editorData.entity.data;
       emitter.emit(SidebarEventTypes.UPDATED_PUBLICATION_WIDGET, {
         ...update,
-        entity: { ...$scope.editorData.entity.data },
+        entity: entity && { ...entity }, // `undefined` after entity deletion.
         spaceId: spaceContext.space.getId(),
         environmentId: spaceContext.getEnvironmentId(),
         isMasterEnvironment: spaceContext.isMasterEnvironment(),
@@ -90,12 +91,10 @@ export default ({ $scope, emitter }) => {
     });
 
     K.onValueScope($scope, $scope.otDoc.sysProperty, sys => {
-      if (!sys.deletedAt) {
-        notifyUpdate({
-          status: $scope.state.current,
-          updatedAt: sys.updatedAt
-        });
-      }
+      notifyUpdate({
+        status: $scope.state.current,
+        updatedAt: sys.updatedAt
+      });
     });
 
     let setNotSavingTimeout;

src/javascripts/app/EntrySidebar/PublicationWidget/PublicationWidgetContainer.es6.js
@@ -63,8 +63,17 @@ export default class PublicationWidgetContainer extends Component {
       <JobsFeatureFlag>
         {({ currentVariation }) => {
           const isJobsFeatureEnabled = currentVariation;
-          const isEntry = entity && entity.sys.type === 'Entry';
-          return isJobsFeatureEnabled && isEntry ? (
+          const isAssetOrDeletedEntry = !entity || entity.sys.type !== 'Entry';
+          return !isJobsFeatureEnabled || isAssetOrDeletedEntry ? (
+            <PublicationWidget
+              status={this.state.status}
+              primary={primary}
+              secondary={secondary}
+              revert={revert}
+              isSaving={this.state.isSaving}
+              updatedAt={this.state.updatedAt}
+            />
+          ) : (
             <JobsWidget
               spaceId={spaceId}
               environmentId={environmentId}
@@ -77,15 +86,6 @@ export default class PublicationWidgetContainer extends Component {
               revert={revert}
               isSaving={isSaving}
               updatedAt={updatedAt}
-            />
-          ) : (
-            <PublicationWidget
-              status={this.state.status}
-              primary={primary}
-              secondary={secondary}
-              revert={revert}
-              isSaving={this.state.isSaving}
-              updatedAt={this.state.updatedAt}
             />
           );
         }}