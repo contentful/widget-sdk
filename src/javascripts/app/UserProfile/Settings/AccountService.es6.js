df2c984a5e8ca871bb224b14942ff0e93a78819d
Tatiana Bessonova <tatiana.bessonova@contentful.com>
2019-09-16 13:54:46 +0200

    ALO-2049: user profile page BE integration: GET & identities DELETE

---
+++ src/javascripts/app/UserProfile/Settings/AccountService.es6.js
@@ -0,0 +1,25 @@
+import { createUsersEndpoint } from 'data/EndpointFactory.es6';
+
+const usersEndpoint = createUsersEndpoint();
+
+export async function fetchUserData() {
+  return await usersEndpoint({
+    method: 'GET',
+    query: { profile: '' }
+  });
+}
+
+export async function updateUserData({ version, ...data }) {
+  return await usersEndpoint({
+    method: 'PUT',
+    data,
+    version
+  });
+}
+
+export async function deleteUserIdentityData(id) {
+  return await usersEndpoint({
+    method: 'DELETE',
+    path: ['identities', id]
+  });
+}
--- src/javascripts/app/UserProfile/Settings/index.es6.js
+++ src/javascripts/app/UserProfile/Settings/index.es6.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Card } from '@contentful/forma-36-react-components';
 import { Workbench } from '@contentful/forma-36-react-components/dist/alpha';
 import DocumentTitle from 'components/shared/DocumentTitle.es6';
-import tokens from '@contentful/forma-36-tokens';
-import { css } from 'emotion';
 import AccountDetails from './Account.es6';
-import MFA from './MFA.es6';
+// import tokens from '@contentful/forma-36-tokens';
+import { css } from 'emotion';
+
+const styles = {
+  userSettingsSection: css({
+    maxWidth: '768px',
+    margin: 'auto'
+  })
+};
 
 export default function Settings({ title, onReady }) {
   React.useEffect(onReady, []);
@@ -16,12 +23,9 @@ export default function Settings({ title, onReady }) {
       <Workbench>
         <Workbench.Header title={title} />
         <Workbench.Content type="default">
-          <Box>
+          <Card className={styles.userSettingsSection}>
             <AccountDetails />
-          </Box>
-          <Box>
-            <MFA />
-          </Box>
+          </Card>
         </Workbench.Content>
       </Workbench>
     </>
@@ -32,15 +36,3 @@ Settings.propTypes = {
   onReady: PropTypes.func.isRequired,
   title: PropTypes.string.isRequired
 };
-
-const boxClass = css({
-  border: `1px solid ${tokens.colorElementLight}`,
-  backgroundColor: tokens.colorWhite,
-  padding: tokens.spacing2Xl,
-  boxShadow: tokens.boxShadowDefault,
-  marginBottom: tokens.spacingXl
-});
-
-function Box({ children }) {
-  return <div className={boxClass}>{children}</div>;
-}
