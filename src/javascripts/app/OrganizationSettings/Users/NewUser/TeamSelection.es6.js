9b6a9616fa7686fe317dcec0016d9f00cb016e16
Guilherme Barbosa <guipvb@gmail.com>
2019-09-23 16:47:45 +0200

    Create TeamSelection

---
+++ src/javascripts/app/OrganizationSettings/Users/NewUser/TeamSelection.es6.js
@@ -0,0 +1,135 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import AutocompleteSelection from 'app/common/AutocompleteSelection.es6';
+import { Typography, Paragraph, Icon, Tooltip } from '@contentful/forma-36-react-components';
+import { css } from 'emotion';
+import pluralize from 'pluralize';
+import {
+  Team as TeamPropType,
+  TeamSpaceMembership as TeamSpaceMembershipPropType
+} from 'app/OrganizationSettings/PropTypes.es6';
+import tokens from '@contentful/forma-36-tokens';
+import { joinWithAnd } from 'utils/StringUtils.es6';
+
+const styles = {
+  content: css({
+    display: 'flex',
+    justifyContent: 'start',
+    alignItems: 'center'
+  }),
+  teamName: css({
+    width: 200
+  }),
+  teamList: css({
+    marginTop: tokens.spacingS
+  }),
+  toggleButton: css({
+    display: 'flex'
+  }),
+  teamRolesTooltip: css({
+    display: 'block',
+    whiteSpace: 'pre',
+    textAlign: 'left'
+  })
+};
+
+export default function TeamSelection({ onRemove, team, teamSpaceMemberships = [] }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <AutocompleteSelection
+      onRemove={onRemove}
+      key={team.sys.id}
+      testId="space-membership-list.item"
+      extraContent={
+        isOpen ? <TeamSpaceMembershipList teamSpaceMemberships={teamSpaceMemberships} /> : null
+      }>
+      <div className={styles.content}>
+        <strong className={styles.teamName}>{team.name}</strong>
+        <Toggle
+          isOpen={isOpen}
+          onClick={() => setIsOpen(!isOpen)}
+          count={teamSpaceMemberships.length}
+        />
+      </div>
+    </AutocompleteSelection>
+  );
+}
+
+TeamSelection.propTypes = {
+  onRemove: PropTypes.func.isRequired,
+  team: TeamPropType.isRequired,
+  teamSpaceMemberships: PropTypes.arrayOf(TeamSpaceMembershipPropType)
+};
+
+function Toggle({ isOpen, onClick, count }) {
+  const style = css({
+    appearance: 'none',
+    display: 'flex'
+  });
+  const hasMemberships = count > 0;
+  const label = pluralize('space', count, true);
+  const iconName = isOpen ? 'ChevronUp' : 'ChevronDown';
+
+  if (!hasMemberships) return 'No spaces';
+
+  return (
+    <button onClick={onClick} className={style}>
+      {label}
+      <Icon icon={iconName} color="muted" />
+    </button>
+  );
+}
+Toggle.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+  count: PropTypes.number.isRequired
+};
+
+function TeamSpaceMembershipList({ teamSpaceMemberships = [] }) {
+  const style = css({
+    marginLeft: 200,
+    marginTop: tokens.spacingS
+  });
+  return (
+    <Typography className={style}>
+      {teamSpaceMemberships &&
+        teamSpaceMemberships.map(membership => (
+          <Paragraph key={membership.sys.id}>
+            {membership.sys.space.name} as {getRolesText(membership)}
+          </Paragraph>
+        ))}
+    </Typography>
+  );
+}
+
+TeamSpaceMembershipList.propTypes = {
+  teamSpaceMemberships: PropTypes.arrayOf(TeamSpaceMembershipPropType).isRequired
+};
+
+// Get a text listing the roles up to a limit
+// i.e:
+// roles: ['Editor', 'Author', 'Translator'], limitToShow: 1
+// => Editor and 2 other space roles
+function getRolesText(membership, limitToShow = 2) {
+  if (membership.admin) return 'Admin';
+  if (membership.roles.length > limitToShow) {
+    const [first, second, ...rest] = membership.roles;
+    return (
+      <Tooltip
+        content={
+          <span className={styles.teamRolesTooltip}>
+            {membership.roles.map(role => `${role.name} \n`)}
+          </span>
+        }>
+        {joinWithAnd([
+          first.name,
+          second.name,
+          `${rest.length} other space ${pluralize('role', rest.length)}`
+        ])}
+      </Tooltip>
+    );
+  }
+
+  return joinWithAnd(membership.roles.map(role => role.name));
+}
