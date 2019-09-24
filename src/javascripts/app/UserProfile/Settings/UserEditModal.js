6063536b6037a56eb55b4bcf0dc3208e6b1daa28
Joshua Smock <jo-sm@users.noreply.github.com>
2019-09-24 12:22:46 +0200

    Prepopulate modal with first and last name, email, and analytics flag

--- src/javascripts/app/UserProfile/Settings/UserEditModal.js
+++ src/javascripts/app/UserProfile/Settings/UserEditModal.js
@@ -6,13 +6,12 @@ import {
   TextField,
   Form,
   CheckboxField,
-  Subheading,
-  Notification
+  Subheading
 } from '@contentful/forma-36-react-components';
 import _ from 'lodash';
 import tokens from '@contentful/forma-36-tokens';
 import { createImmerReducer } from 'redux/utils/createImmerReducer.es6';
-import { updateUserData } from './AccountRepository';
+import { updateUserData } from './AccountService.es6';
 import { User as UserPropType } from './propTypes';
 import { getValidationMessageFor } from './utils';
 import { css } from 'emotion';
@@ -22,8 +21,7 @@ const styles = {
   marginLeftM: css({ marginLeft: tokens.spacingM })
 };
 
-const createFieldData = (initialValue = '') => ({
-  touched: false,
+const createFieldData = (initialValue = null) => ({
   dirty: false,
   value: initialValue,
   serverValidationMessage: null
@@ -36,30 +34,29 @@ const initializeReducer = user => {
       lastName: createFieldData(user.lastName),
       email: createFieldData(user.email),
       currentPassword: createFieldData(),
+      newPassword: createFieldData(),
+      newPasswordConfirm: createFieldData(),
       logAnalyticsFeature: createFieldData(user.logAnalyticsFeature)
     },
-    formInvalid: false,
-    submitting: false
+    formInvalid: false
   };
 };
 
 const reducer = createImmerReducer({
   UPDATE_FIELD_VALUE: (state, { payload }) => {
     state.fields[payload.field].value = payload.value;
-    state.fields[payload.field].dirty = true;
     state.formInvalid = false;
     state.fields[payload.field].serverValidationMessage = null;
   },
-  SET_FIELD_TOUCHED: (state, { payload }) => {
-    state.fields[payload.field].touched = true;
+  SET_FIELD_DIRTY: (state, { payload }) => {
+    if (state.fields[payload.field].value) {
+      state.fields[payload.field].dirty = true;
+    }
   },
-  SET_ALL_FIELDS_TOUCHED: state => {
+  SET_ALL_FIELDS_DIRTY: state => {
     Object.values(state.fields).forEach(fieldData => {
-      fieldData.touched = true;
+      fieldData.dirty = true;
     });
-  },
-  SET_SUBMITTING: (state, { payload }) => {
-    state.submitting = payload;
   },
   SET_FORM_INVALID: state => {
     state.formInvalid = true;
@@ -73,97 +70,56 @@ const reducer = createImmerReducer({
 });
 
 const submitForm = async (formData, user, dispatch, onConfirm) => {
-  dispatch({ type: 'SET_SUBMITTING', payload: true });
-
   const fieldData = formData.fields;
-  let response;
 
-  try {
-    response = await updateUserData({
-      version: user.sys.version,
-      data: {
-        firstName: fieldData.firstName.value,
-        lastName: fieldData.lastName.value,
-        email: fieldData.email.value,
-        currentPassword: fieldData.currentPassword.value,
-        logAnalyticsFeature: fieldData.logAnalyticsFeature.value
+  const updatedUserData = await updateUserData({
+    version: user.sys.version,
+    data: {
+      firstName: fieldData.firstName.value,
+      lastName: fieldData.lastName.value,
+      email: fieldData.email.value,
+      password: fieldData.newPassword.value,
+      currentPassword: fieldData.currentPassword.value,
+      logAnalyticsFeature: fieldData.logAnalyticsFeature.value
+    }
+  });
+
+  if (updatedUserData.sys.type === 'User') {
+    dispatch({ type: 'RESET', payload: { user: updatedUserData } });
+    onConfirm(updatedUserData);
+  } else if (updatedUserData.sys.type === 'Error') {
+    const errorDetails = updatedUserData.details.errors;
+
+    errorDetails.forEach(({ path, name }) => {
+      const pathFieldMapping = {
+        password: 'newPassword',
+        current_password: 'currentPassword',
+        email: 'email'
+      };
+      let message;
+
+      if (path === 'password') {
+        if (name === 'insecure') {
+          message = 'The password you entered is not secure';
+        }
+      } else if (path === 'current_password') {
+        if (name === 'invalid') {
+          message = 'The password you entered is not valid';
+        }
+      } else if (path === 'email') {
+        if (name === 'invalid') {
+          message = 'The email you entered is not valid';
+        }
+      }
+
+      if (message) {
+        dispatch({
+          type: 'SERVER_VALIDATION_FAILURE',
+          payload: { field: pathFieldMapping[path], value: message }
+        });
       }
     });
-  } catch (err) {
-    const { data } = err;
-
-    if (data.sys && data.sys.type === 'Error') {
-      const errorDetails = data.details.errors;
-
-      errorDetails.forEach(({ path, name }) => {
-        const pathFieldMapping = {
-          first_name: 'firstName',
-          last_name: 'lastName',
-          password: 'newPassword',
-          current_password: 'currentPassword',
-          email: 'email'
-        };
-        let message;
-
-        switch (path) {
-          case 'first_name': {
-            if (name === 'length') {
-              message = 'The first name you entered is too long';
-            }
-            break;
-          }
-          case 'last_name': {
-            if (name === 'length') {
-              message = 'The last name you entered is too long';
-            }
-            break;
-          }
-          case 'email': {
-            if (name === 'invalid') {
-              message = 'The email you entered is not valid';
-            }
-            break;
-          }
-          case 'password': {
-            if (name === 'insecure') {
-              message = 'The password you entered is not secure';
-            }
-            break;
-          }
-          case 'current_password': {
-            if (name === 'invalid') {
-              message = 'The password you entered is not valid';
-            }
-            break;
-          }
-        }
-
-        if (message) {
-          dispatch({
-            type: 'SERVER_VALIDATION_FAILURE',
-            payload: { field: pathFieldMapping[path], value: message }
-          });
-        }
-      });
-    } else {
-      Notification.error('Something went wrong. Try again.');
-    }
-
-    dispatch({ type: 'SET_SUBMITTING', payload: false });
-
-    return;
   }
-
-  // Show a notification telling the user to check their inbox if the unconfirmed email in the response is the same
-  // as the email in the formData
-  if (response.unconfirmedEmail === formData.fields.email.value) {
-    Notification.warning('Check your inbox to confirm your new email.');
-  }
-
-  Notification.success('Your profile has been successfully updated.');
-
-  dispatch({ type: 'RESET', payload: { user: response } });
-  onConfirm(response);
 };
 
 export default function UserEditModal({ user, onConfirm, onCancel, isShown }) {
@@ -186,16 +142,10 @@ export default function UserEditModal({ user, onConfirm, onCancel, isShown }) {
   };
 
   const fields = formData.fields;
-  const currentPasswordIsRequired = user.passwordSet && fields.email.dirty;
-  const userHasPassword = user.passwordSet;
-  const submitButtonDisabled =
-    !Object.values(formData.fields).find(field => field.dirty) ||
-    formData.formInvalid ||
-    formData.submitting;
+  const currentPasswordIsRequired = fields.email.value && fields.email.dirty;
 
   return (
     <Modal
-      testId="edit-account-data-modal"
       title="Edit account"
       shouldCloseOnEscapePress={true}
       shouldCloseOnOverlayClick={true}
@@ -210,7 +160,6 @@ export default function UserEditModal({ user, onConfirm, onCancel, isShown }) {
           required
           validationMessage={getValidationMessageFor(formData.fields, 'firstName')}
           id="first-name-field"
-          testId="first-name-field"
           name="first-name"
           value={fields.firstName.value}
           onChange={e =>
@@ -219,19 +168,14 @@ export default function UserEditModal({ user, onConfirm, onCancel, isShown }) {
               payload: { field: 'firstName', value: e.target.value }
             })
           }
-          onBlur={() => dispatch({ type: 'SET_FIELD_TOUCHED', payload: { field: 'firstName' } })}
+          onBlur={() => dispatch({ type: 'SET_FIELD_DIRTY', payload: { field: 'firstName' } })}
           labelText="First Name"
-          textInputProps={{
-            type: 'text',
-            autoComplete: 'off',
-            placeholder: 'Felix'
-          }}
+          textInputProps={{ type: 'text', autoComplete: 'off' }}
         />
         <TextField
           required
           validationMessage={getValidationMessageFor(formData.fields, 'lastName')}
           id="last-name-field"
-          testId="last-name-field"
           name="last-name"
           value={fields.lastName.value}
           onChange={e =>
@@ -240,19 +184,13 @@ export default function UserEditModal({ user, onConfirm, onCancel, isShown }) {
               payload: { field: 'lastName', value: e.target.value }
             })
           }
-          onBlur={() => dispatch({ type: 'SET_FIELD_TOUCHED', payload: { field: 'lastName' } })}
+          onBlur={() => dispatch({ type: 'SET_FIELD_DIRTY', payload: { field: 'lastName' } })}
           labelText="Last Name"
-          textInputProps={{
-            type: 'text',
-            autoComplete: 'off',
-            placeholder: 'Müller'
-          }}
+          textInputProps={{ type: 'text', autoComplete: 'off' }}
         />
         <TextField
           required
-          validationMessage={getValidationMessageFor(formData.fields, 'email')}
           id="email-field"
-          testId="email-field"
           name="email"
           value={fields.email.value}
           onChange={e =>
@@ -261,18 +199,44 @@ export default function UserEditModal({ user, onConfirm, onCancel, isShown }) {
               payload: { field: 'email', value: e.target.value }
             })
           }
-          onBlur={() => dispatch({ type: 'SET_FIELD_TOUCHED', payload: { field: 'email' } })}
+          onBlur={() => dispatch({ type: 'SET_FIELD_DIRTY', payload: { field: 'email' } })}
           labelText="Email"
-          textInputProps={{
-            type: 'email',
-            autoComplete: 'off',
-            placeholder: 'felix.mueller@example.com'
-          }}
-          helpText={
-            userHasPassword && fields.email.dirty
-              ? 'Enter your password to confirm your updated email.'
-              : ''
+          textInputProps={{ type: 'email', autoComplete: 'off' }}
+          helpText="To confirm email changes enter your current newPassword and don’t forget to confirm the new email, you will find a confirmation link in your inbox soon."
+        />
+        <Subheading>Change Password</Subheading>
+        <TextField
+          validationMessage={getValidationMessageFor(formData.fields, 'newPassword')}
+          id="new-password-field"
+          name="new-password"
+          value={fields.newPassword.value}
+          onChange={e =>
+            dispatch({
+              type: 'UPDATE_FIELD_VALUE',
+              payload: { field: 'newPassword', value: e.target.value }
+            })
           }
+          onBlur={() => dispatch({ type: 'SET_FIELD_DIRTY', payload: { field: 'newPassword' } })}
+          labelText="New Password"
+          textInputProps={{ type: 'password', autoComplete: 'off' }}
+          helpText="Create a unique password at least 8 characters long"
+        />
+        <TextField
+          validationMessage={getValidationMessageFor(formData.fields, 'newPasswordConfirm')}
+          id="confirm-new-password-field"
+          name="confirm-new-password"
+          value={fields.newPasswordConfirm.value}
+          onChange={e =>
+            dispatch({
+              type: 'UPDATE_FIELD_VALUE',
+              payload: { field: 'newPasswordConfirm', value: e.target.value }
+            })
+          }
+          onBlur={() =>
+            dispatch({ type: 'SET_FIELD_DIRTY', payload: { field: 'newPasswordConfirm' } })
+          }
+          labelText="Confirm new password"
+          textInputProps={{ type: 'password', autoComplete: 'off' }}
         />
         {currentPasswordIsRequired && (
           <>
@@ -281,7 +245,6 @@ export default function UserEditModal({ user, onConfirm, onCancel, isShown }) {
               required
               validationMessage={getValidationMessageFor(formData.fields, 'currentPassword')}
               id="current-password-field"
-              testId="current-password-field"
               name="current-password"
               value={fields.currentPassword.value}
               onChange={e =>
@@ -291,7 +254,7 @@ export default function UserEditModal({ user, onConfirm, onCancel, isShown }) {
                 })
               }
               onBlur={() =>
-                dispatch({ type: 'SET_FIELD_TOUCHED', payload: { field: 'currentPassword' } })
+                dispatch({ type: 'SET_FIELD_DIRTY', payload: { field: 'currentPassword' } })
               }
               labelText="Current Password"
               textInputProps={{ type: 'password', autoComplete: 'off' }}
@@ -300,7 +263,6 @@ export default function UserEditModal({ user, onConfirm, onCancel, isShown }) {
         )}
         <CheckboxField
           labelText="Allow Contentful to send information to external providers to help us improve the service"
-          labelIsLight
           value={fields.logAnalyticsFeature.value ? 'yes' : 'no'}
           onChange={() =>
             dispatch({
@@ -313,13 +275,11 @@ export default function UserEditModal({ user, onConfirm, onCancel, isShown }) {
         />
         <div className={styles.controlsPanel}>
           <Button
-            testId="confirm-account-data-changes"
             onClick={() => {
               const formIsValid = validateForm(formData);
               formIsValid && submitForm(formData, user, dispatch, onConfirm);
             }}
             type="submit"
-            disabled={submitButtonDisabled}
             buttonType="positive">
             Save changes
           </Button>
