cda99374e2a1b74d5ba7dd8fe924b816ae38a36f
Johannes Bugiel <jo.bugiel@gmail.com>
2019-10-09 15:54:47 +0200

    refactor(UserProfile): Add F36 Workbench

--- src/javascripts/app/UserProfile/Settings/index.js
+++ src/javascripts/app/UserProfile/Settings/index.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Card } from '@contentful/forma-36-react-components';
-import Workbench from 'app/common/Workbench.es6';
+import { Workbench } from '@contentful/forma-36-react-components/dist/alpha';
 import DocumentTitle from 'components/shared/DocumentTitle.es6';
 import tokens from '@contentful/forma-36-tokens';
 import { css } from 'emotion';
@@ -15,7 +15,10 @@ import LoadingState from 'app/common/LoadingState';
 
 const styles = {
   content: css({
-    height: '100%'
+    height: '100%',
+    '> div': {
+      height: '100%'
+    }
   }),
   section: css({
     maxWidth: '768px',
@@ -33,9 +36,7 @@ export default function IndexPage({ title, onReady }) {
     <>
       <DocumentTitle title={title} />
       <Workbench>
-        <Workbench.Header>
-          <Workbench.Title>{title}</Workbench.Title>
-        </Workbench.Header>
+        <Workbench.Header title={title} />
         <Workbench.Content className={styles.content}>
           {isLoading && <LoadingState loadingText="Loading your account…" />}
           {!isLoading && error && <ErrorState />}
