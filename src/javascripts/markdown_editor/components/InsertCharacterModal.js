80ac8d582d09a99ff35ce91f84bdddf54617c9c3
Alexander Suevalov <alex.suevalov@contentful.com>
2019-11-07 16:47:07 +0100

    Fix wrong import causing errors in Karma (#5084)

--- src/javascripts/markdown_editor/components/InsertCharacterModal.js
+++ src/javascripts/markdown_editor/components/InsertCharacterModal.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { css } from 'emotion';
 import tokens from '@contentful/forma-36-tokens';
 import { Modal, Button, Tooltip, DisplayText } from '@contentful/forma-36-react-components';
-import specialCharacters from '../markdown_special_characters.es6.js';
+import specialCharacters from '../markdown_special_characters.es6';
 
 const styles = {
   buttonPanel: css({
