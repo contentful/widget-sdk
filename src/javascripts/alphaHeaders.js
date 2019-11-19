570341bb7ace5d102ae4c619a05cf18806422e22
Guilherme Barbosa <guipvb@gmail.com>
2019-11-19 14:33:41 +0100

    Move MFA alpha header

--- src/javascripts/alphaHeaders.js
+++ src/javascripts/alphaHeaders.js
@@ -1,3 +1,3 @@
 export const SUBSCRIPTIONS_API = 'subscriptions-api'; // Dec 14, 2017
-
+export const MFA_API = 'mfa-api'; // Oct 23, 2019
 export const getAlphaHeader = value => ({ 'x-contentful-enable-alpha-feature': value });
