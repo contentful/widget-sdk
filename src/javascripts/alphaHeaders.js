155a5d4066c843090acefd21137750761e4de178
Guilherme Barbosa <guipvb@gmail.com>
2019-11-19 17:02:53 +0100

    Move environment aliasing alpha header

--- src/javascripts/alphaHeaders.js
+++ src/javascripts/alphaHeaders.js
@@ -3,6 +3,7 @@ export const USAGE_INSIGHTS = 'usage-insights'; // Sep 11, 2018
 export const ORG_USER_MANAGEMENT_API = 'organization-user-management-api'; // Sep 12, 2018
 export const TEAMS_API = 'teams-api'; // Dec 05, 2018
 export const COMMENTS_API = 'comments-api'; // Apr 30, 2019
+export const ENVIRONMENT_ALIASING = 'environment-aliasing'; // Jul 25, 2019
 export const SCHEDULED_JOBS = 'scheduled-jobs'; // Jun 06, 2019
 export const PENDING_ORG_MEMBERSHIP = 'pending-org-membership'; // Sep 12, 2019
 export const TASKS_DASHBOARD = 'tasks-dashboard'; // Oct 23, 2019
