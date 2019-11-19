f0fab0aa89c12d32fe8f9e52a0604be9a5296e03
Guilherme Barbosa <guipvb@gmail.com>
2019-11-19 16:52:39 +0100

    Move user mgmt api and pending org memberships

--- src/javascripts/alphaHeaders.js
+++ src/javascripts/alphaHeaders.js
@@ -1,6 +1,8 @@
 export const SUBSCRIPTIONS_API = 'subscriptions-api'; // Dec 14, 2017
+export const ORG_USER_MANAGEMENT_API = 'organization-user-management-api'; // Sep 12, 2018
 export const TEAMS_API = 'teams-api'; // Dec 05, 2018
 export const COMMENTS_API = 'comments-api'; // Apr 30, 2019
+export const PENDING_ORG_MEMBERSHIP = 'pending-org-membership'; // Sep 12, 2019
 export const TASKS_DASHBOARD = 'tasks-dashboard'; // Oct 23, 2019
 export const MFA_API = 'mfa-api'; // Oct 23, 2019
 
