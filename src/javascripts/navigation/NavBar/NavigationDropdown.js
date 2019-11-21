a002620906ee914b0b8cbcd6bcd4fc23f23e4f42
Johannes Bugiel <jo.bugiel@gmail.com>
2019-11-21 13:26:56 +0100

    fix(NavBar): Disable auto alignment and fix markup (#5117)
    * fix(NavBar): Disable auto alignment and fix markup
    
    * update snapshots

--- src/javascripts/navigation/NavBar/NavigationDropdown.js
+++ src/javascripts/navigation/NavBar/NavigationDropdown.js
@@ -48,71 +48,74 @@ export default function NavigationDropdown(props) {
   };
 
   return (
-    <Dropdown
-      isOpen={isOpen}
-      onClose={onClose}
-      className="app-top-bar__action"
-      position="bottom-left"
-      toggleElement={
-        <a
-          className={cn('nav-bar__link', 'app-top-bar__menu-trigger', {
-            'is-active': Navigator.includes({ path: item.rootSref || item.sref })
-          })}
-          role="button"
-          tabIndex="0"
-          data-view-type={item.dataViewType}
-          onClick={onOpen}
-          onKeyDown={e => {
-            if (e.keyCode === keycodes.ENTER) {
-              onOpen();
+    <li className="app-top-bar__action nav-bar__list-item">
+      <Dropdown
+        isOpen={isOpen}
+        onClose={onClose}
+        isAutoalignmentEnabled={false}
+        className="app-top-bar__action"
+        position="bottom-left"
+        toggleElement={
+          <a
+            className={cn('nav-bar__link', 'app-top-bar__menu-trigger', {
+              'is-active': Navigator.includes({ path: item.rootSref || item.sref })
+            })}
+            role="button"
+            tabIndex="0"
+            data-view-type={item.dataViewType}
+            onClick={onOpen}
+            onKeyDown={e => {
+              if (e.keyCode === keycodes.ENTER) {
+                onOpen();
+              }
+            }}>
+            <span className="nav-bar__list-label">
+              {item.icon && <Icon name={item.icon} />}
+              {item.title}
+            </span>
+            <span className={cn('triangle-down', styles.triangleArrow)} />
+          </a>
+        }>
+        <DropdownList className={styles.dropdownList} testId="navbar-dropdown-menu">
+          {item.children.map((subitem, index) => {
+            if (subitem.separator) {
+              return (
+                <DropdownListItem
+                  key={subitem.label}
+                  isTitle
+                  className={index !== 0 ? styles.separator : ''}>
+                  {subitem.label}
+                  {subitem.tooltip && (
+                    <Tooltip place="bottom" content={subitem.tooltip} className={styles.tooltip}>
+                      <i className={cn('fa', 'fa-question-circle', styles.question)}></i>
+                    </Tooltip>
+                  )}
+                </DropdownListItem>
+              );
             }
-          }}>
-          <span className="nav-bar__list-label">
-            {item.icon && <Icon name={item.icon} />}
-            {item.title}
-          </span>
-          <span className={cn('triangle-down', styles.triangleArrow)} />
-        </a>
-      }>
-      <DropdownList className={styles.dropdownList} testId="navbar-dropdown-menu">
-        {item.children.map((subitem, index) => {
-          if (subitem.separator) {
             return (
               <DropdownListItem
-                key={subitem.label}
-                isTitle
-                className={index !== 0 ? styles.separator : ''}>
-                {subitem.label}
-                {subitem.tooltip && (
-                  <Tooltip place="bottom" content={subitem.tooltip} className={styles.tooltip}>
-                    <i className={cn('fa', 'fa-question-circle', styles.question)}></i>
-                  </Tooltip>
-                )}
+                data-view-type={subitem.dataViewType}
+                key={subitem.title}
+                isActive={Navigator.includes({ path: subitem.rootSref || subitem.sref })}
+                onClick={() => {
+                  Navigator.go({
+                    path: subitem.sref,
+                    params: item.srefParams || {},
+                    options: {
+                      inherit: true,
+                      ...(item.srefOptions || {})
+                    }
+                  });
+                  onClose();
+                }}>
+                {subitem.title}
               </DropdownListItem>
             );
-          }
-          return (
-            <DropdownListItem
-              data-view-type={subitem.dataViewType}
-              key={subitem.title}
-              isActive={Navigator.includes({ path: subitem.rootSref || subitem.sref })}
-              onClick={() => {
-                Navigator.go({
-                  path: subitem.sref,
-                  params: item.srefParams || {},
-                  options: {
-                    inherit: true,
-                    ...(item.srefOptions || {})
-                  }
-                });
-                onClose();
-              }}>
-              {subitem.title}
-            </DropdownListItem>
-          );
-        })}
-      </DropdownList>
-    </Dropdown>
+          })}
+        </DropdownList>
+      </Dropdown>
+    </li>
   );
 }
 
