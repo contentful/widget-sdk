e2d7fa3f09dd5413948a2ec00366a8fae399c9e0
Jakub Elżbieciak <jakub@contentful.com>
2019-11-22 10:51:03 +0100

    Cache extension loader per environment

--- src/javascripts/widgets/ExtensionLoaderInstance.js
+++ src/javascripts/widgets/ExtensionLoaderInstance.js
@@ -1,16 +1,21 @@
 import { getModule } from 'NgRegistry';
+import { get, set } from 'lodash';
 import { createExtensionLoader } from './ExtensionLoader';
 import { getAppDefinitionLoader } from 'app/settings/AppsBeta/AppDefinitionLoaderInstance';
 
-const perSpaceCache = {};
+const perSpaceEnvCache = {};
 
 export function getExtensionLoader() {
   const spaceContext = getModule('spaceContext');
   const spaceId = spaceContext.getId();
+  const environmentId = spaceContext.getEnvironmentId();
 
-  if (!perSpaceCache[spaceId]) {
-    perSpaceCache[spaceId] = createExtensionLoader(getAppDefinitionLoader(), spaceContext.endpoint);
+  let loader = get(perSpaceEnvCache, [spaceId, environmentId]);
+
+  if (!loader) {
+    loader = createExtensionLoader(getAppDefinitionLoader(), spaceContext.endpoint);
+    set(perSpaceEnvCache, [spaceId, environmentId], loader);
   }
 
-  return perSpaceCache[spaceId];
+  return loader;
 }
