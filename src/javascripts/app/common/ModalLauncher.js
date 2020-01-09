bd7ba33c55ce6c980944bf790fe20de827537a2a
Alexander Suevalov <alex.suevalov@contentful.com>
2020-01-09 14:33:02 +0100

    Allow to open several modal windows (#5240)

--- src/javascripts/app/common/ModalLauncher.js
+++ src/javascripts/app/common/ModalLauncher.js
@@ -1,17 +1,17 @@
 import ReactDOM from 'react-dom';
 
-let rootDom = null;
-
-const getRoot = () => {
-  if (rootDom === null) {
-    rootDom = document.createElement('div');
-    rootDom.setAttribute('id', 'modals-root');
-    document.body.appendChild(rootDom);
-  }
-  return rootDom;
-};
-
 export function open(componentRenderer) {
+  let rootDom = null;
+
+  const getRoot = () => {
+    if (rootDom === null) {
+      rootDom = document.createElement('div');
+      rootDom.setAttribute('id', 'modals-root' + Date.now().toString());
+      document.body.appendChild(rootDom);
+    }
+    return rootDom;
+  };
+
   return new Promise(resolve => {
     let currentConfig = { onClose, isShown: true };
 
@@ -26,6 +26,7 @@ export function open(componentRenderer) {
       };
       render(currentConfig);
       resolve(...args);
+      getRoot().remove();
     }
 
     render(currentConfig);
