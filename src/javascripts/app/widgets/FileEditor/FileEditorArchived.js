7acc41ff2919c365770ee8b1cd02ee0b73d264a5
Bruce Felt <bruce.felt@contentful.com>
2020-01-22 16:16:02 +0100

    Drop unnecessary bits in FileEditorArchived

--- src/javascripts/app/widgets/FileEditor/FileEditorArchived.js
+++ src/javascripts/app/widgets/FileEditor/FileEditorArchived.js
@@ -5,7 +5,7 @@ import Thumbnail from 'components/Thumbnail/Thumbnail';
 export function FileEditorArchived(props) {
   return (
     <div className="file-archived">
-      <Thumbnail file={props.file} height="60" icon />{' '}
+      <Thumbnail file={props.file} icon />
       <span className="message">Preview unavailable for archived assets</span>
     </div>
   );
