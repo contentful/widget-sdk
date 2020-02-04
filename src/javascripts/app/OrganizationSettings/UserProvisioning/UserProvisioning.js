f0b58f45a99e549e4f082d0266f1022ed74f6159
Victoria Beleuta <vbeleuta@gmail.com>
2020-02-04 18:08:27 +0100

    Styling new User provisioning page

--- src/javascripts/app/OrganizationSettings/UserProvisioning/UserProvisioning.js
+++ src/javascripts/app/OrganizationSettings/UserProvisioning/UserProvisioning.js
@@ -1,60 +1,95 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Workbench } from '@contentful/forma-36-react-components/dist/alpha';
+import {
+  TextLink,
+  TextInput,
+  Heading,
+  Paragraph,
+  Note,
+  Button
+} from '@contentful/forma-36-react-components';
 import Icon from 'ui/Components/Icon';
-import { getOrgFeature } from 'data/CMA/ProductCatalog';
 import OrgAdminOnly from 'app/common/OrgAdminOnly';
-import StateRedirect from 'app/common/StateRedirect';
-import ForbiddenState from 'app/common/ForbiddenState';
-import UserProvisioningConfiguration from './UserProvisioningConfiguration';
-import createFetcherComponent, { FetcherLoading } from 'app/common/createFetcherComponent';
+import { css } from 'emotion';
+import tokens from '@contentful/forma-36-tokens';
 
-const FeatureFetcher = createFetcherComponent(async ({ orgId }) => {
-  const featureEnabled = await getOrgFeature(orgId, 'scim');
-  return { featureEnabled: featureEnabled };
-});
+const styles = {
+  content: css({
+    width: '700px',
+    display: 'flex',
+    margin: '0 280px',
+    fontFmily: 'Avenir Next',
+    lineHeight: '21px',
+    fontSize: tokens.fontSizeM
+  }),
+  heading: css({
+    marginBottom: tokens.spacingM,
+    marginTop: tokens.spacing2Xl
+  }),
+  subheading: css({
+    height: '21px',
+    width: '161px',
+    color: '#2A3039',
+    fontWeight: tokens.fontWeightMedium,
+    marginBottom: tokens.spacinXs,
+    marginTop: tokens.spacingL
+  }),
+  paragraph: css({
+    color: '#536171',
+    marginBottom: tokens.spacingM
+  })
+};
 
-export default class UserProvisioning extends Component {
-  static propTypes = {
-    orgId: PropTypes.string.isRequired,
-    onReady: PropTypes.func.isRequired
-  };
+const UserProvisioning = ({ orgId, onReady }) => {
+  onReady();
 
-  componentDidMount() {
-    this.props.onReady();
-  }
+  return (
+    <OrgAdminOnly orgId={orgId}>
+      <Workbench>
+        <Workbench.Header icon={<Icon name="page-sso" scale="0.75" />} title="User Provisoning" />
+        <Workbench.Content className={styles.content}>
+          <div className="setup-main">
+            <Heading element="h1" className={styles.heading}>
+              Set up user provisioning with SCIM 2.0
+            </Heading>
+            <Paragraph className={styles.paragraph}>
+              Set up user provisioning for your organization in Contentful in a few
+              steps.&nbsp;&nbsp;
+              <TextLink href="https://www.contentful.com/faq/">Check out the FAQs.</TextLink>
+            </Paragraph>
+            <Note noteType="primary">
+              We strongly recommend using a service account for setting up user provisioning to
+              better manage the organization access in Contentful.&nbsp;&nbsp;
+              <TextLink>Show more</TextLink>
+            </Note>
+          </div>
+          <div className="configuration-details">
+            <Heading element="h1" className={styles.heading}>
+              SCIM configuration details
+            </Heading>
+            <TextInput
+              name="scim-url"
+              testId="scim-url"
+              disabled
+              withCopyButton
+              value="https://app.contentful.com"
+            />
+            <div className={styles.subheading}>Personal Access Token</div>
+            <Paragraph className={styles.paragraph}>
+              As an alternative to OAuth applications, you can also leverage Personal Access Tokens
+              to use the Content Management API. These tokens are always bound to your individual
+              account, with the same permissions you have on all of your spaces and organizations.
+            </Paragraph>
+            <Button>Generate personal access token</Button>
+          </div>
+        </Workbench.Content>
+      </Workbench>
+    </OrgAdminOnly>
+  );
+};
 
-  render() {
-    const { orgId } = this.props;
-
-    return (
-      <OrgAdminOnly orgId={orgId}>
-        <FeatureFetcher orgId={orgId}>
-          {({ isLoading, isError, data }) => {
-            if (isLoading) {
-              return <FetcherLoading message="Loading..." />;
-            }
-            if (isError) {
-              return <StateRedirect path="home" />;
-            }
-            if (!data.featureEnabled) {
-              return <ForbiddenState />;
-            }
-
-            return (
-              <Workbench>
-                <Workbench.Header
-                  icon={<Icon name="page-sso" scale="0.75" />}
-                  title="User Provisioning"
-                />
-                <Workbench.Content>
-                  <UserProvisioningConfiguration orgId={orgId} />
-                </Workbench.Content>
-              </Workbench>
-            );
-          }}
-        </FeatureFetcher>
-      </OrgAdminOnly>
-    );
-  }
-}
+UserProvisioning.propTypes = {
+  orgId: PropTypes.string.isRequired,
+  onReady: PropTypes.func.isRequired
+};
