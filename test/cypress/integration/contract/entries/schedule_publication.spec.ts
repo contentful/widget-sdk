22579e975f9a2396879b705c474984362dd557ab
ronaldronson <ronaldronson@users.noreply.github.com>
2020-02-17 11:25:41 +0100

    [PUL-607] Feat: increase limit of pending jobs (#5416)
    * feat: increase limit of pending jobs

--- test/cypress/integration/contract/entries/schedule_publication.spec.ts
+++ test/cypress/integration/contract/entries/schedule_publication.spec.ts
@@ -75,7 +75,7 @@ describe('Schedule Publication', () => {
       cy.wait(500); // extra wait for notification animation 500ms
       cy.getAllByTestId('cf-ui-notification')
         // .should('be.visible') // This is hidden by the modal
-        .should('contain', 'There is a limit of 100 scheduled entries');
+        .should('contain', 'There is a limit of 200 scheduled entries');
     });
   });
 
