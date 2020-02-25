798aebedb08ab91f8e25c2d6e9d09ceaf0e63eda
Moe Shaaban <info@mshaaban.com>
2020-02-25 14:51:28 +0100

    feat: abstract usage page tabs

--- src/javascripts/account/usage/committed/OrganisationUsagePageNew.js
+++ src/javascripts/account/usage/committed/OrganisationUsagePageNew.js
@@ -3,12 +3,9 @@ import { Tabs, Tab, TabPanel, Heading } from '@contentful/forma-36-react-compone
 import tokens from '@contentful/forma-36-tokens';
 import { css } from 'emotion';
 import { Grid, GridItem } from './common/Grid';
+import UsageTabs from './common/UsageTabs';
 
 const styles = {
-  tabPanel: css({
-    paddingTop: tokens.spacing2Xl,
-    paddingBottom: tokens.spacing2Xl
-  }),
   panelHeading: css({
     fontWeight: tokens.fontWeightMedium,
     color: '#536171'
@@ -20,122 +17,73 @@ const styles = {
   })
 };
 
-const OrgTabs = props => {
-  const [selected, setSelected] = useState('apiRequest');
-
-  const handleSelected = id => {
-    setSelected(id);
-  };
-
-  return (
-    <>
-      <Tabs withDivider={true}>
-        <Tab id="apiRequest" selected={selected === 'apiRequest'} onSelect={handleSelected}>
-          API Requests
-        </Tab>
-        <Tab id="assetBandwidth" selected={selected === 'assetBandwidth'} onSelect={handleSelected}>
-          Asset Bandwidth
-        </Tab>
-      </Tabs>
-      {selected === 'apiRequest' && (
-        <TabPanel id="apiRequest" className={styles.tabPanel}>
-          <Grid columns={'repeat(auto-fill, minmax(100px, 1fr))'}>
-            <GridItem columnStart={1} columnEnd={5}>
-              <Heading element="h2" className={styles.panelHeading}>
-                Total API requests
-              </Heading>
-            </GridItem>
-            <GridItem columnStart={5} columnEnd={12}>
-              CHART
-            </GridItem>
-          </Grid>
-        </TabPanel>
-      )}
-      {selected === 'assetBandwidth' && (
-        <TabPanel id="assetBandwidth" className={styles.tabPanel}>
-          content asset bandwidth chart
-        </TabPanel>
-      )}
-    </>
-  );
-};
+const OrgTabs = props => (
+  <>
+    <UsageTabs
+      tabsData={[
+        {
+          id: 'apiRequest',
+          title: 'API Requests',
+          defaultActive: true,
+          leftComponent: (
+            <Heading element="h2" className={styles.panelHeading}>
+              Total API requests
+            </Heading>
+          ),
+          rightComponent: <div>API Requests</div>
+        },
+        {
+          id: 'assetBandwidth',
+          title: 'Asset Bandwidth',
+          leftComponent: (
+            <Heading element="h2" className={styles.panelHeading}>
+              Total API requests
+            </Heading>
+          ),
+          rightComponent: <div>Asset Bandwidth</div>
+        }
+      ]}
+    />
+  </>
+);
 
 const SpacesTabs = props => {
-  const [selected, setSelected] = useState('cmaRequests');
-
-  const handleSelected = id => {
-    setSelected(id);
-  };
-
   return (
     <>
       <Heading element="h2" className={styles.spacesHeading}>
         View API requests by type and space
       </Heading>
-      <Tabs withDivider={true}>
-        <Tab id="cmaRequests" selected={selected === 'cmaRequests'} onSelect={handleSelected}>
-          CMA Requests
-        </Tab>
-        <Tab id="cdaRequests" selected={selected === 'cdaRequests'} onSelect={handleSelected}>
-          CDA Requests
-        </Tab>
-        <Tab id="cpaRequests" selected={selected === 'cpaRequests'} onSelect={handleSelected}>
-          CPA Requests
-        </Tab>
-        <Tab id="gqlRequests" selected={selected === 'gqlRequests'} onSelect={handleSelected}>
-          GraphQL Requests
-        </Tab>
-      </Tabs>
-      {selected === 'cmaRequests' && (
-        <TabPanel id="cmaRequests" className={styles.tabPanel}>
-          <Grid columns={'repeat(auto-fill, minmax(100px, 1fr))'}>
-            <GridItem columnStart={1} columnEnd={5}>
-              <Heading element="h2" className={styles.panelHeading}>
-                Total API requests
-              </Heading>
-            </GridItem>
-            <GridItem columnStart={5} columnEnd={12}>
-              CHART
-            </GridItem>
-          </Grid>
-        </TabPanel>
-      )}
-      {selected === 'cdaRequests' && (
-        <TabPanel id="cdaRequests" className={styles.tabPanel}>
-          <Grid columns={'repeat(auto-fill, minmax(100px, 1fr))'}>
-            <GridItem columnStart={1} columnEnd={5}>
-              Table
-            </GridItem>
-            <GridItem columnStart={5} columnEnd={12}>
-              CHART
-            </GridItem>
-          </Grid>
-        </TabPanel>
-      )}
-      {selected === 'cpaRequests' && (
-        <TabPanel id="cpaRequests" className={styles.tabPanel}>
-          <Grid columns={'repeat(auto-fill, minmax(100px, 1fr))'}>
-            <GridItem columnStart={1} columnEnd={5}>
-              Table
-            </GridItem>
-            <GridItem columnStart={5} columnEnd={12}>
-              CHART
-            </GridItem>
-          </Grid>
-        </TabPanel>
-      )}
-      {selected === 'gqlRequests' && (
-        <TabPanel id="gqlRequests" className={styles.tabPanel}>
-          <Grid columns={'repeat(auto-fill, minmax(100px, 1fr))'}>
-            <GridItem columnStart={1} columnEnd={5}>
-              Table
-            </GridItem>
-            <GridItem columnStart={5} columnEnd={12}>
-              CHART
-            </GridItem>
-          </Grid>
-        </TabPanel>
-      )}
+
+      <UsageTabs
+        tabsData={[
+          {
+            id: 'cmaRequests',
+            title: 'CMA Request',
+            defaultActive: true,
+            leftComponent: <div>Table</div>,
+            rightComponent: <div>CMA Chart</div>
+          },
+          {
+            id: 'cdaRequests',
+            title: 'CDA Request',
+            leftComponent: <div>Table</div>,
+            rightComponent: <div>CDA Chart</div>
+          },
+          {
+            id: 'cpaRequests',
+            title: 'CPA Request',
+            leftComponent: <div>Table</div>,
+            rightComponent: <div>CPA Chart</div>
+          },
+          {
+            id: 'gqlRequests',
+            title: 'GraphQL Request',
+            defaultActive: true,
+            leftComponent: <div>Table</div>,
+            rightComponent: <div>GraphQL Chart</div>
+          }
+        ]}
+      />
     </>
   );
 };
---
+++ src/javascripts/account/usage/committed/common/UsageTabs.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { Tabs, Tab, TabPanel } from '@contentful/forma-36-react-components';
+import tokens from '@contentful/forma-36-tokens';
+import { Grid, GridItem } from './Grid';
+import { css } from 'emotion';
+import PropTypes from 'prop-types';
+
+const styles = {
+  tabPanel: css({
+    paddingTop: tokens.spacing2Xl,
+    paddingBottom: tokens.spacing2Xl
+  })
+};
+
+const UsageTabs = (props) => {
+  const { tabsData } = props;
+  const defaultActiveTab = tabsData && tabsData.find(item => item.defaultActive);
+  const [selected, setSelected] = useState(
+    defaultActiveTab ? defaultActiveTab.id : tabsData[0]['id']
+  );
+
+  const handleSelected = id => {
+    setSelected(id);
+  };
+
+  return (
+    <>
+      <Tabs withDivider={true}>
+        {tabsData &&
+          tabsData.map(item => (
+            <Tab
+              id={item.id}
+              key={item.id}
+              selected={selected === item.id}
+              onSelect={handleSelected}>
+              {item.title}
+            </Tab>
+          ))}
+      </Tabs>
+      {tabsData &&
+        tabsData.map((item, idx) => (
+          <>
+            {selected === item.id && (
+              <TabPanel key={`${item.id}${idx}`} id={item.id} className={styles.tabPanel}>
+                <Grid columns={'repeat(auto-fill, minmax(100px, 1fr))'}>
+                  <GridItem columnStart={1} columnEnd={5}>
+                    {item.leftComponent}
+                  </GridItem>
+                  <GridItem columnStart={5} columnEnd={12}>
+                    {item.rightComponent}
+                  </GridItem>
+                </Grid>
+              </TabPanel>
+            )}
+          </>
+        ))}
+    </>
+  );
+};
+
+UsageTabs.prototype = {
+  tabsData: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      defaultActive: PropTypes.boolean,
+      leftComponent: PropTypes.elementType.isRequired,
+      rightComponent: PropTypes.elementType.isRequired
+    })
+  )
+};
+
+export default UsageTabs;
