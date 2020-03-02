d921e8d7c4fe326e08c7cf377a9452c4ea7b2e61
Moe Shaaban <info@mshaaban.com>
2020-03-02 17:41:47 +0100

    feat: make OrganizationUsageInfoNew a functional component

--- src/javascripts/account/usage/committed/OrganizationUsageInfoNew.js
+++ src/javascripts/account/usage/committed/OrganizationUsageInfoNew.js
@@ -21,39 +21,41 @@ const styles = {
     fontSize: tokens.fontSizeS
   })
 };
-export default class OrganizationUsageInfoNew extends React.Component {
-  static propTypes = {
-    totalUsage: PropTypes.number.isRequired,
-    includedLimit: PropTypes.number.isRequired
-  };
 
-  render() {
-    const { totalUsage, includedLimit } = this.props;
-    return (
-      <Typography>
-        <Heading element="h2" className={styles.heading}>
-          Total API requests
-        </Heading>
-        <Heading element="p" className={styles.usageNumber}>
-          {totalUsage.toLocaleString('en-US')}
-          {totalUsage > includedLimit && (
-            <small className={styles.overageNumber}>
-              {` +${(totalUsage - includedLimit).toLocaleString('en-US')} overage`}
-            </small>
-          )}
-        </Heading>
-        <Paragraph>
-          Total API calls made this month from a <strong>{`${shorten(includedLimit)}`}</strong>
-          /month quota. This number includes CMA, CDA, CPA, and GraphQL requests. To learn about
-          utility limits, read the
-          <TextLink
-            href="https://www.contentful.com/r/knowledgebase/fair-use/"
-            target="_blank"
-            className="usage-page__learn-more-link">
-            Fair Use Policy.
-          </TextLink>
-        </Paragraph>
-      </Typography>
-    );
-  }
-}
+const OrganizationUsageInfoNew = props => {
+  const { totalUsage, includedLimit } = props;
+
+  return (
+    <Typography>
+      <Heading element="h2" className={styles.heading}>
+        Total API requests
+      </Heading>
+      <Heading element="p" className={styles.usageNumber}>
+        {totalUsage.toLocaleString('en-US')}
+        {totalUsage > includedLimit && (
+          <small className={styles.overageNumber}>
+            {` +${(totalUsage - includedLimit).toLocaleString('en-US')} overage`}
+          </small>
+        )}
+      </Heading>
+      <Paragraph>
+        Total API calls made this month from a <strong>{`${shorten(includedLimit)}`}</strong>
+        /month quota. This number includes CMA, CDA, CPA, and GraphQL requests. To learn about
+        utility limits, read the
+        <TextLink
+          href="https://www.contentful.com/r/knowledgebase/fair-use/"
+          target="_blank"
+          className="usage-page__learn-more-link">
+          Fair Use Policy.
+        </TextLink>
+      </Paragraph>
+    </Typography>
+  );
+};
+
+OrganizationUsageInfoNew.propTypes = {
+  totalUsage: PropTypes.number.isRequired,
+  includedLimit: PropTypes.number.isRequired
+};
+
+export default OrganizationUsageInfoNew;
