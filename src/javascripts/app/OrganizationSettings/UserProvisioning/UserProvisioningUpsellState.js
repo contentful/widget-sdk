0643107881d5adbf9799c862fbc385d455771c41
Victoria Beleuta <vbeleuta@gmail.com>
2020-03-05 10:38:20 +0100

    update upsell state images for access tolls

--- src/javascripts/app/OrganizationSettings/UserProvisioning/UserProvisioningUpsellState.js
+++ src/javascripts/app/OrganizationSettings/UserProvisioning/UserProvisioningUpsellState.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { css } from 'emotion';
-import TeamsEmptyStateImage from 'svg/add-team-illustration.svg';
+import UserProvisioningUpsellImage from 'svg/upsell-state-user-provisioning.svg';
 import {
   Heading,
   Typography,
@@ -28,36 +28,38 @@ function UserProvisioningUpsellState() {
         title="User Provisioning"
       />
       <Workbench.Content type="text">
-        <EmptyStateContainer>
-          <div className={defaultSVGStyle}>
-            <TeamsEmptyStateImage />
-          </div>
-          <Typography>
-            <Heading>Provision users in Contentful using your identity provider</Heading>
-            <Paragraph>
-              You can manage users and teams within your organization in Contentful directly from
-              your identity provider. We support SCIM 2.0 for user provisioning.{' '}
-              {/* TODO uncomment when page is available
-              <TextLink
-                href="https://www.contentful.com/faq/SSO-Groups-Provisioning/
-                "
-                target="_blank">
-                Read our FAQs for more information.
-              </TextLink> */}
-            </Paragraph>
-            <Paragraph>
-              The user provisioning feature is available on selected enterprise-grade platform
-              plans. To add user provisioning to your plan, contact us directly.
-            </Paragraph>
-          </Typography>
-          <Button
-            testId="get-in-touch-btn"
-            className={styles.topMargin}
-            href={salesUrl}
-            target="_blank">
-            Get in touch
-          </Button>
-        </EmptyStateContainer>
+        <div className={styles.pageWrapper}>
+          <EmptyStateContainer>
+            <div className={defaultSVGStyle}>
+              <UserProvisioningUpsellImage />
+            </div>
+            <Typography>
+              <Heading>Provision users in Contentful using your identity provider</Heading>
+              <Paragraph>
+                You can manage users and teams within your organization in Contentful directly from
+                your identity provider. We support SCIM 2.0 for user provisioning.{' '}
+                {/* TODO uncomment when page is available
+                <TextLink
+                  href="https://www.contentful.com/faq/SSO-Groups-Provisioning/
+                  "
+                  target="_blank">
+                  Read our FAQs for more information.
+                </TextLink> */}
+              </Paragraph>
+              <Paragraph>
+                The user provisioning feature is available on selected enterprise-grade platform
+                plans. To add user provisioning to your plan, contact us directly.
+              </Paragraph>
+            </Typography>
+            <Button
+              testId="get-in-touch-btn"
+              className={styles.topMargin}
+              href={salesUrl}
+              target="_blank">
+              Get in touch
+            </Button>
+          </EmptyStateContainer>
+        </div>
       </Workbench.Content>
     </Workbench>
   );
