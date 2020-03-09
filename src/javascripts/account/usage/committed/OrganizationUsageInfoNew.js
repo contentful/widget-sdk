d95bafb19ffdc0060cd672e6e226c52bad2a3f52
Moe Shaaban <info@mshaaban.com>
2020-03-09 12:07:48 +0100

    fix: use <Paragraph /> instead of <Heading />

--- src/javascripts/account/usage/committed/OrganizationUsageInfoNew.js
+++ src/javascripts/account/usage/committed/OrganizationUsageInfoNew.js
@@ -30,14 +30,14 @@ const OrganizationUsageInfoNew = props => {
       <Heading element="h2" className={styles.heading}>
         Total API requests
       </Heading>
-      <Heading element="p" data-test-id="org-usage-total" className={styles.usageNumber}>
+      <Paragraph data-test-id="org-usage-total" className={styles.usageNumber}>
         {totalUsage.toLocaleString('en-US')}
         {totalUsage > includedLimit && (
           <small data-test-id="org-usage-overage" className={styles.overageNumber}>
             {` +${(totalUsage - includedLimit).toLocaleString('en-US')} overage`}
           </small>
         )}
-      </Heading>
+      </Paragraph>
       <Paragraph>
         Total API calls made this month from a{' '}
         <strong data-test-id="org-usage-limit">{`${shorten(includedLimit)}`}</strong>
