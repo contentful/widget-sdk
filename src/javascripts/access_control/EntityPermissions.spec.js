00a012aaa20480610f7b3775c9303cc310614996
Daniel A. R. Werner <daniel.a.r.werner@gmail.com>
2020-03-24 17:46:36 +0100

    test: Reimplement EntityPermissions tests in jest

--- src/javascripts/access_control/EntityPermissions.spec.js
+++ src/javascripts/access_control/EntityPermissions.spec.js
@@ -1,67 +1,94 @@
-describe.skip('EntityPermissions', () => {
-  // TODO:xxx rewrite to jest
+import { create } from './EntityPermissions';
+import * as accessChecker from 'access_control/AccessChecker/index';
+
+jest.mock('access_control/AccessChecker', () => ({
+  canPerformActionOnEntity: jest.fn(),
+  canUpdateEntry: jest.fn(),
+  canUpdateAsset: jest.fn(),
+  canEditFieldLocale: jest.fn()
+}));
+
+const entrySys = { type: 'Entry', id: 'some-entry-id' };
+const assetSys = { type: 'Asset', id: 'some-asset-id' };
+const emptyEntity = sys => ({ data: { sys } });
+
+describe('EntityPermissions', () => {
+  let permissions;
+
+  beforeEach(() => {
+    permissions = create(entrySys);
+  });
+
   describe('#can()', () => {
-    it('delegates to "accessChecker.canPerformActionOnEntity()"', () => {
-      this.stubs.canPerformActionOnEntity.returns(true);
-      expect(this.doc.permissions.can('publish')).toBe(true);
+    it('delegates to `accessChecker.canPerformActionOnEntity()`', () => {
+      accessChecker.canPerformActionOnEntity.mockReturnValue(true);
+      expect(permissions.can('publish')).toBe(true);
 
-      this.stubs.canPerformActionOnEntity.returns(false);
-      expect(this.doc.permissions.can('publish')).toBe(false);
+      accessChecker.canPerformActionOnEntity.mockReturnValue(false);
+      expect(permissions.can('publish')).toBe(false);
 
-      const entity = this.stubs.canPerformActionOnEntity.args[0][1];
-      expect(entity.data.sys.id).toEqual('ENTITY_ID');
+      expect(accessChecker.canPerformActionOnEntity).toHaveBeenCalledTimes(2);
+      expect(accessChecker.canPerformActionOnEntity).toHaveBeenCalledWith(
+        'publish',
+        emptyEntity(entrySys)
+      );
     });
 
-    it('delegates "update" calls to "accessChecker.canUpdateEntry()"', () => {
-      this.stubs.canUpdateEntry.returns(true);
-      expect(this.doc.permissions.can('update')).toBe(true);
+    it('delegates "update" calls to `accessChecker.canUpdateEntry()`', () => {
+      accessChecker.canUpdateEntry.mockReturnValue(true);
+      expect(permissions.can('update')).toBe(true);
 
-      this.stubs.canUpdateEntry.returns(false);
-      expect(this.doc.permissions.can('update')).toBe(false);
+      accessChecker.canUpdateEntry.mockReturnValue(false);
+      expect(permissions.can('update')).toBe(false);
 
-      const entity = this.stubs.canUpdateEntry.args[0][0];
-      expect(entity.data.sys.id).toEqual('ENTITY_ID');
+      expect(accessChecker.canUpdateEntry).toHaveBeenCalledTimes(2);
+      expect(accessChecker.canUpdateEntry).toHaveBeenCalledWith(emptyEntity(entrySys));
     });
 
-    it('delegates "update" calls to "accessChecker.canUpdateAsset()"', () => {
-      const doc = this.createDoc('Asset');
+    it('delegates "update" calls to `accessChecker.canUpdateAsset()`', () => {
+      permissions = create(assetSys);
 
-      this.stubs.canUpdateAsset.returns(true);
-      expect(doc.permissions.can('update')).toBe(true);
+      accessChecker.canUpdateAsset.mockReturnValue(true);
+      expect(permissions.can('update')).toBe(true);
 
-      this.stubs.canUpdateAsset.returns(false);
-      expect(doc.permissions.can('update')).toBe(false);
+      accessChecker.canUpdateAsset.mockReturnValue(false);
+      expect(permissions.can('update')).toBe(false);
 
-      const entity = this.stubs.canUpdateAsset.args[0][0];
-      expect(entity.data.sys.id).toEqual('ENTITY_ID');
+      expect(accessChecker.canUpdateAsset).toHaveBeenCalledTimes(2);
+      expect(accessChecker.canUpdateAsset).toHaveBeenCalledWith(emptyEntity(assetSys));
     });
 
     it('throws when action is unknown', () => {
-      const doc = this.createDoc();
-      expect(_.partial(doc.permissions.can, 'abc')).toThrowError('Unknown entity action "abc"');
+      expect(() => permissions.can('rule the world')).toThrowError(
+        'Unknown entity action "rule the world"'
+      );
     });
   });
 
   describe('#canEditFieldLocale()', () => {
     it('returns false if `update` permission is denied', () => {
-      this.stubs.canUpdateEntry.returns(false);
-      expect(this.doc.permissions.canEditFieldLocale('FIELD', 'LOCALE')).toBe(false);
+      accessChecker.canUpdateEntry.mockReturnValue(false);
+
+      expect(permissions.canEditFieldLocale('FIELD', 'LOCALE')).toBe(false);
+      expect(accessChecker.canUpdateEntry).toHaveBeenCalledTimes(1);
+      expect(accessChecker.canUpdateEntry).toHaveBeenCalledWith(emptyEntity(entrySys));
     });
 
-    it('delegates to "policyAccessChecker"', () => {
-      this.stubs.canUpdateEntry.returns(true);
-      this.stubs.canEditFieldLocale.returns(true);
+    it('delegates to `policyAccessChecker`', () => {
+      accessChecker.canUpdateEntry.mockReturnValue(true);
 
-      expect(this.doc.permissions.canEditFieldLocale('FIELD', 'LOCALE')).toBe(true);
+      accessChecker.canEditFieldLocale.mockReturnValue(true);
+      expect(permissions.canEditFieldLocale('FIELD', 'LOCALE')).toBe(true);
 
-      this.stubs.canEditFieldLocale.returns(false);
-      expect(this.doc.permissions.canEditFieldLocale('FIELD', 'LOCALE')).toBe(false);
+      accessChecker.canEditFieldLocale.mockReturnValue(false);
+      expect(permissions.canEditFieldLocale('FIELD', 'LOCALE')).toBe(false);
 
-      const args = this.stubs.canEditFieldLocale.args[0];
-      const [ctId, { apiName }, { code }] = args;
-      expect(ctId).toBe('CT_ID');
-      expect(apiName).toBe('FIELD');
-      expect(code).toBe('LOCALE');
+      expect(accessChecker.canEditFieldLocale).toHaveBeenCalledTimes(2);
+      expect(accessChecker.canEditFieldLocale).toHaveBeenCalledWith(
+        entrySys,
+        { apiName: 'FIELD' },
+        { code: 'LOCALE' }
+      );
     });
   });
 });
