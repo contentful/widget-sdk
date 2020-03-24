cb6ed9d707f734c89a3847c5e7fc03651118479f
Joshua Smock <jo-sm@users.noreply.github.com>
2020-03-24 18:43:47 +0100

    refactor: Flesh out useForm a bit more
    Add some useful comments

--- src/javascripts/app/common/hooks/useForm.js
+++ src/javascripts/app/common/hooks/useForm.js
@@ -1,154 +1,354 @@
-import { get } from 'lodash';
+import { get, cloneDeep } from 'lodash';
 import { useReducer } from 'react';
 import { createImmerReducer } from 'redux/utils/createImmerReducer';
 
-/*
-  useForm: A hook for handling form data and submission.
+export const FORM_ERROR = '__FORM_ERROR__';
 
-  Usage:
-    function myComponent() {
-      const { onBlur, onChange, onSubmit, fields, form } = useForm({
-        fields: {
-          name: {
-            value: 'John',
-            validator: value => {
-              if (name !== 'John') {
-                return 'First name must be John';
-              }
-            }
-          },
-          lastName: {
-            value: 'Smith'
-          },
-          email: {
-            value: 'john.smith@localhost.dev'
-          }
-        },
-        submitFn: async fields => {
-          // fields: {
-          //   firstName: 'John',
-          //   lastName: 'Smith',
-          //   email: 'john.smith@localhost.dev'
-          // }
-
-
-          await submitData(fields);
-        }
-      })
-    }
-
- */
+const ERROR_TYPES = {
+  VALIDATOR: 'validator',
+  SUBMISSION: 'submission'
+};
 
 const initializeFn = initialValues => {
   return {
     fields: initialValues.fields,
+    fieldsValidator: initialValues.fieldsValidator || (() => {}),
     form: {
       isPending: false,
       invalid: false,
       pristine: true,
-      error: ''
+      error: '',
+      submissionError: false
     },
-    onSubmit: initialValues.onSubmit
+    submitFn: initialValues.submitFn
   };
 };
 
+const isFormInvalid = state => {
+  const fieldValues = Object.values(state.fields);
+
+  const anyFieldHasErrors = !!fieldValues.find(field => !!field.error);
+  const anyFieldIsRequiredAndEmpty = !!fieldValues.find(field => field.required && !field.value);
+
+  if (anyFieldHasErrors || anyFieldIsRequiredAndEmpty) {
+    return true;
+  }
+
+  return false;
+};
+
 const formReducer = createImmerReducer({
-  UPDATE_FIELD_VALUE: (state, { payload }) => {
-    state.fields[payload.fieldName].value = payload.value;
+  UPDATE_FIELD_VALUE: (state, { payload: { fieldName, value } }) => {
+    const field = state.fields[fieldName];
 
-    // TODO: maybe have to separate out submission and non-submission related errors
-    state.fields[payload.fieldName].error = null;
+    field.value = value;
 
-    state.form.invalid = false;
     state.form.pristine = false;
+
+    // Clear the error if the errorType is submission
+    // Non-submission errors are cleared by UNSET_FIELD_ERROR
+    if (field.error && field.errorType === ERROR_TYPES.SUBMISSION) {
+      field.error = '';
+    }
+
+    // Since a submission error may have been cleared, and the field
+    // may not have its own validator fn, we check to see if it's valid
+    // here as well.
+    state.form.invalid = isFormInvalid(state);
   },
   SET_FIELD_BLURRED: (state, { payload: { fieldName } }) => {
     state.fields[fieldName].blurred = true;
   },
   SET_FIELD_ERROR: (state, { payload: { fieldName, message } }) => {
-    if (message !== '') {
-      state.form.invalid = true;
-    } else {
-      state.form.invalid = false;
-    }
-
     state.fields[fieldName].error = message;
+    state.fields[fieldName].errorType = ERROR_TYPES.VALIDATOR;
+
+    state.form.invalid = true;
+  },
+  UNSET_FIELD_ERROR: (state, { payload: { fieldName } }) => {
+    state.fields[fieldName].error = '';
+    state.fields[fieldName].errorType = null;
+
+    state.form.invalid = isFormInvalid(state);
+  },
+  SET_FIELD_SUBMISSION_ERROR: (state, { payload: { fieldName, message } }) => {
+    state.fields[fieldName].error = message;
+    state.fields[fieldName].errorType = ERROR_TYPES.SUBMISSION;
+    state.form.invalid = true;
   },
   SET_FORM_PENDING: (state, { payload: { isPending } }) => {
     state.form.isPending = isPending;
   },
   SET_FORM_ERROR: (state, { payload: { message } }) => {
     state.form.error = message;
+
+    // The form is not invalid simply because of a form error. This is because
+    // an API call may have just had an intermittent error, and so the form should
+    // be able to submit again
   }
 });
 
-const runValidator = (validator, fieldName, value, dispatch) => {
-  const errorMessage = validator(value);
+const fieldErrorMessage = (state, fieldName) => {
+  const field = state.fields[fieldName];
+
+  const emptyValidator = value => {
+    if (!value) {
+      return 'This field is required';
+    }
+  };
+
+  let errorMessage = '';
+
+  // First, run the fieldsValidator. It takes precedence over other validators
+  if (state.fieldsValidator) {
+    errorMessage = get(state.fieldsValidator(state.fields), fieldName, '');
+  }
+
+  if (!errorMessage && field.required) {
+    errorMessage = emptyValidator(field.value);
+  }
+
+  if (!errorMessage && field.validator) {
+    errorMessage = field.validator(field.value);
+  }
+
+  return errorMessage;
+};
+
+const runValidator = (state, fieldName, dispatch) => {
+  const errorMessage = fieldErrorMessage(state, fieldName);
 
   if (errorMessage) {
     dispatch({ type: 'SET_FIELD_ERROR', payload: { fieldName, message: errorMessage } });
   } else {
-    dispatch({ type: 'SET_FIELD_ERROR', payload: { fieldName, message: null } });
+    dispatch({ type: 'UNSET_FIELD_ERROR', payload: { fieldName } });
   }
 };
 
+/**
+ * `useForm` is a hook that handles all form related logic and UX for UI forms. Specifically,
+ * it handles changes, blurs, submissions, as well as encapsulating field and form related
+ * values and metadata.
+ *
+ * When instantiating, you must pass in a `fields` object and a `submitFn` function, and
+ * optionally a `fieldsValidator` function.
+ *
+ * --------
+ *
+ * `fields`
+ *
+ * The keys of `fields` correspond to the name of the field, e.g. `firstName`, and the value of
+ * each field given is an object that can have three keys: `value`, the default value; `validator`,
+ * a validation function; and, `required`, if the field is required.
+ *
+ * Example fields:
+ *
+ * {
+ *   firstName: {
+ *     value: 'John',
+ *     validator: (value) => {
+ *       if (value !== 'John') {
+ *         return 'First name must be John';
+ *       }
+ *     },
+ *     required: true
+ *   },
+ *   lastName: {
+ *     value: 'Smith',
+ *     required: true
+ *   }
+ * }
+ *
+ * `value` is a string.
+ * `validator` is a function that takes the current field value and returns either undefined,
+ *     or a error message string.
+ * `required` is a boolean.
+ *
+ * ----------
+ *
+ * `fieldsValidator`
+ *
+ * If you need additional control over the fields validation, such as a field that is dependent
+ * on another field's value, you can pass in an optional `fieldsValidator` function, which takes
+ * `fields` as its only argument and returns either undefined or an object of field errors (similar
+ * to `submitFn`). `fieldsValidator` errors take precedence over `field.validator` errors.
+ *
+ * Example:
+ *
+ * fieldsValidator(fields) {
+ *   const errors = {};
+ *
+ *   if (fields.firstName.value === 'John' && fields.lastName.value === 'Smith') {
+ *     errors.firstName = 'Sorry, John is not a valid first name for last name Smith';
+ *   }
+ *
+ *   return errors;
+ * }
+ *
+ * Be aware that `fieldsValidator` is "low level" -- the normal UX that's provided by the hook
+ * is overridden (for example, blurred field handling). Use `fieldsValidator` only if necessary.
+ *
+ * ----------
+ *
+ * `submitFn`
+ *
+ * The `submitFn` is an asynchronous function that takes the field values as an argument and
+ * either returns undefined or an object that corresponds to field errors, as well as a
+ * special FORM_ERROR key (exported from this file) which will then be mapped to the fields or form.
+ *
+ * You can also pass arbitrary arguments to `onSubmit`, which will be passed to `submitFn` after `values`.
+ *
+ * If an error is thrown or returned from `submitFn`, the value of `error.message` will be used as the form
+ * error. Otherwise, if anything other than `undefined` is returned, the form error will be `An error occurred.`.
+ *
+ * Example submitFn:
+ *
+ * async submitFn(values, ...submitArgs) => {
+ *   const spaceId = submitArgs[0];
+ *
+ *   try {
+ *     await someAPIReq(endpoint, values);
+ *   } catch (e) {
+ *     const errors = {}
+ *
+ *     if (e.data && e.data.details) {
+ *       e.data.details.forEach(detail => {
+ *         if (detail.path === 'first_name') {
+ *           if (detail.name === 'length') {
+ *             errors.firstName = 'First name is too short'
+ *           } else {
+ *             errors.firstName = 'First name is invalid';
+ *           }
+ *         } else if (detail.path === 'last_name') {
+ *           if (detail.name === 'length') {
+ *             errors.firstName = 'Last name is too short'
+ *           } else {
+ *             errors.firstName = 'Last name is invalid';
+ *           }
+ *         } else {
+ *           errors[FORM_ERROR] = 'An error occurred';
+ *         }
+ *       });
+ *
+ *       return errors;
+ *     }
+ *   }
+ * }
+ *
+ * --------
+ *
+ * General usage:
+ *
+ *   const { onBlur, onChange, onSubmit, fields, form } = useForm({
+ *     fields: { ... },
+ *     submitFn: async (values, ...onSubmitArgs) => { ... },
+ *     fieldsValidator: (fields) => { ... }
+ *  })
+ */
 export default function useForm(initialValues) {
   const [state, dispatch] = useReducer(formReducer, initialValues, initializeFn);
-
-  // onBlur sets the field as blurred
-  // onChange updates the field value
 
   const onBlur = fieldName => {
     dispatch({ type: 'SET_FIELD_BLURRED', payload: { fieldName } });
 
-    const { validator, value } = state.fields[fieldName];
+    // Clone the state and update the blurred key on the field, and pass
+    // the clonedState to the validator runner
+    const clonedState = cloneDeep(state);
+    clonedState.fields[fieldName].blurred = true;
 
-    // Run the validator, and if it fails, set an error
-    if (validator) {
-      runValidator(validator, fieldName, value, dispatch);
-    }
+    runValidator(clonedState, fieldName, dispatch);
   };
 
   const onChange = (fieldName, value) => {
     // Always update the field value
     dispatch({ type: 'UPDATE_FIELD_VALUE', payload: { fieldName, value } });
 
-    const { blurred, validator } = state.fields[fieldName];
+    // Clone the state, update the field value, and pass
+    // the clonedState to the validator runner
+    const clonedState = cloneDeep(state);
+    const field = clonedState.fields[fieldName];
 
-    if (blurred && validator) {
-      runValidator(validator, fieldName, value, dispatch);
+    field.value = value;
+
+    // We don't want to show an error if the user has just started to type, but hasn't left the field yet
+    if (field.blurred) {
+      // We have to pass in the new value as the state hasn't been updated yet
+      runValidator(clonedState, fieldName, dispatch);
     }
   };
 
-  const onSubmit = async () => {
-    // Go through each field, run onBlur for each field, and if there are any errors, do not run the
-    // given onSubmit handler
+  const onSubmit = async (...submitFnArgs) => {
+    // Blur all fields manually, in case some haven't been touched but are required or
+    // have another validator on them
     for (const fieldName in state.fields) {
       onBlur(fieldName);
     }
 
-    if (!Object.values(state.fields).find(field => field.error)) {
+    // Because we don't have the updated state from above (the fields may not be marked as blurred),
+    // but the fieldsValidator should have the most current state, we clone the state and set each field
+    // as blurred, and then check to see if any fields have errors
+    const clonedState = cloneDeep(state);
+
+    clonedState.fields.forEach(field => {
+      field.blurred = true;
+    });
+
+    const anyFieldHasErrors = Object.keys(clonedState.fields).find(fieldName =>
+      fieldErrorMessage(clonedState, fieldName)
+    );
+
+    if (!anyFieldHasErrors) {
       dispatch({ type: 'SET_FORM_PENDING', payload: { isPending: true } });
 
+      let result;
+
+      // Create a mapping of fieldNames and values
+      //
+      // E.g.
+      // {
+      //   firstName: 'John',
+      //   lastName: 'Smith'
+      // }
+      const mappedFields = Object.entries(state.fields).reduce((memo, [fieldName, { value }]) => {
+        memo[fieldName] = value;
+
+        return memo;
+      }, {});
+
       try {
-        // Create a mapping of fieldNames and values
-        //
-        // E.g.
-        // {
-        //   firstName: 'John',
-        //   lastName: 'Smith'
-        // }
-        const mappedFields = Object.entries(state.fields).reduce((memo, [fieldName, { value }]) => {
-          memo[fieldName] = value;
+        result = await state.submitFn(mappedFields, ...submitFnArgs);
+      } catch (e) {
+        const message = e.message;
 
-          return memo;
-        }, {});
+        dispatch({
+          type: 'SET_FORM_ERROR',
+          payload: {
+            message
+          }
+        });
 
-        const result = await state.submitFn(mappedFields);
+        dispatch({
+          type: 'SET_FORM_PENDING',
+          payload: {
+            isPending: false
+          }
+        });
 
-        if (result) {
-          const formError = get(result, 'form', '');
-          const fieldErrors = get(result, 'fields', {});
+        return;
+      }
+
+      if (result !== undefined) {
+        // If the result is not `undefined`, but the result has no keys, then
+        // something seems to be wrong (the result is not formatted correctly).
+        if (Object.keys(result).length === 0) {
+          dispatch({
+            type: 'SET_FORM_ERROR',
+            payload: {
+              message: 'An error occurred.'
+            }
+          });
+        } else {
+          const formError = get(result, FORM_ERROR, '');
 
           if (formError !== '') {
             dispatch({
@@ -159,9 +359,14 @@ export default function useForm(initialValues) {
             });
           }
 
-          Object.entries(fieldErrors).forEach(([fieldName, message]) => {
+          Object.entries(result).forEach(([fieldName, message]) => {
+            // Ignore errors that don't correspond to a field
+            if (!state.fields[fieldName]) {
+              return;
+            }
+
             dispatch({
-              type: 'SET_FIELD_ERROR',
+              type: 'SET_FIELD_SUBMISSION_ERROR',
               payload: {
                 fieldName,
                 message
@@ -169,28 +374,14 @@ export default function useForm(initialValues) {
             });
           });
         }
-
-        dispatch({
-          type: 'SET_FORM_PENDING',
-          payload: {
-            isPending: false
-          }
-        });
-      } catch (e) {
-        dispatch({
-          type: 'SET_FORM_ERROR',
-          payload: {
-            message: 'An error occurred. Try again.'
-          }
-        });
-
-        dispatch({
-          type: 'SET_FORM_PENDING',
-          payload: {
-            isPending: false
-          }
-        });
       }
+
+      dispatch({
+        type: 'SET_FORM_PENDING',
+        payload: {
+          isPending: false
+        }
+      });
     }
   };
 
