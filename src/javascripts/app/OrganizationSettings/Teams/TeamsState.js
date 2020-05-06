850cfca1694b5648313d29dee75e14630d7010e1
Victoria Beleuta <vbeleuta@gmail.com>
2020-05-06 10:34:28 +0200

    fixed TeamDetailsRoute with feature flag checker; updated teamsState;

--- src/javascripts/app/OrganizationSettings/Teams/TeamsState.js
+++ src/javascripts/app/OrganizationSettings/Teams/TeamsState.js
@@ -2,17 +2,12 @@ import React from 'react';
 import { organizationRoute } from 'states/utils';
 import LazyLoadedComponent from 'app/common/LazyLoadedComponent';
 import importer from 'app/OrganizationSettings/importer';
+import { TeamDetailsRoute } from 'features/teams';
 
 const teamDetailState = organizationRoute({
   name: 'detail',
   url: '/:teamId',
-  component: (props) => (
-    <LazyLoadedComponent importer={importer}>
-      {({ TeamPage }) => {
-        return <TeamPage {...props} />;
-      }}
-    </LazyLoadedComponent>
-  ),
+  component: TeamDetailsRoute,
 });
 
 export default organizationRoute({
--- src/javascripts/features/teams/routes/TeamDetailsRoute.js
+++ src/javascripts/features/teams/routes/TeamDetailsRoute.js
@@ -2,7 +2,17 @@ import React from 'react';
 import DocumentTitle from 'components/shared/DocumentTitle';
 import { TeamDetails } from '../components/TeamDetails';
 import { createTeam } from '../services/TeamRepo';
+import { getVariation } from 'LaunchDarkly';
+import { NEW_TEAM_DETAILS } from 'featureFlags';
+import createFetcherComponent from 'app/common/createFetcherComponent';
+import TeamPage from 'app/OrganizationSettings/Teams/TeamPage';
+import StateRedirect from 'app/common/StateRedirect';
+import { FetcherLoading } from 'app/common/createFetcherComponent';
 
+const FeatureFetcher = createFetcherComponent(async () => {
+  const newTeamDetailsEnabled = await getVariation(NEW_TEAM_DETAILS);
+  return { newTeamDetailsEnabled };
+});
 export class TeamDetailsRoute extends React.Component {
   save = async ({ name, description }) => {
     return await createTeam({ name, description });
@@ -12,7 +22,22 @@ export class TeamDetailsRoute extends React.Component {
     return (
       <React.Fragment>
         <DocumentTitle title="Team Details" />
-        <TeamDetails save={this.save} />
+        <FeatureFetcher>
+          {({ isLoading, isError, data }) => {
+            console.log(data);
+            if (isLoading) {
+              return <FetcherLoading message="Loading..." />;
+            }
+            if (isError) {
+              return <StateRedirect path="teams" />;
+            }
+            if (data.newTeamDetailsEnabled) {
+              return <TeamDetails save={this.save} />;
+            }
+
+            return <TeamPage />;
+          }}
+        </FeatureFetcher>
       </React.Fragment>
     );
   }
