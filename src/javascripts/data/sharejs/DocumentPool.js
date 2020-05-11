9b911982dc25fd78ccbdded2e50132cc876e29c9
Danil Zakablukovskii <danil.zakablukovskii@contentful.com>
2020-05-11 17:21:50 +0200

    fix(entity_editor): don't send auto-save header for now (#6034)

--- src/javascripts/data/sharejs/DocumentPool.js
+++ src/javascripts/data/sharejs/DocumentPool.js
@@ -49,7 +49,7 @@ export async function create(docConnection, spaceEndpoint, pubSubClient, organiz
         const entityRepo = createEntityRepo(spaceEndpoint, pubSubClient, {
           skipDraftValidation: true,
           skipTransformation: true,
-          indicateAutoSave: true,
+          indicateAutoSave: false,
         });
         doc = createCmaDoc(entity, contentType, spaceEndpoint, entityRepo);
       } else {
