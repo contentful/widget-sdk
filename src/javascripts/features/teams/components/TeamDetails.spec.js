3aafb43601a3c9595e03fa0753d7cd302aec9655
Victoria Beleuta <vbeleuta@gmail.com>
2020-05-12 16:46:38 +0200

    fix new TeamDetails unit test

--- src/javascripts/features/teams/components/TeamDetails.spec.js
+++ src/javascripts/features/teams/components/TeamDetails.spec.js
@@ -2,18 +2,22 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import * as fake from 'test/helpers/fakeFactory';
 import { TeamDetails } from './TeamDetails';
 
 const activeOrgId = 'testOrgId';
-const teamId = 'testTeamId';
+
 describe('TeamDetails', () => {
   const renderComponent = (props) => {
-    return render(<TeamDetails orgId={activeOrgId} teamId={teamId} {...props} />);
+    const team = fake.Team();
+    return render(
+      <TeamDetails orgId={activeOrgId} team={team} readOnlyPermission={false} {...props} />
+    );
   };
 
-  it('renders the page', () => {
+  it('renders the page', async () => {
     const { getByTestId } = renderComponent();
-    const header = getByTestId('"organization-team-page"');
-    expect(header).toBeInTheDOM();
+    const header = getByTestId('organization-team-page');
+    expect(header).toBeInTheDocument();
   });
 });
