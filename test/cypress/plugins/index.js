4c1dab21de4b2aa5ae44fd61f5e2b8183e146584
Sergiy Tupchiy <sergiy@contentful.com>
2020-05-15 11:04:40 +0000

    chore(test): cypress/typescript no need of special preprocessors (#6053)

--- test/cypress/plugins/index.js
+++ test/cypress/plugins/index.js
@@ -1,7 +1,5 @@
-const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
 const cypressPactPlugin = require('@contentful/cypress-pact/plugin');
 
 module.exports = (on) => {
-  on('file:preprocessor', cypressTypeScriptPreprocessor);
   cypressPactPlugin(on);
 };
