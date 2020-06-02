da7db6008b294b189fd7576ce123558b3f293fe2
Jakub Elżbieciak <jakub@contentful.com>
2020-06-02 14:24:22 +0200

    Extract AppPage styles to a separate file

--- src/javascripts/app/Apps/AppPage.js
+++ src/javascripts/app/Apps/AppPage.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
-import { css, keyframes } from 'emotion';
-import tokens from '@contentful/forma-36-tokens';
 import {
   Button,
   Notification,
@@ -30,6 +28,7 @@ import * as AppLifecycleTracking from './AppLifecycleTracking';
 import { isUsageExceededErrorResponse, USAGE_EXCEEDED_MESSAGE } from './isUsageExceeded';
 import { LOCATION_APP_CONFIG } from 'widgets/WidgetLocations';
 import { AppIcon } from './AppIcon';
+import { styles } from './AppPageStyles';
 
 const BUSY_STATE_INSTALLATION = 'installation';
 const BUSY_STATE_UPDATE = 'update';
@@ -43,78 +42,6 @@ const BUSY_STATE_TO_TEXT = {
 
 const APP_STILL_LOADING_TIMEOUT = 3000;
 const APP_HAS_ERROR_TIMEOUT = 15000;
-
-const fadeIn = keyframes({
-  from: {
-    transform: 'translateY(50px)',
-    opacity: '0',
-  },
-  to: {
-    transform: 'translateY(0)',
-    opacity: '1',
-  },
-});
-
-const styles = {
-  renderer: css({
-    padding: 0,
-    animation: `${fadeIn} 0.8s ease`,
-    '> div': {
-      height: '100%',
-      width: '100%',
-    },
-    overflow: 'hidden',
-  }),
-  hideRenderer: css({
-    display: 'none',
-  }),
-  actionButton: css({
-    marginLeft: tokens.spacingM,
-  }),
-  overlay: css({
-    backgroundColor: 'rgba(255, 255, 255, 0.75)',
-    position: 'fixed',
-    zIndex: 9999,
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    textAlign: 'center',
-  }),
-  busyText: css({
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: '12px',
-    borderRadius: '50px',
-    fontSize: '24px',
-    backgroundColor: 'white',
-    letterSpacing: '1px',
-  }),
-  overlayPill: css({
-    width: '100%',
-    height: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }),
-  spinner: css({
-    marginRight: tokens.spacingS,
-  }),
-  stillLoadingText: css({
-    marginTop: '-80px',
-  }),
-  heading: css({ display: 'flex', alignItems: 'center' }),
-  tag: css({ marginLeft: tokens.spacingS }),
-  noConfigContainer: css({
-    width: '100%',
-    '> div:last-child': {
-      borderTop: `1px solid ${tokens.colorElementLight}`,
-    },
-  }),
-  noConfigSection: css({ maxWidth: '700px', margin: '0 auto', padding: tokens.spacingXl }),
-  noConfigHelpText: css({ marginTop: tokens.spacingM }),
-};
 
 export default class AppRoute extends Component {
   static propTypes = {
