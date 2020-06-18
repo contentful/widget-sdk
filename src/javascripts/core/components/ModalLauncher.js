5e91ad500330ea87a0be1cf123bb5e1c7e4fb66e
Marco Link <marco.link@contentful.com>
2020-06-18 15:02:07 +0200

    fix(modal-launcher): unmount (#6216)
    * fix(modal-launcher): unmount
    
    The current implementation is never unmounting the given component.
    Once mounted react takes care of the container element. calling `remove` has no effect as long as the react component is mounted. React will take care the container of a mounted component exists.
    
    With the changes of this PR we now explicitly unmount the given component and remove the container afterwards.
    The `delay` is needed to show our `Modal`s hide animation.

--- src/javascripts/core/components/ModalLauncher.js
+++ src/javascripts/core/components/ModalLauncher.js
@@ -4,8 +4,11 @@ import ReactDOM from 'react-dom';
  * @param {function} componentRenderer
  * @param {object=} opts
  * @param {string} opts.modalId optional idempotency key to reuse a modal root
+ * @param {string} opts.delay optional ms before removing the component from the tree defaults to 25ms.
  */
 function open(componentRenderer, opts = {}) {
+  opts = { delay: 300, ...opts };
+
   // Allow components to specify they wish to re-use the modal container
   const rootElId = `modals-root${opts.modalId || Date.now()}`;
   let rootDom = null;
@@ -35,14 +38,16 @@ function open(componentRenderer, opts = {}) {
       ReactDOM.render(componentRenderer({ onClose, isShown }), getRoot());
     }
 
-    function onClose(...args) {
+    async function onClose(...args) {
       currentConfig = {
         ...currentConfig,
         isShown: false,
       };
       render(currentConfig);
-      resolve(...args);
+      await new Promise((resolveDelay) => setTimeout(resolveDelay, opts.delay));
+      ReactDOM.unmountComponentAtNode(getRoot());
       getRoot().remove();
+      resolve(...args);
     }
 
     render(currentConfig);
