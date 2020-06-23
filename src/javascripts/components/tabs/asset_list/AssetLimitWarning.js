47a1f35273e1cd234965da26500ca21a2b7f4972
Gui Santos <guilherme.goncalvessantos@gmail.com>
2020-06-23 16:51:11 +0200

    test: add tests for the AssetLimitWarning component

--- src/javascripts/components/tabs/asset_list/AssetLimitWarning.js
+++ src/javascripts/components/tabs/asset_list/AssetLimitWarning.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from 'emotion';
-import { Note, TextLink } from '@contentful/forma-36-react-components';
+import { Note, Paragraph, TextLink } from '@contentful/forma-36-react-components';
 import tokens from '@contentful/forma-36-tokens';
 
 const styles = {
@@ -13,15 +13,16 @@ const styles = {
 function AssetLimitWarning({ isOrgOwner, onUpgradeSpace }) {
   return (
     <div className={styles.warningContainer}>
-      <Note>
-        The free community tier has a size limit of 50MB per asset.
-        <br />
-        To increase your limit,{' '}
-        {isOrgOwner ? (
-          <TextLink onClick={onUpgradeSpace}>upgrade this space</TextLink>
-        ) : (
-          <>the organization admin must upgrade this space.</>
-        )}
+      <Note testId="asset-limit-warning">
+        <Paragraph>The free community tier has a size limit of 50MB per asset.</Paragraph>
+        <Paragraph>
+          To increase your limit,{' '}
+          {isOrgOwner ? (
+            <TextLink onClick={onUpgradeSpace}>upgrade this space.</TextLink>
+          ) : (
+            <>the organization admin must upgrade this space.</>
+          )}
+        </Paragraph>
       </Note>
     </div>
   );
---
+++ src/javascripts/components/tabs/asset_list/AssetLimitWarning.spec.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import AssetLimitWarning from './AssetLimitWarning';
+
+describe('AssetLimitWarning', () => {
+  it('should render the warning note for a user that is NOT the space owner', () => {
+    build();
+
+    expect(screen.getByTestId('asset-limit-warning')).toHaveTextContent(
+      'The free community tier has a size limit of 50MB per asset.To increase your limit, the organization admin must upgrade this space.'
+    );
+  });
+
+  it('should render the warning note for a user that is the space owner', () => {
+    build({ isOrgOwner: true });
+
+    expect(screen.getByTestId('asset-limit-warning')).toHaveTextContent(
+      'The free community tier has a size limit of 50MB per asset.To increase your limit, upgrade this space.'
+    );
+  });
+
+  it('should call onUpgradeSpace when the "upgrade this space" link is clicked', () => {
+    const onUpgradeSpace = jest.fn();
+
+    build({ isOrgOwner: true, onUpgradeSpace });
+    userEvent.click(screen.getByText('upgrade this space.'));
+
+    expect(onUpgradeSpace).toHaveBeenCalled();
+  });
+});
+
+function build(customProps) {
+  const props = {
+    isOrgOwner: false,
+    onUpgradeSpace: jest.fn(),
+    ...customProps,
+  };
+
+  render(<AssetLimitWarning {...props} />);
+}
