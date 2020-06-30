dcea2053b634561a1c559ab60ea54b9495c8417d
Marco Link <marco.link@contentful.com>
2020-06-30 15:11:23 +0200

    chore(cypress-tests): migrate environments (#6304)

--- test/cypress/interactions/environments.ts
+++ test/cypress/interactions/environments.ts
@@ -1,10 +1,59 @@
-import { defaultSpaceId, defaultHeader } from '../util/requests';
+import { defaultHeader, defaultSpaceId } from '../util/requests';
 
-// TODO: Only one environment in environments.json? Looks weird
-const environments = require('../fixtures/responses/environments.json');
+const environmentsResponse = (environments: object[]): Object => ({
+  total: environments.length,
+  limit: 100,
+  skip: 0,
+  sys: {
+    type: 'Array',
+  },
+  items: environments,
+});
+
+const environment = (name: string): Object => {
+  return {
+    name: name,
+    sys: {
+      type: 'Environment',
+      id: name,
+      version: 1,
+      space: {
+        sys: {
+          type: 'Link',
+          linkType: 'Space',
+          id: '1ustzobjs3hp',
+        },
+      },
+      status: {
+        sys: {
+          type: 'Link',
+          linkType: 'Status',
+          id: 'ready',
+        },
+      },
+      createdBy: {
+        sys: {
+          type: 'Link',
+          linkType: 'User',
+          id: '0mHgmvLLYcbQqehqEm7i0w',
+        },
+      },
+      createdAt: '2018-08-16T07:14:10Z',
+      updatedBy: {
+        sys: {
+          type: 'Link',
+          linkType: 'User',
+          id: '0mHgmvLLYcbQqehqEm7i0w',
+        },
+      },
+      updatedAt: '2018-08-16T07:14:10Z',
+    },
+  };
+};
 
 export enum States {
   MASTER = 'environments/only-master',
+  NEW = 'environments/new',
 }
 
 export const queryFirst101EnvironmentsInDefaultSpace = {
@@ -23,10 +72,74 @@ export const queryFirst101EnvironmentsInDefaultSpace = {
       },
       willRespondWith: {
         status: 200,
-        body: environments,
+        body: environmentsResponse([environment('master')]),
+        //        body: environments
       },
     }).as('queryFirst101EnvironmentsInDefaultSpace');
 
     return '@queryFirst101EnvironmentsInDefaultSpace';
+  },
+  willFindTwo() {
+    cy.addInteraction({
+      provider: 'environments',
+      state: States.NEW,
+      uponReceiving: `a query for the first 101 environments in the space "${defaultSpaceId} and 2 environments"`,
+      withRequest: {
+        method: 'GET',
+        path: `/spaces/${defaultSpaceId}/environments`,
+        headers: defaultHeader,
+        query: {
+          limit: '101',
+        },
+      },
+      willRespondWith: {
+        status: 200,
+        body: environmentsResponse([environment('master'), environment('123')]),
+      },
+    }).as('queryFirst101EnvironmentsInDefaultSpaceWithNewEnv');
+
+    return '@queryFirst101EnvironmentsInDefaultSpaceWithNewEnv';
+  },
+};
+
+export const putEnvironmentInDefaultSpace = {
+  willCreate(name: string) {
+    cy.addInteraction({
+      provider: 'environments',
+      state: States.NEW,
+      uponReceiving: `a query for creating environment "123" in the space "${defaultSpaceId}"`,
+      withRequest: {
+        method: 'PUT',
+        path: `/spaces/${defaultSpaceId}/environments/${name}`,
+        headers: defaultHeader,
+      },
+      willRespondWith: {
+        status: 200,
+        body: environment(name),
+      },
+    }).as('createEnvironmentsInDefaultSpace');
+
+    return '@createEnvironmentsInDefaultSpace';
+  },
+};
+
+export const deleteEnvironmentInDefaultSpace = {
+  willDelete(name: string) {
+    cy.addInteraction({
+      provider: 'environments',
+      state: States.NEW,
+      uponReceiving: `a query for deleting environment "123" in the space "${defaultSpaceId}"`,
+      withRequest: {
+        method: 'DELETE',
+        path: `/spaces/${defaultSpaceId}/environments/${name}`,
+        headers: defaultHeader,
+      },
+      willRespondWith: {
+        status: 200,
+        body: '',
+      },
+    }).as('deleteEnvironmentInDefaultSpace');
+
+    return '@deleteEnvironmentInDefaultSpace';
   },
 };
--- test/cypress/interactions/resources.ts
+++ test/cypress/interactions/resources.ts
@@ -1,9 +1,11 @@
 import { defaultSpaceId, defaultEnvironmentId } from '../util/requests';
 
 const resourcesWithLimitsReached = require('../fixtures/responses/resources-with-limits-reached.json');
+const resources = require('../fixtures/responses/resources.json');
 
 enum States {
   SEVERAL_WITH_LIMITS_REACHED = 'resources/several-with-limits-reached',
+  SEVERAL = 'resources/several',
 }
 
 export const getResourcesWithLimitsReached = {
@@ -26,5 +28,28 @@ export const getResourcesWithLimitsReached = {
     }).as('getResourcesWithLimitsReached');
 
     return '@getResourcesWithLimitsReached';
+  },
+};
+
+export const getResourcesWithEnvironment = {
+  willReturnSeveral() {
+    cy.addInteraction({
+      provider: 'resources',
+      state: States.SEVERAL,
+      uponReceiving: `a request to get resources with environments of space "${defaultSpaceId}"`,
+      withRequest: {
+        method: 'GET',
+        path: `/spaces/${defaultSpaceId}/resources/environment`,
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+        },
+      },
+      willRespondWith: {
+        status: 200,
+        body: resources,
+      },
+    }).as('getResourcesWithEnvironment');
+
+    return '@getResourcesWithEnvironment';
   },
 };
