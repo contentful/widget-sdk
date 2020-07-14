d710868b3568bb160aaf8cfebf826c1e898d9521
Jakub Elżbieciak <jakub@contentful.com>
2020-07-14 14:49:06 +0200

    Remove legacy namespace consts

--- src/javascripts/widgets/WidgetNamespaces.js
+++
@@ -1,5 +0,0 @@
-export const NAMESPACE_BUILTIN = 'builtin';
-export const NAMESPACE_EXTENSION = 'extension';
-export const NAMESPACE_SIDEBAR_BUILTIN = 'sidebar-builtin';
-export const NAMESPACE_APP = 'app';
-export const NAMESPACE_EDITOR_BUILTIN = 'editor-builtin';
