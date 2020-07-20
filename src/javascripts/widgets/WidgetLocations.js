073e2f61d08f123dee7d9460688bc3580bebd780
Manuel Spagnolo <spagnolo.manu@gmail.com>
2020-07-20 12:38:42 +0200

    chore: [EXT-2046] remove unused constant

--- src/javascripts/widgets/WidgetLocations.js
+++ src/javascripts/widgets/WidgetLocations.js
@@ -3,4 +3,3 @@ export const LOCATION_ENTRY_FIELD_SIDEBAR = 'entry-field-sidebar';
 export const LOCATION_ENTRY_SIDEBAR = 'entry-sidebar';
 export const LOCATION_DIALOG = 'dialog';
 export const LOCATION_ENTRY_EDITOR = 'entry-editor';
-export const LOCATION_APP_CONFIG = 'app-config';
