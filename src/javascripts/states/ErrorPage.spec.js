be6a53124b044090e055094efe5135769a12f36d
Joshua Smock <jo-sm@users.noreply.github.com>
2020-07-27 13:02:35 +0200

    feat: Create a generic error page for the app

---
+++ src/javascripts/states/ErrorPage.spec.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ErrorPage from './ErrorPage';
+import { screen, render, fireEvent } from '@testing-library/react';
+import { go } from 'states/Navigator';
+
+jest.mock('states/Navigator', () => ({
+  go: jest.fn(),
+}));
+
+describe('ErrorPage', () => {
+  it('should match the snapshot', () => {
+    expect(build()).toMatchSnapshot();
+  });
+
+  it('should go to the home path when the "home" button is clicked', () => {
+    build();
+
+    fireEvent.click(screen.getByTestId('home-button'));
+
+    expect(go).toBeCalledWith({
+      path: ['home'],
+    });
+  });
+});
+
+function build() {
+  render(<ErrorPage />);
+}
