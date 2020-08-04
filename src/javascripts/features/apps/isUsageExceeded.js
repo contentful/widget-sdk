4a960dab67b852f890d306fea333d0ed41e45ce8
Kado Damball <kadodamball@hotmail.com>
2020-08-04 13:36:51 +0200

    fix(apps-limit): Fix message check and add installed apps counter

--- src/javascripts/features/apps/isUsageExceeded.js
+++ src/javascripts/features/apps/isUsageExceeded.js
@@ -2,9 +2,9 @@ import { get } from 'lodash';
 
 // Needs to match the value in:
 // https://github.com/contentful/extensibility-api/blob/master/lib/entities/constants.ts
-const APP_INSTALLATION_LIMIT = 10;
+export const APP_INSTALLATION_LIMIT = 10;
 
-export const USAGE_EXCEEDED_MESSAGE = `You’ve reached the limit of ${APP_INSTALLATION_LIMIT} installed apps in this environment.`;
+export const USAGE_EXCEEDED_MESSAGE = `You’ve reached the limit of ${APP_INSTALLATION_LIMIT} installed apps in this environment. To fix this, uninstall an app or contact support.`;
 
 export function isUsageExceeded(installedApps) {
   return installedApps.length >= APP_INSTALLATION_LIMIT;
@@ -13,7 +13,7 @@ export function isUsageExceeded(installedApps) {
 export function isUsageExceededErrorResponse(err) {
   const isForbidden = parseInt(err.status, 10) === 403;
   const reason = get(err, ['data', 'details', 'reasons']);
-  const isUsageExceededReason = reason === 'Usage exceeded';
+  const isUsageExceededReason = reason === 'Usage exceeded.';
 
   return isForbidden && isUsageExceededReason;
 }
