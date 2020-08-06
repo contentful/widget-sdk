c80ce83f4ecd2fbf5dbc02cb3b1ec2376bd76dc2
Manuel Spagnolo <spagnolo.manu@gmail.com>
2020-08-06 17:57:30 +0200

    test: [EXT-2092] add read-only api tests

--- src/javascripts/app/widgets/NewWidgetApi/createReadOnlyApi.ts
+++ src/javascripts/app/widgets/NewWidgetApi/createReadOnlyApi.ts
@@ -11,16 +11,18 @@ enum ReadOnlyErrorCode {
   CannotOpenDialog = 'READ_ONLY_CANNOT_OPEN_DIALOG',
   CannotModifyEntry = 'READ_ONLY_CANNOT_MODIFY_ENTRY',
   CannotInvokeMethod = 'READ_ONLY_CANNOT_INVOKE_METHOD',
+  GenericError = 'READ_ONLY_GENERIC_ERROR',
 }
 
 enum ReadOnlyErrorMessage {
   CannotNavigate = 'Cannot navigate in read-only mode',
   CannotOpenDialog = 'Cannot open dialogs in read-only mode',
   CannotModifyEntry = 'Cannot modify entries in read-only mode',
-  CannotInvokeMethod = 'Cannot invoke method read-only mode',
+  CannotInvokeMethod = 'Cannot invoke method in read-only mode',
+  GenericError = 'Unexpected error while performing a read-only action',
 }
 
-export const makeReadOnlyApiError = (api: ReadOnlyApi, details?: string) => {
+export const makeReadOnlyApiError = (api: ReadOnlyApi | unknown, details?: string) => {
   switch (api) {
     case ReadOnlyApi.Dialog:
       return Object.assign(new Error(ReadOnlyErrorMessage.CannotOpenDialog), {
@@ -39,6 +41,10 @@ export const makeReadOnlyApiError = (api: ReadOnlyApi, details?: string) => {
       return Object.assign(new Error(ReadOnlyErrorMessage.CannotInvokeMethod), {
         code: ReadOnlyErrorCode.CannotInvokeMethod,
         details,
+      });
+    default:
+      return Object.assign(new Error(ReadOnlyErrorMessage.GenericError), {
+        code: ReadOnlyErrorCode.GenericError,
       });
   }
 };
