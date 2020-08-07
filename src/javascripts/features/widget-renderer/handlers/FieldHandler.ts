dc8f0ad9ba6d9ca8fc7abafff2d8ac388099cae8
Jakub Elżbieciak <jakub@contentful.com>
2020-08-07 11:57:44 +0200

    Wrap field value handlers

--- src/javascripts/features/widget-renderer/handlers/FieldHandler.ts
+++ src/javascripts/features/widget-renderer/handlers/FieldHandler.ts
@@ -1,19 +1,19 @@
 import { FieldAPI } from 'contentful-ui-extensions-sdk';
 
-export const makeSetValueHandler = (field: FieldAPI) => {
-  return function (fieldId: string, localeCode: string, value: any) {
+function withMatchingField(field: FieldAPI, cb: (value?: any) => any) {
+  return function (fieldId: string, localeCode: string, value?: any) {
     if (field.id === fieldId && field.locale === localeCode) {
-      return field.setValue(value);
+      return cb(value);
     }
   };
+}
+
+export const makeSetValueHandler = (field: FieldAPI) => {
+  return withMatchingField(field, (value) => field.setValue(value));
 };
 
 export const makeRemoveValueHandler = (field: FieldAPI) => {
-  return function (fieldId: string, localeCode: string) {
-    if (field.id === fieldId && field.locale === localeCode) {
-      return field.removeValue();
-    }
-  };
+  return withMatchingField(field, () => field.removeValue());
 };
 
 export const makeSetInvalidHandler = (field: FieldAPI) => {
