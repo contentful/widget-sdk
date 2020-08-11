e7c51d12778f9e63e79054b32255a7bba226d6ee
Manuel Spagnolo <spagnolo.manu@gmail.com>
2020-08-11 14:16:27 +0200

    chore: [EXT-2066] move validateState to AppState

--- src/javascripts/features/apps/AppState.spec.ts
+++ src/javascripts/features/apps/AppState.spec.ts
@@ -1,4 +1,4 @@
-import { getCurrentState } from './AppState';
+import { getCurrentState, validateState } from './AppState';
 import { Control, Editor, SidebarItem, WidgetNamespace } from 'features/widget-renderer';
 
 const DEFAULT_WIDGET_ID = 'widgetId';
@@ -273,6 +273,172 @@ describe('AppState', () => {
       expect(result).toEqual({
         EditorInterface: {},
       });
+    });
+  });
+
+  describe('#validateState', () => {
+    it('accepts empty target states', () => {
+      [null, undefined, {}, { EditorInterface: null }, { EditorInterface: [] }].forEach(
+        (targetState) => {
+          expect(() => validateState(targetState)).not.toThrow();
+        }
+      );
+    });
+    it('rejects unknown entity types', () => {
+      expect(() => validateState({ WebhookDefinition: [] })).toThrow();
+    });
+    describe('controls validation', () => {
+      it('accepts empty controls', () => {
+        [[], undefined, null].forEach((controls) => {
+          expect(() => {
+            validateState({
+              EditorInterface: {
+                someCtId: { controls },
+              },
+            });
+          }).not.toThrow();
+        });
+      });
+
+      it('accepts controls for multiple CTs with defined field IDs', () => {
+        expect(() => {
+          validateState({
+            EditorInterface: {
+              someCtId: {
+                controls: [{ fieldId: 'title' }, { fieldId: 'hello-world' }],
+              },
+              otherCt: {
+                controls: [{ fieldId: 'test' }],
+              },
+            },
+          });
+        }).not.toThrow();
+      });
+
+      it('rejects invalid controls', () => {
+        [
+          {}, // no field ID
+          { fieldId: 123 }, // field ID not a string
+          { fieldId: '' }, // field ID is empty
+        ].forEach((control) => {
+          expect(() => {
+            validateState({
+              EditorInterface: {
+                someCtId: { controls: [control] },
+              },
+            });
+          }).toThrow();
+        });
+      });
+    });
+    describe('sidebar validation', () => {
+      it('accepts empty sidebar', () => {
+        [undefined, null].forEach((sidebar) => {
+          expect(() => {
+            validateState({
+              EditorInterface: {
+                someCtId: { sidebar },
+              },
+            });
+          }).not.toThrow();
+        });
+      });
+
+      it('accepts position for multiple CTs', () => {
+        expect(() => {
+          validateState({
+            EditorInterface: {
+              someCtId: {
+                sidebar: { position: 1 },
+              },
+              otherCt: {
+                sidebar: { position: 7 },
+              },
+            },
+          });
+        }).not.toThrow();
+      });
+
+      it('accepts sidebar set to true', () => {
+        expect(() => {
+          validateState({
+            EditorInterface: {
+              someCtId: { sidebar: true },
+            },
+          });
+        }).not.toThrow();
+      });
+
+      it('rejects invalid sidebar', () => {
+        [
+          { position: 'TEST' }, // position is not a number
+          { position: 1.23 }, // position is not an integer
+          { position: -1 }, // position is a negative number
+        ].forEach((sidebar) => {
+          expect(() => {
+            validateState({
+              EditorInterface: {
+                someCtId: { sidebar },
+              },
+            });
+          }).toThrow();
+        });
+      });
+    });
+    describe('editor validation', () => {
+      it('accepts empty editor', () => {
+        [null, undefined].forEach((editor) => {
+          expect(() => {
+            validateState({
+              EditorInterface: {
+                someCtId: { editor },
+              },
+            });
+          }).not.toThrow();
+        });
+      });
+
+      it('accepts editor set to true', () => {
+        expect(() => {
+          validateState({
+            EditorInterface: {
+              someCtId: { editor: true },
+            },
+          });
+        }).not.toThrow();
+      });
+
+      it('reject invalid editor', () => {
+        expect(() => {
+          validateState({
+            EditorInterface: {
+              someCtId: { editor: 'BOOM' },
+            },
+          });
+        }).toThrow();
+      });
+    });
+    it('allows to define all properties on multiple CTs', () => {
+      expect(() => {
+        validateState({
+          EditorInterface: {
+            someCtId: {
+              fields: [{ fieldId: 'title' }, { fieldId: 'hello' }],
+              editor: true,
+            },
+            otherCt: {
+              fields: [{ fieldId: 'author' }],
+              sidebar: {
+                position: 3,
+              },
+            },
+            yetAnotherCt: {
+              sidebar: true,
+              editor: true,
+            },
+          },
+        });
+      }).not.toThrow();
     });
   });
 });
--- src/javascripts/features/apps/AppState.ts
+++ src/javascripts/features/apps/AppState.ts
@@ -1,10 +1,10 @@
-import {set} from "lodash"
-import {Control} from "features/widget-renderer/interfaces";
+import { get, isObject, set } from 'lodash';
+import { Control } from 'features/widget-renderer/interfaces';
 
 export const getCurrentState = async (spaceContext, appId) => {
-  const {items: editorInterfaces} = await spaceContext.cma.getEditorInterfaces();
+  const { items: editorInterfaces } = await spaceContext.cma.getEditorInterfaces();
 
-  const CurrentState = {EditorInterface: {}};
+  const CurrentState = { EditorInterface: {} };
 
   for (const editorInterface of editorInterfaces) {
     const contentTypeId = editorInterface.sys?.contentType?.sys?.id;
@@ -20,27 +20,85 @@ export const getCurrentState = async (spaceContext, appId) => {
     const isIncludedInSidebar = positionInSidebar > -1;
 
     if (isIncludedInEditors(appId, editorInterface)) {
-      set(CurrentState.EditorInterface, [contentTypeId, 'editor'], true)
+      set(CurrentState.EditorInterface, [contentTypeId, 'editor'], true);
     }
 
     if (isIncludedInControls) {
-      const newControls = CurrentState.EditorInterface[contentTypeId]?.controls ?? []
-      set(CurrentState.EditorInterface, [contentTypeId, 'controls'], newControls.concat(controlsUsingApp.map((ei) => ({fieldId: ei.fieldId}))))
+      const newControls = CurrentState.EditorInterface[contentTypeId]?.controls ?? [];
+      set(
+        CurrentState.EditorInterface,
+        [contentTypeId, 'controls'],
+        newControls.concat(controlsUsingApp.map((ei) => ({ fieldId: ei.fieldId })))
+      );
     }
 
     if (isIncludedInSidebar) {
-      set(CurrentState.EditorInterface, [contentTypeId, 'sidebar'], {position: positionInSidebar});
+      set(CurrentState.EditorInterface, [contentTypeId, 'sidebar'], {
+        position: positionInSidebar,
+      });
     }
   }
 
   return CurrentState;
+};
+
+export const isUnsignedInteger = (n) => Number.isInteger(n) && n >= 0;
+
+export function validateState(targetState) {
+  const keys = Object.keys(targetState || {});
+
+  // Right now only target state of Editor Interfaces can be expressed.
+  const validKeys = keys.length === 0 || (keys.length === 1 && keys[0] === 'EditorInterface');
+  if (!validKeys) {
+    throw new Error('Invalid target state declared.');
+  }
+
+  const targetEditorInterfacesState = get(targetState, ['EditorInterface']) || {};
+  const validTargetEditorInterfacesState = isObject(targetEditorInterfacesState);
+  if (!validTargetEditorInterfacesState) {
+    throw new Error('Invalid target state declared for EditorInterface entities.');
+  }
+
+  Object.keys(targetEditorInterfacesState).forEach((ctId) => {
+    const ei = targetEditorInterfacesState[ctId];
+
+    const validControls = !ei.controls || Array.isArray(ei.controls);
+    if (!validControls) {
+      throw new Error(`Invalid target controls declared for EditorInterface ${ctId}.`);
+    }
+
+    (ei.controls || []).forEach((control: Control) => {
+      const validControl =
+        isObject(control) && typeof control.fieldId === 'string' && control.fieldId.length > 0;
+      if (!validControl) {
+        throw new Error(`Invalid target controls declared for EditorInterface ${ctId}.`);
+      }
+    });
+
+    const validSidebar = !ei.sidebar || ei.sidebar === true || isObject(ei.sidebar);
+    if (!validSidebar) {
+      throw new Error(`Invalid target sidebar declared for EditorInterface ${ctId}.`);
+    }
+
+    if (isObject(ei.sidebar)) {
+      const validPosition = !ei.sidebar.position || isUnsignedInteger(ei.sidebar.position);
+      if (!validPosition) {
+        throw new Error(`Invalid target sidebar declared for EditorInterface ${ctId}.`);
+      }
+    }
+
+    const validEditor = !ei.editor || ei.editor === true;
+    if (!validEditor) {
+      throw new Error(`Invalid target editor declared for EditorInterface ${ctId}`);
+    }
+  });
 }
 
 const isIncludedInEditors = (appId, editorInterface): Boolean => {
   if (editorInterface.editor) {
     return appId === editorInterface.editor.widgetId;
   } else if (editorInterface.editors) {
-    return editorInterface.editors.some(({widgetId}) => widgetId === appId);
+    return editorInterface.editors.some(({ widgetId }) => widgetId === appId);
   }
 
   return false;
@@ -48,14 +106,14 @@ const isIncludedInEditors = (appId, editorInterface): Boolean => {
 
 const getControlsUsingApp = (appId, editorInterface): Array<Control> => {
   if (editorInterface.controls) {
-    return editorInterface.controls.filter(({widgetId}) => widgetId === appId);
+    return editorInterface.controls.filter(({ widgetId }) => widgetId === appId);
   }
   return [];
 };
 
 const getPositionInSidebar = (appId, editorInterface) => {
   if (editorInterface.sidebar) {
-    return editorInterface.sidebar.findIndex(({widgetId}) => widgetId === appId);
+    return editorInterface.sidebar.findIndex(({ widgetId }) => widgetId === appId);
   }
   return -1;
 };
