8fcda3f2156c2a182ab4ec1a98d37cc59b71dbeb
Mike Mitchell <mike.mitchell@contentful.com>
2020-08-17 14:07:49 +0200

    feat(entity-list): add tooltips to table cells

--- src/javascripts/core/components/ActionPerformerName/ActionPerformerName.js
+++ src/javascripts/core/components/ActionPerformerName/ActionPerformerName.js
@@ -4,6 +4,10 @@ import { ActionPerformer } from '../ActionPerformer';
 // eslint-disable-next-line no-unused-vars
 export const ActionPerformerName = ({ children, ...props }) => (
   <ActionPerformer {...props}>
-    {({ formattedName }) => <span data-test-id="action-performer-name">{formattedName}</span>}
+    {({ formattedName }) => (
+      <span data-test-id="action-performer-name" title={formattedName}>
+        {formattedName}
+      </span>
+    )}
   </ActionPerformer>
 );
