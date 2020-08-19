81d20dcc8ca36cf5f2b0a93a35eaef766f026885
Jakub Elżbieciak <jakub@contentful.com>
2020-08-19 09:11:23 +0200

    Handle set/removeValue for any arbitrary field

--- src/javascripts/features/widget-renderer/handlers/FieldHandler.ts
+++ src/javascripts/features/widget-renderer/handlers/FieldHandler.ts
@@ -1,19 +1,15 @@
-import { FieldAPI } from 'contentful-ui-extensions-sdk';
+import { FieldAPI, EntryAPI } from 'contentful-ui-extensions-sdk';
 
-function withMatchingField(field: FieldAPI, cb: (value?: any) => any) {
-  return function (fieldId: string, localeCode: string, value?: any) {
-    if (field.id === fieldId && field.locale === localeCode) {
-      return cb(value);
-    }
+export const makeSetValueHandler = (entry: EntryAPI) => {
+  return (fieldId: string, localeCode: string, value: any) => {
+    return entry.fields[fieldId].getForLocale(localeCode).setValue(value);
   };
-}
-
-export const makeSetValueHandler = (field: FieldAPI) => {
-  return withMatchingField(field, (value) => field.setValue(value));
 };
 
-export const makeRemoveValueHandler = (field: FieldAPI) => {
-  return withMatchingField(field, () => field.removeValue());
+export const makeRemoveValueHandler = (entry: EntryAPI) => {
+  return (fieldId: string, localeCode: string) => {
+    return entry.fields[fieldId].getForLocale(localeCode).removeValue();
+  };
 };
 
 export const makeSetInvalidHandler = (field: FieldAPI) => {
