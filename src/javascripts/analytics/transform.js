contentful/widget-sdk

commit 01cba746e4252c89c2991bdac4eda4c715f555bc

refactor(migrate createnewspacedirective): migrate directive to React (#5664)
* refactor(migrate createnewspacedirective): migrate directive to React

* refactor(migrate createnewspacedirective): update formating

* refactor(migrate createnewspacedirective): address PR comments

src/javascripts/services/CreateSpace/utils.js
@@ -1,0 +1,207 @@
+import createResourceService from 'services/ResourceService';
+import client from 'services/client';
+import * as TokenStore from 'services/TokenStore';
+import _ from 'lodash';
+import * as Analytics from 'analytics/Analytics';
+import * as Navigator from 'states/Navigator';
+import { getApiKeyRepo } from 'app/settings/api/services/ApiKeyRepoInstance';
+import { getTemplate } from 'services/SpaceTemplateLoader';
+import * as spaceTemplateCreator from 'services/SpaceTemplateCreator';
+import * as spaceTemplateEvents from 'analytics/events/SpaceCreation';
+import * as logger from 'services/logger';
+import { Notification } from '@contentful/forma-36-react-components';
+
+const DEFAULT_ERROR_MESSAGE =
+  'Could not create Space. If the problem persists please get in contact with us.';
+
+function hasErrorOnField(errors, fieldPath, errorName) {
+  return _.some(errors, (e) => e.path === fieldPath && e.name === errorName);
+}
+
+const handleSpaceCreation = (
+  spaceContext,
+  newSpace,
+  template,
+  selectedLocale,
+  onCloseCreationDialog,
+  setTemplateProgress,
+  setCreationFinished
+) => {
+  const templateName = _.get(template, 'name');
+
+  Navigator.go({
+    path: 'spaces.detail',
+    params: {
+      spaceId: newSpace.sys.id,
+    },
+  })
+    .then(() => {
+      const spaceCreateEventData =
+        templateName === 'Blank'
+          ? { templateName: templateName }
+          : {
+              templateName: templateName,
+              entityAutomationScope: { scope: 'space_template' },
+            };
+
+      Analytics.track('space:create', spaceCreateEventData);
+
+      if (templateName === 'Blank') {
+        getApiKeyRepo().create(
+          'Example Key',
+          'We’ve created an example API key for you to help you get started.'
+        );
+
+        onCloseCreationDialog();
+      } else {
+        setTemplateProgress(true);
+        return loadSelectedTemplate(spaceContext, template, selectedLocale);
+      }
+    })
+    .finally(() => {
+      setCreationFinished(true);
+    });
+};
+
+const createNewSpace = (
+  data,
+  organization,
+  spaceContext,
+  template,
+  onCloseCreationDialog,
+  setTemplateProgress,
+  setCreationFinished
+) => {
+  if (!template) {
+    template = { name: 'Blank' };
+  }
+
+  Analytics.track('space:template_selected', { templateName: template.name });
+
+  const selectedLocale = data.defaultLocale;
+
+  const dataWithUpdatedLocale = Object.assign({}, data, {
+    defaultLocale: selectedLocale,
+  });
+
+  client
+    .createSpace(dataWithUpdatedLocale, organization.sys.id)
+    .then((newSpace) => {
+      // Create space
+      TokenStore.refresh().then(
+        _.partial(
+          handleSpaceCreation,
+          spaceContext,
+          newSpace,
+          template,
+          selectedLocale,
+          onCloseCreationDialog,
+          setTemplateProgress,
+          setCreationFinished
+        )
+      );
+    })
+    .catch((error) => {
+      const errors = _.get(error, 'body.details.errors');
+      const fieldErrors = [
+        { name: 'length', path: 'name', message: 'Space name is too long' },
+        { name: 'invalid', path: 'default_locale', message: 'Invalid locale' },
+      ];
+
+      // If there aren't explicit errors from the response,
+      // this means that something went wrong.
+      if (!errors || !errors.length) {
+        Notification.error(DEFAULT_ERROR_MESSAGE);
+        logger.logServerWarn('Could not create Space', { error: error });
+
+        return;
+      }
+
+      _.forEach(fieldErrors, (e) => {
+        if (hasErrorOnField(errors, e.path, e.name)) {
+          Notification.error(e.message);
+        }
+      });
+    });
+};
+
+function createTemplate(template, retried, spaceContext, templateCreator) {
+  const createTemplatePromises = templateCreator.create(template);
+
+  // we suppress errors, since `contentCreated` will handle them
+  // We need to catch all errors, because http requests
+  // are backed by $q, and we have global handlers on
+  // $q errors
+  createTemplatePromises.spaceSetup.catch(() => {});
+
+  return createTemplatePromises.contentCreated
+    .catch((data) => {
+      if (!retried) {
+        createTemplate(data.template, true);
+      }
+    })
+    .then(() => spaceContext.publishedCTs.refresh());
+}
+
+function loadSelectedTemplate(spaceContext, selectedTemplate, selectedLocale) {
+  const itemHandlers = {
+    // no need to show status of individual items
+    onItemSuccess: function (entityId, entityData, templateName) {
+      spaceTemplateEvents.entityActionSuccess(
+        entityId,
+        Object.assign({}, entityData, { entityAutomationScope: { scope: 'space_template' } }),
+        templateName
+      );
+    },
+    onItemError: _.noop,
+  };
+
+  const templateCreator = spaceTemplateCreator.getCreator(
+    spaceContext,
+    itemHandlers,
+    selectedTemplate,
+    selectedLocale
+  );
+  return getTemplate(selectedTemplate).then((template, retried) =>
+    createTemplate(template, retried, spaceContext, templateCreator)
+  );
+}
+
+export const requestSpaceCreation = (
+  data,
+  organization,
+  spaceContext,
+  template,
+  onCloseCreationDialog,
+  setTemplateProgress,
+  setCreationFinished
+) => {
+  const resources = createResourceService(organization.sys.id, 'organization');
+
+  // First check that there are resources available
+  // to create the space
+  resources
+    .canCreate('space')
+    .then((canCreate) => {
+      if (canCreate) {
+        // Resources are available. Attempt to create a new space
+        createNewSpace(
+          data,
+          organization,
+          spaceContext,
+          template,
+          onCloseCreationDialog,
+          setTemplateProgress,
+          setCreationFinished
+        );
+      } else {
+        resources.messagesFor('space').then((errorObj) => {
+          Notification.error(errorObj.error);
+        });
+      }
+    })
+    .catch((error) => {
+      Notification.error(DEFAULT_ERROR_MESSAGE);
+      logger.logServerWarn('Could not fetch permissions', { error: error });
+    });
+};

src/javascripts/AngularInit.js
@@ -52,9 +52,6 @@ export const angularInitRun = [
       import(/* webpackMode: "eager" */ 'components/field_dialog/validationAssetTypesController'),
       import(/* webpackMode: "eager" */ 'components/field_dialog/validationLinkTypeController'),
       import(/* webpackMode: "eager" */ 'components/shared/cfSelectionDirective'),
-      import(
-        /* webpackMode: "eager" */ 'components/shared/create_new_space/createNewSpaceDirective'
-      ),
       import(/* webpackMode: "eager" */ 'components/shared/endlessContainerDirective'),
       import(/* webpackMode: "eager" */ 'components/shared/listViewsController'),
       import(/* webpackMode: "eager" */ 'components/shared/space-wizard/SpaceWizardDirective'),

src/javascripts/components/shared/create_new_space/Template.js
@@ -1,323 +1,0 @@
-import { h } from 'utils/legacy-html-hyperscript';
-
-export default function () {
-  return h('div.create-new-space-dialog.modal-dialog', [
-    h('div.modal-dialog__header.create-new-space-dialog__header', [
-      h('button.create-new-space-dialog__close.modal-dialog__close', {
-        ngIf: "createSpace.viewState === 'createSpaceForm'",
-        ngClick: 'dialog.cancel()',
-      }),
-    ]),
-    renderCreateSpaceDialog(),
-    renderCreateSpaceInProgress(),
-  ]);
-}
-
-function renderCreateSpaceDialog() {
-  const headers = {
-    onboarding: 'Let’s create your first space now',
-    default: 'Create a new space',
-  };
-  return h(
-    'form',
-    {
-      ngShow: 'createSpace.viewState === "createSpaceForm"',
-      ngSubmit: 'createSpace.requestSpaceCreation()',
-      name: 'newSpaceForm',
-    },
-    [
-      h('div.create-new-space-dialog__content.modal-dialog__content', [
-        h('h2.create-new-space-dialog__heading', [
-          `{{createSpace.isOnboarding ? "${headers.onboarding}" : "${headers.default}"}}`,
-        ]),
-        h('p.create-new-space-dialog__subheading', [
-          'A space is a place where you keep all the content related to a single project. <br> You are creating this space for the organization <em>{{createSpace.newSpace.organization.name}}</em>.',
-        ]),
-        renderCreateSpaceForm(),
-        renderSpaceTemplates(),
-      ]),
-      h('div.modal-dialog__controls.create-new-space-dialog__controls--right', [
-        h(
-          'button.btn-action',
-          {
-            ngClass: "{'is-loading': createSpace.createSpaceInProgress}",
-            ngDisabled: 'createSpace.createSpaceInProgress',
-            dataTestId: 'create-space',
-          },
-          ['Create space']
-        ),
-      ]),
-    ]
-  );
-}
-
-function renderCreateSpaceForm() {
-  return h('div.create-new-space__form', [
-    h('div.create-new-space__fieldset', [
-      renderSpaceNameInput(),
-      renderLanguageSelect(),
-      renderRatePlanSelect(),
-    ]),
-    h(
-      'div.cfnext-form__field',
-      {
-        ngShow: 'createSpace.newSpace.errors.form',
-      },
-      [h('div.cfnext-form__field-error', ['{{createSpace.newSpace.errors.form}}'])]
-    ),
-    renderTemplateToggle(),
-  ]);
-}
-
-function renderSpaceNameInput() {
-  return h('div.cfnext-form__field', [
-    h('react-component', {
-      name: '@contentful/forma-36-react-components/TextField',
-      props: 'createSpace.buildSpaceNameInputProps()',
-    }),
-    renderErrorLabel('createSpace.newSpace.errors.fields.name'),
-  ]);
-}
-
-function renderLanguageSelect() {
-  return h(
-    'div.cfnext-form__field',
-    {
-      ngStyle: '{ visibility: createSpace.newSpace.useTemplate === false ? "visible" : "hidden" }',
-    },
-    [
-      h('react-component', {
-        name: '@contentful/forma-36-react-components/SelectField',
-        props: 'createSpace.buildLanguageSelectProps()',
-      }),
-      renderErrorLabel('createSpace.newSpace.errors.fields.default_locale'),
-    ]
-  );
-}
-
-function renderRatePlanSelect() {
-  return h(
-    'div.cfnext-form__field',
-    {
-      ngIf: 'createSpace.spaceRatePlans',
-    },
-    [
-      h('label', [h('strong', ['Rate Plan'])]),
-      h('select.cfnext-select-box', {
-        dataTestId: 'select-locale',
-        ngModel: 'createSpace.newSpace.data.productRatePlanId',
-        ngOptions: 'opt.id as opt.name for opt in createSpace.spaceRatePlans',
-        ngDisabled: 'createSpace.createSpaceInProgress',
-        ariaInvalid: '{{!!createSpace.newSpace.errors.fields.space_rate_plan}}',
-      }),
-      renderErrorLabel('createSpace.newSpace.errors.fields.space_rate_plan'),
-    ]
-  );
-}
-
-function renderErrorLabel(expression) {
-  return h('p.cfnext-form__field-error', { ngShow: expression }, [`{{${expression}}}`]);
-}
-
-function renderTemplateToggle() {
-  return h('div.cfnext-form__field.create-new-space__form__radios', [
-    h('div.cfnext-form-option.create-new-space__form__option', [
-      h('input#newspace-template-none', {
-        type: 'radio',
-        id: 'newspace-template-none',
-        name: 'useTemplate',
-        ngValue: 'false',
-        ngModel: 'createSpace.newSpace.useTemplate',
-        ngDisabled: 'createSpace.createSpaceInProgress',
-      }),
-      h(
-        'label',
-        {
-          for: 'newspace-template-none',
-        },
-        [
-          h('strong', ['Create an empty space.']),
-          h('span.create-new-space__form__label-description', [
-            ' I’ll fill it with my own content.',
-          ]),
-        ]
-      ),
-    ]),
-    h(
-      'div.cfnext-form-option.create-new-space__form__option',
-      {
-        ngShow: 'createSpace.templates',
-      },
-      [
-        h('input#newspace-template-usetemplate', {
-          type: 'radio',
-          id: 'newspace-template-usetemplate',
-          name: 'useTemplate',
-          ngValue: 'true',
-          ngModel: 'createSpace.newSpace.useTemplate',
-          ngDisabled: 'createSpace.createSpaceInProgress',
-        }),
-        h(
-          'label',
-          {
-            for: 'newspace-template-usetemplate',
-          },
-          [
-            h('strong', ['Create an example space.']),
-            h('span.create-new-space__form__label-description', [
-              ' I’d like to see how things work first.',
-            ]),
-          ]
-        ),
-      ]
-    ),
-  ]);
-}
-
-function renderSpaceTemplates() {
-  return h(
-    'div.modal-dialog__slice.create-new-space__templates',
-    {
-      ngClass:
-        "{'open': createSpace.newSpace.useTemplate && createSpace.templates.length, 'close': !createSpace.newSpace.useTemplate}",
-    },
-    [
-      h('cf-icon', { name: 'arrow-up' }),
-      h('div.create-new-space__templates__inner', [
-        h(
-          'div',
-          {
-            ngShow: '!createSpace.templates || !createSpace.templates.length',
-            style: {
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            },
-          },
-          [
-            h('react-component', {
-              name: '@contentful/forma-36-react-components/Spinner',
-              props: '{size: "large", style: {display: "block"}}',
-            }),
-          ]
-        ),
-        h(
-          'div',
-          {
-            ngShow: 'createSpace.templates && createSpace.templates.length',
-          },
-          [
-            h('div.create-new-space__templates__nav', [
-              h(
-                'a.create-new-space__templates__navitem',
-                {
-                  ngRepeat: 'template in createSpace.templates',
-                  ngClick: 'createSpace.selectTemplate(template)',
-                  ngClass: '{selected: createSpace.newSpace.selectedTemplate === template}',
-                },
-                [
-                  h('cf-icon.create-new-space__templates__navitem__icon', {
-                    ngIf: 'template.svgName',
-                    name: '{{template.svgName}}',
-                  }),
-                  '{{template.name}}',
-                ]
-              ),
-            ]),
-            h(
-              'div.create-new-space__templates__description',
-              {
-                ngShow: 'template === createSpace.newSpace.selectedTemplate',
-                ngRepeat: 'template in createSpace.templates',
-              },
-              [
-                h('img.create-new-space__templates__image', {
-                  ngSrc: '{{template.image.fields.file.url}}',
-                }),
-                h('div.create-new-space__templates__text', [
-                  h('div', {
-                    ngBindHtml: 'template.descriptionV2',
-                  }),
-                ]),
-              ]
-            ),
-          ]
-        ),
-      ]),
-    ]
-  );
-}
-
-function renderCreateSpaceInProgress() {
-  const infoItems = [
-    {
-      icon: 'page-ct',
-      title: 'Content model',
-      description:
-        'The content model is comprised of content types, they work like a stencil which defines the structure of entries. We’re creating a few different content types for you to see how it works.',
-    },
-    {
-      icon: 'page-content',
-      title: 'Content',
-      description:
-        'Your content is made up of entries. The space will include a couple of entries based on the content types mentioned above.',
-    },
-    {
-      icon: 'page-media',
-      title: 'Media',
-      description:
-        'Your media consists of assets, which are external files, from images or videos to documents. Your entries will have a few assets to complement them.',
-    },
-    {
-      icon: 'page-apis',
-      title: 'API keys',
-      description:
-        'An API key is the token that you’ll use to retrieve your content. We created a few API keys so that you can get started fetching your content right away.',
-    },
-  ];
-
-  return h(
-    'div',
-    {
-      ngShow: "createSpace.viewState === 'creatingTemplate'",
-    },
-    [
-      h('div.modal-dialog__content', [
-        h('div.create-new-space__templates__status', [
-          h('div.spinner', { ngShow: 'createSpace.createTemplateInProgress' }),
-          h('cf-icon', {
-            name: 'checkmark',
-            scale: '2',
-            ngShow: '!createSpace.createTemplateInProgress',
-          }),
-        ]),
-        h('h2.create-new-space-dialog__heading', ['Hang on, we’re preparing your space']),
-        h('p.create-new-space-dialog__subheading', [
-          'In the meantime, let us quickly explain the kind of things you’ll find in your space',
-        ]),
-        h('div.create-new-space__templates__entities', infoItems.map(renderInfoItem)),
-      ]),
-      h('div.modal-dialog__controls.create-new-space-dialog__controls--centered', [
-        h(
-          'button.btn-action',
-          {
-            dataTestId: 'get-started',
-            ngDisabled: 'createSpace.createTemplateInProgress',
-            ngClick: 'dialog.confirm()',
-          },
-          ['Get started']
-        ),
-      ]),
-    ]
-  );
-}
-
-function renderInfoItem({ icon, title, description }) {
-  return h('div.create-new-space__templates__entity', [
-    h('cf-icon', { name: icon }),
-    h('div.create-new-space__templates__entity__description', [
-      h('h3', [title]),
-      h('p', [description]),
-    ]),
-  ]);
-}

commit 817f9dbbfd2cbd6801b7b104d367127e4e508318

fix(analytics): register space:template_selected event
The `space:template_selected` event has not been registered when first
implemented in 01cba74 which caused the event to only be tracked as a
`tracking:invalid_event` event.
Also changes to only track the event when a template has actually been
selected.

src/javascripts/analytics/transform.js
@@ -162,6 +162,7 @@ registerActionEvent('api_key:create', EntityAction);
 registerActionEvent('asset:create', EntityAction);
 
 registerActionEvent('space:create', SpaceCreate);
+registerGenericEvent('space:template_selected');
 
 registerSpaceWizardEvent('space_wizard:open');
 registerSpaceWizardEvent('space_wizard:cancel');

src/javascripts/services/CreateSpace/utils.js
@@ -73,9 +73,9 @@ const createNewSpace = (
 ) => {
   if (!template) {
     template = { name: 'Blank' };
+  } else {
+    Analytics.track('space:template_selected', { templateName: template.name });
   }
-
-  Analytics.track('space:template_selected', { templateName: template.name });
 
   const selectedLocale = data.defaultLocale;
 