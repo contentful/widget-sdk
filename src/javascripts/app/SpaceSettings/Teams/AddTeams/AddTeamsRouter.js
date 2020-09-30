afd1483fe98eb8313d3579acf6b2291728b9ac0c
Cezar Sampaio <cezar.sampaio@contentful.com>
2020-09-30 11:30:53 +0200

    refactor: remove angular space context from AddTeamsRouter (#6859)

--- src/javascripts/app/SpaceSettings/Teams/AddTeams/AddTeamsRouter.js
+++ src/javascripts/app/SpaceSettings/Teams/AddTeams/AddTeamsRouter.js
@@ -1,31 +1,21 @@
 import React, { useCallback } from 'react';
-import PropTypes from 'prop-types';
 import { useAsync } from 'core/hooks';
 import ForbiddenPage from 'ui/Pages/Forbidden/ForbiddenPage';
 import UnknownErrorMessage from 'components/shared/UnknownErrorMessage';
 import DocumentTitle from 'components/shared/DocumentTitle';
-import { getSpace } from 'services/TokenStore';
 import { createOrganizationEndpoint, createSpaceEndpoint } from 'data/EndpointFactory';
 import { getAllTeams, getTeamsSpaceMembershipsOfSpace } from 'access_control/TeamRepository';
 import { getInstance as createRoleRepo } from 'access_control/RoleRepository';
 import { getSectionVisibility } from 'access_control/AccessChecker';
-import { getModule } from 'core/NgRegistry';
 import { FetcherLoading } from 'app/common/createFetcherComponent';
 
 import AddTeamsPage from './AddTeamsPage';
+import { useSpaceEnvContext } from 'core/services/SpaceEnvContext/useSpaceEnvContext';
 
-const fetch = (spaceId) => async () => {
-  const spaceContext = getModule('spaceContext');
-
-  const {
-    organization: {
-      sys: { id: organizationId },
-    },
-  } = await getSpace(spaceId);
-
+const fetch = (spaceId, organizationId, space) => async () => {
   const orgEndpoint = createOrganizationEndpoint(organizationId);
   const spaceEndpoint = createSpaceEndpoint(spaceId);
-  const roleRepo = createRoleRepo(spaceContext.space);
+  const roleRepo = createRoleRepo(space);
 
   const [roles, teams, teamSpaceMemberships] = await Promise.all([
     roleRepo.getAll(),
@@ -36,8 +26,15 @@ const fetch = (spaceId) => async () => {
   return { teams, teamSpaceMemberships, roles };
 };
 
-export default function AddTeamsRouter({ spaceId }) {
-  const { isLoading, error, data } = useAsync(useCallback(fetch(spaceId), [spaceId]));
+export default function AddTeamsRouter() {
+  const { currentSpaceId, currentOrganizationId, currentSpace } = useSpaceEnvContext();
+  const { isLoading, error, data } = useAsync(
+    useCallback(fetch(currentSpaceId, currentOrganizationId, currentSpace), [
+      currentSpaceId,
+      currentOrganizationId,
+      currentSpace,
+    ])
+  );
 
   const hasAccess = getSectionVisibility().teams;
   const hasError = error || !data;
@@ -49,7 +46,7 @@ export default function AddTeamsRouter({ spaceId }) {
   }
 
   if (!hasError) {
-    page = <AddTeamsPage spaceId={spaceId} {...data} />;
+    page = <AddTeamsPage spaceId={currentSpaceId} {...data} />;
   }
 
   if (!hasAccess) {
@@ -63,7 +60,3 @@ export default function AddTeamsRouter({ spaceId }) {
     </>
   );
 }
-
-AddTeamsRouter.propTypes = {
-  spaceId: PropTypes.string.isRequired,
-};
