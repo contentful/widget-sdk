0d64b8c7413bcaf2d72e08155dea891c89d864d6
Joshua Smock <jo-sm@users.noreply.github.com>
2020-10-01 18:10:14 +0200

    improvement: Add a loading state for the Zuora CC iframe

--- src/javascripts/features/organization-billing/components/ZuoraCreditCardIframe.js
+++ src/javascripts/features/organization-billing/components/ZuoraCreditCardIframe.js
@@ -1,10 +1,17 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { css, cx } from 'emotion';
+import {
+  Form,
+  TextField,
+  SelectField,
+  Option,
+  Button,
+} from '@contentful/forma-36-react-components';
+import { Grid, GridItem } from '@contentful/forma-36-react-components/dist/alpha';
 
 import { useAsync } from 'core/hooks/useAsync';
 import * as LazyLoader from 'utils/LazyLoader';
-import LoadingState from 'app/common/LoadingState';
 
 import { getHostedPaymentParams } from '../services/PaymentMethodService';
 
@@ -21,11 +28,33 @@ const styles = {
     },
     '> #z_hppm_iframe': {
       backgroundColor: 'transparent',
-      minHeight: '500px',
+      minHeight: '400px',
     },
   }),
   hide: css({
     display: 'none',
+  }),
+  selectContainer: css({
+    display: 'inline-block',
+    width: 'calc(50% - 0.25rem)',
+    marginRight: '0.25rem',
+  }),
+  gridItem: css({
+    marginBottom: '1rem',
+  }),
+  gridFullWidth: css({
+    gridColumn: '1 / 3',
+  }),
+  gridLeft: css({
+    gridColumn: '1 / 2',
+  }),
+  gridRight: css({
+    gridColumn: '2 / 3',
+  }),
+  submitButton: css({
+    marginTop: '1rem',
+    display: 'flex',
+    justifyContent: 'flex-end',
   }),
 };
 
@@ -71,7 +100,7 @@ export function ZuoraCreditCardIframe({ organizationId, countryCode, onSuccess, 
 
   return (
     <>
-      {loading && <LoadingState />}
+      {loading && <IframeLoadingState />}
       <div
         id="zuora_payment"
         data-test-id="zuora-payment-iframe"
@@ -87,3 +116,56 @@ ZuoraCreditCardIframe.propTypes = {
   onSuccess: PropTypes.func.isRequired,
   onError: PropTypes.func,
 };
+
+function IframeLoadingState() {
+  return (
+    <Form>
+      <Grid columns="1fr 1fr">
+        <GridItem className={cx(styles.gridItem, styles.gridFullWidth)}>
+          <TextField
+            name="name"
+            id="name"
+            labelText="Cardholder Name"
+            textInputProps={{ disabled: true }}
+          />
+        </GridItem>
+        <GridItem className={cx(styles.gridItem, styles.gridFullWidth)}>
+          <TextField
+            name="number"
+            id="number"
+            labelText="Card Number"
+            textInputProps={{ disabled: true }}
+          />
+        </GridItem>
+        <GridItem className={cx(styles.gridItem, styles.gridLeft)}>
+          <div className={styles.selectContainer}>
+            <SelectField
+              name="month"
+              id="month"
+              labelText="Month"
+              selectInputProps={{ disabled: true }}>
+              <Option value="">- Select One -</Option>
+            </SelectField>
+          </div>
+          <div className={styles.selectContainer}>
+            <SelectField
+              name="year"
+              id="year"
+              labelText="Year"
+              selectInputProps={{ disabled: true }}>
+              <Option value="">- Select One -</Option>
+            </SelectField>
+          </div>
+        </GridItem>
+        <GridItem className={cx(styles.gridItem, styles.gridRight)}>
+          <TextField name="cvv" id="cvv" labelText="CVV" textInputProps={{ disabled: true }} />
+        </GridItem>
+        <GridItem className={cx(styles.submitButton, styles.gridItem, styles.gridFullWidth)}>
+          <Button disabled loading>
+            Loading...
+          </Button>
+        </GridItem>
+      </Grid>
+    </Form>
+  );
+}
