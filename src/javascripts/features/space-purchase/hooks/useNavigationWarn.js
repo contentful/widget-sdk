37cab97d234b12b7ccd6ac381946f1d5645d3fb7
Gui Santos <guilherme.santos@contentful.com>
2020-10-08 17:00:25 +0200

    Poc/custom hook to handle space creation, template creation and exiting window warn (#6925)
    * chore: renaming functions
    
    * feat: move logic to a custom hook
    
    * refactor: move exting flow warn to its own component and hook
    
    * refactor: rename navigation warn hook
    
    * refactor: remove notifications from custom hook

---
+++ src/javascripts/features/space-purchase/hooks/useNavigationWarn.js
@@ -0,0 +1,59 @@
+import React, { useEffect } from 'react';
+import { ModalLauncher } from '@contentful/forma-36-react-components/dist/alpha';
+
+import { getModule } from 'core/NgRegistry';
+import { go } from 'states/Navigator';
+
+import { ConfirmNavigateModal } from '../components/ConfirmNavigateModal';
+
+function useNavigationWarn(selectedTemplate, pending) {
+  useEffect(() => {
+    let offStateChangeStart;
+
+    const handleBeforeUnload = (event) => {
+      event.preventDefault();
+      event.returnValue = '';
+    };
+
+    // We want to ensure that users don't click away if their space is not yet created
+    // or if the template is actively being created
+    if (pending) {
+      const $rootScope = getModule('$rootScope');
+
+      // Angular $on functions return a callback that is the event listener
+      // remover, rather than $rootScope.$off.
+      offStateChangeStart = $rootScope.$on(
+        '$stateChangeStart',
+        async (event, toState, toParams) => {
+          event.preventDefault();
+
+          const confirmed = await ModalLauncher.open(({ isShown, onClose }) => (
+            <ConfirmNavigateModal
+              isShown={isShown}
+              onClose={onClose}
+              withTemplate={!!selectedTemplate}
+            />
+          ));
+
+          if (confirmed) {
+            offStateChangeStart();
+
+            go({
+              path: toState,
+              params: toParams,
+            });
+          }
+        }
+      );
+
+      window.addEventListener('beforeunload', handleBeforeUnload);
+    }
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+      offStateChangeStart && offStateChangeStart();
+    };
+  }, [selectedTemplate, pending]);
+}
+
+export { useNavigationWarn };
