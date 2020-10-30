df03fe57fa4235682ea331c7d981ea2b4ec5c912
Cezar Sampaio <cezar.sampaio@contentful.com>
2020-10-30 09:40:03 +0100

    refactor: remove publishedCTs from content preview new route (#7034)

--- src/javascripts/features/content-preview/routes/ContentPreviewNewRoute.js
+++ src/javascripts/features/content-preview/routes/ContentPreviewNewRoute.js
@@ -1,64 +1,40 @@
-import React, { Component } from 'react';
-import { getModule } from 'core/NgRegistry';
+import React from 'react';
 import PropTypes from 'prop-types';
-import createFetcherComponent from 'app/common/createFetcherComponent';
 import { AdminOnly } from './AdminOnly';
-import StateRedirect from 'app/common/StateRedirect';
 import DocumentTitle from 'components/shared/DocumentTitle';
 
 import { ContentPreviewFormPage } from '../ContentPreviewFormPage';
-import { ContentPreviewFormSkeleton } from '../skeletons/ContentPreviewFormSkeleton';
 import { contentPreviewToInternal } from '../services/contentPreviewToInternal';
+import { useSpaceEnvContext } from 'core/services/SpaceEnvContext/useSpaceEnvContext';
 
-const ContentTypesFetcher = createFetcherComponent(() => {
-  const spaceContext = getModule('spaceContext');
+export function ContentPreviewNewRoute(props) {
+  const { currentSpaceContentTypes } = useSpaceEnvContext();
 
-  return spaceContext.publishedCTs.refreshBare();
-});
+  const initialValue = contentPreviewToInternal(
+    {
+      name: '',
+      description: '',
+      sys: {
+        version: 0,
+      },
+    },
+    currentSpaceContentTypes
+  );
 
-export class ContentPreviewNewRoute extends Component {
-  static propTypes = {
-    registerSaveAction: PropTypes.func.isRequired,
-    setDirty: PropTypes.func.isRequired,
-  };
-
-  render() {
-    return (
-      <AdminOnly>
-        <ContentTypesFetcher>
-          {({ isLoading, isError, data }) => {
-            if (isLoading) {
-              return <ContentPreviewFormSkeleton />;
-            }
-            if (isError) {
-              return <StateRedirect path="^.list" />;
-            }
-
-            const initialValue = contentPreviewToInternal(
-              {
-                name: '',
-                description: '',
-                sys: {
-                  version: 0,
-                },
-              },
-              data
-            );
-
-            return (
-              <React.Fragment>
-                <DocumentTitle title={['New Preview', 'Content Preview']} />
-                <ContentPreviewFormPage
-                  isNew
-                  initialValue={initialValue}
-                  registerSaveAction={this.props.registerSaveAction}
-                  setDirty={this.props.setDirty}
-                />
-              </React.Fragment>
-            );
-          }}
-        </ContentTypesFetcher>
-      </AdminOnly>
-    );
-  }
+  return (
+    <AdminOnly>
+      <DocumentTitle title={['New Preview', 'Content Preview']} />
+      <ContentPreviewFormPage
+        isNew
+        initialValue={initialValue}
+        registerSaveAction={props.registerSaveAction}
+        setDirty={props.setDirty}
+      />
+    </AdminOnly>
+  );
 }
+
+ContentPreviewNewRoute.propTypes = {
+  registerSaveAction: PropTypes.func.isRequired,
+  setDirty: PropTypes.func.isRequired,
+};
