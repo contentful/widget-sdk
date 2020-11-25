af14d444db80a12c34825b034cb93dbae496f3fc
Tanya Bessonova <tatiana.bessonova@contentful.com>
2020-11-25 10:47:53 +0100

    test(content type editor): add contract tests for content type editor (#7160)
    * test(content type editor): add contract tests for content type editor
    
    * test(content type editor): run prettier

---
+++ test/cypress/fixtures/requests/content-types.ts
@@ -0,0 +1,50 @@
+import { Matchers } from '@pact-foundation/pact-web';
+import { cloneDeep } from 'lodash';
+const severalContentTypesBody = require('../responses/content-types-several.json');
+const editorInterfaceWithoutSidebarResponseBody = require('../responses/editor-interface-without-sidebar.json');
+
+const defaultContentType = severalContentTypesBody.items[0];
+
+export const createReguestWithNewField = ({ name, apiName, type, linkType }) => {
+  const newField = {
+    name,
+    apiName,
+    type,
+    linkType,
+    id: Matchers.string(),
+  };
+  const contentType = cloneDeep(defaultContentType);
+
+  contentType.fields.push(newField);
+  return contentType;
+};
+
+export const createResponseWithNewField = ({ name, apiName, type, linkType }) => {
+  const newField = {
+    name,
+    apiName,
+    type,
+    linkType,
+    id: 'pGKCEEBPbMDHwO69',
+    localized: false,
+    required: false,
+    validations: [],
+    disabled: false,
+    omitted: false,
+  };
+  const contentType = cloneDeep(defaultContentType);
+
+  contentType.fields.push(newField);
+  return contentType;
+};
+
+export const createEditorInterfaceRequestWithNewField = ({ apiName, widgetId }) => {
+  const request = cloneDeep(editorInterfaceWithoutSidebarResponseBody);
+  const control = {
+    fieldId: apiName,
+    widgetId,
+    widgetNamespace: 'builtin',
+  };
+  request.controls.push(control);
+  return request;
+};
