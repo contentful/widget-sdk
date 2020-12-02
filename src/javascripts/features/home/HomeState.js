1496495994a83083a3728492ed8146e7f43a4bca
Joshua Smock <jo-sm@users.noreply.github.com>
2020-12-02 15:34:36 +0100

    pr: Add comment to home route declaration

--- src/javascripts/features/home/HomeState.js
+++ src/javascripts/features/home/HomeState.js
@@ -1,6 +1,7 @@
 import EmptyNavigationBar from 'navigation/EmptyNavigationBar';
 import { EmptyHome } from './EmptyHome';
 
+// This routing declaration refers to the "root" home, rather than the space home.
 export const homeState = {
   name: 'home',
   url: '/',
