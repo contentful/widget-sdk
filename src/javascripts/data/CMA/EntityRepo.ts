7a18f399dd058dd1c0e1154af718ed1f65c3c471
Kado Damball <kadodamball@hotmail.com>
2020-12-10 15:16:04 +0100

    fix(UnpublishEvent): Stop ignoring deleted Entity events because we end up ignoring unpublish

--- src/javascripts/data/CMA/EntityRepo.ts
+++ src/javascripts/data/CMA/EntityRepo.ts
@@ -86,8 +86,9 @@ export function create(
   function onContentEntityChanged(entitySys, callback) {
     const handler = (msg: ContentEntityUpdatedPayload) => {
       const envId = spaceEndpoint.envId || 'master';
+      const isSameEntityType = msg.entityType === entitySys.type || msg.entityType === `Deleted${entitySys.type}`; // deleted entities return entityType=DeletedEntry
       if (
-        msg.entityType === entitySys.type &&
+        isSameEntityType &&
         msg.entityId === entitySys.id &&
         msg.environmentId === envId
       ) {
