e90a7f0a78ebe9dee4855db0089f40865c559297
Tanya Bessonova <tatiana.bessonova@contentful.com>
2020-12-21 17:13:48 +0100

    fix(content-model-editor): fix for ZEND-417 (#7314)
    exclude disabled nodes from field validations

--- src/javascripts/features/content-model-editor/field-dialog/utils/helpers.js
+++ src/javascripts/features/content-model-editor/field-dialog/utils/helpers.js
@@ -14,7 +14,7 @@ export const rangeTypes = {
 export const extractRichTextNodesValidations = (fields) => {
   const nodes = {};
   Object.values(fields).forEach((field) => {
-    if (areSettingsEmpty(field.settings)) return;
+    if (areSettingsEmpty(field.settings) || !field.enabled) return;
     const validation = {};
     validation[field.type] = field.settings;
     validation.message = field.message;
