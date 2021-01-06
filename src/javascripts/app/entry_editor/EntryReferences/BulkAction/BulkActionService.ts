9609a8bfae8f20bd97d73a88cef3be43707d77aa
Luiz Fonseca <eu@luiz.cc>
2021-01-06 16:09:11 +0100

    fix: increase timeout for BulkAction refresh

--- src/javascripts/app/entry_editor/EntryReferences/BulkAction/BulkActionService.ts
+++ src/javascripts/app/entry_editor/EntryReferences/BulkAction/BulkActionService.ts
@@ -1,7 +1,7 @@
 import type { PublishableEntity, Link } from '@contentful/types';
 import { executeBulkAction, getBulkAction } from '../referencesService';
 
-const BULKACTION_REFRESH_LIMIT = 15; // number of times we want to perform a refresh
+const BULKACTION_REFRESH_LIMIT = 60; // number of times we want to perform a refresh
 const BULKACTION_REFRESH_INTERVAL = 1000; // in MS
 
 type VersionedLink = {
