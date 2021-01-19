8d24cb04cbc183b4f8c490dd4e8d2f3b2122bc51
Luiz Fonseca <eu@luiz.cc>
2021-01-19 16:59:07 +0100

    fix: improve references.spec test readability

--- test/cypress/integration/contract/entries/references.spec.ts
+++ test/cypress/integration/contract/entries/references.spec.ts
@@ -11,6 +11,7 @@ import {
   queryLinksToDefaultEntry,
   getFirst7SnapshotsOfDefaultEntry,
   getEntryReferences,
+  queryForDefaultEntries,
 } from '../../../interactions/entries';
 import { defaultEntryId, defaultSpaceId } from '../../../util/requests';
 import { FeatureFlag } from '../../../util/featureFlag';
@@ -19,34 +20,62 @@ import {
   queryForBasicAppsInDefaultSpace,
   queryForScheduledPublishingOnEntryPage,
   queryForContentTagsInDefaultSpace,
+  queryForReleasesInDefaultSpace,
+  getLaunchAppFeatureInDefaultSpace,
 } from '../../../interactions/product_catalog_features';
 import { getBulkAction, publishBulkAction } from '../../../interactions/bulk_actions';
 
+function setupTestServers(): void {
+  cy.resetAllFakeServers();
+  cy.startFakeServers({
+    consumer: 'user_interface',
+    providers: ['jobs', 'entries', 'users', 'product_catalog_features', 'releases', 'bulk-actions'],
+    cors: true,
+    dir: Cypress.env('pactDir'),
+    spec: 2,
+  });
+
+  cy.verifyAndResetAllFakeServers;
+  cy.server();
+  cy.route('**/channel/**', []).as('shareJS');
+}
+
 describe('Entry references', () => {
-  let interactions: string[];
+  const setupInteractions = () => {
+    defaultRequestsMock({
+      publicContentTypesResponse: getAllPublicContentTypesInDefaultSpace.willReturnOne,
+    });
+    queryFirst100UsersInDefaultSpace.willFindSeveral();
+    getDefaultEntry.willReturnIt();
+    queryLinksToDefaultEntry.willReturnNone();
+    getFirst7SnapshotsOfDefaultEntry.willReturnNone();
+    getEditorInterfaceForDefaultContentType.willReturnOneWithoutSidebar();
+    queryForTasksInDefaultSpace.willFindFeatureEnabled();
+    queryForBasicAppsInDefaultSpace.willFindFeatureEnabled();
+    queryForScheduledPublishingOnEntryPage.willFindFeatureEnabled();
+    queryForContentTagsInDefaultSpace.willFindFeatureEnabled();
+    queryForReleasesInDefaultSpace.willFindFeatureEnabled();
+    getLaunchAppFeatureInDefaultSpace.willFindFeatureEnabled();
+  };
 
   beforeEach(() => {
+    setupTestServers();
+    setupInteractions();
     cy.enableFeatureFlags([FeatureFlag.RELEASES]);
-    interactions = basicServerSetUp();
   });
 
   describe('Validation', () => {
     beforeEach(() => {
       cy.visit(`/spaces/${defaultSpaceId}/entries/${defaultEntryId}`);
-      cy.wait(interactions, { timeout: 20000 });
-      cy.resetAllFakeServers();
     });
 
     it('validates release without errors', () => {
-      const getEntryReferencesInteraction = getEntryReferences.willReturnSeveral();
-      const validateEntryTreeInteraction = validateEntryReferencesResponse.willReturnNoErrors();
+      getEntryReferences.willReturnSeveral();
+      validateEntryReferencesResponse.willReturnNoErrors();
 
       cy.findByTestId('test-id-editor-builtin-reference-tree').click();
       cy.findByTestId('selectAllReferences').check();
-      cy.wait(getEntryReferencesInteraction);
-
       cy.findByTestId('validateReferencesBtn').click();
-      cy.wait(validateEntryTreeInteraction);
 
       cy.findByTestId('cf-ui-notification')
         .click({ timeout: 5000 }) // official cypress workaround for animation
@@ -55,15 +84,12 @@ describe('Entry references', () => {
     });
 
     it('validates release with errors', () => {
-      const getEntryReferencesInteraction = getEntryReferences.willReturnSeveral();
-      const validateEntryTreeInteraction = validateEntryReferencesResponse.willReturnErrors();
+      getEntryReferences.willReturnSeveral();
+      validateEntryReferencesResponse.willReturnErrors();
 
       cy.findByTestId('test-id-editor-builtin-reference-tree').click();
       cy.findByTestId('selectAllReferences').check();
-      cy.wait(getEntryReferencesInteraction);
-
       cy.findByTestId('validateReferencesBtn').click();
-      cy.wait(validateEntryTreeInteraction);
 
       cy.findByTestId('cf-ui-notification')
         .click({ timeout: 5000 })
@@ -75,21 +101,15 @@ describe('Entry references', () => {
   describe('Publication', () => {
     beforeEach(() => {
       cy.visit(`/spaces/${defaultSpaceId}/entries/${defaultEntryId}`);
-      cy.wait(interactions, { timeout: 20000 });
-      cy.resetAllFakeServers();
     });
 
     it('publishes release successfully', () => {
-      const getEntryReferencesInteraction = getEntryReferences.willReturnSeveral();
-      const publishEntryTreeInteraction = publishEntryReferencesResponse.willReturnNoErrors();
+      getEntryReferences.willReturnSeveral();
+      publishEntryReferencesResponse.willReturnNoErrors();
 
       cy.findByTestId('test-id-editor-builtin-reference-tree').click();
       cy.findByTestId('selectAllReferences').check();
-      cy.wait(getEntryReferencesInteraction);
-
       cy.findByTestId('publishReferencesBtn').click();
-      cy.wait(publishEntryTreeInteraction);
-      cy.wait(getEntryReferencesInteraction);
 
       cy.findByTestId('cf-ui-notification')
         .click({ timeout: 5000 })
@@ -98,15 +118,12 @@ describe('Entry references', () => {
     });
 
     it('publishes release returns validation errors', () => {
-      const getEntryReferencesInteraction = getEntryReferences.willReturnSeveral();
-      const publishEntryTreeInteraction = publishEntryReferencesResponse.willReturnErrors();
+      getEntryReferences.willReturnSeveral();
+      publishEntryReferencesResponse.willReturnErrors();
 
       cy.findByTestId('test-id-editor-builtin-reference-tree').click();
       cy.findByTestId('selectAllReferences').check();
-      cy.wait(getEntryReferencesInteraction);
-
       cy.findByTestId('publishReferencesBtn').click();
-      cy.wait(publishEntryTreeInteraction);
 
       cy.findByTestId('cf-ui-notification')
         .click({ timeout: 5000 })
@@ -115,15 +132,12 @@ describe('Entry references', () => {
     });
 
     it('publishes release fails', () => {
-      const getEntryReferencesInteraction = getEntryReferences.willReturnSeveral();
-      const publishEntryTreeInteraction = publishEntryReferencesResponse.willFail();
+      getEntryReferences.willReturnSeveral();
+      publishEntryReferencesResponse.willFail();
 
       cy.findByTestId('test-id-editor-builtin-reference-tree').click();
       cy.findByTestId('selectAllReferences').check();
-      cy.wait(getEntryReferencesInteraction);
-
       cy.findByTestId('publishReferencesBtn').click();
-      cy.wait(publishEntryTreeInteraction);
 
       cy.findByTestId('cf-ui-notification')
         .click({ timeout: 5000 })
@@ -132,16 +146,13 @@ describe('Entry references', () => {
     });
 
     it.skip('publishes release skipping unresolved entities', () => {
-      const getEntryReferencesInteraction = getEntryReferences.willReturnSeveralWithUnresolved();
-      const publishEntryTreeInteraction = publishEntryReferencesResponse.willReturnNoErrors();
+      getEntryReferences.willReturnSeveralWithUnresolved();
+      publishEntryReferencesResponse.willReturnNoErrors();
 
       cy.findByTestId('test-id-editor-builtin-reference-tree').click();
       cy.findByTestId('selectAllReferences').check();
-      cy.wait(getEntryReferencesInteraction);
 
       cy.findByTestId('publishReferencesBtn').click();
-      cy.wait(publishEntryTreeInteraction);
-      cy.wait(getEntryReferencesInteraction);
 
       cy.findByTestId('cf-ui-notification')
         .click({ timeout: 5000 })
@@ -157,24 +168,18 @@ describe('Entry references', () => {
         FeatureFlag.REFERENCE_TREE_BULK_ACTIONS_SUPPORT,
       ]);
       cy.visit(`/spaces/${defaultSpaceId}/entries/${defaultEntryId}`);
-      cy.wait(interactions, { timeout: 20000 });
-      cy.resetAllFakeServers();
     });
 
     it('should publish Entities successfully', () => {
-      const getEntryReferencesInteraction = getEntryReferences.willReturnSeveralWithVersion();
-
-      const publishEntryTreeInteraction = publishBulkAction.willSucceed();
-      const getBulkActionRequest = getBulkAction.willReturnStatusSucceeded();
+      getEntryReferences.willReturnSeveralWithVersion();
+      queryForDefaultEntries.willFindMultiple();
+      publishBulkAction.willSucceed();
+      getBulkAction.willReturnStatusSucceeded();
 
       cy.findByTestId('test-id-editor-builtin-reference-tree').click();
       cy.findByTestId('selectAllReferences').check();
-      cy.wait(getEntryReferencesInteraction);
 
       cy.findByTestId('publishReferencesBtn').click();
-      cy.wait(publishEntryTreeInteraction);
-      cy.wait(getBulkActionRequest);
-
       cy.findByTestId('cf-ui-notification')
         .click({ timeout: 5000 })
         .should('be.visible')
@@ -182,19 +187,15 @@ describe('Entry references', () => {
     });
 
     it('should display an error message when a BulkAction fails', () => {
-      const getEntryReferencesInteraction = getEntryReferences.willReturnSeveralWithVersion();
-
-      const publishEntryTreeInteraction = publishBulkAction.willSucceed();
-      const getBulkActionRequest = getBulkAction.willReturnStatusFailed();
+      getEntryReferences.willReturnSeveralWithVersion();
+      queryForDefaultEntries.willFindMultiple();
+      publishBulkAction.willSucceed();
+      getBulkAction.willReturnStatusFailed();
 
       cy.findByTestId('test-id-editor-builtin-reference-tree').click();
       cy.findByTestId('selectAllReferences').check();
-      cy.wait(getEntryReferencesInteraction);
 
       cy.findByTestId('publishReferencesBtn').click();
-      cy.wait(publishEntryTreeInteraction);
-      cy.wait(getBulkActionRequest);
-
       cy.findByTestId('cf-ui-notification')
         .click({ timeout: 5000 })
         .should('be.visible')
@@ -202,33 +203,3 @@ describe('Entry references', () => {
     });
   });
 });
-
-function basicServerSetUp(): string[] {
-  cy.resetAllFakeServers();
-  // TODO: move this to a before block
-  cy.startFakeServers({
-    consumer: 'user_interface',
-    providers: ['jobs', 'entries', 'users', 'product_catalog_features', 'releases', 'bulk-actions'],
-    cors: true,
-    dir: Cypress.env('pactDir'),
-    spec: 2,
-  });
-
-  cy.server();
-  cy.route('**/channel/**', []).as('shareJS');
-
-  return [
-    ...defaultRequestsMock({
-      publicContentTypesResponse: getAllPublicContentTypesInDefaultSpace.willReturnOne,
-    }),
-    queryFirst100UsersInDefaultSpace.willFindSeveral(),
-    getDefaultEntry.willReturnIt(),
-    queryLinksToDefaultEntry.willReturnNone(),
-    getFirst7SnapshotsOfDefaultEntry.willReturnNone(),
-    getEditorInterfaceForDefaultContentType.willReturnOneWithoutSidebar(),
-    queryForTasksInDefaultSpace.willFindFeatureEnabled(),
-    queryForBasicAppsInDefaultSpace.willFindFeatureEnabled(),
-    queryForScheduledPublishingOnEntryPage.willFindFeatureEnabled(),
-    queryForContentTagsInDefaultSpace.willFindFeatureEnabled(),
-  ];
-}
