d5d66c7ff0fa0282d24cc6c8d45f6200a2e7f432
Gosia Szporer <15679449+gosiaszporer@users.noreply.github.com>
2021-02-02 17:24:44 +0100

    feat:Add styling

--- src/javascripts/features/organization-subscription/components/ContentfulApps.js
+++ src/javascripts/features/organization-subscription/components/ContentfulApps.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import _ from 'lodash';
 import {
   Heading,
@@ -9,21 +9,21 @@ import {
   DropdownList,
   DropdownListItem,
   ModalLauncher,
+  Flex,
 } from '@contentful/forma-36-react-components';
 import { DeleteAppsModal } from './DeleteAppsModal';
-import { css } from 'emotion';
-
-const styles = {
-  dropdown: css({
-    // position: 'absolute',
-    // right: tokens.spacingM,
-  }),
-};
 
 const openDeleteAppsModal = async () => {
   const result = await ModalLauncher.open(({ isShown, onClose }) => {
     return (
-      <DeleteAppsModal onConfirm={onClose} onCancel={() => onClose(false)} isShown={isShown} />
+      <DeleteAppsModal
+        onConfirm={() => {
+          console.log('remove everything');
+          onClose(false);
+        }}
+        onCancel={() => onClose(false)}
+        isShown={isShown}
+      />
     );
   });
   if (result === false) {
@@ -32,40 +32,39 @@ const openDeleteAppsModal = async () => {
 };
 
 export function ContentfulApps() {
-  const [isOpen, setOpen] = React.useState(false);
-  const handleAction = (action) => {
+  const [isOpen, setOpen] = useState(false);
+  const openModal = () => {
     setOpen(false);
-    action();
+    openDeleteAppsModal();
   };
 
   return (
     <Card>
-      <Heading>Compose + Launch</Heading>
-      <Dropdown
-        className={styles.dropdown}
-        isOpen={isOpen}
-        onClose={() => setOpen(false)}
-        toggleElement={
-          <IconButton
-            testId="subscription-page.delete-apps"
-            onClick={() => {
-              setOpen(true);
-            }}
-            label="Actions"
-            iconProps={{
-              icon: 'MoreHorizontal',
-            }}
-            buttonType="muted"
-          />
-        }>
-        <DropdownList>
-          <DropdownListItem
-            testId="userlist.row.actions.reinvite"
-            onClick={() => handleAction(openDeleteAppsModal())}>
-            Remove apps from organization
-          </DropdownListItem>
-        </DropdownList>
-      </Dropdown>
+      <Flex justifyContent="space-between" marginBottom="spacingM">
+        <Heading>Compose + Launch</Heading>
+        <Dropdown
+          isOpen={isOpen}
+          onClose={() => setOpen(false)}
+          toggleElement={
+            <IconButton
+              testId="subscription-page.delete-apps"
+              onClick={() => {
+                setOpen(true);
+              }}
+              label="Actions"
+              iconProps={{
+                icon: 'MoreHorizontal',
+              }}
+              buttonType="muted"
+            />
+          }>
+          <DropdownList>
+            <DropdownListItem testId="userlist.row.actions.reinvite" onClick={openModal}>
+              Remove apps from organization
+            </DropdownListItem>
+          </DropdownList>
+        </Dropdown>
+      </Flex>
       <Paragraph>Install the apps on any Space Home.</Paragraph>
     </Card>
   );
--- src/javascripts/features/organization-subscription/components/DeleteAppsModal.js
+++ src/javascripts/features/organization-subscription/components/DeleteAppsModal.js
@@ -1,90 +1,83 @@
-import React, { useState, action } from 'react';
+import React, { useState } from 'react';
 import _ from 'lodash';
 import {
   Paragraph,
   TextInput,
   Typography,
-  Button,
   ModalConfirm,
-  ModalConfirmProps,
   FieldGroup,
   CheckboxField,
 } from '@contentful/forma-36-react-components';
 
-export function DeleteAppsModal(props: ModalConfirmProps) {
-  const [isShown, setShown] = useState(true);
-  const [isLoading, setLoading] = useState(false);
+export function DeleteAppsModal(props) {
   const [repeat, setRepeat] = useState('');
+  const [optionOne, setOptionOne] = useState(false);
+  const [optionTwo, setOptionTwo] = useState(false);
+  const [optionThree, setOptionThree] = useState(false);
+  const [optionFour, setOptionFour] = useState(false);
+  const [optionFive, setOptionFive] = useState(false);
+
+  const disableConfirm = !optionOne && !optionTwo && !optionThree && !optionFour && !optionFive;
 
   return (
-    <div>
-      <Button buttonType="negative" onClick={() => setShown(true)}>
-        Delete something
-      </Button>
-      <ModalConfirm
-        isShown={isShown}
-        intent="negative"
-        isConfirmDisabled={repeat !== 'unlock'}
-        isConfirmLoading={isLoading}
-        title="Remove apps from organization"
-        confirmLabel="Remove apps from organization"
-        onCancel={() => {
-          setShown(false);
-          action('onCancel')();
-        }}
-        onConfirm={() => {
-          setLoading(true);
-          setTimeout(() => {
-            setLoading(false);
-            setShown(false);
-            setRepeat('');
-          }, 1500);
-          action('onConfirm')();
-        }}
-        {...props}>
-        <Typography>
-          <Paragraph>You are about to remove Compose + Launch from your organization</Paragraph>
-          <Paragraph>
-            Removing apps will remove Compose + Launch from all spaces in your organization. You
-            will lose access to the platforms, none of your content will be lost.
-          </Paragraph>
-          <Paragraph>
-            <strong>Why are you removing the apps?</strong>
-          </Paragraph>
-          <FieldGroup>
-            <CheckboxField
-              id="Checkbox1"
-              value="Does not do what I expected"
-              labelText="Does not do what I expected"
-            />
-            <CheckboxField
-              id="Checkbox2"
-              value="Not needed anymore"
-              labelText="Not needed anymore"
-            />
-            <CheckboxField
-              id="Checkbox2"
-              value="Apps are not performing well"
-              labelText="Apps are not performing well"
-            />
-            <CheckboxField
-              id="Checkbox2"
-              value="I have created my own solution"
-              labelText="I have created my own solution"
-            />
-            <CheckboxField
-              id="Checkbox2"
-              value="I was just testing it out"
-              labelText="I was just testing it out"
-            />
-          </FieldGroup>
-          <br />
-          <Paragraph>
-            <strong>What can we improve about the apps?</strong>
-          </Paragraph>
-        </Typography>
-        <TextInput value={repeat} onChange={(e) => setRepeat(e.target.value)} />
-      </ModalConfirm>
-    </div>
+    <ModalConfirm
+      intent="negative"
+      isConfirmDisabled={disableConfirm}
+      title="Remove apps from organization"
+      confirmLabel="Remove apps from organization"
+      {...props}>
+      <Typography>
+        <Paragraph>You are about to remove Compose + Launch from your organization</Paragraph>
+        <Paragraph>
+          Removing apps will remove Compose + Launch from all spaces in your organization. You will
+          lose access to the platforms, none of your content will be lost.
+        </Paragraph>
+        <Paragraph>
+          <strong>Why are you removing the apps?</strong>
+        </Paragraph>
+        <FieldGroup>
+          <CheckboxField
+            id="reason1"
+            value="Does not do what I expected"
+            labelText="Does not do what I expected"
+            checked={optionOne}
+            onChange={(e) => setOptionOne(e.target.checked)}
+          />
+          <CheckboxField
+            id="reason2"
+            value="Not needed anymore"
+            labelText="Not needed anymore"
+            checked={optionTwo}
+            onChange={(e) => setOptionTwo(e.target.checked)}
+          />
+          <CheckboxField
+            id="reason3"
+            value="Apps are not performing well"
+            labelText="Apps are not performing well"
+            checked={optionThree}
+            onChange={(e) => setOptionThree(e.target.checked)}
+          />
+          <CheckboxField
+            id="reason4"
+            value="I have created my own solution"
+            labelText="I have created my own solution"
+            checked={optionFour}
+            onChange={(e) => setOptionFour(e.target.checked)}
+          />
+          <CheckboxField
+            id="reason5"
+            value="I was just testing it out"
+            labelText="I was just testing it out"
+            checked={optionFive}
+            onChange={(e) => setOptionFive(e.target.checked)}
+          />
+        </FieldGroup>
+        <br />
+        <Paragraph>
+          <strong>What can we improve about the apps?</strong>
+        </Paragraph>
+      </Typography>
+      <TextInput value={repeat} onChange={(e) => setRepeat(e.target.value)} />
+    </ModalConfirm>
   );
 }
