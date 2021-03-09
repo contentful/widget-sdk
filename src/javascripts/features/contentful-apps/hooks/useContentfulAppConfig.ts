796db28e78d900c42ea129342f5ea917d3ff3ce3
Aris Plakias <aris.plakias@gmail.com>
2021-03-09 15:41:39 +0200

    feat(apps-trial): show buy CTA during trials

--- src/javascripts/features/contentful-apps/hooks/useContentfulAppConfig.ts
+++ src/javascripts/features/contentful-apps/hooks/useContentfulAppConfig.ts
@@ -2,7 +2,8 @@ import { useState, useEffect } from 'react';
 import { getOrgFeature, FEATURES } from 'data/CMA/ProductCatalog';
 import { getVariation, FLAGS } from 'LaunchDarkly';
 import { getAppsRepo } from 'features/apps-core';
-import { canStartAppTrial } from 'features/trials';
+import { createOrganizationEndpoint } from 'data/EndpointFactory';
+import { canStartAppTrial, createAppTrialRepo, isActiveAppTrial } from 'features/trials';
 
 /**
  * Add new app config here and use it like
@@ -36,7 +37,7 @@ export const fetchContentfulAppsConfig = async ({
   spaceId,
   environmentId,
 }: UseContentfulAppsConfig): Promise<AppState> => {
-  const [isPurchased, isEnabled, isInstalled, isTrialAvailable] = await Promise.all(
+  const [isPurchased, isEnabled, isInstalled, isTrialAvailable, isTrialActive] = await Promise.all(
     [
       getOrgFeature(organizationId, APPS_CONFIG[appId].catalogFlag, false),
       getVariation(APPS_CONFIG[appId].featureFlag, {
@@ -45,7 +46,11 @@ export const fetchContentfulAppsConfig = async ({
         environmentId,
       } as any),
       checkInstalledApp(appId),
+      // FIXME: Drop this once Trial info moves away from product_catalog_features
       canStartAppTrial(organizationId as string),
+      createAppTrialRepo(createOrganizationEndpoint(organizationId)).getTrial(
+        APPS_CONFIG[appId].catalogFlag
+      ),
     ].map(fallBackClose)
   );
 
@@ -54,13 +59,16 @@ export const fetchContentfulAppsConfig = async ({
     isEnabled,
     isInstalled,
     isTrialAvailable,
+    isTrialActive: isActiveAppTrial(isTrialActive),
   };
 };
+
 export interface AppState {
   isInstalled?: boolean;
   isEnabled?: boolean;
   isPurchased?: boolean;
   isTrialAvailable?: boolean;
+  isTrialActive?: boolean;
 }
 
 export interface UseContentfulAppsConfig {
