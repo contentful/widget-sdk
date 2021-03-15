280ca54db94f08704f9855ec7316fd15afb38c3a
Joshua Smock <jo-sm@users.noreply.github.com>
2021-03-15 17:01:18 +0100

    improvement: Separate config creation into own plugin file

--- test/smoke/plugins/index.ts
+++ test/smoke/plugins/index.ts
@@ -1,21 +1,10 @@
-/**
- * Although some of this could be put into the configuration (like env.email), I chose to put
- * this all here so that there's one single place to find it.
- */
-export default function plugins(_, config: Cypress.PluginConfigOptions) {
-  const { SMOKE_TEST_USER_EMAIL, SMOKE_TEST_USER_PASSWORD, SMOKE_TEST_DOMAIN } = process.env;
+import { setupConfig } from './setupConfig';
 
-  if (!SMOKE_TEST_USER_PASSWORD) {
-    throw new Error('Environment variable SMOKE_TEST_USER_PASSWORD expected but not given');
-  }
-
-  const email = SMOKE_TEST_USER_EMAIL ? SMOKE_TEST_USER_EMAIL : 'test@contentful.com';
-  const domain = SMOKE_TEST_DOMAIN ? SMOKE_TEST_DOMAIN : 'contentful.com';
-
-  config.baseUrl = `https://app.${SMOKE_TEST_DOMAIN}`;
-  config.env.email = email;
-  config.env.password = SMOKE_TEST_USER_PASSWORD;
-  config.env.domain = domain;
+export default function setupPlugins(
+  on: Cypress.PluginEvents,
+  initialConfig: Cypress.PluginConfigOptions
+) {
+  const config = setupConfig(on, initialConfig);
 
   return config;
 }
---
+++ test/smoke/plugins/setupConfig.ts
@@ -0,0 +1,21 @@
+/**
+ * Although some of this could be put into the configuration (like env.email), I chose to put
+ * this all here so that there's one single place to find it.
+ */
+export function setupConfig(_, config: Cypress.PluginConfigOptions) {
+  const { SMOKE_TEST_USER_EMAIL, SMOKE_TEST_USER_PASSWORD, SMOKE_TEST_DOMAIN } = process.env;
+
+  if (!SMOKE_TEST_USER_PASSWORD) {
+    throw new Error('Environment variable SMOKE_TEST_USER_PASSWORD expected but not given');
+  }
+
+  const email = SMOKE_TEST_USER_EMAIL ? SMOKE_TEST_USER_EMAIL : 'test@contentful.com';
+  const domain = SMOKE_TEST_DOMAIN ? SMOKE_TEST_DOMAIN : 'contentful.com';
+
+  config.baseUrl = `https://app.${SMOKE_TEST_DOMAIN}`;
+  config.env.email = email;
+  config.env.password = SMOKE_TEST_USER_PASSWORD;
+  config.env.domain = domain;
+
+  return config;
+}
