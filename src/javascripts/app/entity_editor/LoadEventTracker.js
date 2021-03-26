contentful/widget-sdk

commit ea663211dcf912a98e26da505d0daf3a2bde66f7

chore: Cleanup entity field migration leftovers [REACT-399] (#7701)

src/javascripts/app/entity_editor/LoadEventTracker.js
@@ -133,7 +133,7 @@ export function createWidgetLinkRenderEventsHandler({
   widget,
   locale,
   loadEvents,
-  editorData,
+  getValue,
   trackLinksRendered,
 }) {
   return function handleWidgetLinkRenderEvents() {
@@ -159,7 +159,7 @@ export function createWidgetLinkRenderEventsHandler({
       widget,
       locale,
       loadEvents,
-      editorData,
+      getValue,
       trackLinksRendered,
       getLinkCountForField,
     });
@@ -170,14 +170,12 @@ function handleField({
   widget,
   locale,
   loadEvents,
-  editorData,
+  getValue,
   trackLinksRendered,
   getLinkCountForField,
 }) {
-  // TODO: We shouldn't have to deal with `editorData` but a simple entity in here.
   const fieldId = widget.fieldId;
-  const internalFieldId = widget.field.id;
-  const localeFieldOrNull = getLocaleFieldOrNull(editorData, internalFieldId, locale);
+  const localeFieldOrNull = getValue() || null;
   const linkCount = localeFieldOrNull ? getLinkCountForField(localeFieldOrNull) : 0;
   if (linkCount === 0) {
     trackLinksRendered();
@@ -236,13 +234,4 @@ function isMultiReferenceField(field) {
 
 function isLinkField(field) {
   return isRichTextField(field) || isSingleReferenceField(field) || isMultiReferenceField(field);
-}
-
-function getLocaleFieldOrNull(editorData, fieldId, locale) {
-  const { fields } = editorData.entity.data;
-  const field = fields[fieldId];
-  if (!field) {
-    return null;
-  }
-  return field[locale.internal_code] || null;
 }

src/javascripts/app/entity_editor/WidgetRenderer.js
@@ -1,14 +1,18 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import * as K from 'core/utils/kefir';
 import { noop, defer } from 'lodash';
 import WidgetRenderWarning from 'widgets/WidgetRenderWarning';
-import * as LoadEventTracker from 'app/entity_editor/LoadEventTracker';
-import { WidgetNamespace, isCustomWidget, WidgetLocation } from '@contentful/widget-renderer';
+import {
+  createLinksRenderedEvent,
+  createWidgetLinkRenderEventsHandler,
+} from 'app/entity_editor/LoadEventTracker';
+import {
+  WidgetNamespace,
+  isCustomWidget,
+  WidgetLocation,
+  WidgetRenderer as WidgetRendererExternal,
+} from '@contentful/widget-renderer';
 import { toRendererWidget } from 'widgets/WidgetCompat';
-import { WidgetRenderer as WidgetRendererExternal } from '@contentful/widget-renderer';
-
-const { createLinksRenderedEvent, createWidgetLinkRenderEventsHandler } = LoadEventTracker;
 
 function newNoopLoadEvents() {
   return {
@@ -16,10 +20,7 @@ function newNoopLoadEvents() {
   };
 }
 
-function WidgetRendererInternal(props) {
-  const { widget, locale, editorData, loadEvents } = props;
-  const { problem, renderFieldEditor } = widget;
-
+function getTrackingEvents({ loadEvents, widget, locale, widgetApi }) {
   let trackLinksRendered = noop;
   let handleWidgetLinkRenderEvents = noop;
 
@@ -29,99 +30,125 @@ function WidgetRendererInternal(props) {
       widget,
       locale,
       loadEvents,
-      editorData,
+      getValue: () => widgetApi.field.getValue(),
       trackLinksRendered,
     });
   }
+  return {
+    handleWidgetLinkRenderEvents,
+    trackLinksRendered,
+  };
+}
 
-  if (problem) {
-    trackLinksRendered();
-    return <WidgetRenderWarning message={problem} />;
-  } else if (isCustomWidget(widget.widgetNamespace)) {
-    trackLinksRendered();
+const useTrackedRenderingType = ({ locale, loadEvents, widgetApi, widget }) => {
+  const { problem: hasProblem, widgetNamespace } = widget;
+  const isCustom = isCustomWidget(widgetNamespace);
+  const isBuiltIn = widgetNamespace === WidgetNamespace.BUILTIN;
+
+  const { trackLinksRendered, handleWidgetLinkRenderEvents } = getTrackingEvents({
+    loadEvents,
+    widget,
+    locale,
+    widgetApi,
+  });
+
+  // The rendering needs only to be tracked once
+  React.useEffect(() => {
+    if (hasProblem || isCustom) {
+      trackLinksRendered();
+    } else if (isBuiltIn) {
+      handleWidgetLinkRenderEvents();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return { hasProblem, isCustom, isBuiltIn };
+};
+
+function WidgetRendererInternal(props) {
+  const { widget, entityType, locale, loadEvents, onFocus, onBlur, widgetApi } = props;
+
+  const { hasProblem, isBuiltIn, isCustom } = useTrackedRenderingType({
+    locale,
+    loadEvents,
+    widgetApi,
+    widget,
+  });
+
+  if (hasProblem) {
+    return <WidgetRenderWarning message={widget.problem} />;
+  }
+
+  if (isCustom) {
     return (
       <WidgetRendererExternal
         location={WidgetLocation.ENTRY_FIELD}
         widget={toRendererWidget(widget.descriptor)}
-        sdk={props.widgetApi}
-        onFocus={() => props.scope.fieldLocale.setActive(true)}
-        onBlur={() => props.scope.fieldLocale.setActive(false)}
+        sdk={widgetApi}
+        onFocus={onFocus}
+        onBlur={onBlur}
       />
     );
-  } else if (widget.widgetNamespace === WidgetNamespace.BUILTIN) {
-    const content = renderFieldEditor({
-      $scope: props.scope,
+  }
+
+  if (isBuiltIn) {
+    return widget.renderFieldEditor({
       loadEvents: loadEvents || newNoopLoadEvents(),
-      widgetApi: props.widgetApi,
-      entityType: editorData.entityInfo.type,
+      widgetApi,
+      entityType,
     });
-
-    handleWidgetLinkRenderEvents();
-
-    return content;
   }
 
   return null;
 }
 
 WidgetRendererInternal.propTypes = {
-  scope: PropTypes.object.isRequired,
   locale: PropTypes.object.isRequired,
   widget: PropTypes.object.isRequired,
-  editorData: PropTypes.object.isRequired,
-  loadEvents: PropTypes.object.isRequired,
+  loadEvents: PropTypes.object,
+  onBlur: PropTypes.func.isRequired,
+  onFocus: PropTypes.func.isRequired,
   widgetApi: PropTypes.object.isRequired,
+  entityType: PropTypes.string.isRequired,
 };
 
-export function WidgetRenderer(props) {
-  const { widget, locale, editorData, loadEvents } = props.scope;
-  const ref = React.createRef();
+// NOTE The fileEditor widget renderer needs to be memoized due to
+// some weird rerendering glitch that is related to the Angular/React mix
+const MemoizedWidgetRendererInternal = React.memo(WidgetRendererInternal, (_, nextProps) => {
+  const memoizedWidgetIds = ['fileEditor'];
+  const shouldNotRerender = memoizedWidgetIds.includes(nextProps.widget.widgetId);
+  return shouldNotRerender;
+});
+
+export function WidgetRenderer({ hasInitialFocus, isRtl, ...props }) {
+  const ref = React.useRef(null);
 
   React.useEffect(() => {
-    if (props.hasInitialFocus) {
-      K.onValueScope(props.scope, props.scope.otDoc.state.loaded$, (loaded) => {
-        if (loaded) {
-          const input = ref.current.querySelector('input');
-          if (input && typeof input.focus === 'function') {
-            defer(() => {
-              input.focus();
-            });
-          }
-        }
-      });
+    if (hasInitialFocus) {
+      const input = ref.current?.querySelector('input');
+      defer(() => input?.focus?.());
     }
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [hasInitialFocus, ref]);
 
   return (
     <div
       ref={ref}
-      className={props.isRtl ? 'x--dir-rtl' : ''}
-      onFocus={() => {
-        props.scope.$applyAsync(() => {
-          props.scope.fieldLocale.setActive(true);
-        });
-      }}
-      onBlur={() => {
-        props.scope.$applyAsync(() => {
-          props.scope.fieldLocale.setActive(false);
-          props.scope.fieldLocale.revalidate();
-        });
-      }}>
-      <WidgetRendererInternal
-        widget={widget}
-        locale={locale}
-        editorData={editorData}
-        loadEvents={loadEvents}
-        widgetApi={props.widgetApi}
-        scope={props.scope}
-      />
+      className={isRtl ? 'x--dir-rtl' : ''}
+      onFocus={props.onFocus}
+      onBlur={props.onBlur}>
+      <MemoizedWidgetRendererInternal {...props} />
     </div>
   );
 }
 
 WidgetRenderer.propTypes = {
-  hasInitialFocus: PropTypes.bool.isRequired,
   isRtl: PropTypes.bool.isRequired,
-  scope: PropTypes.object.isRequired,
+  hasInitialFocus: PropTypes.bool,
+  locale: PropTypes.object.isRequired,
+  widget: PropTypes.object.isRequired,
+  onFocus: PropTypes.func.isRequired,
+  onBlur: PropTypes.func.isRequired,
+  loadEvents: PropTypes.object,
   widgetApi: PropTypes.object.isRequired,
+  entityType: PropTypes.string.isRequired,
 };

src/javascripts/app/entity_editor/WidgetRenderer.spec.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render, waitFor } from '@testing-library/react';
-import { WidgetRenderer } from './WidgetRendererNew';
+import { WidgetRenderer } from './WidgetRenderer';
 import * as WidgetRendererExternal from '@contentful/widget-renderer';
-import * as LoadEventTracker from 'app/entity_editor/LoadEventTrackerNew';
+import * as LoadEventTracker from 'app/entity_editor/LoadEventTracker';
 
 jest.mock('widgets/WidgetRenderWarning', () => () => (
   <div data-test-id="widget-renderer-warning" />
@@ -12,7 +12,7 @@ jest.mock('widgets/WidgetCompat', () => ({
   toRendererWidget: jest.fn(),
 }));
 
-jest.mock('app/entity_editor/LoadEventTrackerNew');
+jest.mock('app/entity_editor/LoadEventTracker');
 const trackLinksRendered = jest.fn();
 LoadEventTracker.createLinksRenderedEvent = jest.fn().mockReturnValue(trackLinksRendered);
 const handleWidgetLinkRenderEvents = jest.fn();

src/javascripts/AngularInit.js
@@ -24,11 +24,8 @@ export const angularInitRun = [
 
     const modules = await Promise.all([
       import(/* webpackMode: "eager" */ 'app/ContentModel/Editor/contentTypeEditorController'),
-      import(/* webpackMode: "eager" */ 'app/entity_editor/EntityField/cfEntityFieldDirective'),
       import(/* webpackMode: "eager" */ 'services/exceptionHandler'),
       import(/* webpackMode: "eager" */ 'ui/Framework/ReactDirective'),
-      // TODO Remove temporary FieldLocaleController as soon as `cf-entity-field` is migrated
-      import(/* webpackMode: "eager" */ 'app/entity_editor/fieldLocaleControllerBridge'),
     ]);
     modules.forEach((module) => module.default());
 

src/javascripts/LaunchDarkly.js
@@ -36,7 +36,6 @@ export const FLAGS = {
   ADD_TO_RELEASE: 'feature-pulitzer-05-2020-add-to-release',
   SHAREJS_REMOVAL: 'feature-pen-04-2020-sharejs-removal-multi',
   NEW_FIELD_DIALOG: 'react-migration-new-content-type-field-dialog',
-  NEW_ENTITY_FIELD: 'react-migration-12-2020-new-entity-field',
   SSO_SETUP_NO_REDUX: 'feature-hejo-08-2020-sso-setup-no-redux',
   ENTITLEMENTS_API: 'feature-hejo-11-2020-entitlements-api',
   NEW_PURCHASE_FLOW: 'feature-ogg-08-2020-enable-space-purchase-flow',
@@ -72,7 +71,6 @@ const FALLBACK_VALUES = {
   [FLAGS.SHAREJS_REMOVAL]: { Asset: false, Entry: false },
   [FLAGS.PRICING_2020_WARNING]: true,
   [FLAGS.NEW_FIELD_DIALOG]: false,
-  [FLAGS.NEW_ENTITY_FIELD]: false,
   [FLAGS.SSO_SETUP_NO_REDUX]: false,
   [FLAGS.ENTITLEMENTS_API]: false,
   [FLAGS.NEW_PURCHASE_FLOW]: false,

src/javascripts/app/asset_editor/AssetEditorWorkbench.js
@@ -16,7 +16,6 @@ import StatusNotification from 'app/entity_editor/StatusNotification';
 import { ContentTagsTab } from 'app/entity_editor/ContentTagsTab';
 import { goToPreviousSlideOrExit } from 'navigation/SlideInNavigator';
 import EntrySidebar from 'app/EntrySidebar/EntrySidebar';
-import AngularComponent from 'ui/Framework/AngularComponent';
 import tokens from '@contentful/forma-36-tokens';
 import { useFieldLocaleListeners } from 'app/entry_editor/makeFieldLocaleListeners';
 import { filterWidgets } from 'app/entry_editor/formWidgetsController';
@@ -25,7 +24,6 @@ import { styles as editorStyles } from './../entry_editor/styles';
 import { useSpaceEnvContext } from 'core/services/SpaceEnvContext/useSpaceEnvContext';
 import { isUnscopedRoute } from 'core/services/SpaceEnvContext/utils';
 import { getModule } from 'core/NgRegistry';
-import { useMigratedEntityField } from 'app/entity_editor/EntityField/useEntityFieldFeatureFlag';
 import { EntityField } from 'app/entity_editor/EntityField/EntityField';
 
 const styles = {
@@ -98,8 +96,6 @@ const AssetEditorWorkbench = ({
     goToPreviousSlideOrExit('arrow_back');
   };
 
-  const migratedEntityFieldEnabled = useMigratedEntityField();
-
   return (
     <div className="asset-editor">
       <Workbench>
@@ -166,39 +162,20 @@ const AssetEditorWorkbench = ({
                 'entity-editor-form cf-workbench-content workbench-layer--is-current cf-workbench-content-type__text'
               }>
               <StatusNotification {...statusNotificationProps} />
-              {migratedEntityFieldEnabled ? (
-                widgets.map((widget, index) => (
-                  <EntityField
-                    editorContext={editorContext}
-                    editorData={editorData}
-                    fieldLocaleListeners={fieldLocaleListeners}
-                    fields={fields}
-                    index={index}
-                    key={widget.fieldId}
-                    localeData={localeData}
-                    doc={otDoc}
-                    preferences={preferences}
-                    widget={widget}
-                  />
-                ))
-              ) : (
-                <AngularComponent
-                  with$Apply
-                  template={
-                    '<cf-entity-field ng-repeat="widget in widgets track by widget.fieldId"></cf-entity-field>'
-                  }
-                  scope={{
-                    fieldLocaleListeners,
-                    widgets,
-                    localeData,
-                    editorContext,
-                    fields,
-                    entityInfo,
-                    otDoc,
-                    editorData,
-                  }}
+              {widgets.map((widget, index) => (
+                <EntityField
+                  editorContext={editorContext}
+                  editorData={editorData}
+                  fieldLocaleListeners={fieldLocaleListeners}
+                  fields={fields}
+                  index={index}
+                  key={widget.fieldId}
+                  localeData={localeData}
+                  doc={otDoc}
+                  preferences={preferences}
+                  widget={widget}
                 />
-              )}
+              ))}
             </div>
           </TabPanel>
           <TabPanel

src/javascripts/app/entity_editor/EntityField/EntityField.spec.tsx
@@ -15,7 +15,7 @@ jest.mock('app/widgets/ExtensionSDKs', () => ({
   createFieldWidgetSDK: jest.fn().mockReturnValue({}),
 }));
 
-jest.mock('app/entity_editor/WidgetRendererNew', () => ({
+jest.mock('app/entity_editor/WidgetRenderer', () => ({
   WidgetRenderer: jest.fn(({ onBlur, onFocus }) => (
     <div onBlur={onBlur} onFocus={onFocus} tabIndex={1} data-test-id="widget-renderer" />
   )),

src/javascripts/app/entity_editor/EntityField/EntityFieldControl.tsx
@@ -7,9 +7,34 @@ import { createFieldWidgetSDK } from 'app/widgets/ExtensionSDKs';
 import { getEntityLink } from 'app/common/EntityStateLink';
 import { useSpaceEnvContext } from 'core/services/SpaceEnvContext/useSpaceEnvContext';
 import { isCurrentEnvironmentMaster } from 'core/services/SpaceEnvContext/utils';
+import { LocaleData } from './types';
+import { Preferences } from 'app/widgets/ExtensionSDKs/createEditorApi';
 import { usePubSubClient } from 'core/hooks';
 
-export function EntityFieldControl(props: { scope: any; hasInitialFocus: boolean }) {
+type EntityFieldControlProps = {
+  hasInitialFocus: boolean;
+  widget: any;
+  locale: {
+    code: any;
+  };
+  editorData: {
+    entityInfo: {
+      contentType: any;
+      type: any;
+    };
+  };
+  doc: any;
+  fieldLocale: any;
+  loadEvents: Function;
+  localeData: LocaleData;
+  preferences: Preferences;
+  setInvalid: Function;
+  onBlur: (...args: any[]) => any;
+  onFocus: (...args: any[]) => any;
+  fieldLocaleListeners: any;
+};
+
+export function EntityFieldControl(props: EntityFieldControlProps) {
   const {
     currentSpace,
     currentSpaceId,
@@ -21,16 +46,25 @@ export function EntityFieldControl(props: { scope: any; hasInitialFocus: boolean
   const pubSubClient = usePubSubClient();
   const isMasterEnvironment = isCurrentEnvironmentMaster(currentSpace);
 
+  const {
+    hasInitialFocus,
+    widget,
+    locale,
+    editorData,
+    doc,
+    fieldLocale,
+    loadEvents,
+    localeData,
+    preferences,
+    setInvalid,
+    fieldLocaleListeners,
+    onBlur,
+    onFocus,
+  } = props;
+
+  const internalContentType = editorData.entityInfo.contentType;
+
   const widgetApi = React.useMemo(() => {
-    const {
-      widget,
-      locale,
-      editorData,
-      fieldController,
-      localeData,
-      preferences,
-      otDoc: doc,
-    } = props.scope;
     const { widgetNamespace, widgetId, fieldId, parameters } = widget;
 
     if (!currentSpaceId) {
@@ -38,17 +72,17 @@ export function EntityFieldControl(props: { scope: any; hasInitialFocus: boolean
     }
 
     return createFieldWidgetSDK({
-      fieldId,
       localeCode: locale.code,
-      widgetNamespace,
-      widgetId,
       editorData,
-      setInvalid: fieldController?.setInvalid,
+      setInvalid,
       localeData,
       preferences,
       doc,
-      internalContentType: props.scope.entityInfo.contentType,
-      fieldLocaleListeners: props.scope.fieldLocaleListeners,
+      internalContentType,
+      fieldLocaleListeners,
+      widgetNamespace,
+      widgetId,
+      fieldId,
       parameters,
       spaceId: currentSpaceId,
       environmentAliasId: currentEnvironmentAliasId,
@@ -59,13 +93,21 @@ export function EntityFieldControl(props: { scope: any; hasInitialFocus: boolean
       pubSubClient,
     });
   }, [
-    currentEnvironment,
+    widget,
+    locale.code,
+    editorData,
+    setInvalid,
+    localeData,
+    preferences,
+    doc,
+    internalContentType,
+    fieldLocaleListeners,
+    currentSpaceId,
     currentEnvironmentAliasId,
     currentEnvironmentId,
     currentSpace,
+    currentEnvironment,
     currentSpaceContentTypes,
-    currentSpaceId,
-    props.scope,
     pubSubClient,
   ]);
 
@@ -77,13 +119,18 @@ export function EntityFieldControl(props: { scope: any; hasInitialFocus: boolean
     <>
       <div className="entity-editor__control-group">
         <WidgetRenderer
-          scope={props.scope}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          hasInitialFocus={hasInitialFocus}
+          isRtl={isRtlLocale(locale.code)}
+          loadEvents={loadEvents}
+          entityType={editorData.entityInfo.type}
+          locale={locale}
+          widget={widget}
           widgetApi={widgetApi}
-          hasInitialFocus={props.hasInitialFocus}
-          isRtl={isRtlLocale(props.scope.locale.code)}
         />
         <Collaborators
-          users={props.scope.fieldLocale.collaborators}
+          users={fieldLocale.collaborators}
           className="entity-editor__field-collaborators"
         />
       </div>

commit ffad749d7eadb9d362107b36a92a50874a8e62ac

fix: Entry editor loading times tracking (#7827)
Tracking got broken in ea66321 (PR #7701), as basically
bootstrapEntryEditorLoadEvents wasn't called early enough.

This also improves the references/media editors link rendered reporting to use the
mechanism built into LoadEventTracker instead of keeping track of links in two
places, which seemed to be another cause for some events not being tracked or
tracked in time after .

Adds a couple of TODOs as some of the logic could be improved and the event
seems broken for the single locale mode.

src/javascripts/app/entity_editor/LoadEventTracker.js
@@ -70,6 +70,8 @@ export function createLoadEventTracker({
   };
 }
 
+// TODO: Consider a class instead of passing around `loadEvents` to ensure this method is called before
+//  the fields report, otherwise the info gets lost and the event is never tracked.
 export function bootstrapEntryEditorLoadEvents(otDoc, loadEvents, editorData, trackLoadEvent) {
   let loadLinksRendered = false;
   let loadShareJSConnected = false;
@@ -104,9 +106,10 @@ export function bootstrapEntryEditorLoadEvents(otDoc, loadEvents, editorData, tr
   }
 
   loadEvents.stream.onValue(({ actionName }) => {
-    if (actionName !== 'linksRendered') {
+    if (actionName !== 'allFieldLinksRendered') {
       return;
     }
+    // TODO: Instead of a counter, introduce a map with field locale IDs. A simple counter is more prone to bugs.
     fieldsInteractiveCount++;
     if (fieldsInteractiveCount === renderableLinkFieldInstanceCount) {
       trackLinksRenderedEvent();
@@ -117,6 +120,9 @@ export function bootstrapEntryEditorLoadEvents(otDoc, loadEvents, editorData, tr
 export function getRenderableLinkFieldInstanceCount(fieldTypes) {
   const localizedFieldCount = fieldTypes.filter((f) => f.localized).length;
   const nonLocalizedFieldCount = fieldTypes.length - localizedFieldCount;
+  // TODO: Make this work for single locale mode! TheLocaleStore.getActiveLocales() returns multiple locales
+  //  if the multi-locale mode was previously active with multiple locales. To support single locale mode we'll also
+  //  have to take into account that the default locale fields might not be visible at all.
   const activeLocaleCount = TheLocaleStore.getActiveLocales().length;
   return localizedFieldCount * activeLocaleCount + nonLocalizedFieldCount;
 }
@@ -124,7 +130,7 @@ export function getRenderableLinkFieldInstanceCount(fieldTypes) {
 export function createLinksRenderedEvent(loadEvents) {
   return once(() => {
     if (loadEvents) {
-      loadEvents.emit({ actionName: 'linksRendered' });
+      loadEvents.emit({ actionName: 'allFieldLinksRendered' });
     }
   });
 }
@@ -140,8 +146,6 @@ export function createWidgetLinkRenderEventsHandler({
     const { field } = widget;
 
     if (!isLinkField(field)) {
-      // TODO: why do track "links rendered" for a non-link field? it increases the counter of loaded fields, even though we didn't count it as a field that must load links
-      // trackLinksRendered();
       return;
     }
 
@@ -150,8 +154,6 @@ export function createWidgetLinkRenderEventsHandler({
       getLinkCountForField = getRichTextLinkCount;
     } else if (isSingleReferenceField(field) || isMultiReferenceField(field)) {
       getLinkCountForField = getLinkFieldLinkCount;
-      // TODO: Remove once we get rid of legacy Angular link field editors:
-      handleAngularReferenceFieldEditor(loadEvents, trackLinksRendered);
     } else {
       throw new Error('Unknown link field type');
     }
@@ -175,8 +177,8 @@ function handleField({
   getLinkCountForField,
 }) {
   const fieldId = widget.fieldId;
-  const localeFieldOrNull = getValue() || null;
-  const linkCount = localeFieldOrNull ? getLinkCountForField(localeFieldOrNull) : 0;
+  const localeFieldValueOrNull = getValue() || null;
+  const linkCount = localeFieldValueOrNull ? getLinkCountForField(localeFieldValueOrNull) : 0;
   if (linkCount === 0) {
     trackLinksRendered();
     return;
@@ -187,8 +189,8 @@ function handleField({
 
   function handleAction({ actionName, ...data }) {
     if (
-      linksRenderedCount < linkCount &&
       actionName === 'linkRendered' &&
+      linksRenderedCount < linkCount &&
       data.field.id === fieldId &&
       data.field.locale === locale.code
     ) {
@@ -208,16 +210,6 @@ function getRichTextLinkCount(localeField) {
 
 function getLinkFieldLinkCount(localeField) {
   return Array.isArray(localeField) ? localeField.length : Number(!!localeField);
-}
-
-function handleAngularReferenceFieldEditor(loadEvents, trackLinksRendered) {
-  if (loadEvents) {
-    loadEvents.stream.onValue(({ actionName }) => {
-      if (actionName === 'referenceLinksRendered') {
-        trackLinksRendered();
-      }
-    });
-  }
 }
 
 function isRichTextField(field) {

src/javascripts/app/entity_editor/WidgetRenderer.js
@@ -45,19 +45,22 @@ const useTrackedRenderingType = ({ locale, loadEvents, widgetApi, widget }) => {
   const isCustom = isCustomWidget(widgetNamespace);
   const isBuiltIn = widgetNamespace === WidgetNamespace.BUILTIN;
 
-  const { trackLinksRendered, handleWidgetLinkRenderEvents } = getTrackingEvents({
-    loadEvents,
-    widget,
-    locale,
-    widgetApi,
-  });
-
   // The rendering needs only to be tracked once
   React.useEffect(() => {
-    if (hasProblem || isCustom) {
-      trackLinksRendered();
-    } else if (isBuiltIn) {
+    const { trackLinksRendered, handleWidgetLinkRenderEvents } = getTrackingEvents({
+      loadEvents,
+      widget,
+      locale,
+      widgetApi,
+    });
+    if (isBuiltIn && !hasProblem) {
       handleWidgetLinkRenderEvents();
+    } else {
+      // TODO: This is wrong, we have to check whether this is actually a link type field! Perhaps just send the
+      //  field locale and ID and use a map to keep track of fields instead of a counter.
+
+      // Custom widget renderer (app or ui-extension) that we can't measure or an issue (e.g. no widget renderer)
+      trackLinksRendered();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);

src/javascripts/app/entry_editor/EntryEditor.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useLayoutEffect } from 'react';
 import PropTypes from 'prop-types';
 import * as K from 'core/utils/kefir';
 import { bootstrapEntryEditorLoadEvents } from 'app/entity_editor/LoadEventTracker';
@@ -50,7 +50,7 @@ export const EntryEditor = (props) => {
   });
 
   const loadEvents = K.useLifeline();
-  useEffect(() => {
+  useLayoutEffect(() => {
     bootstrapEntryEditorLoadEvents(otDoc, loadEvents, editorData, trackLoadEvent);
   }, [editorData, loadEvents, otDoc, trackLoadEvent]);
 

src/javascripts/app/widgets/ReferenceEditor/MediaEditorWithTracking.js
@@ -28,7 +28,7 @@ const onMediaAction = (loadEvents, sdk) => (action) => {
       break;
     case 'rendered':
       loadEvents.emit({
-        actionName: 'linksRendered',
+        actionName: 'linkRendered',
         field: {
           id: sdk.field.id,
           locale: sdk.field.locale,

src/javascripts/app/widgets/ReferenceEditor/ReferenceEditorWithTracking.js
@@ -14,9 +14,6 @@ export function getCtId(entry) {
 }
 
 const onEntryAction = (loadEvents, sdk) => {
-  const toRenderCount = (sdk.field.getValue() || []).length;
-  let renderedCount = 0;
-
   return (action) => {
     switch (action.type) {
       case 'select_and_link':
@@ -43,16 +40,13 @@ const onEntryAction = (loadEvents, sdk) => {
         }
         break;
       case 'rendered':
-        renderedCount += 1;
-        if (!toRenderCount || renderedCount === toRenderCount) {
-          loadEvents.emit({
-            actionName: 'linksRendered',
-            field: {
-              id: sdk.field.id,
-              locale: sdk.field.locale,
-            },
-          });
-        }
+        loadEvents.emit({
+          actionName: 'linkRendered',
+          field: {
+            id: sdk.field.id,
+            locale: sdk.field.locale,
+          },
+        });
         break;
       default:
         break;

src/javascripts/app/entity_editor/WidgetRenderer.spec.js
@@ -139,6 +139,7 @@ describe('WidgetRenderer', () => {
       WidgetRendererExternal.isCustomWidget = jest.fn().mockReturnValue(true);
 
       const { queryByTestId } = renderComponent((props) => {
+        props.widget.widgetNamespace = `NOT_${WidgetRendererExternal.WidgetNamespace.BUILTIN}`;
         props.loadEvents = {};
         return props;
       });