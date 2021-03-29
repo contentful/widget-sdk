7669da6e4492c56a88eb729cbeca9845faf8be39
Patrycja Radaczyńska <trycjapa@gmail.com>
2021-03-29 14:32:59 +0200

    feat[HEJO-2389]: display description of the feature in modal (#7835)

--- src/javascripts/features/high-value-modal/components/FeatureModal.tsx
+++ src/javascripts/features/high-value-modal/components/FeatureModal.tsx
@@ -1,23 +1,56 @@
 import React from 'react';
+import { css } from 'emotion';
+import tokens from '@contentful/forma-36-tokens';
 import { Button, Modal } from '@contentful/forma-36-react-components';
+import { ContentfulRichText } from 'core/services/ContentfulCDA';
+import { Document } from '@contentful/rich-text-types';
 
+const styles = {
+  // TODO: fix in F36
+  modalControllsFullWith: css({
+    width: '100%',
+  }),
+  buttonWithSpaceRight: css({
+    marginRight: tokens.spacingS,
+  }),
+};
+
+interface ModalFeatureContentProps {
+  isShown: boolean;
+  onClose: () => boolean;
+  name: string;
+  longDescription: Document;
+  primaryCtaAdmins: string;
+  helpCenterUrl: string;
+  secondaryCtaForAdmins: string;
+  learnMore: string;
+}
+
+// Content displayed in the modal is fetched from Contentful ProdDev space.
+// To fetch content use fetchWebappContentByEntryID('your_entry_id) function
 const FeatureModal = ({
   isShown,
   onClose,
   name,
+  longDescription,
   primaryCtaAdmins,
   helpCenterUrl,
   secondaryCtaForAdmins,
   learnMore,
-}) => {
+}: ModalFeatureContentProps) => {
   return (
     <Modal isShown={isShown} onClose={onClose} size="large">
       {() => (
         <div>
           <Modal.Header title={`${name}`} onClose={onClose} />
-          <Modal.Content>Content from rich content editor</Modal.Content>
-          <Modal.Controls position="right">
-            <Button buttonType="muted" href={learnMore}>
+          <Modal.Content>
+            <>
+              <div>Illustration placeholder</div>
+              <ContentfulRichText document={longDescription} />
+            </>
+          </Modal.Content>
+          <Modal.Controls position="right" className={styles.modalControllsFullWith}>
+            <Button buttonType="muted" href={learnMore} className={styles.buttonWithSpaceRight}>
               {secondaryCtaForAdmins}
             </Button>
             <Button buttonType="primary" href={helpCenterUrl}>
