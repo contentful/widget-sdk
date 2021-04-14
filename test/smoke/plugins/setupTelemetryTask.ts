3e99f0e3335269047a384b01e78c865fb6dbdb48
Joshua Smock <jo-sm@users.noreply.github.com>
2021-04-14 16:51:29 +0200

    improvement: Modify the metrics we send to Librato for the smoke tests
    The old metrics were really difficult to get more broad insights into
    (like, what's the overall success rate for all smoke tests).

--- test/smoke/plugins/setupTelemetryTask.ts
+++ test/smoke/plugins/setupTelemetryTask.ts
@@ -2,11 +2,9 @@ import * as telemetry from '../telemetry';
 
 export function setupTelemetryTask(on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) {
   on('task', {
-    async measure({ name: rawName, value }) {
+    async measure({ name, value }) {
       const libratoAuthToken = config.env.libratoAuthToken;
       const environment = config.env.environment;
-
-      const name = `testcase.${rawName}`;
 
       return telemetry.measure(libratoAuthToken, environment, [{ name, value }]);
     },
