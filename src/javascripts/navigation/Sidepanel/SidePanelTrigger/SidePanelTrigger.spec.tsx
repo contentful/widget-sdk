be9a9d215d7cd89357ae4031d66e903b5b3658e3
Thomas Kellermeier <thomas.kellermeier@contentful.com>
2021-04-22 13:23:27 +0200

    test: fix and refactor SidePanelTrigger

--- src/javascripts/navigation/Sidepanel/SidePanelTrigger/SidePanelTrigger.spec.tsx
+++ src/javascripts/navigation/Sidepanel/SidePanelTrigger/SidePanelTrigger.spec.tsx
@@ -2,27 +2,29 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import SidePanelTrigger from './index';
 
-const mockOnClick = jest.fn();
-
 describe('SidePanelTrigger', () => {
-  it('should render single button by default', () => {
-    const { container } = render(<SidePanelTrigger onClickOrganization={mockOnClick} />);
-    expect(container.querySelectorAll('button')).toHaveLength(1);
+  const renderComponent = () => {
+    const props = {
+      onClickOrganization: jest.fn(),
+      openAppSwitcher: jest.fn(),
+    };
+    const component = render(<SidePanelTrigger {...props} />);
+    return { ...component, props };
+  };
+  it('should render both buttons by default', () => {
+    const { container } = renderComponent();
+    expect(container.querySelectorAll('button')).toHaveLength(2);
   });
 
   it('button should call onClick prop', () => {
-    const { getByTestId } = render(<SidePanelTrigger onClickOrganization={mockOnClick} />);
+    const { getByTestId, props } = renderComponent();
     fireEvent.click(getByTestId('sidepanel-trigger'));
-    expect(mockOnClick).toBeCalledTimes(1);
+    expect(props.onClickOrganization).toBeCalledTimes(1);
   });
 
-  it('should render experience switcher with contentful apps enabled', () => {
-    const { getByTestId } = render(
-      <SidePanelTrigger onClickOrganization={mockOnClick} openAppSwitcher={mockOnClick} />
-    );
-
-    fireEvent.click(getByTestId('sidepanel-trigger'));
-    fireEvent.click(getByTestId('sidepanel-trigger-apps'));
-    expect(mockOnClick).toBeCalledTimes(2);
+  it('should render experience switch with contentful apps enabled', () => {
+    const { getByLabelText, props } = renderComponent();
+    fireEvent.click(getByLabelText('Switch Contentful app'));
+    expect(props.openAppSwitcher).toBeCalledTimes(1);
   });
 });
