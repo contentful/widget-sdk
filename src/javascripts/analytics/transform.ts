contentful/widget-sdk

commit f3a1acee555bc461e74c4dcbf016b2d66c4d6692

chore: use solely entity-search from shared package [FE-49] (#7639)
* chore: use solely entity-search from shared package

* chore: introduce core/typings based on plain CMA

* chore: some TS migration

* fix: entity search loads users async, adjusted tests

src/javascripts/features/entity-search/EntitySearch.tsx
@@ -3,13 +3,8 @@ import type { EntityType, ExternalSearchState } from '@contentful/entity-search/
 import { upperFirst } from 'lodash';
 import { CustomInputRenderers, Search, SearchFeatures } from '@contentful/entity-search';
 import { useSearchSdk } from './useSearchSdk';
-import { MetadataTagBridge } from './MetadataTagBridge';
-
-type ListViewContext = {
-  getView: () => any;
-  assignView: (data: Record<string, any>, done: Function) => any;
-  lastAction: string;
-};
+import { MetadataTagBridge } from './ValueInput/MetadataTagBridge';
+import type { ListViewContext, ViewCallback } from './useListView';
 
 type EntitySearchProps = {
   className: string;
@@ -17,19 +12,19 @@ type EntitySearchProps = {
   entityType: EntityType;
   isLoading?: boolean;
   withMetadata: boolean;
-  onUpdate: Function;
+  onUpdate: ViewCallback;
   listViewContext: ListViewContext;
 };
 
 type EntitySearchContextProps = {
-  onUpdate: Function;
+  onUpdate: ViewCallback;
   listViewContext: ListViewContext;
   features: SearchFeatures;
   customInputRenderers?: CustomInputRenderers;
 };
 
 // we want to re-render complete search on external changes
-function useEntitySearchKey({ lastAction, view }: { lastAction: string; view: any }) {
+function useEntitySearchKey({ lastAction, view }: { lastAction?: string; view: any }) {
   const externalUpdateKey = useRef(0);
   const key = useMemo(() => {
     const action = lastAction;

src/javascripts/LaunchDarkly.js
@@ -56,8 +56,6 @@ export const FLAGS = {
   APP_TRIAL: 'feature-moi-01-2021-app-trial-mechanics',
   REACT_MIGRATION_CT: 'react-migration-10-2020-content-type-editor',
 
-  ENTITY_SEARCH: 'migration-02-2021-entity-search',
-
   // So that we can test the fallback mechanism without needing to rely on an actual
   // flag above, we use these special flags.
   __FLAG_FOR_UNIT_TESTS__: 'test-flag',
@@ -95,7 +93,6 @@ const FALLBACK_VALUES = {
   [FLAGS.REFERENCE_TREE_BULK_ACTIONS_SUPPORT]: false,
 
   [FLAGS.REACT_MIGRATION_CT]: false,
-  [FLAGS.ENTITY_SEARCH]: false,
 
   // See above
   [FLAGS.__FLAG_FOR_UNIT_TESTS__]: 'fallback-value',

src/javascripts/access_control/Users/UserListPresentation.js
@@ -11,7 +11,6 @@ import { List } from '@contentful/forma-36-react-components';
 import { SpaceMember as SpaceMemberPropType } from 'app/OrganizationSettings/PropTypes';
 import DocumentTitle from 'components/shared/DocumentTitle';
 import { FilterPill } from 'features/entity-search';
-import { FilterValueInputs as ValueInput } from 'core/services/ContentQuery';
 import UserListRow from './UserListRow';
 
 import AddUsersToSpaceNote from './AddUsersToSpaceNote';
@@ -27,7 +26,6 @@ const styles = {
   groupSelect: css({
     marginLeft: '67px',
     marginTop: tokens.spacingXl,
-    display: 'inline-flex',
   }),
   sidebar: {
     heading: css({
@@ -106,11 +104,11 @@ const UserListPresentation = ({
             className={styles.groupSelect}
             filter={{
               label: 'Group by',
-              valueInput: ValueInput.Select(
-                Object.keys(VIEW_LABELS).map((key) => [key, VIEW_LABELS[key]])
-              ),
+              queryKey: 'users',
+              value: selectedView,
+              filterType: 'select',
+              options: Object.entries(VIEW_LABELS).map(([value, label]) => ({ value, label })),
             }}
-            value={selectedView}
             onChange={onChangeSelectedView}
           />
           {map(userGroups, (members, label) => (

src/javascripts/app/Releases/ReleaseDetail/ReleaseWorkBenchContent.js
@@ -4,7 +4,6 @@ import { cx } from 'emotion';
 import pluralize from 'pluralize';
 import { Workbench, Subheading } from '@contentful/forma-36-react-components';
 import { FilterPill } from 'features/entity-search';
-import { FilterValueInputs as ValueInput } from 'core/services/ContentQuery';
 import { ReleasesContext } from '../ReleasesWidget/ReleasesContext';
 import ReleasesEmptyStateMessage from '../ReleasesPage/ReleasesEmptyStateMessage';
 import { VIEW_LABELS } from './utils';
@@ -58,11 +57,11 @@ const ReleaseWorkBenchContent = ({
               className={styles.layoutPills}
               filter={{
                 label: 'View',
-                valueInput: ValueInput.Select(
-                  Object.keys(VIEW_LABELS).map((key) => [key, VIEW_LABELS[key]])
-                ),
+                queryKey: 'view',
+                value: entitiesLayout,
+                filterType: 'select',
+                options: Object.entries(VIEW_LABELS).map(([value, label]) => ({ value, label })),
               }}
-              value={entitiesLayout}
               onChange={setEntitiesLayout}
             />
           </div>

src/javascripts/core/services/ContentQuery/Filters.js
@@ -1,5 +1,5 @@
-import { cloneDeep, find, get, has, map, memoize } from 'lodash';
-import { assign, concat, push } from 'utils/Collections';
+import { find, get, has, map } from 'lodash';
+import { assign } from 'utils/Collections';
 import { equality as equalityOperator, getOperatorsByType } from './Operators';
 import mimetype from '@contentful/mimetype';
 
@@ -8,20 +8,8 @@ import { METADATA_TAGS_ID } from 'data/MetadataFields';
 
 const CT_QUERY_KEY_PREFIX = 'fields';
 
-const SUPPORTED_CT_FIELD_TYPES = [
-  'Symbol',
-  'Text',
-  'Integer',
-  'Number',
-  'Date',
-  'Boolean',
-  'Array',
-  'Link',
-  'RichText',
-];
-
-export const statusQueryKey = '__status';
-export const Status = {
+const statusQueryKey = '__status';
+const Status = {
   Any: '',
   Published: 'published',
   Changed: 'changed',
@@ -134,79 +122,6 @@ const assetsFieldFilters = [
 }));
 
 /**
- * Create the filter for the contentType field of an entry.
- *
- * Takes a list of available content types as an argument. This is used
- * to contstruct the available options to select from.
- */
-export function contentTypeFilter(contentTypes) {
-  let valueInputSelect = [...contentTypes.map((ct) => [ct.sys.id, ct.name])];
-
-  if (contentTypes.length !== 1) {
-    valueInputSelect = [['', 'Any'], ...valueInputSelect];
-  }
-
-  return {
-    name: 'Content type',
-    queryKey: 'content_type',
-    operators: [equalityOperator],
-    valueInput: ValueInput.Select(valueInputSelect),
-  };
-}
-
-/**
- *
- * @param {API.ContentType[]} contentTypes List of content types
- * @param {string} contentTypeId ContentType id
- *
- * @returns {API.ContentType?}
- */
-export function getContentTypeById(contentTypes, contentTypeId) {
-  return find(contentTypes, (ct) => ct.sys.id === contentTypeId);
-}
-
-export function getFiltersFromQueryKey({
-  contentTypes,
-  searchFilters,
-  contentTypeId,
-  users,
-  withAssets = false,
-  withMetadata = false,
-}) {
-  const contentType = getContentTypeById(contentTypes, contentTypeId);
-  const filters = searchFilters
-    .map(([queryKey, op, value]) => [
-      buildFilterFieldByQueryKey(contentType, queryKey, withAssets, withMetadata),
-      op,
-      value,
-    ])
-    .filter(([queryKey]) => queryKey !== undefined);
-
-  return setUserFieldsFilters(users, filters);
-}
-
-export function sanitizeSearchFilters(
-  filters = [],
-  contentTypes,
-  contentTypeId,
-  withAssets = false,
-  withMetadata = false
-) {
-  const contentType = getContentTypeById(contentTypes, contentTypeId);
-
-  return filters.filter(([queryKey]) => {
-    if (contentType && isContentTypeField(queryKey)) {
-      return getFieldByApiName(contentType, getApiName(queryKey)) !== undefined;
-    } else {
-      return (
-        find(getFilters(withAssets, withMetadata), (filter) => filter.queryKey === queryKey) !==
-        undefined
-      );
-    }
-  });
-}
-
-/**
  * Returns a field filter for given queryKey from
  * the fields of provided Content Type or sys field filters.
  *
@@ -236,131 +151,6 @@ export function buildFilterFieldByQueryKey(
   }
 
   return filterField;
-}
-
-/**
- * Checks if provided queryKey is applicable to the ContentType.
- *
- * @param {API.ContentType} contentType
- * @param {string} queryKey
- *
- * @returns {boolean}
- */
-export function isFieldFilterApplicableToContentType(contentType, queryKey) {
-  if (isContentTypeField(queryKey)) {
-    if (!contentType) {
-      return false;
-    }
-
-    const field = getFieldByApiName(contentType, getApiName(queryKey));
-
-    return field !== undefined;
-  } else {
-    return true;
-  }
-}
-
-/**
- * Returns a list of all filters.
- *
- * This list consists of
- * - The contentType filter
- * - Filters for sys fields common to each content type
- * - A filter for each field of the given content types
- */
-// Memoized to improve performance on huge lists (>1500 elements).
-const allFilters = memoize(
-  (contentTypes, withAssets = false, withMetadata = false) => {
-    const ctFieldFilters = contentTypes.reduce((filters, ct) => {
-      return ct.fields.reduce((filters, ctField) => {
-        return push(filters, buildFilterField(ct, ctField));
-      }, filters);
-    }, []);
-
-    const fields = concat(getFilters(withAssets, withMetadata), withAssets ? [] : ctFieldFilters);
-    return fields;
-  },
-  (contentTypes, withAssets, withMetadata) =>
-    contentTypes.concat([{ withAssets: withAssets, withMetadata: withMetadata }])
-);
-
-/**
- * Returns a list of filters that begin with the search string and
- * match the selected content type ID or
- * a list of all filters if there was no match by name.
- *
- * @param {string} searchString
- *   Only return filters whose name starts with this string
- * @param {string?} contentTypeID
- *   If given return only filters for fields on this content type.
- * @param {API.ContentType[]} availableContentTypes
- * @returns {Filter[]}
- */
-// Memoized to improve performance on huge lists (>1500 elements).
-export const getMatchingFilters = (
-  searchString,
-  contentTypeId,
-  availableContentTypes,
-  withAssets,
-  withMetadata
-) => {
-  let filters = allFilters(availableContentTypes, withAssets, withMetadata);
-  filters = filterByName(filters, searchString);
-  filters = filterByContentType(filters, contentTypeId);
-
-  return withAssets ? filters : filterBySupportedTypes(filters);
-};
-
-function filterBySupportedTypes(filters) {
-  return filters.filter(({ queryKey, type }) => {
-    if (isContentTypeField(queryKey)) {
-      return SUPPORTED_CT_FIELD_TYPES.indexOf(type) > -1;
-    }
-    return true;
-  });
-}
-
-function filterByName(filters, searchString = '') {
-  searchString = searchString.trim().toLowerCase();
-  return filters.filter((filter) => {
-    return filter.name.toLowerCase().includes(searchString);
-  });
-}
-
-function filterByContentType(filters, contentTypeId) {
-  if (contentTypeId) {
-    // Remove all filters that do not apply to the given Content Type
-    return filters.filter((field) => {
-      if (field.contentType) {
-        return field.contentType.id === contentTypeId;
-      } else {
-        return true;
-      }
-    });
-  } else {
-    return filters;
-  }
-}
-
-function setUserFieldsFilters(users, filters) {
-  const getName = (user) => (user.firstName ? user.firstName : user.email);
-  const byName = (curr, prev) => getName(curr).localeCompare(getName(prev));
-  const usersOptions = users.sort(byName).map((user) => {
-    const label = user.firstName ? `${user.firstName} ${user.lastName}` : user.email;
-    const value = user.sys.id;
-
-    return [value, label];
-  });
-
-  return filters.map(([filter, op, value]) => {
-    const filterClone = cloneDeep(filter);
-
-    if (filterClone && filterClone.type === 'User') {
-      filterClone.valueInput = ValueInput.Select([['', 'Any'], ...usersOptions]);
-    }
-
-    return [filterClone, op, value];
-  });
 }
 
 function isContentTypeField(queryKey) {

src/javascripts/core/services/ContentQuery/__tests__/Filters.spec.js
@@ -1,203 +1,0 @@
-import { contentTypes } from './helpers';
-import * as Filters from '../Filters';
-
-describe('app/ContentList/Search/Filters', () => {
-  const isApplicableMacro = (message, contentType, queryKey, expected) => {
-    it(message, () => {
-      expect(Filters.isFieldFilterApplicableToContentType(contentType, queryKey)).toEqual(expected);
-    });
-  };
-
-  describe('isFieldFilterApplicableToContentType with contentType', () => {
-    const isApplicableMacroWithContentType = (message, queryKey, expected) =>
-      isApplicableMacro(message, contentTypes[0], queryKey, expected);
-
-    isApplicableMacroWithContentType('sys field', 'sys.id', true);
-    isApplicableMacroWithContentType('content type field', 'fields.companyName', true);
-    isApplicableMacroWithContentType(
-      'non-existent content type field',
-      'fields.companyName1',
-      false
-    );
-  });
-
-  describe('isFieldFilterApplicableToContentType without contentType', () => {
-    const isApplicableMacroWithoutContentType = (message, queryKey, expected) =>
-      isApplicableMacro(message, null, queryKey, expected);
-
-    isApplicableMacroWithoutContentType('sys field', 'sys.id', true);
-    isApplicableMacroWithoutContentType('content type field', 'fields.companyName', false);
-    isApplicableMacroWithoutContentType(
-      'non-existent content type field',
-      'fields.companyName1',
-      false
-    );
-  });
-
-  describe('getMatchingFilters', () => {
-    describe('without assets', () => {
-      const getMatchingFiltersMacro = (message, { searchQuery, contentType }, expected) => {
-        it(message, () => {
-          const filters = Filters.getMatchingFilters(
-            searchQuery,
-            contentType,
-            contentTypes,
-            false,
-            true
-          );
-          const queryKeys = filters.map(({ queryKey }) => queryKey);
-
-          expect(queryKeys).toEqual(expected);
-
-          expect(filters.find(({ type }) => type === 'Location')).toBeUndefined();
-
-          expect(filters.find(({ type }) => type === 'Object')).toBeUndefined();
-        });
-      };
-
-      getMatchingFiltersMacro(
-        'returns all sys and all ct fields',
-        {
-          searchQuery: '',
-          contentType: null,
-        },
-        [
-          'sys.updatedAt',
-          'sys.createdAt',
-          'sys.publishedAt',
-          'sys.firstPublishedAt',
-          'sys.updatedBy.sys.id',
-          'sys.createdBy.sys.id',
-          'sys.publishedBy.sys.id',
-          'sys.version',
-          'sys.id',
-          '__status',
-          'metadata.tags.sys.id',
-          'fields.companyName',
-          'fields.logo.sys.id',
-          'fields.companyDescription',
-          'fields.website',
-          'fields.twitter',
-          'fields.email',
-          'fields.phone',
-          'fields.symbol1',
-          'fields.symbol2',
-          'fields.productName',
-          'fields.slug',
-          'fields.productDescription',
-          'fields.sizetypecolor',
-          'fields.image.sys.id',
-          'fields.tags',
-          'fields.categories.sys.id',
-          'fields.price',
-          'fields.brand.sys.id',
-          'fields.quantity',
-          'fields.sku',
-          'fields.website',
-          'fields.createdAt',
-        ]
-      );
-
-      getMatchingFiltersMacro(
-        'returns brand sys and ct fields',
-        {
-          searchQuery: '',
-          contentType: contentTypes[0].sys.id,
-        },
-        [
-          'sys.updatedAt',
-          'sys.createdAt',
-          'sys.publishedAt',
-          'sys.firstPublishedAt',
-          'sys.updatedBy.sys.id',
-          'sys.createdBy.sys.id',
-          'sys.publishedBy.sys.id',
-          'sys.version',
-          'sys.id',
-          '__status',
-          'metadata.tags.sys.id',
-          'fields.companyName',
-          'fields.logo.sys.id',
-          'fields.companyDescription',
-          'fields.website',
-          'fields.twitter',
-          'fields.email',
-          'fields.phone',
-          'fields.symbol1',
-          'fields.symbol2',
-        ]
-      );
-
-      getMatchingFiltersMacro(
-        'returns filters if apiName startsWith searchQuery "cr"',
-        {
-          searchQuery: 'cr',
-          contentType: null,
-        },
-        [
-          'sys.createdAt',
-          'sys.createdBy.sys.id',
-          'fields.companyDescription',
-          'fields.productDescription',
-          'fields.createdAt',
-        ]
-      );
-
-      getMatchingFiltersMacro(
-        'returns filters if apiName startsWith searchQuery "t"',
-        {
-          searchQuery: 't',
-          contentType: null,
-        },
-        [
-          'sys.updatedAt',
-          'sys.createdAt',
-          'sys.publishedAt',
-          'sys.firstPublishedAt',
-          'sys.updatedBy.sys.id',
-          'sys.createdBy.sys.id',
-          '__status',
-          'metadata.tags.sys.id',
-          'fields.companyDescription',
-          'fields.website',
-          'fields.twitter',
-          'fields.productName',
-          'fields.productDescription',
-          'fields.sizetypecolor',
-          'fields.tags',
-          'fields.categories.sys.id',
-          'fields.quantity',
-          'fields.website',
-          'fields.createdAt',
-        ]
-      );
-
-      getMatchingFiltersMacro(
-        'returns filters if apiName startsWith searchQuery "twi"',
-        {
-          searchQuery: 'twi',
-          contentType: null,
-        },
-        ['fields.twitter']
-      );
-
-      getMatchingFiltersMacro(
-        'returns filters if apiName startsWith searchQuery "web"',
-        {
-          searchQuery: 'web',
-          contentType: null,
-        },
-        ['fields.website', 'fields.website']
-      );
-
-      getMatchingFiltersMacro(
-        'returns empty array if there is no matching filters "witter"',
-        {
-          searchQuery: 'witter',
-          contentType: null,
-        },
-        ['fields.twitter']
-      );
-    });
-  });
-});

src/javascripts/core/services/ContentQuery/index.js
@@ -1,11 +1,4 @@
-export {
-  contentTypeFilter,
-  sanitizeSearchFilters,
-  getFiltersFromQueryKey,
-  getMatchingFilters,
-  getContentTypeById,
-  buildFilterFieldByQueryKey,
-} from './Filters';
+export { buildFilterFieldByQueryKey } from './Filters';
 export { FilterValueInputs } from './FilterValueInputs';
 export { buildQuery } from './QueryBuilder';
 export { Operator } from './Operators';

src/javascripts/core/services/ContentQuery/listQuery.js
@@ -38,20 +38,6 @@ export async function getForAssets(opts) {
   const assetContentType = { data: EditorialConstants.assetContentType };
   return prepareEntityListQuery(assetContentType, opts);
 }
-/**
- * @description
- * Prepares an API query for the user list.
- *
- * @param {Paginator} opts.paginator
- * @returns {object}
- */
-export async function getForUsers(opts) {
-  const userContentType = {
-    data: {},
-    getId: _.constant(undefined),
-  };
-  return prepareEntityListQuery(userContentType, opts);
-}
 
 function prepareEntityListQuery(contentType, opts) {
   const queryObject = {

commit 0457fe7a2109c3b19619ae1ef05dc4861e812c4f

fix: search:filter_added and search:filter_removed events
These got broken in #7639

src/javascripts/analytics/transform.ts
@@ -1,4 +1,4 @@
-import { get as getAtPath, snakeCase } from 'lodash';
+import { snakeCase } from 'lodash';
 import { getSnowplowSchema } from './SchemasSnowplow';
 import * as segmentTypewriterPlans from './events';
 import { getSegmentSchema } from './SchemasSegment';
@@ -354,9 +354,9 @@ registerSnowplowEvent('search:search_terms_migrated', 'ui_config_migrate', Searc
 
 // TODO Remove ":" from Segment schema names.
 registerSegmentEvent('search:entry_clicked', 'search:entry_clicked', SearchAndViewsWithSequence);
-// TODO: Re-implement tracking or remove:
 registerSegmentEvent('search:filter_added', 'search:filter_added', SearchAndViewsWithSequence);
 registerSegmentEvent('search:filter_removed', 'search:filter_removed', SearchAndViewsWithSequence);
+// TODO: Re-implement tracking or remove:
 registerSegmentEvent('search:query_changed', 'search:query_changed', SearchAndViewsWithSequence);
 
 registerSnowplowEvent(
@@ -568,8 +568,8 @@ function registerEnvironmentAliasesEvent(event) {
   registerSnowplowEvent(event, 'environment_aliases', EnvironmentAliases);
 }
 
-export function eventExists(eventName) {
-  return !!getAtPath(_events, [eventName]);
+export function eventExists(event) {
+  return !!_events[event];
 }
 
 /**

src/javascripts/analytics/transformers/SearchAndViews.js
@@ -1,25 +1,20 @@
 import { addUserOrgSpace } from './Decorators';
-import { get as getAtPath, omit, extend } from 'lodash';
+import { get, omit } from 'lodash';
 
 const ENTITY_TYPE_MAPPING = { entries: 'entry', assets: 'asset' };
-const EVENTS_WITHOUT_ENTITY_TYPE = ['search:search_terms_migrated'];
 const PROPS_TO_OMIT = ['userId', 'spaceId', 'organizationId', 'currentState'];
 
-export default addUserOrgSpace((event, data) => {
+export default addUserOrgSpace((_event, data) => {
   return {
-    data: extend(
-      eventHasEntityType(event) ? {} : { entity_type: getEntityType(data) },
-      omit(data, PROPS_TO_OMIT)
-    ),
+    data: {
+      entity_type: getEntityType(data),
+      ...omit(data, PROPS_TO_OMIT),
+    },
   };
 });
 
 function getEntityType(data) {
-  const currentState = getAtPath(data, ['currentState'], '');
+  const currentState = get(data, ['currentState'], '');
   const entityTypeSegment = currentState.split('.')[2];
   return ENTITY_TYPE_MAPPING[entityTypeSegment];
-}
-
-function eventHasEntityType(event) {
-  return EVENTS_WITHOUT_ENTITY_TYPE.indexOf(event) > -1;
 }

src/javascripts/features/entity-search/EntitySearch.tsx
@@ -4,6 +4,7 @@ import { upperFirst } from 'lodash';
 import { CustomInputRenderers, Search, SearchFeatures } from '@contentful/entity-search';
 import { useSearchSdk } from './useSearchSdk';
 import { MetadataTagBridge } from './ValueInput/MetadataTagBridge';
+import * as Analytics from 'analytics/Analytics';
 import type { ListViewContext, ViewCallback } from './useListView';
 
 type EntitySearchProps = {
@@ -77,6 +78,13 @@ export function EntitySearch(props: EntitySearchProps) {
     },
   });
 
+  const onTrack = ({ type, filter }: { type: string; filter?: { name: string } }) => {
+    if (['filter_added', 'filter_removed'].includes(type)) {
+      // `search:filter_added` and `search:filter_removed`
+      Analytics.track(`search:${type}`, { filter: filter?.name });
+    }
+  };
+
   return (
     <Search
       key={key}
@@ -87,6 +95,7 @@ export function EntitySearch(props: EntitySearchProps) {
       features={features}
       onChange={onChange}
       onChangeDebounce={200}
+      onTrack={onTrack}
       sdk={searchSdk}
     />
   );