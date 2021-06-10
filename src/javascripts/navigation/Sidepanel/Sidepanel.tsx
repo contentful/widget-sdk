74273bb8660af200f96f45da2320f2a0204cf491
Kado Damball <kadodamball@hotmail.com>
2021-06-10 11:43:33 +0200

    chore(a11y): use href instead of onClick for links

--- src/javascripts/navigation/Sidepanel/Sidepanel.tsx
+++ src/javascripts/navigation/Sidepanel/Sidepanel.tsx
@@ -37,12 +37,16 @@ const styles = {
   }),
   orgSettingsButton: css({
     display: 'flex',
-    alignItems: 'center',
-    borderTop: `1px solid ${tokens.colorElementLight}`,
-    padding: tokens.spacingM,
     cursor: 'pointer',
-    '&:hover': {
-      backgroundColor: tokens.colorElementLight,
+    borderTop: `1px solid ${tokens.colorElementLight}`,
+    '&:link': {
+      display: 'flex',
+      padding: tokens.spacingM,
+      alignItems: 'center',
+      '&:hover': {
+        backgroundColor: tokens.colorElementLight,
+        textDecoration: 'none',
+      },
     },
   }),
 };
@@ -141,9 +145,7 @@ export class Sidepanel extends React.Component<SidepanelProps, SidepanelState> {
     }));
   };
 
-  gotoOrgSettings = () => {
-    this.props.closeSidePanel();
-
+  getLinktoOrgSettings = () => {
     if (this.state.currOrg) {
       const orgId = this.state.currOrg.sys.id;
       const isPricingV2 = this.state.currOrg.pricingVersion === 'pricing_version_2';
@@ -152,11 +154,11 @@ export class Sidepanel extends React.Component<SidepanelProps, SidepanelState> {
         OrgRoles.isOwnerOrAdmin(this.state.currOrg) || isOrganizationOnTrial(this.state.currOrg);
 
       if (isOwnerOrAdminOrOnTrial && isPricingV2) {
-        router.navigate({ path: 'organizations.subscription.overview', orgId });
+        return router.href({ path: 'organizations.subscription.overview', orgId });
       } else if (isOwnerOrAdminOrOnTrial && !isPricingV2) {
-        router.navigate({ path: 'organizations.subscription_v1', orgId });
+        return router.href({ path: 'organizations.subscription_v1', orgId });
       } else {
-        router.navigate({ path: 'organizations.teams', orgId });
+        return router.href({ path: 'organizations.teams', orgId });
       }
     }
   };
@@ -246,18 +248,21 @@ export class Sidepanel extends React.Component<SidepanelProps, SidepanelState> {
               setOpenedSpaceId={this.setOpenedSpaceId}
             />
 
-            <TextLink
-              className={styles.orgSettingsButton}
-              onClick={this.gotoOrgSettings}
-              data-test-id="sidepanel-org-actions-settings">
-              <Icon
-                className={css({ marginRight: tokens.spacingXs })}
-                icon="Settings"
-                color="muted"
-              />
-              Organization settings
-              {showSubscriptionSettings && ' & subscriptions'}
-            </TextLink>
+            {this.state.currOrg && (
+              <TextLink
+                className={styles.orgSettingsButton}
+                linkType="muted"
+                href={this.getLinktoOrgSettings()}
+                data-test-id="sidepanel-org-actions-settings">
+                <Icon
+                  className={css({ marginRight: tokens.spacingXs })}
+                  icon="Settings"
+                  color="muted"
+                />
+                Organization settings
+                {showSubscriptionSettings && ' & subscriptions'}
+              </TextLink>
+            )}
           </>
         )}
 
