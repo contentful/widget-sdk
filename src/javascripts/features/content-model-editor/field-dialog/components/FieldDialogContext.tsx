25aa27f72b52b285e493b704ed00ff975ed61b6b
denkristoffer <denkristoffer@users.noreply.github.com>
2021-06-11 14:26:26 +0200

    chore: types

--- src/javascripts/features/content-model-editor/field-dialog/components/FieldDialogContext.tsx
+++ src/javascripts/features/content-model-editor/field-dialog/components/FieldDialogContext.tsx
@@ -1,14 +1,19 @@
 import constate from 'constate';
+import keyBy from 'lodash/keyBy';
+import type { ContentType, ContentTypeField, EditorInterface } from 'core/typings';
 
 type FieldDialogProps = {
-  field: any;
-  contentType: any;
-  editorInterface: any;
+  field: ContentTypeField;
+  contentType: ContentType;
+  editorInterface: EditorInterface;
 };
 
 const useFieldDialog = (props: FieldDialogProps) => {
   const { field, contentType, editorInterface } = props;
-  const instance = editorInterface.controls.find((c) => c.field.apiName === field.apiName);
+  // const instance = editorInterface.controls?.find((c) => c.field.apiName === field.apiName);
+  const instance = field.apiName
+    ? keyBy(editorInterface.controls, 'fieldId')[field.apiName]
+    : undefined;
 
   return {
     field,
