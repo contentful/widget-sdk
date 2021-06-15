19f3da6b0e8cfd03fe9116167c43ec8ee2956075
denkristoffer <denkristoffer@users.noreply.github.com>
2021-06-15 09:17:55 +0200

    chore: convert FeedbackButton to TS

--- src/javascripts/app/common/FeedbackButton.tsx
+++ src/javascripts/app/common/FeedbackButton.tsx
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { useCallback } from 'react';
 import { Notification, TextLink } from '@contentful/forma-36-react-components';
 import { ModalLauncher } from '@contentful/forma-36-react-components';
-import FeedbackDialog from './FeedbackDialog';
 import * as Analytics from 'analytics/Analytics';
 
-export default class FeedbackButton extends Component {
-  static propTypes = {
-    about: PropTypes.string.isRequired,
-    target: PropTypes.string.isRequired,
-    label: PropTypes.string,
-  };
+import FeedbackDialog from './FeedbackDialog';
 
-  onClick = async () => {
-    const { about, target } = this.props;
+export interface FeedbackButtonProps {
+  about: string;
+  target: string;
+  label?: string;
+}
 
+export default function FeedbackButton({ about, label, target }: FeedbackButtonProps) {
+  const onClick = useCallback(async () => {
     const feedback = await ModalLauncher.open(({ isShown, onClose }) => (
       <FeedbackDialog
         key={`${Date.now()}`}
@@ -29,9 +27,7 @@ export default class FeedbackButton extends Component {
       Analytics.track('feedback:give', { about, target, feedback });
       Notification.success('Thank you for your feedback!');
     }
-  };
+  }, [about, target]);
 
-  render() {
-    return <TextLink onClick={this.onClick}>{this.props.label || 'Give feedback'}</TextLink>;
-  }
+  return <TextLink onClick={onClick}>{label || 'Give feedback'}</TextLink>;
 }
