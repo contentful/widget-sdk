contentful/widget-sdk

commit ed98775ab5857fae56ea10d4f84036473021066f

feat: update typewriter events [] (#8381)
Updates `Analytics.tracking.experimentStarted` and
`Analytics.tracking.editorLoaded` typewriter tracking
calls to comply with schema changes of
contentful/segment-schema-registry#179

src/javascripts/analytics/Analytics.ts
@@ -181,16 +181,20 @@ export const tracking: Plan = _.mapValues(segment.plan, (planFn, planKey) => (pr
 });
 
 /**
- * Returns props used by all legacy Snowplow events to be used with `Analytics.tracking.` as they have
- * to be passed manually as there's no more transformers adding them automatically.
+ * Returns props that should be used by all new Segment events tracked via typewriter.
+ *
+ * TODO: Add these automatically inside `Analytics.typewriter.` functions so that tracking
+ *  calls don't have to call this function.
  */
-export function legacyEventProps() {
-  const { userId, organizationId, spaceId } = getBasicPayload();
-  return {
-    executing_user_id: userId,
-    organization_id: organizationId,
-    space_id: spaceId,
-  };
+export function defaultEventProps() {
+  return _.pickBy(
+    {
+      space_key: getSessionData('space.sys.id', VALUE_UNKNOWN),
+      organization_key: getSessionData('organization.sys.id', VALUE_UNKNOWN),
+      environment_key: getSessionData('environment.sys.id', VALUE_UNKNOWN),
+    },
+    (val) => val !== VALUE_UNKNOWN
+  );
 }
 
 /**

src/javascripts/analytics/events.ts
@@ -1,31 +1,50 @@
 export {
-  pageViewed,
-  accountDropdownPendingTasksFetched,
-  apiKeyClipboardCopy,
-  appManagementCreated,
-  appManagementUpdated,
+  appOpen,
+  appTheExampleAppOpen,
+  appUninstallationReason,
   appsLifecycleEvent,
   assetCreated,
   assetListAddAssetMultiple,
   assetListAddAssetSingle,
   assetPublished,
+  boilerplate,
   bulkEditorClose,
   bulkEditorEditInEntryEditor,
   bulkEditorOpen,
   bulkEditorOpenSlideIn,
+  contentPreview,
+  contentType,
+  dialog,
+  editorInterfaceFieldUpdated,
   editorLoaded,
   editorWorkbenchTabOpen,
   elementClick,
-  editorInterfaceFieldUpdated,
   entityEditorEditConflict,
-  entryCreate,
+  entityEditorEditConflict2,
+  entry,
   entryPublish,
-  entryEditorView,
+  entryCreate,
+  entryCreated,
+  entryPublish1,
+  entryReferences,
   entryReferencesDialogOpen,
   entryReferencesPublish,
   entryReferencesValidate,
-  experimentStart,
+  entryView,
+  environmentAliases,
+  environmentBranching,
+  experiment,
+  experimentStarted,
+  extensionActivate,
+  extensionRender,
+  extensionSetValue,
+  featureBulkEditor,
+  featureCommentsAndTasks,
+  featurePlayground,
   featureReferenceAction,
+  featureSnapshot,
+  featureSsoSelfConfiguration,
+  featureTextEditor,
   globalAppLoaded,
   globalDialog,
   globalSpaceChanged,
@@ -36,10 +55,13 @@ export {
   incomingLinksQuery,
   jobsCancel,
   jobsCreate,
+  jobsProcessing,
+  pageView,
   perfDomContentLoaded,
   perfFirstContentfulPaint,
   perfTimeToInteractive,
   personalAccessTokenAction,
+  previewApiKey,
   releaseCreated,
   releaseDialogBox,
   releaseEntityAdded,
@@ -56,26 +78,22 @@ export {
   searchViewCreated,
   searchViewDeleted,
   searchViewLoaded,
-  slideInEditorArrowBack,
-  slideInEditorDelete,
-  slideInEditorOpen,
-  slideInEditorOpenCreate,
-  slideInEditorPeekClick,
+  searchPerform,
+  sharejsCmaEntityVersionMismatch,
+  slideInEditor,
   spaceCreate,
-  spaceWizardCancel,
-  spaceWizardConfirm,
-  spaceWizardEnteredDetails,
-  spaceWizardLinkClick,
-  spaceWizardNavigate,
-  spaceWizardOpen,
-  spaceWizardSelectPlan,
-  spaceWizardSpaceCreate,
-  spaceWizardSpaceTypeChange,
   teamsInSpaceUsersAdded,
   textEditorAction,
   trackingInvalidEvent,
-  translationSidebarChangeFocusedLocale,
-  translationSidebarDeselectActiveLocale,
-  translationSidebarToggleWidgetMode,
-  translationSidebarUpdateActiveLocales,
+  translationSidebar,
+  uiConfigMigrate,
+  uiExtensionInstall,
+  uiExtensionSave,
+  uiWebhookEditorSave,
+  viewCreate,
+  viewDelete,
+  viewEdit,
+  viewLoad,
+  widgetRendererFallbackRendered,
+  widgetRendererFallbackWarningShown,
 } from './generated';

src/javascripts/app/entity_editor/LoadEventTracker.js
@@ -54,7 +54,7 @@ export function createLoadEventTracker({
     const totalSlideCount = keys(slideStates).length;
     const slideLevel = findIndex(slideStates, (state) => isEqual(state.slide, slide));
     const baseData = {
-      ...Analytics.legacyEventProps(),
+      ...Analytics.defaultEventProps(),
       slides_controller_uuid: slidesControllerUuid,
       slide_uuid: slideUuid,
       total_slide_count: totalSlideCount,

src/javascripts/components/shared/stack-onboarding/screens/ChoiceScreen.js
@@ -4,7 +4,7 @@ import FullScreen from 'components/shared/stack-onboarding/components/FullScreen
 import Button from '../components/Button';
 import { getBrowserStorage } from 'core/services/BrowserStorage';
 import { getStoragePrefix } from 'components/shared/auto_create_new_space/CreateModernOnboardingUtils';
-import { updateUserInSegment, tracking } from 'analytics/Analytics';
+import { updateUserInSegment, tracking, defaultEventProps } from 'analytics/Analytics';
 import { FLAGS, getVariation } from 'LaunchDarkly';
 import { ModalLauncher } from '@contentful/forma-36-react-components';
 import { FlexibleOnboardingDialog } from 'features/onboarding';
@@ -51,7 +51,8 @@ class ChoiceScreen extends React.Component {
       isDevPathPending: true,
     });
     if (this.state.growthExperimentVariant !== null) {
-      tracking.experimentStart({
+      tracking.experimentStarted({
+        ...defaultEventProps(),
         experiment_id: FLAGS.EXPERIMENT_ONBOARDING_MODAL,
         experiment_variation: this.state.growthExperimentVariant
           ? 'flexible-onboarding'

src/javascripts/features/space-home/AdminSpaceHome.js
@@ -18,7 +18,7 @@ import {
 } from 'features/onboarding';
 import { useSpaceEnvContext } from 'core/services/SpaceEnvContext/useSpaceEnvContext';
 import { FLAGS, getVariation } from 'LaunchDarkly';
-import { tracking } from 'analytics/Analytics';
+import { defaultEventProps, tracking } from 'analytics/Analytics';
 import { isOwnerOrAdmin } from 'services/OrganizationRoles';
 import { getOrganization } from 'services/TokenStore';
 
@@ -60,7 +60,8 @@ export const AdminSpaceHome = ({
       );
 
       if (newOnboardingExperimentVariation !== null) {
-        tracking.experimentStart({
+        tracking.experimentStarted({
+          ...defaultEventProps(),
           experiment_id: FLAGS.EXPERIMENT_ONBOARDING_MODAL,
           experiment_variation: newOnboardingExperimentVariation
             ? 'flexible-onboarding'

src/javascripts/features/space-home/SpaceHome.js
@@ -29,7 +29,7 @@ import {
 import { getModule } from 'core/NgRegistry';
 import { ExpiredTrialSpaceHome } from './ExpiredTrialSpaceHome';
 import { getVariation, FLAGS } from 'LaunchDarkly';
-import * as Analytics from 'analytics/Analytics';
+import { tracking, defaultEventProps } from 'analytics/Analytics';
 import { useTrialSpace } from 'features/trials';
 
 const isTEASpace = (contentTypes, currentSpace) => {
@@ -65,11 +65,10 @@ const fetchData =
       organizationId: currentOrganizationId,
     });
     if (testExperimentVariation !== null) {
-      Analytics.tracking.experimentStart({
+      tracking.experimentStarted({
+        ...defaultEventProps(),
         experiment_id: FLAGS.EXPERIMENT_A_A,
         experiment_variation: testExperimentVariation ? 'treatment' : 'control',
-        space_id: currentSpaceId,
-        organization_id: currentOrganizationId,
       });
     }
 
@@ -81,11 +80,10 @@ const fetchData =
     );
 
     if (inviteCardExperimentEnabled !== null) {
-      Analytics.tracking.experimentStart({
+      tracking.experimentStarted({
+        ...defaultEventProps(),
         experiment_id: FLAGS.EXPERIMENT_NEW_COWORKER_INVITE_CARD,
         experiment_variation: inviteCardExperimentEnabled ? 'treatment' : 'control',
-        space_id: currentSpaceId,
-        organization_id: currentOrganizationId,
       });
     }
 

commit 196122b709f5692067bbb309d3a8c70aa8e503de

fix: `editor_load` Snowplow tracking (#8440)
The tracking got broken in ed98775 where Segment event tracking was
updated to use `_key` instead of `_id` props but this broke the Snowplow
tracking as it's not expected in the schema.

src/javascripts/analytics/Analytics.ts
@@ -1,5 +1,9 @@
 import * as Config from 'Config';
-import segment, { Plan, transformSnowplowToSegmentData } from 'analytics/segment';
+import segment, {
+  Plan,
+  transformSnowplowToSegmentData,
+  transformSegmentToSnowplowData,
+} from 'analytics/segment';
 import * as snowplow from 'analytics/snowplow';
 import stringifySafe from 'json-stringify-safe';
 import { prepareUserData } from 'analytics/UserData';
@@ -155,15 +159,11 @@ export const tracking: Plan = _.mapValues(segment.plan, (planFn, planKey) => (pr
     // Track to Segment by using original plan function:
     planFn(props);
 
-    // Depending on the `data` format we get and the event we're dealing with, we've got to ensure it's
-    // in the right format for Snowplow where we never wrapped { data } while this is done in most
-    // migrated events' Snowplow schemas due to an old tracking bug where data was accidentally wrapped.
-    const likeTransformedData: TransformedEventData = _.isObject(props.data)
-      ? props
-      : { data: props };
+    let likeTransformedData: TransformedEventData | undefined;
     const snowplowSchema = getSnowplowSchemaForEvent(planKey);
 
     if (snowplowSchema) {
+      likeTransformedData = transformSegmentToSnowplowData(props, getBasicPayload().userId);
       snowplow.track(planKey, likeTransformedData);
     }
 

src/javascripts/analytics/analyticsConsoleController.js
@@ -83,7 +83,7 @@ export function add(name, { rawData, transformedSegmentData, transformedSnowplow
   };
 
   const snowplowSchema = getSnowplowSchemaForEvent(name);
-  if (snowplowSchema) {
+  if (snowplowSchema && transformedSnowplowData) {
     event.snowplow = {
       name: snowplowSchema.name,
       version: snowplowSchema.version,

src/javascripts/analytics/segment.ts
@@ -246,3 +246,32 @@ export function transformSnowplowToSegmentData(
   }
   return { payload };
 }
+
+/**
+ * Basic transformer to support transform.ts `migratedLegacyEvent()` events that still get tracked to
+ * snowplow on Segment typewriter calls via `Analytics.tracking.`.
+ *
+ * NOTE: This only takes care of basic default props, no `contexts` and schema specific differences.
+ */
+export function transformSegmentToSnowplowData(
+  props: Record<string, unknown>,
+  userId?: string
+): TransformedEventData {
+  const data = { ...props };
+  if (userId) {
+    data.executing_user_id = userId;
+  }
+  delete data.environment_key; // This wasn't tracked in any Snowplow events.
+
+  const RENAME_PROPS = {
+    space_key: 'space_id',
+    organization_key: 'organization_id',
+  };
+  _.forEach(RENAME_PROPS, (renamed, original) => {
+    if (original in data) {
+      data[renamed] = data[original];
+      delete data[original];
+    }
+  });
+  return { data };
+}

src/javascripts/analytics/segment.spec.ts
@@ -1,7 +1,7 @@
 import { noop } from 'lodash';
 import { getSegmentSchemaForEvent } from './transform';
 import { TransformedEventData, SegmentSchema as Schema } from './types';
-import { transformSnowplowToSegmentData } from './segment';
+import { transformSnowplowToSegmentData, transformSegmentToSnowplowData } from './segment';
 
 let mockAnalytics;
 
@@ -141,6 +141,25 @@ describe('Segment', () => {
         const result = transformSnowplowToSegmentData(genericEvent, complexData, 'env-id-1');
         expect(result).toStrictEqual({ payload: complexData });
       });
+    });
+  });
+});
+
+describe('transformSegmentToSnowplowData()', () => {
+  it('removes environment_key and renames other space/org _key props and adds user ID', () => {
+    const props = {
+      organization_key: 'org-id-1',
+      space_key: 'space-id-1',
+      environment_key: 'env-1',
+      foo: 'bar',
+    };
+    expect(transformSegmentToSnowplowData(props, 'user-id-1')).toStrictEqual({
+      data: {
+        executing_user_id: 'user-id-1',
+        organization_id: 'org-id-1',
+        space_id: 'space-id-1',
+        foo: 'bar',
+      },
     });
   });
 });